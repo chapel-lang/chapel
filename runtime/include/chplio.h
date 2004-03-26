bd98fe5e460d4f922bb56303e6c56e3bc2cf6e39
Brad Chamberlain <bradcray@users.noreply.github.com>
2004-03-26 23:28:57 +0000

    Added support for printing strings (string literals, at least).
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@161 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/include/chplio.h
+++ runtime/include/chplio.h
@@ -5,6 +5,7 @@
 #include "chpltypes.h"
 
 void _write_integer64(FILE* outfile, char* format, _integer64 val);
+void _write_string(FILE* outfile, char* format, _string val);
 
 #endif
 
--- runtime/include/chpltypes.h
+++ runtime/include/chpltypes.h
@@ -2,8 +2,10 @@
 #define _chpltypes_H_
 
 typedef long long _integer64;
+typedef char* _string;
 
-#define _default_format_integer64 " %lld"
+#define _default_format_integer64 "%lld"
+#define _default_format_string "%s"
 
 #endif
 
