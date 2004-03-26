2941f1a605a0dfdefcada868a42354763f5c75cf
Brad Chamberlain <bradcray@users.noreply.github.com>
2004-03-26 23:29:32 +0000

    Oops, I'm getting too fast.  This should've gone with the last checkin.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@164 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/src/chplio.c
+++ runtime/src/chplio.c
@@ -3,3 +3,8 @@
 void _write_integer64(FILE* outfile, char* format, _integer64 val) {
   fprintf(outfile, format, val);
 }
+
+
+void _write_string(FILE* outfile, char* format, _string val) {
+  fprintf(outfile, format, val);
+}
