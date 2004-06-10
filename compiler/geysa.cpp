9b2d4b6d3394ff170f39653ee4b3b7ae8625db2b
Brad Chamberlain <bradcray@users.noreply.github.com>
2004-06-10 21:21:05 +0000

    Fixed up depend rules to work with g++ 3.4 -- used a new trick to get geysa.h's dependences by adding an unused .cpp file that #includes geysa.h.  For future reference, this is because (a) g++ 3.3 (and previous) didn't allow the -M commands to be applied to .h files, (b) because g++ 3.4 created a warning due to the -Wno-invalid-offsetof flag, and (c) it uses a more similar CCDEP command as the rest of our source files (which will make it easier to refactor this when the day finally comes that I get around to it).
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@354 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

---
+++ compiler/geysa.cpp
@@ -0,0 +1,8 @@
+#include "geysa.h"
+
+/* This file is only here for the purpose of generating the header
+files included by geysa.h -- gcc 3.3 and previous wouldn't allow the
+-M options to be applied to a header file */
+
+Dont compile me
+
