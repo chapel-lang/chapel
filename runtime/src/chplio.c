bcb9f416eced94bc510a014a4e43517431b61fb0
Brad Chamberlain <bradcray@users.noreply.github.com>
2004-08-17 01:11:39 +0000

    Improved the testing system so that the -valgrind flag uses valgrind not only to test the compiler, but also the generated binary (when one is created by the test).  This new feature may be suppressed for a particular subdirectory by adding a NOVGRBIN file to the directory in question, which will suppress the use of valgrind on the generated binary.  I added one of these files to John's lowlevelcg directory since valgrinding its binaries is not as important (and, since they use GC, it raises difficulties even if we did want to valgrind it).
    With this change, found a subtle bug in some of the I/O code
    for arrays that I had just checked in, and updated output
    to reflect the fix.
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@604 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/src/chplio.c
+++ runtime/src/chplio.c
@@ -24,13 +24,13 @@ void _write_domain1(FILE* outfile, char* format, _domain1 val) {
 
 void _write_array1_integer64(FILE* outfile, char* format, _array1_integer64 arr) {
   _domain1 dom;
-  int i, j;
+  int i;
 
   dom = *(arr.domain);
 
   for (i=dom.dim[0].lo; i<=dom.dim[0].hi; i+=dom.dim[0].str) {
     fprintf(outfile, format, _ACC1(arr, i));
-    if (j<dom.dim[1].hi) {
+    if (i<dom.dim[0].hi) {
       fprintf(outfile, " ");
     }
   }
