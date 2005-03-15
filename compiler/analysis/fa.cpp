f6fbdd09dc833dc94557717fbc3e08cd0cbb6d82
John Plevyak <jplevyak>
2005-03-15 22:55:40 +0000

    Make handling of coerce more robust, fixes functions/deitz/test_operator_overloading1 Change .future since it now fails in codegen.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@1710 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- compiler/analysis/fa.cpp
+++ compiler/analysis/fa.cpp
@@ -1439,7 +1439,16 @@ add_send_edges_pnode(PNode *p, EntrySet *es) {
       }
       case P_prim_coerce: {
 	assert(p->rvals.v[1]->sym->abstract_type);
-	update_in(result, p->rvals.v[1]->sym->abstract_type);
+	Sym *s = p->rvals.v[1]->sym;
+	AVar *rhs = make_AVar(p->rvals.v[2], es);
+	Vec<CreationSet *> css;
+	forv_CreationSet(cs, *rhs->out)
+	  if (cs->sym == p->rvals.v[1]->sym)
+	    css.set_add(cs);
+	if (css.n)
+	  update_in(result, make_AType(css));
+	else if (s->type->num_kind || s->type == sym_string || s->type->is_symbol)
+	  update_in(result, p->rvals.v[1]->sym->abstract_type);
 	break;
       }
       case P_prim_cast: {
