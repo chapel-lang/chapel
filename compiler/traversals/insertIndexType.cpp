5b642db0c80253affb3d514915fef259674422e9
Roxana Diaconescu <roxana>
2005-04-25 21:50:50 +0000

    Added support for indices to be created for anonymous domains as well. To that effect, modified Domain type to keep track of its initialization expression, so that indices can be created based on this expression.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@2531 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- compiler/traversals/insertIndexType.cpp
+++ compiler/traversals/insertIndexType.cpp
@@ -15,7 +15,7 @@ void InsertIndexType::preProcessType(Type* type) {
     return;
   }
   
-  char* name;
+  char* name = NULL;
   
   if (!(domain_type->parent)){
     name = glomstrings(3, "_index_", intstring(domain_type->numdims), "d");
@@ -24,21 +24,43 @@ void InsertIndexType::preProcessType(Type* type) {
     name = glomstrings(2, "_index_", domain_type->symbol->name);
   }
   
-  IndexType* index_type = dynamic_cast<IndexType*>(domain_type->idxType);
+  if (!domain_type->initExpr){
+    INT_FATAL(domain_type, "Domain has no initialization expression");
+  }
+  
+  IndexType* index_type = 
+  dynamic_cast<IndexType*>(domain_type->idxType);
   if (!index_type){
-    return;
+    //RED This should be an anonymous domain, and thus, arithmetic
+    //It would be more elegant if the domain type would have an init_expr field
+    index_type = new IndexType(domain_type->initExpr);
   } 
 
   Symbol* index_sym = Symboltable::lookupInScope(name, commonModule->modScope);
   if (index_sym) {
-    type = index_sym->type;
+    index_type = (IndexType*)index_sym->type;
+    domain_type->idxType = index_sym->type;
+    type = domain_type;
+    /*** SJD: I couldn't figure out where to put this, but the
+         defaultVal needs to be set for IndexTypes.  I don't know why
+         this was working without this. **/
+         //RED -- this code was never executed -- wonder how it worked with it :)
+         //since type is always domain at this point...
+    //if (TupleType* tuple_type = dynamic_cast<TupleType*>(type->getType())) {
+    if (TupleType* tuple_type = dynamic_cast<TupleType*>(index_type->getType())) {
+      tuple_type->rebuildDefaultVal();
+      index_type->defaultVal = tuple_type->defaultVal->copy();
+      fixup_expr(index_sym->defPoint);
+    }
   } else {
     SymScope* saveScope = Symboltable::setCurrentScope(commonModule->modScope);
     TypeSymbol* index_sym = new TypeSymbol(name, index_type);
     index_type->addSymbol(index_sym);
     DefExpr* def_expr = new DefExpr(index_sym);
     DefStmt* def_stmt = new DefStmt(def_expr);
-    domain_type->idxType = index_type;
+    if (!index_type->defaultVal) {
+      index_type->defaultVal = index_type->idxType->defaultVal->copy();
+    }
     commonModule->stmts->insertBefore(def_stmt);
     Symboltable::setCurrentScope(saveScope);
   }
