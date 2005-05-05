f9b053a2c06ed2e4cd3dedc9eff48f90575ebf91
Steve Deitz <deitz>
2005-05-05 20:17:14 +0000

    Removed comments I accidentally left in the PrintSymtab traversal.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@2661 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- compiler/symtab/printSymtab.cpp
+++ compiler/symtab/printSymtab.cpp
@@ -6,20 +6,20 @@ PrintSymtab::PrintSymtab(FILE* init_outfile) :
 { }
   
 void PrintSymtab::preProcessScope(SymScope* scope) {
-  //  if (!scope->isEmpty()) {
+  if (!scope->isEmpty()) {
     scope->printHeader(outfile, scope->parentLength() * 2);
-    //  }
+  }
 }
 
 void PrintSymtab::postProcessScope(SymScope* scope) {
-  //  if (!scope->isEmpty()) {
+  if (!scope->isEmpty()) {
     scope->printFooter(outfile, scope->parentLength() * 2);
-    //  }
+  }
 }
 
 void PrintSymtab::processSymbol(Symbol* sym) {
-//   for (int i = 0; i < sym->parentScope->parentLength() * 2; i++) {
-//     printf(" ");
-//   }
-//   fprintf(outfile, "%s\n", sym->name);
+  for (int i = 0; i < sym->parentScope->parentLength() * 2; i++) {
+    printf(" ");
+  }
+  fprintf(outfile, "%s\n", sym->name);
 }
