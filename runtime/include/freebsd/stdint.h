20aacdb0fe52b0f364309d4c0d02f8b9ac7afded
Brad Chamberlain <bradcray@users.noreply.github.com>
2005-05-06 19:50:32 +0000

    Did a similar stdint.h hack for freebsd as it didn't seem to support the file either.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@2704 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

---
+++ runtime/include/freebsd/stdint.h
@@ -0,0 +1,9 @@
+#ifndef _FREEBSD_STDINT_H_
+#define _FREEBSD_STDINT_H_
+
+// The existence of this file is a stupid hack because our FreeBSD
+// platform doesn't seem to support stdint.h
+
+#include <inttypes.h>
+
+#endif
