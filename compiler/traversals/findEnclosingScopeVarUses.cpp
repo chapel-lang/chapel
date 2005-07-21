230e545a45262d98af675aa8cdaa139e7415ac1c
Mackale Joyner <mjoyner>
2005-07-21 19:22:44 +0000

    Fixed bug that was counting nested function var uses found inside of nested function definitions as variables that needed to be added to the argument list.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@3705 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- compiler/traversals/findEnclosingScopeVarUses.cpp
+++ compiler/traversals/findEnclosingScopeVarUses.cpp
@@ -9,19 +9,19 @@ FindEnclosingScopeVarUses::FindEnclosingScopeVarUses(SymScope* encl_scope) {
 
 void FindEnclosingScopeVarUses::postProcessExpr(Expr* expr) {
   if (Variable* v = dynamic_cast<Variable*>(expr)) {
-    //variable defined in enclosing function scope
-    Symbol* v_sym = v->var;
-    SymScope* var_sym_scope = v_sym->parentScope;
-    SymScope* encl_scope = _encl_scope;
-    //look up outer scopes until module scope is reached or symbol is found
-    do {
-      if (var_sym_scope == encl_scope) {
-        _var_uses_vec->add_exclusive(v_sym);
-        return;
-      }
-      encl_scope = encl_scope->parent;
-    }while (encl_scope->type != SCOPE_MODULE);
-      
+    if (VarSymbol* v_sym = dynamic_cast<VarSymbol*>(v->var)) {
+      //variable defined in enclosing function scope
+      SymScope* var_sym_scope = v_sym->parentScope;
+      SymScope* encl_scope = _encl_scope;
+      //look up outer scopes until module scope is reached or symbol is found
+      do {
+        if (var_sym_scope == encl_scope) {
+          _var_uses_vec->add_exclusive(v_sym);
+          return;
+        }
+        encl_scope = encl_scope->parent;
+      }while (encl_scope->type != SCOPE_MODULE);
+    }
   }
 }
 
