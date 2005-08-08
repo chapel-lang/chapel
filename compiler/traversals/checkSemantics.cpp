a8d0a518722ad4aeeeca199b82eb000fa72fa122
Steve Deitz <deitz>
2005-08-08 23:39:28 +0000

    Added an error message to flag uninitialized params in functions.
    This fixes regression trivial/roxana/test_param_decl3.
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@3904 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- compiler/traversals/checkSemantics.cpp
+++ compiler/traversals/checkSemantics.cpp
@@ -1,5 +1,6 @@
 #include "checkSemantics.h"
 #include "expr.h"
+#include "symscope.h"
 
 
 void CheckSemantics::preProcessExpr(Expr* expr) {
@@ -15,6 +16,10 @@ void CheckSemantics::preProcessExpr(Expr* expr) {
     if (defExpr->sym->isParam()) {
       if (defExpr->init && !dynamic_cast<IntLiteral*>(defExpr->init)) {
         USR_FATAL(defExpr, "Initializing parameter to a variable expression.");
+      }
+      if (!defExpr->init && dynamic_cast<FnSymbol*>(defExpr->parentSymbol) &&
+          defExpr->parentScope->type != SCOPE_PARAM) {
+        USR_FATAL(defExpr, "Top-level params must be initialized.");
       }
     }
   }
