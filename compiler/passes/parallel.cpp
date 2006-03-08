62e04f11c5b8a449268f9566cbfc7d13f54402aa
Wayne Wong <waynew>
2006-03-08 22:28:27 +0000

    Tweaked cobegin() to use for_alist instead of getElements to traverse statements. Hopefully, replaced tabs with spaces.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@5618 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- compiler/passes/parallel.cpp
+++ compiler/passes/parallel.cpp
@@ -5,7 +5,6 @@
 #include "astutil.h"
 #include "expr.h"
 #include "passes.h"
-//#include "runtime.h"
 #include "stmt.h"
 #include "symbol.h"
 #include "symtab.h"
@@ -16,7 +15,6 @@
 void 
 cobegin () {
   static int ufid = 1;
-  // flag to enable pass?
 
   forv_Vec (ModuleSymbol, mod, allModules) {
     Vec<BaseAST*> asts;
@@ -24,16 +22,14 @@ cobegin () {
     forv_Vec (BaseAST, ast, asts) {
       if (BlockStmt *b=dynamic_cast<BlockStmt*>(ast)) {
 	if (BLOCK_COBEGIN == b->blockType) {
-	  Vec<Stmt*> bstmts;
-	  b->body->getElements (bstmts);
-	  forv_Stmt (stmt, bstmts) {
+          for_alist (Stmt, stmt, b->body) {
 	    char *fname = stringcat ("_cobegin_stmt", intstring (ufid++));
 	    FnSymbol *fn = new FnSymbol (fname, NULL, new AList<DefExpr>(), dtVoid);
+	    stmt->insertBefore (new CallExpr (fname)); // in block add call to fn
 	    stmt->remove ();
 	    fn->insertAtTail (stmt);		// move stmt to new function
-	    b->insertBefore (new DefExpr (fn)); // add def to before cobegin blk
-	    b->insertAtTail (new CallExpr (fname)); // in block add call to fn
-	  }
+	    b->insertBefore (new DefExpr (fn));	// add def to before cobegin blk
+          }
 	}
       }
     }
@@ -44,7 +40,7 @@ cobegin () {
 void
 parallel (void) {
   if (parallelPass) {
-    cobegin ();		// move cobegin stmts to within a function
+    cobegin ();			// move cobegin stmts to within a function
   }
 }
 
