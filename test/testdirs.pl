e26a0ae70d4f6ef8d766f31d38acb97300fcc1d1
Wayne Wong <waynew>
2006-03-10 19:39:37 +0000

    Handles blanks lines in input file.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@5670 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- test/testdirs.pl
+++ test/testdirs.pl
@@ -7,19 +7,26 @@ $SUCCESS_INDEX = 4;
 $FAILURE_INDEX = 8;
 $FUTURES_INDEX = 12;
 
-if ($#ARGV > 0) {	# if more than one arg
+
+sub collect_logs {
+    local ($dir) = @_;
+}
+
+
+if ($#ARGV > 0) {       # if more than one arg
     print STDERR "usage: testdirs.pl [dirfile]\n";
     exit (1);
-} else {		# else, either 0 or 1 arg
+} else {                # else, either 0 or 1 arg
     if (0 > $#ARGV) {
-	push @testdir_list, ".";
+        push @testdir_list, ".";
     } else {
-	$dirfile = $ARGV[0];
-	open dirfile or die "Cannot open directory file '$dirfile'\n";
-	while (<dirfile>) {
-	    chomp;
-	    push @testdir_list, $_;
-	}
+        $dirfile = $ARGV[0];
+        open dirfile or die "Cannot open directory file '$dirfile'\n";
+        while (<dirfile>) {
+            next if /^$/;
+            chomp;
+            push @testdir_list, $_;
+        }
     }
 }
 
@@ -40,19 +47,19 @@ if (-e $tmplogfile) {
 $goods = 0;
 $bads = 0;
 $todos = 0;
-foreach $testdir (@testdir_list) {	# run tests on each dir
+foreach $testdir (@testdir_list) {      # run tests on each dir
     system ("./start_test -startdir $testdir");
     if (-e $logfile) {
-	$summary = `grep "^\\[Summary" $logfile`;
-	system ("echo [Test directory $testdir] >> $tmplogfile");
-	system ("cat $logfile >> $tmplogfile");
-	chomp $summary;
-	@ssumm = split (/[\s]|\[|\]/, $summary);
-	$goods += $ssumm[$SUCCESS_INDEX];
-	$bads += $ssumm[$FAILURE_INDEX];
-	$todos += $ssumm[$FUTURES_INDEX];
+        $summary = `grep "^\\[Summary" $logfile`;
+        system ("echo [Test directory $testdir] >> $tmplogfile");
+        system ("cat $logfile >> $tmplogfile");
+        chomp $summary;
+        @ssumm = split (/[\s]|\[|\]/, $summary);
+        $goods += $ssumm[$SUCCESS_INDEX];
+        $bads += $ssumm[$FAILURE_INDEX];
+        $todos += $ssumm[$FUTURES_INDEX];
     } else {
-	print STDERR "warning: no logfile for test directory '$testdir'\n";
+        print STDERR "warning: no logfile for test directory '$testdir'\n";
     }
 }
 
@@ -62,9 +69,9 @@ print "\n$results\n";
 $eresults = "\\[Summary: \\#Successes = $goods \\| \\#Failures = $bads \\| \\#Futures = $todos\\]";
 
 unlink $logfile;
-system ("mv $tmplogfile $logfile");	# copy temp log back to orig name
-system ("echo $eresults >> $logfile");	# add summary
+system ("mv $tmplogfile $logfile");     # copy temp log back to orig name
+system ("echo $eresults >> $logfile");  # add summary
 
 system ("grep \"^\\[Error\" $logfile > $logfile.summary");
 system ("grep \"^Future\" $logfile >> $logfile.summary");
-system ("echo $eresults >> $logfile.summary");	# add summary
+system ("echo $eresults >> $logfile.summary");  # add summary
