b08da4d53ac8a680e3ddc08b73d0c5a90b9edbd9
Steve Deitz <deitz>
2006-03-16 18:50:37 +0000

    Removed hack that kept functions from being added to the visible function list during analysis.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@5759 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- compiler/symtab/symscope.cpp
+++ compiler/symtab/symscope.cpp
@@ -222,8 +222,6 @@ void SymScope::codegenFunctions(FILE* outfile) {
 void SymScope::addVisibleFunction(FnSymbol* fn) {
   if (!fn->visible)
     return;
-  if (inAnalysis)
-    return;
   int is_setter = (fn->name[0] == '=' && !OPERATOR_CHAR(fn->name[1]) &&
                    fn->name[1] != '\0');
   char* n = cannonicalize_string(fn->name + (is_setter ? 1 : 0));
