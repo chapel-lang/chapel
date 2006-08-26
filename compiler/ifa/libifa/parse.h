448f842e8a1c66811465da5f3587ff72a847802d
Brad Chamberlain <bradcray@users.noreply.github.com>
2006-08-26 00:04:19 +0000

    Killed libifa/ code which had escaped my notice before in cleaning up the ifa/ directory.  (Note that I don't believe this code was ever counted in our nightly code size check, so this was never accounted for and we won't see any further shrinkage on the graphs due to this check-in).
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@7080 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- compiler/ifa/libifa/parse.h
+++
@@ -1,8 +0,0 @@
-/* -*-Mode: c++;-*-
-*/
-#ifndef _parse_H_
-#define _parse_H_
-
-int compile_one_file(char *fn);
-
-#endif
--- compiler/ifa/libifa/version.h
+++
@@ -1,6 +0,0 @@
-#ifndef _version_H_
-#define _version_H_
-
-void ifa_version(char *version);
-
-#endif
