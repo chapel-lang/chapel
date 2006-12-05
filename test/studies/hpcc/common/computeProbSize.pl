d2d8d7a308e6e13a736f4ae69f4b899f4f37f505
Brad Chamberlain <bradcray@users.noreply.github.com>
2006-12-05 21:54:11 +0000

    Mdae the testProbSize.chpl test compute a .good file using the output of the countMemory test, since the output is machine-dependent based on the amount of memory, yet we don't want to have to maintain two memory-related tests on a platform- by-platform basis.
    I've tested this on 4 platforms and it seems to
    be working.  Let me know if you find that it
    doesn't for another machine.
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@8644 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

---
+++ test/studies/hpcc/common/computeProbSize.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+use integer;
+
+$memoryInBytes = @ARGV[0];
+
+$memoryTarget = $memoryInBytes / 4;
+
+$streamProbSize = $memoryTarget / (3*8);
+$raProbSize = $memoryTarget / (1*8);
+$fftProbSize = $memoryTarget / (2*8);
+
+printf "For default settings:\n";
+printf "  Stream = $streamProbSize\n";
+printf "  RA     = $raProbSize\n";
+printf "  FFT    = $fftProbSize\n";
+printf "\n";
+
+$streamPow2ProbSize = 0;
+$raPow2ProbSize = 0;
+$fftPow2ProbSize = 0;
+$pow2ProbSize = 1;
+while (($streamPow2ProbSize == 0) || ($raPow2ProbSize == 0) || ($fftPow2ProbSize == 0)) {
+    $pow2ProbSize *= 2;
+    if (($pow2ProbSize*3*8 > $memoryTarget) && ($streamPow2ProbSize == 0)) {
+        $streamPow2ProbSize = $pow2ProbSize;
+    }
+    if (($pow2ProbSize*1*8 > $memoryTarget) && ($raPow2ProbSize == 0)) {
+        $raPow2ProbSize = $pow2ProbSize;
+    }
+    if (($pow2ProbSize*2*8 > $memoryTarget) && ($fftPow2ProbSize == 0)) {
+        $fftPow2ProbSize = $pow2ProbSize;
+    }
+}
+
+printf "For 2**k case:\n";
+printf "  Stream = $streamPow2ProbSize\n";
+printf "  RA     = $raPow2ProbSize\n";
+printf "  FFT    = $fftPow2ProbSize\n";
+
