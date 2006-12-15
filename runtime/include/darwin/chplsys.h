ba4eeccc4ff0a8549219757b6d9420c9fa4b7e94
Steve Deitz <deitz>
2006-12-15 02:32:31 +0000

    Disabled bytesPerLocale on Mac OS X to get the runtime to compile.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@8823 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

---
+++ runtime/include/darwin/chplsys.h
@@ -0,0 +1,8 @@
+#ifndef _DARWIN_CHPLSYS_H_
+#define _DARWIN_CHPLSYS_H_
+
+#include "../chplsys.h"
+
+#define NO_BYTES_PER_LOCALE
+
+#endif
--- runtime/src/chplsys.c
+++ runtime/src/chplsys.c
@@ -2,14 +2,20 @@
 #include <unistd.h>
 #include "chplrt.h"
 #include "chplsys.h"
+#include "error.h"
 
 #ifndef chplGetPageSize
 #define chplGetPageSize() sysconf(_SC_PAGE_SIZE)
 #endif
 
 _uint64 _bytesPerLocale(void) {
+#ifdef NO_BYTES_PER_LOCALE
+  printInternalError("sorry- bytesPerLocale not supported on this platform");
+  return 0;
+#else
   _uint64 numPages = sysconf(_SC_PHYS_PAGES);
   _uint64 pageSize = chplGetPageSize();
   _uint64 total = numPages * pageSize;
   return total;
+#endif
 }
