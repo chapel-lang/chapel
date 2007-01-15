4ffe99db8724f7b25b08b7a8552ccf071ffcda68
Steve Deitz <deitz>
2007-01-15 22:03:52 +0000

    Moved cullOverReferences from after inlining to before parallel2 and inlining.  This decreases time spent culling excess references by 64% (0.181 to 0.066) and better organizes the pass list into categories. Added some documentation to the passlist file.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@8960 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- compiler/main/passlist.h
+++ compiler/main/passlist.h
@@ -1,50 +1,62 @@
 #ifndef _PASSLIST_H_
 #define _PASSLIST_H_
 
-void parse();
-void check_parsed();
-void parallel1();
-void cleanup();
-void scopeResolve();
-void normalize();
-void check_normalized();
+//
+// prototypes of functions that are called as passes (alphabetical)
+//
 void build_default_functions();
-void resolve();
+void check_normalized();
+void check_parsed();
 void check_resolved();
-void flattenFunctions();
-void parallel2();
-void inlineFunctions();
-void cullOverReferences();
-void copyPropagation();
-void memoryManage();
+void cleanup();
 void codegen();
+void copyPropagation();
+void cullOverReferences();
+void flattenFunctions();
+void inlineFunctions();
+void memoryManage();
+void normalize();
+void parallel1();
+void parallel2();
+void parse();
+void resolve();
+void scopeResolve();
 
+//
+// passlist: contains passes in the order that they are called
+//
 PassInfo passlist[] = {
   FIRST,
-  RUN(parse),              // parse files and create AST
-  RUN(check_parsed),       // checks semantics of parsed AST
+  // Chapel to AST
+  RUN(parse),        // parse files and create AST
+  RUN(check_parsed), // checks semantics of parsed AST
 
-  RUN(parallel1),          // parallel transforms, part 1
+  // Scope resolution and normalization
+  RUN(parallel1),        // parallel transforms, part 1
+  RUN(cleanup),          // post parsing transformations
+  RUN(scopeResolve),     // resolve symbols by scope
+  RUN(normalize),        // normalization transformations
+  RUN(check_normalized), // check semantics of normalized AST
 
-  RUN(cleanup),            // post parsing transformations
-  RUN(scopeResolve),       // resolve symbols by scope
-  RUN(normalize),          // normalization transformations
-  RUN(check_normalized),   // check semantics of normalized AST
-
+  // Creation of default functions
   RUN(build_default_functions), // build default functions
 
-  RUN(resolve),            // resolves function calls and types
-  RUN(check_resolved),     // checks semantics of resolved AST
+  // Function resolution and shallow type inference
+  RUN(resolve),        // resolves function calls and types
+  RUN(check_resolved), // checks semantics of resolved AST
 
+  // Post-resolution cleanup
   RUN(flattenFunctions),   // denest nested functions
+  RUN(cullOverReferences), // remove excess references
   RUN(parallel2),          // parallel transforms, part 2
-  RUN(inlineFunctions),    // function inlining
 
-  RUN(cullOverReferences),
+  // Optimizations
+  RUN(inlineFunctions), // function inlining
+  RUN(copyPropagation), // copy propagation and other low-level optimizations
+  RUN(memoryManage),    // insert memory frees, garbage collection
 
-  RUN(copyPropagation),
-  RUN(memoryManage),        // insert memory frees, garbage collection
-  RUN(codegen),
+  // AST to C
+  RUN(codegen), // generate C code and build it
   LAST
 };
 
