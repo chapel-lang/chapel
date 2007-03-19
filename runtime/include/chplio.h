bcfc9d1d13f8ccfc56e5891eee9b6a68ce3d5fe0
Brad Chamberlain <bradcray@users.noreply.github.com>
2007-03-19 17:56:35 +0000

    Removed some dead code from the runtime relating to how scalars are formatted in I/O and how config vars/consts are parsed from the command-line.  This has been dead for some time, just noticed it.  There's likely more to do here, but I was just grabbing at some low-hanging fruit while working on CG.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@9476 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/include/chplio.h
+++ runtime/include/chplio.h
@@ -4,35 +4,7 @@
 #include <stdio.h>
 #include "chpltypes.h"
 
-#define _default_format_write_bool        "%s"
-#define _default_format_read_bool         "%s"
-
-#define _default_format_read_int32        "%d"
-#define _default_format_read_int64        "%lld" /* long long int */
-
-#define _default_format_write_int8        "%d"
-#define _default_format_write_int16       "%d"
-#define _default_format_write_int32       "%d"
-#define _default_format_write_int64       "%lld" /* long long int */
-
-#define _default_format_read_uint32       "%u"
-#define _default_format_read_uint64       "%llu"
-
-#define _default_format_write_uint8       "%u"
-#define _default_format_write_uint16      "%u"
-#define _default_format_write_uint32      "%u"
-#define _default_format_write_uint64      "%llu"
-
-#define _default_format_read_real64      "%lg"  /* double */
-
-#define _default_format_write_real32     "%lg"
-#define _default_format_write_real64     "%lg"  /* double */
-#define _default_format_write_imag32     "%lgi"
-#define _default_format_write_imag64     "%lgi"  /* double */
-
-#define _default_format_read_complex64    "%g + %g%c"
-#define _default_format_read_complex128   "%lg + %lg%c"
-#define _default_format_read_complex256   "%llg + %llg%c"
+#define _default_format_read_int64        "%lld"
 
 extern char* _default_format_write_complex64;
 extern char* _default_format_write_complex128;
--- runtime/include/config.h
+++ runtime/include/config.h
@@ -16,14 +16,6 @@ void printConfigVarTable(void);
 void initSetValue(char* varName, char* value, char* moduleName);
 char* lookupSetValue(char* varName, char* moduleName);
 void installConfigVar(char* varName, char* value, char* moduleName);
-int setInCommandLine_int32(char* varName, _int32* value, char* moduleName);
-int setInCommandLine_uint32(char* varName, _uint32* value, char* moduleName);
-int setInCommandLine_int64(char* varName, _int64* value, char* moduleName);
-int setInCommandLine_uint64(char* varName, _uint64* value, char* moduleName);
-int setInCommandLine_real64(char* varName, _real64* value, char* moduleName);
-int setInCommandLine_bool(char* varName, _bool* value, char* moduleName);
-int setInCommandLine_string(char* varName, _string* value, char* moduleName);
-int setInCommandLine_complex128( char* varName, _complex128* value, char* moduleName);
 
 #endif
 
