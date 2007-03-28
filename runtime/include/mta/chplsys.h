cd7a0cc98de309a970c34fd5d65cd73a9b0bc17a
Brad Chamberlain <bradcray@users.noreply.github.com>
2007-03-28 18:21:49 +0000

    Added some initial support for building Chapel for the MTA including initial support for cross-compilation:
    Cross Compiling
    ---------------
    To build the compiler to run on one platform
    (say the linux64 front-end to an MTA), but
    have the runtime and compiler-generated code
    get built for another, set an additional
    variable CHPL_CROSS_PLATFORM which specifies
    the platform from which you'll be cross-
    compiling.  For example, on linux-ion, you
    would set:
    
    setenv CHPL_PLATFORM mta
    setenv CHPL_CROSS_PLATFORM linux64
    
    You will also likely want to make your path
    refer to the cross-compiler version of the
    chpl compiler:
    
    set path = ($CHPL_HOME/bin/$CHPL_CROSS_PLATFORM $path)
    
    
    MTA-specific changes
    --------------------
    Added initial MTA Makefile settings file as well
    as some mta-specific runtime header files (e.g.,
    the MTA does not yet support stdint.h so I've set
    up some of the features we rely on from it in a
    spoofed file there.  More will need to be added,
    I'm sure).
    
    Changed some Makefile.include syntax to be more
    MTA-compatible (though we're using gmake ?!) and
    consistent with how we invoke the util/platform
    script in Makefile.platform.
    
    
    Misc
    ----
    Added a "printplatform" target to Makefile.base
    so that from within any other directory that
    supports a Makefile, you can do "make printplatform"
    to see what that directory and its Makefile think
    the platform is.  When cross-compiling, for example,
    "make printplatform" in the compiler directory
    should print your cross-compiling platform whereas
    the runtime and generated code directories should
    print the target platform.
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@9585 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

---
+++ runtime/include/mta/chplsys.h
@@ -0,0 +1,7 @@
+#ifndef _MTA_CHPL_SYS_H_
+#define _MTA_CHPL_SYS_H_
+
+#define NO_BYTES_PER_LOCALE
+#include "../chplsys.h"
+
+#endif
---
+++ runtime/include/mta/stdint.h
@@ -0,0 +1,8 @@
+#ifndef _MTA_STDINT_H_
+#define _MTA_STDINT_H_
+
+typedef unsigned long intptr_t;
+#define INT32_MAX (1<<31)
+#define INT64_MAX (1<<63)
+
+#endif
