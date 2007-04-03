dd38d9c40071271c00a4c6ab1c4fdc10e591f100
Steve Deitz <deitz>
2007-04-03 18:33:48 +0000

    Specialized the iterator interface methods for iterators that have a stylized form consisting of a single for loop with a yield statement in that loop.
    The function isSingleLoopIterator determines whether an iterator has
    this form by checking the following conditions:
    
      1. There is exactly one for-loop and no other loops.
      2. The single for-loop is top-level to the function.
      3. There is exactly one yield.
      4. The single yield is top-level to the for-loop.
      5. There are no goto statements.
    
    The single loop iterator has the following form:
    
      iterator foo() {
        BLOCK I
        for loop {
          BLOCK II
          yield statement
          BLOCK III
        }
        BLOCK IV
      }
    
    The getHeadCursor and getNextCursor methods are specialized to
    simplify the control flow that would be created using the general
    transformation.  The getHeadCursor method contains blocks I, II, and
    IV where II and IV are guarded in a conditional.  The getNextCursor
    method contains blocks III, II, and IV where II and IV are guarded in
    a conditional.
    
    Performance Results
    -------------------
    
    RA (Problem size = 2**25)
    C                         22.013
    CHPL before this change   33.432  51.9% overhead
    CHPL                      25.849  17.4% overhead
    
    RA (Problem size = 2**20)
    C                          0.490
    CHPL before this change    0.774  58.0% overhead
    CHPL                       0.617  25.9% overhead
    
    STREAM (Problem size = 11051520)
    CHPL no promotion version  1.526
    CHPL before this change    8.058  428% overhead
    CHPL                       7.819  412% overhead
    
    These tests show the expected gain on RA.  This optimization does not
    help STREAM nearly as much.  That will require different changes.
    
    We don't yet have a C version of STREAM to test against.  My guess,
    based on the test_for2d performance issue, is that even the no
    promotion version shows some significant overhead.
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@9656 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- compiler/AST/iterator.cpp
+++ compiler/AST/iterator.cpp
@@ -181,6 +181,209 @@ buildGetValue(FnSymbol* fn, Symbol* value) {
 }
 
 
+//
+// Determines that an iterator has a single loop with a single yield
+// in it by checking the following conditions:
+//
+//   1. There is exactly one for-loop and no other loops.
+//   2. The single for-loop is top-level to the function.
+//   3. There is exactly one yield.
+//   4. The single yield is top-level to the for-loop.
+//   5. There are no goto statements.
+//
+// I believe these conditions can be relaxed.
+//
+static CallExpr*
+isSingleLoopIterator(FnSymbol* fn, Vec<BaseAST*>& asts) {
+  BlockStmt* singleFor = NULL;
+  CallExpr* singleYield = NULL;
+  forv_Vec(BaseAST, ast, asts) {
+    if (CallExpr* call = dynamic_cast<CallExpr*>(ast)) {
+      if (call->isPrimitive(PRIMITIVE_YIELD)) {
+        if (singleYield) {
+          return NULL;
+        } else if (BlockStmt* block = dynamic_cast<BlockStmt*>(call->parentExpr)) {
+          if (block->loopInfo && block->loopInfo->isPrimitive(PRIMITIVE_LOOP_FOR)) {
+            singleYield = call;
+          } else {
+            return NULL;
+          }
+        } else {
+          return NULL;
+        }
+      }
+    } else if (BlockStmt* block = dynamic_cast<BlockStmt*>(ast)) {
+      if (block->loopInfo) {
+        if (singleFor) {
+          return NULL;
+        } else if (block->loopInfo->isPrimitive(PRIMITIVE_LOOP_FOR) &&
+                   block->parentExpr == fn->body) {
+          singleFor = block;
+        } else {
+          return NULL;
+        }
+      }
+    } else if (ast->astType == STMT_GOTO) {
+      return NULL;
+    }
+  }
+  if (singleFor && singleYield)
+    return singleYield;
+  else
+    return NULL;
+}
+
+
+//
+// Builds the iterator interface methods for a single loop iterator as
+// determined by isSingleLoopIterator.
+//
+// A single loop iterator has the form:
+//
+//  iterator foo() {
+//    BLOCK I
+//    for loop {
+//      BLOCK II
+//      yield statement
+//      BLOCK III
+//    }
+//    BLOCK IV
+//  }
+//
+static void
+buildSingleLoopMethods(FnSymbol* fn,
+                       Vec<BaseAST*>& asts,
+                       Map<Symbol*,Symbol*>& local2field,
+                       Vec<Symbol*>& locals,
+                       Symbol* value,
+                       CallExpr* yield) {
+  IteratorInfo* ii = fn->iteratorInfo;
+  BlockStmt* loop = dynamic_cast<BlockStmt*>(yield->parentExpr);
+
+  Symbol* headIterator = ii->getHeadCursor->getFormal(1);
+  Symbol* headCursor = newTemp(ii->getHeadCursor, ii->getHeadCursor->retType);
+  Symbol* nextIterator = ii->getNextCursor->getFormal(1);
+  Symbol* nextCursor = ii->getNextCursor->getFormal(2);
+
+  ASTMap headCopyMap; // copy map of iterator to getHeadCursor; note:
+                      // there is no map for getNextCursor since the
+                      // asts are moved (not copied) to getNextCursor
+
+  //
+  // add local variable defs to getNextCursor and getHeadCursor
+  //
+  forv_Vec(BaseAST, ast, asts) {
+    if (DefExpr* def = dynamic_cast<DefExpr*>(ast)) {
+      if (dynamic_cast<ArgSymbol*>(def->sym))
+        continue;
+      ii->getHeadCursor->insertAtHead(def->copy(&headCopyMap));
+      ii->getNextCursor->insertAtHead(def->remove());
+    }
+  }
+
+  //
+  // add BLOCK I to getHeadCursor method
+  //
+  for_alist(Expr, expr, fn->body->body) {
+    if (expr == loop)
+      break;
+    ii->getHeadCursor->insertAtTail(expr->copy(&headCopyMap));
+    expr->remove();
+  }
+
+  //
+  // add BLOCK III to getNextCursor method
+  //
+  bool postYield = false;
+  for_alist(Expr, expr, loop->body) {
+    if (!postYield) {
+      if (expr == yield)
+        postYield = true;
+      continue;
+    }
+    ii->getNextCursor->insertAtTail(expr->remove());
+  }
+
+  //
+  // add BLOCK II to conditional then clause for both getHeadCursor and
+  // getNextCursor methods; set cursor to 1
+  //
+  BlockStmt* headThen = new BlockStmt();
+  BlockStmt* nextThen = new BlockStmt();
+  for_alist(Expr, expr, loop->body) {
+    if (expr == yield)
+      break;
+    headThen->insertAtTail(expr->copy(&headCopyMap));
+    nextThen->insertAtTail(expr->remove());
+  }
+  headThen->insertAtTail(new CallExpr(PRIMITIVE_MOVE, headCursor, new_IntSymbol(1)));
+  nextThen->insertAtTail(new CallExpr(PRIMITIVE_MOVE, nextCursor, new_IntSymbol(1)));
+
+  //
+  // add BLOCK IV to conditional else clause for both getHeadCursor and
+  // getNextCursor methods; set cursor to 0
+  //
+  BlockStmt* headElse = new BlockStmt();
+  BlockStmt* nextElse = new BlockStmt();
+  loop->remove();
+  for_alist(Expr, expr, fn->body->body) {
+    if (!expr->next) // ignore return statement
+      break;
+    headElse->insertAtTail(expr->copy(&headCopyMap));
+    nextElse->insertAtTail(expr->remove());
+  }
+  headElse->insertAtTail(new CallExpr(PRIMITIVE_MOVE, headCursor, new_IntSymbol(0)));
+  nextElse->insertAtTail(new CallExpr(PRIMITIVE_MOVE, nextCursor, new_IntSymbol(0)));
+
+  //
+  // add conditional to getHeadCursor and getNextCursor methods
+  //
+  Expr* headCond = loop->loopInfo->get(1)->copy(&headCopyMap);
+  Expr* nextCond = loop->loopInfo->get(1)->remove();
+  ii->getHeadCursor->insertAtTail(new CondStmt(headCond, headThen, headElse));
+  ii->getNextCursor->insertAtTail(new CondStmt(nextCond, nextThen, nextElse));
+  ii->getHeadCursor->insertAtTail(new CallExpr(PRIMITIVE_RETURN, headCursor));
+  ii->getNextCursor->insertAtTail(new CallExpr(PRIMITIVE_RETURN, nextCursor));
+
+  // load local variables from fields at return points and update
+  // fields when local variables change
+  forv_Vec(Symbol, local, locals) {
+    Symbol* field = local2field.get(local);
+    if (dynamic_cast<ArgSymbol*>(local)) {
+      Symbol* newlocal = newTemp(ii->getNextCursor, local->type, local->name);
+      ASTMap map;
+      map.put(local, newlocal);
+      update_symbols(ii->getNextCursor, &map);
+
+      Symbol* newlocal2 = newTemp(ii->getHeadCursor, local->type, local->name);
+      {
+        ASTMap map;
+        map.put(local, newlocal2);
+        update_symbols(ii->getHeadCursor, &map);
+      }
+      local = newlocal;
+      headCopyMap.put(newlocal, newlocal2);
+    }
+    ii->getHeadCursor->insertAtHead(new CallExpr(PRIMITIVE_MOVE, headCopyMap.get(local), new CallExpr(PRIMITIVE_GET_MEMBER, headIterator, field)));
+    ii->getNextCursor->insertAtHead(new CallExpr(PRIMITIVE_MOVE, local, new CallExpr(PRIMITIVE_GET_MEMBER, nextIterator, field)));
+    forv_Vec(SymExpr, se, local->defs) {
+      if (CallExpr* parent = dynamic_cast<CallExpr*>(se->parentExpr))
+        parent->getStmtExpr()->insertAfter(
+          new CallExpr(PRIMITIVE_SET_MEMBER, nextIterator, field,
+            new SymExpr(local)));
+      if ((se = dynamic_cast<SymExpr*>(headCopyMap.get(se))))
+        if (CallExpr* parent = dynamic_cast<CallExpr*>(se->parentExpr))
+          parent->getStmtExpr()->insertAfter(
+            new CallExpr(PRIMITIVE_SET_MEMBER, headIterator, field,
+              new SymExpr(dynamic_cast<Symbol*>(headCopyMap.get(local)))));
+    }
+  }
+
+  buildIsValidCursor(fn);
+  buildGetValue(fn, value);
+}
+
+
 void lowerIterator(FnSymbol* fn) {
   IteratorInfo* ii = fn->iteratorInfo;
 
@@ -212,12 +415,19 @@ void lowerIterator(FnSymbol* fn) {
   }
 
   Symbol* value = local2field.get(fn->getReturnSymbol());
-  buildGetNextCursor(fn, asts, local2field, locals);
-  buildGetHeadCursor(fn);
-  buildIsValidCursor(fn);
-  buildGetValue(fn, value);
+  if (CallExpr* yield = isSingleLoopIterator(fn, asts)) {
+    buildSingleLoopMethods(fn, asts, local2field, locals, value, yield);
+  } else {
+    buildGetNextCursor(fn, asts, local2field, locals);
+    buildGetHeadCursor(fn);
+    buildIsValidCursor(fn);
+    buildGetValue(fn, value);
+  }
 
   // rebuild iterator function
+
+  for_alist(Expr, expr, fn->body->body)
+    expr->remove();
   fn->defPoint->remove();
   fn->retType = ii->classType;
   Symbol* t1 = newTemp(fn, ii->classType);
