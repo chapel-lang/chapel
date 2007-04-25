012fb0c7fee22d28b0b539cbcf68495e79729eab
Brad Chamberlain <bradcray@users.noreply.github.com>
2007-04-25 00:31:59 +0000

    Fixing an uninitialized variable that breaks the build when compiling with OPTIMIZE=1.  Looking for a gcc flag setting that will show these warnings when not compiling -O3.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@9924 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- compiler/passes/lowerIterators.cpp
+++ compiler/passes/lowerIterators.cpp
@@ -17,7 +17,7 @@ expandIteratorInline(CallExpr* call) {
   FnSymbol* iterator = ic->type->defaultConstructor;
   ASTMap map;
   BlockStmt* ibody = iterator->body->copy(&map);
-  CallExpr* yield;
+  CallExpr* yield = NULL;
   body = dynamic_cast<BlockStmt*>(call->parentExpr);
 call->remove();
 body->replace(ibody);
