e420c5b1122db0f67e2f5749599acdc82c12420e
Steve Deitz <deitz>
2007-06-25 22:17:33 +0000

    Fixed a bug in which the returned symbol in a var iterator was omitted from the iterator class (because it is a reference) when the --no-live-variables-iterator-class-opt flag is used.  This change makes it so that the returned symbol is never omitted.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@10621 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- compiler/AST/iterator.cpp
+++ compiler/AST/iterator.cpp
@@ -797,10 +797,11 @@ addLocalVariables(Vec<Symbol*>& syms, FnSymbol* fn) {
     forv_Vec(Expr, expr, bb->exprs) {
       if (DefExpr* def = dynamic_cast<DefExpr*>(expr)) {
         if (dynamic_cast<VarSymbol*>(def->sym)) {
-          if (def->sym->type->refType) { // references are short-lived
-                                         // and should never need to be
-                                         // stored in an iterator class
-                                         // (hopefully)
+          // references are short-lived and should never need to be
+          // stored in an iterator class (hopefully) except for the
+          // returned reference in the case of var iterators
+          if (!isReference(def->sym->type) ||
+              def->sym == fn->getReturnSymbol()) {
             syms.add(def->sym);
           }
         }
