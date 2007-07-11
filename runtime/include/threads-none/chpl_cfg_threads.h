e861270cd14efc0b37d5cd2983530c1332a22847
Steve Deitz <deitz>
2007-07-11 20:48:58 +0000

    Added initial support for setting your threads package to none by setting CHPL_THREADS to none.  This has the same semantic effect as compiling with the --serial flag.  Note that -lpthread is still linked into the executable since this is added to the libray flags in the compiler.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@10908 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

---
+++ runtime/include/threads-none/chpl_cfg_threads.h
@@ -0,0 +1,22 @@
+#ifndef _chpl_cfg_threads_h_
+#define _chpl_cfg_threads_h_
+
+// bogus mutex type (no mutexes)
+typedef int _chpl_mutex_t;
+typedef _chpl_mutex_t *_chpl_mutex_p;
+
+// bogus condition variable type (no condition variables)
+typedef int _chpl_condvar_t;
+typedef _chpl_condvar_t *_chpl_condvar_p;
+
+// thread-related
+typedef void* (*_chpl_threadfp_t)(void*);  // function pointer
+typedef void* _chpl_threadarg_t;           // function argument
+typedef int _chpl_thread_attr_t;           // bogus thread attributes
+typedef int _chpl_thread_t;                // bogus thread handle
+typedef struct {                           // temporary work space
+  _chpl_thread_t thread;                   // thread handle for join/wait
+  int            error;                    // to store fork error code
+} _chpl_cobegin_wkspace_t;
+
+#endif
---
+++ runtime/threads-none/chpl_cfg_threads.c
@@ -0,0 +1,47 @@
+#include <stdint.h>
+#include "chplrt.h"
+#include "chplthreads.h"
+#include "chplmem.h"
+#include "error.h"
+
+_chpl_mutex_p _chpl_mutex_new(void) { return 0; }
+int _chpl_mutex_init(_chpl_mutex_p mutex) { return 0; }
+int _chpl_mutex_lock(_chpl_mutex_p mutex) { return 0; }
+int _chpl_mutex_trylock(_chpl_mutex_p mutex) { return 0; }
+int _chpl_mutex_unlock(_chpl_mutex_p mutex) { return 0; }
+int _chpl_mutex_destroy(_chpl_mutex_p mutex) { return 0; }
+
+_chpl_condvar_p _chpl_condvar_new(void) { return 0; }
+int _chpl_condvar_init (_chpl_condvar_p cond) { return 0; }
+int _chpl_condvar_destroy(_chpl_condvar_p cond) { return 0; }
+int _chpl_condvar_signal(_chpl_condvar_p cond) { return 0; }
+int _chpl_condvar_broadcast(_chpl_condvar_p cond) { return 0; }
+int _chpl_condvar_wait(_chpl_condvar_p cond, _chpl_mutex_p mutex) { return 0; }
+
+void _chpl_serial_delete(_bool *p) { }
+void initChplThreads() { }
+void exitChplThreads() { }
+void _chpl_thread_init(void) { }
+
+_uint64 _chpl_thread_id(void) { return 0; }
+_bool _chpl_get_serial(void) { return true; }
+void _chpl_set_serial(_bool state) { }
+
+int
+_chpl_cobegin(int                      nthreads, 
+              _chpl_threadfp_t        *fp, 
+              _chpl_threadarg_t       *a, 
+              _chpl_cobegin_wkspace_t *twrk) {
+  int t;
+
+  for (t=0; t<nthreads; t++) {
+    (*fp[t])(a[t]);
+  }
+  return 0;
+}
+
+int
+_chpl_begin(_chpl_threadfp_t fp, _chpl_threadarg_t a) {
+  (*fp)(a);
+  return 0;
+}
