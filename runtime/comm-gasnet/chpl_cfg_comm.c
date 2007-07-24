9707cfc55a97252a6033a99b804652112f01a855
Steve Deitz <deitz>
2007-07-24 23:13:13 +0000

    Removed tabs.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@10999 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/comm-gasnet/chpl_cfg_comm.c
+++ runtime/comm-gasnet/chpl_cfg_comm.c
@@ -16,32 +16,30 @@
 #endif // _DIST_DEBUG
 
 /* The following macros were taken from the GasNet test.h distribution */
-#define GASNET_Safe(fncall) do {					\
-    int _retval;							\
-    if ((_retval = fncall) != GASNET_OK) {				\
-      fprintf(stderr, "ERROR calling: %s\n"				\
-	      " at: %s:%i\n"						\
-	      " error: %s (%s)\n",					\
-              #fncall, __FILE__, __LINE__,				\
-              gasnet_ErrorName(_retval), gasnet_ErrorDesc(_retval));	\
-      fflush(stderr);							\
-      gasnet_exit(_retval);						\
-    }									\
+#define GASNET_Safe(fncall) do {                                        \
+    int _retval;                                                        \
+    if ((_retval = fncall) != GASNET_OK) {                              \
+      fprintf(stderr, "ERROR calling: %s\n"                             \
+              " at: %s:%i\n"                                            \
+              " error: %s (%s)\n",                                      \
+              #fncall, __FILE__, __LINE__,                              \
+              gasnet_ErrorName(_retval), gasnet_ErrorDesc(_retval));    \
+      fflush(stderr);                                                   \
+      gasnet_exit(_retval);                                             \
+    }                                                                   \
   } while(0)
 
 // AM functions
-#define FORK_NB	  128           // (non-blocking) async fork 
-
-#define FORK	  129           // synchronous fork
-#define SIGNAL	  130           // ack of synchronous fork
-
-#define PTR_TABLE 131           // get address of remote pointer table
-#define PTR_RET   132           // return pointer value
+#define FORK_NB   128    // (non-blocking) async fork 
+#define FORK      129    // synchronous fork
+#define SIGNAL    130    // ack of synchronous fork
+#define PTR_TABLE 131    // get address of remote pointer table
+#define PTR_RET   132    // return pointer value
 
 typedef struct {
-  int   *done;          // local pointer to blocking condition
-  void  *val;           // return value; if 0 is not legal value, fold with done
-  void **val_addr;      // where to store ti
+  int   *done;     // local pointer to blocking condition
+  void  *val;      // return value; if 0 is not legal value, fold with done
+  void **val_addr; // where to store ti
 } _AM_ptr_ret_t;
 
 typedef struct {
@@ -49,7 +47,7 @@ typedef struct {
   int    *ack;
   func_p  fun;
   int     arg_size;
-  void   *arg;         // variable-sized data here
+  void   *arg;      // variable-sized data here
 } dist_fork_t;
 
 void _AM_fork_nb(gasnet_token_t token, void *buf, size_t nbytes);
@@ -59,39 +57,38 @@ void _AM_ptr_table(gasnet_token_t token, _AM_ptr_ret_t *ret_info, size_t nbytes)
 void _AM_ptr_ret(gasnet_token_t token, _AM_ptr_ret_t *ret_info, size_t nbytes);
 
 gasnet_handlerentry_t ftable[] = {
-  { FORK, _AM_fork},                    // fork remote thread asynchronously
-  { FORK_NB, _AM_fork_nb},              // fork remote thread synchronously
-  { SIGNAL, _AM_signal},                // set remote (int) condition
-  { PTR_TABLE, _AM_ptr_table},          // get address of global global ptr table
-  { PTR_RET, _AM_ptr_ret}               // reply, return pointer value
+  {FORK, _AM_fork},           // fork remote thread asynchronously
+  {FORK_NB, _AM_fork_nb},     // fork remote thread synchronously
+  {SIGNAL, _AM_signal},       // set remote (int) condition
+  {PTR_TABLE, _AM_ptr_table}, // get address of global global ptr table
+  {PTR_RET, _AM_ptr_ret}      // reply, return pointer value
 };
 
 void _AM_fork_nb_wrapper(dist_fork_t *i) {
   (*(i->fun))(&(i->arg));
   _chpl_free(i, 0, 0);
-
   /*
-  pthread_mutex_lock(&sysinfo.local_thread_cnt_lock);
-  sysinfo.local_thread_cnt--;
-  pthread_cond_signal(&sysinfo.process_can_exit);
-  pthread_mutex_unlock(&sysinfo.local_thread_cnt_lock);
+    pthread_mutex_lock(&sysinfo.local_thread_cnt_lock);
+    sysinfo.local_thread_cnt--;
+    pthread_cond_signal(&sysinfo.process_can_exit);
+    pthread_mutex_unlock(&sysinfo.local_thread_cnt_lock);
   */
 }
 
 
 // AM non-blocking fork handler, medium sized, receiver side
 void _AM_fork_nb(gasnet_token_t token, 
-                 void          *buf,
-                 size_t         nbytes) {
-  pthread_t     t;
+                 void   *buf,
+                 size_t  nbytes) {
+  pthread_t    t;
   dist_fork_t *fork_info;
 
   fork_info = (dist_fork_t*) _chpl_malloc(nbytes, sizeof(char), "", 0, 0);
   bcopy(buf, fork_info, nbytes);
   /*
-  pthread_mutex_lock(&sysinfo.local_thread_cnt_lock);
-  sysinfo.local_thread_cnt++;
-  pthread_mutex_unlock(&sysinfo.local_thread_cnt_lock);
+    pthread_mutex_lock(&sysinfo.local_thread_cnt_lock);
+    sysinfo.local_thread_cnt++;
+    pthread_mutex_unlock(&sysinfo.local_thread_cnt_lock);
   */
   pthread_create(&t, NULL, (thread_p)_AM_fork_nb_wrapper, (void*)fork_info);
 }
@@ -110,18 +107,18 @@ void _AM_fork_wrapper(dist_fork_t *i) {
                                       sizeof(i->ack)));
   _chpl_free(i, 0, 0);
   /*
-  pthread_mutex_lock(&sysinfo.local_thread_cnt_lock);
-  sysinfo.local_thread_cnt--;
-  pthread_cond_signal(&sysinfo.process_can_exit);
-  pthread_mutex_unlock(&sysinfo.local_thread_cnt_lock);
+    pthread_mutex_lock(&sysinfo.local_thread_cnt_lock);
+    sysinfo.local_thread_cnt--;
+    pthread_cond_signal(&sysinfo.process_can_exit);
+    pthread_mutex_unlock(&sysinfo.local_thread_cnt_lock);
   */
 }
 
 
 // AM sfork handler, medium sized, receiver side
 void _AM_fork(gasnet_token_t  token, 
-              void           *buf,
-              size_t          nbytes) {
+       void    *buf,
+       size_t   nbytes) {
   pthread_t     t;
   dist_fork_t *fork_info;
 
@@ -130,9 +127,9 @@ void _AM_fork(gasnet_token_t  token,
   fork_info = (dist_fork_t*) _chpl_malloc(nbytes, sizeof(char), "", 0, 0);
   bcopy(buf, fork_info, nbytes);
   /*
-  pthread_mutex_lock(&sysinfo.local_thread_cnt_lock);
-  sysinfo.local_thread_cnt++;
-  pthread_mutex_unlock(&sysinfo.local_thread_cnt_lock);
+    pthread_mutex_lock(&sysinfo.local_thread_cnt_lock);
+    sysinfo.local_thread_cnt++;
+    pthread_mutex_unlock(&sysinfo.local_thread_cnt_lock);
   */
   pthread_create(&t, NULL, (thread_p)_AM_fork_wrapper, (void*)fork_info);
 }
@@ -140,8 +137,8 @@ void _AM_fork(gasnet_token_t  token,
 
 // AM signal handler, medium sized, receiver side
 void _AM_signal(gasnet_token_t  token, 
-                void           *buf,
-                size_t          nbytes) {
+                void    *buf,
+                size_t   nbytes) {
   int **done = (int**)buf;
 
   PRINTF("_AM_signal");
@@ -161,7 +158,7 @@ void _AM_ptr_ret(gasnet_token_t token, _AM_ptr_ret_t *ret_info, size_t nbytes) {
 void _AM_ptr_table(gasnet_token_t token, _AM_ptr_ret_t *ret_info, size_t nbytes) {
   PRINTF("_AM_ptr_table");
   // ret_info = (global_ret_int_t*) buf;
-  //  ret_info->val = (void*) globals_table;
+  // ret_info->val = (void*) globals_table;
 
   GASNET_Safe(gasnet_AMReplyMedium0(token, PTR_RET, ret_info, sizeof(_AM_ptr_ret_t)));
 }
@@ -170,9 +167,9 @@ void _chpl_comm_init(int *argc_p, char ***argv_p) {
   gasnet_init(argc_p, argv_p);
 
   GASNET_Safe(gasnet_attach(ftable, 
-			    sizeof(ftable)/sizeof(gasnet_handlerentry_t),
-			    0,			// share everything
-			    0));
+                            sizeof(ftable)/sizeof(gasnet_handlerentry_t),
+                            0,   // share everything
+                            0));
   printf("starting locale %d of %d locale(s)\n", gasnet_mynode(), gasnet_nodes());
   fflush(stdout);
   _chpl_comm_barrier("_chpl_comm_init");
