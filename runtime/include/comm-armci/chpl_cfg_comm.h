61b80855dc5aec4a712abde3bfbc45914b5a47c4
Brad Chamberlain <bradcray@users.noreply.github.com>
2007-08-06 22:21:21 +0000

    Added this required, but currently unused header file.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@11138 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

---
+++ runtime/include/comm-armci/chpl_cfg_comm.h
@@ -0,0 +1,9 @@
+#ifndef _chpl_cfg_comm_h_
+#define _chpl_cfg_comm_h_
+
+// Any ARMCI-specific stuff that needs to be in a header file can go
+// here.  We don't seem to use this for any of the other comm layers,
+// and its not clear what would ever need to go in a directory like
+// this.
+
+#endif
