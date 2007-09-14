0c993a978982a125a47ab99e4e8ff01c9639dcd1
Brad Chamberlain <bradcray@users.noreply.github.com>
2007-09-14 18:56:05 +0000

    Oops, in my previous check-in, hadn't noticed that the machine/limits.h file on the MTA didn't define the 64-bit versions of the macros I was hoping for.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@11817 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/include/mta/stdint.h
+++ runtime/include/mta/stdint.h
@@ -1,7 +1,14 @@
 #ifndef _MTA_STDINT_H_
 #define _MTA_STDINT_H_
 
+/* These defines many of the INT<N>_MIN/MAX macros we need */
 #include <machine/limits.h>
+
+/* Here area  few that it doesn't, defined in other constants that
+   the MTA defines (noting that INT on MTA is 64-bits */
+#define INT64_MIN INT_MIN
+#define INT64_MAX INT_MAX
+#define UINT64_MAX UINT_MAX
 
 typedef unsigned long intptr_t;
 
