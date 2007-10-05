b525a887a9ea1309333069ef49e92d06bc701ec7
Brad Chamberlain <bradcray@users.noreply.github.com>
2007-10-05 00:00:50 +0000

    Fixing --baseline regression for the redefineTypeNames.chpl test by munging any user identifiers named "int".  This is a lazy fix (as are most of these that are trying to protect against conflicts with C) in that I've only done what's necessary to fix this test, haven't tried to think of other identifiers that might have similar issues (like, say, "float").  I think I keep waiting for us to come up with a more elegant solution to this problem.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@12093 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- compiler/passes/codegen.cpp
+++ compiler/passes/codegen.cpp
@@ -143,6 +143,7 @@ static void codegen_header(void) {
   cnames.put("isnan", 1);
   cnames.put("random", 1);
   cnames.put("truncate", 1);
+  cnames.put("int", 1);
 
   forv_Vec(BaseAST, ast, gAsts) {
     if (CallExpr* call = toCallExpr(ast))
