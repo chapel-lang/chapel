d421212ea9915decb1a347ceb316ac4ea7a80cdb
Brad Chamberlain <bradcray@users.noreply.github.com>
2008-02-12 00:38:38 +0000

    Attempting to remove the BUILD_VERSION file as follows:
    - removed BUILD_VERSION
    - added it (and lib/ and bin/) to the ignore
      properties for the root directory
    - modified the compiler Makefiles to create a
      BUILD_VERSION file if one is necessary
    - put all the logic for this into a new
      utility script updateBuildVersion.pl which
      is designed to work correctly for developers,
      non-developers, and releases whether or not
      CHPL_HOME is set.
    
    In order to work for a release, this checkin
    needs some changes to the gen_release scripts,
    but I haven't gotten those working yet (and
    can't until getting these files checked in).
    
    
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@13400 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

---
+++ util/updateBuildVersion.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+
+#
+# In general this value should be set to the SVN revision number
+# at the time of the last release.
+#
+$LAST_RELEASE_BUILD_NUM = 13146;
+#
+# For this first stage of converting to subversion, I've set it
+# artificially since all of the automated CVS updates to CVSLOG 
+# and BUILD_VERSION were counted as revisions, doubling the count
+# compared to how we normally count BUILD_VERSIONS
+#
+
+$chplhome = $ENV{'CHPL_HOME'};
+$chplroot = $ARGV[0];
+
+if ($chplroot eq "") {
+    print "usage: computeBuildVersion.pl <directory to place BUILD_VERSION";
+    exit(1);
+}
+
+$build_version_file = "$chplroot/BUILD_VERSION";
+
+if (-r "$build_version_file") {
+    $last_build_version = `cat $build_version_file`;
+    chomp($last_build_version);
+} else {
+    $last_build_version = "!!!";
+}
+
+if (defined $ENV{'CHPL_DEVELOPER'} &&
+    defined $ENV{'CHPL_HOME'} &&
+    -e "$chplhome/.svn") {
+
+    $revline = `svn info $ENV{'CHPL_HOME'} | grep Revision`;
+    chomp($revline);
+
+    if ($revline =~ m/Revision: (\d*)/) {
+        $CURRENT_BUILD_NUM = $1;
+        $build_version = $CURRENT_BUILD_NUM - $LAST_RELEASE_BUILD_NUM;
+    } else {
+        print "ERROR: computeBuildVersion.pl failed!";
+        exit(1);
+    }
+} else {
+    $build_version = "-999";
+}
+
+if ($build_version eq $last_build_version || 
+    ($build_version eq "-999" && $last_build_version ne "!!!")) {
+#    print "Build version is up-to-date\n";
+} else {
+#    print "Updating build version ($build_version != $last_build_version)\n";
+    open BUILD_VERSION, ">$build_version_file" or die "can't open $build_version_file for writing $!";
+    print BUILD_VERSION "$build_version\n";
+    close BUILD_VERSION;
+}
