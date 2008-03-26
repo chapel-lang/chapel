chapel-lang/chapel

commit 40f0990e7352ab6298c5556cffafde7f054e3353

added error check for .locale/on expressions; added two tests
Added checks to make sure that the expression to which .locale is
applied (as well as the expression in an on-statement) is an lvalue or
a class or a locale.

Added a test case for this error.



git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@13756 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

compiler/passes/functionResolution.cpp
@@ -2301,12 +2301,25 @@ preFold(Expr* expr) {
         }
       }
 
+      if (type->symbol->hasPragma("ref"))
+        type = getValueType(type);
+
+      //
+      // ensure .locale (and on) are applied to lvalues or classes
+      // (locale type is a class)
+      //
+      if (call->argList.length() > 0) { // if because of replace for
+                                        // integral parameters
+        SymExpr* se = toSymExpr(call->get(1));
+        ClassType* ct = toClassType(type);
+        if (se->var->isExprTemp && (!ct || ct->classTag != CLASS_CLASS))
+          USR_FATAL_CONT(se, "expression must be an lvalue, locale, or class");
+      }
+
       //
       // if .locale is applied to an expression of locale type,
       // replace this primitive with an access of the id field
       //
-      if (type->symbol->hasPragma("ref"))
-        type = getValueType(type);
       if (type->symbol->hasPragma("locale")) {
         result = new CallExpr("id", gMethodToken, call->get(1)->remove());
         call->replace(result);

test/multilocale/deitz/test_dot_locale_error.chpl
@@ -1,0 +1,13 @@
+class C {}
+
+def bar() return new C();
+
+writeln(bar().locale);
+
+def foo() return 1;
+
+on foo() {
+  writeln("hi");
+}
+
+writeln(foo().locale);

test/multilocale/deitz/test_dot_locale_error.good
@@ -1,0 +1,2 @@
+test_dot_locale_error.chpl:9: error: expression must be an lvalue, locale, or class
+test_dot_locale_error.chpl:13: error: expression must be an lvalue, locale, or class

test/multilocale/deitz/test_dot_locale4.chpl
@@ -1,0 +1,12 @@
+class C { }
+on Locales(0) {
+  var x: int;
+  var c: C;
+  on Locales(1) {
+    on Locales(2) {
+      c = new C();
+    }
+    writeln(x.locale);
+    writeln(c.locale);
+  }
+}

test/multilocale/deitz/test_dot_locale4.good
@@ -1,0 +1,2 @@
+0
+2

test/multilocale/deitz/test_dot_locale4.numlocales
@@ -1,0 +1,1 @@
+3

commit bd500f7f7fb78c82465c1af0cb3597c85a34dac1

changed error introduced in r13756 to a warning
This warning now says "warning: accessing the locale of a local expression".
Updated the test case of this error and cleaned up the specification.



git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@13757 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

compiler/passes/functionResolution.cpp
@@ -2313,7 +2313,7 @@ preFold(Expr* expr) {
         SymExpr* se = toSymExpr(call->get(1));
         ClassType* ct = toClassType(type);
         if (se->var->isExprTemp && (!ct || ct->classTag != CLASS_CLASS))
-          USR_FATAL_CONT(se, "expression must be an lvalue, locale, or class");
+          USR_WARN(se, "accessing the locale of a local expression");
       }
 
       //

spec/Locality_and_Distribution.tex
@@ -88,28 +88,16 @@ running on the locale given by \chpl{Locales(0)}.
 \label{Querying_the_Locale_of_a_Variable}
 \index{locale@\chpl{locale}}
 
-The locale associated with expressions that are lvalues or of class or
-locale type can be queried using the following syntax:
+The locale associated with an expression (where the expression is
+stored) is queried using the following syntax:
 \begin{syntax}
 locale-access-expression:
-  locale-access-sub-expression . `locale'
-
-locale-access-sub-expression:
-  lvalue-expression
-  class-expression
-  locale-expression
-
-class-expression:
-  expression
-
-locale-expression:
-  expression
+  expression . `locale'
 \end{syntax}
-When the left operand to the locale query is an lvalue, the access
-returns the locale on which that lvalue exists.  When it is of class
-type (whether or not it is also an lvalue), the access returns the
-locale on which the class object exists.  When it is of locale type,
-the access returns the expression itself.
+When the expression is a class, the access returns the locale on which
+the class object exists rather than the reference to the class.  If
+the expression is a value, it is considered local.  The implementation
+may warn about this behavior.
 
 \begin{example}
 Given some class C, the code
@@ -131,9 +119,9 @@ results in the output
 0
 2
 \end{chapel}
-The variable \chpl{x} is a value.  It is declared and exists
-on \chpl{Locales(0)}.  The variable \chpl{c} is a reference.  The
-reference exists on \chpl{Locales(0)} but the object itself exists
+The variable \chpl{x} is declared and exists on \chpl{Locales(0)}.
+The variable \chpl{c} is a class reference.  The reference exists
+on \chpl{Locales(0)} but the object itself exists
 on \chpl{Locales(2)}.  The locale access returns the locale where the
 object exists.
 \end{example}
@@ -154,19 +142,18 @@ The on statement controls on which locale a computation or data should
 be placed.  The syntax of the on statement is given by
 \begin{syntax}
 on-statement:
-  `on' locale-access-sub-expression `do' statement
-  `on' locale-access-sub-expression block-level-statement
+  `on' expression `do' statement
+  `on' expression block-level-statement
 \end{syntax}
-The expression must be a legal expression for accessing a locale.
-See~\rsec{Querying_the_Locale_of_a_Variable} for details.  Execution
-of the statement occurs on the locale given by
-the \sntx{locale-access-sub-expression}.  Execution continues after
-the \chpl{on-statement} after execution of the \sntx{statement}
-completes.
+The locale of the expression is automatically queried as described
+in~\rsec{Querying_the_Locale_of_a_Variable}.  Execution of the
+statement occurs on this specified locale and then continues after
+the \chpl{on-statement}.
 
 By default, when new variables and data objects are created, they are
 created in the locale where the computation is running.  This locale
-can be changed by using the \chpl{on} keyword.
+can be changed by using the \chpl{on} keyword as discussed
+in~\rsec{remote_variable_declarations}.
 
 The following statements may not be lexically enclosed in
 on-statements: yield-statements and return-statements.
@@ -218,7 +205,7 @@ but omitting the \chpl{do} keyword and braces.  The syntax is given
 by:
 \begin{syntax}
 remote-variable-declaration-statement:
-  `on' locale-access-sub-expression variable-declaration-statement
+  `on' expression variable-declaration-statement
 \end{syntax}
 
 \subsection{Distributions}

test/multilocale/deitz/test_dot_locale_error.chpl
@@ -2,7 +2,7 @@ class C {}
 
 def bar() return new C();
 
-writeln(bar().locale);
+writeln(bar().locale.id);
 
 def foo() return 1;
 
@@ -10,4 +10,4 @@ on foo() {
   writeln("hi");
 }
 
-writeln(foo().locale);
+writeln(foo().locale.id);

test/multilocale/deitz/test_dot_locale_error.good
@@ -1,2 +1,5 @@
-test_dot_locale_error.chpl:9: error: expression must be an lvalue, locale, or class
-test_dot_locale_error.chpl:13: error: expression must be an lvalue, locale, or class
+test_dot_locale_error.chpl:9: warning: accessing the locale of a local expression
+test_dot_locale_error.chpl:13: warning: accessing the locale of a local expression
+0
+hi
+0