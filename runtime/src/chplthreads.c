ab5233c55a14ba705e693ed604e4d2f306ff8a48
Tito Figueroa <figueroa>
2008-04-08 02:02:06 +0000

    Corrected how much memory needs to be allocated for a task list entry and renamed the local variable "task" to "task_entry" to avoid build problems on the MTA and XMT, as it appears "task" is some sort of reserved word.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@13886 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/src/chplthreads.c
+++ runtime/src/chplthreads.c
@@ -7,11 +7,11 @@
 #include "chplmem.h"
 
 void chpl_add_to_task_list (chpl_threadfp_t fun, chpl_threadarg_t arg, chpl_task_list_p *task_list) {
-  chpl_task_list_p task = (chpl_task_list_p)_chpl_malloc(1, sizeof(chpl_task_list_p), "task list entry", 0, 0);
-  task->fun = fun;
-  task->arg = arg;
-  task->next = *task_list;
-  *task_list = task;
+  chpl_task_list_p task_entry = (chpl_task_list_p)_chpl_malloc(1, sizeof(struct chpl_task_list), "task list entry", 0, 0);
+  task_entry->fun = fun;
+  task_entry->arg = arg;
+  task_entry->next = *task_list;
+  *task_list = task_entry;
 }
 
 void chpl_process_task_list (chpl_task_list_p task_list) {
