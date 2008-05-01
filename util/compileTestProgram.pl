59f500731d822ae400c8392c5a72b4aa0d1b5994
Steve Deitz <deitz>
2008-05-01 18:32:57 +0000

    added util/compileTestProgram.pl
    This script prints and executes a command to execute a test program
    from the current directory with the options specified in the test's
    .compopts file or the directory's COMPOPTS file.  It also changes the
    relative paths of the extra .chpl files listed in these files.  For
    example,
    
      util/compileTestProgram.pl test/release/examples/hpcc/stream.chpl
    
    prints and executes the command
    
      chpl test/release/examples/hpcc/probSize.chpl test/release/examples/hpcc/stream.chpl
    
    In addition, the script passes any other arguments along to the
    underlying chpl command.
    
    Since I often work in the chapel/ directory I find that I often have
    to recreate this command.  Aliasing this script to tchpl allows
    debugging the compilation of an associative array test with many
    compiler options easy.  Write
    
      tchpl test/associative/deitz/arrays/test_indefinite1.chpl --gdb
    
    to print and execute
    
      chpl test/associative/deitz/arrays/../../common/printHelp.chpl --main-module=test_indefinite1 test/associative/deitz/arrays/test_indefinite1.chpl
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@14023 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

---
+++ util/compileTestProgram.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+
+use File::Basename;
+
+$found_test = 0;
+$command .= "chpl ";
+while (@ARGV) {
+    $arg = shift @ARGV;
+    if ($arg =~ m/.chpl$/) {
+        $found_test = 1;
+
+        ($filename, $path) = fileparse($arg);
+
+        $compopts_file = "$path"."COMPOPTS";
+
+        if (-e $compopts_file) {
+            $compopts = `cat $compopts_file`;
+            chomp $compopts;
+        }
+
+        $filebase = $filename;
+        $filebase =~ s/.chpl$//;
+
+        $compopts_file = "$path"."$filebase".".compopts";
+        
+        if (-e $compopts_file) {
+            if ($compopts) {
+                $compopts .= " ";
+            }
+            $compopts .= `cat $compopts_file`;
+            chomp $compopts;
+        }
+
+        @compopts = split / /, $compopts;
+
+        foreach $compopt (@compopts) {
+            if ($compopt =~ m/.chpl$/) {
+                $command .= "$path";
+            }
+            $command .= "$compopt ";
+        }
+        $command .= "$path$filename ";
+    } else {
+        $command .= "$arg ";
+    }
+}
+
+die "no test specified" unless $found_test == 1;
+
+print "$command\n";
+system $command;
