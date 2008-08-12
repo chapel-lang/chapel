2e16724e10a36e56e453355d21663390a0ab2861
Andy Stone <stonea>
2008-08-12 23:56:31 +0000

    Moved function declerations in chplcgfns for ChapelRuntime into a section that will only be compiled when not in generated code.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@14478 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/include/chplcgfns.h
+++ runtime/include/chplcgfns.h
@@ -17,15 +17,15 @@ extern void CreateConfigVarTable(void);
 /* If we're not in the generated code, define an enum _class_id; */
 #ifndef CHPL_GEN_CODE
 typedef enum { _e_bogus } _class_id;
-#endif
-
-extern size_t cid2size(_class_id cid);
-extern size_t* cid2offsets(_class_id cid);
 
 /* Task list functions coming from ChapelRuntime module */
 void chpldev_taskTable_add(
     uint64_t threadID, uint32_t lineno, _string filename);
 void chpldev_taskTable_remove(uint64_t threadID);
 void chpldev_taskTable_print(void);
+#endif
+
+extern size_t cid2size(_class_id cid);
+extern size_t* cid2offsets(_class_id cid);
 
 #endif
