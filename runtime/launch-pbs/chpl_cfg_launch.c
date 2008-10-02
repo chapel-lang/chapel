5e4cd0322326cea7c05fce276ead7d766f075536
Brad Chamberlain <bradcray@users.noreply.github.com>
2008-10-02 23:13:49 +0000

    PBS launcher: made the PBS launcher a bit more qsub-version-sensitive
    Different versions of qsub accept different arguments, particularly
    in specifying the number of nodes to use.  Started to distinguish
    between different versions, though the only two cases we currently
    distinguish between is ours and ORNL's (which is hard to distinguish
    due to the lack of a supported --version flag; which is exactly what
    we look for...).
    
    Haven't quite tested this on our XTs yet, owing to the difficulty
    of getting source over there.  About to try after this commit.
    
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@14774 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/launch-pbs/chpl_cfg_launch.c
+++ runtime/launch-pbs/chpl_cfg_launch.c
@@ -9,14 +9,53 @@
 static const char* pbsFilename = ".chpl-pbs-qsub";
 static const char* expectFilename = ".chpl-expect";
 
+#define sysFilename ".chpl-tmp"
+/* copies of binary to run per node */
+#define procsPerNode 1  
+
 #define launcherAccountEnvvar "CHPL_LAUNCHER_ACCOUNT"
+
+typedef enum {
+  pbspro,
+  ccse,
+  unknown
+} qsubVersion;
+
+static qsubVersion determineQsubVersion(void) {
+  char version[81] = "";
+  FILE* sysFile;
+  system("qsub --version > "sysFilename);
+  sysFile = fopen(sysFilename, "r");
+  fscanf(sysFile, "version %80s", version);
+  fclose(sysFile);
+  if (strcmp(version, "") == 0) {
+    return ccse;
+  } else if (strstr(version, "PBSPro")) {
+    return pbspro;
+  } else {
+    return unknown;
+  }
+}
+
+static void genNumLocalesOptions(FILE* pbsFile, qsubVersion qsub, 
+				 int32_t numLocales) {
+  switch (qsub) {
+  case pbspro:
+  case unknown:
+    fprintf(pbsFile, "#PBS -l mppwidth=%d\n", numLocales);
+    fprintf(pbsFile, "#PBS -l mppnppn=%d\n", procsPerNode);
+    break;
+  case ccse:
+    fprintf(pbsFile, "#PBS -l size=%d\n", 4*numLocales);
+    break;
+  }
+}
 
 char* chpl_launch_create_command(int argc, char* argv[], int32_t numLocales) {
   int i;
   int size;
   char baseCommand[256];
   char* command;
-  int procsPerNode = 1;  // BLC -- is this the value we want to use?
   FILE* pbsFile, *expectFile;
   char* projectString = getenv(launcherAccountEnvvar);
   char* basenamePtr = strrchr(argv[0], '/');
@@ -26,12 +65,11 @@ char* chpl_launch_create_command(int argc, char* argv[], int32_t numLocales) {
       basenamePtr++;
   }
 
+  determineQsubVersion();
   pbsFile = fopen(pbsFilename, "w");
   fprintf(pbsFile, "#!/bin/sh\n\n");
   fprintf(pbsFile, "#PBS -N Chapel-%s\n", basenamePtr);
-  //  fprintf(pbsFile, "#PBS -l size=%d\n", 4*numLocales);
-  fprintf(pbsFile, "#PBS -l mppwidth=%d\n", numLocales);
-  fprintf(pbsFile, "#PBS -l mppnppn=%d\n", procsPerNode);
+  genNumLocalesOptions(pbsFile, determineQsubVersion(), numLocales);
   fprintf(pbsFile, "#PBS -l walltime=30\n");    // BLC -- ditto?
   if (projectString && strlen(projectString) > 0)
     fprintf(pbsFile, "#PBS -A %s\n", projectString);
@@ -45,18 +83,23 @@ char* chpl_launch_create_command(int argc, char* argv[], int32_t numLocales) {
   fprintf(expectFile, "set prompt \"(%|#|\\$|>) $\"\n");
   fprintf(expectFile, "spawn qsub -z -I %s\n", pbsFilename);
   fprintf(expectFile, "expect {\n");
-  fprintf(expectFile, "  \"A project was not specified\" {send_user \"Error: A project account must be specified via \\$" launcherAccountEnvvar "\\n\" ; exit 1}\n");
+  fprintf(expectFile, "  \"A project was not specified\" {send_user "
+	  "\"Error: A project account must be specified via \\$" 
+	  launcherAccountEnvvar "\\n\" ; exit 1}\n");
   fprintf(expectFile, "  -re $prompt\n");
   fprintf(expectFile, "}\n");
   fprintf(expectFile, "send \"cd \\$PBS_O_WORKDIR\\n\"\n");
   fprintf(expectFile, "expect -re $prompt\n");
   fprintf(expectFile, "send \"aprun -q -b -n1 -N1 ls %s_real\\n\"\n", argv[0]);
   fprintf(expectFile, "expect {\n");
-  fprintf(expectFile, "  \"failed: chdir\" {send_user \"Error: %s/%s_real must be launched from and/or stored on a cross-mounted file system\\n\" ; exit 1}\n", basenamePtr, basenamePtr);
+  fprintf(expectFile, "  \"failed: chdir\" {send_user "
+	  "\"Error: %s/%s_real must be launched from and/or stored on a "
+	  "cross-mounted file system\\n\" ; exit 1}\n", 
+	  basenamePtr, basenamePtr);
   fprintf(expectFile, "  -re $prompt\n");
   fprintf(expectFile, "}\n");
-  fprintf(expectFile, "send \"aprun -q -n%d -N%d %s_real",
-          numLocales, procsPerNode, argv[0]);
+  fprintf(expectFile, "send \"aprun -q -n%d -N%d %s_real", 
+	  numLocales, procsPerNode, argv[0]);
   for (i=1; i<argc; i++) {
     fprintf(expectFile, " %s", argv[i]);
   }
@@ -86,10 +129,11 @@ void chpl_launch_cleanup(void) {
   char command[1024];
 
   sprintf(command, "rm %s", pbsFilename);
-  printf("system: %s\n", command);
   system(command);
 
   sprintf(command, "rm %s", expectFilename);
-  printf("system: %s\n", command);
+  system(command);
+
+  sprintf(command, "rm %s", sysFilename);
   system(command);
 }
