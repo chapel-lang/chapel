b1d1ba40f35c5cdeb9a2071cd17a678c22524919
David Iten <daviditen@users.noreply.github.com>
2008-11-05 15:53:40 +0000

    Change a few tabs to spaces
    The new launcher code had a few tabs.
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@14939 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/launch-gasnetrun_ibv/chpl_cfg_launch.c
+++ runtime/launch-gasnetrun_ibv/chpl_cfg_launch.c
@@ -16,7 +16,7 @@ char* chpl_launch_create_command(int argc, char* argv[], int32_t numLocales) {
   char baseCommand[256];
   char* command;
   sprintf(baseCommand, "gasnetrun_ibv -spawner=ssh -n %d %s_real", numLocales,
-	  argv[0]);
+          argv[0]);
 
   size = strlen(WRAP_TO_STR(LAUNCH_PATH)) + strlen(baseCommand) + 1;
 
@@ -25,7 +25,7 @@ char* chpl_launch_create_command(int argc, char* argv[], int32_t numLocales) {
   }
 
   command = chpl_malloc(size, sizeof(char*), "gasnetrun_ibv command buffer", 
-			-1, "");
+                        -1, "");
   
   sprintf(command, "%s%s", WRAP_TO_STR(LAUNCH_PATH), baseCommand);
   for (i=1; i<argc; i++) {
