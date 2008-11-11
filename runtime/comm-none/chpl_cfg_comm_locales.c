dd1171d32d592222be0ac70d81d5b6e3259b4255
Brad Chamberlain <bradcray@users.noreply.github.com>
2008-11-11 09:31:00 +0000

    Small change to comm-none -nl > 1 executions
    Made our error message for -nl > 1 executions for comm-none
    point to the README.multilocale file in order to avoid this
    becoming a FAQ.
    
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@14988 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/comm-none/chpl_cfg_comm_locales.c
+++ runtime/comm-none/chpl_cfg_comm_locales.c
@@ -8,6 +8,8 @@ int _chpl_comm_default_num_locales(void) {
 
 void _chpl_comm_verify_num_locales(int32_t proposedNumLocales) {
   if (proposedNumLocales != 1) {
-    chpl_error("Only 1 locale may be used for CHPL_COMM layer 'none'", 0, 0);
+    chpl_error("Only 1 locale may be used for CHPL_COMM layer 'none'\n"
+               "To use multiple locales, see $CHPL_HOME/doc/README.multilocale",
+               0, 0);
   }
 }
