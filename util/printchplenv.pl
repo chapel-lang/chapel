e16f7af4886beee8b86d98043b31f9e4b535de68
Brad Chamberlain <bradcray@users.noreply.github.com>
2009-02-20 20:02:25 +0000

    Added a new script to debug the CHPL_ environment
    I added a new script -- printchplenv.pl -- that prints relevant
    Chapel environment variables and their values, whether manually
    set or inferred.  This should be helpful in debugging environments,
    particularly as the number of environment variables increases.
    Note that some environment variables are only printed out if they
    are meaningful.  For example, CHPL_COMM_SUBSTRATE is only printed
    if CHPL_COMM is not 'none' and CHPL_GASNET_SEGMENT is only printed
    if CHPL_COMM is 'gasnet'.
    
    Fixed a bug in the chplmake script that this new script revealed in
    which setting CHPL_MAKE would not override the value that was used.
    
    Updated README.chplenv to mention this script and README.bugs to
    request that users send the output of this script along with any bug
    reports.
    
    
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@15259 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

---
+++ util/printchplenv.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+
+use File::Basename;
+
+$utildirname = dirname($0);
+
+$uname = `uname -a`;
+$pwd = `pwd`; chomp($pwd);
+
+print "uname -a: $uname";
+
+print "\$CHPL_HOME: $ENV{'CHPL_HOME'}\n";
+print "pwd: $pwd\n";
+print "script location: $utildirname\n";
+
+$hostplatform = `$utildirname/platform.pl --host`; chomp($hostplatform);
+$hostcompiler = `$utildirname/compiler.pl --host`; chomp($hostcompiler);
+
+print "CHPL_HOST_PLATFORM: $hostplatform\n";
+print "CHPL_HOST_COMPILER: $hostcompiler\n";
+
+
+$targplatform = `$utildirname/platform.pl --target`; chomp($targplatform);
+$targcompiler = `$utildirname/compiler.pl --target`; chomp($targcompiler);
+
+print "CHPL_TARGET_PLATFORM: $targplatform\n";
+print "CHPL_TARGET_COMPILER: $targcompiler\n";
+
+$comm = `$utildirname/comm.pl`; chomp($comm);
+print "CHPL_COMM: $comm\n";
+if ($comm eq "none") {
+} else {
+    $commsubstrate = `$utildirname/commSubstrate.pl`; chomp($commsubstrate);
+    print "  CHPL_COMM_SUBSTRATE: $commsubstrate\n";
+    if ($comm eq "gasnet") {
+        $commsegment = `$utildirname/commSegment.pl`; chomp($commsegment);
+        print "  CHPL_GASNET_SEGMENT: $commsegment\n";a
+    }
+}
+
+$threads = `$utildirname/threads.pl`; chomp($threads);
+print "CHPL_THREADS: $threads\n";
+$launcher = `$utildirname/launcher.pl`; chomp($launcher);
+print "CHPL_LAUNCHER: $launcher\n";
+$mem = `$utildirname/mem.pl`; chomp($mem);
+print "CHPL_MEM: $mem\n";
+$make = `$utildirname/chplmake`; chomp($make);
+print "CHPL_MAKE: $make\n";
