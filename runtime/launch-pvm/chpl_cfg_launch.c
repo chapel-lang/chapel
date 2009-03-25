fffda68d8cedc7b89110b933a3107e14981c12df
Lee Prokowich <ljprokow>
2009-03-25 20:34:18 +0000

    Adding functional code to PVM launcher.
    This commit adds functional code to the PVM launcher modeled after the GASNet launcher. It provides basic functionality, and it allows the user to spawn applications if the Chapel application shares a name with a PVM application. A lot of work needs to be done surrounding number of locales and selecting the correct application. This change also cleans up the tabs.
    
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@15350 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/launch-pvm/chpl_cfg_launch.c
+++ runtime/launch-pvm/chpl_cfg_launch.c
@@ -5,24 +5,55 @@
 #include "chplmem.h"
 #include "error.h"
 
+// TODO: Un-hard-code this stuff:
+static const char* pvmrunPath = "/tmp/Chapel/pvm3/lib/";
 
 char* chpl_launch_create_command(int argc, char* argv[], int32_t numLocales) {
+  int i;
+  int size;
   char baseCommand[256];
   char* command;
+  sprintf(baseCommand, "echo \"spawn -> %s\" | %spvm", argv[0], pvmrunPath);
 
-  sprintf(baseCommand, "echo \"spawn -> %s\" | pvm", argv[0]);
+  size = strlen(baseCommand) + 1;
+  for ( i=1; i<argc; i++ ) {
+    size += strlen(argv[i]) + 3;
+  }
 
-  fprintf(stderr, "Called into chpl_launch_create_command(..., numLocales=%d\n",
-	  numLocales);
+  command = chpl_malloc(size, sizeof(char*), "pvm command buffer", -1, "");
+  sprintf(command, "%s", baseCommand);
+  for ( i=1; i<argc; i++ ) {
+    strcat(command, " '");
+    strcat(command, argv[i]);
+    strcat(command, "'");
+  }
+
+  if (strlen(command)+1 > size) {
+    chpl_internal_error("buffer overflow");
+  }
+
+  fprintf(stderr, "command is: %s\n", command);
   return command;
 }
 
 void chpl_launch_sanity_checks(int argc, char* argv[], const char* cmd) {
-  fprintf(stderr, "chpl_launch_sanity_checks()\n");
-  return;
+  // Do sanity checks just before launching.
+  struct stat statBuf;
+  char realName[256];
+  int retVal;
+
+  retVal = snprintf(realName, 256, "%s", argv[0]);
+  if (retVal < 0 || retVal >= 256) {
+    chpl_internal_error("error generating back-end filename");
+  }
+
+  // Make sure the _real binary exists
+  if (stat(realName, &statBuf) != 0) {
+    char errorMsg[256];
+    sprintf(errorMsg, "unable to locate file: %s", realName);
+    chpl_error(errorMsg, -1, "<internal>");
+  }
 }
 
 void chpl_launch_cleanup(void) {
-  fprintf(stderr, "chpl_launch_cleanup()\n");
-  return;
 }
