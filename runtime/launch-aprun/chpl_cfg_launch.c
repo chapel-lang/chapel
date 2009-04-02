0d6609add49725f0e0d41af60ad788de8b0ff719
Steve Deitz <deitz>
2009-04-02 22:24:32 +0000

    added -d option to aprun launcher and made it grab the max number of cores
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@15390 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/launch-aprun/chpl_cfg_launch.c
+++ runtime/launch-aprun/chpl_cfg_launch.c
@@ -1,19 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include "chpllaunch.h"
 #include "chplmem.h"
 #include "error.h"
 
+#define baseSysFilename ".chpl-sys-"
+char sysFilename[FILENAME_MAX];
+
 
 // TODO: Un-hard-code this stuff:
+
+static int getNumCoresPerLocale(void) {
+  FILE* sysFile;
+  int coreMask;
+  int bitMask = 0x1;
+  int numCores = 1;
+  int maxNumCores = 1;
+  pid_t mypid;
+
+#ifndef DEBUG_LAUNCH
+  mypid = getpid();
+#else
+  mypid = 0;
+#endif
+  sprintf(sysFilename, "%s%d", baseSysFilename, (int)mypid);
+
+  /* BLC: This code is fairly specific to xt-cle, but currently will
+     only be called for the NCCS version of qsub */
+  char* command = chpl_glom_strings(2, "cnselect -Lcoremask > ", sysFilename);
+  system(command);
+  sysFile = fopen(sysFilename, "r");
+  while (!feof(sysFile)) {
+    if (fscanf(sysFile, "%d\n", &coreMask) != 1) {
+      chpl_error("Cannot determine number of cores per node", 0, 0);
+    }
+    coreMask >>= 1;
+    while (coreMask & bitMask) {
+      coreMask >>= 1;
+      numCores += 1;
+    }
+    if (numCores > maxNumCores)
+      maxNumCores = numCores;
+  }
+  fclose(sysFile);
+  sprintf(command, "rm %s", sysFilename);
+  system(command);
+  return numCores;
+}
 
 char* chpl_launch_create_command(int argc, char* argv[], int32_t numLocales) {
   int i;
   int size;
   char baseCommand[256];
   char* command;
-  sprintf(baseCommand, "aprun -n%d -N1 %s_real", numLocales, argv[0]);
+  sprintf(baseCommand, "aprun -d%d -n%d -N1 %s_real", getNumCoresPerLocale(), numLocales, argv[0]);
 
   size = strlen(baseCommand) + 1;
 
