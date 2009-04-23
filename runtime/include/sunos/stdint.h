671843b9df51a5e5f4e483341853f5f6889376e6
Samuel Figueroa <SamuelFigueroa>
2009-04-23 00:54:18 +0000

    Removed the include/sunos directories, since they aren't needed on a Sun system with a reasonable gcc installation.  This may break regression testing on our Sun system, but testing on that system is not particularly valuable, since it is unlikely to represent what a real Chapel user would have.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@15497 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/include/sunos/stdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SUN_STDINT_H_
-#define _SUN_STDINT_H_
-
-// The existence of this file is a stupid hack because our Suns don't
-// seem to support stdint.h
-
-#include <inttypes.h>
-
-#endif
