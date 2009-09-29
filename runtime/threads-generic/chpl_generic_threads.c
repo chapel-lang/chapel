a612eee90ec86eca543a3b25e915d5824add25e4
Greg Titus <gbtitus@users.noreply.github.com>
2009-09-29 21:25:22 +0000

    Restructure generic threading.
    Move the generic threading #include file and implementation into their own
    subdirectories in the runtime.  This adds a small amount of complication to
    the Makefiles, but makes things a lot more symmetric.  A future mod will
    straighten out function naming in the threading interface.
    
    As a positive side effect, the X2 build now works again.  However, there are
    two new failures in X2 "release" testing:
      test/release/examples/programs/prodCons
      test/release/examples/tutorials/taskParallel
    I will tackle those next.
    
    While I was here, I also changed the name of the source file that implements
    "pthreads-minimal" threading, so as to match the convention used by the other
    threading implementations.
    
    The specific file changes are:
    
    runtime/src/chpl_sync_single_generic.c
    runtime/src/chpl_tasks_generic.c
      (Both removed.)
      Remove the old generic threading implementation.
    
    runtime/src/Makefile.share
      No longer compile the two files removed above.
    
    runtime/threads-generic
    runtime/threads-generic/Makefile
    runtime/threads-generic/Makefile.include
    runtime/threads-generic/Makefile.share
    runtime/threads-generic/chpl_generic_threads.c
      (All added.)
      Combine the contents of the two files removed above into a single file, and
      create a new separate subdirectory for the generic implementation. 
    
    runtime/include/chpl_threads_generic.h
      (Removed.)
      Remove the old generic threading implementation #include file.
    
    runtime/include/threads-generic
    runtime/include/threads-generic/chpl_threads_generic.h
      (Both added.)
      Create a new separate subdirectory for the generic threading implementation
      #include file.
    
    runtime/threads-pthreads-minimal/chpl_threads.c
      (Removed.)
    runtime/threads-pthreads-minimal/chpl_pthreads_minimal_threads.c
      (Added).
    runtime/threads-pthreads-minimal/Makefile.share
      Change the name of the source file that implements "pthreads-minimal"
      threading.
    
    runtime/threads-pthreads/Makefile.include
    runtime/threads-pthreads-minimal/Makefile.include
      Set a "make" variable indicating that the generic threading implementation
      will be used, and thus needs to be built.
    
    runtime/Makefile.help
      When it is needed, include the generic threading implementation in runtime
      and TAGS builds.
    
    runtime/make/Makefile.runtime.head
      When building the runtime, search the generic threading implementation
      include subdirectory in additional to that for the specific implementation.
    
    runtime/etc/Makefile.include
      When building a user program, search the generic threading implementation
      include subdirectory in additional to that for the specific implementation.
    
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@16084 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/threads-generic/chpl_generic_threads.c
+++ runtime/threads-generic/chpl_generic_threads.c
@@ -1,5 +1,5 @@
 //
-// Generic implementation of Chapel tasking interface
+// Generic implementation of Chapel threading interface
 //
 
 #ifdef __OPTIMIZE__
@@ -22,13 +22,6 @@
 #include <errno.h>
 #include <sys/time.h>
 #include <unistd.h>
-
-
-//
-// This file is effectively empty unless the thread-specific configuration
-// file says it is needed.
-//
-#ifdef USE_GENERIC_TASK_MANAGEMENT
 
 
 //
@@ -112,6 +105,156 @@ static task_pool_p add_to_task_pool(chpl_fn_p,
                                     void*,
                                     chpl_bool,
                                     chpl_task_list_p);
+
+
+// Sync variables
+
+static void sync_wait_and_lock(chpl_sync_aux_t *s,
+                               chpl_bool want_full,
+                               int32_t lineno,
+                               chpl_string filename) {
+  chpl_mutex_lock(s->lock);
+  while (s->is_full != want_full) {
+    if (chpl_thread_set_block_loc(lineno, filename)) {
+      // all other tasks appear to be blocked
+      struct timeval deadline, now;
+      chpl_bool timed_out;
+      gettimeofday(&deadline, NULL);
+      deadline.tv_sec += 1;
+      do {
+        timed_out = threadlayer_sync_suspend(s, &deadline);
+        if (s->is_full != want_full && !timed_out)
+            gettimeofday(&now, NULL);
+      } while (s->is_full != want_full
+               && !timed_out
+               && (now.tv_sec < deadline.tv_sec
+                   || (now.tv_sec == deadline.tv_sec
+                       && now.tv_usec < deadline.tv_usec)));
+      if (s->is_full != want_full)
+        chpl_thread_check_for_deadlock();
+    }
+    else {
+      do {
+        (void) threadlayer_sync_suspend(s, NULL);
+      } while (s->is_full != want_full);
+    }
+    chpl_thread_unset_block_loc();
+  }
+}
+
+void chpl_sync_lock_generic(chpl_sync_aux_t *s) {
+  chpl_mutex_lock(s->lock);
+}
+
+void chpl_sync_unlock_generic(chpl_sync_aux_t *s) {
+  chpl_mutex_unlock(s->lock);
+}
+
+void chpl_sync_wait_full_and_lock_generic(chpl_sync_aux_t *s,
+                                          int32_t lineno,
+                                          chpl_string filename) {
+  sync_wait_and_lock(s, true, lineno, filename);
+}
+
+void chpl_sync_wait_empty_and_lock_generic(chpl_sync_aux_t *s,
+                                           int32_t lineno,
+                                           chpl_string filename) {
+  sync_wait_and_lock(s, false, lineno, filename);
+}
+
+void chpl_sync_mark_and_signal_full_generic(chpl_sync_aux_t *s) {
+  s->is_full = true;
+  threadlayer_sync_awaken(s);
+  chpl_sync_unlock_generic(s);
+}
+
+void chpl_sync_mark_and_signal_empty_generic(chpl_sync_aux_t *s) {
+  s->is_full = false;
+  threadlayer_sync_awaken(s);
+  chpl_sync_unlock_generic(s);
+}
+
+chpl_bool chpl_sync_is_full_generic(void *val_ptr,
+                                    chpl_sync_aux_t *s,
+                                    chpl_bool simple_sync_var) {
+  return s->is_full;
+}
+
+void chpl_init_sync_aux_generic(chpl_sync_aux_t *s) {
+  s->is_full = false;
+  s->lock = chpl_mutex_new();
+  threadlayer_init_sync(s);
+}
+
+void chpl_destroy_sync_aux_generic(chpl_sync_aux_t *s) {
+  chpl_free(s->lock, 0, 0);
+  threadlayer_destroy_sync(s);
+}
+
+
+// Single variables
+
+void chpl_single_lock_generic(chpl_single_aux_t *s) {
+  chpl_mutex_lock((chpl_mutex_t*)s->lock);
+}
+
+void chpl_single_unlock_generic(chpl_single_aux_t *s) {
+  chpl_mutex_unlock((chpl_mutex_t*)s->lock);
+}
+
+void chpl_single_wait_full_generic(chpl_single_aux_t *s,
+                                   int32_t lineno, chpl_string filename) {
+  chpl_mutex_lock(s->lock);
+  while (!s->is_full) {
+    if (chpl_thread_set_block_loc(lineno, filename)) {
+      // all other tasks appear to be blocked
+      struct timeval deadline, now;
+      chpl_bool timed_out;
+      gettimeofday(&deadline, NULL);
+      deadline.tv_sec += 1;
+      do {
+        timed_out = threadlayer_single_suspend(s, &deadline);
+        if (!s->is_full && !timed_out)
+            gettimeofday(&now, NULL);
+      } while (!s->is_full
+               && !timed_out
+               && (now.tv_sec < deadline.tv_sec
+                   || (now.tv_sec == deadline.tv_sec
+                       && now.tv_usec < deadline.tv_usec)));
+      if (!s->is_full)
+        chpl_thread_check_for_deadlock();
+    }
+    else {
+      do {
+        (void) threadlayer_single_suspend(s, NULL);
+      } while (!s->is_full);
+    }
+    chpl_thread_unset_block_loc();
+  }
+}
+
+void chpl_single_mark_and_signal_full_generic(chpl_single_aux_t *s) {
+  s->is_full = true;
+  threadlayer_single_awaken(s);
+  chpl_single_unlock_generic(s);
+}
+
+chpl_bool chpl_single_is_full_generic(void *val_ptr,
+                                      chpl_single_aux_t *s,
+                                      chpl_bool simple_single_var) {
+  return s->is_full;
+}
+
+void chpl_init_single_aux_generic(chpl_single_aux_t *s) {
+  s->is_full = false;
+  s->lock = chpl_mutex_new();
+  threadlayer_init_single(s);
+}
+
+void chpl_destroy_single_aux_generic(chpl_single_aux_t *s) {
+  chpl_free(s->lock, 0, 0);
+  threadlayer_destroy_single(s);
+}
 
 
 // Tasks
@@ -1027,6 +1170,3 @@ void chpl_thread_check_for_deadlock() {
 
   chpl_exit_any(1);
 }
-
-
-#endif  // USE_GENERIC_TASK_MANAGEMENT
