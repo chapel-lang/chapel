6377f8146f389f45fa7e25d146455f5054aa41d7
Steve Deitz <stevedeitz>
2009-10-02 04:45:34 +0000

    Extended the optimization removeUnnecessaryAutoCopyCalls to remove unnecessary initCopy calls for primitive types and records made up of primitive types or other records to which this optimization applies.  This optimization mainly impacts generated code size.  These initCopy calls were copying the fields of these records into a newly constructed record.  When our constructor/copy constructor story is better fleshed out, this optimization may need to be revisited since it would short-circuit calls to the constructor.
    For the function
      def foo() { forall i in 1..10 do i; }
    this change decreases the number of lines of generated code from 130 to 80.
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@16120 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- compiler/optimizations/removeUnnecessaryAutoCopyCalls.cpp
+++ compiler/optimizations/removeUnnecessaryAutoCopyCalls.cpp
@@ -73,7 +73,60 @@ void removeUnnecessaryAutoCopyCalls(FnSymbol* fn) {
   }
 }
 
+static bool
+isPrimitiveInitCopy(Vec<Type*>& primitiveInitCopyTypeSet, Type* type) {
+  if (primitiveInitCopyTypeSet.set_in(type))
+    return true;
+  if (is_bool_type(type) || is_int_type(type) || is_uint_type(type) ||
+      is_real_type(type) || is_imag_type(type) || is_complex_type(type) ||
+      is_enum_type(type) || isClass(type))
+    return true;
+  if (type->symbol->hasFlag(FLAG_ARRAY) ||
+      type->symbol->hasFlag(FLAG_DOMAIN) ||
+      type->symbol->hasFlag(FLAG_DISTRIBUTION) ||
+      type->symbol->hasFlag(FLAG_SYNC) ||
+      type->symbol->hasFlag(FLAG_REF) ||
+      type->symbol->hasFlag(FLAG_ITERATOR_RECORD))
+    return false;
+  if (isRecord(type)) {
+    ClassType* ct = toClassType(type);
+    INT_ASSERT(ct);
+    for_fields(field, ct) {
+      if (!isPrimitiveInitCopy(primitiveInitCopyTypeSet, field->type))
+        return false;
+    }
+    return true;
+  }
+  return false;
+}
+
 void removeUnnecessaryAutoCopyCalls() {
+  //
+  // remove pointless initCopy calls, e.g., initCopy calls on records of
+  // primitive types
+  //
+  compute_call_sites();
+
+  Vec<Type*> primitiveInitCopyTypeSet;
+  
+  forv_Vec(FnSymbol, fn, gFnSymbols) {
+    if (!strcmp(fn->name, "chpl__initCopy") &&
+        fn->numFormals() == 1 &&
+        fn->getFormal(1)->type == fn->retType) {
+      if (isPrimitiveInitCopy(primitiveInitCopyTypeSet, fn->retType)) {
+        forv_Vec(CallExpr, call, *fn->calledBy) {
+          CallExpr* move = toCallExpr(call->parentExpr);
+          INT_ASSERT(move && move->isPrimitive(PRIM_MOVE));
+          call->replace(call->get(1)->remove());
+        }
+      }
+    }
+  }
+
+  //
+  // remove matched pairs of autoCopy and autoDestroy calls marked with the
+  // removable auto destroy pragma
+  //
   forv_Vec(FnSymbol, fn, gFnSymbols) {
     removeUnnecessaryAutoCopyCalls(fn);
   }
