81b20520229fdcb051b64d9af119a1d51945148e
Brad Chamberlain <bradcray@users.noreply.github.com>
2010-06-07 21:58:15 +0000

    Renaming 'nanos' to 'nanox' for consistency
    ('nanox' is the alphanumeric name for Nanos++ which is what the
    project's going by these days, so it seemed like the type of
    identifier we'd typically use -- up to this point I was using
    'nanox' for the third-party directory and 'nanos' for the CHPL_TASKS
    setting).
    
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@17285 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/include/tasks/nanos/tasks-nanos.h
+++
@@ -1,6 +0,0 @@
-#ifndef _tasks_nanos_h_
-#define _tasks_nanos_h_
-
-#include <chpl_nanos.h>
-
-#endif
---
+++ runtime/include/tasks/nanox/tasks-nanox.h
@@ -0,0 +1,6 @@
+#ifndef _tasks_nanox_h_
+#define _tasks_nanox_h_
+
+#include <chpl_nanos.h>
+
+#endif
