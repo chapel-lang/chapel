chapel-lang/chapel

commit 998b2edbe0475ced30190e1f0e26e38eb107e2b4

Runtime modifications: (1) Added support for distributed transactions that span multiple locales. -- Added runtime support for stm_tx_comm_fork, stm_tx_comm_get, and stm_tx_comm_put calls in the stm/comm/gasnet layer using gasnet AM messages. -- Extended stm_tx_abort and stm_tx_commit calls to perform abort and commit operations spanning multiple locales. This is also implemented only for the comm-gasnet layer using gasnet AM messages. -- Added support for managing transaction descriptors / state on multiple locales. -- When a distributed transaction aborts, we first do a partial rollback to the tx_fork_wrapper routine within the stm/comm/gasnet layer. This routine in turn communicates the failure to the locale from which sent the request.
(2) Added support for dynamic memory allocation / free within transactions.
-- Added runtime support for stm_tx_malloc and stm_tx_free calls. 
-- Memory freed within a transaction is freed only when a transaction
commits. Likewise, memory allocated within a transaction is made globally
visible only when a transaction commits. 


Compiler modifications -- 
(1) Bug fixes in the codegen pass.
(2) Cleaned up the transactional cloning mechanism and added support for
cloning functions that are called via the stm_tx_fork call. 
-- As in the past, we are still ignoring any atomic blocks within cloned
routines. This will need to change once we support anything other than
flat-nested transactions (say open- or closed- nesting). 



git-svn-id: http://svn.code.sf.net/p/chapel/code/branches/collaborations/gtm@17371 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

runtime/include/stm/gtm/stm-gtm.h
@@ -1,14 +1,15 @@
 #ifndef _stm_gtm_h_
 #define _stm_gtm_h_
 
+#include <assert.h>
 #include <setjmp.h> 
+#include "stm-gtm-memory.h"
 
 //
 // GTM specific macros
 //
 
-#define TX_OK 1
-#define TX_FAIL 0
+enum {TX_FAIL = 0, TX_OK, TX_BUSY};
 
 #define GTM_Safe(tmptx, fncall) do {		\
     if (fncall != TX_OK) {			\
@@ -20,7 +21,7 @@
 
 #define NOLOCALE -1
 
-enum { TX_IDLE = 0, TX_ACTIVE, TX_COMMIT };
+enum { TX_IDLE = 0, TX_ACTIVE, TX_COMMIT, TX_ABORT, TX_AMACTIVE, TX_AMCOMMIT, TX_AMABORT };
 
 extern int32_t chpl_localeID, chpl_numLocales;   // see src/chplcomm.c
 #define MYLOCALE chpl_localeID
@@ -31,7 +32,8 @@ typedef gtm_word_t* gtm_word_p;
 #define GTMWORDSIZE sizeof(gtm_word_t)
 
 typedef jmp_buf chpl_stm_tx_env_t;
-#define chpl_stm_setjmp(txenv) setjmp(txenv)
+
+#define chpl_stm_tx_set_env(txenv) setjmp(txenv)
 
 //
 // read set entry and read set
@@ -74,16 +76,18 @@ typedef struct __writeSet_t {
 //
 
 typedef struct __chpl_stm_tx_t {
+  int32_t id;
   int32_t status;            // TX_IDLE, TX_LACTIVE, etc.
   int32_t nestlevel;         // flat nesting  
-  int32_t srclocale;         // locale that called tx_create 
-  int32_t txlocale;          // locale that called outermost tx_begin
+  int32_t locale;            // locale that called tx_create 
   int32_t numremlocales;     // number of remote locales tx has state
   int32_t* remlocales;       // list of locales on which tx has state
   read_set_t readset;               
   write_set_t writeset;      
   gtm_word_t timestamp;      // commit timestamp
+  chpl_bool rollback;
   chpl_stm_tx_env_t env;     // stores program execution state
+  memset_t* memset;          // tracks memory allocated / freed 
 } chpl_stm_tx_t;
 
 
@@ -106,34 +110,53 @@ static volatile gtm_word_t locks[LOCK_ARRAY_SIZE];
 static volatile gtm_word_t gclock;
 
 //
-// internal interface
+// internal interface for descriptor management (purely local)
 //
 
-void gtm_tx_initialize(chpl_stm_tx_t* tx, int32_t srclocale);
+void gtm_tx_init(void);
+chpl_stm_tx_t* gtm_tx_create(int32_t txid, int32_t txlocale);
 void gtm_tx_cleanup(chpl_stm_tx_t* tx);
+void gtm_tx_destroy(chpl_stm_tx_t* tx);
+
+chpl_stm_tx_t* gtm_tx_comm_create(int32_t txid, int32_t txlocale, int32_t txstatus); 
+void gtm_tx_comm_register(chpl_stm_tx_t* tx, int32_t dstlocale); 
+void gtm_tx_comm_destroy(chpl_stm_tx_t* tx);
+
+//
+// internal interface for transactional operations (purely local)
+//
 
 int gtm_tx_commitPh1(chpl_stm_tx_t* tx);
 int gtm_tx_commitPh2(chpl_stm_tx_t* tx);
 void gtm_tx_abort(chpl_stm_tx_t* tx);
-
 int gtm_tx_load(chpl_stm_tx_t* tx, void* dstaddr, void* srcaddr, size_t size);
 int gtm_tx_load_wrap(chpl_stm_tx_t* tx, void* dstaddr, void* srcaddr, size_t size);
 int gtm_tx_load_word(chpl_stm_tx_t* tx, gtm_word_p dstaddr, gtm_word_p srcaddr);
-
 int gtm_tx_store(chpl_stm_tx_t* tx, void* srcaddr, void* dstaddr, size_t size);
 int gtm_tx_store_wrap(chpl_stm_tx_t* tx, void* srcaddr, void*  dstaddr, size_t size);
 int gtm_tx_store_word(chpl_stm_tx_t* tx, gtm_word_p srcaddr, gtm_word_p dstaddr, gtm_word_t mask);
 
-void gtm_comm_init(void);
-void gtm_comm_exit(void);
+//
+// internal interface for transactional operations (purely remote)
+//
 
-int gtm_comm_tx_commitPh1(chpl_stm_tx_t* tx);
-int gtm_comm_tx_commitPh2(chpl_stm_tx_t* tx);
-void gtm_comm_tx_abort(chpl_stm_tx_t* tx);
+int gtm_tx_comm_commitPh1(chpl_stm_tx_t* tx);
+int gtm_tx_comm_commitPh2(chpl_stm_tx_t* tx);
+void gtm_tx_comm_abort(chpl_stm_tx_t* tx);
+int gtm_tx_comm_get(chpl_stm_tx_t* tx, void* dstaddr, int32_t srclocale, void* srcaddr, size_t size);
+int gtm_tx_comm_put(chpl_stm_tx_t* tx, void* srcaddr, int32_t dstlocale, void* dstaddr, size_t size);
+int gtm_tx_comm_fork(chpl_stm_tx_t* tx, int32_t dstlocale, chpl_fn_int_t fid, void *arg, size_t argsize);
 
-int gtm_comm_tx_get(chpl_stm_tx_t* tx, void* dstaddr, int32_t srclocale, void* srcaddr, size_t size);
-int gtm_comm_tx_put(chpl_stm_tx_t* tx, void* srcaddr, int32_t dstlocale, void* dstaddr, size_t size);
+//
+// internal interface for memory management (purely local)
+//
 
-int gtm_comm_tx_fork(chpl_stm_tx_t* tx, int tgtlocale, chpl_fn_int_t fid, void *arg, int arg_size);
+memset_t* gtm_tx_memset_create(chpl_stm_tx_t* tx);
+void gtm_tx_memset_destroy(chpl_stm_tx_t* tx);
+void gtm_tx_memset_cleanup(chpl_stm_tx_t* tx);
+void* gtm_tx_malloc(chpl_stm_tx_t* tx, size_t number, size_t size, chpl_memDescInt_t description, int32_t lineno, chpl_string filename);
+void gtm_tx_free(chpl_stm_tx_t* tx, void* addr, int32_t lineno, chpl_string filename);
+void gtm_tx_memset_commit(chpl_stm_tx_t* tx);
+void gtm_tx_memset_abort(chpl_stm_tx_t* tx);
 
 #endif

commit 0266e266f3af4d20496f023081417b725fac330e

Fixed a nasty bug in the stm-gtm-comm-gasnet implementation where we were returning from the comm layer sooner than we ought to.
Fixed another nasty bug that was introduced in r17371 when I refactored
the code while implementing non-aligned 4-bit STM read/write operations.

Both these cases were either not happening when running things within
gdb or on the single-locale case.  

Changed how and when we set an unique id for transactions, required
to maintain distributed transactional state. Note, the id is unique only
on a given locale, but is globally unique since we use <localeId, txid>
to identify transactions globally. Transactions that execute on a single
node and perform no remote operations no longer incur this overhead. 



git-svn-id: http://svn.code.sf.net/p/chapel/code/branches/collaborations/gtm@17475 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

runtime/include/stm/gtm/stm-gtm.h
@@ -17,7 +17,7 @@ enum {TX_FAIL = 0, TX_OK, TX_BUSY};
     }						\
   } while (0)
 
-#define RWSETSIZE 4096
+#define RWSETSIZE 128
 
 #define NOLOCALE -1
 

runtime/src/stm/gtm/comm/gasnet/stm-gtm-comm-gasnet.c
@@ -78,19 +78,25 @@ void tx_abort_wrapper(tx_generic_t* buf) {
   chpl_stm_tx_p tx = NULL;
   int status = TX_OK;  
 
-  tx = gtm_tx_comm_create(buf->txid, buf->txlocale, buf->txstatus);
+  //chpl_msg(0, "As ");
 
+  // perform the transactional abort
+  tx = gtm_tx_comm_create(buf->txid, buf->txlocale, buf->txstatus);
   gtm_tx_abort(tx);
 
   GASNET_Safe(gasnet_AMRequestMedium1(buf->caller, TX_SIGNAL,
 				      &(buf->commstatus), sizeof(int*), 
 				      status));
 
-  chpl_free(buf, 0, 0);
+  chpl_free(buf, __LINE__, __FILE__);
+
+  //chpl_msg(0, "Ae ");
 }
 
 void AM_tx_abort(gasnet_token_t token, void* msg, size_t nbytes) {
-  tx_generic_t *buf = (tx_generic_t*) chpl_malloc(nbytes, sizeof(char), CHPL_RT_MD_STM_AM_GENERIC_T, 0, 0);
+  tx_generic_t *buf = (tx_generic_t*) chpl_malloc(nbytes, sizeof(char),
+						  CHPL_RT_MD_STM_AM_GENERIC_T,
+						  __LINE__, __FILE__);
   memcpy(buf, msg, nbytes);
   CHPL_BEGIN((chpl_fn_p)tx_abort_wrapper, (void*)buf, true, true, NULL);
 }
@@ -101,28 +107,31 @@ void gtm_tx_comm_abort(chpl_stm_tx_p tx) {
   int32_t i; 
   int commstatus = TX_BUSY;
 
-  buf = (tx_generic_t*) chpl_malloc(1, bufsize, CHPL_RT_MD_STM_AM_GENERIC_T, 0, 0);
+  buf = (tx_generic_t*) chpl_malloc(1, bufsize, 
+				    CHPL_RT_MD_STM_AM_GENERIC_T, 
+				    __LINE__, __FILE__);
   buf->txid = tx->id;
   buf->txlocale = tx->locale;
   buf->caller = chpl_localeID;
   buf->txstatus = tx->status;
   buf->commstatus = &commstatus;
-
+  
   for (i = 0; i <= tx->numremlocales; i++) {
     GASNET_Safe(gasnet_AMRequestMedium0(tx->remlocales[i], TX_ABORT, buf, bufsize));
     GASNET_BLOCKUNTIL(commstatus != TX_BUSY);
   }
   
-  chpl_free(buf, 0, 0);
+  chpl_free(buf, __LINE__, __FILE__);
 }
 
 void tx_commitPh1_wrapper(tx_generic_t* buf) {
   chpl_stm_tx_p tx = NULL;
   int status = TX_OK;  
 
+  //chpl_msg(0, "C1s ");
+  
+  // perform the transactional commit
   tx = gtm_tx_comm_create(buf->txid, buf->txlocale, buf->txstatus);
-
-  assert(tx->status == TX_AMCOMMIT);
   GTM_Safe(tx, gtm_tx_commitPh1(tx));
   if (tx->status == TX_AMABORT)
     status = TX_FAIL;
@@ -131,11 +140,15 @@ void tx_commitPh1_wrapper(tx_generic_t* buf) {
 				      &(buf->commstatus), sizeof(int*), 
 				      status));
 
-  chpl_free(buf, 0, 0);
+  chpl_free(buf, __LINE__, __FILE__);
+
+  //chpl_msg(0, "C1e ");
 }
 
 void AM_tx_commitPh1(gasnet_token_t token, void* msg, size_t nbytes) {
-  tx_generic_t *buf = (tx_generic_t*) chpl_malloc(nbytes, sizeof(char), CHPL_RT_MD_STM_AM_GENERIC_T, 0, 0);
+  tx_generic_t *buf = (tx_generic_t*) chpl_malloc(nbytes, sizeof(char),
+						  CHPL_RT_MD_STM_AM_GENERIC_T,
+						  __LINE__, __FILE__);
   memcpy(buf, msg, nbytes);
   CHPL_BEGIN((chpl_fn_p)tx_commitPh1_wrapper, (void*)buf, true, true, NULL);
 }
@@ -144,9 +157,11 @@ int gtm_tx_comm_commitPh1(chpl_stm_tx_p tx) {
   tx_generic_t *buf;
   size_t bufsize = sizeof(tx_generic_t);
   int32_t i;
-  int commstatus;
+  int commstatus = TX_BUSY;
 
-  buf = (tx_generic_t*) chpl_malloc(1, bufsize, CHPL_RT_MD_STM_AM_GENERIC_T, 0, 0);
+  buf = (tx_generic_t*) chpl_malloc(1, bufsize,
+				    CHPL_RT_MD_STM_AM_GENERIC_T,
+				    __LINE__, __FILE__);
   buf->txid = tx->id;
   buf->txlocale = tx->locale;
   buf->caller = chpl_localeID;
@@ -157,12 +172,13 @@ int gtm_tx_comm_commitPh1(chpl_stm_tx_p tx) {
     GASNET_Safe(gasnet_AMRequestMedium0(tx->remlocales[i], TX_COMMITPH1, buf, bufsize));
     GASNET_BLOCKUNTIL(commstatus != TX_BUSY);
     if (commstatus == TX_FAIL) {
-      chpl_free(buf, 0, 0);
+      chpl_free(buf, __LINE__, __FILE__);
       return TX_FAIL;
     }
   }
 
-  chpl_free(buf, 0, 0);
+  chpl_free(buf, __LINE__, __FILE__);
+
   return TX_OK;
 }
 
@@ -170,9 +186,10 @@ void tx_commitPh2_wrapper(tx_generic_t* buf) {
   chpl_stm_tx_p tx = NULL;
   int status = TX_OK;  
 
-  tx = gtm_tx_comm_create(buf->txid, buf->txlocale, buf->txstatus);
+  //chpl_msg(0, "C2s ");
 
-  assert(tx->status == TX_AMCOMMIT);
+  // perform the transactional commit
+  tx = gtm_tx_comm_create(buf->txid, buf->txlocale, buf->txstatus);
   GTM_Safe(tx, gtm_tx_commitPh2(tx));
   if (tx->status == TX_AMABORT)
     status = TX_FAIL;
@@ -181,12 +198,17 @@ void tx_commitPh2_wrapper(tx_generic_t* buf) {
 				      &(buf->commstatus), sizeof(int*), 
 				      status));
 
+  // free the transaction descriptor
   gtm_tx_comm_destroy(tx);
-  chpl_free(buf, 0, 0);
+  chpl_free(buf, __LINE__, __FILE__);
+
+  //chpl_msg(0, "C2e ");
 }
 
 void AM_tx_commitPh2(gasnet_token_t token, void* msg, size_t nbytes) {
-  tx_generic_t *buf = (tx_generic_t*) chpl_malloc(nbytes, sizeof(char), CHPL_RT_MD_STM_AM_GENERIC_T, 0, 0);
+  tx_generic_t *buf = (tx_generic_t*) chpl_malloc(nbytes, sizeof(char), 
+						  CHPL_RT_MD_STM_AM_GENERIC_T,
+						  __LINE__, __FILE__);
   memcpy(buf, msg, nbytes);
   CHPL_BEGIN((chpl_fn_p)tx_commitPh2_wrapper, (void*)buf, true, true, NULL);
 }
@@ -195,9 +217,11 @@ int gtm_tx_comm_commitPh2(chpl_stm_tx_p tx) {
   tx_generic_t *buf;
   size_t bufsize = sizeof(tx_generic_t);
   int32_t i;
-  int commstatus;
+  int commstatus = TX_BUSY;
 
-  buf = (tx_generic_t*) chpl_malloc(1, bufsize, CHPL_RT_MD_STM_AM_GENERIC_T, 0, 0);
+  buf = (tx_generic_t*) chpl_malloc(1, bufsize, 
+				    CHPL_RT_MD_STM_AM_GENERIC_T,
+				    __LINE__, __FILE__);
   buf->txid = tx->id;
   buf->txlocale = tx->locale;
   buf->caller = chpl_localeID;
@@ -207,13 +231,10 @@ int gtm_tx_comm_commitPh2(chpl_stm_tx_p tx) {
   for (i = 0; i <= tx->numremlocales; i++) {
     GASNET_Safe(gasnet_AMRequestMedium0(tx->remlocales[i], TX_COMMITPH2, buf, bufsize));
     GASNET_BLOCKUNTIL(commstatus != TX_BUSY);
-    if (commstatus == TX_FAIL) {
-      chpl_free(buf, 0, 0);
-      return TX_FAIL;
-    }
   }
 
-  chpl_free(buf, 0, 0);
+  chpl_free(buf, __LINE__, __FILE__);
+
   return TX_OK;
 }
 
@@ -230,17 +251,18 @@ void tx_get_wrapper(tx_get_t* buf) {
   tx_getdata_t* getdata;
   size_t getdatasize = sizeof(tx_getdata_t) + buf->datasize;
 
-  tx = gtm_tx_comm_create(buf->txid, buf->txlocale, buf->txstatus);
+  //chpl_msg(0, "Gs ");
 
   // build the return payload
   getdata = (tx_getdata_t*) chpl_malloc(1, getdatasize, 
-					CHPL_RT_MD_STM_AM_GETDATA_T, 0, 0);
+					CHPL_RT_MD_STM_AM_GETDATA_T, 
+					__LINE__, __FILE__);
   getdata->retaddr = buf->retaddr;
   getdata->commstatus = buf->commstatus;
   getdata->datasize = buf->datasize;
 
   // perform transactional load
-  assert(tx->status == TX_AMACTIVE);
+  tx = gtm_tx_comm_create(buf->txid, buf->txlocale, buf->txstatus);
   GTM_Safe(tx, gtm_tx_load_wrap(tx, &(getdata->data), buf->remaddr, buf->datasize));
   if (tx->status == TX_AMABORT) {
     status = TX_FAIL;
@@ -253,12 +275,16 @@ void tx_get_wrapper(tx_get_t* buf) {
 					getdata, getdatasize, status));
   }
 
-  chpl_free(buf, 0, 0);
-  chpl_free(getdata, 0, 0);
+  chpl_free(buf, __LINE__, __FILE__);
+  chpl_free(getdata, __LINE__, __FILE__);
+
+  //chpl_msg(0, "Ge ");
 }
  
 void AM_tx_get (gasnet_token_t token, void* msg, size_t nbytes) {
-  tx_get_t *buf = (tx_get_t*) chpl_malloc(nbytes, sizeof(char), CHPL_RT_MD_STM_AM_GET_T, 0, 0);
+  tx_get_t *buf = (tx_get_t*) chpl_malloc(nbytes, sizeof(char), 
+					  CHPL_RT_MD_STM_AM_GET_T, 
+					  __LINE__, __FILE__);
   memcpy(buf, msg, nbytes);
   CHPL_BEGIN((chpl_fn_p)tx_get_wrapper, (void*)buf, true, true, NULL);
 }
@@ -270,9 +296,10 @@ int gtm_tx_comm_get(chpl_stm_tx_p tx, void* addr, int32_t remlocale, void* remad
 
   gtm_tx_comm_register(tx, remlocale);
 
-  if (bufsize > gasnet_AMMaxMedium()) 
-    chpl_error("arg too large for GASNet AM Medium", 0, 0);
-  buf = (tx_get_t*) chpl_malloc(1, bufsize, CHPL_RT_MD_STM_AM_GET_T, 0, 0);
+  assert(bufsize < gasnet_AMMaxMedium()); 
+  buf = (tx_get_t*) chpl_malloc(1, bufsize, 
+				CHPL_RT_MD_STM_AM_GET_T, 
+				__LINE__, __FILE__);
   buf->txid = tx->id;
   buf->txlocale = tx->locale;
   buf->caller = chpl_localeID;
@@ -285,7 +312,8 @@ int gtm_tx_comm_get(chpl_stm_tx_p tx, void* addr, int32_t remlocale, void* remad
   GASNET_Safe(gasnet_AMRequestMedium0(remlocale, TX_GET, buf, bufsize));
   GASNET_BLOCKUNTIL(commstatus != TX_BUSY);
 
-  chpl_free(buf, 0, 0);
+  chpl_free(buf, __LINE__, __FILE__);
+
   return commstatus;
 }
 
@@ -293,9 +321,9 @@ void tx_put_wrapper(tx_put_t* buf) {
   chpl_stm_tx_p tx = NULL;
   int status = TX_OK;  
 
-  tx = gtm_tx_comm_create(buf->txid, buf->txlocale, buf->txstatus);
+  //chpl_msg(0, "Ps ");
 
-  assert(tx->status == TX_AMACTIVE);
+  tx = gtm_tx_comm_create(buf->txid, buf->txlocale, buf->txstatus);
   GTM_Safe(tx, gtm_tx_store_wrap(tx, &(buf->data), buf->remaddr, buf->datasize));
   if (tx->status == TX_AMABORT)
     status = TX_FAIL;
@@ -304,11 +332,15 @@ void tx_put_wrapper(tx_put_t* buf) {
 				      &(buf->commstatus), sizeof(int*), 
 				      status));
 
-  chpl_free(buf, 0, 0);
+  chpl_free(buf, __LINE__, __FILE__);
+
+  //chpl_msg(0, "Pe ");
 }
 
 void AM_tx_put (gasnet_token_t token, void* msg, size_t nbytes) {
-  tx_put_t *buf = (tx_put_t*) chpl_malloc(nbytes, sizeof(char), CHPL_RT_MD_STM_AM_PUT_T, 0, 0);
+  tx_put_t *buf = (tx_put_t*) chpl_malloc(nbytes, sizeof(char), 
+					  CHPL_RT_MD_STM_AM_PUT_T, 
+					  __LINE__, __FILE__);
   memcpy(buf, msg, nbytes);
   CHPL_BEGIN((chpl_fn_p)tx_put_wrapper, (void*)buf, true, true, NULL);
 }
@@ -320,9 +352,10 @@ int gtm_tx_comm_put(chpl_stm_tx_p tx, void* addr, int32_t remlocale, void* remad
 
   gtm_tx_comm_register(tx, remlocale);
 
-  if (bufsize > gasnet_AMMaxMedium()) 
-    chpl_error("arg too large for GASNet AM Medium", 0, 0);
-  buf = (tx_put_t*) chpl_malloc(1, bufsize, CHPL_RT_MD_STM_AM_PUT_T, 0, 0);
+  assert(bufsize < gasnet_AMMaxMedium()); 
+  buf = (tx_put_t*) chpl_malloc(1, bufsize, 
+				CHPL_RT_MD_STM_AM_PUT_T, 
+				__LINE__, __FILE__);
   buf->txid = tx->id;
   buf->txlocale = tx->locale;
   buf->caller = chpl_localeID;
@@ -335,7 +368,8 @@ int gtm_tx_comm_put(chpl_stm_tx_p tx, void* addr, int32_t remlocale, void* remad
   GASNET_Safe(gasnet_AMRequestMedium0(remlocale, TX_PUT, buf, bufsize));
   GASNET_BLOCKUNTIL(commstatus != TX_BUSY);
 
-  chpl_free(buf, 0, 0);
+  chpl_free(buf, __LINE__, __FILE__);
+
   return commstatus;
 }
 
@@ -343,9 +377,9 @@ void tx_fork_wrapper(tx_fork_t* buf) {
   chpl_stm_tx_p tx = NULL;
   int status = TX_OK;  
 
-  tx = gtm_tx_comm_create(buf->txid, buf->txlocale, buf->txstatus);
+  //chpl_msg(0, "Fs ");
 
-  assert(tx->status == TX_AMACTIVE);
+  tx = gtm_tx_comm_create(buf->txid, buf->txlocale, buf->txstatus);
   tx->rollback = true;
   if (!setjmp(tx->env)) {
     if (buf->argsize) 
@@ -363,11 +397,15 @@ void tx_fork_wrapper(tx_fork_t* buf) {
 				      &(buf->commstatus), sizeof(int*), 
 				      status));
 
-  chpl_free(buf, 0, 0);
+  chpl_free(buf, __LINE__, __FILE__);
+
+  //chpl_msg(0, "Fe ");
 }
 
 void AM_tx_fork(gasnet_token_t token, void* msg, size_t nbytes) {
-  tx_fork_t *buf = (tx_fork_t*) chpl_malloc(nbytes, sizeof(char), CHPL_RT_MD_STM_AM_FORK_T, 0, 0);
+  tx_fork_t *buf = (tx_fork_t*) chpl_malloc(nbytes, sizeof(char),
+					    CHPL_RT_MD_STM_AM_FORK_T,
+					    __LINE__, __FILE__);
   memcpy(buf, msg, nbytes);
   CHPL_BEGIN((chpl_fn_p)tx_fork_wrapper, (void*)buf, true, true, NULL);
 }
@@ -379,8 +417,10 @@ int gtm_tx_comm_fork(chpl_stm_tx_p tx, int32_t remlocale, chpl_fn_int_t fid, voi
   
   gtm_tx_comm_register(tx, remlocale);
 
-  assert(bufsize <= gasnet_AMMaxMedium()); 
-  buf = (tx_fork_t*) chpl_malloc(1, bufsize, CHPL_RT_MD_STM_AM_FORK_T, 0, 0);
+  assert(bufsize < gasnet_AMMaxMedium()); 
+  buf = (tx_fork_t*) chpl_malloc(1, bufsize,
+				 CHPL_RT_MD_STM_AM_FORK_T,
+				 __LINE__, __FILE__);
   buf->txid = tx->id;
   buf->txlocale = tx->locale;
   buf->caller = chpl_localeID;
@@ -394,6 +434,7 @@ int gtm_tx_comm_fork(chpl_stm_tx_p tx, int32_t remlocale, chpl_fn_int_t fid, voi
   GASNET_Safe(gasnet_AMRequestMedium0(remlocale, TX_FORK, buf, bufsize));
   GASNET_BLOCKUNTIL(commstatus != TX_BUSY);
 
-  chpl_free(buf, 0, 0);
+  chpl_free(buf, __LINE__, __FILE__);
+
   return commstatus;
 }

runtime/src/stm/gtm/stm-gtm-RV_EA_WB.c
@@ -84,9 +84,7 @@ int gtm_tx_commitPh1(chpl_stm_tx_t* tx) {
       chpl_error("STM Error: exceeded maximum version number", 0, 0);
     tx->timestamp = commitstamp;
   }
-  if (gtm_tx_readset_validate(tx) == TX_FAIL)
-    return TX_FAIL;
-  return TX_OK;
+  return gtm_tx_readset_validate(tx);
 }
 
 int gtm_tx_commitPh2(chpl_stm_tx_t* tx) {
@@ -104,7 +102,6 @@ int gtm_tx_commitPh2(chpl_stm_tx_t* tx) {
     }
   }
   gtm_tx_memset_commit(tx);
-  gtm_tx_cleanup(tx);
   return TX_OK;
 }
 

runtime/src/stm/gtm/stm-gtm-desc.c
@@ -25,16 +25,13 @@ void gtm_tx_init() {
 chpl_stm_tx_p gtm_tx_create(int32_t txid, int32_t txlocale) {
   chpl_stm_tx_p tx = (chpl_stm_tx_p) chpl_malloc(1, sizeof(chpl_stm_tx_t), 
 				     CHPL_RT_MD_STM_TX_DESCRIPTOR, 0 , 0);
-  int i;
 
+  tx->id = txid;
   tx->locale = txlocale;
   tx->status = TX_IDLE;
   tx->nestlevel = 0;
   tx->numremlocales = -1;
-  tx->remlocales = (int32_t*) chpl_malloc(NLOCALES, sizeof(int32_t), CHPL_RT_MD_STM_TX_REMLOCALES, 0, 0);
-  for (i = 0; i <= tx->numremlocales; i++) 
-    tx->remlocales[i] = -1;
-
+  tx->remlocales = NULL; 
   tx->readset.numentries = 0;
   tx->readset.size = RWSETSIZE;
   tx->readset.reallocate = 0;
@@ -43,46 +40,25 @@ chpl_stm_tx_p gtm_tx_create(int32_t txid, int32_t txlocale) {
   tx->writeset.size = RWSETSIZE;
   tx->writeset.reallocate = 0;
   tx->writeset.entries = (write_entry_t*) chpl_malloc(tx->writeset.size, sizeof(write_entry_t), CHPL_RT_MD_STM_TX_WRITESET, 0, 0);
-
   tx->timestamp = GET_CLOCK;
   tx->rollback = false;
   tx->memset = gtm_tx_memset_create(tx);
-
-  if (txid == -1) {
-    assert(txlocale == MYLOCALE);
-    tx->id = -1;
-    for (i = 0; i < MAXTDS; i++) {
-      CHPL_MUTEX_LOCK(&txarraylock);
-      if (gtm_tx_array[txlocale][i] == NULL) {
-	gtm_tx_array[txlocale][i] = tx;
-	tx->id = i; 
-	CHPL_MUTEX_UNLOCK(&txarraylock);
-	break;
-      }
-      CHPL_MUTEX_UNLOCK(&txarraylock);
-    }
-    if (tx->id == -1) 
-      chpl_error("Run out of tx descriptors", 0, 0);
-  } else {
-    assert(txlocale != MYLOCALE);
-    gtm_tx_array[txlocale][txid] = tx;
-    tx->id = txid;
-  }
     
   return tx;
 }
 
 void gtm_tx_destroy(chpl_stm_tx_p tx) {
-  chpl_free(tx->remlocales, 0, 0);
   chpl_free(tx->readset.entries, 0, 0);
   chpl_free(tx->writeset.entries, 0, 0);
   gtm_tx_memset_destroy(tx);
-  if (tx->locale == MYLOCALE) {
+  if (tx->id != -1 && tx->locale == MYLOCALE) {
+    assert(tx->numremlocales >= 0 && tx->remlocales != NULL);
+    chpl_free(tx->remlocales, 0, 0);
     CHPL_MUTEX_LOCK(&txarraylock);
     gtm_tx_array[tx->locale][tx->id] = NULL; 
     CHPL_MUTEX_UNLOCK(&txarraylock);
   } else {
-    gtm_tx_array[tx->locale][tx->id] = NULL; 
+    assert(tx->numremlocales == -1 && tx->remlocales == NULL);
   }
   chpl_free(tx, 0, 0);
 }
@@ -102,6 +78,34 @@ void gtm_tx_cleanup(chpl_stm_tx_p tx) {
 
 void gtm_tx_comm_register(chpl_stm_tx_p tx, int32_t dstlocale) {
   int i;
+
+  assert(tx->status == TX_ACTIVE);
+  if (tx->id == -1) {
+    // First time we are doing a remote operation
+    // Allocate and initialize remote locales array
+    assert(tx->numremlocales == -1 && tx->remlocales == NULL);
+    tx->remlocales = (int32_t*) chpl_malloc(NLOCALES, sizeof(int32_t), 
+					    CHPL_RT_MD_STM_TX_REMLOCALES, 
+					    0, 0);
+    for (i = 0; i < NLOCALES; i++) 
+      tx->remlocales[i] = -1;
+    
+    // Get a global id for this transaction
+    for (i = 0; i < MAXTDS; i++) {
+      CHPL_MUTEX_LOCK(&txarraylock);
+      if (gtm_tx_array[tx->locale][i] == NULL) {
+	gtm_tx_array[tx->locale][i] = tx;
+	CHPL_MUTEX_UNLOCK(&txarraylock);
+	tx->id = i; 
+	break;
+      }
+      CHPL_MUTEX_UNLOCK(&txarraylock);
+    }
+    if (tx->id == -1) 
+      chpl_error("Run out of tx descriptors", 0, 0);
+  }
+    
+  // Register the remote operation
   for (i = 0; i <= tx->numremlocales; i++) {
     if (tx->remlocales[i] == dstlocale) return;
   }
@@ -112,23 +116,36 @@ void gtm_tx_comm_register(chpl_stm_tx_p tx, int32_t dstlocale) {
 chpl_stm_tx_p gtm_tx_comm_create(int32_t txid, int32_t txlocale, int32_t txstatus) {
   chpl_stm_tx_p tx = NULL;
 
+  assert(txstatus != TX_IDLE);
   assert(txid >= 0 && txid < MAXTDS);
-  if ((tx = gtm_tx_array[txlocale][txid]) == NULL) {
-    tx = gtm_tx_create(txid, txlocale);
+
+  if (txstatus == TX_ACTIVE) { 
+    if ((tx = gtm_tx_array[txlocale][txid]) == NULL) {
+      tx = gtm_tx_create(txid, txlocale);
+      gtm_tx_array[txlocale][txid] = tx;
+    }
+    tx->status= TX_AMACTIVE;
+  } else if (txstatus == TX_COMMIT) {
+    if ((tx = gtm_tx_array[txlocale][txid]) == NULL) {
+      chpl_error("Descriptor not found for commit", 0, 0);
+    }
+    tx->status = TX_AMCOMMIT;
+  } else if (txstatus == TX_ABORT) {
+    if ((tx = gtm_tx_array[txlocale][txid]) == NULL) {
+      chpl_error("Descriptor not found for abort.", 0, 0);
+    }
+    tx->status = TX_AMABORT;
+  } else {
+    chpl_error("Descriptor setup for comm operation failed.", 0, 0);
   }
 
-  if (txstatus == TX_ACTIVE) 
-    tx->status= TX_AMACTIVE;
-  else if (txstatus == TX_COMMIT)
-    tx->status = TX_AMCOMMIT;
-  else if (txstatus == TX_ABORT)
-    tx->status = TX_AMABORT;
-  else
-    chpl_error("Descriptor setup for comm operation failed.", 0, 0);
+  assert(tx->rollback == false);
 
   return tx;
 }
 
 void gtm_tx_comm_destroy(chpl_stm_tx_p tx) {
+  assert(tx->id != -1 && tx->locale != MYLOCALE);
+  gtm_tx_array[tx->locale][tx->id] = NULL; 
   gtm_tx_destroy(tx);
 }

runtime/src/stm/gtm/stm-gtm-memory.c
@@ -14,7 +14,8 @@
 
 memset_t* gtm_tx_memset_create(chpl_stm_tx_p tx) {
   memset_t* mem  = (memset_t*) chpl_malloc(1, sizeof(memset_t), 
-					   CHPL_RT_MD_STM_TX_MEMSET, 0, 0);
+					   CHPL_RT_MD_STM_TX_MEMSET, 
+					   __LINE__, __FILE__);
   mem->allocated = mem->freed = NULL;
   return mem;
 }

runtime/src/stm/gtm/stm-gtm.c
@@ -27,7 +27,7 @@ chpl_stm_tx_p chpl_stm_tx_create() {
 }
 
 void chpl_stm_tx_destroy(chpl_stm_tx_p tx) {
-  assert(tx != NULL && tx->status == TX_IDLE);
+  assert(tx != NULL);
   gtm_tx_destroy(tx);
 }
 
@@ -102,6 +102,7 @@ void chpl_stm_tx_abort(chpl_stm_tx_p tx) {
     // clone spawned by the fork then rollback to the fork_wrapper.
     // for all other operations, just set the status and return 
     if (tx->env != NULL && tx->rollback) {
+      tx->status = TX_AMABORT;
       longjmp(tx->env, 1);
     } else {
       tx->status = TX_AMABORT; 
@@ -132,23 +133,16 @@ int gtm_tx_load(chpl_stm_tx_t* tx, void* dstaddr, void* srcaddr, size_t size) {
     dbyteaddr = (uint8_t*) dstaddr;
     for (; align < sizeof(gtm_word_t) && size > 0; align++, size--)
       *dbyteaddr++ = dval.b[align];
-    while (size >= GTMWORDSIZE) { 
-      dval.w = 0;
-      if (gtm_tx_load_word(tx, &dval.w, saddr++) == TX_FAIL) 
-	return TX_FAIL; 
-      for (align = 0; size > 0; align++, size--)
-	*dbyteaddr++ = dval.b[align];
-      size -= GTMWORDSIZE;
-    }
     daddr = (gtm_word_p) dbyteaddr;
   } else {
     saddr = (gtm_word_p) srcaddr;
     daddr = (gtm_word_p) dstaddr; 
-    while (size >= GTMWORDSIZE) { 
-      if (gtm_tx_load_word(tx, daddr++, saddr++) == TX_FAIL) 
-	return TX_FAIL;
-      size -= GTMWORDSIZE;
-    }
+  }
+   
+  while (size >= GTMWORDSIZE) { 
+    if (gtm_tx_load_word(tx, daddr++, saddr++) == TX_FAIL) 
+      return TX_FAIL;
+    size -= GTMWORDSIZE;
   }
 
   if (size > 0) {
@@ -199,7 +193,7 @@ int gtm_tx_load_wrap(chpl_stm_tx_p tx, void* dstaddr, void* srcaddr, size_t size
 
 void chpl_stm_tx_load(chpl_stm_tx_p tx, void* dstaddr, void* srcaddr, size_t size, int ln, chpl_string fn) {
   assert(tx != NULL);
-  assert(dstaddr != NULL && srcaddr != NULL);
+  assert(dstaddr != NULL && srcaddr != NULL && size > 0);
 
   if (size == 0) return;
   
@@ -222,26 +216,16 @@ int gtm_tx_store(chpl_stm_tx_p tx, void* srcaddr, void* dstaddr, size_t size) {
     }
     if (gtm_tx_store_word(tx, &sval.w, daddr++, mask.w) == TX_FAIL) 
       return TX_FAIL;
-    while (size >= GTMWORDSIZE) { 
-      sval.w = mask.w = 0;
-      for (align = 0; size > 0; align++, size--) {
-	sval.b[align] = *(uint8_t*) sbyteaddr++;
-	mask.b[align] = 0xFF;
-      }
-      if (gtm_tx_store_word(tx, &sval.w, daddr++, mask.w) == TX_FAIL) 
-	return TX_FAIL;
-      size -= GTMWORDSIZE;
-    }
     saddr = (gtm_word_p) sbyteaddr;
   } else {
     saddr = (gtm_word_p) srcaddr;
     daddr = (gtm_word_p) dstaddr; 
-    while (size >= GTMWORDSIZE) { 
-      if (gtm_tx_store_word(tx, saddr++, daddr++, ~(gtm_word_t) 0) == TX_FAIL) 
-	return TX_FAIL;
-      size -= GTMWORDSIZE;
-    }
   }
+  while (size >= GTMWORDSIZE) { 
+    if (gtm_tx_store_word(tx, saddr++, daddr++, ~(gtm_word_t) 0) == TX_FAIL) 
+      return TX_FAIL;
+    size -= GTMWORDSIZE;
+    }
 
   if (size > 0) {
     sval.w = mask.w = 0;   
@@ -287,27 +271,23 @@ int gtm_tx_store_wrap(chpl_stm_tx_p tx, void* srcaddr, void* dstaddr, size_t siz
 
 void chpl_stm_tx_store(chpl_stm_tx_p tx, void* srcaddr, void* dstaddr, size_t size, int ln, chpl_string fn) {
   assert(tx != NULL);
-  assert(dstaddr != NULL && srcaddr != NULL && size != 0);
+  assert(dstaddr != NULL && srcaddr != NULL && size > 0);
   
-  if (size == 0) return;
-
   GTM_Safe(tx, gtm_tx_store_wrap(tx, srcaddr, dstaddr, size));
 }
 
 void chpl_stm_tx_get(chpl_stm_tx_p tx, void* dstaddr, int32_t srclocale, void* srcaddr, size_t size, int ln, chpl_string fn) {
   assert(tx != NULL);
   assert(srclocale != MYLOCALE);
-
-  if (size == 0) return;
-  
+  assert(dstaddr != NULL && srcaddr != NULL && size > 0);
+ 
   GTM_Safe(tx, gtm_tx_comm_get(tx, dstaddr, srclocale, srcaddr, size));
 }
 
 void chpl_stm_tx_put(chpl_stm_tx_p tx, void* srcaddr, int32_t dstlocale, void* dstaddr, size_t size, int ln, chpl_string fn) {
   assert(tx != NULL);
   assert(dstlocale != MYLOCALE);
-  
-  if (size == 0) return;
+  assert(dstaddr != NULL && srcaddr != NULL && size > 0);
   
   GTM_Safe(tx, gtm_tx_comm_put(tx, srcaddr, dstlocale, dstaddr, size));
 }