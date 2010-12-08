03cbdc9d568682564dbdd115ddce031aae1da6c5
Sung-Eun Choi <sungeunchoi@users.noreply.github.com>
2010-12-08 00:05:59 +0000

    Update the zebra launcher to use chpl_launch_using_exec() as implemented in r18010.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@18032 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/src/launch/zebra/launch-zebra.c
+++ runtime/src/launch/zebra/launch-zebra.c
@@ -5,43 +5,27 @@
 #include "error.h"
 
 
-static char* chpl_launch_create_command(int argc, char* argv[],
-                                        int32_t numLocales) {
-  int i;
-  int size;
-  char baseCommand[256];
-  char* command;
+static char** chpl_launch_create_argv(int argc, char* argv[]) {
+  const int largc = 3;
+  char *largv[largc];
 
-  chpl_compute_real_binary_name(argv[0]);
+  largv[0] = (char *) "zebra";
+  largv[1] = (char *) "-fast";
+  largv[2] = (char *) "-r";
 
-  sprintf(baseCommand, "zebra -fast -r %s", chpl_get_real_binary_name());
-
-  size = strlen(baseCommand) + 1;
-
-  for (i=1; i<argc; i++) {
-    size += strlen(argv[i]) + 3;
-  }
-
-  command = chpl_malloc(size, sizeof(char), CHPL_RT_MD_COMMAND_BUFFER, -1, "");
-  
-  sprintf(command, "%s", baseCommand);
-  for (i=1; i<argc; i++) {
-    strcat(command, " '");
-    strcat(command, argv[i]);
-    strcat(command, "'");
-  }
-
-  if (strlen(command)+1 > size) {
-    chpl_internal_error("buffer overflow");
-  }
-
-  return command;
+  return chpl_bundle_exec_args(argc, argv, largc, largv);
 }
 
 
 int chpl_launch(int argc, char* argv[], int32_t numLocales) {
-  return chpl_launch_using_system(chpl_launch_create_command(argc, argv, numLocales),
-                                  argv[0]);
+  if (numLocales != 1) {
+    // This error should be taken care of before we get to this point
+    chpl_internal_error("The XMT launcher only supports numLocales==1");
+  }
+
+  return chpl_launch_using_exec("zebra",
+                                chpl_launch_create_argv(argc, argv),
+                                argv[0]);
 }
 
 
