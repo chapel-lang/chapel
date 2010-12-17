3b7c9e67c64d88e3d740a86914d7e663b06c96c4
Jonathan Claridge <claridge>
2010-12-17 00:29:39 +0000

    Updating regression script to take in the name of its output file as an argument, and to provide more useful information on failure.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@18062 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- test/studies/amr/lib/python/regression_test.py
+++ test/studies/amr/lib/python/regression_test.py
@@ -1,15 +1,19 @@
-import os, re
-
+import os, re, sys
 
 
 #|\""""""""""""""""""""""""|\
 #| >    approximateDiff    | >
 #|/________________________|/
 
-#---------------------------------------------------------
+#-----------------------------------------------------------
 # Takes the "approximate diff" of two files, allowing for
 # differences in real data up to the specified tolerance.
-#---------------------------------------------------------
+#
+# This isn't as sophisticated as a true diff in terms of
+# matching largest common subsections.  It's just meant
+# to speed the user's analysis if changing the code results
+# in structural or numerical differences.
+#-----------------------------------------------------------
 
 def approximateDiff(file_name_1, file_name_2, tolerance):
   
@@ -30,37 +34,47 @@ def approximateDiff(file_name_1, file_name_2, tolerance):
     lines_2 = file_2.readlines()
     difference = False
     any_difference = False
-  
-    if not(len(lines_1) == len(lines_2)):
-        print "Files are of different length"
-        difference = True
-        any_difference = True
     
-    if not(difference):
-        for i in range(len(lines_1)):
-            s1 = lines_1[i].split();
-            s2 = lines_2[i].split();
-            difference = False
-      
-            #==== Compare line lengths ====
-            if not(len(s1) == len(s2)):
-                print "Line ", i, ": number of elements differs"
-                print file_name_1, ": ", lines_1[i]
-                print file_name_2, ": ", lines_2[i]
-                difference = True
-                any_difference = True
-       
-            #==== Compare individual elements ====
-            if not(difference):
-                for j in range(len(s1)):
-                    difference = not(approximatelyEqual(s1[j],s2[j],tolerance))
-            
-                    if difference:
-                        print "Line ", i, ", element ", j, ": elements differ"
-                        print file_name_1, ": ", s1[j]
-                        print file_name_2, ": ", s2[j]
+    diff_output = []
+  
+    #==== Check file lengths ====
+    #--------------------------------------------------------------------
+    # A problem here will indicate that something is structurally wrong.
+    #--------------------------------------------------------------------
+    if not(len(lines_1) == len(lines_2)):
+        diff_output.append("Files are of different length")
 
-    return any_difference                    
+
+    #==== Check line by line ====
+    #----------------------------------------------------------------------
+    # This is where numerical differences will be highlighted.  Also, if
+    # the files are comparable up to a certain point, this will show where
+    # they begin to diverge.
+    #----------------------------------------------------------------------
+    for i in range(min(len(lines_1), len(lines_2))):
+        split_1 = lines_1[i].split();
+        split_2 = lines_2[i].split();
+  
+        if len(split_1) == len(split_2):
+            #-----------------------------------------------------------
+            # If lines have the same number of elements, then check for
+            # numerical differences.
+            #-----------------------------------------------------------
+            for j in range(len(split_1)):
+                if not(approximatelyEqual(split_1[j],split_2[j],tolerance)):
+                    diff_output.append("Line " +  str(i+1) + ", element " + str(j+1) + ": elements differ")
+                    diff_output.append("  " + file_name_1 + ": " + split_1[j])
+                    diff_output.append("  " + file_name_2 + ": " + split_2[j])
+        else:
+            #-----------------------------------------------------------
+            # If lines have a different number of elements, then print
+            # their contents.
+            #-----------------------------------------------------------
+            diff_output.append("Line " + str(i+1) + ": number of elements differs")
+            diff_output.append("  " + file_name_1 + ": " + lines_1[i])
+            diff_output.append("  " + file_name_2 + ": " + lines_2[i])
+
+    return diff_output                    
 # /|""""""""""""""""""""""""/|
 #< |    approximateDiff    < |
 # \|________________________\|
@@ -68,11 +82,19 @@ def approximateDiff(file_name_1, file_name_2, tolerance):
 
 
 
+#==== Name of the diff output file ====
+#----------------------------------------------------------
+# The name of this script is sys.argv[0].  The name of the
+# output file should follow immediately.
+#----------------------------------------------------------
+output_file = open(sys.argv[1], 'w')
 
+
+#==== Tolerance for approximate diff-ing ====
 tolerance = 1e-4
 
 
-#---- Locate data files in the regression_data directory ----
+#==== Locate data files in the regression_data directory ====
 regression_files = os.listdir("regression_data")
 data_pattern = re.compile("fort.q\d{4}")
 data_files = []
@@ -82,28 +104,28 @@ for file_name in regression_files:
         data_files.append(file_name)
         
         
-#---- Compare each data file to the same file in _output ----
-difference = False
+#==== Compare each data file to the same file in _output ====
+difference_detected = False
 failure = False
 for data_file in data_files:
     try:
-        difference = difference or approximateDiff("_output/"+data_file, "regression_data/"+data_file, tolerance)
+        diff_results = approximateDiff("_output/"+data_file, "regression_data/"+data_file, tolerance)
+        if len(diff_results) > 0:
+            for line in diff_results:
+                output_file.write(line + "\n")
+                difference_detected = True
     except:
+        #---- If the diff fails, indicate the file with which it crashed ----
+        output_file.write("Test failed.\n")
+        output_file.write("Could not use approximateDiff with file " + data_file + ".\n")
         failure = True
+        break
 
+#==== Write an OK message if there were no problems ====
+if not(difference_detected) and not(failure):
+    output_file.write("No change detected.\n")
+    
 
-
-#---- Write outcome to status file ----
-status_file = open("regression_status.txt", "w")
-
-if failure:
-    status_file.write("Warning: Test failed.\n")
-else:
-    if difference:
-        status_file.write("Warning: Change in data.\n")
-    else:
-        status_file.write("No change in data.\n")
-
-status_file.close()
+output_file.close()
 
 
