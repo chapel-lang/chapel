42864e8e09c03faf3ed8da83a42bbef2a920c0b7
Vassily Litvinov <vasslitvinov>
2011-02-03 02:06:01 +0000

    Compiler now generates an error if a constructor or destructor method specifies any return type.
    Added tests to verify the error.
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@18232 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- compiler/passes/semanticChecks.cpp
+++ compiler/passes/semanticChecks.cpp
@@ -140,6 +140,8 @@ checkNormalized(void) {
         USR_FATAL_CONT(fn, "iterators may not yield or return parameters");
     } else if (fn->hasFlag(FLAG_CONSTRUCTOR) &&
                !fn->hasFlag(FLAG_DEFAULT_CONSTRUCTOR)) {
+      if (fn->retExprType)
+        USR_FATAL_CONT(fn, "constructors may not declare a return type");
       for_formals(formal, fn) {
         Vec<SymExpr*> symExprs;
         collectSymExprs(formal, symExprs);
@@ -150,6 +152,9 @@ checkNormalized(void) {
           }
         }
       }
+    } else if (fn->hasFlag(FLAG_DESTRUCTOR)) {
+      if (fn->retExprType)
+        USR_FATAL_CONT(fn, "destructors may not declare a return type");
     }
   }
 }
