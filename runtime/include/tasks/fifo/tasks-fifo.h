adff85dd1139ec383ccbcbb9e9a758963c805311
Greg Titus <gbtitus@users.noreply.github.com>
2011-04-21 22:23:21 +0000

    Separate the threading layer interface into its own #include file.
    The threading layer interface use to be defined in the tasking layer's
    model-specific header file.  This change moves that definition into a
    standalone file.  Whether or not to use a threading layer is still up to
    the specific tasking layer, however.  If the tasking layer wants to do
    so, it should #include chpl-threads.h in the header file specific to its
    tasking model.
    
    While I was here, I also adjusted the preprocessor symbols that name the
    model-specific tasking and threading #include files.  They were overly
    general before.
    
    The changes in each file are as follows.
    
    include/chpl-tasks.h
      Fix the name of the preprocessor symbol that protects against
      #including this file multiple times.  (I didn't adjust it when I
      renamed this file in r18734.)
      Change the preprocessor symbol that names the model-specific tasking
      #include file:
        CHPL_TASKS_H --> CHPL_TASKS_MODEL_H
    
    include/chpl-threads.h
      This new file is adapted from include/tasks/fifo/tasks-fifo.h, which
      used to define the threading layer used by FIFO tasking.  Now it's a
      standalone, defining the common threading layer used by any tasking
      layer than uses a threading layer.
    
    include/threads/pthreads/threads-pthreads.h
      Improve the comment describing the type of a thread ID.
      Adjust to the threadlayer_mutex_p declaration having moved into the
      common chpl-threads.h file.
    
    include/tasks/fifo/tasks-fifo.h
      Remove all the declarations for the threading layer interface, which
      are not in chpl-threads.h.
    
    etc/Makefile.include
    make/Makefile.runtime.head
      Change the preprocessor symbols that name the model-specific tasking
      and threading #include files:
        CHPL_TASKS_H   --> CHPL_TASKS_MODEL_H
        CHPL_THREADS_H --> CHPL_THREADS_MODEL_H
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@18744 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/include/tasks/fifo/tasks-fifo.h
+++ runtime/include/tasks/fifo/tasks-fifo.h
@@ -2,57 +2,14 @@
 #define _tasks_fifo_h_
 
 #include <stdint.h>
-#include CHPL_THREADS_H
 
+#include "chpl-threads.h"
 
 //
 // The FIFO implementation of tasking is a least-common-denominator
-// version whose purpose is to minimize the work needed to get Chapel
-// tasking working on top of some new threading layer.
+// version designed for general usefulness, simplicity and limited
+// demands on the threading layer.
 //
-// The threading layer only has to supply a small amount of support in
-// the form of supplementary types and callback functions.  The
-// complete list is:
-//
-// For mutexes
-//   type(s)
-//     threadlayer_mutex_t
-//     threadlayer_mutex_p
-//   functions
-//     threadlayer_mutex_init()
-//     threadlayer_mutex_new()
-//     threadlayer_mutex_lock()
-//     threadlayer_mutex_unlock()
-//
-// For thread management
-//   type(s)
-//     <none>
-//   functions
-//     threadlayer_thread_id()
-//     threadlayer_yield()
-//
-// For task management
-//   type(s)
-//     <none>
-//   functions
-//     threadlayer_init()
-//     threadlayer_thread_create()
-//     threadlayer_get_thread_private_data()
-//     threadlayer_set_thread_private_data()
-//     threadlayer_call_stack_size()
-//     threadlayer_call_stack_size_limit()
-//
-// The types are declared in the threads-*.h file for each specific
-// threading layer, and the callback functions are declared here.  The
-// interfaces and requirements for these other types and callback
-// functions are described elsewhere in this file.
-//
-// Although the above list may seem long, in practice many of the
-// functions are quite simple, and with luck also easily extrapolated
-// from what is done for other threading layers.  For an example of an
-// implementation, see "pthreads" threading.
-//
-
 
 //
 // Type (and default value) used to communicate task identifiers
@@ -61,99 +18,12 @@
 typedef uint64_t chpl_taskID_t;
 #define chpl_nullTaskID 0
 
-
-//
-// Thread management
-//
-threadlayer_threadID_t threadlayer_thread_id(void);
-void threadlayer_yield(void);
-
-
 //
 // Sync variables
 //
 typedef struct {
-  volatile chpl_bool is_full;
+  volatile chpl_bool  is_full;
   threadlayer_mutex_t lock;
 } chpl_sync_aux_t;
-
-
-//
-// The remaining declarations are all for callback functions to be
-// provided by the threading layer.
-//
-
-//
-// These are called once each, from chpl_task_init(), chpl_task_exit(),
-// and chpl_task_perPthreadInit(), respectively.
-//
-void threadlayer_init(int32_t, uint64_t, void(*)(void*), void(*)(void));
-void threadlayer_exit(void);
-void threadlayer_perPthreadInit(void);
-
-
-//
-// Mutexes
-//
-void threadlayer_mutex_init(threadlayer_mutex_p);
-threadlayer_mutex_p threadlayer_mutex_new(void);
-void threadlayer_mutex_lock(threadlayer_mutex_p);
-void threadlayer_mutex_unlock(threadlayer_mutex_p);
-
-
-//
-// Task management
-//
-
-//
-// Is the thread layer willing to start another thread (to host another
-// task)?
-//
-chpl_bool threadlayer_can_start_thread(void);
-
-//
-// Create a new thread.
-//
-int threadlayer_thread_create(void*);
-
-//
-// Destroy the calling thread.  The threading layer is allowed to return
-// without destroying the thread, so this function is really an advisory
-// one, to say that the thread is no longer needed.  Also, the threading
-// layer is actually prohibited from destroying the thread if doing so
-// would leave no threads running on the processor.
-//
-void threadlayer_thread_destroy(void);
-
-
-//
-// Thread private data
-//
-// These set and get a pointer to thread private data associated with
-// each thread.  This is for the use of the tasking layer itself.  If
-// the threading layer also needs to store some data private to each
-// thread, it must make other arrangements to do so.
-//
-void  threadlayer_set_thread_private_data(void*);
-void* threadlayer_get_thread_private_data(void);
-
-
-//
-// Get the maximum number of threads that can exist.
-//
-uint32_t threadlayer_get_max_threads(void);
-
-
-//
-// Get the number of threads currently in existence.
-//
-uint32_t threadlayer_get_num_threads(void);
-
-
-//
-// Thread stack sizes
-//
-uint64_t threadlayer_call_stack_size(void);
-uint64_t threadlayer_call_stack_size_limit(void);
 
 #endif
