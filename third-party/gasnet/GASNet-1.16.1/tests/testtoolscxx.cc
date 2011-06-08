b91cee3cc8392e776851386a78f93cde8a9abcae
Sung-Eun Choi <sungeunchoi@users.noreply.github.com>
2011-06-08 22:25:06 +0000

    Remove older version of GASNet.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@18841 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- third-party/gasnet/GASNet-1.16.1/tests/testtoolscxx.cc
+++
@@ -1,9 +0,0 @@
-/*   $Source: /var/local/cvs/gasnet/tests/testtoolscxx.cc,v $
- *     $Date: 2006/08/02 23:10:50 $
- * $Revision: 1.1 $
- * Description: General GASNet correctness tests in C++
- * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
- * Terms of use are as specified in license.txt
- */
-
-#include "testtools.c"
