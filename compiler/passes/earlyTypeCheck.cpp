ed7f2d68fe539acc49e5bd4904c66c02ff19a33d
Jonathan Turner <jonathanturner>
2011-09-25 03:46:03 +0000

    Added congruence closure to early typecheck, replacing the earlier equality checker.  This allows equality-style where clauses.  Now that this is in place, code will be refactored to better use it.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/branches/collaborations/concepts@19195 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- compiler/passes/earlyTypeCheck.cpp
+++ compiler/passes/earlyTypeCheck.cpp
@@ -7,6 +7,250 @@
 #include "stringutil.h"
 #include "symbol.h"
 
+//FIXME: Convert these over to the Chapel structures
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <utility>
+
+struct CCNode {
+  CCNode* parent;
+
+  int unique_id;
+
+  std::vector<CCNode*> contains;
+  std::vector<CCNode*> contained_by;
+
+  CCNode() : parent(0) {}
+};
+
+typedef std::vector<std::pair<int, CCNode *> > CCNodeAssocList;
+typedef std::vector<std::pair<int, CCNode *> >::iterator CCNodeAssocListIter;
+typedef std::vector<std::pair<int, CCNode *> >::reverse_iterator CCNodeAssocListRIter;
+
+struct CongruenceClosure {
+  CCNodeAssocList node_assoc_list;
+  std::vector<unsigned> scope_stops;
+
+  void enter_scope() {
+    scope_stops.push_back(node_assoc_list.size());
+  }
+
+  void exit_scope() {
+    unsigned last_scope_stop;
+
+    INT_ASSERT(!scope_stops.empty() && "Popping an empty scope");
+    last_scope_stop = scope_stops.back();
+    scope_stops.pop_back();
+
+    //FIXME: this is pessimized, you can make this faster
+    while (node_assoc_list.size() != last_scope_stop) {
+      node_assoc_list.pop_back();
+    }
+  }
+
+  CCNode *representative(CCNode *node) {
+    while (node->parent)
+      node = node->parent;
+
+    return node;
+  }
+
+  CCNode *find_or_insert(BaseAST *ast) {
+    int unique_id = get_symbol_id(ast);
+    std::vector<CCNode *> children_nodes;
+
+    if (BlockStmt *bs = toBlockStmt(ast)) {
+      //FIXME: This is a shortcut but isn't strictly correct
+      find_or_insert(bs->body.head);
+    }
+    else if (CallExpr *call = toCallExpr(ast)) {
+      //Collect all the children first
+
+      for_alist(arg, call->argList) {
+        //FIXME: Not sure if this is strictly correct to use the representative here
+        children_nodes.push_back(representative(find_or_insert(arg)));
+      }
+    }
+
+    for (CCNodeAssocListRIter i = node_assoc_list.rbegin(), e = node_assoc_list.rend(); i != e; ++i) {
+      if (unique_id == i->first) {
+        CCNode *current_node = i->second;
+
+        bool found_match = true;
+
+        if (children_nodes.size() == current_node->contains.size()) {
+          for (unsigned j = 0; j < children_nodes.size(); ++j) {
+            if (children_nodes[j] != current_node->contains[j]) {
+              found_match = false;
+              break;
+            }
+          }
+        }
+        if (!found_match)
+          continue;
+
+        return current_node;
+      }
+    }
+
+    // Not found, so we insert and return that
+    CCNode *new_node = new CCNode();
+    new_node->unique_id = unique_id;
+    for (unsigned i = 0; i < children_nodes.size(); ++i) {
+      // Set up the bi-directional link between containing type and contained type
+      children_nodes[i]->contained_by.push_back(new_node);
+      new_node->contains.push_back(children_nodes[i]);
+
+      for (unsigned j = 0; j < children_nodes[i]->contained_by.size(); ++j) {
+        equate_if_children_match(new_node, children_nodes[i]->contained_by[j]);
+      }
+    }
+
+    node_assoc_list.push_back(std::make_pair(unique_id, new_node));
+
+    return new_node;
+  }
+
+  //FIXME: This probably already exists in Chapel, but I couldn't find it
+  int get_symbol_id(BaseAST *ast) {
+    if (isType(ast)) {
+      return ast->id;
+    }
+    else if (VarSymbol *vs = toVarSymbol(ast)) {
+      return vs->type->id;
+    }
+    else if (Symbol *s = toSymbol(ast)) {
+      return s->id;
+    }
+    else if (isExpr(ast)) {
+      if (DefExpr *de = toDefExpr(ast)) {
+        if (de->init) {
+          return get_symbol_id(de->init);
+        }
+        else if (de->exprType) {
+          return get_symbol_id(de->exprType);
+        }
+        else if (de->sym) {
+          return get_symbol_id(de->sym);
+        }
+      }
+      else if (SymExpr *se = toSymExpr(ast)) {
+        return get_symbol_id(se->var);
+      }
+      else if (CallExpr *ce = toCallExpr(ast)) {
+        return get_symbol_id(ce->baseExpr);
+      }
+      else if (BlockStmt *bs = toBlockStmt(ast)) {
+        //FIXME: This is a shortcut, it isn't strictly correct
+        return get_symbol_id(bs->body.head);
+      }
+      else {
+        INT_FATAL("Unimplemented case in getSymbolId(expr)");
+      }
+    }
+    else {
+      printf("TAG: %i\n", ast->astTag);
+      INT_FATAL("Unimplemented case in getSymbolId(ast)");
+    }
+    INT_FATAL("Unimplemented case in getSymbolId");
+    return 0;
+  }
+
+  CCNode *get_node(int id) {
+    for (CCNodeAssocListRIter i = node_assoc_list.rbegin(), e = node_assoc_list.rend(); i != e; ++i) {
+      if (id == i->first) {
+        return i->second;
+      }
+    }
+    return (CCNode *)0;
+  }
+
+  bool is_equal(BaseAST *ast1, BaseAST *ast2) {
+    CCNode *node1 = find_or_insert(ast1);
+    CCNode *node2 = find_or_insert(ast2);
+
+    return is_equal_helper(node1, node2);
+  }
+
+  bool is_equal_helper(CCNode *node1, CCNode *node2) {
+    node1 = representative(node1);
+    node2 = representative(node2);
+
+    return node1 == node2;
+  }
+
+  void equate(BaseAST *ast1, BaseAST *ast2) {
+    CCNode *node1 = find_or_insert(ast1);
+    CCNode *node2 = find_or_insert(ast2);
+
+    equate_helper(node1, node2);
+  }
+  void equate_helper(CCNode *node1, CCNode *node2) {
+    CCNode *node1_tmp = representative(node1);
+    CCNode *node2_tmp = representative(node2);
+
+    if (node1_tmp == node2_tmp)
+      return;
+
+    // Union the two trees
+    node2_tmp->parent = node1_tmp;
+
+    // Then, make sure that the "contained by" information is propagated to representative
+    for (unsigned i = 0; i < node2->contained_by.size(); ++i) {
+      node1_tmp->contained_by.push_back(node2->contained_by[i]);
+    }
+
+    // Do the rest of the closure.
+
+    // Step #1, equate all contained nodes
+
+    if (!node1->contains.empty() && !node2->contains.empty())
+      INT_ASSERT(node1->contains.size() == node2->contains.size() && "Mismatched shape during union");
+
+    if (node1->contains.size() == node2->contains.size()) {
+      // if we have the same shape, we can pattern match and also union the contained nodes
+      for (unsigned i = 0; i < node1->contains.size(); ++i) {
+        equate_helper(node1->contains[i], node2->contains[i]);
+      }
+    }
+
+    if (!node1->contained_by.empty() && !node2->contained_by.empty()) {
+      for (unsigned i = 0; i < node1->contained_by.size(); ++i) {
+        for (unsigned j = 0; j < node2->contained_by.size(); ++j) {
+          equate_if_children_match(node1->contained_by[i], node2->contained_by[j]);
+        }
+      }
+    }
+  }
+
+  void equate_if_children_match(CCNode *node1, CCNode *node2) {
+    if (representative(node1) == representative(node2))
+      return;
+
+    if (node1->contains.size() == node2->contains.size()) {
+      for (unsigned i = 0; i < node1->contains.size(); ++i) {
+        if (!is_equal_helper(node1->contains[i], node2->contains[i]))
+          return;
+      }
+
+      //std::cout << "Found matching children" << std::endl;
+      CCNode *pnode1 = get_node(node1->unique_id);
+      CCNode *pnode2 = get_node(node2->unique_id);
+
+      if (pnode1 && (pnode1 == pnode2)) {
+        equate_helper(node1, node2);
+      }
+      else
+        return;
+    }
+  }
+};
+
+CongruenceClosure cclosure;
+
+
 Expr *typeCheckFn(FnSymbol *fn);
 
 class VisibleFunctionBlock2 {
@@ -155,7 +399,7 @@ static void buildVisibleFunctionMap2() {
   }
   nVisibleFunctions = gFnSymbols.n;
 }
-
+/*
 bool compareTypeExprs(Expr *lhs, Expr *rhs) {
   if (lhs && rhs) {
     printf("Checking %p (%i) %p (%i)\n", lhs, lhs->astTag, rhs, rhs->astTag);
@@ -174,7 +418,7 @@ bool compareTypeExprs(Expr *lhs, Expr *rhs) {
         //flexible.
         if (VarSymbol *rhs_v = toVarSymbol(rhs_se->var)) {
           printf("  rhs is a var symbol\n");
-          if (/*lhs_v->immediate && */ rhs_v->immediate) {
+          if (lhs_v->immediate &&  rhs_v->immediate) {
             printf("  rhs is an immediate\n");
             //If we've already resolved to a type, just use that
             return lhs_se->var->type == rhs_se->var->type;
@@ -189,14 +433,14 @@ bool compareTypeExprs(Expr *lhs, Expr *rhs) {
       }
       printf("Both are sym exprs: %s %s\n", lhs_se->var->cname,
              rhs_se->var->cname);
-      /*
-      if (VarSymbol *v = toVarSymbol(lhs_se->var)) {
-        if (v->immediate && v->immediate->const_kind == NUM_KIND_INT &&
-            v->immediate->num_index == INT_SIZE_32) {
-          return (!strcmp(rhs_se->var->cname, "int32_t"));
-        }
+
+      //if (VarSymbol *v = toVarSymbol(lhs_se->var)) {
+      //  if (v->immediate && v->immediate->const_kind == NUM_KIND_INT &&
+      //      v->immediate->num_index == INT_SIZE_32) {
+      //    return (!strcmp(rhs_se->var->cname, "int32_t"));
+      //  }
       }
-      */
+
       return (lhs_se->var == rhs_se->var);
     }
   }
@@ -233,6 +477,24 @@ bool compareTypeExprs(Expr *lhs, Expr *rhs) {
   }
   return false;
 }
+*/
+Type *translateToType(Expr *type_expr) {
+  //if it already has a type, use that
+  if (type_expr->getValType() != dtUnknown) {
+    return type_expr->getValType();
+  }
+  else {
+    //Do something
+    return NULL;
+  }
+}
+
+Expr *translateToTypeExpr(Type *type) {
+  if (type == dtBool) {
+  }
+  return NULL;
+
+}
 
 Expr *typeCheckExpr(Expr *currentExpr, Expr *expectedReturnTypeExpr) {
   if (SymExpr *se_actual = toSymExpr(currentExpr)) {
@@ -248,7 +510,7 @@ Expr *typeCheckExpr(Expr *currentExpr, Expr *expectedReturnTypeExpr) {
       printf ("     Arg(defexpr)->%p %p %i\n", defPt->exprType, defPt->init,
               defPt->sym->astTag);
       if (defPt->exprType && defPt->init) {
-        if (compareTypeExprs(defPt->exprType, defPt->init)) {
+        if (cclosure.is_equal(defPt->exprType, defPt->init)) {
           printf("Declared variable type matches initialization expression\n");
         }
         else {
@@ -268,7 +530,7 @@ Expr *typeCheckExpr(Expr *currentExpr, Expr *expectedReturnTypeExpr) {
         printf("ERROR: Generic variable without initializing expression\n");
       }
     }
-    else if (Type *actualType = se_actual->var->type) {
+    else if (/*Type *actualType =*/ se_actual->var->type) {
       // Stop here, we've hit an expression that's been resolved already
       return currentExpr;
       /*
@@ -300,105 +562,141 @@ Expr *typeCheckExpr(Expr *currentExpr, Expr *expectedReturnTypeExpr) {
     else {
       printf("  Primitive: %s\n", call->primitive->name);
     }
-    printf("  BEGIN ARGS\n");
+    //printf("  BEGIN ARGS\n");
 
-    for_alist(e, call->argList) {
+    Vec<Expr*> checked_arg_exprs;
+    /*
+    //for_alist(e, call->argList) {
+    for_actuals(e, call) {
       printf("  PRE-TYPECHECK:\n");
       Expr *e_typeExpr = typeCheckExpr(e, expectedReturnTypeExpr);
       printf("  TYPECHECKED: %p\n", e_typeExpr);
-      if (call->primitive && (call->primitive->tag == PRIM_RETURN)) {
-        if ((!isBlockStmt(e_typeExpr)) && (isBlockStmt(expectedReturnTypeExpr))) {
-          BlockStmt *retBody = toBlockStmt(expectedReturnTypeExpr);
-          if (!compareTypeExprs(e_typeExpr, retBody->body.head)) {
-            printf("**********ERROR: Mismatched type expressions in return\n");
-          }
-          else {
-            printf("**********SUCCESS: Return expressions match: %p\n",
-                   e_typeExpr);
-
-            return e_typeExpr;
-          }
+      checked_arg_exprs.add(e_typeExpr);
+    }
+    */
+    if (call->primitive && (call->primitive->tag == PRIM_RETURN)) {
+      printf("-->Expression check of return argument\n");
+      Expr *e_typeExpr = typeCheckExpr(call->argList.head, expectedReturnTypeExpr);
+      printf("<--Done with expression check of return argument\n");
+      if ((!isBlockStmt(e_typeExpr)) && (isBlockStmt(expectedReturnTypeExpr))) {
+        BlockStmt *retBody = toBlockStmt(expectedReturnTypeExpr);
+        if (!cclosure.is_equal(e_typeExpr, retBody->body.head)) {
+          printf("**********ERROR: Mismatched type expressions in return\n");
         }
-        else{
-          if (!compareTypeExprs(e_typeExpr, expectedReturnTypeExpr)) {
-            printf("**********ERROR: Mismatched type expressions in return\n");
-          }
-          else {
-            printf("**********SUCCESS: Return expressions match: %p\n",
-                   e_typeExpr);
+        else {
+          printf("**********SUCCESS: Return expressions match: %p\n",
+                 e_typeExpr);
 
-            return e_typeExpr;
-          }
+          return e_typeExpr;
         }
       }
-      else if (!call->primitive) {
-        CallInfo2 info(call);
-
-        Vec<FnSymbol*> visibleFns;                    // visible functions
-        /*
-        Vec<FnSymbol*> candidateFns;
-        Vec<Vec<ArgSymbol*>*> candidateActualFormals; // candidate functions
-        */
-
-        if (gFnSymbols.n != nVisibleFunctions)
-          buildVisibleFunctionMap2();
-
-        if (!call->isResolved()) {
-          if (!info.scope) {
-            Vec<BlockStmt*> visited;
-            getVisibleFunctions(getVisibilityBlock(call), info.name, visibleFns,
-                                visited);
-          } else {
-            if (VisibleFunctionBlock2* vfb = visibleFunctionMap.get(info.scope))
-              if (Vec<FnSymbol*>* fns = vfb->visibleFunctions.get(info.name))
-                visibleFns.append(*fns);
-          }
-        } else {
-          visibleFns.add(call->isResolved());
+      else{
+        if (!cclosure.is_equal(e_typeExpr, expectedReturnTypeExpr)) {
+          printf("**********ERROR: Mismatched type expressions in return\n");
         }
+        else {
+          printf("**********SUCCESS: Return expressions match: %p\n",
+                 e_typeExpr);
 
-        forv_Vec(FnSymbol, visibleFn, visibleFns) {
-          printf("$$$ Checking against: %s %s\n", visibleFn->name,
-                 visibleFn->cname);
-          bool mismatch = false;
-          Expr *e_actual = call->argList.head;
-          ArgSymbol *s_formal = ((visibleFn)->formals.head) ?
-            toArgSymbol(toDefExpr((visibleFn)->formals.head)->sym) : NULL;
-          for (; e_actual; e_actual = e_actual->next,
-                 s_formal = (s_formal && s_formal->defPoint->next) ?
-                 toArgSymbol(toDefExpr((s_formal)->defPoint->next)->sym) :
-                 NULL) {
-            if (!s_formal) {
+          return e_typeExpr;
+        }
+      }
+    }
+    else if (call->primitive) {
+      printf("  PRIMITIVE OP\n");
+    }
+    else if (!call->primitive) {
+      //First, check to see if it should be a primitive but it hasn't been
+      //resolved yet
+
+      if (UnresolvedSymExpr *use = toUnresolvedSymExpr(call->baseExpr)) {
+        printf("  Unresolved SymExpr: %s\n", use->unresolved);
+        if (PrimitiveOp *op = primitives_map.get(use->unresolved)) {
+          printf("Found a matching primitive op.  Type: %p\n",
+              op->returnInfo(call));
+          //FIXME: Come up with a better way to resolve here.
+          if (BlockStmt *block = toBlockStmt(expectedReturnTypeExpr)) {
+            expectedReturnTypeExpr = block->body.head;
+          }
+          if (SymExpr *expectedReturn_se = toSymExpr(expectedReturnTypeExpr)) {
+            printf("  Expected type: %s %p\n", expectedReturn_se->var->cname,
+                expectedReturn_se->var->type);
+            printf("  Op type: %s %p\n", op->returnInfo(call)->symbol->cname,
+                op->returnInfo(call));
+            if (expectedReturn_se->var->type == op->returnInfo(call)) {
+              return expectedReturnTypeExpr;
+            }
+          }
+
+        }
+      }
+
+      CallInfo2 info(call);
+
+      Vec<FnSymbol*> visibleFns;                    // visible functions
+      /*
+      Vec<FnSymbol*> candidateFns;
+      Vec<Vec<ArgSymbol*>*> candidateActualFormals; // candidate functions
+      */
+
+      if (gFnSymbols.n != nVisibleFunctions)
+        buildVisibleFunctionMap2();
+
+      if (!call->isResolved()) {
+        if (!info.scope) {
+          Vec<BlockStmt*> visited;
+          getVisibleFunctions(getVisibilityBlock(call), info.name, visibleFns,
+                              visited);
+        } else {
+          if (VisibleFunctionBlock2* vfb = visibleFunctionMap.get(info.scope))
+            if (Vec<FnSymbol*>* fns = vfb->visibleFunctions.get(info.name))
+              visibleFns.append(*fns);
+        }
+      } else {
+        visibleFns.add(call->isResolved());
+      }
+
+      forv_Vec(FnSymbol, visibleFn, visibleFns) {
+        printf("$$$ Checking against: %s %s\n", visibleFn->name,
+               visibleFn->cname);
+        bool mismatch = false;
+        Expr *e_actual = call->argList.head;
+        ArgSymbol *s_formal = ((visibleFn)->formals.head) ?
+          toArgSymbol(toDefExpr((visibleFn)->formals.head)->sym) : NULL;
+        for (; e_actual; e_actual = e_actual->next,
+               s_formal = (s_formal && s_formal->defPoint->next) ?
+               toArgSymbol(toDefExpr((s_formal)->defPoint->next)->sym) :
+               NULL) {
+          if (!s_formal) {
+            mismatch = true;
+            break;
+          }
+          printf("Comparing %i to %i\n", e_actual->astTag,
+                 s_formal->typeExpr->astTag);
+          if ((!isBlockStmt(e_actual)) && (isBlockStmt(s_formal->typeExpr))) {
+            printf("Looking inside of body\n");
+            BlockStmt *retBody = toBlockStmt(s_formal->typeExpr);
+            if (!cclosure.is_equal(e_actual, retBody->body.head)) {
               mismatch = true;
               break;
             }
-            printf("Comparing %i to %i\n", e_actual->astTag,
-                   s_formal->typeExpr->astTag);
-            if ((!isBlockStmt(e_actual)) && (isBlockStmt(s_formal->typeExpr))) {
-              printf("Looking inside of body\n");
-              BlockStmt *retBody = toBlockStmt(s_formal->typeExpr);
-              if (!compareTypeExprs(e_actual, retBody->body.head)) {
-                mismatch = true;
-                break;
-              }
-            }
-            else {
-              printf("NOT looking inside of body\n");
-              if (!compareTypeExprs(e_actual, s_formal->typeExpr)) {
-                mismatch = true;
-                break;
-              }
+          }
+          else {
+            printf("NOT looking inside of body\n");
+            if (!cclosure.is_equal(e_actual, s_formal->typeExpr)) {
+              mismatch = true;
+              break;
             }
           }
-          if (!mismatch) {
-            printf("!Found matching function to check!\n");
-            return typeCheckFn(visibleFn);
-          }
+        }
+        if (!mismatch) {
+          printf("!Found matching function to check!\n");
+          return typeCheckFn(visibleFn);
         }
       }
     }
 
-    printf("  END ARGS\n");    
+    //printf("  END ARGS\n");
   }
   else if (UnresolvedSymExpr *use = toUnresolvedSymExpr(currentExpr)) {
     printf("  Unresolved SymExpr: %s\n", use->unresolved);
@@ -453,6 +751,21 @@ Expr *typeCheckFn(FnSymbol *fn) {
     printf("  Return: %p\n", fn->retExprType);
   }
   
+  if (fn->where) {
+    for_alist(expr, fn->where->body) {
+      if (CallExpr *ce = toCallExpr(expr)) {
+        if (UnresolvedSymExpr *callsymexpr = toUnresolvedSymExpr(ce->baseExpr)) {
+          if (!strcmp(callsymexpr->unresolved, "==")) {
+            //Equality constraint, let's add it
+            BaseAST *arg1 = ce->argList.get(1);
+            BaseAST *arg2 = ce->argList.get(2);
+            cclosure.equate(arg1, arg2);
+          }
+        }
+      }
+    }
+  }
+
   printf("Body:\n");
 
   // Now, look through the function body
