7189b5bf1e97efeaffb8bf7d085e433df1de87f0
Greg Titus <gbtitus@users.noreply.github.com>
2011-11-02 23:27:31 +0000

    Greatly simplify the tcmalloc memory layer.
    If we insist that anyone needing a contiguous heap managed by tcmalloc must
    supply the memory themselves, instead of supplying the size and having
    tcmalloc acquire the space, things are a lot simpler.  This commit makes that
    change.  As a result of this change, the GASNet comm layer now works with the
    tcmalloc memory layer.
    
    The specific file changes are as follows.
    
    runtime/src/mem/tcmalloc/mem-tcmalloc.c
      Allow the caller of chpl_mem_layerInit() to specify a heap starting address
      and size.  Change the interface to the C++ part of the tcmalloc heap setup
      code, so that it takes a starting address and length and returns nothing,
      instead of taking a desired length and returning a starting address and
      actual length.  Add code to use up any memory tcmalloc had already acquired
      from other system allocators, prior to when ours was registered.
    
    runtime/src/mem/tcmalloc/tcmalloc-interface.h
    runtime/src/mem/tcmalloc/tcmalloc-interface.cc
      Change the interface to the C++ part of the tcmalloc heap setup code, so
      that it takes a starting address and length and returns nothing, instead of
      taking a desired length and returning a starting address and actual length.
      Remove everything having to do with contiguous heaps, hugepages, and the
      special tcmalloc static system allocator.  The new tcmalloc routine that
      disables all but one of the system allocators subsumes the functionality of
      the special static system allocator, and works better anyway because in some
      cases (when using the GASNet comm layer, for example) we really do need to
      have all of the heap in the one supplied block of memory, and the special
      static allocator broke that.
    
    third-party/tcmalloc/google-perftools-1.7/src/system-alloc.h
    third-party/tcmalloc/google-perftools-1.7/src/system-alloc.cc
      Add DisableAllButThisSystemAllocator(), a new routine that disables all of
      the system allocators except the one with the given priority.  After a user
      provided system allocator has been registered, this routine can be called to
      disable all the others.  All subsequent system memory will come only from
      the newly registered system allocator.  In order to also ensure that all
      subsequent tcmalloc-provided memory comes from the newly registered system
      allocator, though, one also has to do tc_malloc() allocations in order to
      acquire all the tcmalloc-managed memory that had been provided by other
      system allocators before the new one was registered.  See the code in
      runtime/src/mem/tcmalloc/tcmalloc-interface.cc for how this can be done.
    
      In system-alloc.cc, remove the code that interacted with the special
      Chapel-supplied static system allocator, which no longer exists.
    
      Also, while here, revert to their original values the two constants telling
      how many system allocators are present.  There actually wasn't any need to
      change these in the first place.
    
    third-party/tcmalloc/README
      Update the descriptions of modifications made to the distributed sources.
    
    third-party/tcmalloc/tcmalloc.patch
      Remove this file.  It's actually easier to migrate the changes made by the
      Chapel team from one third party release to the next using the existing svn
      capabilities, operating on the repository, than it is to maintain a patch
      file.
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@19429 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/src/mem/tcmalloc/mem-tcmalloc.c
+++ runtime/src/mem/tcmalloc/mem-tcmalloc.c
@@ -1,4 +1,3 @@
-#include <errno.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,38 +20,13 @@ static size_t saved_heap_size = 0;
 
 void chpl_mem_layerInit(void)
 {
-  void* start;
+  void* addr;
   size_t size;
 
-  chpl_comm_desired_shared_heap(&start, &size);
+  chpl_comm_desired_shared_heap(&addr, &size);
 
-  if (start)
-    chpl_warning("CHPL_MEM=tcmalloc ignores a starting heap address", 0, 0);
-
-  if (!size) {
-    char* p;
-
-    if ((p = getenv("TCMALLOC_PREALLOCATED_HEAP")) != NULL) {
-      char units;
-
-      if (sscanf(p, "%zi%c", &size, &units) != 2
-          || strchr("kKmMgG", units) == NULL) {
-        printf("Cannot parse TCMALLOC_PREALLOCATED_HEAP environment variable; "
-               "assume 4m\n");
-        size = 4 * 1024 * 1024;
-      }
-      else {
-        switch (units) {
-        case 'k' : case 'K': size *= 1024;               break;
-        case 'm' : case 'M': size *= 1024 * 1024;        break;
-        case 'g' : case 'G': size *= 1024 * 1024 * 1024; break;
-        }
-      }
-    }
-  }
-
-  if (!size)
-    return;
+  if (addr != NULL && size == 0)
+    chpl_internal_error("if heap address is specified, size must be also");
 
   //
   // Do a first allocation, to allow tcmalloc to set up its internal
@@ -62,27 +36,55 @@ void chpl_mem_layerInit(void)
     void* p;
 
     if ((p = tc_malloc(1)) == NULL)
-      chpl_error("cannot init heap: tc_malloc() failed", 0, 0);
+      chpl_internal_error("cannot init heap: tc_malloc() failed");
     tc_free(p);
   }
 
+  saved_heap_base = addr;
+  saved_heap_size = size;
+
   //
-  // Initialize our tcmalloc hugepage allocator.
+  // Initialize our tcmalloc system allocator.
   //
-  saved_heap_base = tcmallocChapelInit_c(size, &saved_heap_size);
-  if (saved_heap_base == NULL)
-    chpl_error("cannot init heap: ChapelHugepageSysAllocatorInit_c() failed",
-               0, 0);
+  tcmallocChapelInit_c(addr, size);
+
+  //
+  // If the heap has to come from the memory supplied to us (say, in
+  // order that the comm layer be able to depend on all allocations
+  // having come from it), use up all the system memory tcmalloc had
+  // acquired before we set up our own system allocator just now.
+  // All allocations after this will come from the memory supplied
+  // to us.
+  //
+  // Note that this can waste up to twice INITIAL_USE_UP_SIZE bytes
+  // of the memory supplied to us, plus overhead.
+  //
+  if (addr != NULL) {
+#define INITIAL_USE_UP_SIZE ((size_t) 4 * 1024)
+
+    size_t size;
+    char*  p;
+
+    for (size = INITIAL_USE_UP_SIZE; size > 0; size /= 2) {
+      do {
+        p = tc_malloc(size);
+      } while (p != NULL
+               && (p < (char*) saved_heap_base
+                   || p > (char*) saved_heap_base + saved_heap_size));
+
+#undef INITIAL_USE_UP_SIZE
+    }
+  }
 }
 
 
 void chpl_mem_layerExit(void) { }
 
 
-void chpl_mem_layerActualSharedHeap(void** start_p, size_t* size_p)
+void chpl_mem_layerActualSharedHeap(void** addr_p, size_t* size_p)
 {
-  *start_p = saved_heap_base;
-  *size_p  = saved_heap_size;
+  *addr_p = saved_heap_base;
+  *size_p = saved_heap_size;
 }
 
 
