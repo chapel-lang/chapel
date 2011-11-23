f043178b8e8b3534132ddc7055a7d9188a1ca514
Michael Ferguson <mppf>
2011-11-23 15:37:53 +0000

    Bringing changes from previous I/O + GMP branch into their own branch. If this commits OK, it is ready for merging.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/branches/collaborations/ferguson/gmp@19498 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

---
+++ runtime/include/chplgmp.h
@@ -0,0 +1,26 @@
+
+#include "chpltypes.h"
+
+#include <stdarg.h>
+#include <stdio.h>
+
+
+#ifndef CHPL_NO_GMP
+#include <gmp.h>
+
+
+// set the GMP allocation routines to use chpl_malloc, etc.
+void chpl_gmp_init(void);
+
+void chpl_gmp_get_mpz(mpz_t ret, int32_t src_locale, __mpz_struct from);
+
+void chpl_gmp_get_randstate(gmp_randstate_t not_inited_state, int32_t src_locale, __gmp_randstate_struct from);
+
+uint64_t chpl_gmp_mpz_nlimbs(__mpz_struct from);
+
+void chpl_gmp_mpz_print(mpz_t x);
+
+chpl_string chpl_gmp_mpz_get_str(int32_t base, mpz_t x);
+
+#endif
+
