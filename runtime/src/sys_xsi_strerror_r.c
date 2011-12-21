b17402a3f70c5f004d0e98194fe3242737210a8a
Michael Ferguson <mppf>
2011-12-21 14:17:54 +0000

    Added comments, never actually includes chplrt.h, so it should build OK on AIX now.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@19561 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/src/sys_xsi_strerror_r.c
+++ runtime/src/sys_xsi_strerror_r.c
@@ -1,3 +1,10 @@
+/*
+ * Since GNU environments like to define their own
+ * non-portable strerror_r, we need a separate
+ * source file that does not define _GNU_SOURCE
+ * in order to get the portable one.
+ */
+
 #define _POSIX_C_SOURCE 20112L
 #define _XOPEN_SOURCE 600
 #undef _GNU_SOURCE
@@ -11,8 +18,8 @@ int sys_xsi_strerror_r(int errnum, char* buf, size_t buflen)
   return strerror_r(errnum, buf, buflen);
 }
 
-#ifndef SIMPLE_TEST
-// Squelch the warning about not includnig chplrt.
+#if 0
+// Squelch the warning about not including the chplrt; not necessary here
 #include "chplrt.h"
 #endif
 
