chapel-lang/chapel

commit 740625360f64f7770e12482e1146b0d9e4645b29

Remove chpl_error_noexit primitive and fix a race condition in halt.
This commit removes the chpl_error_noexit primitive from the compiler
and the corresponding calls in the runtime.  We were only using them
for the varargs versions of halt() and assert() to separately print
out the first part of the error message.  Brad hypothesized that this
was to avoid leaking more strings by concatenating everything together
into a new string and passing it to the chpl_error, but since we're
exiting anyways it doesn't seem like we should worry about it.  This
will guarantee that error messages are not interleaved.

There was also a known race condition in spinhaltIfAlreadyExiting() in
error.c.  Since we now have an atomics interface, we can fix that race
condition with an atomic test_and_set.  Note that we do not use the
atomics when compiling the launcher since our launcher is single
threaded.

[reviewed by bradc]



git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@19782 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

runtime/include/error.h
@@ -14,6 +14,4 @@ void chpl_msg(int verbose_level, const char* fmt, ...)
   __attribute__((format(printf, 2, 3)));
 #define printf PRINTF_DEF
 
-void chpl_error_noexit(const char* message, int32_t lineno, chpl_string filename);
-
 #endif

runtime/src/error.c
@@ -6,6 +6,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#ifndef LAUNCHER
+#include "chpl-atomics.h"
+#endif
 
 int verbosity = 1;
 
@@ -23,17 +26,17 @@ void chpl_warning(const char* message, int32_t lineno, chpl_string filename) {
     fprintf(stderr, "warning: %s\n", message);
 }
 
-static volatile int thisLocaleAlreadyExiting = 0;
 
 static void spinhaltIfAlreadyExiting(void) {
+#ifndef LAUNCHER
+  static atomic_flag thisLocaleAlreadyExiting = false;
   volatile int temp;
-  if (thisLocaleAlreadyExiting == 0) {
-    thisLocaleAlreadyExiting = 1;
-    return;
+  if (atomic_flag_test_and_set(&thisLocaleAlreadyExiting)) {
+    // spin forever if somebody else already set it to 1
+    temp = 1;
+    while (temp);
   }
-  // spin forever if somebody else already set it to 1
-  temp = 1;
-  while (temp);
+#endif
 }
 
 static void chpl_error_common(const char* message, int32_t lineno, chpl_string filename) {
@@ -52,10 +55,6 @@ void chpl_error(const char* message, int32_t lineno, chpl_string filename) {
   chpl_error_common(message, lineno, filename);
   fprintf(stderr, "\n");
   chpl_exit_any(1);
-}
-
-void chpl_error_noexit(const char* message, int32_t lineno, chpl_string filename) {
-  chpl_error_common(message, lineno, filename);
 }
 
 

compiler/AST/primitive.cpp
@@ -530,7 +530,6 @@ initPrimitive() {
   prim_def("chpl_setMemFlags", returnInfoVoid, true);
 
   prim_def(PRIM_RT_ERROR, "chpl_error", returnInfoVoid, true, true);
-  prim_def(PRIM_RT_ERROR_NOEXIT, "chpl_error_noexit", returnInfoVoid, true, true);
   prim_def(PRIM_RT_WARNING, "chpl_warning", returnInfoVoid, true, true);
 
   prim_def(PRIM_NEW_PRIV_CLASS, "chpl_newPrivatizedClass", returnInfoVoid, true);

modules/internal/ChapelIO.chpl
@@ -332,12 +332,9 @@ extern proc chpl_exit_any(status:int);
 
 proc assert(test: bool, args ...?numArgs) {
   if !test {
-    //chpl_error_noexit("assert failed - ", -1, "");
-    __primitive("chpl_error_noexit", "assert failed - ");
-    stderr.writeln((...args));
-    chpl_exit_any(1);
-    //exit(1);
-    //__primitive("chpl_error", "assert failed");
+    var tmpstring: string;
+    tmpstring.write((...args));
+    __primitive("chpl_error", "assert failed - " + tmpstring);
   }
 }
 
@@ -350,12 +347,9 @@ proc halt(s:string) {
 }
 
 proc halt(args ...?numArgs) {
-  //chpl_error_noexit("halt reached - ", -1, "");
-  __primitive("chpl_error_noexit", "halt reached - ");
-  stderr.writeln((...args));
-  chpl_exit_any(1);
-  //exit(1);
-  //__primitive("chpl_error", "halt reached");
+  var tmpstring: string;
+  tmpstring.write((...args));
+  __primitive("chpl_error", "halt reached - " + tmpstring);
 }
 
 proc _ddata.writeThis(f: Writer) {

commit a6c6aa35900d7819bbd2024d27fb56e62fd5d4d6

Move atomic initialization for error handling to main().
This fixes a build problem caused by r19782 for CHPL_ATOMICS=locks.
The original code assumed the CHPL_ATOMICS=intrinsics and used an
initializer (false) to initialize the atomic flag used in error
exiting.  There is now a new function called chpl_error_init() that is
called early in main() to do the initialization by calling
atomic_init_flag().



git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@19789 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

runtime/include/error.h
@@ -14,4 +14,8 @@ void chpl_msg(int verbose_level, const char* fmt, ...)
   __attribute__((format(printf, 2, 3)));
 #define printf PRINTF_DEF
 
+#ifndef LAUNCHER
+void chpl_error_init(void);
+#endif
+
 #endif

runtime/src/error.c
@@ -27,9 +27,15 @@ void chpl_warning(const char* message, int32_t lineno, chpl_string filename) {
 }
 
 
+#ifndef LAUNCHER
+static atomic_flag thisLocaleAlreadyExiting;
+void chpl_error_init(void) {
+  atomic_init_flag(&thisLocaleAlreadyExiting, false);
+}
+#endif
+
 static void spinhaltIfAlreadyExiting(void) {
 #ifndef LAUNCHER
-  static atomic_flag thisLocaleAlreadyExiting = false;
   volatile int temp;
   if (atomic_flag_test_and_set(&thisLocaleAlreadyExiting)) {
     // spin forever if somebody else already set it to 1

runtime/src/main.c
@@ -66,6 +66,7 @@ int main(int argc, char* argv[]) {
   // indicated by the locale environment variables.
   setlocale(LC_CTYPE,"");
 
+  chpl_error_init();  // This does local-only initialization
   chpl_comm_init(&argc, &argv);
   chpl_mem_init();
   chpl_comm_post_mem_init();