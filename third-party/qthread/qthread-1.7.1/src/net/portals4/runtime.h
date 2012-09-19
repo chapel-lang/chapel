44dc48afa8547c7ccaf44a9e54cea528d8cac8f7
Brad Chamberlain <bradcray@users.noreply.github.com>
2012-09-19 22:23:52 +0000

    Removing qthreads 1.7.1 from SVN
    I think we're in general agreement that 1.8 is the copy to release
    (if not 1.8.1 should it be ready in time and represent a step forward).
    
    Updated README to reflect the lack of local patches.
    
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@20471 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- third-party/qthread/qthread-1.7.1/src/net/portals4/runtime.h
+++
@@ -1,8 +0,0 @@
-
-int qthread_internal_net_driver_runtime_init(void);
-int qthread_internal_net_driver_runtime_fini(void);
-ptl_process_t* qthread_internal_net_driver_runtime_get_mapping(void);
-void qthread_internal_net_driver_runtime_barrier(void);
-
-int qthread_internal_net_driver_get_rank(void);
-int qthread_internal_net_driver_get_size(void);
