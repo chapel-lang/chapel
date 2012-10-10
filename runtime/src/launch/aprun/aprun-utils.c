7069d99e816d98736ec1dfeb0e18a4d7ea57091a
Greg Titus <gbtitus@users.noreply.github.com>
2012-10-10 16:24:59 +0000

    Support cnselect coremask attribute as a backstop to numcores.
    [Reviewed by Sung.]
    
    Some time ago we removed support for the cnselect coremask attribute as
    a way to find out the number of cores on a compute locale, because it
    was deprecated and the numcores attribute was a better alternative.  But
    it turns out that the version of cnselect on Cray XT systems still only
    supports coremask.  So, add support for that as a backstop to numcores.
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@20676 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/src/launch/aprun/aprun-utils.c
+++ runtime/src/launch/aprun/aprun-utils.c
@@ -112,31 +112,65 @@ int getCoresPerLocale() {
       chpl_warning("CHPL_LAUNCHER_CORES_PER_LOCALE set to invalid value.", 0, 0);
   }
 
-  if (numCores <= 0) {
+  if (numCores > 0)
+    return numCores;
+
+  if (strstr(CNA, "numcores") != NULL) {
     const int buflen = 1024;
     char buf[buflen];
     char* argv[3];
-
-    if (strstr(CNA, "numcores") == NULL) {
-      // numcores not available in this version
-      chpl_error("Error trying to determine number of cores per node", 0, 0);
-    }
 
     argv[0] = (char *) "cnselect";
     argv[1] = (char *) "-Lnumcores";
     argv[2] = NULL;
   
     memset(buf, 0, buflen);
-    if (chpl_run_utility1K("cnselect", argv, buf, buflen) <= 0) {
+    if (chpl_run_utility1K("cnselect", argv, buf, buflen) <= 0)
       chpl_error("Error trying to determine number of cores per node", 0, 0);
-    }
 
-    if (sscanf(buf, "%d", &numCores) != 1) {
-      chpl_error("unable to determine number of cores per locale; please set CHPL_LAUNCHER_CORES_PER_LOCALE", 0, 0);
-    }
+    if (sscanf(buf, "%d", &numCores) != 1)
+      chpl_error("unable to determine number of cores per locale; "
+                 "please set CHPL_LAUNCHER_CORES_PER_LOCALE", 0, 0);
+
+    return numCores;
   }
 
-  return numCores;
+  if (strstr(CNA, "coremask") != NULL) {
+    const int buflen = 1024;
+    char buf[buflen];
+    char* argv[3];
+ 
+    argv[0] = (char *) "cnselect";
+    argv[1] = (char *) "-Lcoremask";
+    argv[2] = NULL;
+  
+    memset(buf, 0, buflen);
+    if (chpl_run_utility1K("cnselect", argv, buf, buflen) <= 0)
+      chpl_error("Error trying to determine number coremask on node", 0, 0);
+
+    {
+      int coreMask;
+      int bitMask = 0x1;
+
+      if (sscanf(buf, "%d", &coreMask) != 1)
+        chpl_error("unable to determine coremask for locale; "
+                   "please set CHPL_LAUNCHER_CORES_PER_LOCALE", 0, 0);
+
+      coreMask >>= 1;
+      numCores = 1;
+      while (coreMask & bitMask) {
+        coreMask >>= 1;
+        numCores += 1;
+      }
+    }
+
+    return numCores;
+  }
+
+  // neither numcores nor coremask is available in this version
+  chpl_error("Error trying to determine number of cores per node", 0, 0);
+
+  return 0;
 }
 
 const char* getLocalesPerNodeStr() {
