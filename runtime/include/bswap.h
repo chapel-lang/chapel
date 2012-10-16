b6743d808c3b670792224a018ecc5a057f311fdf
Tom Hildebrandt <hilde@cray.com>
2012-10-16 03:22:05 +0000

    bswap fix, take 3.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@20693 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/include/bswap.h
+++ runtime/include/bswap.h
@@ -9,17 +9,13 @@
 #include <sys/param.h>
 #ifdef __GLIBC__
 #include <endian.h>
+// Present for PGI, Intel and Cray compilers?
+// Add guards if not.
 #include <byteswap.h>
 #endif
 
-#ifdef htobe64
-// we should have htobe etc.
-
-#else
-
-#ifdef __bswap_64
-// OK, we have __bswap_64
-#else
+// Should be defined in byteswap.h.
+#ifndef __bswap_64
 // slow compatability method.
 // stolen from glibc bits/byteswap.h
 # define __bswap_64(x) \
@@ -33,7 +29,19 @@
       | (((x) & 0x00000000000000ffull) << 56))
 #endif
 
+// Should be defined in byteswap.h
+#ifndef __bswap_32
+// slow compatability method.
+// stolen from glibc bits/byteswap.h
+#define __bswap_32(x) \
+  ((((x) & 0xff000000) >> 24) | (((x) & 0x00ff0000) >>  8) |               \
+  (((x) & 0x0000ff00) <<  8) | (((x) & 0x000000ff) << 24))
+// Assume if we don't have __bswap_32 we also don't have __bswap_16.
+#define __bswap_16(x) \
+  ((uint16_t) ((((x) >> 8) & 0xff) | (((x) & 0xff) << 8)))
+#endif
 
+#ifndef htobe64
 // stolen from glibc endian.h
 # if __BYTE_ORDER == __LITTLE_ENDIAN
 #  define htobe16(x) __bswap_16 (x)
@@ -50,7 +58,7 @@
 #  define htole64(x) (x)
 #  define be64toh(x) __bswap_64 (x)
 #  define le64toh(x) (x)
-# else
+# else // __BYTE_ORDER == __BIG_ENDIAN
 #  define htobe16(x) (x)
 #  define htole16(x) __bswap_16 (x)
 #  define be16toh(x) (x)
