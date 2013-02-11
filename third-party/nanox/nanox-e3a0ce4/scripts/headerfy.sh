8777760c278fa0306015c41a0e1808949c0e7488
Greg Titus <gbtitus@users.noreply.github.com>
2013-02-11 16:53:05 +0000

    Remove the nanos tasking layer.
    [Reviewed by Sung.]
    
    This removes the nanos tasking layer and support thereof in the scripts
    and documentation.  It has languished recently and we were working with
    a pretty old copy already, so it seems unlikely to revive.  If interest
    returns we can resurrect it, but for now retiring it will reduce the
    maintenance burden.
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@20994 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- third-party/nanox/nanox-e3a0ce4/scripts/headerfy.sh
+++
@@ -1,8 +0,0 @@
-#Run this script from the base directory
-
-find . -name "*.hpp" -exec headache -h scripts/preamble.txt -c scripts/headache.cfg {} +
-find . -name "*.cpp" -exec headache -h scripts/preamble.txt -c scripts/headache.cfg {} +
-find . -name "*.c" -exec headache -h scripts/preamble.txt -c scripts/headache.cfg {} +
-find . -name "*.h" -exec headache -h scripts/preamble.txt -c scripts/headache.cfg {} +
-find . -name "Makefile.am" -exec headache -h scripts/preamble.txt -c scripts/headache.cfg {} +
-
