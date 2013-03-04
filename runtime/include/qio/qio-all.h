fdffa80dc050693b56a8758774d08492e0c79a44
Michael Ferguson <mppf>
2013-03-04 22:29:45 +0000

    Forgot to include this file in r21083
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@21084 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

---
+++ runtime/include/qio/qio-all.h
@@ -0,0 +1,12 @@
+#include "sys_basic.h"
+#include "qio_error.h"
+#include "bswap.h"
+#include "deque.h"
+#include "qbuffer.h"
+#include "qio.h"
+#include "qio_formatted.h"
+#include "qio_regexp.h"
+#include "qio_style.h"
+#include "bulkget.h"
+#include "sys.h"
+
