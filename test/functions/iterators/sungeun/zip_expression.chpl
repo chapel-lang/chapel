chapel-lang/chapel

commit 35578ab98bf0eb367b694741710ae1d159e3813b

Enable homogenous tuple iteration
[Reviewed by Brad, Vass]

This is commit enables tuple iteration, as started by John K. in
r20452.  Unfortunately, zippered tuple in forall loops do not work,
but it was decided that we will leave those for another time.

- Rewrote the initial tuple iteration code from r20452 (which was not
  enabled) to use the chunking routines from DSIUtil

- Changed _getIteratorZip() to enable zippering of tuples for for
  loops only

- Updated existing tests and added new tests for zippering, including
  a future for the forall case



git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@21004 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

modules/internal/ChapelArray.chpl
@@ -1879,12 +1879,12 @@ module ChapelArray {
   inline proc _getIteratorZip(x: _tuple) {
     inline proc _getIteratorZipInternal(x: _tuple, param dim: int) {
       if dim == x.size then
-        return tuple(_getIteratorZip(x(dim)));
+        return tuple(_getIterator(x(dim)));
       else
-        return (_getIteratorZip(x(dim)), (..._getIteratorZipInternal(x, dim+1)));
+        return (_getIterator(x(dim)), (..._getIteratorZipInternal(x, dim+1)));
     }
     if x.size == 1 then
-      return _getIteratorZip(x(1));
+      return _getIterator(x(1));
     else
       return _getIteratorZipInternal(x, 1);
   }

modules/internal/ChapelTuple.chpl
@@ -96,18 +96,15 @@ module ChapelTuple {
         halt("tuple access out of bounds: ", i);
     return __primitive("get svec member", this, i);
   }
-  
+
   //
   // iterator support for tuples
   //
   iter _tuple.these() {
-  
-    compilerError("Iteration over tuples is not yet supported. If you intended to use zippered iteration, you need to add the new keyword 'zip' before the tuple of iteratable expressions.");
-  
-    // johnk: This is just a draft.
-    // At the time of writting only homogeneous tuple iteration was supported
-    // and it was decided to not permit tuple iteration at all.  Rather than 
-    // remove this code, it was left in to save reimplementation.
+
+    if !isHomogeneousTuple(this) then
+      compilerError("Cannot iterate over non-homogeneous tuples. If you intended to use zippered iteration, add the new keyword 'zip' before the tuple ofiteratable expressions.");
+
     for i in 1..this.size {
       yield(this(i));
     }
@@ -116,34 +113,26 @@ module ChapelTuple {
   iter _tuple.these(param tag:iterKind) 
       where tag == iterKind.leader 
   {
-    
-    compilerError("Iteration over tuples is not yet supported. If you intended to use zippered iteration, you need to add the new keyword 'zip' before the tuple of iteratable expressions.");
-  
-    // johnk: This is just a draft.
-    // At the time of writting only homogeneous tuple iteration was supported
-    // and it was decided to not permit tuple iteration at all.  Rather than 
-    // remove this code, it was left in to save reimplementation.
-    const maxTasks = dataParTasksPerLocale;
+
+    const numTasks = if dataParTasksPerLocale==0 then here.numCores
+                     else dataParTasksPerLocale;
     const ignoreRunning = dataParIgnoreRunningTasks;
-    const minSize = dataParMinGranularity;
-    const chunks = _computeNumChunks(maxTasks, ignoreRunning, minSize, this.size);
-    const chunkSize = this.size;
-  
-    if chunks == 0 {
-      yield 1..this.size;
+    const minIndicesPerTask = dataParMinGranularity;
+    const length = this.size;
+    const myRange = 1..#length;
+    var (numChunks, _) = _computeChunkStuff(numTasks, ignoreRunning,
+                                            minIndicesPerTask,
+                                            tuple(myRange));
+
+    if numChunks == 1 {
+      yield myRange;
     } else {
   
-      const chunkSize = this.size / chunks;
-      const spill = if this.size % chunks then 1 else 0;
-  
-      coforall i in 1..(chunks+spill) {
-        var low = ((i - 1) * chunkSize) + 1;
-        var high = low + chunkSize - 1;
-     
-        //Upper bound
-        if high > this.size then high = this.size;
-  
-        yield(low..high);
+      coforall chunk in 0..#numChunks {
+        // _computeBlock assumes 0-based ranges
+        const (lo,hi) = _computeBlock(length, numChunks, chunk, length-1);
+        // adjust for 1-based indexing
+        yield lo+1..hi+1;
       }
     }
   }
@@ -151,13 +140,7 @@ module ChapelTuple {
   iter _tuple.these(param tag:iterKind, followThis)
       where tag == iterKind.follower
   {
-    compilerError("Iteration over tuples is not yet supported. If you intended to use zippered iteration, you need to add the new keyword 'zip' before the tuple of iteratable expressions.");
-    
-    // johnk: This is just a draft.
-    // At the time of writting only homogeneous tuple iteration was supported
-    // and it was decided to no permit tuple iteration.  Rather than remove
-    // this code, it was left in to save reimplementation.
-    for i in followThis do yield(this(i));;
+    for i in followThis do yield this(i);
   }
   
   //

test/types/tuple/johnk/homogeneous_tuple_iterations.chpl
@@ -1,7 +1,17 @@
-for i in zip(1..8, 2..60, 3..39) do writeln(i);
+//This should no longer be implicitly zippered!
+//An error will result if it is considered zippered.
+for i in (1..8, 2..60, 3..39) do writeln(i);
 
-forall i in (1,2,3) {}
+for i in (1,2,3) {
+  writeln(i);
+}
 
-coforall (i,j) in ((1,2),(3,4),(5,6),(7,8),(9,10)) {}
+for i in ((1,2),(3,4),(5,6),(7,8),(9,10)) {
+    writeln(i);
+}
+
+forall i in ((1,2),(3,4),(5,6),(7,8),(9,10)) {}
+
+coforall i in ((1,2),(3,4),(5,6),(7,8),(9,10)) {}
 
 writeln("SUCCESS");

test/types/tuple/johnk/homogeneous_tuple_iterations.future
@@ -1,1 +1,0 @@
-feature request: As iteration over heterogeneous tuples is unable to supported, I elected to not permit iteration of homogeneous tuples for consistency.  If desired, the compilerError calls in _tuple.these calls can be removed after 1.6 to permit iteration over homogeneous tuples. 

test/types/tuple/johnk/homogeneous_tuple_iterations.good
@@ -1,4 +1,12 @@
 1..8
 2..60
 3..39
+1
+2
+3
+(1, 2)
+(3, 4)
+(5, 6)
+(7, 8)
+(9, 10)
 SUCCESS

test/types/tuple/sungeun/iteration/forall.chpl
@@ -1,0 +1,23 @@
+config var x = 1.0;
+config var y = -1.0;
+config var z = 83.0;
+
+var A = [x, y, z];
+var B = [y, z, x];
+var C = [z, x, y];
+
+forall e in (x,y,z) do writeln(e);
+
+forall e in (A,B,C) do writeln(e);
+
+var t = (A,B,C);
+forall e in t do writeln(e);
+
+config param s = 33;
+var t1: s*x.type;
+var sum: atomic x.type;
+
+forall e in t1 do e = sum.fetchAdd(1);
+
+if sum.read() != s:x.type then halt("Sum should be ", s, "!");
+forall e in t1 do if e!=0.0 then halt("Element should be zero!");

test/types/tuple/sungeun/iteration/forall.execopts
@@ -1,0 +1,1 @@
+--dataParTasksPerLocale=1

test/types/tuple/sungeun/iteration/forall.good
@@ -1,0 +1,9 @@
+1.0
+-1.0
+83.0
+1.0 -1.0 83.0
+-1.0 83.0 1.0
+83.0 1.0 -1.0
+1.0 -1.0 83.0
+-1.0 83.0 1.0
+83.0 1.0 -1.0

commit bd5988557c1d5115dc8120fb5de9c5a67f5c462f

Fix outstanding tuple iteration regressions from r21004
[discussed with bradc and diten]

This commit fixes the two outstanding regressions from the tuple
iteration commit (r21004) and adds a couple of futures surrounding the
issues.

The two failing tests used nested zippered iteration, which we
no longer support (both were incorrectly updated when we added the zip
keyword).

functions/deitz/iterators/temps/test_iterator_temp8.chpl:

  Made this an error message future, as we are getting a message about
  heterogenous tuple iteration even though the tuples are actually
  homogenous.

types/tuple/sungeun/iteration/iteratable.chpl

  This future captures what is really going wrong with the previous
  test.  Currently we cannot iterate over a tuple of iteratable
  expressions, as their types in function resolution are represented
  by the proto iterator records.  It's possible the fix is relatively
  simple, but I did not investigate.

functions/deitz/iterators/test_var_iterator6.chpl

  Removed the nested zippered iteration

functions/iterators/sungeun/zip_expression.chpl

  Added this as a semantic future for zip() as a stand-alone
  expression.  This would effectively enable nested zippered
  iteration.



git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@21089 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

test/functions/deitz/iterators/temps/test_iterator_temp8.future
@@ -1,0 +1,11 @@
+error message: Incorrect error message when zippering in a tuple with an array
+
+The original intent of this test was to test nested zippered
+iteration, which is not something we support any more.  The test was
+updated incorrectly.
+
+The current error message states that we are trying to zipper a
+heterogeneous tuple, but that is incorrect.
+
+See test/types/tuple/sungeun/iteration.future for details on they
+actual bug here.

test/functions/deitz/iterators/temps/test_iterator_temp8.good
@@ -1,4 +1,1 @@
-((1, 4), 1)
-((2, 3), 2)
-((3, 2), 3)
-((4, 1), 4)
+FIX ME!

test/functions/deitz/iterators/test_var_iterator6.chpl
@@ -8,16 +8,6 @@ for (a, b, c, i, j, k) in zip(A, B, C, 1..5, 6..10, 11..15) {
 
 writeln((A, B, C));
 
-A = 0; B = 0; C = 0; // reset
-
-for (a, (b, (c, (i, (j, k))))) in zip(A, (B, (C, (1..5, (6..10, 11..15))))) {
-  a = i;
-  b = j;
-  c = k;
-}
-
-writeln((A, B, C));
-
 for (a, b, c) in zip(A, B, C) {
   a += b + c;
 }

test/functions/deitz/iterators/test_var_iterator6.good
@@ -1,4 +1,3 @@
 (1 2 3 4 5, 6 7 8 9 10, 11 12 13 14 15)
-(1 2 3 4 5, 6 7 8 9 10, 11 12 13 14 15)
 (18 21 24 27 30, 6 7 8 9 10, 11 12 13 14 15)
 (18 21 24 27 30, 35 40 45 50 55, 11 12 13 14 15)

test/functions/iterators/sungeun/zip_expression.chpl
@@ -1,0 +1,12 @@
+var A, B, C: [1..5] int;
+
+var z0 = zip(6..10, 11..15);
+var z1 = zip(1..5, z0);
+var z2 = zip(C, z1);
+var z3 = zip(B, z2);
+
+for (a, (b, (c, (i, (j, k))))) in zip(A, z3) {
+  a = i;
+  b = j;
+  c = k;
+}

test/functions/iterators/sungeun/zip_expression.future
@@ -1,0 +1,4 @@
+semantic: Should zip() be a stand-alone expression
+
+As shown in this test case, this would effectively enable nested
+zippered iteration.

test/functions/iterators/sungeun/zip_expression.good
@@ -1,0 +1,1 @@
+(1 2 3 4 5, 6 7 8 9 10, 11 12 13 14 15)

test/types/tuple/sungeun/iteration/iteratable.chpl
@@ -12,8 +12,6 @@ iter i2() {
   yield 1;
 }
 
-for ij in zip((1,2), (1,2), (1, 2)) do
+for ij in (i1(), i2()) do
   writeln(ij);
 
-for ij in zip((i1(), i2()), i1()) do
-  writeln(ij);

test/types/tuple/sungeun/iteration/iteratable.future
@@ -1,0 +1,11 @@
+bug: Cannot iterate over a tuple of iteratable expressions
+
+In this example:
+
+   for ij in (i1(), i2()) do
+     writeln(ij);
+
+the first tuple (i1(), i2()) is a tuple of arrays, yet their types are
+represented by the iterator record, so that the test for homogenous
+tuple fails during function resolution.
+

test/types/tuple/sungeun/iteration/iteratable.good
@@ -1,0 +1,4 @@
+(1, 4)
+(2, 3)
+(3, 2)
+(4, 1)