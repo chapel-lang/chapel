chapel-lang/chapel

commit 0b55742a81278f95d4fa9cbac0d2edaa9a695da6

Rename compiler-generated types for classes to make legal/clearer
[reviewed by mppf]

This commit renames the compiler-generated types generated to
implement Chapel classes in order to make them legal and clearer.

In particular, given Chapel code:

  class C {
    ...
  }

traditionally, Chapel has generated:

  typedef struct __C {
  } _C;

  typedef struct __C* C;

C reserves identifiers starting with underscores.  In spite of this,
Chapel has used leading underscores for some time and gotten away with
it.  That is, until recently when tests that declare a class named 'C'
started failing on Cygwin due to our use of _C and __C as symbol names
in the generated code which conflict with some built-in symbols in the
Cygwin header.

[Over time, I'd like to remove all leading underscores from
Chapel-generated identifiers, so this can be considered a
first step with the goal of making Cygwin pass all release
tests again.]

With this change, we add a 'chpl_' prefix to these identifiers and a
suffix to make their purpose clearer, resulting in:

  typedef struct chpl_C_s {
  } chpl_C_object;

  typedef struct chpl_C_s* C;

Far more importantly, the code that generates these struct names has
been refactored from a half dozen places scattered throughout the
compiler into a single method:

         char* ClassType::classStructName(bool standalone)

whose role is to return the name of the C struct corresponding to the
class type -- "chpl_C_s" or "chpl_C_object" depending on whether
'standalone' is false or true, respectively.  ("standalone" means
"the identifier should stand alone without a 'struct' keyword").


Added ClassType::classStructName() and used if in a few places
--------------------------------------------------------------
compiler/include/type.h
compiler/AST/type.cpp

Made use of ClassType::classStructName()
----------------------------------------
compiler/AST/symbol.cpp
compiler/AST/expr.cpp

Corresponding renaming of chpl wide string structs

compiler/AST/type.cpp
@@ -538,16 +538,57 @@ void ClassType::replaceChild(BaseAST* old_ast, BaseAST* new_ast) {
   INT_FATAL(this, "Unexpected case in ClassType::replaceChild");
 }
 
+#define CLASS_STRUCT_PREFIX "chpl_"
+
+//
+// Construct the name of the struct used in the generated code to
+// represent the object definition for a ClassType.
+//
+// When 'standalone'== false, we generates an identifier designed to
+// be used in combination with "struct "; true generates a name that's
+// been typedef'd such that the 'struct ' is not required (i.e., it
+// stands alone).
+//
+// By example, for a Chapel class named C, we'd generate:
+//
+//   typedef struct <classStructName(false)> {
+//     ...class members here...
+//   } <classStructName(true)>;
+//  typedef <classStructName(true)>* C;
+//
+// which in the current implementation amounts to:
+//
+//   typedef struct chpl_C_s {
+//     ...class members here...
+//   } chpl_C_object;
+//  typedef struct chpl_C_s* C;
+//
+// For records and unions (classTag != CLASS_CLASS), the pointer
+// version above isn't used, so the original identifier is used to
+// name the typedef for the struct itself.
+//
+const char* ClassType::classStructName(bool standalone) {
+  if (standalone) {
+    const char* basename = symbol->cname;
+    if (classTag == CLASS_CLASS && !symbol->hasFlag(FLAG_EXTERN))
+      return astr(CLASS_STRUCT_PREFIX, basename, "_object");
+    else
+      return basename;
+  } else {
+    return astr(CLASS_STRUCT_PREFIX, symbol->cname, "_s");
+  }
+}
+
 GenRet ClassType::codegenClassStructType()
 {
   GenInfo* info = gGenInfo;
   GenRet ret;
   if(classTag == CLASS_CLASS) {
     if( info->cfile ) {
-      ret.c = std::string("_") + symbol->cname;
+      ret.c = std::string(classStructName(true));
     } else {
 #ifdef HAVE_LLVM
-      ret.type = info->lvt->getType(std::string("_") + symbol->cname);
+      ret.type = info->lvt->getType(classStructName(true));
 #endif
     }
   } else {
@@ -614,7 +655,7 @@ void ClassType::codegenDef() {
     }
   } else {
     if( outfile ) {
-      fprintf(outfile, "typedef struct __%s", symbol->cname);
+      fprintf(outfile, "typedef struct %s", this->classStructName(false));
       if (classTag == CLASS_CLASS && dispatchParents.n > 0) {
         /* Add a comment to class definitions listing super classes */
         bool first = true;
@@ -652,10 +693,7 @@ void ClassType::codegenDef() {
         if (this->fields.length != 0)
           fprintf(outfile, "} _u;\n");
       }
-      fprintf(outfile, "} ");
-      if (classTag == CLASS_CLASS)
-        fprintf(outfile, "_");
-      fprintf(outfile, "%s;\n\n", symbol->codegen().c.c_str());
+      fprintf(outfile, "} %s;\n\n", this->classStructName(true));
     } else {
 #ifdef HAVE_LLVM
       int paramID = 0;
@@ -707,7 +745,7 @@ void ClassType::codegenDef() {
         for_fields(field, this) {
           llvm::Type* fieldType = field->type->symbol->codegen().type;
           if(field->hasFlag(FLAG_SUPER_CLASS))
-            fieldType = info->lvt->getType(std::string("_") + field->type->symbol->cname);
+            fieldType = info->lvt->getType(classStructName(false));
           INT_ASSERT(fieldType);
           params.push_back(fieldType);
           GEPMap.insert(std::pair<std::string, int>(field->cname, paramID++));
@@ -754,8 +792,8 @@ void ClassType::codegenPrototype() {
   else if (symbol->hasFlag(FLAG_DATA_CLASS)) return; // done in codegenDef
   else if (classTag == CLASS_CLASS) {
     if( info->cfile ) {
-      fprintf(info->cfile, "typedef struct __%s *%s;\n",
-              symbol->cname, symbol->cname);
+      fprintf(info->cfile, "typedef struct %s* %s;\n", 
+              this->classStructName(false), symbol->cname);
     } else {
 #ifdef HAVE_LLVM
       std::string sname("_"); sname += symbol->cname; // ie _ClassType

compiler/AST/expr.cpp
@@ -4003,21 +4003,11 @@ GenRet CallExpr::codegen() {
     }
     case PRIM_CHPL_ALLOC:
     case PRIM_CHPL_ALLOC_PERMIT_ZERO: {
-      bool needs_underbar = false;
-
-      // if Chapel class or record
-      if (TypeSymbol *t = toTypeSymbol(typeInfo()->symbol)) {
-        if (toClassType(t->type)) {
-          needs_underbar = true;
-        }
-      }
-
       GenRet size;
-      if( needs_underbar ) {
-        std::string struct_name;
-        struct_name += "_";
-        struct_name += typeInfo()->symbol->cname;
-        size = codegenSizeof(struct_name.c_str());
+
+      // If Chapel class or record
+      if (ClassType* ct = toClassType(toTypeSymbol(typeInfo()->symbol)->type)) {
+        size = codegenSizeof(ct->classStructName(true));
       } else {
         size = codegenSizeof(typeInfo());
       }
@@ -4476,9 +4466,12 @@ GenRet CallExpr::codegen() {
     if (argType == NULL) {
       INT_FATAL("could not get a type symbol");
     }
- 
-    std::string ctype = "_";
-    ctype += argType->typeInfo()->symbol->cname;
+    
+    ClassType* ct = toClassType(argType->typeInfo());
+    if (!ct) {
+      INT_FATAL("Expected a class type in %s argument", fname);
+    }
+    std::string ctype = ct->classStructName(true);
 
     genComment(fn->cname, true);
     GenRet nodeVal = codegenValue(codegenFieldPtr(get(1), "node"));

compiler/AST/symbol.cpp
@@ -509,11 +509,12 @@ void VarSymbol::codegenDefC() {
 
   if (type == dtVoid)
     return;
-  const char* _ifsuper = "";
-  if (this->hasFlag(FLAG_SUPER_CLASS))
-    _ifsuper = "_";
-  std::string str = _ifsuper + type->codegen().c + " " + cname;
-  if (ClassType* ct = toClassType(type)) {
+  ClassType* ct = toClassType(type);
+  std::string typestr =  (this->hasFlag(FLAG_SUPER_CLASS) ?
+                          std::string(ct->classStructName(true)) :
+                          type->codegen().c);
+  std::string str = typestr + " " + cname;
+  if (ct) {
     if (ct->classTag == CLASS_CLASS) {
       if (isFnSymbol(defPoint->parentSymbol)) {
         str += " = NULL";  

compiler/include/type.h
@@ -124,6 +124,7 @@ class ClassType : public Type {
   GenRet codegenClassStructType();
   void codegenDef();
   void codegenPrototype();
+  const char* classStructName(bool standalone);
   int codegenStructure(FILE* outfile, const char* baseoffset);
   int codegenFieldStructure(FILE* outfile, bool nested, const char* baseoffset);
 

runtime/include/chpltypes.h
@@ -155,7 +155,7 @@ int8_t ascii(chpl_string s) {
   return (int8_t) *s;
 }
 
-struct __chpl____wide_chpl_string;
+struct chpl_chpl____wide_chpl_string_s;
 
 //
 // stopgap formatting
@@ -167,9 +167,9 @@ char* chpl_glom_strings(int numstrings, ...);
 
 chpl_bool string_contains(chpl_string x, chpl_string y);
 chpl_string string_copy(chpl_string x, int32_t lineno, chpl_string filename);
-chpl_string chpl_wide_string_copy(struct __chpl____wide_chpl_string* x, int32_t lineno, chpl_string filename);
-void chpl_string_widen(struct __chpl____wide_chpl_string* x, chpl_string from);
-void chpl_comm_wide_get_string(chpl_string* local, struct __chpl____wide_chpl_string* x, int32_t tid, int32_t lineno, chpl_string filename);
+chpl_string chpl_wide_string_copy(struct chpl_chpl____wide_chpl_string_s* x, int32_t lineno, chpl_string filename);
+void chpl_string_widen(struct chpl_chpl____wide_chpl_string_s* x, chpl_string from);
+void chpl_comm_wide_get_string(chpl_string* local, struct chpl_chpl____wide_chpl_string_s* x, int32_t tid, int32_t lineno, chpl_string filename);
 chpl_string string_concat(chpl_string x, chpl_string y, int32_t lineno, chpl_string filename);
 chpl_string string_index(chpl_string x, int i, int32_t lineno, chpl_string filename);
 chpl_string string_select(chpl_string x, int low, int high, int32_t lineno, chpl_string filename);

runtime/src/chpltypes.c
@@ -66,12 +66,12 @@ typedef struct __chpl_localeID_t {
   c_subloc_t subloc;
 } chpl_localeID_t;
 
-struct __chpl____wide_chpl_string {
+struct chpl_chpl____wide_chpl_string_s {
   chpl_localeID_t locale;
   chpl_string addr;
   int64_t size;
 };
-typedef struct __chpl____wide_chpl_string chpl____wide_chpl_string;
+typedef struct chpl_chpl____wide_chpl_string_s chpl____wide_chpl_string;
 
 chpl_string
 chpl_wide_string_copy(chpl____wide_chpl_string* x, int32_t lineno, chpl_string filename) {
@@ -101,7 +101,7 @@ chpl_string_widen(chpl____wide_chpl_string* x, chpl_string from)
 
 // un-macro'd CHPL_COMM_WIDE_GET_STRING
 void
-chpl_comm_wide_get_string(chpl_string* local, struct __chpl____wide_chpl_string* x, int32_t tid, int32_t lineno, chpl_string filename)
+chpl_comm_wide_get_string(chpl_string* local, struct chpl_chpl____wide_chpl_string_s* x, int32_t tid, int32_t lineno, chpl_string filename)
 {
   char* chpl_macro_tmp =
       chpl_mem_allocMany(x->size, sizeof(char),

commit 09a7870a0eca419884c088e3e2a617a9bff8ff93

Fixing bug with extern classes
[reviewed by sungeun]

This commit fixes a bug that I inadvertantly introduced in r21075 when
I changed the names of the structures generated by the compiler for
classes.  For external classes, I accidentally broke the strings that
were code generated for the malloc()s and ended up malloc-ing the size
of a pointer rather than the struct representing the object.  None of
our tests picked this up (so far as I can tell) because our extern
class tests all use classes that are no bigger than a pointer in size.

To prevent this mistake from occurring again, I'm checking in a test
that uses a bigger class which results in memory problems (under
valgrind and in my executions also outside of valgrind) should it
happen again.

One note to point out is that, traditionally, we have required an
external class named "myclass" to name its struct "_myclass".  In
r21075, I changed the compiler-generated name for such a class to be
chpl_myclass_object (a name that Michael and I agreed upon at the
time).  In this commit, I originally thought I'd unify these names for
consistency, but it seems dumb to require the user class to start with
a chpl_ prefix, and verbose/ugly to force them to add a _object suffix
(something that I hadn't considered in the original commit).  So
rather than requiring them to unify the name with internal Chapel
practices, I kept the naming requirements the same as in previous
releases for simplicity.  I think it's worth considering using a
different name -- preferably one without a leading underscore -- but
left that conversation/decision for another day.




git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@21222 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

compiler/AST/type.cpp
@@ -563,9 +563,18 @@ void ClassType::replaceChild(BaseAST* old_ast, BaseAST* new_ast) {
 const char* ClassType::classStructName(bool standalone) {
   if (standalone) {
     const char* basename = symbol->cname;
-    if (classTag == CLASS_CLASS && !symbol->hasFlag(FLAG_EXTERN))
-      return astr(CLASS_STRUCT_PREFIX, basename, "_object");
-    else
+    if (classTag == CLASS_CLASS) {
+      //
+      // For extern classes, we've traditionally required them to be
+      // named _C; we could use a different naming convention, but
+      // have never agreed upon one.
+      //
+      if (symbol->hasFlag(FLAG_EXTERN)) {
+        return astr("_", basename);
+      } else {
+        return astr(CLASS_STRUCT_PREFIX, basename, "_object");
+      }
+    } else
       return basename;
   } else {
     return astr(CLASS_STRUCT_PREFIX, symbol->cname, "_s");

test/extern/bradc/classes/extern_big_class.c
@@ -1,0 +1,6 @@
+#include "extern_big_class.h"
+
+int64_t myfunc(myclass c)
+{
+  return c->e;
+}

test/extern/bradc/classes/extern_big_class.chpl
@@ -1,0 +1,13 @@
+extern class myclass {
+  var a:int(64);
+  var b:int(64);
+  var c:int(64);
+  var d:int(64);
+  var e:int(64);
+}
+
+extern proc myfunc(c:myclass):int(64);
+
+var c = new myclass(e=5);
+writeln( c );
+writeln( myfunc(c) );

test/extern/bradc/classes/extern_big_class.compopts
@@ -1,0 +1,1 @@
+extern_big_class.c extern_big_class.h

test/extern/bradc/classes/extern_big_class.good
@@ -1,0 +1,2 @@
+(a = 0, b = 0, c = 0, d = 0, e = 5)
+5

test/extern/bradc/classes/extern_big_class.h
@@ -1,0 +1,11 @@
+#include <stdint.h>
+
+typedef struct myclass_s {
+  int64_t a;
+  int64_t b;
+  int64_t c;
+  int64_t d;
+  int64_t e;
+} _myclass, *myclass;
+
+int64_t myfunc(myclass c);