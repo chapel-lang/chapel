chapel-lang/chapel

commit 7bfcf5b4d018dfa98a2d58279e234a4c9956965d

Fix a couple of alloc/free mismatches.
[Reviewed by hildeth.]

Fix a couple of places where we were allocating memory through one set
of memory management functions and freeing it through another.  With
this change we pass multilocale and parallel testing with comm=gasnet,
tasks=qthreads, mem=dlmalloc.  We used to fail a number of such tests
due to dlmalloc internal checks that intercepted chpl_mem_free() calls
which were attempting to free memory that was not in the preregistered
dlmalloc arena.

The details are:

compiler/passes/parallel.cpp
  We emit a call to chpl_task_alloc() to allocate on-wrapper argument
  bundles at parallel.cpp:117, but at line 309 we emit a call to
  chpl_mem_free() to free them.  Emit a call to chpl_task_free()
  instead.

modules/internal/ChapelArray.chpl
  We emit a call to chpl_here_alloc() to allocate integrator class
  objects at functionResolution.cpp:588, but we free those using
  chpl_mem_free() at modules/internal/ChapelArray.chpl:1970.  Free them
  using chpl_here_free() instead.

compiler/include/primitive.h
compiler/AST/primitive.cpp
compiler/AST/expr.cpp
compiler/AST/checkAST.cpp
compiler/resolution/functionResolution.cpp
compiler/optimizations/scalarReplace.cpp
compiler/optimizations/narrowWideReferences.cpp
  Add a primitive (PRIM_HERE_FREE) for chpl_here_free(), to support the
  change to ChapelArray.chpl.


git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@21707 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

compiler/AST/primitive.cpp
@@ -483,6 +483,9 @@ initPrimitive() {
   prim_def(PRIM_CHPL_ALLOC, "chpl_mem_alloc", returnInfoChplAlloc, true, true);
   prim_def(PRIM_CHPL_FREE, "chpl_mem_free", returnInfoVoid, true, true);
 
+  // These are (this is) satisfied by the module.
+  prim_def(PRIM_HERE_FREE, "chpl_here_free", returnInfoVoid);
+
   prim_def(PRIM_INIT_FIELDS, "chpl_init_record", returnInfoVoid, true);
   prim_def(PRIM_PTR_EQUAL, "ptr_eq", returnInfoBool);
   prim_def(PRIM_PTR_NOTEQUAL, "ptr_neq", returnInfoBool);

compiler/AST/checkAST.cpp
@@ -204,6 +204,7 @@ void checkPrimitives()
      case PRIM_CHPL_MEMHOOK_FREE:       // The free hook function.
      case PRIM_CHPL_ALLOC:
      case PRIM_CHPL_FREE:               // only for variables on heap?
+     case PRIM_HERE_FREE:
      case PRIM_INIT_FIELDS:             // initialize fields of a temporary record
      case PRIM_PTR_EQUAL:
      case PRIM_PTR_NOTEQUAL:

compiler/AST/expr.cpp
@@ -4950,6 +4950,24 @@ GenRet CallExpr::codegen() {
                   get(2), get(3));
       break;
     }
+    case PRIM_HERE_FREE: // This is used internally.
+    {
+      if (fNoMemoryFrees)
+        break;
+      INT_ASSERT(numActuals() == 3);
+
+      Expr * ptrExpr = get(1);
+      if( ptrExpr->typeInfo()->symbol->hasFlag(FLAG_DATA_CLASS))
+        INT_FATAL(this, "cannot delete data class");
+      GenRet ptr; 
+      ptr = codegenValue(ptrExpr);
+      if (ptrExpr->typeInfo()->symbol->hasFlag(FLAG_WIDE_CLASS)) {
+        ptr = codegenRaddr(ptr);
+      }
+      codegenCall("chpl_here_free", codegenCastToVoidStar(ptr),
+                  get(2), get(3));
+      break;
+    }
     case PRIM_CAST: 
     {
       if (typeInfo()->symbol->hasFlag(FLAG_WIDE_CLASS) ||

compiler/include/primitive.h
@@ -115,6 +115,7 @@ enum PrimitiveTag {
   PRIM_CHPL_MEMHOOK_FREE,       // The free hook function.
   PRIM_CHPL_ALLOC,
   PRIM_CHPL_FREE,               // only for variables on heap?
+  PRIM_HERE_FREE,               // Locale-specific free()
   PRIM_INIT_FIELDS,             // initialize fields of a temporary record
   PRIM_PTR_EQUAL,
   PRIM_PTR_NOTEQUAL,

compiler/optimizations/narrowWideReferences.cpp
@@ -273,7 +273,8 @@ narrowSym(Symbol* sym, WideInfo* wi) {
           (call->isPrimitive(PRIM_STRING_COPY)) ||
           (call->isPrimitive(PRIM_SETCID)) ||
           (call->isPrimitive(PRIM_CHPL_ALLOC) && call->get(1) == use) ||
-          (call->isPrimitive(PRIM_CHPL_FREE) && call->get(1) == use))
+          (call->isPrimitive(PRIM_CHPL_FREE) && call->get(1) == use) ||
+          (call->isPrimitive(PRIM_HERE_FREE) && call->get(1) == use))
         continue;
       if (call->isResolved() ||
           (call->isPrimitive(PRIM_SET_MEMBER) && call->get(3) == use) ||

compiler/optimizations/scalarReplace.cpp
@@ -205,6 +205,7 @@ scalarReplaceClass(ClassType* ct, Symbol* sym) {
             call->isPrimitive(PRIM_SETCID) ||
             call->isPrimitive(PRIM_TASK_FREE) ||
             call->isPrimitive(PRIM_CHPL_FREE) ||
+            call->isPrimitive(PRIM_HERE_FREE) ||
             (call->isResolved() &&
              call->isResolved()->hasFlag(FLAG_ALLOCATOR))))
         return false;
@@ -255,7 +256,8 @@ scalarReplaceClass(ClassType* ct, Symbol* sym) {
         addUse(useMap, use);
       } else if (call->isPrimitive(PRIM_SETCID) ||
                  call->isPrimitive(PRIM_TASK_FREE) ||
-                 call->isPrimitive(PRIM_CHPL_FREE)) {
+                 call->isPrimitive(PRIM_CHPL_FREE) ||
+                 call->isPrimitive(PRIM_HERE_FREE)) {
         //
         // we can remove the setting of the cid because it is never
         // used and we are otherwise able to remove the class

compiler/passes/parallel.cpp
@@ -306,7 +306,7 @@ static void call_block_fn_wrapper(FnSymbol* fn, CallExpr* fcall, VarSymbol* temp
     fcall->insertBefore(new CallExpr(wrap_fn, tempc));
 
   if (fn->hasFlag(FLAG_ON))
-    fcall->insertAfter(new CallExpr(PRIM_CHPL_FREE, tempc));
+    fcall->insertAfter(new CallExpr(PRIM_TASK_FREE, tempc));
   else
     ; // wrap_fn will free the formal
 

compiler/resolution/functionResolution.cpp
@@ -4415,6 +4415,7 @@ postFold(Expr* expr) {
                call->isPrimitive(PRIM_EXECUTE_TASKS_IN_LIST) ||
                call->isPrimitive(PRIM_FREE_TASK_LIST) ||
                call->isPrimitive(PRIM_CHPL_FREE) ||
+               call->isPrimitive(PRIM_HERE_FREE) ||
                (call->primitive && 
                 (!strncmp("_fscan", call->primitive->name, 6) ||
                  !strcmp("_readToEndOfLine", call->primitive->name) ||

commit 9f979af7881516d55e5709d1286c4d1f5b8ee34c

Fix compiler internal errors caused by r21707.
[Reviewed by hildeth.]

When the --baseline option is used, the changes in r21707 cause compiler
internal errors.  These are due to the emitted code not adding line and
file parameters to calls to chpl_here_alloc().  Force these to be
present.


git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@21727 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

compiler/AST/primitive.cpp
@@ -484,7 +484,7 @@ initPrimitive() {
   prim_def(PRIM_CHPL_FREE, "chpl_mem_free", returnInfoVoid, true, true);
 
   // These are (this is) satisfied by the module.
-  prim_def(PRIM_HERE_FREE, "chpl_here_free", returnInfoVoid);
+  prim_def(PRIM_HERE_FREE, "chpl_here_free", returnInfoVoid, false, true);
 
   prim_def(PRIM_INIT_FIELDS, "chpl_init_record", returnInfoVoid, true);
   prim_def(PRIM_PTR_EQUAL, "ptr_eq", returnInfoBool);

modules/internal/ChapelLocale.chpl
@@ -290,6 +290,7 @@ module ChapelLocale {
   // The allocator pragma is used by scalar replacement.
   pragma "allocator"
   pragma "no sync demotion"
+  pragma "insert line file info"
   proc chpl_here_alloc(x, md:int(16)) {
     var nbytes = __primitive("sizeof", x);
     chpl_memhook_malloc_pre(1, nbytes, md + chpl_memhook_md_num());
@@ -300,6 +301,7 @@ module ChapelLocale {
 
   pragma "allocator"
   pragma "no sync demotion"
+  pragma "insert line file info"
   proc chpl_here_calloc(x, number:int, md:int(16)) {
     extern proc chpl_task_calloc(number:int, nbytes:int) : opaque;
     var nbytes = __primitive("sizeof", x);
@@ -311,6 +313,7 @@ module ChapelLocale {
 
   pragma "allocator"
   pragma "no sync demotion"
+  pragma "insert line file info"
   proc chpl_here_realloc(x, md:int(16)) {
     var nbytes = __primitive("sizeof", x);
     chpl_memhook_realloc_pre(x, nbytes, md + chpl_memhook_md_num());
@@ -320,6 +323,7 @@ module ChapelLocale {
   }
 
   pragma "no sync demotion"
+  pragma "insert line file info"
   proc chpl_here_free(x) {
     // TODO: The pointer should really be of type opaque, but we don't 
     // handle object ==> opaque casts correctly.  (In codegen, opaque behaves 