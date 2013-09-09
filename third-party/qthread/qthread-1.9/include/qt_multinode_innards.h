2e5901562b8402fc2e6e648c57d8e4c088da0a01
Brad Chamberlain <bradcray@users.noreply.github.com>
2013-09-09 19:55:13 +0000

    Removing Qthreads version 1.9
    Version 1.10 of Qthreads seems to be performing as correctly as 1.9 did, so
    blowing away the old version.
    
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@21833 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- third-party/qthread/qthread-1.9/include/qt_multinode_innards.h
+++
@@ -1,8 +0,0 @@
-#ifndef QT_MULTINODE_INNARDS_H
-#define QT_MULTINODE_INNARDS_H
-
-#include "qt_visibility.h"
-
-int INTERNAL qthread_multinode_initialize(void);
-
-#endif /* #ifndef QT_MULTINODE_INNARDS_H */
