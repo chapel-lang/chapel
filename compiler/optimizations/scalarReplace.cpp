chapel-lang/chapel

commit b20e1b2ba00f2d6cda008f7dc2b205cbd2e160d3

[Co-authored with Greg.  Reviewed by David and Greg]
This commit replaces calls to chpl_mem_alloc() and chpl_mem_free()
with calls to the locale model specific chpl_here_alloc() and
chpl_here_free().

In the compiler, such calls can be inserted both before and after
resolution.  Since type information is incomplete before resolution,
we must fix up the type information for some of the primitives
inserted for chpl_here_alloc() and chpl_here_free().  In particular,
for chpl_here_alloc(), we fix up the type for sizeof and insert a cast
to the type.  For chpl_here_free() we dereference any ref types that
were used.

Currently both locale models directly call the corresponding
chpl_mem_* functions.

This change exposes an existing bug in iterator inlining.  After
discussions with the group, it was decided that we would live with the
additional memory leaks.  Also, test_nested_var_iterator2.chpl fails
now, but it is already known to expose this bug when scalar
replacement is disabled (that's what test_nested_var_iterator3.chpl
is).  It has been turned into a future, that points to
test_nested_var_iterator3.future.

=======
modules/internal/localeModels/flat/LocaleModel.chpl:
modules/internal/localeModels/numa/LocaleModel.chpl:
- Have chp_here_* allocator and free routines call the chpl_mem_*
  varieties

modules/internal/ChapelArray.chpl:
- Replace call to chpl_mem_free with chpl_here_free

modules/internal/ChapelSyncvar.chpl:
- Don't need special chpl_here* functions any more

compiler/include/symbol.h:
compiler/AST/symbol.cpp:
- Added gChplHereAlloc and gChplHereFree, used for inserting calls
  post-resolution

compiler/include/flags_list.h:
- Added FLAG_LOCALE_MODEL_ALLOC and FLAG_LOCALE_MODEL_FREE

compiler/include/primitive.h:
compiler/AST/primitive.cpp:
compiler/AST/checkAST.cpp:
- Remove PRIM_CHPL_ALLOC and PRIM_CHPL_FREE
- Fixed up return type of PRIM_SIZEOF to be SIZE_TYPE

compiler/include/expr.h:
- remove here_alloc()

compiler/optimizations/scalarReplace.cpp
@@ -180,9 +180,8 @@ scalarReplaceClass(ClassType* ct, Symbol* sym) {
   if (!alloc)
     return false;
   if (!
-      (alloc->isPrimitive(PRIM_CHPL_ALLOC) ||
-       (alloc->isResolved() &&
-        alloc->isResolved()->hasFlag(FLAG_ALLOCATOR))))
+      (alloc->isResolved() &&
+       alloc->isResolved()->hasFlag(FLAG_ALLOCATOR)))
     return false;
 
   //
@@ -197,15 +196,14 @@ scalarReplaceClass(ClassType* ct, Symbol* sym) {
       // The use must appear as the first argument in the containing expression.
       if (se != call->get(1))
         return false;
-
       // The use must be the first argument of one of the following primitives.
       if (!(call->isPrimitive(PRIM_SET_MEMBER) ||
             call->isPrimitive(PRIM_GET_MEMBER) ||
             call->isPrimitive(PRIM_GET_MEMBER_VALUE) ||
             call->isPrimitive(PRIM_SETCID) ||
-            call->isPrimitive(PRIM_CHPL_FREE) ||
             (call->isResolved() &&
-             call->isResolved()->hasFlag(FLAG_ALLOCATOR))))
+             (call->isResolved()->hasFlag(FLAG_ALLOCATOR) ||
+             call->isResolved()->hasFlag(FLAG_LOCALE_MODEL_FREE)))))
         return false;
     }
   }
@@ -253,7 +251,8 @@ scalarReplaceClass(ClassType* ct, Symbol* sym) {
         call->replace(use);
         addUse(useMap, use);
       } else if (call->isPrimitive(PRIM_SETCID) ||
-                 call->isPrimitive(PRIM_CHPL_FREE)) {
+                 (call->isResolved() &&
+                  call->isResolved()->hasFlag(FLAG_LOCALE_MODEL_FREE))) {
         //
         // we can remove the setting of the cid because it is never
         // used and we are otherwise able to remove the class

test/arrays/deitz/part7/test_descriptor_frees.good
@@ -1,2 +1,2 @@
 10.0 11.0 12.0 13.0 14.0 15.0 16.0 17.0 18.0 19.0
-0
+640

test/functions/deitz/nested/test_nested_var_iterator2.future
@@ -1,0 +1,4 @@
+bug: Constant propagation is improperly applied when aliases are present.
+
+See test_nested_var_iterator3.future
+

test/memory/figueroa/LeakedMemory2.good
@@ -1,2 +1,2 @@
-Amount of leaked memory after calling foo(): 0
+Amount of leaked memory after calling foo(): 64
 sum is 102

test/memory/figueroa/LeakedMemory5.good
@@ -1,4 +1,4 @@
 1 1 1 1
-Amount of leaked memory after calling foo(): 8
+Amount of leaked memory after calling foo(): 136
 sum is 100
 0 1 1 1 1

test/memory/figueroa/LeakedMemoryArrayOfClasses.good
@@ -1,1 +1,1 @@
-Amount of leaked memory after deleting array c: 0
+Amount of leaked memory after deleting array c: 64

test/memory/sungeun/refCount/domainMaps.good
@@ -49,12 +49,12 @@ total:
 Replicated Dist
 ===============
 Copy of domain map:
-	0 bytes leaked
+	80 bytes leaked
 Return domain map:
-	128 bytes leaked
+	288 bytes leaked
 Create domain:
 	320 bytes leaked
 Create domain and array:
 	1088 bytes leaked
 total:
-	1792 bytes leaked
+	2192 bytes leaked

compiler/AST/checkAST.cpp
@@ -162,8 +162,6 @@ void checkPrimitives()
      case PRIM_GET_SERIAL:              // get serial state
      case PRIM_SET_SERIAL:              // set serial state to true or false
      case PRIM_SIZEOF:
-     case PRIM_CHPL_ALLOC:
-     case PRIM_CHPL_FREE:               // only for variables on heap?
      case PRIM_INIT_FIELDS:             // initialize fields of a temporary record
      case PRIM_PTR_EQUAL:
      case PRIM_PTR_NOTEQUAL:

commit e83a0988ed453aeac7e074ff1f413af822edff15

Fix scalar replacement for classes (broken by r21945)
[Reviewed by David]

This commit fixes scalar replacement for classes which was broken by
r21945.

Specifically, when considering replacement for class objects, the code
only considers the variable if the first definition is an allocation
(a call to chpl_mem_alloc prior to r21945).  As of r21945, the
compiler inserted calls to chpl_here_alloc() require in a PRIM_CAST,
so we must now look for the cast as the first definition as well as
the call to the allocation in the previous statement.  Similarly, when
looking at the uses of the variable, the calls to chpl_here_free()
take an argument wrapped in PRIM_CAST_TO_VOID_STAR, and so this case
had to be handled.

As noted in the commit message for r21945, disabling scalar
replacement for classes exposed the iterator inlining bug.  This
commit reverts the .good files and removes the future that was added
for that issue.

Note: LeakedMemory5.chpl still leaks one extra object due to r21944
(using sublocales in leader iterators).  David is currently testing a
fix for it.



git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@21953 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

compiler/optimizations/scalarReplace.cpp
@@ -173,10 +173,19 @@ scalarReplaceClass(ClassType* ct, Symbol* sym) {
   Vec<SymExpr*>* defs = defMap.get(sym);
   if (!defs || defs->n != 1)
     return false;
+  // As of r21945, if this variable is allocated in this scope, the
+  // first definition is the cast to the appropriate type.  The
+  // statement previous to that is the allocation.
   CallExpr* move = toCallExpr(defs->v[0]->parentExpr);
   if (!move || !move->isPrimitive(PRIM_MOVE))
     return false;
-  CallExpr* alloc = toCallExpr(move->get(2));
+  CallExpr* cast = toCallExpr(move->get(2));
+  if (!cast || !cast->isPrimitive(PRIM_CAST))
+    return false;
+  CallExpr* prevMove = toCallExpr(move->prev);
+  if (!prevMove || !prevMove->isPrimitive(PRIM_MOVE))
+    return false;
+  CallExpr* alloc = toCallExpr(prevMove->get(2));
   if (!alloc)
     return false;
   if (!
@@ -193,17 +202,24 @@ scalarReplaceClass(ClassType* ct, Symbol* sym) {
       if (!call)
         return false;
 
-      // The use must appear as the first argument in the containing expression.
+      // The use must appear as the first argument in the containing
+      // expression.
       if (se != call->get(1))
         return false;
-      // The use must be the first argument of one of the following primitives.
+      // The use must be the first argument of one of the following
+      // primitives or allocation functions.
       if (!(call->isPrimitive(PRIM_SET_MEMBER) ||
             call->isPrimitive(PRIM_GET_MEMBER) ||
             call->isPrimitive(PRIM_GET_MEMBER_VALUE) ||
             call->isPrimitive(PRIM_SETCID) ||
+            // As of r21945, compiler inserted calls to
+            // chpl_here_free() have as its first argument a void *
+            call->isPrimitive(PRIM_CAST_TO_VOID_STAR) ||
             (call->isResolved() &&
              (call->isResolved()->hasFlag(FLAG_ALLOCATOR) ||
-             call->isResolved()->hasFlag(FLAG_LOCALE_MODEL_FREE)))))
+              // TODO: don't know this is necessary as the arg to free
+              // is a void *
+              call->isResolved()->hasFlag(FLAG_LOCALE_MODEL_FREE)))))
         return false;
     }
   }
@@ -229,9 +245,10 @@ scalarReplaceClass(ClassType* ct, Symbol* sym) {
   sym->defPoint->remove();
 
   //
-  // remove the only def
+  // remove the only def and the allocation
   //
   move->remove();
+  prevMove->remove();
 
   //
   // replace uses of sym with new vars
@@ -251,6 +268,8 @@ scalarReplaceClass(ClassType* ct, Symbol* sym) {
         call->replace(use);
         addUse(useMap, use);
       } else if (call->isPrimitive(PRIM_SETCID) ||
+                 // TODO: don't know if this is still needed.  The
+                 // PRIM_CAST_TO_VOID_STAR case may take care of it.
                  (call->isResolved() &&
                   call->isResolved()->hasFlag(FLAG_LOCALE_MODEL_FREE))) {
         //
@@ -260,6 +279,15 @@ scalarReplaceClass(ClassType* ct, Symbol* sym) {
         // class reference, we can remove the call to free it
         //
         call->remove();
+      } else if (call->isPrimitive(PRIM_CAST_TO_VOID_STAR)) {
+        CallExpr* parent = toCallExpr(call->parentExpr);
+        INT_ASSERT(parent);
+        CallExpr* parentNext = toCallExpr(parent->next);
+        if (parentNext &&
+            parentNext->isResolved() &&
+            parentNext->isResolved()->hasFlag(FLAG_LOCALE_MODEL_FREE))
+          parentNext->remove();
+        parent->remove();
       } else if (call->isPrimitive(PRIM_SET_MEMBER)) {
         SymExpr* member = toSymExpr(call->get(2));
         call->primitive = primitives[PRIM_MOVE];

test/arrays/deitz/part7/test_descriptor_frees.good
@@ -1,2 +1,2 @@
 10.0 11.0 12.0 13.0 14.0 15.0 16.0 17.0 18.0 19.0
-640
+0

test/functions/deitz/nested/test_nested_var_iterator2.future
@@ -1,4 +1,0 @@
-bug: Constant propagation is improperly applied when aliases are present.
-
-See test_nested_var_iterator3.future
-

test/memory/figueroa/LeakedMemory2.good
@@ -1,2 +1,2 @@
-Amount of leaked memory after calling foo(): 64
+Amount of leaked memory after calling foo(): 0
 sum is 102

test/memory/figueroa/LeakedMemory5.good
@@ -1,4 +1,4 @@
 1 1 1 1
-Amount of leaked memory after calling foo(): 136
+Amount of leaked memory after calling foo(): 8
 sum is 100
 0 1 1 1 1

test/memory/figueroa/LeakedMemoryArrayOfClasses.good
@@ -1,1 +1,1 @@
-Amount of leaked memory after deleting array c: 64
+Amount of leaked memory after deleting array c: 0

test/memory/sungeun/refCount/domainMaps.good
@@ -49,12 +49,12 @@ total:
 Replicated Dist
 ===============
 Copy of domain map:
-	80 bytes leaked
+	0 bytes leaked
 Return domain map:
-	288 bytes leaked
+	128 bytes leaked
 Create domain:
 	320 bytes leaked
 Create domain and array:
 	1088 bytes leaked
 total:
-	2192 bytes leaked
+	1792 bytes leaked