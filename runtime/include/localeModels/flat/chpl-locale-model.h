11a98e3205d3ccc203a9cc3e071847d7720c2f76
Greg Titus <gbtitus@users.noreply.github.com>
2013-10-14 20:10:09 +0000

    In the flat locale model, a global locale ID does not need a sublocale.
    [Reviewed by Sung.]
    
    The flat locale model does not have sublocales, so it does not need a
    sublocale member in its chpl_localeID_t struct.  We used to have to have
    one anyway, because the compiler contained an assumption (in its wide
    pointer support) that chpl_localeID_t was a 2-element composite data
    type.  We removed that assumption in r22118, so now we can clean up the
    chpl_localeID_t type for the flat locale model.  Specifically, here we
    remove the (unused) ignore_subloc member, and adjust the symbolic
    CHPL_LOCALEID_T_INIT initializer value to just have one value (for the
    node).
    
    On the side, I also converted the node member from a 32-bit bitfield of
    plain int type to a non-bitfield member of type int32_t.  It was only a
    bitfield as a result of an earlier effort to make the unused sublocale
    field be just 1 bit long.
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@22125 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/include/localeModels/flat/chpl-locale-model.h
+++ runtime/include/localeModels/flat/chpl-locale-model.h
@@ -13,15 +13,14 @@
 // This is the type of a global locale ID.
 //
 typedef struct {
-  int node:          32;
-  int ignore_subloc: 32;  // needed only for compiler-emitted initializations
+  int32_t node;
 } chpl_localeID_t;
 
 //
 // This is the initializer for a chpl_localeID_t.  This macro is
 // referenced explicitly in the compiler, in symbol.cpp.
 //
-#define CHPL_LOCALEID_T_INIT  {0, 0}
+#define CHPL_LOCALEID_T_INIT  {0}
 
 //
 // This is the external copy constructor for a chpl_localeID_t, specified
