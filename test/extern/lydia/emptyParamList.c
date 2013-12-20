2e331852908ab1e25fb70f4e7224c5fd298f466c
Lydia Duncan <lydia.duncan16@gmail.com>
2013-12-20 22:59:54 +0000

    Fixing this future a little, as Brad pointed out that with a few tweaks and throwing --no-cc-warnings the test will pass.  It is now a feature request instead of a bug, as I believe the behavior is still not ideal.
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@22469 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- test/extern/lydia/emptyParamList.c
+++ test/extern/lydia/emptyParamList.c
@@ -1,12 +1,11 @@
+#include "emptyParamList.h"
+
 #include <stdio.h>
 
-void foo ();
-void bar (void);
-
 void foo() {
-  printf("I'm just an empty method, nobody loves me!");
+  printf("I'm just an empty method, nobody loves me!\n");
 }
 
 void bar(void) {
-  printf("He's just an empty method, from an empty function tree!");
+  printf("He's just an empty method, from an empty function tree!\n");
 }
