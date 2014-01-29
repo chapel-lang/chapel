00c06e263de3d2295ac98a83d13fa362d585af5c
Thomas Van Doren <thomas.vandoren@gmail.com>
2014-01-29 00:07:54 +0000

    Fix testing log location for other tests.
    r: hildeth, lydia-duncan
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@22602 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- util/cron/common-memleaks.bash
+++ util/cron/common-memleaks.bash
@@ -6,3 +6,6 @@
 source $(cd $(dirname $0) ; pwd)/common.bash
 
 export CHPL_COMM=none
+export CHPL_NIGHTLY_LOGDIR=/data/sea/cascade/chapel/NightlyMemLeaks
+export CHPL_NIGHTLY_STATDIR=$CHPL_NIGHTLY_LOGDIR/Stats
+export CHPL_NIGHTLY_CRON_LOGDIR=$CHPL_NIGHTLY_LOGDIR
