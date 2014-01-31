chapel-lang/chapel

commit d4cdefd16a1a5bdd7be4914543fec9644d4b9da0

Put the result of pthread_mutex_lock() calls into a variable. [trivial]
This should make the code easier to debug without affecting performance.

git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@22610 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

runtime/src/threads/pthreads/threads-pthreads.c
@@ -85,12 +85,14 @@ chpl_thread_mutex_p chpl_thread_mutexNew(void) {
 }
 
 void chpl_thread_mutexLock(chpl_thread_mutex_p mutex) {
-  if (pthread_mutex_lock((pthread_mutex_t*) mutex))
+  int result = pthread_mutex_lock((pthread_mutex_t*) mutex);
+  if (result)
     chpl_internal_error("pthread_mutex_lock() failed");
 }
 
 void chpl_thread_mutexUnlock(chpl_thread_mutex_p mutex) {
-  if (pthread_mutex_unlock((pthread_mutex_t*) mutex))
+  int result = pthread_mutex_unlock((pthread_mutex_t*) mutex);
+  if (result)
     chpl_internal_error("pthread_mutex_unlock() failed");
 }
 

commit 79fe490433f0253a81035be8ae36fa34eb544063

End of week update time!
REGRESSIONS:
- Fix reviewed-for-accuracy date.
- Restore the cyclic distributions regressions that appeared to be
  gone.  We only thought they were gone as a result of the log file
  issues associated with the test script changes this week.  They're
  really still here.

TRIAGE:
- Add new info for studies/amr/diffusion/level/Level_DiffusionBE_driver.
- Add a note about lnx-diten testing not working on 1/31.  This seems to
  have been a sporadic internal network issue and I should be able to
  remove this entry on Monday before the handoff.
- Add a note about baseline failures caused by r22610; Sung is working
  on these.  Some other tests also started working after this commit, due
  to not leaking memory any more.
- Add a note about failures due to test script changes this week.  I
  think all of these issues are actually resolved as of today, but I'm
  leaving the note there to remind myself to check on this before the
  handoff on Monday.


git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@22616 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

test/REGRESSIONS
@@ -202,8 +202,13 @@ block distribution regressions
 
 ================================
 cyclic distribution regressions
-(Reviewed for accuracy 01/27/14)
+(Reviewed for accuracy 01/28/14)
 ================================
+
+communication counts changed due to Vass' task functions commit (05/30/13)
+---------------------------------------------------------------------------
+[Error matching program output for distributions/robust/arithmetic/performance/assign]
+[Error matching program output for distributions/robust/arithmetic/performance/reduce]
 
 
 ========================================

test/TRIAGE
@@ -31,7 +31,6 @@ New problems
 (Ones that prove persistent should be moved to REGRESSIONS.)
 =========================================================
 
-
 darwin-gnu and darwin-clang non-deterministic compiler failures
 ---------------------------------------------------------------
 
@@ -81,6 +80,17 @@ nondet. baseline output
 [Error matching program output for studies/amr/diffusion/level/Level_DiffusionBE_driver (compopts: 1)]
 11/3/13: has been failing frequently (for --baseline) since 9/28/13
 12/29/2013:
+gbt: This seems to be due to memory corruption.  Because of the way the
+     test post-processes its output it's not obvious, but compiling and
+     running it manually I saw this in the output:
+       *** glibc detected *** ./Level_DiffusionBE_driver: double free or
+       corruption (out): 0x00007f6345179b80 ***
+     This results in a SIGABRT (probably caused by calling abort()),
+     which kills the program, and this in turn means that the file the
+     output postprocessing wants to work on doesn't exist.  A gripe
+     about this missing file is the most obvious thing in the output if
+     start_test is used.
+
 
 primers/locales fails semi-regularly for cray-prgenv-cray (11/14)
 -----------------------------------------------------------------
@@ -95,6 +105,41 @@ front-end
 =========================================================
 Problems analyzed and assigned that should be fixed soon:
 =========================================================
+
+lnx-diten cron job failed on 1/31/14
+------------------------------------
+Appears to be a Cray networking problem that made ~chapelu inaccessible
+from lnx-diten.  Watch to make sure we get lnx-diten results on 2/1/14.
+
+
+linux64.gnu.none.qthreads.numa failures due to r22610 on 1/31/14
+----------------------------------------------------------------
+Sung is working on these.  Note: they work with 16m stacks!
+[Error matching program output for distributions/robust/arithmetic/basics/test_zipper_default]
+[Error matching program output for release/examples/benchmarks/lulesh/lulesh (compopts: 1, execopts: 2)]
+[Error matching program output for release/examples/benchmarks/lulesh/lulesh (compopts: 1, execopts: 3)]
+[Error matching program output for release/examples/benchmarks/lulesh/lulesh (compopts: 1, execopts: 4)]
+[Error matching program output for release/examples/benchmarks/lulesh/lulesh (compopts: 2, execopts: 2)]
+[Error matching program output for release/examples/benchmarks/lulesh/lulesh (compopts: 2, execopts: 3)]
+[Error matching program output for release/examples/benchmarks/lulesh/lulesh (compopts: 2, execopts: 4)]
+
+But this same commit also caused some tests to start passing:
+arrays/deitz/part7/test_descriptor_frees
+memory/figueroa/LeakedMemory2
+memory/figueroa/LeakedMemoryArrayOfClasses
+memory/sungeun/refCount/arraysAndDomains
+memory/sungeun/refCount/arrays
+
+
+churn due to testing script changes
+-----------------------------------
+Inadvertent placement of the Dist block, Dist cyclic, and Baseline
+results in the /data/sea/cascade/chapel/Nightly/ directory instead of
+NightlyDist or NightlyBaseline, respectively, caused false reporting of
+new and resolved errors.  There were various problems with rsync'ing the
+performance results to SourceForge.  All these problems should be fixed
+by Fri 1/31.
+
 
 extra thread required for multilocale startup
 ---------------------------------------------