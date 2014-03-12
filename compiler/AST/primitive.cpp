1681c0c7db741750656022a66aaf37ccec67a820
Tom Hildebrandt <hilde@cray.com>
2014-03-12 23:12:30 +0000

    The conversions from c_string to string and reverse need not be marked essential. [reviewed by sungeun]
    It is OK to let deadCodeRemoval remove one of these conversion primitives when
    its output variable is unused thereafter.
    
    [tested with --no-local]
    [test-built and smoke-tested with extern/ferguson and extern/gbt]
    
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@22877 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- compiler/AST/primitive.cpp
+++ compiler/AST/primitive.cpp
@@ -554,8 +554,8 @@ initPrimitive() {
   prim_def("string_index", returnInfoString, true, true);
   prim_def(PRIM_STRING_COPY, "string_copy", returnInfoString, false, true);
   prim_def(PRIM_STRING_NORMALIZE, "string_normalize", returnInfoVoid, true, false);
-  prim_def(PRIM_STRING_FROM_C_STRING, "string_from_c_string", returnInfoString, true, true);
-  prim_def(PRIM_C_STRING_FROM_STRING, "c_string_from_string", returnInfoStringC, true, true);
+  prim_def(PRIM_STRING_FROM_C_STRING, "string_from_c_string", returnInfoString, false, true);
+  prim_def(PRIM_C_STRING_FROM_STRING, "c_string_from_string", returnInfoStringC, false, true);
   prim_def(PRIM_CAST_TO_VOID_STAR, "cast_to_void_star", returnInfoOpaque, true, false);
   prim_def("string_select", returnInfoString, true, true);
   prim_def("sleep", returnInfoVoid, true);
