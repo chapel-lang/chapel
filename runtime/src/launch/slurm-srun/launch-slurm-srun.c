61e232d15949b7e0b4f94e822c9639242a0abbed
Thomas Van Doren <thomas.vandoren@gmail.com>
2014-05-16 03:56:01 +0000

    Promote r23394 to release/1.9 branch.
    Cleanup and fixes to SLURM launcher
    
    
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/branches/release/1.9@23402 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- runtime/src/launch/slurm-srun/launch-slurm-srun.c
+++ runtime/src/launch/slurm-srun/launch-slurm-srun.c
@@ -14,15 +14,21 @@
 #define baseExpectFilename ".chpl-expect-"
 #define baseSysFilename ".chpl-sys-"
 
+#define CHPL_WALLTIME_FLAG "--walltime"
+#define CHPL_GENERATE_SBATCH_SCRIPT "--generate-sbatch-script"
+
+static char* debug = NULL;
+static char* walltime = NULL;
+static int generate_sbatch_script = 0;
+
 char slurmFilename[FILENAME_MAX];
 char expectFilename[FILENAME_MAX];
 char sysFilename[FILENAME_MAX];
 
 /* copies of binary to run per node */
+#define procsPerNode 1
+
 #define versionBuffLen 80
-
-#define launcherAccountEnvvar "CHPL_LAUNCHER_ACCOUNT"
-
 typedef enum {
   slurmpro,
   nccs,
@@ -73,21 +79,40 @@ static sbatchVersion determineSlurmVersion(void) {
 }
 
 
-// Get the number of locales from the environment setting or return 
-// zero if unset. Currently this is unused for slurm 
-static int getNumCoresPerLocale(void) {
-  int numCores = 0;
+// Get the number of locales from the environment variable or if that is not 
+// set just use sinfo to get the number of cpus. 
+static int getCoresPerLocale() {
+  int numCores = -1;
+  const int buflen = 1024;
+  char buf[buflen];
+  char* argv[6];
   char* numCoresString = getenv("CHPL_LAUNCHER_CORES_PER_LOCALE");
 
   if (numCoresString) {
     numCores = atoi(numCoresString);
-    if (numCores != 0)
+    if (numCores > 0)
       return numCores;
+    chpl_warning("CHPL_LAUNCHER_CORES_PER_LOCALE must be > 0.", 0, 0);
   }
+
+  argv[0] = (char *)  "sinfo";       // use sinfo to get num cpus
+  argv[1] = (char *)  "--exact";     // get exact otherwise you get 16+, etc
+  argv[2] = (char *)  "--format=%c"; // format to get num cpu per node (%c)
+  argv[3] = (char *)  "--sort=+=#c"; // sort by num cpu (lower to higher)
+  argv[4] = (char *)  "--noheader";  // don't show header (hide "CPU" header)
+  argv[5] = NULL;
+
+
+  memset(buf, 0, buflen);
+  if (chpl_run_utility1K("sinfo", argv, buf, buflen) <= 0)
+    chpl_error("Error trying to determine number of cores per node", 0, 0);
+
+  if (sscanf(buf, "%d", &numCores) != 1)
+    chpl_error("unable to determine number of cores per locale; "
+               "please set CHPL_LAUNCHER_CORES_PER_LOCALE", 0, 0);
 
   return numCores;
 }
-
 
 // create the command that will actually launch the program and 
 // create any files needed for the launch like the batch script 
@@ -98,12 +123,16 @@ static char* chpl_launch_create_command(int argc, char* argv[],
   char baseCommand[256];
   char* command;
   FILE* slurmFile, *expectFile;
-  char* projectString = getenv(launcherAccountEnvvar);
+  char* account = getenv("CHPL_LAUNCHER_ACCOUNT");
   char* constraint = getenv("CHPL_LAUNCHER_CONSTRAINT");
-  char* walltime = getenv("CHPL_LAUNCHER_WALLTIME");
   char* outputfn = getenv("CHPL_LAUNCHER_SLURM_OUTPUT_FILENAME");
   char* basenamePtr = strrchr(argv[0], '/');
   pid_t mypid;
+
+  // command line walltime takes precedence over env var
+  if (!walltime) {
+    walltime = getenv("CHPL_LAUNCHER_WALLTIME");
+  }
 
   if (basenamePtr == NULL) {
       basenamePtr = argv[0];
@@ -112,74 +141,85 @@ static char* chpl_launch_create_command(int argc, char* argv[],
   }
   chpl_compute_real_binary_name(argv[0]);
 
-#ifndef DEBUG_LAUNCH
-  mypid = getpid();
-#else
-  mypid = 0;
-#endif
+  if (debug) {
+    mypid = 0;
+  } else {
+    mypid = getpid();
+  }
+
   // set the filenames 
   sprintf(expectFilename, "%s%d", baseExpectFilename, (int)mypid);
   sprintf(slurmFilename, "%s%d", baseSBATCHFilename, (int)mypid);
 
   // if were running a batch job 
-  if (getenv("CHPL_LAUNCHER_USE_SBATCH") != NULL) {
+  if (getenv("CHPL_LAUNCHER_USE_SBATCH") != NULL || generate_sbatch_script) {
     // open the batch file and create the header 
     slurmFile = fopen(slurmFilename, "w");
     fprintf(slurmFile, "#!/bin/sh\n\n");
     
     // set the job name 
-    fprintf(slurmFile, "#SBATCH -J Chpl-%.10s\n", basenamePtr);
+    fprintf(slurmFile, "#SBATCH --job-name=Chpl-%.10s\n", basenamePtr);
+ 
+    // suppress informational messages, will still display errors 
+    fprintf(slurmFile, "#SBATCH --quiet\n");
+    
+    // request the number of locales, with 1 task per node, and number of cores
+    // cpus-per-task. We probably don't need --nodes and --ntasks specified
+    // since 1 task-per-node with n --tasks implies -n nodes
+    fprintf(slurmFile, "#SBATCH --nodes=%d\n", numLocales);
+    fprintf(slurmFile, "#SBATCH --ntasks=%d\n", numLocales);
+    fprintf(slurmFile, "#SBATCH --ntasks-per-node=%d\n", procsPerNode);
+    fprintf(slurmFile, "#SBATCH --cpus-per-task=%d\n", getCoresPerLocale());
+    
+    //request exclusive access to nodes 
+    fprintf(slurmFile, "#SBATCH --exclusive\n");
 
     // Set the walltime if it was specified 
     if (walltime) { 
       fprintf(slurmFile, "#SBATCH --time=%s\n", walltime);
     }
-    
-    // request the number of locales and claim 1 task per node (more chapel
-    // tasks can run per node, this is just for slurm) 
-    fprintf(slurmFile, "#SBATCH --nodes=%d\n", numLocales);
-    fprintf(slurmFile, "#SBATCH --ntasks-per-node=1\n");
 
     // If needed a constraint can be specified with the env var CHPL_LAUNCHER_CONSTRAINT
     if (constraint) {
       fprintf(slurmFile, "#SBATCH --constraint=%s\n", constraint);
     }
-    
-    //request exclusive access to nodes 
-    fprintf(slurmFile, "#SBATCH --exclusive\n");
-    
+
     // set the account name if one was provided  
-    if (projectString && strlen(projectString) > 0) {
-      fprintf(slurmFile, "#SBATCH -A %s\n", projectString);
+    if (account && strlen(account) > 0) {
+      fprintf(slurmFile, "#SBATCH --account=%s\n", account);
     }
  
     // set the output name to either the user specified.<jobID>.out 
     // or to the binaryName.<jobID>.out if none specified
     if (outputfn!=NULL) {
-      fprintf(slurmFile, "#SBATCH -o %s.%%j.out\n", outputfn);
+      fprintf(slurmFile, "#SBATCH --output=%s.%%j.out\n", outputfn);
     }
     else {
-      fprintf(slurmFile, "#SBATCH -o %s.%%j.out\n", argv[0]);
+      fprintf(slurmFile, "#SBATCH --output=%s.%%j.out\n", argv[0]);
     }
 
     // add the srun command 
-    fprintf(slurmFile, "srun -n %d %s ", numLocales, chpl_get_real_binary_name());
+    fprintf(slurmFile, "srun %s ", chpl_get_real_binary_name());
     
-    //add any arguments passed to the launcher to the binary 
+    // add any arguments passed to the launcher to the binary 
     for (i=1; i<argc; i++) {
       fprintf(slurmFile, " '%s'", argv[i]);
     }
     fprintf(slurmFile, "\n");
 
-    //close the batch file and change permissions 
+    // close the batch file and change permissions 
     fclose(slurmFile);
-    chmod( slurmFilename, 0755);
+    chmod(slurmFilename, 0755);
+
+    if (generate_sbatch_script) {
+      fprintf(stdout, "SBATCH script written to '%s'\n", slurmFilename);
+    }
 
     // the baseCommand is what will call the batch file
     // that was just created 
     sprintf(baseCommand, "sbatch %s\n", slurmFilename);
   }
-  // if we're running an interactive job 
+  // else we're running an interactive job 
   else {
     // expect is used to launch an interactive job 
     // create the file and set some things for expect 
@@ -187,32 +227,45 @@ static char* chpl_launch_create_command(int argc, char* argv[],
     fprintf(expectFile, "set timeout -1\n");
     fprintf(expectFile, "set prompt \"(%%|#|\\\\$|>) $\"\n");
     
-    // create a silent salloc command and set 
-    fprintf(expectFile, "spawn -noecho salloc ");
+    // create a silent salloc command
+    fprintf(expectFile, "spawn -noecho srun ");
 
-    //set the job name 
-    fprintf(expectFile, "-J %.10s ",basenamePtr);  
+    // set the job name 
+    fprintf(expectFile, "--job-name=CHPL-%.10s ",basenamePtr);  
     
-    // request the number of locales and claim 1 task per node (more chapel
-    // tasks can run per node, this is just for slurm) 
-    fprintf(expectFile, "-N %d ",numLocales); 
-    fprintf(expectFile, "--ntasks-per-node=1 ",numLocales); 
+    // suppress informational messages, will still display errors 
+    fprintf(expectFile, "--quiet ");
+
+    // request the number of locales, with 1 task per node, and number of cores
+    // cpus-per-task. We probably don't need --nodes and --ntasks specified
+    // since 1 task-per-node with n --tasks implies -n nodes
+    fprintf(expectFile, "--nodes=%d ",numLocales); 
+    fprintf(expectFile, "--ntasks=%d ", numLocales); 
+    fprintf(expectFile, "--ntasks-per-node=%d ", procsPerNode); 
+    fprintf(expectFile, "--cpus-per-task=%d ", getCoresPerLocale()); 
     
     // request exclusive access
-    fprintf(expectFile, "--exclusive "); //  give exclusive access to the nodes
+    fprintf(expectFile, "--exclusive ");
     
-    // Set the walltime, this is required for interactive jobs 
-    fprintf(expectFile, "--time=%s ",walltime); 
+    // Set the walltime if i was specified 
+    if (walltime) {
+      fprintf(expectFile, "--time=%s ",walltime); 
+    }
     
     // set any constraints 
     if (constraint) {
-      fprintf(expectFile, " -C %s", constraint);
+      fprintf(expectFile, " --constraint=%s ", constraint);
+    }
+    
+    // set the account name if one was provided  
+    if (account && strlen(account) > 0) {
+      fprintf(expectFile, "--account=%s ", account);
     }
 
     // the actual srun command 
-    fprintf(expectFile, "srun -n %d %s ", numLocales, chpl_get_real_binary_name());
+    fprintf(expectFile, "%s", chpl_get_real_binary_name());
     
-    //add any arguments passed to the launcher to the binary 
+    // add any arguments passed to the launcher to the binary 
     for (i=1; i<argc; i++) {
       fprintf(expectFile, " %s", argv[i]);
     }
@@ -237,48 +290,98 @@ static char* chpl_launch_create_command(int argc, char* argv[],
   return command;
 }
 
+
+static void genSBatchScript(int argc, char *argv[], int numLocales) {
+  chpl_launch_create_command(argc, argv, numLocales);
+}
+
+
 // clean up the batch file or expect file in an interactive job 
 static void chpl_launch_cleanup(void) {
-#ifndef DEBUG_LAUNCH
-  char command[1024];
-
-  if (getenv("CHPL_LAUNCHER_USE_SBATCH") == NULL) {
-    sprintf(command, "rm %s", expectFilename);
-    system(command);
-  } else {
-    sprintf(command, "rm %s", slurmFilename);
-    system(command);
+  // leave file around if we're debugging 
+  if (!debug) {
+    // check if this is interactive or batch
+    char* fileToRemove = NULL;
+    if (getenv("CHPL_LAUNCHER_USE_SBATCH") == NULL) {
+      fileToRemove = expectFilename;
+    } else {
+      fileToRemove = slurmFilename; 
+    }
+    
+    // actually remove file 
+    if (unlink(fileToRemove)) {
+      char msg[1024];
+      snprintf(msg, 1024, "Error removing temporary file '%s': %s",
+               fileToRemove, strerror(errno));
+      chpl_warning(msg, 0, 0);
+    }
   }
-
-#endif
 }
 
 
 int chpl_launch(int argc, char* argv[], int32_t numLocales) {
-
+  int retcode;
+  
+  // check the slurm version before continuing 
   sbatchVersion sVersion = determineSlurmVersion();
   if (sVersion != slurm) {
     printf("Error: This launcher is only compatible with native slurm\n");
     printf("Slurm version was %d\n", sVersion);
     return 1;
   }
+ 
+  debug = getenv("CHPL_LAUNCHER_DEBUG");
+ 
+  // generate a batch script and exit if user wanted to 
+  if (generate_sbatch_script) {
+    genSBatchScript(argc, argv, numLocales);
+    retcode = 0;
+  }
+  // otherwise generate the batch file or expect script and execute it
+  else {
+    retcode = chpl_launch_using_system(chpl_launch_create_command(argc, argv, 
+              numLocales), argv[0]);
+    
+    chpl_launch_cleanup();
+  }
 
-  int retcode =
-    chpl_launch_using_system(chpl_launch_create_command(argc, argv, numLocales),
-                             argv[0]);
-  chpl_launch_cleanup();
   return retcode;
 }
 
-// Currently the only args that are handled are the num locales
-// We could probably also  handle walltime here instead of just 
-// as an environment variable and we will probably need to handle 
-// flags for numa-ness 
+
+// handle launcher args 
 int chpl_launch_handle_arg(int argc, char* argv[], int argNum,
                            int32_t lineno, chpl_string filename) {
+  // handle --walltime <walltime> or --walltime=<walltime>
+  if (!strcmp(argv[argNum], CHPL_WALLTIME_FLAG)) {
+    walltime = argv[argNum+1];
+    return 2;
+  } else if (!strncmp(argv[argNum], CHPL_WALLTIME_FLAG"=", strlen(CHPL_WALLTIME_FLAG))) {
+    walltime = &(argv[argNum][strlen(CHPL_WALLTIME_FLAG)+1]);
+    return 1;
+  }
+
+  // handle --generate-sbatch-script 
+  if (!strcmp(argv[argNum], CHPL_GENERATE_SBATCH_SCRIPT)) {
+    generate_sbatch_script = 1;
+    return 1;
+  }
+
+  // TODO we should have a core binding option here similar to aprun's -cc to
+  // handle binding to cores / numa domains, etc 
+  // For now you can just set the SLURM_CPU_BIND env var 
+
   return 0;
 }
 
 
+
 void chpl_launch_print_help(void) {
+  fprintf(stdout, "LAUNCHER FLAGS:\n");
+  fprintf(stdout, "===============\n");
+  fprintf(stdout, "  %s : generate an sbatch script and exit\n", CHPL_GENERATE_SBATCH_SCRIPT);
+  fprintf(stdout, "  %s <HH:MM:SS>  : specify a wallclock time limit\n", CHPL_WALLTIME_FLAG);
+  fprintf(stdout, "                           (or use $CHPL_LAUNCHER_WALLTIME)\n");
+
+
 }
