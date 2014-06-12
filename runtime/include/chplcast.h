chapel-lang/chapel

commit de8bd0726c1009ecdeffeb8ebb0ae7f3f746575f

Make an explicit distinction between string and c_string
[reviewed by David, Michael, and others (test changes)]

This patch makes a (more) clear distinction between Chapel strings and
the c_string type in preparation for changing the string
implementation to use records.  Specifically:

- All runtime interfaces use c_string, not Chapel string
- Eliminate Chapel string literals, replacing them with literals of
  type c_string
- Eliminate param strings (use c_string params if needed)

For the time being, we do not clean up c_strings after casts to string
used in IO as well as some of the string operations such as
concatenation, substring, etc.  These will show up as "string copy
data" in the memory leaks log.  I envision most of these cases being
taken care of when strings become records, but I have marked them with
comments for future reference.

To make sure using string literals is seamless for the end user, a
number of changes were made to the compiler in order to hide the fact
that literals are of type c_string.  Specifically,

- Variables/fields/formals/tuples declared without a type but with a
  string literal initializing expression will be of type string
- Domain and array literals that use string literals will have
  index/element types of string
- Generics instantiated with string literals will have string type for
  the corresponding arguments


Additional notes:

- This commit will increase the memory leaks due to "string copy data"
  for a couple of reasons.  First, we when we coerce c_string to
  string, we need to allocate space for that data.  Second, various
  operations on c_strings allocate space (e.g., + and toString()) and
  there is currently no way to reclaim that space.  These are for the
  most part in the IO code.  Looking forward to a better string
  implementation, most cases will be handled with record semantics and
  a string creation function that take an already allocated c_string,

runtime/include/chplcast.h
@@ -7,76 +7,89 @@
 
 /* invalid is a flag indicating valid or not
    invalidChar is the character that was invalid */
-int8_t chpl_string_to_int8_t_precise(const char* str, int* invalid, char* invalidChar);
-int16_t chpl_string_to_int16_t_precise(const char* str, int* invalid, char* invalidChar);
-int32_t chpl_string_to_int32_t_precise(const char* str, int* invalid, char* invalidChar);
-int64_t chpl_string_to_int64_t_precise(const char* str, int* invalid, char* invalidChar);
-uint8_t chpl_string_to_uint8_t_precise(const char* str, int* invalid, char* invalidChar);
-uint16_t chpl_string_to_uint16_t_precise(const char* str, int* invalid, char* invalidChar);
-uint32_t chpl_string_to_uint32_t_precise(const char* str, int* invalid, char* invalidChar);
-uint64_t chpl_string_to_uint64_t_precise(const char* str, int* invalid, char* invalidChar);
+int8_t c_string_to_int8_t_precise(c_string str, int* invalid, char* invalidChar);
+int16_t c_string_to_int16_t_precise(c_string str, int* invalid, char* invalidChar);
+int32_t c_string_to_int32_t_precise(c_string str, int* invalid, char* invalidChar);
+int64_t c_string_to_int64_t_precise(c_string str, int* invalid, char* invalidChar);
+uint8_t c_string_to_uint8_t_precise(c_string str, int* invalid, char* invalidChar);
+uint16_t c_string_to_uint16_t_precise(c_string str, int* invalid, char* invalidChar);
+uint32_t c_string_to_uint32_t_precise(c_string str, int* invalid, char* invalidChar);
+uint64_t c_string_to_uint64_t_precise(c_string str, int* invalid, char* invalidChar);
 
 /* string to every other primitive type */
-int8_t chpl_string_to_int8_t(const char* str, int lineno, const char* filename);
-int16_t chpl_string_to_int16_t(const char* str, int lineno, const char* filename);
-int32_t chpl_string_to_int32_t(const char* str, int lineno, const char* filename);
-int64_t chpl_string_to_int64_t(const char* str, int lineno, const char* filename);
-uint8_t chpl_string_to_uint8_t(const char* str, int lineno, const char* filename);
-uint16_t chpl_string_to_uint16_t(const char* str, int lineno, const char* filename);
-uint32_t chpl_string_to_uint32_t(const char* str, int lineno, const char* filename);
-uint64_t chpl_string_to_uint64_t(const char* str, int lineno, const char* filename);
+int8_t c_string_to_int8_t(c_string str, int lineno, c_string filename);
+int16_t c_string_to_int16_t(c_string str, int lineno, c_string filename);
+int32_t c_string_to_int32_t(c_string str, int lineno, c_string filename);
+int64_t c_string_to_int64_t(c_string str, int lineno, c_string filename);
+uint8_t c_string_to_uint8_t(c_string str, int lineno, c_string filename);
+uint16_t c_string_to_uint16_t(c_string str, int lineno, c_string filename);
+uint32_t c_string_to_uint32_t(c_string str, int lineno, c_string filename);
+uint64_t c_string_to_uint64_t(c_string str, int lineno, c_string filename);
 
-chpl_bool chpl_string_to_chpl_bool(const char* str, int lineno, const char* filename);
+chpl_bool c_string_to_chpl_bool(c_string str, int lineno, c_string filename);
 
-_real32 chpl_string_to_real32_precise(const char* str, int* invalid, char* invalidCh);
-_real64 chpl_string_to_real64_precise(const char* str, int* invalid, char* invalidCh);
-_imag32 chpl_string_to_imag32_precise(const char* str, int* invalid, char* invalidCh);
-_imag64 chpl_string_to_imag64_precise(const char* str, int* invalid, char* invalidCh);
-_complex64 chpl_string_to_complex64_precise(const char* str, int* invalid, char* invalidCh);
-_complex128 chpl_string_to_complex128_precise(const char* str, int* invalid, char* invalidCh);
+_real32 c_string_to_real32_precise(c_string str, int* invalid, char* invalidCh);
+_real64 c_string_to_real64_precise(c_string str, int* invalid, char* invalidCh);
+_imag32 c_string_to_imag32_precise(c_string str, int* invalid, char* invalidCh);
+_imag64 c_string_to_imag64_precise(c_string str, int* invalid, char* invalidCh);
+_complex64 c_string_to_complex64_precise(c_string str, int* invalid, char* invalidCh);
+_complex128 c_string_to_complex128_precise(c_string str, int* invalid, char* invalidCh);
 
-_real32 chpl_string_to_real32(const char* str, int lineno, const char* filename);
-_real64 chpl_string_to_real64(const char* str, int lineno, const char* filename);
-_imag32 chpl_string_to_imag32(const char* str, int lineno, const char* filename);
-_imag64 chpl_string_to_imag64(const char* str, int lineno, const char* filename);
-_complex64 chpl_string_to_complex64(const char* str, int lineno, const char* filename);
-_complex128 chpl_string_to_complex128(const char* str, int lineno, const char* filename);
+_real32 c_string_to_real32(c_string str, int lineno, c_string filename);
+_real64 c_string_to_real64(c_string str, int lineno, c_string filename);
+_imag32 c_string_to_imag32(c_string str, int lineno, c_string filename);
+_imag64 c_string_to_imag64(c_string str, int lineno, c_string filename);
+_complex64 c_string_to_complex64(c_string str, int lineno, c_string filename);
+_complex128 c_string_to_complex128(c_string str, int lineno, c_string filename);
 
 
 /* every other primitive type to string */
-chpl_string int8_t_to_chpl_string(int8_t x);
-chpl_string int16_t_to_chpl_string(int16_t x);
-chpl_string int32_t_to_chpl_string(int32_t x);
-chpl_string int64_t_to_chpl_string(int64_t x);
-chpl_string uint8_t_to_chpl_string(uint8_t x);
-chpl_string uint16_t_to_chpl_string(uint16_t x);
-chpl_string uint32_t_to_chpl_string(uint32_t x);
-chpl_string uint64_t_to_chpl_string(uint64_t x);
-chpl_string _real32_to_chpl_string(_real32 x);
-chpl_string _real64_to_chpl_string(_real64 x);
-chpl_string _imag32_to_chpl_string(_imag32 x);
-chpl_string _imag64_to_chpl_string(_imag64 x);
+c_string int8_t_to_c_string(int8_t x);
+c_string int16_t_to_c_string(int16_t x);
+c_string int32_t_to_c_string(int32_t x);
+c_string int64_t_to_c_string(int64_t x);
+c_string uint8_t_to_c_string(uint8_t x);
+c_string uint16_t_to_c_string(uint16_t x);
+c_string uint32_t_to_c_string(uint32_t x);
+c_string uint64_t_to_c_string(uint64_t x);
+c_string _real32_to_c_string(_real32 x);
+c_string _real64_to_c_string(_real64 x);
+c_string _imag32_to_c_string(_imag32 x);
+c_string _imag64_to_c_string(_imag64 x);
 
 static ___always_inline
-chpl_string chpl_bool_to_chpl_string(chpl_bool x) {
+c_string chpl_bool_to_c_string(chpl_bool x) {
   return x ? "true" : "false";
 }
 static ___always_inline
-chpl_string chpl_bool8_to_chpl_string(chpl_bool8 x) {
+c_string chpl_bool8_to_c_string(chpl_bool8 x) {
   return x ? "true" : "false";
 }
 static ___always_inline
-chpl_string chpl_bool16_to_chpl_string(chpl_bool16 x) {
+c_string chpl_bool16_to_c_string(chpl_bool16 x) {
   return x ? "true" : "false";
 }
 static ___always_inline
-chpl_string chpl_bool32_to_chpl_string(chpl_bool32 x) {
+c_string chpl_bool32_to_c_string(chpl_bool32 x) {
   return x ? "true" : "false";
 }
 static ___always_inline
-chpl_string chpl_bool64_to_chpl_string(chpl_bool64 x) {
+c_string chpl_bool64_to_c_string(chpl_bool64 x) {
   return x ? "true" : "false";
 }
 
+static __always_inline
+c_string chpl_string_to_c_string(chpl_string s, int lineno, c_string filename) {
+  c_string ret;
+  c_string_from_string(&ret, &s, -1, "");
+  return ret; // leaky?
+}
+
+static __always_inline
+chpl_string c_string_to_chpl_string(c_string s, int lineno, c_string filename) {
+  chpl_string ret;
+  string_from_c_string(&ret, s, 0, -1, -1, "");
+  return ret; // leaky?
+}
 
 #endif

commit e6a329887a4097442eda65d4c24c7b888fd1c833

Fix type-o with ___always_inline that broke clang build in r23558
[discussed via chat]

I accidentally left off a _, which works for gcc, but not for clang.
This started a discussion that maybe we should rename this #define to
something more clear like chpl_always_inline.



git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@23560 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

runtime/include/chplcast.h
@@ -78,14 +78,14 @@ c_string chpl_bool64_to_c_string(chpl_bool64 x) {
   return x ? "true" : "false";
 }
 
-static __always_inline
+static ___always_inline
 c_string chpl_string_to_c_string(chpl_string s, int lineno, c_string filename) {
   c_string ret;
   c_string_from_string(&ret, &s, -1, "");
   return ret; // leaky?
 }
 
-static __always_inline
+static ___always_inline
 chpl_string c_string_to_chpl_string(c_string s, int lineno, c_string filename) {
   chpl_string ret;
   string_from_c_string(&ret, s, 0, -1, -1, "");