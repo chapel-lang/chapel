04ee336652dadafcba1ab2bbb903d892687c219a
Thomas Van Doren <thomas.vandoren@gmail.com>
2014-07-02 17:30:41 +0000

    Add new script for running tests on a single compiler configuration on a Cray whitebox.
    This will eventually replace nightly.chpbld01, but first it needs to be
    integrated into the current testing environment.
    
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@23701 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

---
+++ util/cron/test-whitebox-examples.bash
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+#
+# Test a particular compiler with a specific environment on a white against the
+# examples.
+
+CWD=$(cd $(dirname $0) ; pwd)
+source $CWD/functions.bash
+
+# FIXME: Do we need to source /etc/profile? (thomasvandoren, 2014-07-01)
+
+# Ensure module commands are available.
+local_bashrc=/etc/bash.bashrc.local
+if [ -z "$(type module 2> /dev/null)" -a -f $local_bashrc ] ; then
+    log_info "module command not available. Attempting to source ${local_bashrc}"
+    source $local_bashrc
+    if [ -z "$(type module 2> /dev/null)" ] ; then
+        log_error "Could not access module command after sourceing local bashrc (${local_bashrc}). Exiting."
+        exit 1
+    fi
+elif [ -z "$(type module 2> /dev/null)" ] ; then
+    log_error "module command not available and local bashrc (${local_bashrc}) does not exist. Exiting."
+    exit 2
+fi
+
+# Setup vars that will help load the correct compiler module.
+case $COMP_TYPE in
+    TARGET)
+        module_prefix=PrgEnv-
+        set_chpl_host=false
+        chpl_host_prefix=""
+        ;;
+    HOST-TARGET)
+        module_prefix=PrgEnv-
+        set_chpl_host=true
+        chpl_host_prefix=cray-prgenv-
+        ;;
+    HOST-TARGET-no-PrgEnv)
+        module_prefix=""
+        set_chpl_host=true
+        chpl_host_prefix=""
+        ;;
+    *)
+        log_error "Unknown COMP_TYPE value: ${COMP_TYPE}. Exiting."
+        exit 3
+        ;;
+esac
+
+# Load compiler module.
+case $COMPILER in
+    cray)
+        log_info "Loading module: ${module_prefix}${COMPILER}"
+        module load ${module_prefix}${COMPILER}
+
+        # swap out network modules to get "host-only" environment
+        log_info "Swap network module for host-only environment."
+        module swap craype-network-aries craype-target-local_host
+
+        # TODO: Is this still needed? (thomasvandoren, 2014-07-02)
+        log_info "Unloading cray-libsci module."
+        module unload cray-libsci
+        ;;
+    pgi|intel|gnu)
+        log_info "Loading module: ${module_prefix}${COMPILER}"
+        module load ${module_prefix}${COMPILER}
+        ;;
+    *)
+        log_error "Unknown COMPILER value: ${COMPILER}. Exiting."
+        exit 4
+        ;;
+esac
+
+export CHPL_HOME=$(cd $CWD/../.. ; pwd)
+
+# FIXME: Generalize this for XE and XC. (thomasvandoren, 2014-07-01)
+export CHPL_HOST_PLATFORM=cray-xc
+export CHPL_TARGET_PLATFORM=cray-xc
+
+# Set CHPL_HOST_COMPILER.
+if [ "${set_chpl_host}" = "true" ] ; then
+    export CHPL_HOST_COMPILER="${chpl_host_prefix}${COMPILER}"
+    log_info "Set CHPL_HOST_COMPILER to: ${CHPL_HOST_COMPILER}"
+fi
+
+# Disable launchers, comm.
+export CHPL_LAUNCHER=none
+export CHPL_COMM=none
+
+# Set some vars that nightly cares about.
+export CHPL_NIGHTLY_LOGDIR=/data/sea/chapel/Nightly
+export CHPL_NIGHTLY_STATDIR=/data/sea/chapel/Nightly/Stats
+export CHPL_NIGHTLY_CRON_LOGDIR="$CHPL_NIGHTLY_LOGDIR"
+
+log_info "Current loaded modules:"
+module list
+
+log_info "Chapel environment:"
+$CHPL_HOME/util/printchplenv
+
+# Run the tests!
+nightly_args="-cron -examples"
+log_info "Calling nightly with args: ${nightly_args}"
+$CWD/nightly ${nightly_args}
+log_info "Finished running nightly."
+
+case $COMPILER in
+    cray)
+        # restore network module...
+        module swap craype-target-local_host craype-network-aries
+        ;;
+    *)
+        : # nothing to see here
+        ;;
+esac
