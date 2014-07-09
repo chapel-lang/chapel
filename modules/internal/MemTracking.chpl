chapel-lang/chapel

commit a330e9710583932f64b4765ba9fc04d164a17b07

Repair runtime initiation of memory tracking.
[Reviewed by Sung.]

In r22223 I moved the initiation of memory tracking from the module code
to the runtime.  However, that broke a few tests, irretrievably in one
case: distributions/robust/arithmetic/reindexing/simple-alias-mem-use.
That is apparently the only test where we turn on memory tracking by
giving -smemTrack at compile time, and the emitted code for the
MemTracking module defaults its memTrack config const to true in the
absence of any explicit setting at execution time.  This wasn't handled
properly by the new code, because it was only looking at the config vars
known to the runtime.  In the simple-alias-mem-use case the memTrack
config var is not specified (either true or false) at execution time, so
the runtime doesn't have a set value for it.  As a result we didn't turn
on memory tracking, and the output for the test was incorrect.

Here we fix this problem, by once again making the runtime rely on the
values of the module's copies of the memtracking config consts, rather
than on the runtime's limited notion of the config const values.

Note: We should move the CHPL_TASK_STD_MODULES_INITIALIZED() call from
      where it currently is in chpl_main() to the pre-user-code hook.  I
      didn't mention this when I added that hook, but it does seem like
      the natural place for that call to occur.  (In fact, I can imagine
      a generalized pre-user-code callback capability, for the various
      parts of the runtime to register functions to be called right
      before the user code starts.)

The specific file changes are as follows.

modules/internal/MemTracking.chpl
  Add chpl_memTracking_returnConfigVals(), an exported proc the runtime
  can call when it wants the values of the memtracking config consts.
  The values are returned via ref arguments.  As an associated change,
  make memMax and memThreshold int(64), so that we can explicitly match
  the types for these in the caller and callee.

modules/internal/MemTracking.chpl
@@ -8,11 +8,36 @@ module MemTracking
     memStats: bool = false, 
     memLeaks: bool = false,
     memLeaksTable: bool = false,
-    memMax: int = 0,
-    memThreshold: int = 0,
+    memMax: int(64) = 0,
+    memThreshold: int(64) = 0,
     memLog: string = "";
   
   pragma "no auto destroy"
   config const
     memLeaksLog: string = "";
+
+  //
+  // This communicates the settings of the various memory tracking
+  // config consts to the runtime code that actually implements the
+  // memory tracking.
+  //
+  pragma "local args"
+  export
+  proc chpl_memTracking_returnConfigVals(ref ret_memTrack: bool,
+                                         ref ret_memStats: bool,
+                                         ref ret_memLeaks: bool,
+                                         ref ret_memLeaksTable: bool,
+                                         ref ret_memMax: int(64),
+                                         ref ret_memThreshold: int(64),
+                                         ref ret_memLog: c_string,
+                                         ref ret_memLeaksLog: c_string) {
+    ret_memTrack = memTrack;
+    ret_memStats = memStats;
+    ret_memLeaks = memLeaks;
+    ret_memLeaksTable = memLeaksTable;
+    ret_memMax = memMax;
+    ret_memThreshold = memThreshold;
+    ret_memLog = __primitive("c_string_from_string", memLog);
+    ret_memLeaksLog = __primitive("c_string_from_string", memLeaksLog);
+  }
 }

runtime/include/chplmemtrack.h
@@ -13,11 +13,11 @@
 // Memory tracking activated?
 extern chpl_bool chpl_memTrack;
 
+void chpl_setMemFlags(void);
 uint64_t chpl_memoryUsed(int32_t lineno, chpl_string filename);
 void chpl_printMemStat(int32_t lineno, chpl_string filename);
 void chpl_printMemTable(int64_t threshold, int32_t lineno, chpl_string filename);
 void chpl_reportMemInfo(void);
-void chpl_setMemFlags(void);
 void chpl_track_malloc(void* memAlloc, size_t number, size_t size,
                        chpl_mem_descInt_t description,
                        int32_t lineno, chpl_string filename);

runtime/src/chplmemtrack.c
@@ -4,6 +4,7 @@
 #include "chpl-mem.h"
 #include "chpl-mem-desc.h"
 #include "chpl-tasks.h"
+#include "chpltypes.h"
 #include "chpl-comm.h"
 #include "chplcgfns.h"
 #include "config.h"
@@ -11,10 +12,23 @@
 
 #include <assert.h>
 #include <string.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
 
+
+//
+// This is in the modules, in MemTracking.chpl.
+//
+extern void chpl_memTracking_returnConfigVals(chpl_bool*,
+                                              chpl_bool*,
+                                              chpl_bool*,
+                                              chpl_bool*,
+                                              int64_t*,
+                                              int64_t*,
+                                              c_string*,
+                                              c_string*);
 
 chpl_bool chpl_memTrack = false;
 
@@ -28,7 +42,7 @@ typedef struct memTableEntry_struct { /* table entry */
   chpl_mem_descInt_t description;
   void* memAlloc;
   int32_t lineno;
-  chpl_string filename;
+  c_string filename;
   struct memTableEntry_struct* nextInBucket;
 } memTableEntry;
 
@@ -46,11 +60,11 @@ static memTableEntry** memTable = NULL;
 static _Bool memLeaks = false;
 static _Bool memLeaksTable = false;
 static _Bool memStats = false;
-static uint64_t memMax = 0;
-static uint64_t memThreshold = 0;
-static chpl_string memLog = NULL;
+static size_t memMax = 0;
+static size_t memThreshold = 0;
+static c_string memLog = NULL;
 static FILE* memLogFile = NULL;
-static chpl_string memLeaksLog = NULL;
+static c_string memLeaksLog = NULL;
 
 static size_t totalMem = 0;       /* total memory currently allocated */
 static size_t maxMem = 0;         /* maximum total memory during run  */
@@ -59,6 +73,65 @@ static size_t totalFreed = 0;     /* total memory freed */
 static size_t totalEntries = 0;     /* number of entries in hash table */
 
 static chpl_sync_aux_t memTrack_sync;
+
+
+void chpl_setMemFlags(void) {
+  chpl_bool local_memTrack;
+
+  //
+  // Get the values of the memTracking config consts from the module.
+  // The runtime also has the execution-time settings for these same
+  // config consts, but if their default values are changed at compile
+  // time only the emitted code for the module reflects that; the
+  // runtime doesn't know about it.  So, we need to get the values
+  // from the module.
+  //
+  // We use local_memTrack here so that the module code doesn't store
+  // directly to our flag and effectively turn on memory tracking too
+  // early.  In the first version of this code I passed &chpl_memTrack
+  // itself, and for comm=gasnet we ended up tracking an extra 2 bytes
+  // of space the comm layer allocated as a result of our own call to
+  // chpl_memTracking_returnConfigVals().
+  //
+  chpl_memTracking_returnConfigVals(&local_memTrack,
+                                    &memStats,
+                                    &memLeaks,
+                                    &memLeaksTable,
+                                    &memMax,
+                                    &memThreshold,
+                                    &memLog,
+                                    &memLeaksLog);
+
+  if (local_memTrack
+      || memStats
+      || memLeaks
+      || memLeaksTable
+      || memMax > 0
+      || strcmp(memLeaksLog, "") != 0) {
+    chpl_memTrack = true;
+  }
+
+  if (strcmp(memLog, "") == 0) {
+    memLogFile = stdout;
+  } else {
+    if (chpl_numNodes == 1) {
+      memLogFile = fopen(memLog, "w");
+    } else {
+      char* filename = (char*)malloc((strlen(memLog)+10)*sizeof(char));
+      sprintf(filename, "%s.%" FORMAT_c_nodeid_t, memLog, chpl_nodeID);
+      memLogFile = fopen(filename, "w");
+      free(filename);
+    }
+  }
+
+  if (chpl_memTrack) {
+    chpl_sync_initAux(&memTrack_sync);
+    hashSizeIndex = 0;
+    hashSize = hashSizes[hashSizeIndex];
+    memTable = calloc(hashSize, sizeof(memTableEntry*));
+  }
+}
+
 
 static unsigned hash(void* memAlloc, int hashSize) {
   unsigned hashValue = 0;
@@ -136,7 +209,7 @@ static void addMemTableEntry(void* memAlloc, size_t number, size_t size, chpl_me
   memEntry->description = description;
   memEntry->memAlloc = memAlloc;
   memEntry->lineno = lineno;
-  memEntry->filename = filename;
+  c_string_from_string(&memEntry->filename, &filename, 0, NULL);
   memEntry->number = number;
   memEntry->size = size;
   increaseMemStat(number*size, lineno, filename);
@@ -175,71 +248,6 @@ static memTableEntry* removeMemTableEntry(void* address) {
       resizeTable(-1);
   }
   return deletedBucket;
-}
-
-
-void chpl_setMemFlags(void) {
-  const char* cv;
-
-  if ((cv = chpl_config_get_value("memTrack", "Built-in")) != NULL
-      && strcmp(cv, "true") == 0) {
-    chpl_memTrack = true;
-  }
-
-  if ((cv = chpl_config_get_value("memStats", "Built-in")) != NULL
-      && strcmp(cv, "true") == 0) {
-    memStats = true;
-    chpl_memTrack = true;
-  }
-
-  if ((cv = chpl_config_get_value("memLeaks", "Built-in")) != NULL
-      && strcmp(cv, "true") == 0) {
-    memLeaks = true;
-    chpl_memTrack = true;
-  }
-
-  if ((cv = chpl_config_get_value("memLeaksTable", "Built-in")) != NULL
-      && strcmp(cv, "true") == 0) {
-    memLeaksTable = true;
-    chpl_memTrack = true;
-  }
-
-  if ((cv = chpl_config_get_value("memMax", "Built-in")) != NULL) {
-    assert(sscanf(cv, "%" SCNu64, &memMax) == 1);
-    if (memMax > 0)
-      chpl_memTrack = true;
-  }
-
-  if ((cv = chpl_config_get_value("memThreshold", "Built-in")) != NULL) {
-    assert(sscanf(cv, "%" SCNu64, &memThreshold) == 1);
-  }
-
-  if ((memLog = chpl_config_get_value("memLog", "Built-in")) == NULL)
-    memLog = "";
-  if (strcmp(memLog, "") == 0) {
-    memLogFile = stdout;
-  } else {
-    if (chpl_numNodes == 1) {
-      memLogFile = fopen(memLog, "w");
-    } else {
-      char* filename = (char*)malloc((strlen(memLog)+10)*sizeof(char));
-      sprintf(filename, "%s.%" FORMAT_c_nodeid_t, memLog, chpl_nodeID);
-      memLogFile = fopen(filename, "w");
-      free(filename);
-    }
-  }
-
-  if ((memLeaksLog = chpl_config_get_value("memLeaksLog", "Built-in")) == NULL)
-    memLeaksLog = "";
-  if (strcmp(memLeaksLog, "") != 0)
-    chpl_memTrack = true;
-
-  if (chpl_memTrack) {
-    chpl_sync_initAux(&memTrack_sync);
-    hashSizeIndex = 0;
-    hashSize = hashSizes[hashSizeIndex];
-    memTable = calloc(hashSize, sizeof(memTableEntry*));
-  }
 }
 
 
@@ -463,32 +471,51 @@ void chpl_printMemTable(int64_t threshold, int32_t lineno, chpl_string filename)
 void chpl_track_malloc(void* memAlloc, size_t number, size_t size,
                        chpl_mem_descInt_t description,
                        int32_t lineno, chpl_string filename) {
+  c_string c_filename;
+
   if (number * size > memThreshold) {
     if (chpl_memTrack) {
       chpl_sync_lock(&memTrack_sync);
       addMemTableEntry(memAlloc, number, size, description, lineno, filename);
       chpl_sync_unlock(&memTrack_sync);
     }
-    if (chpl_verbose_mem)
-      fprintf(memLogFile, "%" FORMAT_c_nodeid_t ": %s:%"PRId32": allocate %zuB of %s at %p\n", chpl_nodeID, (filename ? filename : "--"), lineno, number*size, chpl_mem_descString(description), memAlloc);
+    if (chpl_verbose_mem) {
+      c_string_from_string(&c_filename, &filename, 0, NULL);
+      fprintf(memLogFile,
+              "%" FORMAT_c_nodeid_t ": %s:%" PRId32
+              ": allocate %zuB of %s at %p\n",
+              chpl_nodeID, (c_filename ? c_filename : "--"), lineno,
+              number*size, chpl_mem_descString(description), memAlloc);
+    }
   }
 }
 
 
 void chpl_track_free(void* memAlloc, int32_t lineno, chpl_string filename) {
   memTableEntry* memEntry = NULL;
+  c_string c_filename;
 
   if (chpl_memTrack) {
     chpl_sync_lock(&memTrack_sync);
     memEntry = removeMemTableEntry(memAlloc);
     if (memEntry) {
-      if (chpl_verbose_mem)
-        fprintf(memLogFile, "%" FORMAT_c_nodeid_t ": %s:%"PRId32": free %zuB of %s at %p\n", chpl_nodeID, (filename ? filename : "--"), lineno, memEntry->number*memEntry->size, chpl_mem_descString(memEntry->description), memAlloc);
+      if (chpl_verbose_mem) {
+        c_string_from_string(&c_filename, &filename, 0, NULL);
+        fprintf(memLogFile,
+                "%" FORMAT_c_nodeid_t ": %s:%" PRId32
+                ": free %zuB of %s at %p\n",
+                chpl_nodeID, (c_filename ? c_filename : "--"), lineno,
+                memEntry->number*memEntry->size,
+                chpl_mem_descString(memEntry->description), memAlloc);
+      }
       free(memEntry);
     }
     chpl_sync_unlock(&memTrack_sync);
   } else if (chpl_verbose_mem && !memEntry) {
-    fprintf(memLogFile, "%" FORMAT_c_nodeid_t ": %s:%"PRId32": free at %p\n", chpl_nodeID, (filename ? filename : "--"), lineno, memAlloc);
+    c_string_from_string(&c_filename, &filename, 0, NULL);
+    fprintf(memLogFile,
+            "%" FORMAT_c_nodeid_t ": %s:%" PRId32 ": free at %p\n",
+            chpl_nodeID, (c_filename ? c_filename : "--"), lineno, memAlloc);
   }
 }
 
@@ -514,14 +541,22 @@ void chpl_track_realloc_post(void* moreMemAlloc,
                          void* memAlloc, size_t size,
                          chpl_mem_descInt_t description,
                          int32_t lineno, chpl_string filename) {
+  c_string c_filename;
+
   if (size > memThreshold) {
     if (chpl_memTrack) {
       chpl_sync_lock(&memTrack_sync);
       addMemTableEntry(moreMemAlloc, 1, size, description, lineno, filename);
       chpl_sync_unlock(&memTrack_sync);
     }
-    if (chpl_verbose_mem)
-      fprintf(memLogFile, "%" FORMAT_c_nodeid_t ": %s:%"PRId32": reallocate %zuB of %s at %p -> %p\n", chpl_nodeID, (filename ? filename : "--"), lineno, size, chpl_mem_descString(description), memAlloc, moreMemAlloc);
+    if (chpl_verbose_mem) {
+      c_string_from_string(&c_filename, &filename, 0, NULL);
+      fprintf(memLogFile,
+              "%" FORMAT_c_nodeid_t ": %s:%" PRId32
+              ": reallocate %zuB of %s at %p -> %p\n",
+              chpl_nodeID, (c_filename ? c_filename : "--"), lineno,
+              size, chpl_mem_descString(description), memAlloc, moreMemAlloc);
+    }
   }
 }
 

runtime/src/main.c
@@ -106,12 +106,14 @@ static void chpl_main(void) {
 // paths for locale 0 and non-0 locales diverge.
 //
 void chpl_rt_preUserCodeHook(void) {
+  chpl_comm_barrier("pre-user-code hook begin");
+
   //
   // Set up any memory tracking requested.
   //
   chpl_setMemFlags();
 
-  chpl_comm_barrier("pre-user-code hook");
+  chpl_comm_barrier("pre-user-code hook end");
 }
 
 

test/memory/figueroa/LeakedMemory6.skipif
@@ -1,0 +1,5 @@
+# Don't test this for CHPL_COMM!=none, because in that case we run afoul of
+# the problem described by functions/sungeun/promotionWithNoInline.future due
+# throwing --no-inline when we need coerce and promotion wrappers in the code
+# for modules/internal/MemTracking.chpl:chpl_memTracking_returnConfigVals().
+CHPL_COMM != none

test/memory/shannon/printFinalMemStat.no-local.good
@@ -4,6 +4,6 @@ Memory Statistics
 ==============================================================
 Current Allocated Memory               256
 Maximum Simultaneous Allocated Memory  473
-Total Allocated Memory                 1793
-Total Freed Memory                     1537
+Total Allocated Memory                 1673
+Total Freed Memory                     1417
 ==============================================================

commit 80206e86108eaefb04e96b7cdc6a3c2f58a63be9

"Fix" multilocale memory tracking
[reviewed by Greg and David]

This commit "fixes" (more on the quotes below) multilocale memory
tracking, which has actually been broken since r22257.  The issue is
that we were trying to access strings (logfile names) from a remote
locale from within the runtime during memory tracking initialization
in chpl_memTracking_returnConfigVals().  This bug was mostly masked
because we don't test multilocale memory tracking with explicit
logfiles, but it broke the gasnet configuration on OSX with the big
c_string commit (r23558).

This commit actually abuses a current unexpected behavior in the
compiler where under certain circumstances, strings are copied to
remote locales.  In this specific case, when the locale.id is
accessed, the string is copied from locale 0 to the remote locale, and
can be assigned to a local temporary string which can then be passed
to the runtime.  When the string implementation is revamped using
records, we will simply change the locale.id access to one that checks
here.id.

FYI, Greg will be committing a test that locks in the correct behavior
for multilocale memory tracking.



git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@23777 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

modules/internal/MemTracking.chpl
@@ -11,10 +11,12 @@ module MemTracking
     memMax: size_t = 0,
     memThreshold: size_t = 0,
     memLog: c_string = "";
-  
+  const s_memLog = toString(memLog);
+
   pragma "no auto destroy"
   config const
     memLeaksLog: c_string = "";
+  const s_memLeaksLog = toString(memLeaksLog);
 
   //
   // This communicates the settings of the various memory tracking
@@ -36,7 +38,28 @@ module MemTracking
     ret_memLeaksTable = memLeaksTable;
     ret_memMax = memMax;
     ret_memThreshold = memThreshold;
-    ret_memLog = memLog;
-    ret_memLeaksLog = memLeaksLog;
+    // The following is an abuse of a hack used to copy strings to
+    // remote locales.  When strings are implemented as records,
+    // XXX.locale.id should be replaced by 'here.id' as noted below.
+    //
+    // HACK: For some reason, when accessing a member field of a
+    // string (.locale is the only such field), the string is copied
+    // from the remote locale into a temporary.  Here we abuse that by
+    // accessing the locale id.  The subsequent assignment to 'ts' is
+    // from the compiler inserted temporary.
+    if s_memLog.locale.id /* here.id */ != 0 {
+      // Remote strings must be copied before accessing c_str()
+      const ts = s_memLog;
+      ret_memLog = ts.c_str();
+    } else {
+      ret_memLog = memLog;
+    }
+    if s_memLeaksLog.locale.id /* here.id */ != 0 {
+      // Remote strings must be copied before accessing c_str()
+      const ts = s_memLeaksLog;
+      ret_memLeaksLog = ts.c_str();
+    } else {
+      ret_memLeaksLog = memLeaksLog;
+    }
   }
 }