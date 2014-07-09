4c38033da4421f91545a3d18384c43c21d9133e5
Lydia Duncan <lydia.duncan16@gmail.com>
2014-07-09 22:12:21 +0000

    This test did not follow our specification for extern classes by failing to declare a struct type that was typedef'd to have the name "_data_set".  This was caught by my default initialization alterations. [approved by bradc]
    git-svn-id: http://svn.code.sf.net/p/chapel/code/trunk@23774 3a8e244f-b0f2-452b-bcba-4c88e055c3ca

--- test/users/bugzilla/bug794131/bug794131.h
+++ test/users/bugzilla/bug794131/bug794131.h
@@ -1,5 +1,9 @@
 #include "stdint.h"
 
-typedef void* data_set;
+//typedef void* data_set;
+
+typedef struct __data_set {
+
+} _data_set, *data_set;
 
 int64_t read_stuff(data_set location, void* things, uint64_t start, uint64_t size, uint64_t count);
