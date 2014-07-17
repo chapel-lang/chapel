927165a9dd15fa7f676778d34d42f2e347a060f3
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2014-07-17 13:14:14 -0700

    Trying out Brad's change

--- test/users/bugzilla/bug794131/bug794131.h
+++ test/users/bugzilla/bug794131/bug794131.h
@@ -3,7 +3,7 @@
 //typedef void* data_set;
 
 typedef struct __data_set {
-
+  int x;
 } _data_set, *data_set;
 
 int64_t read_stuff(data_set location, void* things, uint64_t start, uint64_t size, uint64_t count);
