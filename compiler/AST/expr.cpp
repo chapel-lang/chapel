b2982704a8a953544f7d2af7b339e0a164179a9e
Thomas H Hildebrandt <hilde@cray.com>
2014-07-23 11:36:03 -0500

    Fix a few bugs with PRIM_TYPE_INIT.
    resolveCall() must apply default generic type resolution to the PRIM_TYPE_INIT primitive.
    
    CallExpr::prettyPrint() had to be updated to recognize PRIM_TYPE_INIT as an "unusual"
    case.

--- compiler/AST/expr.cpp
+++ compiler/AST/expr.cpp
@@ -3492,7 +3492,8 @@ void CallExpr::prettyPrint(std::ostream *o) {
       baseExpr->prettyPrint(o);
     }
   } else if (primitive != NULL) {
-    if (primitive->tag == PRIM_INIT) {
+    if (primitive->tag == PRIM_INIT ||
+      primitive->tag == PRIM_TYPE_INIT) {
       unusual = true;
       argList.head->prettyPrint(o);
     }
--- compiler/resolution/functionResolution.cpp
+++ compiler/resolution/functionResolution.cpp
@@ -2920,6 +2920,7 @@ resolveCall(CallExpr* call)
      case PRIM_TUPLE_EXPAND:        resolveTupleExpand(call);           break;
      case PRIM_SET_MEMBER:          resolveSetMember(call);             break;
      case PRIM_MOVE:                resolveMove(call);                  break;
+     case PRIM_TYPE_INIT:
      case PRIM_INIT:                resolveDefaultGenericType(call);    break;
      case PRIM_NEW:                 resolveNew(call);                   break;
     }
