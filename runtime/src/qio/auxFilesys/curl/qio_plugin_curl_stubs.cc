596d242b02fbe24537b058a1fea13655dfbb4b3d
Tim Zakian <timzakian@gmail.com>
2014-07-24 11:43:58 -0700

    Fixing stubs file to work with gcc 4.3.4 and Cray compiler

--- runtime/src/qio/auxFilesys/curl/qio_plugin_curl_stubs.cc
+++ runtime/src/qio/auxFilesys/curl/qio_plugin_curl_stubs.cc
@@ -18,50 +18,49 @@
 
 #include "qio_plugin_curl.h"
 
-#undef exit
-#define CURL_ERROR {\
+#define CURL_ERROR(ret){\
   chpl_internal_error("No Curl Support");\
-  exit(EXIT_FAILURE);\
+  return ret;\
 }
 
 qio_file_functions_t curl_function_struct = {
-    .writev = &curl_writev,
-    .readv = &curl_readv,
-    .pwritev = NULL,
-    .preadv = &curl_preadv,
-    .close  = &curl_close,
-    .open   = &curl_open,
-    .seek   = &curl_seek,
-    .filelength = &curl_getlength,
-    .getpath = &curl_getpath,
-    .fsync = NULL,
-    .getcwd = NULL,
+    &curl_writev,
+    &curl_readv,
+    NULL,
+    &curl_preadv,
+    &curl_close,
+    &curl_open,
+    &curl_seek,
+    &curl_getlength,
+    &curl_getpath,
+    NULL,
+    NULL,
 };
 
 const qio_file_functions_ptr_t curl_function_struct_ptr = &curl_function_struct;
 
-qioerr curl_readv(void* file, const struct iovec *vector, int count, ssize_t* num_read_out, void* fs) CURL_ERROR
+qioerr curl_readv(void* file, const struct iovec *vector, int count, ssize_t* num_read_out, void* fs) CURL_ERROR(0)
 
-qioerr curl_preadv(void* file, const struct iovec *vector, int count, off_t offset, ssize_t* num_read_out, void* fs) CURL_ERROR
+qioerr curl_preadv(void* file, const struct iovec *vector, int count, off_t offset, ssize_t* num_read_out, void* fs) CURL_ERROR(0)
 
-qioerr curl_pwritev(void* fd, const struct iovec* iov, int iovcnt, off_t see_to_offset, ssize_t* num_read_out, void* fs) CURL_ERROR
+qioerr curl_pwritev(void* fd, const struct iovec* iov, int iovcnt, off_t see_to_offset, ssize_t* num_read_out, void* fs) CURL_ERROR(0)
 
-qioerr curl_writev(void* fl, const struct iovec* iov, int iovcnt, ssize_t* num_written_out, void* fs) CURL_ERROR
+qioerr curl_writev(void* fl, const struct iovec* iov, int iovcnt, ssize_t* num_written_out, void* fs) CURL_ERROR(0)
 
-qioerr curl_open(void** fd, const char* path, int* flags, mode_t mode, qio_hint_t iohints, void* fs) CURL_ERROR
+qioerr curl_open(void** fd, const char* path, int* flags, mode_t mode, qio_hint_t iohints, void* fs) CURL_ERROR(0)
 
-qioerr curl_close(void* fl, void* fs) CURL_ERROR
+qioerr curl_close(void* fl, void* fs) CURL_ERROR(0)
 
-qioerr curl_seek(void* fl, off_t offset, int whence, off_t* offset_out, void* fs) CURL_ERROR
+qioerr curl_seek(void* fl, off_t offset, int whence, off_t* offset_out, void* fs) CURL_ERROR(0)
 
-qioerr curl_getpath(void* file, const char** string_out, void* fs) CURL_ERROR
+qioerr curl_getpath(void* file, const char** string_out, void* fs) CURL_ERROR(0)
 
-qioerr curl_getlength(void* fl, int64_t* len_out, void* fs) CURL_ERROR
+qioerr curl_getlength(void* fl, int64_t* len_out, void* fs) CURL_ERROR(0)
 
-qioerr chpl_curl_set_opt(qio_file_t* fl, int opt, ...) CURL_ERROR
+qioerr chpl_curl_set_opt(qio_file_t* fl, int opt, ...) CURL_ERROR(0)
 
-qioerr chpl_curl_perform(qio_file_t* fl) CURL_ERROR
+qioerr chpl_curl_perform(qio_file_t* fl) CURL_ERROR(0)
 
-qioerr chpl_curl_stream_string(qio_file_t* fl, const char** str) CURL_ERROR
+qioerr chpl_curl_stream_string(qio_file_t* fl, const char** str) CURL_ERROR(0)
 
-qioerr chpl_curl_stream_file(qio_file_t* fl_curl, qio_file_t* fl_local) CURL_ERROR
+qioerr chpl_curl_stream_file(qio_file_t* fl_curl, qio_file_t* fl_local) CURL_ERROR(0)
