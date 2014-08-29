2858650cb3627e47111e3c495625395ffceea951
Elliot Ronaghan <ronawho@gmail.com>
2014-08-29 14:04:56 -0700

    See the performance implications of qthreads without hwloc

--- util/cron/test-perf-chap04-qthreads.bash
+++ util/cron/test-perf-chap04-qthreads.bash
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 #
-# Run performance tests on chap04 with tasks=qthreads.
 
 CWD=$(cd $(dirname $0) ; pwd)
 source $CWD/common-perf.bash
 source $CWD/common-qthreads.bash
-export CHPL_MEM=tcmalloc
+export CHPL_HWLOC=none
 # releasePerformance still generates results based on the fifo timings. It's
 # run here again, otherwise syncing the qthreads results blows away the
 # directory with the releaseOverRelease graphs in
-$CWD/nightly -cron -execopts --dataParTasksPerLocale=8 -performance-description 'qthreads --genGraphOpts "-m default -m qthreads"' -releasePerformance -numtrials 5 -startdate 07/28/12
+$CWD/nightly -cron -performance-description 'qthreads --genGraphOpts "-m default -m qthreads"' -releasePerformance -numtrials 5 -startdate 07/28/12
