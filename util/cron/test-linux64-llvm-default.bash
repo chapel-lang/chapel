cc4fde39d38a85b0b36902a056b5fc3786ab145f
Thomas Van Doren <thomas.vandoren@gmail.com>
2014-09-11 17:41:07 -0700

    word smithing

--- util/cron/test-linux64-llvm-default.bash
+++ util/cron/test-linux64-llvm-default.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Test default configuration with CHPL_LLVM=llvm and throwing --llvm flag to
+# Test default configuration with CHPL_LLVM=llvm and pass --llvm flag to
 # compiler on linux64.
 
 CWD=$(cd $(dirname $0) ; pwd)
