adba620919adc1517b68620f1719a891925510fe
Elliot Ronaghan <ronawho@gmail.com>
2014-09-14 16:51:02 -0700

    Move my performance "playground"
    My playground originated as doing qthreads performance testing alongside of
    fifo and devolved into a testing a bunch of different performance related
    things. I believe having the playground around is useful longterm to
    investigate performance related things without having to manually kick off
    runs. However, it's not useful being on the main performance webpage and is
    confusing to most people on the team and almost certainly all people off the
    core team.
    
    This just moves it from being at the page to being on it's own that is not
    publicly linked.
    
    E.G instead of being chapel.sourceforge.net/perf/chap04, it will be
    chapel.sourceforge.net/perf/chap04/playground

--- util/cron/test-perf-chap04-qthreads.bash
+++ util/cron/test-perf-chap04-qthreads.bash
@@ -5,7 +5,4 @@ CWD=$(cd $(dirname $0) ; pwd)
 source $CWD/common-perf.bash
 source $CWD/common-qthreads.bash
 export CHPL_RT_NUM_THREADS_PER_LOCALE=16
-# releasePerformance still generates results based on the fifo timings. It's
-# run here again, otherwise syncing the qthreads results blows away the
-# directory with the releaseOverRelease graphs in
-$CWD/nightly -cron -performance-description 'qthreads --genGraphOpts "-m default -m qthreads"' -releasePerformance -numtrials 5 -startdate 07/28/12
+$CWD/nightly -cron -sync-dest-path playground -performance-description 'playground --genGraphOpts "-m default -m playground"' -numtrials 5 -startdate 07/28/12
