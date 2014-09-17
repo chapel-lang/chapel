fa596e362457d7818bb23abe51ccbf42a7c300e2
Brad Chamberlain <bradcray@users.noreply.github.com>
2014-09-17 11:13:30 -0700

    Fixing C declaration of main() in test
    Sadly, darwin was the only platform kind enough to complain about this
    thinko.

--- test/Samples/Performance/foo-c.test.c
+++ test/Samples/Performance/foo-c.test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(int argc, char* argv) {
+int main(int argc, char* argv[]) {
   printf("Time: %g seconds\n", 183.7);
   printf("Memory: %d GB\n", 24);
   printf("Validation: SUCCESS\n");
