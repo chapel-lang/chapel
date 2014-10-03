dcc7a0718b8327ff1c34928f7161ce284c7e3403
Thomas Van Doren <thomas.vandoren@gmail.com>
2014-10-02 17:50:16 -0700

    Update tarball script to support version detection.
    Will be useful should we want to build a tarball after every commit or every
    night...

--- util/cron/create_release_tarball.bash
+++ util/cron/create_release_tarball.bash
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 #
 # Create and store a release tarball. CHPL_VERSION must be set in the
-# environment. TARBALL_DIR can optionally be set in the environment. If set,
-# the tarball will be copied to that directory after it is built.
+# environment. The special value, "detect", can be used to detect the version
+# number using version_num.h and the sha1 of the working directory. This should
+# only be used for non-release builds (e.g. nightly or HEAD builds).
+#
+# TARBALL_DIR can optionally be set in the environment. If set, the tarball
+# will be copied to that directory after it is built.
 
 CWD=$(cd $(dirname $0) ; pwd)
 source $CWD/functions.bash
@@ -20,6 +24,26 @@ fi
 
 export CHPL_HOME=$(cd $CWD/../.. ; pwd)
 log_info "Setting CHPL_HOME to: ${CHPL_HOME}"
+
+# Allow caller to tell this script to detect the current version based on the
+# values in version_num.h and the current sha1. Assumes the current working
+# directory has a version of the code that is suitable for building from.
+if [ "${CHPL_VERSION}" = "detect" ] ; then
+
+    if [ ! -d $CHPL_HOME/.git ] ; then
+        log_error "Expected \$CHPL_HOME to be a git repo. .git dir does not exist at: ${CHPL_HOME}/.git"
+        exit 1
+    fi
+
+    version_num_text=$(cat $CHPL_HOME/compiler/main/version_num.h)
+    major=$(echo "${version_num_text}" | grep MAJOR_VERSION | cut -f3 -d" " | sed "s/\"//g")
+    minor=$(echo "${version_num_text}" | grep MINOR_VERSION | cut -f3 -d" " | sed "s/\"//g")
+    patch=$(echo "${version_num_text}" | grep UPDATE_VERSION | cut -f3 -d" " | sed "s/\"//g")
+    sha1=$(git rev-parse --short HEAD)
+
+    export CHPL_VERSION="${major}.${minor}.${patch}.${sha1}"
+    log_info "Detected CHPL_VERSION to be: ${CHPL_VERSION}"
+fi
 
 short_version=$(echo $CHPL_VERSION | cut -d. -f1,2)
 export CHPL_GEN_RELEASE_BRANCH=release/$short_version
