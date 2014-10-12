f249f70ab993f86b7af735ed9f5022d1d9255a91
Thomas Van Doren <thomas.vandoren@gmail.com>
2014-10-12 15:20:57 -0700

    Add parallel execution of make to lots of testing scripts.

--- util/cron/test-cray-xc-knc-default-examples.bash
+++ util/cron/test-cray-xc-knc-default-examples.bash
@@ -4,7 +4,11 @@ CWD=$(cd $(dirname $0) ; pwd)
 source $CWD/functions.bash
 source $CWD/common-knc.bash
 
+# Number of logical processes on current system. Will be used as number of jobs
+# when calling make with parallel execution.
+num_procs=$(python -c "import multiprocessing; print(multiprocessing.cpu_count())")
+
 # Ensure it builds!
 log_info "Calling make..."
-make -C $CWD/../..
+make -j${num_procs} -C $CWD/../..
 log_info "Finished building chpl for knc."
