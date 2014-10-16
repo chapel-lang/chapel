e28ca319429dbd2cdb9a152de417edd8bedc70a3
Thomas Van Doren <thomas.vandoren@gmail.com>
2014-10-15 17:39:00 -0700

    Source common-gasnet.bash in dist- test scripts.
    I removed common-dists.bash, but did not add the common-gasnet sourcing to the
    test scripts. Good catch by the reviewer, @ronawho.

--- util/cron/test-linux64-dist-block-default.bash
+++ util/cron/test-linux64-dist-block-default.bash
@@ -3,7 +3,7 @@
 # Test block distribution using default configuration.
 
 CWD=$(cd $(dirname $0) ; pwd)
-source $CWD/common.bash
+source $CWD/common-gasnet.bash
 
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="dist-block"
 
--- util/cron/test-linux64-dist-replicated-default.bash
+++ util/cron/test-linux64-dist-replicated-default.bash
@@ -3,7 +3,7 @@
 # Test replicated distribution using default configuration.
 
 CWD=$(cd $(dirname $0) ; pwd)
-source $CWD/common.bash
+source $CWD/common-gasnet.bash
 
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="dist-replicated"
 
