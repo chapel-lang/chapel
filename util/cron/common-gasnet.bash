b22a3b7cd9f3c19a09edfc2fb878239291e5ad4f
Thomas Van Doren <thomas.vandoren@gmail.com>
2014-11-12 21:30:08 -0800

    Increase timeout for test systems w/ <= 8 logical cores.
    Suggested by @ronawho to get rid of final timeouts.

--- util/cron/common-gasnet.bash
+++ util/cron/common-gasnet.bash
@@ -33,9 +33,6 @@ if [ "${tasks}" == "qthreads" ] ; then
     # hardware we get the performance we expect
     logicalCores=`python -c 'import multiprocessing ; print multiprocessing.cpu_count()'`
     if [ $logicalCores -le 8 ] ; then
-        export CHPL_TEST_TIMEOUT=900
-    fi
-    if [ $logicalCores -le 4 ] ; then
         export CHPL_TEST_TIMEOUT=1800
     fi
 fi
