f9850bf1d83597b7509bc4ee7b7725de0904b1e6
Michael Noakes <c17114@cray.com>
2014-11-17 17:04:54 -0800

    Exploit the new isStmtExpr() in deadExpressionElimination

--- compiler/optimizations/deadCodeElimination.cpp
+++ compiler/optimizations/deadCodeElimination.cpp
@@ -174,7 +174,7 @@ void deadExpressionElimination(FnSymbol* fn) {
     } else if (exprAst->parentExpr == NULL) { // expression already removed
 
     } else if (SymExpr* expr = toSymExpr(ast)) {
-      if (isInLoopHeader(expr) == false && expr == expr->getStmtExpr()) {
+      if (isInLoopHeader(expr) == false && expr->isStmtExpr() == true) {
         expr->remove();
       }
 
@@ -184,7 +184,7 @@ void deadExpressionElimination(FnSymbol* fn) {
           expr->isPrimitive(PRIM_GET_MEMBER) ||
           expr->isPrimitive(PRIM_DEREF) ||
           expr->isPrimitive(PRIM_ADDR_OF)) {
-        if (expr == expr->getStmtExpr())
+        if (expr->isStmtExpr())
           expr->remove();
       }
 
