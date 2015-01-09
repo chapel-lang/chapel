d11d13c6596c32b73e8d719dc2073f49a47c2f81
Thomas Van Doren <thomas.vandoren@gmail.com>
2015-01-09 10:24:33 -0800

    Only test examples for RHEL.

--- util/cron/test-rhel.linux64.bash
+++ util/cron/test-rhel.linux64.bash
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #
-# Test default configuration against full suite on RHEL linux64 system.
+# Test default configuration against examples on RHEL linux64 system.
 
 CWD=$(cd $(dirname $0) ; pwd)
 source $CWD/common.bash
 
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="rhel.linux64"
 
-$CWD/nightly -cron
+$CWD/nightly -cron -examples
