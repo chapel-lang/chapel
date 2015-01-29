2adaa10870cd1ce7c83a5f49d1b533398a09aa35
Thomas Van Doren <thomas.vandoren@gmail.com>
2015-01-29 08:58:23 -0800

    Add sphinxcontrib.chapeldomain extension.

--- third-party/chpldoc-venv/chpldoc-sphinx-project/source/conf.py
+++ third-party/chpldoc-venv/chpldoc-sphinx-project/source/conf.py
@@ -30,7 +30,9 @@ needs_sphinx = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinxcontrib.chapeldomain',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
