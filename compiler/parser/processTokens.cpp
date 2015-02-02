b8d7ab1b128b7d7d52e380c383ec6ed7edbd0bd8
Michael Noakes <mnoakes@cray.com>
2015-02-02 09:05:12 -0800

    Drop modulefinder per Brad

--- compiler/parser/processTokens.cpp
+++ compiler/parser/processTokens.cpp
@@ -1,15 +1,15 @@
 /*
  * Copyright 2004-2015 Cray Inc.
  * Other additional copyright holders may be indicated within.
- * 
+ *
  * The entirety of this work is licensed under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
- * 
+ *
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,7 @@
 
 #include "processTokens.h"
 
+#include "chapel.tab.h"
 #include "countTokens.h"
 #include "misc.h"
 #include "stringutil.h"
@@ -26,17 +27,6 @@
 
 #include <cstring>
 #include <cctype>
-
-#ifndef MODULE_FINDER
-#include "chapel.tab.h"
-#else
-#include "modulefinder.tab.h"
-
-#define countNewline()
-#define countSingleLineComment(x)
-#define countMultiLineComment(x)
-#define countCommentLine()
-#endif
 
 static int stringBuffLen = 0;
 static int stringLen = 0;
