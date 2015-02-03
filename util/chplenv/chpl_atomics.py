ffd3671ed96f0787309ae69958067d7672257d29
Elliot Ronaghan <ronawho@gmail.com>
2015-02-03 15:48:39 -0800

    Change compiler comparison based on kyles feedback.

--- util/chplenv/chpl_atomics.py
+++ util/chplenv/chpl_atomics.py
@@ -21,21 +21,21 @@ def get(flag='target'):
 
             # we currently support intrinsics for gcc, intel, cray and clang.
             # gcc added initial support in 4.1, and added support for 64 bit
-            # atomics on 32 bit platforms with 4.8. clang and intel support 64
-            # bit atomics on 32 bit platforms and the cray compiler will never
-            # run on a 32 bit machine. For pgi, or 32 bit platforms with older
-            # gcc, we fall back to locks
+            # atomics on 32 bit platforms with 4.8. clang and intel also
+            # support 64 bit atomics on 32 bit platforms and the cray compiler
+            # will never run on a 32 bit machine. For pgi or 32 bit platforms
+            # with an older gcc, we fall back to locks
             if compiler_val == 'gnu' or compiler_val == 'cray-prgenv-gnu':
                 version = utils.get_compiler_version('gnu')
                 if version >= 4.8:
                     atomics_val = 'intrinsics'
                 elif version >= 4.1 and not platform_val.endswith('32'):
                     atomics_val = 'intrinsics'
-            elif 'intel' in compiler_val or compiler_val == 'cray-prgenv-intel':
+            elif compiler_val == 'intel' or compiler_val == 'cray-prgenv-intel':
                 atomics_val = 'intrinsics'
             elif compiler_val == 'cray-prgenv-cray':
                 atomics_val = 'intrinsics'
-            elif 'clang' in compiler_val:
+            elif compiler_val == 'clang':
                 atomics_val = 'intrinsics'
 
             # we can't use intrinsics, fall back to locks
