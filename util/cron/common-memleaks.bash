7b6c94e0f658b9388fa369177b324525aa09ca0c
Thomas Van Doren <thomas.vandoren@gmail.com>
2015-02-06 11:50:56 -0800

    Update memleaks testing to keep logs in one place, and memleaks data in another.
    The logs from start_test will be kept in the regular nightly location,
    CHPL_NIGHTLY_[CRON_]LOGDIR aka /data/sea/chapel/Nightly. The raw memleaks
    output, the combined data, and the .dat files will be kept in
    CHPL_NIGHTLY_MEMLEAKS_DIR, aka /data/sea/chapel/NightlyMemLeaks/.
    
    This allows the memleaks start_test logs live in the same place as all other
    logs, without polluting that place with a bunch of memleaks-specific files.

--- util/cron/common-memleaks.bash
+++ util/cron/common-memleaks.bash
@@ -3,6 +3,7 @@
 # Configure environment for memory leaks testing. This should be sourced by
 # other scripts that wish to make use of the variables set here.
 
+export CHPL_NIGHTLY_MEMLEAKS_DIR=${CHPL_NIGHTLY_MEMLEAKS_DIR:-$logdir_prefix/NightlyMemLeaks}
 export CHPL_COMM=none
 
 function memleaks_log()
@@ -30,7 +31,7 @@ function save_memleaks_log()
     local tests=$1
     local logfile=$(memleaks_log $tests)
     local abs_logfile=${CHPL_NIGHTLY_TMPDIR:-${TMPDIR:-/tmp}}/$logfile
-    local destination=${CHPL_NIGHTLY_LOGDIR}/FULL.memleaks/
+    local destination=${CHPL_NIGHTLY_MEMLEAKS_DIR}/FULL.memleaks/
 
     log_info "Saving raw memory leaks log file."
     cp $abs_logfile $destination
