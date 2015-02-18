642b16ee00ca27800a5363cfea9c020c40533c02
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2015-02-18 11:53:20 -0800

    Add a (commented out) flag to skip the documentation of extern functions.
    Most of this functionality was already present, but we had not created a flag
    for it yet.  However, since we believe that any externs visible at the module
    level in our standard modules should be in a smaller scope, we decided to leave
    the default as is and not make the flag visible, thereby forcing the documenter
    to make this change instead of giving them an easy way out.  We should discuss
    further whether we would like this flag present (but since it was a Five Minute
    Fix (TM), there isn't much harm in placing it in the repo so long as the work
    has been done.)

--- compiler/passes/docs.cpp
+++ compiler/passes/docs.cpp
@@ -369,7 +369,7 @@ void printModule(std::ofstream *file, ModuleSymbol *mod, std::string name) {
     forv_Vec(VarSymbol, var, variables) {
       printGlobal(file, var, false);
     }
-    Vec<FnSymbol*> fns = mod->getTopLevelFunctions(true);
+    Vec<FnSymbol*> fns = mod->getTopLevelFunctions(fDocsIncludeExterns);
     // If alphabetical option passed, fDocsAlphabetizes the output
     if (fDocsAlphabetize)
       qsort(fns.v, fns.n, sizeof(fns.v[0]), compareNames);
