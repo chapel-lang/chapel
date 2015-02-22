f7af7ffa6c98e385df103f5927d8c6a024b85551
Thomas Van Doren <thomas.vandoren@gmail.com>
2015-02-21 20:09:57 -0800

    Start on printDocs() visitor.
    This will be used to traverse module/classes/records in source order.

---
+++ compiler/AST/AstPrintDocs.cpp
@@ -0,0 +1,213 @@
+/*
+ * Copyright 2004-2015 Cray Inc.
+ * Other additional copyright holders may be indicated within.
+ *
+ * The entirety of this work is licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "AstPrintDocs.h"
+
+
+AstPrintDocs::AstPrintDocs() {
+}
+
+
+AstPrintDocs::~AstPrintDocs() {
+}
+
+
+bool AstPrintDocs::enterAggrType(AggregateType* node) {
+  return true;
+}
+
+
+void AstPrintDocs::exitAggrType(AggregateType* node) {
+}
+
+
+bool AstPrintDocs::enterEnumType(EnumType* node) {
+  return true;
+}
+
+
+void AstPrintDocs::exitEnumType(EnumType* node) {
+}
+
+
+void AstPrintDocs::visitPrimType(PrimitiveType* node) {
+}
+
+
+bool AstPrintDocs::enterArgSym(ArgSymbol* node) {
+  return true;
+}
+
+
+void AstPrintDocs::exitArgSym(ArgSymbol* node) {
+}
+
+
+void AstPrintDocs::visitEnumSym(EnumSymbol* node) {
+}
+
+
+bool AstPrintDocs::enterFnSym(FnSymbol* node) {
+  return true;
+}
+
+
+void AstPrintDocs::exitFnSym(FnSymbol* node) {
+}
+
+
+void AstPrintDocs::visitIpeSym(IpeSymbol* node) {
+}
+
+
+void AstPrintDocs::visitLabelSym(LabelSymbol* node) {
+}
+
+
+bool AstPrintDocs::enterModSym(ModuleSymbol* node) {
+  return true;
+}
+
+
+void AstPrintDocs::exitModSym(ModuleSymbol* node) {
+}
+
+
+bool AstPrintDocs::enterTypeSym(TypeSymbol* node) {
+  return true;
+}
+
+
+void AstPrintDocs::exitTypeSym(TypeSymbol* node) {
+}
+
+
+void AstPrintDocs::visitVarSym(VarSymbol* node) {
+}
+
+
+bool AstPrintDocs::enterCallExpr(CallExpr* node) {
+  return true;
+}
+
+
+void AstPrintDocs::exitCallExpr(CallExpr* node) {
+}
+
+
+bool AstPrintDocs::enterDefExpr(DefExpr* node) {
+  return true;
+}
+
+
+void AstPrintDocs::exitDefExpr(DefExpr* node) {
+}
+
+
+bool AstPrintDocs::enterNamedExpr(NamedExpr* node) {
+  return true;
+}
+
+
+void AstPrintDocs::exitNamedExpr(NamedExpr* node) {
+}
+
+
+void AstPrintDocs::visitSymExpr(SymExpr* node) {
+}
+
+
+void AstPrintDocs::visitUsymExpr(UnresolvedSymExpr* node) {
+}
+
+
+bool AstPrintDocs::enterBlockStmt(BlockStmt* node) {
+  return true;
+}
+
+
+void AstPrintDocs::exitBlockStmt(BlockStmt* node) {
+}
+
+
+bool AstPrintDocs::enterWhileDoStmt(WhileDoStmt* node) {
+  return true;
+}
+
+
+void AstPrintDocs::exitWhileDoStmt(WhileDoStmt* node) {
+}
+
+
+bool AstPrintDocs::enterDoWhileStmt(DoWhileStmt* node) {
+  return true;
+}
+
+
+void AstPrintDocs::exitDoWhileStmt(DoWhileStmt* node) {
+}
+
+
+bool AstPrintDocs::enterCForLoop(CForLoop* node) {
+  return true;
+}
+
+
+void AstPrintDocs::exitCForLoop(CForLoop* node) {
+}
+
+
+bool AstPrintDocs::enterForLoop(ForLoop* node) {
+  return true;
+}
+
+
+void AstPrintDocs::exitForLoop(ForLoop* node) {
+}
+
+
+bool AstPrintDocs::enterParamForLoop(ParamForLoop* node) {
+  return true;
+}
+
+
+void AstPrintDocs::exitParamForLoop(ParamForLoop* node) {
+}
+
+
+bool AstPrintDocs::enterCondStmt(CondStmt* node) {
+  return true;
+}
+
+
+void AstPrintDocs::exitCondStmt(CondStmt* node) {
+}
+
+
+void AstPrintDocs::visitEblockStmt(ExternBlockStmt* node) {
+}
+
+
+bool AstPrintDocs::enterGotoStmt(GotoStmt* node) {
+  return true;
+}
+
+
+void AstPrintDocs::exitGotoStmt(GotoStmt* node) {
+}
---
+++ compiler/include/AstPrintDocs.h
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2004-2015 Cray Inc.
+ * Other additional copyright holders may be indicated within.
+ *
+ * The entirety of this work is licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _AST_PRINT_DOCS_H_
+#define _AST_PRINT_DOCS_H_
+
+#include "AstVisitor.h"
+
+
+class AstPrintDocs : public AstVisitor {
+public:
+                   AstPrintDocs();
+  virtual         ~AstPrintDocs();
+
+  //
+  // The sub-classes of Type
+  //
+  virtual bool   enterAggrType    (AggregateType*     node);
+  virtual void   exitAggrType     (AggregateType*     node);
+
+  virtual bool   enterEnumType    (EnumType*          node);
+  virtual void   exitEnumType     (EnumType*          node);
+
+  virtual void   visitPrimType    (PrimitiveType*     node);
+
+  //
+  // The sub-classes of Symbol
+  //
+  virtual bool   enterArgSym      (ArgSymbol*         node);
+  virtual void   exitArgSym       (ArgSymbol*         node);
+
+  virtual void   visitEnumSym     (EnumSymbol*        node);
+
+  virtual bool   enterFnSym       (FnSymbol*          node);
+  virtual void   exitFnSym        (FnSymbol*          node);
+
+  virtual void   visitIpeSym      (IpeSymbol*         node);
+
+  virtual void   visitLabelSym    (LabelSymbol*       node);
+
+  virtual bool   enterModSym      (ModuleSymbol*      node);
+  virtual void   exitModSym       (ModuleSymbol*      node);
+
+  virtual bool   enterTypeSym     (TypeSymbol*        node);
+  virtual void   exitTypeSym      (TypeSymbol*        node);
+
+  virtual void   visitVarSym      (VarSymbol*         node);
+
+  //
+  // The sub-classes of Expr
+  //
+  virtual bool   enterCallExpr    (CallExpr*          node);
+  virtual void   exitCallExpr     (CallExpr*          node);
+
+  virtual bool   enterDefExpr     (DefExpr*           node);
+  virtual void   exitDefExpr      (DefExpr*           node);
+
+  virtual bool   enterNamedExpr   (NamedExpr*         node);
+  virtual void   exitNamedExpr    (NamedExpr*         node);
+
+  virtual void   visitSymExpr     (SymExpr*           node);
+
+  virtual void   visitUsymExpr    (UnresolvedSymExpr* node);
+
+  //
+  // The sub-classes of Stmt
+  //
+  virtual bool   enterBlockStmt   (BlockStmt*         node);
+  virtual void   exitBlockStmt    (BlockStmt*         node);
+
+  virtual bool   enterWhileDoStmt (WhileDoStmt*       node);
+  virtual void   exitWhileDoStmt  (WhileDoStmt*       node);
+
+  virtual bool   enterDoWhileStmt (DoWhileStmt*       node);
+  virtual void   exitDoWhileStmt  (DoWhileStmt*       node);
+
+  virtual bool   enterCForLoop    (CForLoop*          node);
+  virtual void   exitCForLoop     (CForLoop*          node);
+
+  virtual bool   enterForLoop     (ForLoop*           node);
+  virtual void   exitForLoop      (ForLoop*           node);
+
+  virtual bool   enterParamForLoop(ParamForLoop*      node);
+  virtual void   exitParamForLoop (ParamForLoop*      node);
+
+  virtual bool   enterCondStmt    (CondStmt*          node);
+  virtual void   exitCondStmt     (CondStmt*          node);
+
+  virtual void   visitEblockStmt  (ExternBlockStmt*   node);
+
+  virtual bool   enterGotoStmt    (GotoStmt*          node);
+  virtual void   exitGotoStmt     (GotoStmt*          node);
+};
+
+#endif
