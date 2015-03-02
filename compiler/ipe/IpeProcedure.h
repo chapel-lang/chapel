76ce998809e2c51844f4084cdb8486df51190559
Michael Noakes <mnoakes@cray.com>
2015-03-02 12:39:14 -0800

    IPE: Merge the new files to the repository

---
+++ compiler/ipe/IpeProcedure.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2004-2015 Cray Inc.
+ * Other additional copyright holders may be indicated within.
+ *
+ * The entirety of this work is licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _IPE_PROCEDURE_H_
+#define _IPE_PROCEDURE_H_
+
+#include <vector>
+
+class FnSymbol;
+class IpeVars;
+class IpeScope;
+class IpeScopeProcedure;
+class Type;
+
+// NOAKES 2015/02/24 Only handles module-level functions
+class IpeProcedure
+{
+public:
+                          IpeProcedure(FnSymbol* sym, IpeScope* scope);
+                         ~IpeProcedure();
+
+  const char*             name()                                     const;
+
+  IpeScopeProcedure*      scope()                                    const;
+
+  FnSymbol*               fnSymbol()                                 const;
+
+  bool                    exactMatch(std::vector<Type*>& actualTypes);
+  bool                    ensureBodyResolved();
+
+  void                    frameSizeSet(int size);
+  int                     frameSize()                                const;
+
+  void                    describe(int offset)                       const;
+
+private:
+  enum State
+  {
+    kUnresolved,
+    kFormalsResolved,
+    kResolved
+  };
+                          IpeProcedure();
+
+  FnSymbol*               mFnDecl;
+  IpeScopeProcedure*      mScope;
+  IpeVars*                mVars;
+  State                   mState;
+  int                     mFrameSize;
+};
+
+#endif
