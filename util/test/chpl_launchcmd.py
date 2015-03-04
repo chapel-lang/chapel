c66aa1c49d266eb08a187af85a84fa41aa95670f
Vassily Litvinov <vasslitvinov@users.noreply.github.com>
2015-03-04 13:22:43 -0800

    Switched the order of sleep() and status().

--- util/test/chpl_launchcmd.py
+++ util/test/chpl_launchcmd.py
@@ -284,10 +284,12 @@ class AbstractJob(object):
             status = job_status(job_id, output_file)
             while status != 'C':
                 if not alreadyRunning and status == 'R':
+                    # It would be great, at this point, to have 'start_test'
+                    # print a message "execution started". Unclear how to.
                     alreadyRunning = True
                     exec_start_time = time.time()
-                status = job_status(job_id, output_file)
                 time.sleep(.5)
+                status = job_status(job_id, output_file)
 
             exec_time = time.time() - exec_start_time
             # Note that this time isn't very accurate as we don't get the exact
