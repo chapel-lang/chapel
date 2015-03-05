8d6229d29a17b0eb9640063307e04365e01b99b5
Greg Titus <gbtitus@users.noreply.github.com>
2015-03-04 18:31:04 -0600

    Only add -lrt in cases where we definitely need it and adding it is safe.

--- util/chplenv/chpl_3p_qthreads_configs.py
+++ util/chplenv/chpl_3p_qthreads_configs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import utils
 from utils import memoize
-import chpl_locale_model
+import chpl_compiler, chpl_llvm, chpl_locale_model
 import third_party_utils
 
 @memoize
@@ -11,9 +11,13 @@ def get_uniq_cfg_path():
 
 @memoize
 def get_link_args():
-    return third_party_utils.default_get_link_args('qthread',
-                                                   ucp=get_uniq_cfg_path(),
-                                                   libs=['libqthread_chpl.la',
-                                                         '-lchpl',
-                                                         'libqthread.la',
-                                                         '-lrt'])
+    link_args = \
+        third_party_utils.default_get_link_args('qthread',
+                                                ucp=get_uniq_cfg_path(),
+                                                libs=['libqthread_chpl.la',
+                                                      '-lchpl',
+                                                      'libqthread.la'])
+    if ( chpl_compiler.get('target').startswith('cray-prgenv') and
+         chpl_llvm.get() == 'llvm' ):
+        link_args.append('-lrt')
+    return link_args
