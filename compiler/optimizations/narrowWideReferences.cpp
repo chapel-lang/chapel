81e29e25c4ba47a05d8f421d91040bd4e875c90f
Ben Harshbarger <ben.harshb@gmail.com>
2015-03-06 11:44:39 -0800

    Widen string_from_c_string conservatively to avoid more copies; Fix uninitialized pointer errors for coverity; Improve logic that resulted in unnecessarily wide references

--- compiler/optimizations/narrowWideReferences.cpp
+++ compiler/optimizations/narrowWideReferences.cpp
@@ -113,11 +113,12 @@ public:
   // destinations of a deref can also be narrow.
   std::vector<SymExpr*> destsToNarrow;
 
-  WideInfo() : sym(NULL), mustBeWide(false), valIsWide(false), fnToNarrow(NULL) { }
+  WideInfo() : sym(NULL), mustBeWide(false), wideType(NULL), valIsWide(false), fnToNarrow(NULL) { }
   WideInfo(Symbol* isym) : sym(isym), mustBeWide(false), valIsWide(false), fnToNarrow(NULL) {
     if (isWideType(sym)) {
       wideType = toAggregateType(sym->type);
-    }
+    } else
+      wideType = NULL;
   }
 
   void setWide(SymExpr* se, const char* msg) {
@@ -603,6 +604,7 @@ narrowSym(Symbol* sym, WideInfo* wi,
           if (rhs->isPrimitive(PRIM_STRING_FROM_C_STRING)) {
             // The destination string need not be wide if we're initializing from
             // a c_string.
+            wi->mustBeWide = true;
             continue;
           }
           if (rhs->isPrimitive(PRIM_VIRTUAL_METHOD_CALL)) {
@@ -617,7 +619,7 @@ narrowSym(Symbol* sym, WideInfo* wi,
             if (fn->hasFlag(FLAG_LOCALE_MODEL_ALLOC))
               continue;
             bool retIsWide = isWideType(fn->retType->symbol);
-            if (retIsWide && (isWideRef || isWideObj)) {
+            if (retIsWide && (isWideObj)) {
                 addNarrowDep(fn->getReturnSymbol(), sym);
             }
             if (isRef(sym)) {
@@ -903,8 +905,10 @@ narrowArg(ArgSymbol* arg, WideInfo* wi,
         wideInfoMap->get(actual->var)->exprsToWiden.push_back(actual);
         
         // sync _val wideness
-        wi->refsToNarrow.push_back(actual->var);
-        wideInfoMap->get(actual->var)->refsToNarrow.push_back(arg);
+        if (isRef(actual->var))
+          wi->refsToNarrow.push_back(actual->var);
+        if (isRef(arg))
+          wideInfoMap->get(actual->var)->refsToNarrow.push_back(arg);
       }
     }
 
@@ -1081,6 +1085,7 @@ static void resolveHelper(WideInfo* wi) {
     }
     for_vector(Symbol, se, wi->refsToNarrow) {
       WideInfo* nwi = wideInfoMap->get(se);
+      INT_ASSERT(isRef(nwi->sym));
       DEBUG_PRINTF("REF %s (%d)", nwi->sym->cname, nwi->sym->id);
       if (!nwi->valIsWide) {
         DEBUG_PRINTF(" (%d)\n", wi->sym->id);
@@ -1112,6 +1117,7 @@ static void resolveHelper(WideInfo* wi) {
     // refs to this variable should wrap a narrow _val.
     for_vector(Symbol, se, wi->refsToNarrow) {
       WideInfo* nwi = wideInfoMap->get(se);
+      INT_ASSERT(nwi->sym);
       DEBUG_PRINTF("REF %s (%d): val is ", nwi->sym->cname, nwi->sym->id);
       if (wi->valIsWide) DEBUG_PRINTF("wide");
       else DEBUG_PRINTF("not wide");
