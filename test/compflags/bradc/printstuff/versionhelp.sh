729955ea124dbdeca7b4659baddc52d5239ebd59
Thomas Van Doren <thomas.vandoren@gmail.com>
2015-03-14 12:27:57 -0700

    fix versionhelp.sh so it works for everybody

--- test/compflags/bradc/printstuff/versionhelp.sh
+++ test/compflags/bradc/printstuff/versionhelp.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+CWD=$(cd $(dirname $0) ; pwd)
 compiler=$3
+
 echo -n `basename $compiler`
-cat ${1}.goodstart
-diff ../../../../compiler/main/BUILD_VERSION ./zero.txt > /dev/null 2>&1 && echo "" || \
-    { echo -n "." && cat ../../../../compiler/main/BUILD_VERSION | tr -d \" ; }
+cat $CWD/version.goodstart
+diff $CWD/../../../../compiler/main/BUILD_VERSION $CWD/zero.txt > /dev/null 2>&1 && echo "" || \
+    { echo -n "." && cat $CWD/../../../../compiler/main/BUILD_VERSION | tr -d \" ; }
