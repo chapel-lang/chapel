d6aea66e9dc1b371297cbe3f306112610a99ef2f
Thomas Van Doren <thomas.vandoren@gmail.com>
2015-03-14 12:21:14 -0700

    Add tests for chpldoc --version.
    Repurpose compflags/bradc/printstuff/versionhelp.sh

--- test/compflags/bradc/printstuff/versionhelp.sh
+++ test/compflags/bradc/printstuff/versionhelp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 compiler=$3
 echo -n `basename $compiler`
-cat version.goodstart 
+cat ${1}.goodstart
 diff ../../../../compiler/main/BUILD_VERSION ./zero.txt > /dev/null 2>&1 && echo "" || \
     { echo -n "." && cat ../../../../compiler/main/BUILD_VERSION | tr -d \" ; }
