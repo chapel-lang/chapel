37f23f3d8663c82233c8c750d0c8b93e611cf35a
Michael Ferguson <mferguson@cray.com>
2015-04-27 21:34:43 +0000

    fix version and checkout order

--- third-party/re2/update-re2.sh
+++ third-party/re2/update-re2.sh
@@ -10,10 +10,10 @@ else
 
 echo Checkout RE2
 git clone https://github.com/google/re2 re2
-git checkout 1deddeb2fe8674852fc425f03727e4ab0903357b
+cd re2
+git checkout cc56ba02d9d2bdafa614ad5ebf564dde287625bb
 git checkout -b chapel-branch
 echo Applying Patches
-cd re2
 patch -p1 < ../hg_diff_g.patch
 echo Copying file_strings.h and file_strings.cc
 cp ../file_strings.h re2/
