d5de8a0296a8c3e9cb6328ff7b4de764aef687e5
Phil Nelson <phil@pcnelson.net>
2015-05-20 14:40:32 -0700

    Tweaks to InfoBar, check-in for push

--- tools/chplvis/InfoBar.cxx
+++ tools/chplvis/InfoBar.cxx
@@ -24,51 +24,62 @@
 #include <FL/fl_draw.H>
 #include <FL/x.H>
 
-static const int CR_Left = 25;
+static const int CR_Left = 220;
+
+#ifdef __APPLE__
+static const int Y_OFF = 10;
+#else
+static const int Y_OFF = 0;
+#endif
 
 void InfoBar::draw(void)
 {
+  // dimensions and locations
+  int y_center = y() + (h()-Y_OFF) / 2 + Y_OFF;  // Center line of infobar
+  int cr_h = (h()-Y_OFF) / 2;               // Height of color reference
+  int cr_y = y() + Y_OFF + cr_h / 2;        // Y of color reference
+
   Fl_Box::draw();
 
-#ifdef __APPLE__
-  printf ("InfoBar draw: fl_mac_os_version is %d.\n", fl_mac_os_version);
-#endif
-
+  // Separation line between InfoBar and the display field
+  
   fl_color(FL_BLACK);
   fl_line_style(FL_SOLID, 2, NULL);
-  fl_line(x(),y()+h(),x()+w(),y()+h());
+  fl_line(x(),y()+h()-Y_OFF,x()+w(),y()+h()-Y_OFF);
 
   // Color Reference
   int ix;
-  for (ix = 0; ix < 30; ix++) {
-    fl_color(heatColor(ix+1,30));
+  for (ix = 0; ix <= 30; ix++) {
+    fl_color(heatColor(ix+1,31));
     fl_line_style(FL_SOLID,3,NULL);
-    fl_line(x()+CR_Left+3*ix,y()+5,x()+CR_Left+3*ix,y()+h()-10);
-    if ((ix%3) == 0 && ix != 0) {
+    fl_line(x()+CR_Left+3*ix,cr_y,x()+CR_Left+3*ix,cr_y+cr_h);
+    if ((ix%3) == 0) {
       fl_color(FL_BLACK);
       fl_line_style(FL_SOLID,1,NULL);
-      fl_line(x()+CR_Left+3*ix, y()+h()-9, x()+CR_Left+3*ix, y()+h()-5);
+      fl_line(x()+CR_Left+3*ix, cr_y+cr_h+2, x()+CR_Left+3*ix, cr_y+cr_h+6);
     }
   }
   fl_color(FL_BLACK);
   fl_line_style(FL_SOLID,1,NULL);
-  fl_draw("1", x()+CR_Left-20, y()+5, 15, h()-10, FL_ALIGN_CENTER, NULL, 0);
-  fl_draw("Max", x()+CR_Left+100, y()+5, 15, h()-10, FL_ALIGN_CENTER, NULL, 0);
+  fl_draw("1", x()+CR_Left-20, y_center-20, 15, 40, FL_ALIGN_CENTER, NULL, 0);
+  //  fl_draw("Max", x()+CR_Left+100, y()+5, 15, h()-10, FL_ALIGN_CENTER, NULL, 0);
 
-  // Messages on max counts
+  // Messages on max counts, next to the color reference
   char mesg[150];
   if (maxTasks > 0) {
     if (showtasks)
-      snprintf (mesg, 150, "maxTasks: %d", maxTasks);
+      snprintf (mesg, 150, "max Tasks: %d", maxTasks);
     else
-      snprintf (mesg, 150, "maxCPU: %f", maxCpu);
-    fl_draw(mesg, x()+CR_Left+135, y()+5, 120, h()-10, FL_ALIGN_LEFT, NULL, 0);
+      snprintf (mesg, 150, "max CPU: %f", maxCpu);
+    fl_draw(mesg, x()+CR_Left+105, y_center-20, 120, 20, FL_ALIGN_LEFT, NULL, 0);
     if (showcomms)
-      snprintf (mesg, 150, "maxComms: %d", maxComms);
+      snprintf (mesg, 150, "max Comms: %d", maxComms);
     else
-      snprintf (mesg, 150, "maxData: %ld", maxSize);
-    fl_draw(mesg, x()+330, y()+5, 120, h()-10, FL_ALIGN_LEFT, NULL, 0);
+      snprintf (mesg, 150, "max Data: %ld", maxSize);
+    fl_draw(mesg, x()+CR_Left+105, y_center, 120, 20, FL_ALIGN_LEFT, NULL, 0);
   }
+
+  // Messages about file names / tags and so forth
 }
 
 
