584c85dc6e0cf559a633a116581972a73b05a4da
Michael Ferguson <mppf@users.noreply.github.com>
2015-06-16 17:34:05 -0400

    add boolean type (like integral for bools)

--- compiler/AST/type.cpp
+++ compiler/AST/type.cpp
@@ -1533,6 +1533,9 @@ void initPrimitiveTypes() {
   dtIntegral = createInternalType ("integral", "integral");
   dtIntegral->symbol->addFlag(FLAG_GENERIC);
 
+  dtBoolean = createInternalType ("boolean", "boolean");
+  dtBoolean->symbol->addFlag(FLAG_GENERIC);
+
   dtAnyComplex = createInternalType("chpl_anycomplex", "complex");
   dtAnyComplex->symbol->addFlag(FLAG_GENERIC);
 
