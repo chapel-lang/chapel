22899612bb3be9b71a30db95bee5165133780bb3
Kyle Brady <kbrady@cray.com>
2015-08-18 18:19:12 -0700

    Add two new configs for string-as-rec nightly testing
    We don't have a clean way to do this purely within jenkins at the
    moment, so I have to add these two files.

---
+++ util/cron/test-str-as-rec-gasnet-everything.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Test gasnet (segment everything) against full suite on linux64.
+
+CWD=$(cd $(dirname $0) ; pwd)
+source $CWD/common-gasnet.bash
+
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="string-as-rec-gasnet-everything"
+
+export GASNET_QUIET=Y
+
+# Test a GASNet compile using the default segment (everything for linux64)
+export CHPL_GASNET_SEGMENT=everything
+
+git checkout string-as-rec
+
+$CWD/nightly -cron -futures
