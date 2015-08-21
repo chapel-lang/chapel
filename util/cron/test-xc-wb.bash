b547be0b47a9df163ecb097b6b08a805ead0402f
Elliot Ronaghan <ronawho@gmail.com>
2015-08-21 14:21:27 -0700

    Add temporary prediff for whitebox testing to quiet libu warning
    Since upgrading to cce 8.4 we get a warning from libu saying:
      "WARNING: libu: memcpy AMD cpuid detection failed"
    
    Until this can be resolved, I'm adding a prediff to filter the warning out.

--- util/cron/test-xc-wb.bash
+++ util/cron/test-xc-wb.bash
@@ -7,6 +7,9 @@ CWD=$(cd $(dirname $0) ; pwd)
 source $CWD/functions.bash
 source $CWD/common-whitebox.bash
 
+# quiet libu warning about cpuid detection failure untill it's fixed in CCE 8.4
+export CHPL_SYSTEM_PREDIFF="${CHPL_HOME}/util/test/prediff-for-libu-warning"
+
 # Run the tests!
 nightly_args="-cron"
 log_info "Calling nightly with args: ${nightly_args}"
