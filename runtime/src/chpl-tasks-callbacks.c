89b0eee16916b28aca96aa427b5bee7e3a32e6c3
Greg Titus <gbtitus@users.noreply.github.com>
2015-08-27 15:01:39 -0600

    Move most task callback support into common code.
    The bulk of task callback support, minus common type definitions and
    such, had been implemented separately in tasks=fifo and =qthreads.  Here
    we move most of it into common code.  The functions that users call to
    install and uninstall callbacks, and the one the tasking layers call to
    actually do the callbacks, are in chpl-tasks-callbacks.c.  Definitions
    for the latter and wrappers that can be inlined to reduce overhead are
    in chpl-tasks-callbacks-internal.h.  All of the callback-related code
    except the calls to actually do the callbacks is removed from the
    qthreads and fifo tasking layers.

---
+++ runtime/src/chpl-tasks-callbacks.c
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2004-2015 Cray Inc.
+ * Other additional copyright holders may be indicated within.
+ * 
+ * The entirety of this work is licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * 
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//
+// Shared code for tasking callbacks.
+//
+#include "chplrt.h"
+
+#include "chpl-comm.h"
+#include "error.h"
+#include "chpl-tasks-callbacks.h"
+#include "chpl-tasks-callbacks-internal.h"
+
+
+//
+// Tasking callback support.
+//
+#define MAX_CBS_PER_EVENT 10
+
+static struct cb_info {
+  chpl_task_cb_fn_t fns[MAX_CBS_PER_EVENT];
+  chpl_task_cb_info_kind_t info_kinds[MAX_CBS_PER_EVENT];
+} cb_info[chpl_task_cb_num_event_kinds];
+
+
+//
+// Tasking callback support.
+//
+int chpl_task_install_callback(chpl_task_cb_event_kind_t event_kind,
+                               chpl_task_cb_info_kind_t info_kind,
+                               chpl_task_cb_fn_t cb_fn) {
+  int i;
+
+  if (event_kind >= chpl_task_cb_num_event_kinds) {
+    errno = ERANGE;
+    return -1;
+  }
+
+  i = chpl_task_callback_counts[event_kind];
+
+  if (i >= MAX_CBS_PER_EVENT) {
+    errno = ENOMEM;
+    return -1;
+  }
+
+  chpl_task_callback_counts[event_kind]++;
+
+  cb_info[event_kind].fns[i]= cb_fn;
+  cb_info[event_kind].info_kinds[i] = info_kind;
+
+  return 0;
+}
+
+
+int chpl_task_uninstall_callback(chpl_task_cb_event_kind_t event_kind,
+                                 chpl_task_cb_fn_t cb_fn) {
+  int i;
+  int found_i;
+
+  if (event_kind >= chpl_task_cb_num_event_kinds) {
+    errno = ERANGE;
+    return -1;
+  }
+
+  for (i = 0, found_i = -1; i < chpl_task_callback_counts[event_kind]; i++) {
+    if (cb_info[event_kind].fns[i] == cb_fn) {
+      found_i = i;
+      break;
+    }
+  }
+
+  if (found_i < 0) {
+    errno = ENOENT;
+    return -1;
+  }
+
+  for (i = found_i + 1; i < chpl_task_callback_counts[event_kind]; i++) {
+    cb_info[event_kind].fns[i - 1] = cb_info[event_kind].fns[i];
+    cb_info[event_kind].info_kinds[i - 1] = cb_info[event_kind].info_kinds[i];
+  }
+
+  chpl_task_callback_counts[event_kind]--;
+
+  return 0;
+}
+
+
+void chpl_task_do_callbacks_really(chpl_task_cb_event_kind_t event_kind,
+                                   const char* filename,
+                                   int lineno,
+                                   uint64_t id,
+                                   int is_executeOn) {
+  struct cb_info* cbp;
+  chpl_task_cb_info_t info;
+  int i;
+
+  cbp = &cb_info[event_kind];
+
+  info.nodeID = chpl_nodeID;
+  info.event_kind = event_kind;
+
+  for (i = 0; i < chpl_task_callback_counts[event_kind]; i++) {
+    info.info_kind = cbp->info_kinds[i];
+
+    switch (cbp->info_kinds[i]) {
+    case chpl_task_cb_info_kind_full:
+      info.iu.full.filename = filename;
+      info.iu.full.lineno = lineno;
+      info.iu.full.id = id;
+      info.iu.full.is_executeOn = is_executeOn;
+      break;
+
+    case chpl_task_cb_info_kind_id_only:
+      info.iu.id_only.id = id;
+      break;
+
+    default:
+      chpl_internal_error("bad callback info kind");
+      break;
+    }
+
+    (*cbp->fns[i])((const chpl_task_cb_info_t*) &info);
+  }
+}
