fb95b6236c4b247d816eae0ea70227c190bad074
Brad Chamberlain <bradcray@users.noreply.github.com>
2015-09-11 15:41:52 -0700

    Rearrange the components of an internal error
    As others have commented, the logic around printing out errors and
    warnings is completely spaghetti.  I thought that reordering the
    elements of the internal error messages would be trivial, but it's
    not and has taken me awhile to reach this point, so I'm pausing
    to catch my breath before going on.

--- compiler/util/misc.cpp
+++ compiler/util/misc.cpp
@@ -120,8 +120,6 @@ print_user_internal_error() {
   char version[128];
   get_version(version);
   fprintf(stderr, "chpl Version %s\n", version);
-  if (err_fatal)
-    clean_exit(1);
 }
 
 
@@ -163,8 +161,8 @@ setupError(const char *filename, int lineno, int tag) {
 }
 
 
-static void
-printDevelErrorHeader(BaseAST* ast) {
+static bool
+printErrorHeader(BaseAST* ast) {
   if (!err_print) {
     if (Expr* expr = toExpr(ast)) {
       Symbol* parent = expr->parentSymbol;
@@ -221,25 +219,7 @@ printDevelErrorHeader(BaseAST* ast) {
     }
   }
 
-  bool apologize = !err_user;
   bool guess = filename && !have_ast_line;
-
-  if ( apologize ) {
-    fprintf(stderr, " Unfortunately the Chapel compiler has encountered\n"
-                    " an internal error. Please file a bug report that\n"
-                    " includes a program reproducing the problem as well\n"
-                    " as this output. See %s for\n"
-                    " further instructions on filing bug reports.\n"
-                    "\n", help_url);
-    if ( guess ) {
-      // Print out our best guess for the location of an error
-      // if we had no source location
-      fprintf(stderr, " The error may be related to this location:\n"
-                      " %s:%d\n",
-                      filename, linenum);
-    }
-  }
-
 
   // TODO: indicate that the file/line is a guess if
   //  (err_user && filename && guess && !developer)
@@ -247,9 +227,6 @@ printDevelErrorHeader(BaseAST* ast) {
 
   if (filename) {
     fprintf(stderr, "%s:%d: ", filename, linenum);
-    if( developer && guess ) {
-      fprintf(stderr, "[source location guessed] ");
-    }
   }
 
   if (err_print) {
@@ -264,15 +241,29 @@ printDevelErrorHeader(BaseAST* ast) {
     fprintf(stderr, "warning: ");
   }
 
-  if (!err_user && !developer) {
-    print_user_internal_error();
+  if (!err_user) {
+    if (!developer) {
+      print_user_internal_error();
+    }
   }
-
+  return guess;
 }
 
-static void printDevelErrorFooter(void) {
+
+static void printErrorFooter(bool guess) {
+  fprintf(stderr, "I'm in!");
   if (developer)
     fprintf(stderr, " [%s:%d]", err_filename, err_lineno);
+
+  if (guess) {
+    fprintf(stderr, "\n(this source location is a guess)");
+  }
+  if (!developer) {
+    fprintf(stderr, "\nThis is the new apology message which references %s\n", 
+            help_url);
+  }
+  if (!err_user && !developer && err_fatal)
+    clean_exit(1);
 }
 
 
@@ -333,22 +324,28 @@ void handleError(const char *fmt, ...) {
   if (err_ignore)
     return;
 
-  printDevelErrorHeader(NULL);
+  bool guess = printErrorHeader(NULL);
 
-  if (!err_user && !developer)
-    return;
+  //
+  // Only print out the arguments if this is a user error or we're
+  // in developer mode.
+  //
+  if (err_user || developer) {
+    va_list args;
 
-  va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+  }
 
-  va_start(args, fmt);
-  vfprintf(stderr, fmt, args);
-  va_end(args);
-
-  printDevelErrorFooter();
+  printErrorFooter(guess);
 
   fprintf(stderr, "\n");
 
   printCallStackOnError();
+
+  if (!err_user && !developer)
+    return;
 
   if (exit_immediately) {
     if (ignore_errors_for_pass) {
@@ -380,24 +377,27 @@ static void vhandleError(FILE* file, BaseAST* ast, const char *fmt, va_list args
   if (err_ignore)
     return;
 
+  bool guess = false;
   if (file == stderr)
-    printDevelErrorHeader(ast);
+    guess = printErrorHeader(ast);
 
-  if (!err_user && !developer)
-    return;
-
-  vfprintf(file, fmt, args);
+  if (err_user || developer) {
+    vfprintf(file, fmt, args);
+  }
 
   if (fPrintIDonError && ast)
     fprintf(file, " [%d]", ast->id);
 
   if (file == stderr)
-    printDevelErrorFooter();
+    printErrorFooter(guess);
 
   fprintf(file, "\n");
 
   if (file == stderr)
     printCallStackOnError();
+
+  if (!err_user && !developer)
+    return;
 
   if (exit_immediately) {
     if (ignore_errors_for_pass) {
