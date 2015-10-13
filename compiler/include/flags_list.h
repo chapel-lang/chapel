5e75ae0bb5e6cc0915800b0c676812331a95917b
Michael Ferguson <mppf@users.noreply.github.com>
2015-10-13 15:50:29 -0400

    Put new flags in alphabetical order

--- compiler/include/flags_list.h
+++ compiler/include/flags_list.h
@@ -104,16 +104,6 @@ symbolFlag( FLAG_FUNCTION_PROTOTYPE , npr, "function prototype" , "signature for
 symbolFlag( FLAG_GENERIC , npr, "generic" , "generic types, functions and arguments" )
 symbolFlag( FLAG_GLOBAL_TYPE_SYMBOL, npr, "global type symbol", "is accessible through a global type variable")
 symbolFlag( FLAG_HAS_RUNTIME_TYPE , ypr, "has runtime type" , "type that has an associated runtime type" )
-
-// Is this type a Plain-Old Data (POD) type - ie no autocopy/destructor/=
-// need ever be called - bit copies will do.
-// Note that FLAG_POD and FLAG_NOT_POD can only be relied upon
-// after resolution. During resolution, use the propagateNotPOD
-// function. After resolution, use isPOD() to find out if something
-// is a POD type.
-symbolFlag( FLAG_POD , ypr, "plain old data" , "data can be bit copied")
-symbolFlag( FLAG_NOT_POD , ypr, "not plain old data" , "bit copy overridden")
-
 symbolFlag( FLAG_HEAP , npr, "heap" , ncm )
 symbolFlag( FLAG_IMPLICIT_ALIAS_FIELD , npr, "implicit alias field" , ncm )
 symbolFlag( FLAG_INDEX_VAR , npr, "index var" , ncm )
@@ -174,6 +164,10 @@ symbolFlag( FLAG_NO_PARENS , npr, "no parens" , "function without parentheses" )
 symbolFlag( FLAG_NO_PROTOTYPE , ypr, "no prototype" , "do not generate a prototype this symbol" )
 symbolFlag( FLAG_NO_WIDE_CLASS , ypr, "no wide class" , ncm )
 symbolFlag( FLAG_NO_REMOTE_MEMORY_FENCE , ypr, "no remote memory fence" , ncm)
+
+// See FLAG_POD below
+symbolFlag( FLAG_NOT_POD , ypr, "not plain old data" , "bit copy overridden")
+
 symbolFlag( FLAG_OBJECT_CLASS , npr, "object class" , ncm )
 symbolFlag( FLAG_OMIT_FROM_CONSTRUCTOR , ypr, "omit from constructor" , ncm )
 
@@ -201,6 +195,15 @@ symbolFlag( FLAG_PARAM , npr, "param" , "parameter (compile-time constant)" )
 
 symbolFlag( FLAG_PARTIAL_COPY, npr, "partial copy", ncm )
 symbolFlag( FLAG_PARTIAL_TUPLE, npr, "partial tuple", ncm)
+
+// Is this type a Plain-Old Data (POD) type - ie no autocopy/destructor/=
+// need ever be called - bit copies will do.
+// Note that FLAG_POD and FLAG_NOT_POD can only be relied upon
+// after resolution. During resolution, use the propagateNotPOD
+// function. After resolution, use isPOD() to find out if something
+// is a POD type.
+symbolFlag( FLAG_POD , ypr, "plain old data" , "data can be bit copied")
+
 
 symbolFlag( FLAG_PRIMITIVE_TYPE , ypr, "primitive type" , "attached to primitive types to keep them from being deleted" )
 symbolFlag( FLAG_PRINT_MODULE_INIT_FN , ypr, "print module init fn" , ncm )
