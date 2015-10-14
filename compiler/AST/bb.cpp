8cdf54043a9159fd36b69436f8680bfca6bfcf62
Michael Noakes <mnoakes@cray.com>
2015-10-14 16:10:09 -0700

    Update removeEmptyBlocks to invoke ::remove() and then actually delete the bb

--- compiler/AST/bb.cpp
+++ compiler/AST/bb.cpp
@@ -369,20 +369,12 @@ void BasicBlock::removeEmptyBlocks(FnSymbol* fn) {
     if (bb->ins.size() == 0 && bb->exprs.size() == 0) {
       // This block will be removed.  It is no longer a predecessor of anyone,
       // so we must update the back links.
-      for_vector(BasicBlock, succ, bb->outs) {
-        BasicBlockVector::iterator i;
+      bb->remove();
 
-        for (i = succ->ins.begin(); i != succ->ins.end(); ++i)
-          if (*i == bb)
-            break;
-
-        INT_ASSERT(i != succ->ins.end());
-
-       succ->ins.erase(i);
-      }
-
+      delete bb;
     } else {
       bb->id = newId++;
+
       newBlocks->push_back(bb);
     }
   }
@@ -454,6 +446,7 @@ bool BasicBlock::isOK() {
 
   return true;
 }
+
 
 // This routine removes unreachable (interior) blocks from the flow graph
 // without modifying the underlying AST.  It is a workaround for the fact that
