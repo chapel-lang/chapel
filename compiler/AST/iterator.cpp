b72bab20f7eb0e0bc38989358443aa0b2ed8274a
Vassily Litvinov <vasslitvinov@users.noreply.github.com>
2015-10-19 19:33:32 -0700

    Reverted a change I made too optimistically in 9f93452

--- compiler/AST/iterator.cpp
+++ compiler/AST/iterator.cpp
@@ -225,9 +225,12 @@ static void replaceLocalWithFieldTemp(SymExpr*       se,
 
   if (call && call->isPrimitive(PRIM_GET_MEMBER)) {
     // Get member returns the address of the member, so we convert the
-    // type of the corresponding temp to a reference type.
-    INT_ASSERT(tmp->type->refType);
-    tmp->type = tmp->type->refType;
+    // type of the corresponding temp to a reference type if necessary.
+    if (! tmp->type->symbol->hasFlag(FLAG_REF))
+    {
+      INT_ASSERT(tmp->type->refType);
+      tmp->type = tmp->type->refType;
+    }
   }
 
   // OK, insert the declaration.
