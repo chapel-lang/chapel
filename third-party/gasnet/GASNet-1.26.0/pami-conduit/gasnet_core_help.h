2a9bc3330b0b2309f6076b660ebdf571f9c8ea65
Greg Titus <gbtitus@users.noreply.github.com>
2015-12-14 18:49:50 -0600

    Add GASNet 1.26.0.

---
+++ third-party/gasnet/GASNet-1.26.0/pami-conduit/gasnet_core_help.h
@@ -0,0 +1,20 @@
+/*   $Source: bitbucket.org:berkeleylab/gasnet.git/pami-conduit/gasnet_core_help.h $
+ * Description: GASNet PAMI conduit core Header Helpers (Internal code, not for client use)
+ * Copyright 2012, Lawrence Berkeley National Laboratory
+ * Terms of use are as specified in license.txt
+ */
+
+#ifndef _IN_GASNET_H
+  #error This file is not meant to be included directly- clients should include gasnet.h
+#endif
+
+#ifndef _GASNET_CORE_HELP_H
+#define _GASNET_CORE_HELP_H
+
+GASNETI_BEGIN_EXTERNC
+
+#include <gasnet_help.h>
+
+GASNETI_END_EXTERNC
+
+#endif
