aab61b360982859b3eac8e66c91c79a65f5a116f
Elliot Ronaghan <ronawho@gmail.com>
2015-12-18 13:14:24 -0500

    Fixes from Greg's feedback:
     - Remove comments/questions in chpl_3p_jemalloc_configs.py that Greg answered
     - Remove CRAYPE_LINK_TYPE=dynamic, this was leftover from the tcmalloc one
     - Update link to source code (point to github, not landing page)

--- util/chplenv/chpl_3p_jemalloc_configs.py
+++ util/chplenv/chpl_3p_jemalloc_configs.py
@@ -14,16 +14,7 @@ def get_uniq_cfg_path():
 
 @memoize
 def get_link_args():
-    # -lpthread required to satisfy "undefined reference to `pthread_atfork"
-    # and others. Note that this only appears to be needed because we build
-    # jemalloc statically. When using a .so the -lpthread is not required.
-    # @gbt any idea why that is/anything to change or is this ok?
+    # We have to manually link libpthread since we build statically and .a's
+    # don't have a way to specify dependencies.
     return third_party_utils.default_get_link_args('jemalloc',
                                                    libs=['-ljemalloc', '-lpthread'])
-
-# TODO (EJR 12/17/15): should we be manually linking the .a like  we do for
-# tcmalloc? i.e: `libs=['libjemalloc.a'])`
-#
-# If the answer is yes, then we'll have to update third_party_utils.py to
-# search for .a's just like we do for .la's.
-# @gbt any thoughts?
