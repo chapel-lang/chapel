3b7203b9e450f6f9cd6cde6a47b7562a936731d4
Michael Ferguson <mppf@users.noreply.github.com>
2016-01-05 17:10:42 -0500

    Revert "Add dtBoolean"
    This reverts commit cfebdccef4da4a541a34755c63467e79f28dfaeb.

--- compiler/AST/type.cpp
+++ compiler/AST/type.cpp
@@ -1565,9 +1565,6 @@ void initPrimitiveTypes() {
   dtIntegral = createInternalType ("integral", "integral");
   dtIntegral->symbol->addFlag(FLAG_GENERIC);
 
-  dtBoolean = createInternalType ("boolean", "boolean");
-  dtBoolean->symbol->addFlag(FLAG_GENERIC);
-
   dtAnyComplex = createInternalType("chpl_anycomplex", "complex");
   dtAnyComplex->symbol->addFlag(FLAG_GENERIC);
 
