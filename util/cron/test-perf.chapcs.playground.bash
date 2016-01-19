6af8d72a9812c3af999ed0aa910d54f9989ef8f4
Elliot Ronaghan <ronawho@gmail.com>
2016-01-18 17:37:15 -0800

    Update the playground's startdate

--- util/cron/test-perf.chapcs.playground.bash
+++ util/cron/test-perf.chapcs.playground.bash
@@ -10,7 +10,7 @@ source $CWD/common-perf.bash
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.chapcs.playground"
 
 perf_args="-performance-description noSlice -performance-configs default:v,noSlice:v -sync-dir-suffix noSlice"
-perf_args="${perf_args} -performance -numtrials 5 -startdate 01/19/16"
+perf_args="${perf_args} -performance -numtrials 5 -startdate 01/18/16"
 perf_args="${perf_args} -compopts -sassertNoSlicing"
 
 $CWD/nightly -cron ${nightly_args} ${perf_args}
