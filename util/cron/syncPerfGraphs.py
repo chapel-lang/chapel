91f0e797eb4821e29ff281435556ef0eb1d587c4
Elliot Ronaghan <ronawho@gmail.com>
2016-01-20 13:34:34 -0800

    Use string.format in syncPerfGraphs script
    use code like `'{0}'.format(var)` in syncPerfGraphs instead of the older
    `'%s'%(var)` or just plain old string concatenation.
    
    This is just cleanup in preparation of some other changes

--- util/cron/syncPerfGraphs.py
+++ util/cron/syncPerfGraphs.py
@@ -44,26 +44,28 @@ def main():
 # file wasn't found in the directory that is being synced
 def syncToSourceForge(dirToSync, destDir, logFile):
 
-    logFile.write('SF sync log for: ' + time.strftime("%m/%d/%Y") + '\n\n')
+    logFile.write('SF sync log for: {0} \n\n'.format(time.strftime("%m/%d/%Y")))
 
     successFile = os.path.join(dirToSync, 'SUCCESS')
 
     if not os.path.isfile(successFile):
-        logFile.write('SUCCESS file did not exist in %s. Assuming genGraph was '
-          'unsuccessful. Graphs will NOT be synced.\n' % dirToSync)
+        logFile.write('SUCCESS file did not exist in {0}. Assuming genGraph was '
+          'unsuccessful. Graphs will NOT be synced.\n'.format(dirToSync))
         return 124
 
     # Assumes correct username and authentication for web.sourceforge.net is
     # configured for the current system.
-    sfBaseDest = 'web.sourceforge.net:/home/project-web/chapel/htdocs/perf/'
-    sfPerfDest = posixpath.join(sfBaseDest, destDir)
+    sfHost = 'web.sourceforge.net'
+    sfPerfBaseDir = '/home/project-web/chapel/htdocs/perf/'
+    sfPerfDir = posixpath.join(sfPerfBaseDir, destDir)
+    rsyncDest = '{0}:{1}'.format(sfHost, sfPerfDir)
 
-    logFile.write('Attempting to rsync %s to %s\n'%(dirToSync, sfPerfDest))
+    logFile.write('Attempting to rsync {0} to {1}\n'.format(dirToSync, rsyncDest))
 
     # The rsync command that we will execute -- authenticates over ssh
     # --del to remove any old data (graphs merged, changed names, removed etc)
-    rsyncCommand = 'rsync -avz --del -e ssh ' + dirToSync + ' ' + sfPerfDest
-    logFile.write('Sync to sourceforge command was: ' + rsyncCommand + '\n\n')
+    rsyncCommand = 'rsync -avz -e ssh {0} {1}'.format(dirToSync, rsyncDest)
+    logFile.write('Sync to sourceforge command was: {0}\n\n'.format(rsyncCommand))
     logFile.write('Rsync output is: \n')
     logFile.flush()
 
