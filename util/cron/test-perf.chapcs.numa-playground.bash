e0926d4e5228d214699bc54ec50d41eb3d025246
Elliot Ronaghan <ronawho@gmail.com>
2016-01-22 17:08:16 -0800

    Reduce numa timeout from 20 minutes to 10 and bump to 3 trials
    With #3146, numa performance testing finishes much faster. Looking at the log,
    there's only a few tests that take more than 5 minutes:
    
    studies/shootout/nbody/bradc/nbody-blc-slice:     ~9.5 min
    studies/shootout/nbody/sidelnik/nbody_forloop_3:  ~9.0 min
    release/examples/benchmarks/hpcc/ptrans:          ~6.0 min
    release/examples/benchmarks/hpcc/ra-atomics       ~5.1 min

--- util/cron/test-perf.chapcs.numa-playground.bash
+++ util/cron/test-perf.chapcs.numa-playground.bash
@@ -10,7 +10,7 @@ source $CWD/common-numa.bash
 
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.chapcs.numa-playground"
 
-export CHPL_TEST_TIMEOUT=1200
+export CHPL_TEST_TIMEOUT=600
 
 
 # Test performance of numa with work stealing turned off
@@ -28,5 +28,5 @@ git checkout -b $GITHUB_USER-$GITHUB_BRANCH
 git pull https://github.com/$GITHUB_USER/chapel.git $GITHUB_BRANCH
 
 perf_args="-performance-description $SHORT_NAME -performance-configs default:v,numa:v,$SHORT_NAME:v -sync-dir-suffix $SHORT_NAME"
-perf_args="${perf_args} -numtrials 1 -startdate $START_DATE"
+perf_args="${perf_args} -numtrials 3 -startdate $START_DATE"
 $CWD/nightly -cron ${perf_args} ${nightly_args}
