c8d88ceb7131528f7eed765fec85e55c339687f9
Elliot Ronaghan <ronawho@gmail.com>
2016-02-10 11:13:36 -0800

    Convert fifo testing to quickstart testing
    Change our fifo and gasnet.fifo testing to quickstart and gasnet.quickstart
    testing. Currently this doesn't have much of am impact, but it will when say
    jemalloc is made the default.
    
    This also reduces gasnet.quickstart testing to only test multilocale instead of
    the full suite since we don't really need the full coverage, just some sanity
    checks.
    
    Note that valgrind still just uses fifo since I expect valgrind to work out of
    the box with jemalloc.

--- util/cron/test-gasnet.quickstart.bash
+++ util/cron/test-gasnet.quickstart.bash
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 #
-# Test fifo + gasnet configuration on full suite on linux64.
+# Test quickstart + gasnet configuration on full suite on linux64.
 
 CWD=$(cd $(dirname $0) ; pwd)
 source $CWD/common.bash
-source $CWD/common-fifo.bash
-source $CWD/common-gasnet.bash
+source $CWD/common-quickstart.bash
+source $CWD/common-gasnet.bash # must come after quickstart source
 
-export CHPL_NIGHTLY_TEST_CONFIG_NAME="gasnet.fifo"
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="gasnet.quickstart"
 
 export GASNET_QUIET=Y
 
-$CWD/nightly -cron
+$CWD/nightly -cron -multilocale
