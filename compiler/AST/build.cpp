4b09a8f53082248eb387d9384f156b96fa73760f
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2016-02-17 11:12:44 -0800

    Change warning in build about duplicate new names to a fatal error
    This would cause a naming conflict, but we can't/likely shouldn't store that
    in the map just to check it later, so generate an error immediately.

--- compiler/AST/build.cpp
+++ compiler/AST/build.cpp
@@ -445,7 +445,7 @@ BlockStmt* buildUseStmt(Expr* mod, std::vector<OnlyRename*>* names, bool except)
           if (renameMap.count(new_name->unresolved) == 0) {
             renameMap[new_name->unresolved] = old_name->unresolved;
           } else {
-            USR_WARN(elem->first, "already renamed '%s' to '%s', ignoring '%s'", renameMap[new_name->unresolved], new_name->unresolved, old_name->unresolved);
+            USR_FATAL_CONT(elem->first, "already renamed '%s' to '%s'", renameMap[new_name->unresolved], new_name->unresolved);
           }
         } else {
           useListError(elem->first, except);
