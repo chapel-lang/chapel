b8d5eb2e552eeec189192b205d14a79005631277
Greg Titus <gbtitus@users.noreply.github.com>
2016-03-18 11:07:01 -0600

    Support lldb more explicitly; don't assume xterm path.
    (Theses changes reflect very helpful review feedback -- thanks!)
    
    The previous work assumed that Mac users would always want to use lldb.
    This isn't the case; some will want to use gdb instead.  Here, add
    support for a CHPL_COMM_USE_LLDB environment variable to specify the use
    of lldb instead of gdb.  A programmer selects which debugger to use by
    setting either the old CHPL_COMM_USE_GDB environment variable or this
    new one.
    
    Also, replace the use of fixed paths for the xterm executable with a
    PATH-based search analogous to the one a shell would use.  (We don't
    have to do the same for the gdb or lldb executable because the shell
    running in the xterm will handle that.  But GASNet will use an execve(2)
    to run the xterm, and that only accepts a full path; it doesn't do a
    search.

--- third-party/gasnet/GASNet-1.26.0/other/amudp/amudp_spmd.cpp
+++ third-party/gasnet/GASNet-1.26.0/other/amudp/amudp_spmd.cpp
@@ -558,35 +558,57 @@ extern int AMUDP_SPMDStartup(int *argc, char ***argv,
 
     //
     // For ease-of-debugging with Chapel, hacked this so that GASNet
-    // will launch each task in an xterm running gdb if
+    // will launch each task in an xterm running a debugger if
     // CHPL_COMM_USE_GDB is set.
     //
-    if (getenv("CHPL_COMM_USE_GDB")) {
-      //
-      // Create a new argv with space for -E options for the env vars.
-      //
-      int new_argc = 4 + *argc + 1;
-      char** new_argv = (char**) AMUDP_malloc(new_argc * sizeof((*new_argv)));
+    if (getenv("CHPL_COMM_USE_GDB") != NULL
+        || getenv("CHPL_COMM_USE_LLDB") != NULL) {
+      char xtermPath[1 << 12];
+      char *pp, *ppn;
+      int new_argc = 4 + *argc;
+      char** new_argv
+             = (char**) AMUDP_malloc((new_argc + 1) * sizeof((*new_argv)));
 
-      //
-      // Instead of running the program directly, run an xterm which
-      // runs lldb on the program and its args.
-      //
-#if defined __APPLE__
-      new_argv[0] = (char *) "/opt/X11/bin/xterm";
-      new_argv[1] = (char *) "-e";
-      new_argv[2] = (char *) "lldb";
-      new_argv[3] = (char *) "--";
-#else
-      new_argv[0] = (char *) "/usr/bin/xterm";
-      new_argv[1] = (char *) "-e";
-      new_argv[2] = (char *) "gdb";
-      new_argv[3] = (char *) "--args";
-#endif
-      memcpy(&new_argv[4], *argv, (*argc + 1) * sizeof(*new_argv));
+      if ((pp = getenv("PATH")) == NULL || *pp == '\0')
+        AMUDP_FatalErr("CHPL_COMM_USE_(G|LL)DB: no PATH.  Exiting...");
+      else {
+        do {
+          ppn = strchr(pp, ':');
+          if (ppn == NULL)
+            (void) snprintf(xtermPath, sizeof(xtermPath), "%s/xterm",
+                            (*pp == '\0') ? "." : pp);
+          else if (ppn == pp)
+            (void) snprintf(xtermPath, sizeof(xtermPath), "./xterm");
+          else
+            (void) snprintf(xtermPath, sizeof(xtermPath), "%.*s/xterm",
+                            (int) (ppn - pp), pp);
+          if (access(xtermPath, X_OK) == 0)
+            break;
+          xtermPath[0] = '\0';
+          pp = ppn + 1;
+        } while (ppn != NULL);
 
-      *argc = new_argc;
-      *argv = new_argv;
+        if (xtermPath[0] == '\0')
+          AMUDP_FatalErr("CHPL_COMM_USE_(G|LL)DB: no xterm in PATH.  "
+                         "Exiting...");
+        else {
+          new_argv[0] = (char*) AMUDP_malloc(strlen(xtermPath) + 1);
+          strcpy(new_argv[0], xtermPath);
+          new_argv[1] = (char *) "-e";
+
+          if (getenv("CHPL_COMM_USE_GDB") != NULL) {
+            new_argv[2] = (char *) "gdb";
+            new_argv[3] = (char *) "--args";
+          } else {
+            new_argv[2] = (char *) "lldb";
+            new_argv[3] = (char *) "--";
+          }
+
+          memcpy(&new_argv[4], *argv, (*argc + 1) * sizeof(*new_argv));
+          *argc = new_argc;
+          *argv = new_argv;
+        }
+      }
     }
 
     // call system-specific spawning routine
