3b13b68720ab163fc2ab16440c178f17e102f165
Ben Albrecht <benalbrecht@pitt.edu>
2016-04-25 13:41:34 -0500

    Increase proc/mount path name length from 512 -> 2048
    CLE6 support

--- third-party/hwloc/hwloc-1.10.1/src/topology-linux.c
+++ third-party/hwloc/hwloc-1.10.1/src/topology-linux.c
@@ -1674,7 +1674,7 @@ hwloc_strdup_mntpath(const char *escapedpath, size_t length)
 static void
 hwloc_find_linux_cpuset_mntpnt(char **cgroup_mntpnt, char **cpuset_mntpnt, int fsroot_fd)
 {
-#define PROC_MOUNT_LINE_LEN 512
+#define PROC_MOUNT_LINE_LEN 2048
   char line[PROC_MOUNT_LINE_LEN];
   FILE *fd;
 
