d78579ca690695ccca9e8e44e1ccccb8760576e6
Nikhil Padmanabhan <nikhil.padmanabhan@gmail.com>
2016-04-29 02:18:31 +0000

    Cleanup extraneous files, move MPI to modules/packages, and update Makefile to reflect that

--- test/users/npadmana/mpi/c_bindings_1.1/mpi_dummy.h
+++
@@ -1,8 +0,0 @@
-typedef int MPI_Datatype;
-typedef int MPI_Comm;
-typedef int MPI_Op;
-typedef int MPI_Status;
-typedef int MPI_Request;
-typedef int MPI_Aint;
-typedef int MPI_Errhandler;
-typedef int MPI_Group;
