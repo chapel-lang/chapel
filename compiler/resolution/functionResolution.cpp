4d1d5ab6bb2481a4377c78994e82750868ba2d33
Michael Ferguson <mppf@users.noreply.github.com>
2016-07-25 14:18:58 -0400

    Improve debug output, comments

--- compiler/resolution/functionResolution.cpp
+++ compiler/resolution/functionResolution.cpp
@@ -3903,7 +3903,7 @@ FnSymbol* tryResolveCall(CallExpr* call) {
 FnSymbol* resolveNormalCall(CallExpr* call, bool checkonly) {
 
   if( call->id == breakOnResolveID ) {
-    printf("breaking on call:\n");
+    printf("breaking on resolve call:\n");
     print_view(call);
     gdbShouldBreakHere();
   }
