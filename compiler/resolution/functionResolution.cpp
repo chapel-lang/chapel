7b009c19fbaf4fd4c8cd7ca385eb2965d346921b
Michael Ferguson <mppf@users.noreply.github.com>
2016-07-28 16:29:05 -0400

    Allow breakOnResolveID to break on setting type of VarSymbol

--- compiler/resolution/functionResolution.cpp
+++ compiler/resolution/functionResolution.cpp
@@ -4609,8 +4609,12 @@ static void resolveMove(CallExpr* call) {
     }
   }
 
-  if (lhs->type == dtUnknown || lhs->type == dtNil)
+  if (lhs->type == dtUnknown || lhs->type == dtNil) {
+    if (lhs->id == breakOnResolveID )
+      gdbShouldBreakHere();
+
     lhs->type = rhsType;
+  }
 
   Type* lhsType = lhs->type;
 
