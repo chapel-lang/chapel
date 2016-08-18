4982d4a6f194c875f51b58765e70294834cb900c
Tony Wallace <awallace@cray.com>
2016-08-17 20:59:10 -0700

    add new cron scripts to test gcc 4.7, 4.9, 5.4, and 6.1

---
+++ util/cron/test-linux64-gcc61.examples.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Test default configuration on examples only, on linux64, with compiler gcc-6.1
+
+CWD=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
+
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="linux64-gcc61.examples"
+
+gcc_setup=/data/cf/chapel/setup_gcc61.bash
+if source "${gcc_setup}"
+then
+    source $CWD/common.bash
+    $CWD/nightly -cron -examples ${nightly_args}
+else
+    echo >&2 "error in ${BASH_SOURCE[0]}: source ${gcc_setup}"
+    exit 2
+fi
