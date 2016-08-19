caf9315eb90df3ace7080d19f576bcae63fa60a4
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2016-08-19 16:06:08 -0700

    Add logic to resolveNormalCall to support super.init() when super == object, etc
    Prior to this change, I had a ton of failures which were due to object's
    default constructor being _construct_object, when super.init() calls within the
    body of initializers that inherited from it expected to have a init() function
    to call.  Similarly, if the class being inherited from did not define any
    constructor or initializer, we would encounter this issue.  This problem is a
    temporary one, due to not having initializers implemented in a strong enough
    state that they can be used as the compiler generated default for a class.  So
    I inserted a temporary replacement that occurs when the type having init called
    upon it did not define either an initializer or a constructor (and an error when
    the type having init called upon it defined a constructor).
    
    With this change, 17 new initializer futures now pass.

--- compiler/resolution/functionResolution.cpp
+++ compiler/resolution/functionResolution.cpp
@@ -3908,6 +3908,36 @@ FnSymbol* resolveNormalCall(CallExpr* call, bool checkonly) {
     gdbShouldBreakHere();
   }
 
+  if (UnresolvedSymExpr* usym = toUnresolvedSymExpr(call->baseExpr)) {
+    // Support super.init() calls (for instance) when the super type does not
+    // define either an initializer or a constructor.  Also ignores errors from
+    // improperly inserted .init() calls (so be sure to check here if something
+    // is behaving oddly - Lydia, 08/19/16)
+    if (!strcmp(usym->unresolved, "init")) {
+      for_actuals(actual, call) {
+        if (NamedExpr* named = toNamedExpr(actual)) {
+          if (!strcmp(named->name, "meme")) {
+            if (SymExpr* sym = toSymExpr(named->actual)) {
+              if (AggregateType* ct = toAggregateType(sym->var->type)) {
+                if (ct->initializerStyle == DEFINES_NONE_USE_DEFAULT) {
+                  // This code should be removed when the compiler generates
+                  // initializers as the default method of construction and
+                  // initialization for a type (Lydia note, 08/19/16)
+                  usym->unresolved = astr("_construct_", ct->symbol->name);
+                } else if (ct->initializerStyle == DEFINES_CONSTRUCTOR) {
+                  // This code should be removed when initializers are fully
+                  // supported and old style constructors are deprecated
+                  // (Lydia note, 08/19/16)
+                  USR_FATAL(call, "can't make init call on type with old constructor style");
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
   resolveDefaultGenericType(call);
 
   CallInfo info(call, checkonly);
