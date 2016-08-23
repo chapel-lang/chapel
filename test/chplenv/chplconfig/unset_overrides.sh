6f789f003174f5524d75052ed67598d716b55880
Ben Albrecht <benalbrecht@pitt.edu>
2016-08-23 09:11:49 -0700

    Use full path to printchplenv, add gitignores for goods

--- test/chplenv/chplconfig/unset_overrides.sh
+++ test/chplenv/chplconfig/unset_overrides.sh
@@ -1,5 +1,9 @@
-# Unset any env vars currently set
-CHPL_OVERRIDES=$(printchplenv --overrides | awk -F'=' '{print $1}')
+# Modify environment, removing any non-home CHPL_* vars and add $PRINTCHPLENV
+
+export PRINTCHPLENV=$CHPL_HOME/util/printchplenv
+
+CHPL_OVERRIDES=$(${PRINTCHPLENV} --overrides | awk -F'=' '{print $1}')
+
 for CHPL_VAR in ${CHPL_OVERRIDES}; do
     if [ ${CHPL_VAR} != "CHPL_HOME" ]; then
         unset ${CHPL_VAR}
