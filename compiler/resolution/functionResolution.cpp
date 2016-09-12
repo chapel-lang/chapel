7ce26e0c5133b0a23737177f4436128d5ef89480
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2016-09-12 16:52:46 -0500

    Revert "Merge pull request #4476 from lydia-duncan/defaultModule"
    This reverts commit 05c25d1b7da57d3063887780c075ddb6faf37fbe, reversing
    changes made to 2ca76c10b604dddaff614388f1f5a665228b7f61.
    
    Though this implementation is buggy, we aren't willing to accept the 10% slow
    down in compilation that it yields.  Ideally, this optimization will be fixed
    so that it will be more closely tied to the operation it is trying to mimic,
    but I don't think there is time to do that before the release.  Additionally,
    we think that Ben's work to limit the scope of certain uses will lower the
    likelihood of encountering this bug to the point where it is livable for the
    time being (although the List module will still encounter this problem).

--- compiler/resolution/functionResolution.cpp
+++ compiler/resolution/functionResolution.cpp
@@ -367,6 +367,7 @@ static void buildVirtualMaps();
 static void
 addVirtualMethodTableEntry(Type* type, FnSymbol* fn, bool exclusive = false);
 static void resolveTypedefedArgTypes(FnSymbol* fn);
+static void computeStandardModuleSet();
 static void unmarkDefaultedGenerics();
 static void resolveUses(ModuleSymbol* mod);
 static void resolveExports();
@@ -3211,6 +3212,11 @@ static void buildVisibleFunctionMap() {
         block = theProgram->block;
       } else {
         block = getVisibilityBlock(fn->defPoint);
+        //
+        // add all functions in standard modules to theProgram
+        //
+        if (standardModuleSet.set_in(block))
+          block = theProgram->block;
       }
       VisibleFunctionBlock* vfb = visibleFunctionMap.get(block);
       if (!vfb) {
@@ -3234,6 +3240,12 @@ getVisibleFunctions(BlockStmt* block,
                     Vec<FnSymbol*>& visibleFns,
                     Vec<BlockStmt*>& visited,
                     CallExpr* callOrigin) {
+  //
+  // all functions in standard modules are stored in a single block
+  //
+  if (standardModuleSet.set_in(block))
+    block = theProgram->block;
+
   //
   // avoid infinite recursion due to modules with mutual uses
   //
@@ -8253,9 +8265,40 @@ static void resolveTypedefedArgTypes(FnSymbol* fn)
   }
 }
 
+
+static void
+computeStandardModuleSet() {
+  standardModuleSet.set_add(rootModule->block);
+  standardModuleSet.set_add(theProgram->block);
+
+  Vec<ModuleSymbol*> stack;
+  stack.add(standardModule);
+
+  while (ModuleSymbol* mod = stack.pop()) {
+    if (mod->block->modUses) {
+      for_actuals(expr, mod->block->modUses) {
+        UseStmt* use = toUseStmt(expr);
+        INT_ASSERT(use);
+        SymExpr* se = toSymExpr(use->src);
+        INT_ASSERT(se);
+        if (ModuleSymbol* usedMod = toModuleSymbol(se->var)) {
+          INT_ASSERT(usedMod);
+          if (!standardModuleSet.set_in(usedMod->block)) {
+            stack.add(usedMod);
+            standardModuleSet.set_add(usedMod->block);
+          }
+        }
+      }
+    }
+  }
+}
+
+
 void
 resolve() {
   parseExplainFlag(fExplainCall, &explainCallLine, &explainCallModule);
+
+  computeStandardModuleSet();
 
   // call _nilType nil so as to not confuse the user
   dtNil->symbol->name = gNil->name;
