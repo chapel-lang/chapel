ee5c51c52d6a630257689ac538af2d0381cb1018
Michael Ferguson <mppf@users.noreply.github.com>
2016-09-15 10:44:32 -0400

    comments clean up in type.cpp

--- compiler/AST/type.cpp
+++ compiler/AST/type.cpp
@@ -2035,7 +2035,9 @@ bool isString(Type* type) {
 // In the longer term we plan to further broaden the cases that the new
 // logic can handle and reduce the exceptions that are filtered out here.
 //
-
+// MPF 2016-09-15
+// This function now includes tuples, distributions, domains, and arrays.
+//
 bool isUserDefinedRecord(Type* type) {
   bool retval = false;
 
@@ -2046,24 +2048,9 @@ bool isUserDefinedRecord(Type* type) {
     // Must be a record type
     if (aggr->aggregateTag != AGGREGATE_RECORD) {
 
-    // Not a tuple
-    /*
-    } else if (sym->hasFlag(FLAG_TUPLE)              == true) {
-    */
-
     // Not a range
     } else if (sym->hasFlag(FLAG_RANGE)              == true) {
 
-    // Not a distribution
-/*    } else if (sym->hasFlag(FLAG_DISTRIBUTION)       == true) {
-
-    // Not a domain
-    } else if (sym->hasFlag(FLAG_DOMAIN)             == true) {
-
-    // Not an array or an array alias
-    } else if (sym->hasFlag(FLAG_ARRAY)              == true ||
-               sym->hasFlag(FLAG_ARRAY_ALIAS)        == true) {
-*/
     // Not an atomic type
     } else if (sym->hasFlag(FLAG_ATOMIC_TYPE)        == true) {
 
