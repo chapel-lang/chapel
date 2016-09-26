708abe22684a34458e326d47df421250531127b9
Michael Noakes <mnoakes@cray.com>
2016-09-26 08:59:17 -0700

    Wrap 5 GMP macros in C functions to support our LLVM backend

--- runtime/include/chplgmp.h
+++ runtime/include/chplgmp.h
@@ -1,15 +1,15 @@
 /*
  * Copyright 2004-2016 Cray Inc.
  * Other additional copyright holders may be indicated within.
- * 
+ *
  * The entirety of this work is licensed under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
- * 
+ *
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,13 +31,24 @@ void chpl_gmp_init(void);
 
 void chpl_gmp_get_mpz(mpz_t ret, int64_t src_locale, __mpz_struct from);
 
-void chpl_gmp_get_randstate(gmp_randstate_t not_inited_state, int64_t src_locale, __gmp_randstate_struct from);
+void chpl_gmp_get_randstate(gmp_randstate_t        not_inited_state,
+                            int64_t                src_locale,
+                            __gmp_randstate_struct from);
 
 uint64_t chpl_gmp_mpz_nlimbs(__mpz_struct from);
 
 void chpl_gmp_mpz_print(mpz_t x);
 
 c_string_copy chpl_gmp_mpz_get_str(int base, mpz_t x);
+
+// These functions wrap the equivalent GMP macros to support LLVM backend
+int chpl_mpz_cmp_si(const mpz_t op1, long op2);
+int chpl_mpz_cmp_ui(const mpz_t op1, unsigned long op2);
+
+int chpl_mpz_sgn(const mpz_t op);
+
+int chpl_mpz_odd_p(const mpz_t op);
+int chpl_mpz_even_p(const mpz_t op);
 
 #endif
 
