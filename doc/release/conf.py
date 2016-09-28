541f9e487642166b5d987b8b79a17ede35c264fe
Ben Albrecht <benalbrecht@pitt.edu>
2016-09-28 15:34:59 -0700

    Docs build _actually_ works now.. moved spec back

--- doc/release/conf.py
+++ doc/release/conf.py
@@ -83,7 +83,7 @@ copyright = u'2016, {0}'.format(author_text)
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['build']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
