c0e4b4b9a8d76ccdbf34127850cecc5ba334ea08
Ben Albrecht <benalbrecht@pitt.edu>
2016-09-28 15:43:57 -0700

    Take it up a level

--- doc/conf.py
+++ doc/conf.py
@@ -83,7 +83,7 @@ copyright = u'2016, {0}'.format(author_text)
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['build']
+exclude_patterns = ['Makefile', 'Makefile.sphinx', 'quickReference', 'util', 'build', 'developer']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
