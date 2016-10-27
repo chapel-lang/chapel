90823810b81ea2b86337c8c2c89fc2dc7c6fd22d
Elliot Ronaghan <ronawho@gmail.com>
2016-10-27 09:01:12 -0700

    Stop testing arr/ref branches in perf playgrounds
    Both branches have been merged to master. Delete the array playground, and
    switch to testing distrib vs. nemesis schedulers in the regular playground.

--- util/cron/test-perf.chapcs.playground.bash
+++ util/cron/test-perf.chapcs.playground.bash
@@ -9,22 +9,12 @@ source $CWD/common-perf.bash
 
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.chapcs.playground"
 
-# Test performance of the blank-ref-intent branch (subset/prereq of arrays work)
-GITHUB_USER=benharsh
-GITHUB_BRANCH=ben-blank-ref-intent
-ARRAYS_SHORT_NAME=arrays
-REF_SHORT_NAME=ref
-START_DATE=08/30/16
-SYNC_SUFFIX=arrays
+# Test perf of qthreads distrib scheduler compared to nemesis
+export CHPL_QTHREAD_SCHEDULER=distrib
 
-# Normally we'd just merge the branch in with master here, but it doesn't merge
-# cleanly so just check it out instead. This means we actually destroy this
-# file out from under ourselves.. but that's "ok" because it's already running
-git branch -D $GITHUB_USER-$GITHUB_BRANCH
-git remote add $GITHUB_USER https://github.com/$GITHUB_USER/chapel.git
-git fetch $GITHUB_USER
-git checkout -b $GITHUB_USER-$GITHUB_BRANCH $GITHUB_USER/$GITHUB_BRANCH
+SHORT_NAME=distrib
+START_DATE=10/27/16
 
-perf_args="-performance-description $REF_SHORT_NAME -performance-configs default:v,$REF_SHORT_NAME:v,$ARRAYS_SHORT_NAME:v -sync-dir-suffix $SYNC_SUFFIX"
+perf_args="-performance-description $SHORT_NAME -performance-configs default:v,$SHORT_NAME:v -sync-dir-suffix $SHORT_NAME"
 perf_args="${perf_args} -numtrials 5 -startdate $START_DATE"
 $CWD/nightly -cron ${perf_args} ${nightly_args}
