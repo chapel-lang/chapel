a5a7cdfdca1bea1f2959ca2ee2fbf03e2dcf4acf
Elliot Ronaghan <ronawho@gmail.com>
2016-10-28 14:09:45 -0700

    Update nightly shootout jobs
    Have the release job test 1.14 instead of 1.13 and have the regular job run the
    no-op/startup time test since it impacts the shootouts (and in particular
    represents a significant portion of meteor's execution time.)

--- util/cron/test-perf.chapel-shootout.bash
+++ util/cron/test-perf.chapel-shootout.bash
@@ -8,5 +8,5 @@ source $CWD/common-perf.bash
 
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.chapel-shootout"
 
-export CHPL_NIGHTLY_TEST_DIRS="release/examples/benchmarks/shootout studies/shootout"
+export CHPL_NIGHTLY_TEST_DIRS="release/examples/benchmarks/shootout studies/shootout performance/elliot"
 $CWD/nightly -cron -performance -numtrials 5 -startdate 11/17/14
