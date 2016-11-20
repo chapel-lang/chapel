90436185c30fca68825dc1d8f000876c71791ecf
Elliot Ronaghan <ronawho@gmail.com>
2016-11-20 09:32:10 -0500

    Fix OpenMP task spawn test
    Fix skipif and guard OpenMP constructs with _OPENMP

--- test/parallel/taskCompare/elliot/ompTaskSpawn.h
+++ test/parallel/taskCompare/elliot/ompTaskSpawn.h
@@ -1,12 +1,20 @@
-#include <omp.h>
 #include <stdint.h>
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 
 static void ompTaskSpawn(int64_t trials, int64_t numTasks) {
   int i, j;
+
+  #ifdef _OPENMP
   omp_set_num_threads(numTasks);
+  #endif
 
   for (i=0; i<trials; i++) {
+    #ifdef _OPENMP
     #pragma omp parallel for
+    #endif
     for (j=0; j<numTasks; j++) { }
   }
 }
