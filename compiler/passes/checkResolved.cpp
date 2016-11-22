9347eb784e84ab21a4f428882542ff83281bbe90
Michael Ferguson <mppf@users.noreply.github.com>
2016-11-22 16:58:45 -0500

    Fix improperly moved check from functionResolution

--- compiler/passes/checkResolved.cpp
+++ compiler/passes/checkResolved.cpp
@@ -436,14 +436,16 @@ checkBadAddrOf(CallExpr* call)
       //
       // check that the operand of 'addr of' is a legal lvalue.
       if (SymExpr* rhs = toSymExpr(call->get(1))) {
-        if (rhs->symbol()->hasFlag(FLAG_EXPR_TEMP) ||
-            rhs->symbol()->isConstant() || rhs->symbol()->isParameter()) {
-          if (lhs && lhs->symbol()->hasFlag(FLAG_REF_VAR)) {
-            if (rhs->symbol()->isImmediate()) {
-              USR_FATAL_CONT(call, "Cannot set a non-const reference to a literal value.");
-            } else {
-              // This case should be handled elsewhere in the compiler
-              INT_FATAL(call, "Cannot set a non-const reference to a const variable.");
+        if (!(lhs && lhs->symbol()->hasFlag(FLAG_REF_VAR) && lhs->symbol()->hasFlag(FLAG_CONST))) {
+          if (rhs->symbol()->hasFlag(FLAG_EXPR_TEMP) ||
+              rhs->symbol()->isConstant() || rhs->symbol()->isParameter()) {
+            if (lhs && lhs->symbol()->hasFlag(FLAG_REF_VAR)) {
+              if (rhs->symbol()->isImmediate()) {
+                USR_FATAL_CONT(call, "Cannot set a non-const reference to a literal value.");
+              } else {
+                // This case should be handled elsewhere in the compiler
+                INT_FATAL(call, "Cannot set a non-const reference to a const variable.");
+              }
             }
           }
         }
