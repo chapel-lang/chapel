a6db59466b60201aaf25c786689846b3801c03c8
Michael Noakes <mnoakes@cray.com>
2016-11-27 07:32:33 -0800

    Whitespace Slurm: Straighten out #includes

--- runtime/src/launch/slurm-srun/launch-slurm-srun.c
+++ runtime/src/launch/slurm-srun/launch-slurm-srun.c
@@ -1,15 +1,15 @@
 /*
  * Copyright 2004-2016 Cray Inc.
  * Other additional copyright holders may be indicated within.
- * 
+ *
  * The entirety of this work is licensed under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
- * 
+ *
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,17 +17,20 @@
  * limitations under the License.
  */
 
+#include "chpllaunch.h"
+
+#include "chpl-mem.h"
+#include "chpltypes.h"
+#include "error.h"
+
 #include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <unistd.h>
-#include "chpllaunch.h"
-#include "chpl-mem.h"
-#include "chpltypes.h"
-#include "error.h"
+
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #define baseSBATCHFilename ".chpl-slurm-sbatch-"
 #define baseSysFilename ".chpl-sys-"
