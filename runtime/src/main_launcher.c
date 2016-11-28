0d8eed51e7f7e4c921ccf2ec28a6d79fd68ef37b
Michael Noakes <mnoakes@cray.com>
2016-11-27 17:33:41 -0800

    Whitespace main: Remove trailing white space

--- runtime/src/main_launcher.c
+++ runtime/src/main_launcher.c
@@ -1,15 +1,15 @@
 /*
  * Copyright 2004-2016 Cray Inc.
  * Other additional copyright holders may be indicated within.
- * 
+ *
  * The entirety of this work is licensed under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
- * 
+ *
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -330,9 +330,9 @@ char* chpl_get_enviro_keys(char sep)
   return ret;
 }
 
-int handleNonstandardArg(int* argc, char* argv[], int argNum, 
+int handleNonstandardArg(int* argc, char* argv[], int argNum,
                          int32_t lineno, int32_t filename) {
-  int numHandled = chpl_launch_handle_arg(*argc, argv, argNum, 
+  int numHandled = chpl_launch_handle_arg(*argc, argv, argNum,
                                           lineno, filename);
   if (numHandled == 0) {
     if (mainHasArgs) {
@@ -377,7 +377,7 @@ void chpl_compute_real_binary_name(const char* argv0) {
   if (NULL == real_suffix) {
     real_suffix = launcher_real_suffix;
   }
-  
+
   length = strlen(argv0);
   if (length + strlen(launcher_real_suffix) >= BIN_NAME_SIZE)
     chpl_internal_error("Real executable name is too long.");
