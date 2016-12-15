f0fccdb2ab842f9f59599940fc82ca603ce6e579
Michael Ferguson <mppf@users.noreply.github.com>
2016-12-15 12:08:39 -0500

    Adjust normalize workaround for no chpl__typeAliasInit

--- compiler/passes/normalize.cpp
+++ compiler/passes/normalize.cpp
@@ -921,6 +921,16 @@ static void applyGetterTransform(CallExpr* call) {
   }
 }
 
+static bool moveMakesTypeAlias(CallExpr* call)
+{
+  if (call->isPrimitive(PRIM_MOVE)) {
+    if (SymExpr* se = toSymExpr(call->get(1)))
+      if (VarSymbol* var = toVarSymbol(se->symbol()))
+        if (var->isType()) return true;
+  }
+  return false;
+}
+
 static void insert_call_temps(CallExpr* call)
 {
   Expr* stmt = call->getStmtExpr();
@@ -990,7 +1000,7 @@ static void insert_call_temps(CallExpr* call)
       //  * making an array type alias, or
       //  * passing the result into the 2nd argument of buildArrayRuntimeType.
       while (cur != NULL) {
-        if (/* TODO cur->isNamed("chpl__typeAliasInit") || */
+        if (moveMakesTypeAlias(cur) ||
             (cur->isNamed("chpl__buildArrayRuntimeType") && cur->get(2) == sub))
           break;
         sub = cur;
@@ -1133,9 +1143,8 @@ static void fix_def_expr(VarSymbol* var) {
     stmt->insertAfter(new CallExpr(PRIM_MOVE,
                                    var,
                                    init->copy()));
-    // TODO
     // note: insert_call_temps adjusts auto-destroy in this case
-    // by checking for chpl__typeAliasInit
+    // by checking for it with moveMakesTypeAlias
     return;
   }
 
