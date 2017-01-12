d4ef6c1be0d6b0507ac924b2e67ab5beeff90d31
Preston Sahabu <psahabu@cray.com>
2017-01-11 17:14:16 -0800

    ensure FLAG_EPILOGUE_LABEL is maintained and removed, as appropriate

--- compiler/include/optimizations.h
+++ compiler/include/optimizations.h
@@ -42,8 +42,7 @@ class FnSymbol;
 class Symbol;
 class SymExpr;
 
-void removeUnnecessaryGotos(FnSymbol* fn);
-void removeUnusedLabels(FnSymbol* fn);
+void removeUnnecessaryGotos(FnSymbol* fn, bool removeEpilogueLabel = false);
 size_t localCopyPropagation(FnSymbol* fn);
 size_t globalCopyPropagation(FnSymbol* fn);
 void eliminateSingleAssignmentReference(Map<Symbol*,Vec<SymExpr*>*>& defMap,
