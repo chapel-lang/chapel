c7c7b915fdba54e2fab7fef721d1cd28acb5e5a3
Elliot Ronaghan <ronawho@gmail.com>
2017-01-26 15:19:40 -0800

    Test perf of using a shift instead of a divide for multi-ddata in playground

--- util/cron/test-perf.chapcs.playground.bash
+++ util/cron/test-perf.chapcs.playground.bash
@@ -6,19 +6,21 @@ CWD=$(cd $(dirname $0) ; pwd)
 export CHPL_TEST_PERF_CONFIG_NAME='chapcs'
 
 source $CWD/common-perf.bash
+source $CWD/common-numa.bash
+
 
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.chapcs.playground"
 
-# Test performance of having qthreads use chapel's allocator
-GITHUB_USER=ronawho
-GITHUB_BRANCH=use-chpl-alloc-for-qt
-SHORT_NAME=qtChplAlloc
-START_DATE=01/20/17
+# Test performance of multi-ddata using a shift instead of a divide
+GITHUB_USER=gbtitus
+GITHUB_BRANCH=multi-ddata-shift-for-div
+SHORT_NAME=multi-ddata-numa-shift
+START_DATE=01/26/17
 
 git branch -D $GITHUB_USER-$GITHUB_BRANCH
 git checkout -b $GITHUB_USER-$GITHUB_BRANCH
 git pull https://github.com/$GITHUB_USER/chapel.git $GITHUB_BRANCH
 
-perf_args="-performance-description $SHORT_NAME -performance-configs default:v,$SHORT_NAME:v -sync-dir-suffix $SHORT_NAME"
+perf_args="-performance-description $SHORT_NAME -performance-configs numa:v,$SHORT_NAME:v -sync-dir-suffix $SHORT_NAME"
 perf_args="${perf_args} -numtrials 5 -startdate $START_DATE"
 $CWD/nightly -cron ${perf_args} ${nightly_args}
