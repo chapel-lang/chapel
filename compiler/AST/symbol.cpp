15adcc9fe5034348cae0e96a50f4ee0ac98201f5
Lydia Duncan <lydia@cray.com>
2017-02-17 08:28:47 -0800

    Remove rest of renameInstantiatedType's body into the series of methods
    The last bit is now in TypeSymbol::renameInstantiatedEnd, while the rest of
    it is sitting in renameInstantiatedMulti while I devise how to move it around.
    
    Verified no difference in log or ccode output beyond normal variability

--- compiler/AST/symbol.cpp
+++ compiler/AST/symbol.cpp
@@ -869,6 +869,62 @@ void TypeSymbol::accept(AstVisitor* visitor) {
 
 void TypeSymbol::renameInstantiatedMulti(SymbolMap& subs, FnSymbol* fn) {
   renameInstantiatedStart();
+
+  bool first = false;
+  for_formals(formal, fn) {
+    if (Symbol* value = subs.get(formal)) {
+      if (TypeSymbol* ts = toTypeSymbol(value)) {
+        if (!first && this->hasFlag(FLAG_TUPLE)) {
+          if (this->hasFlag(FLAG_STAR_TUPLE)) {
+            this->name = astr(istr(fn->numFormals()-1), "*", ts->name);
+            this->cname = astr(this->cname, "star_", ts->cname);
+            return;
+          } else {
+            this->name = astr("(");
+          }
+        }
+        if (!this->hasFlag(FLAG_STAR_TUPLE)) {
+          if (first) {
+            this->name = astr(this->name, ",");
+            this->cname = astr(this->cname, "_");
+          }
+          this->name = astr(this->name, ts->name);
+          this->cname = astr(this->cname, ts->cname);
+        }
+        first = true;
+      } else {
+        if (first) {
+          this->name = astr(this->name, ",");
+          this->cname = astr(this->cname, "_");
+        }
+        VarSymbol* var = toVarSymbol(value);
+        if (var && var->immediate) {
+          Immediate* immediate = var->immediate;
+          if (var->type == dtString || var->type == dtStringC)
+            renameInstantiatedTypeString(this, var);
+          else if (immediate->const_kind == NUM_KIND_BOOL) {
+            // Handle boolean types specially.
+            const char* name4bool = immediate->bool_value() ? "true" : "false";
+            const char* cname4bool = immediate->bool_value() ? "T" : "F";
+            this->name = astr(this->name, name4bool);
+            this->cname = astr(this->cname, cname4bool);
+          } else {
+            const size_t bufSize = 128;
+            char imm[bufSize];
+            snprint_imm(imm, bufSize, *var->immediate);
+            this->name = astr(this->name, imm);
+            this->cname = astr(this->cname, imm);
+          }
+        } else {
+          this->name = astr(this->name, value->cname);
+          this->cname = astr(this->cname, value->cname);
+        }
+        first = true;
+      }
+    }
+  }
+
+  renameInstantiatedEnd();
 }
 
 void TypeSymbol::renameInstantiatedStart() {
@@ -886,7 +942,13 @@ void TypeSymbol::renameInstantiatedStart() {
   this->cname = astr(this->cname, "_");
 }
 
+void TypeSymbol::renameInstantiatedIndividual(Symbol* sym) {
 
+}
+
+void TypeSymbol::renameInstantiatedEnd() {
+  this->name = astr(this->name, ")");
+}
 
 /************************************* | **************************************
 *                                                                             *
