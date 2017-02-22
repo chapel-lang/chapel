543de393e592ff3375b8ba62b6a75cabe40b058d
Michael Noakes <mnoakes@cray.com>
2017-02-22 14:07:30 -0800

    Remove a layer of indentation that doesn't seem to serve a purpose

--- compiler/passes/normalize.cpp
+++ compiler/passes/normalize.cpp
@@ -524,67 +524,64 @@ static void checkUseBeforeDefs() {
 
       std::vector<BaseAST*> asts;
 
-      // Walk the asts in this function.
       collect_asts_postorder(fn, asts);
 
       for_vector(BaseAST, ast, asts) {
-        if (Symbol* symbol = theDefinedSymbol(ast)) {
-          defined.insert(symbol);
+        if (Symbol* sym = theDefinedSymbol(ast)) {
+          defined.insert(sym);
 
-        } else {
-          // The AST in question is not one of our methods of declaration so now
-          // we check if it is a (resolved/unresolved) symbol and make sure
-          // that symbol is not defined/declared before use
-          if (SymExpr* sym = toSymExpr(ast)) {
-            if (isModuleSymbol(sym->symbol()) == true) {
-              if (isFnSymbol(fn->defPoint->parentSymbol) == false) {
-                if (isUseStmt(sym->parentExpr) == false) {
-                  SymExpr* prev = toSymExpr(sym->prev);
+        // The AST in question is not one of our methods of declaration so now
+        // we check if it is a (resolved/unresolved) symbol and make sure
+        // that symbol is not defined/declared before use
+        } else if (SymExpr* sym = toSymExpr(ast)) {
+          if (isModuleSymbol(sym->symbol()) == true) {
+            if (isFnSymbol(fn->defPoint->parentSymbol) == false) {
+              if (isUseStmt(sym->parentExpr) == false) {
+                SymExpr* prev = toSymExpr(sym->prev);
 
-                  if (prev == NULL || prev->symbol() != gModuleToken) {
-                    USR_FATAL_CONT(sym,
-                                   "illegal use of module '%s'",
-                                   sym->symbol()->name);
-                  }
-                }
-              }
-
-            } else if (isLcnSymbol(sym->symbol())) {
-              if (sym->symbol()->defPoint->parentExpr != rootModule->block &&
-                  (sym->symbol()->defPoint->parentSymbol == fn ||
-                   (sym->symbol()->defPoint->parentSymbol == mod && mod->initFn == fn))) {
-                if (defined.find(sym->symbol())   == defined.end()) {
-                  if (!sym->symbol()->hasEitherFlag(FLAG_ARG_THIS,FLAG_EXTERN) &&
-                      !sym->symbol()->hasFlag(FLAG_TEMP)) {
-                    // Only complain one time
-                    if (undefined.find(sym->symbol()) == undefined.end()) {
-                      USR_FATAL_CONT(sym,
-                                     "'%s' used before defined (first used here)",
-                                     sym->symbol()->name);
-
-                      undefined.insert(sym->symbol());
-                    }
-                  }
+                if (prev == NULL || prev->symbol() != gModuleToken) {
+                  USR_FATAL_CONT(sym,
+                                 "illegal use of module '%s'",
+                                 sym->symbol()->name);
                 }
               }
             }
 
-          } else if (UnresolvedSymExpr* sym = toUnresolvedSymExpr(ast)) {
-            CallExpr* call = toCallExpr(sym->parentExpr);
+          } else if (isLcnSymbol(sym->symbol())) {
+            if (sym->symbol()->defPoint->parentExpr != rootModule->block &&
+                (sym->symbol()->defPoint->parentSymbol == fn ||
+                 (sym->symbol()->defPoint->parentSymbol == mod && mod->initFn == fn))) {
+              if (defined.find(sym->symbol())   == defined.end()) {
+                if (!sym->symbol()->hasEitherFlag(FLAG_ARG_THIS,FLAG_EXTERN) &&
+                    !sym->symbol()->hasFlag(FLAG_TEMP)) {
+                  // Only complain one time
+                  if (undefined.find(sym->symbol()) == undefined.end()) {
+                    USR_FATAL_CONT(sym,
+                                   "'%s' used before defined (first used here)",
+                                   sym->symbol()->name);
 
-            if (!call ||
-                (call->baseExpr != sym &&
-                 !call->isPrimitive(PRIM_CAPTURE_FN_FOR_CHPL) &&
-                 !call->isPrimitive(PRIM_CAPTURE_FN_FOR_C))) {
-              if (isFnSymbol(fn->defPoint->parentSymbol) == false) {
-                // Only complain one time
-                if (undeclared.find(sym->unresolved) == undeclared.end()) {
-                  USR_FATAL_CONT(sym,
-                                 "'%s' undeclared (first use this function)",
-                                 sym->unresolved);
-
-                  undeclared.insert(sym->unresolved);
+                    undefined.insert(sym->symbol());
+                  }
                 }
+              }
+            }
+          }
+
+        } else if (UnresolvedSymExpr* sym = toUnresolvedSymExpr(ast)) {
+          CallExpr* call = toCallExpr(sym->parentExpr);
+
+          if (!call ||
+              (call->baseExpr != sym &&
+               !call->isPrimitive(PRIM_CAPTURE_FN_FOR_CHPL) &&
+               !call->isPrimitive(PRIM_CAPTURE_FN_FOR_C))) {
+            if (isFnSymbol(fn->defPoint->parentSymbol) == false) {
+              // Only complain one time
+              if (undeclared.find(sym->unresolved) == undeclared.end()) {
+                USR_FATAL_CONT(sym,
+                               "'%s' undeclared (first use this function)",
+                               sym->unresolved);
+
+                undeclared.insert(sym->unresolved);
               }
             }
           }
