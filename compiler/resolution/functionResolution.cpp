eb60654d2e302cc8bdd4ba8560b430cf39ad70f6
Michael Ferguson <mppf@users.noreply.github.com>
2017-02-28 16:28:38 -0500

    Adjust formalRequiresTemp, backendRequiresCopyForIn
    formalRequiresTemp made INTENT_IN / INTENT_CONST_IN behavior depend upon
    if the function had FLAG_INLINE but as far as I can tell the condition
    was reversed from what it should be. Either way, removing the dependence
    on FLAG_INLINE on master seems to work; it passed full local testing on
    master except for several RA benchmarks (which gave a warning about while
    loop over constant variable (while arg, arg has in intent)). That is to
    say, whatever this check was meant to work around no longer seems to be a
    big issue.
    
    This dependency on FLAG_INLINE originated in commit 2cd1856f5e.
    
    backendRequiresCopyForIn returns true now for unions because they should
    have similar semantics to records.

--- compiler/resolution/functionResolution.cpp
+++ compiler/resolution/functionResolution.cpp
@@ -5554,6 +5554,7 @@ resolveCoerce(CallExpr* call) {
 //
 static bool backendRequiresCopyForIn(Type* t) {
   return (isRecord(t) ||
+          isUnion(t) ||
           t->symbol->hasFlag(FLAG_ARRAY) ||
           t->symbol->hasFlag(FLAG_DOMAIN));
 }
@@ -5582,7 +5583,7 @@ formalRequiresTemp(ArgSymbol* formal) {
      // inlined.
      //
      ((formal->intent == INTENT_IN || formal->intent == INTENT_CONST_IN) &&
-      (backendRequiresCopyForIn(formal->type) || !fn->hasFlag(FLAG_INLINE)))
+      (backendRequiresCopyForIn(formal->type)))
      );
 }
 
