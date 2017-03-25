ef3268efada28c30b079ce3785d6f30b1e3599c7
bmooreii <bmooreii@login0.crc.pitt.edu>
2017-03-25 14:06:56 -0400

    Initial commit for a gasnetrun_psm launcher

---
+++ runtime/src/launch/gasnetrun_psm/launch-gasnetrun_psm.c
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2004-2017 Cray Inc.
+ * Other additional copyright holders may be indicated within.
+ * 
+ * The entirety of this work is licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * 
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "chpllaunch.h"
+#include "chpl-mem.h"
+#include "error.h"
+
+#define LAUNCH_PATH_HELP WRAP_TO_STR(LAUNCH_PATH)
+#define WRAP_TO_STR(x) TO_STR(x)
+#define TO_STR(x) #x
+
+// TODO: Un-hard-code this stuff:
+
+static char _nlbuf[16];
+static char** chpl_launch_create_argv(const char *launch_cmd,
+                                      int argc, char* argv[],
+                                      int32_t numLocales) {
+  const int largc = 5;
+  char *largv[largc];
+
+  largv[0] = (char *) launch_cmd;
+  largv[1] = (char *) "-n";
+  sprintf(_nlbuf, "%d", numLocales);
+  largv[2] = _nlbuf;
+  largv[3] = (char*) "-E";
+  largv[4] = chpl_get_enviro_keys(',');
+
+  return chpl_bundle_exec_args(argc, argv, largc, largv);
+}
+
+
+int chpl_launch(int argc, char* argv[], int32_t numLocales) {
+  int len = strlen(WRAP_TO_STR(LAUNCH_PATH)) + strlen("gasnetrun_psm") + 1;
+  char *cmd = chpl_mem_allocMany(len, sizeof(char), CHPL_RT_MD_COMMAND_BUFFER, -1, 0);
+  sprintf(cmd, "%sgasnetrun_psm", WRAP_TO_STR(LAUNCH_PATH));
+
+  return chpl_launch_using_exec(cmd,
+                                chpl_launch_create_argv(cmd, argc, argv,
+                                                        numLocales),
+                                argv[0]);
+}
+
+
+int chpl_launch_handle_arg(int argc, char* argv[], int argNum,
+                           int32_t lineno, int32_t filename) {
+  return 0;
+}
+
+
+void chpl_launch_print_help(void) {
+}
