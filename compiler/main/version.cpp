ff3f1b34eea0b0edfba621d60a099aaa646e2b55
Brad Chamberlain <bradcray@users.noreply.github.com>
2017-04-05 22:08:26 -0700

    Add '(pre-release)' label to version output with SHA
    For some time now, we've been discussing bumping our compiler
    version number just after a release rather than just before it.
    In doing so, we wanted to clearly label the version as being
    a pre-release version.  This mod notes that this condition is
    met whenever we print the SHA of the GitHub repo and simply
    extends the logic that prints the SHA with a print of "(pre-release)".
    It also updates the script that creates the .good file for our
    test of the version number similarly.

--- compiler/main/version.cpp
+++ compiler/main/version.cpp
@@ -26,6 +26,6 @@ void
 get_version(char *v) {
   v += sprintf(v, "%d.%s.%s", MAJOR_VERSION, MINOR_VERSION, UPDATE_VERSION);
   if (strcmp(BUILD_VERSION, "0") != 0 || developer)
-    sprintf(v, ".%s", BUILD_VERSION);
+    sprintf(v, ".%s (pre-release)", BUILD_VERSION);
 }
 
