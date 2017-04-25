ea3504e056c64966ee9a913d18a767d2b3d99917
Elliot Ronaghan <ronawho@gmail.com>
2017-04-25 13:12:39 -0700

    Test perf of a faster divide for multi-data in playground

--- util/cron/test-perf.chapcs.playground-2.bash
+++ util/cron/test-perf.chapcs.playground-2.bash
@@ -6,19 +6,21 @@ CWD=$(cd $(dirname $0) ; pwd)
 export CHPL_TEST_PERF_CONFIG_NAME='chapcs'
 
 source $CWD/common-perf.bash
+source $CWD/common-numa.bash
 
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.chapcs.playground-2"
 
-# Test performance of limiting qthread memory pools 
-GITHUB_USER=ronawho
-GITHUB_BRANCH=limit-qt-pool-size
-SHORT_NAME=limit-qt-pool-size
-START_DATE=03/18/17
+# Test performance of a faster "divide" for multi-ddata
+export CHPL_LIBDIVIDE=libdivide
+GITHUB_USER=gbtitus
+GITHUB_BRANCH=multi-dd-fast-divide
+SHORT_NAME=multi-dd-fast-divide
+START_DATE=04/25/17
 
 git branch -D $GITHUB_USER-$GITHUB_BRANCH
 git checkout -b $GITHUB_USER-$GITHUB_BRANCH
 git pull https://github.com/$GITHUB_USER/chapel.git $GITHUB_BRANCH
 
-perf_args="-performance-description $SHORT_NAME -performance-configs default:v,$SHORT_NAME:v -sync-dir-suffix $SHORT_NAME"
+perf_args="-performance-description $SHORT_NAME -performance-configs numa:v,$SHORT_NAME:v -sync-dir-suffix $SHORT_NAME"
 perf_args="${perf_args} -numtrials 5 -startdate $START_DATE"
 $CWD/nightly -cron ${perf_args} ${nightly_args}
