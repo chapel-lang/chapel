332782d516899ffed5b80a68d8dc69ba623aad67
Elliot Ronaghan <ronawho@gmail.com>
2017-05-03 15:28:32 -0700

    PEP8/linter cleanups

--- util/devel/lookForAlloc.py
+++ util/devel/lookForAlloc.py
@@ -29,32 +29,36 @@ def get_alloc_funcs():
     misc = ['strdup', 'strndup']
     return std + align + p_align + misc
 
+
 def find_files(search_dir, extensions):
     """Return a list of absolute paths to files with any of the provided
        extensions in the search_dir."""
     source_files = []
-    for root, dirnames, filenames in os.walk(search_dir):
+    for root, _, filenames in os.walk(search_dir):
         for ext in extensions:
             for filename in fnmatch.filter(filenames, '*.{0}'.format(ext)):
                 source_files.append(os.path.join(root, filename))
     return source_files
+
 
 def find_source_files(search_dir):
     """Return a list of absolute paths to any C/C++ source files in
        the search_dir"""
     cpp_sources = ['cc', 'cp', 'cxx', 'cpp', 'CPP', 'c++', 'C']
     cpp_headers = ['hh', 'H', 'hp', 'hxx', 'hpp', 'HPP', 'h++', 'tcc']
-    c_sources   = ['c']
-    c_headers   = ['h']
+    c_sources = ['c']
+    c_headers = ['h']
     code_file_exts = set(cpp_sources + cpp_headers + c_sources + c_headers)
     return find_files(search_dir, code_file_exts)
 
+
 def get_allowed_files():
-   """Return a list of files that are allowed to call the system allocator"""
+    """Return a list of files that are allowed to call the system allocator"""
     # The sys alloc wrappers and the cstdlib (sys alloc) memory implementation
     # are the only files that are allowed to directly call the system allocator
-   return ['chpl-mem-sys.h',
-           os.path.join('cstdlib', 'chpl-mem-impl.h')]
+    return ['chpl-mem-sys.h',
+            os.path.join('cstdlib', 'chpl-mem-impl.h')]
+
 
 def get_non_allowed_files(search_dir):
     """Return a list of absolute paths to files that are not allowed to call
@@ -65,16 +69,19 @@ def get_non_allowed_files(search_dir):
         srcs = [s for s in srcs if not s.endswith(allowed_file)]
     return srcs
 
+
 def build_cscope_ref(src_files):
     """Build a cscope cross-reference for crazy fast searching of src_files"""
     cscope_cmd = ['cscope', '-b', '-q', '-k'] + src_files
     run_command(cscope_cmd)
+
 
 def cscope_find_calls(func_call):
     """Run cscope searching for calls to func_call"""
     # -L3 -- line based "Find functions calling this function"
     cscope_cmd = ['cscope', '-d', '-L3{0}'.format(func_call)]
     return run_command(cscope_cmd)
+
 
 def check_runtime_for_alloc_calls(chpl_home):
     """ Check $CHPL_HOME/runtime for calls to the system allocator"""
@@ -94,6 +101,7 @@ def check_runtime_for_alloc_calls(chpl_home):
             sys.stdout.write(out.replace(chpl_home + os.path.sep, ''))
             sys.stdout.write('\n')
 
+
 def main():
     parser = optparse.OptionParser(description=__doc__)
     parser.add_option('--chpl_home', dest='chpl_home', default=get_chpl_home())
@@ -101,6 +109,7 @@ def main():
     chpl_home = os.path.normpath(options.chpl_home)
 
     check_runtime_for_alloc_calls(chpl_home)
+
 
 if __name__ == "__main__":
     main()
