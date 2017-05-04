de8ed549d0b587a6e04ced23a7cfc591300bb5ac
Elliot Ronaghan <ronawho@gmail.com>
2017-05-04 16:20:50 -0700

    Improvements based on actually using script to clean up the runtime
     - Also check for asprintf, vasprintf, getline, and getdelim
     - Only allow the system alloc wrapper to call the system allocator. Don't
       allow the cstdlib implementation to
     - Describe the cscope args better
     - Move the runtime path building into main

--- util/devel/lookForAlloc.py
+++ util/devel/lookForAlloc.py
@@ -7,7 +7,7 @@ allocator, not the system one.
 
 Finds all C/C++ files in the runtime, and uses cscope to check for calls to
 system allocation/deallocation routines. Note that we don't check files that
-are allowed to call the system allocator (sys wrapper, CHPL_MEM=cstdlib)"""
+are allowed to call the system allocator (sys alloc wrapper)"""
 
 import fnmatch
 import optparse
@@ -25,9 +25,11 @@ def get_alloc_funcs():
     """Return a list of the possible C alloc/dealloc routines"""
     std = ['malloc', 'calloc', 'realloc', 'free']
     align = ['aligned_alloc', 'posix_memalign', 'memalign']
-    p_align = ['valloc', 'pvalloc']
-    misc = ['strdup', 'strndup']
-    return std + align + p_align + misc
+    page_align = ['valloc', 'pvalloc']
+    string = ['strdup', 'strndup', 'asprintf', 'vasprintf']
+    obscure = ['getline', 'getdelim']
+
+    return std + align + page_align + string + obscure
 
 
 def find_files(search_dir, extensions):
@@ -54,10 +56,9 @@ def find_source_files(search_dir):
 
 def get_allowed_files():
     """Return a list of files that are allowed to call the system allocator"""
-    # The sys alloc wrappers and the cstdlib (sys alloc) memory implementation
-    # are the only files that are allowed to directly call the system allocator
-    return ['chpl-mem-sys.h',
-            os.path.join('cstdlib', 'chpl-mem-impl.h')]
+    # The system alloc wrapper is the only place that's allowed to directly
+    # call the system allocator
+    return ['chpl-mem-sys.h']
 
 
 def get_non_allowed_files(search_dir):
@@ -72,24 +73,27 @@ def get_non_allowed_files(search_dir):
 
 def build_cscope_ref(src_files):
     """Build a cscope cross-reference for crazy fast searching of src_files"""
+    # -b -- build cross ref only
+    # -q -- enable faster symbol lookup
+    # -k -- turns off default include dir (don't include/parse system files
+    #       since we only care if our files call the system allocator)
     cscope_cmd = ['cscope', '-b', '-q', '-k'] + src_files
     run_command(cscope_cmd)
 
 
 def cscope_find_calls(func_call):
     """Run cscope searching for calls to func_call"""
+    # -d  -- don't rebuild the cross-reference, use the one we already built
     # -L3 -- line based "Find functions calling this function"
     cscope_cmd = ['cscope', '-d', '-L3{0}'.format(func_call)]
     return run_command(cscope_cmd)
 
 
-def check_runtime_for_alloc_calls(chpl_home):
-    """ Check $CHPL_HOME/runtime for calls to the system allocator. Returns
-        True if alloc calls were found, False otherwise"""
-    # TODO check that runtime dir exists
-    # TODO print out number of runtime files that we check?
-    runtime_dir = os.path.join(chpl_home, 'runtime')
-    src_files = get_non_allowed_files(runtime_dir)
+def check_for_alloc_calls(search_dir, rel_dir=''):
+    """ Check src files in search_dir for calls to the system allocator. Report
+        files relative to rel_dir.  Returns True if alloc calls were found,
+        False otherwise"""
+    src_files = get_non_allowed_files(search_dir)
 
     # TODO check that cscope is available
     # TODO check for errors in calls to cscope
@@ -101,8 +105,7 @@ def check_runtime_for_alloc_calls(chpl_home):
         if out:
             found_alloc_calls = True
             sys.stdout.write('Error: found "{0}" calls:\n'.format(alloc_func))
-            sys.stdout.write(out.replace(chpl_home + os.path.sep, ''))
-            sys.stdout.write('\n')
+            sys.stdout.write(out.replace(rel_dir, '') + '\n')
     return found_alloc_calls
 
 
@@ -113,7 +116,11 @@ def main():
     options = parser.parse_args()[0]
     chpl_home = os.path.normpath(options.chpl_home)
 
-    return check_runtime_for_alloc_calls(chpl_home)
+    # TODO check that runtime dir exists
+    # TODO print out number of runtime files that we check?
+    runtime_dir = os.path.join(chpl_home, 'runtime')
+    rel_dir = chpl_home + os.path.sep
+    return check_for_alloc_calls(runtime_dir, rel_dir)
 
 
 if __name__ == "__main__":
