f6db52718f6153e7363f47126cb7af7f0b52bb25
Michael Ferguson <mppf@users.noreply.github.com>
2017-05-09 14:00:01 -0400

    Check for whitelisted hwloc versions

--- util/chplenv/chpl_3p_hwloc_configs.py
+++ util/chplenv/chpl_3p_hwloc_configs.py
@@ -5,7 +5,7 @@ chplenv_dir = os.path.dirname(__file__)
 sys.path.insert(0, os.path.abspath(chplenv_dir))
 
 import chpl_locale_model, third_party_utils
-from utils import memoize
+from utils import error, memoize
 
 
 @memoize
@@ -19,6 +19,13 @@ def get_link_args(hwloc):
         return third_party_utils.default_get_link_args('hwloc',
                                                        ucp=get_uniq_cfg_path())
     elif hwloc == 'system':
+        # Check that hwloc version is OK
+        okversions = ('1.11.5', '1.11.6')
+        vers = third_party_utils.pkgconfig_get_system_version('hwloc')
+        if not vers in okversions:
+          err = "CHPL_HWLOC=system but unsupported version {0} was found.\nPlease use one of the following versions {1}\n".format(vers, ' '.join(okversions))
+          error(err, ValueError)
+
         return ['-lhwloc']
     else:
         return []
