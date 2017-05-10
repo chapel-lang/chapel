aaeeef840973989ff8bdedc3def0529cd2d69608
Ben Albrecht <benalbrecht@pitt.edu>
2017-05-10 16:52:23 -0700

    Add gasnet MPI testing

---
+++ util/cron/test-mpicc.gasnet.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# Test CHPL_COMM=gasnet && CHPL_TARGET_COMPILER=mpi-gnu for MPI module testing
+#
+CWD=$(cd $(dirname $0) ; pwd)
+source $CWD/common.bash
+
+echo >&2 module load mpi
+module load mpi
+
+set -x
+: confirm mpi module is loaded
+module list -l 2>&1 | grep -E '\bmpi/mpich\b' || exit $?
+
+export MPICH_MAX_THREAD_SAFETY=multiple
+
+export CHPL_TARGET_COMPILER=mpi-gnu
+export CHPL_TASKS=fifo
+export CHPL_COMM=gasnet
+export CHPL_COMM_SUBSTRATE=mpi
+
+export CHPL_NIGHTLY_TEST_DIRS="release/examples/hello*.chpl modules/packages/mpi"
+
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="mpicc.gasnet"
+
+$CWD/nightly -cron -no-buildcheck ${nightly_args}
