849bcb31ad1b05b24a138cc6be32b88cf89c9925
Ben Albrecht <benalbrecht@pitt.edu>
2017-05-22 14:42:45 -0700

    Drop gasnet setting from test-mpicc.gasnet.bash
    Also reorder exports since tasking layer affects common-gasnet.bash

--- util/cron/test-mpicc.gasnet.bash
+++ util/cron/test-mpicc.gasnet.bash
@@ -2,6 +2,11 @@
 #
 # Test CHPL_COMM=gasnet && CHPL_TARGET_COMPILER=mpi-gnu for MPI module testing
 #
+
+export CHPL_TARGET_COMPILER=mpi-gnu
+export CHPL_TASKS=fifo
+export CHPL_COMM_SUBSTRATE=mpi
+
 CWD=$(cd $(dirname $0) ; pwd)
 source $CWD/common-gasnet.bash
 
@@ -11,11 +16,6 @@ module load mpi
 set -x
 : confirm mpi module is loaded
 module list -l 2>&1 | grep -E '\bmpi/mpich\b' || exit $?
-
-export CHPL_TARGET_COMPILER=mpi-gnu
-export CHPL_TASKS=fifo
-export CHPL_COMM=gasnet
-export CHPL_COMM_SUBSTRATE=mpi
 
 export CHPL_NIGHTLY_TEST_DIRS="release/examples/hello*.chpl modules/packages/mpi"
 
