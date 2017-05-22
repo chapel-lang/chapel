aa2c6dbb17babbabe298af8550ed2aa8d1a4ffd2
Preston Sahabu <psahabu@cray.com>
2017-05-22 14:37:26 -0700

    revert emacs more

--- highlight/emacs/chpl-mode.el
+++ highlight/emacs/chpl-mode.el
@@ -177,6 +177,7 @@ or variable identifier (that's being defined)."
   "Keywords besides constants and operators that start primary expressions."
   chpl '("new" "delete")) ;; Not really a keyword, but practically works as one.
 
+
 (c-lang-defconst c-other-kwds
   "Keywords not accounted for by any other `*-kwds' language constant."
   chpl '("align" "atomic" "begin" "by" "cobegin" "coforall" "dmapped" "for" "forall" "if" "in" "inout" "local" "noinit" "on" "out" "reduce" "ref" "scan" "serial" "single" "sparse" "sync" "where" "while" "with" "zip"))
