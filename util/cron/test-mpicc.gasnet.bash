28e3e70b16f80edfffe1ad4f41b8f266e7a33554
Ben Albrecht <benalbrecht@pitt.edu>
2017-05-23 09:41:18 -0700

    Missed 1 redundant env var

--- util/cron/test-mpicc.gasnet.bash
+++ util/cron/test-mpicc.gasnet.bash
@@ -6,7 +6,6 @@ CWD=$(cd $(dirname $0) ; pwd)
 source $CWD/common-mpicc.bash
 source $CWD/common-gasnet.bash
 
-export CHPL_TARGET_COMPILER=mpi-gnu
 export CHPL_COMM_SUBSTRATE=mpi
 
 export CHPL_NIGHTLY_TEST_DIRS="release/examples/hello*.chpl modules/packages/mpi"
