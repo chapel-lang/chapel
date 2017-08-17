dfcad387624bae87f97e0c605b66fec7fa91b970
coodie <poppinggoovie@gmail.com>
2017-08-17 23:18:04 +0200

    Remove generation to functions that aren't de facto builtins and describe method to check if function is de facto builtin

--- clang_builtin_gen.py
+++ clang_builtin_gen.py
@@ -68,76 +68,111 @@ def genBuiltin(functionName, typeString, attrs):
             argName for (argType, argName) in zip(argTypes, argNames)]
 
     print(
-        "static " + returnType + " " + functionName + "(" + ", ".join(args) + ")\n" +
+        "static inline " + returnType + " " + functionName + "(" + ", ".join(args) + ")\n" +
         "{\n" +
         "  return " + "__builtin_" + functionName + "(" + ", ".join(argNames) + ");\n" +
         "}\n"
     )
 
-# Standard libc/libm functions:
-genBuiltin("cabs", "dXd", "Fnc")
-genBuiltin("cabsf", "fXf", "Fnc")
-genBuiltin("cabsl", "LdXLd", "Fnc")
-genBuiltin("cacos", "XdXd", "Fnc")
-genBuiltin("cacosf", "XfXf", "Fnc")
-genBuiltin("cacosh", "XdXd", "Fnc")
-genBuiltin("cacoshf", "XfXf", "Fnc")
-genBuiltin("cacoshl", "XLdXLd", "Fnc")
-genBuiltin("cacosl", "XLdXLd", "Fnc")
-genBuiltin("carg", "dXd", "Fnc")
-genBuiltin("cargf", "fXf", "Fnc")
-genBuiltin("cargl", "LdXLd", "Fnc")
-genBuiltin("casin", "XdXd", "Fnc")
-genBuiltin("casinf", "XfXf", "Fnc")
-genBuiltin("casinh", "XdXd", "Fnc")
-genBuiltin("casinhf", "XfXf", "Fnc")
-genBuiltin("casinhl", "XLdXLd", "Fnc")
-genBuiltin("casinl", "XLdXLd", "Fnc")
-genBuiltin("catan", "XdXd", "Fnc")
-genBuiltin("catanf", "XfXf", "Fnc")
-genBuiltin("catanh", "XdXd", "Fnc")
-genBuiltin("catanhf", "XfXf", "Fnc")
-genBuiltin("catanhl", "XLdXLd", "Fnc")
-genBuiltin("catanl", "XLdXLd", "Fnc")
-genBuiltin("ccos", "XdXd", "Fnc")
-genBuiltin("ccosf", "XfXf", "Fnc")
-genBuiltin("ccosl", "XLdXLd", "Fnc")
-genBuiltin("ccosh", "XdXd", "Fnc")
-genBuiltin("ccoshf", "XfXf", "Fnc")
-genBuiltin("ccoshl", "XLdXLd", "Fnc")
-genBuiltin("cexp", "XdXd", "Fnc")
-genBuiltin("cexpf", "XfXf", "Fnc")
-genBuiltin("cexpl", "XLdXLd", "Fnc")
 genBuiltin("cimag", "dXd", "Fnc")
 genBuiltin("cimagf", "fXf", "Fnc")
 genBuiltin("cimagl", "LdXLd", "Fnc")
-genBuiltin("conj", "XdXd", "Fnc")
-genBuiltin("conjf", "XfXf", "Fnc")
-genBuiltin("conjl", "XLdXLd", "Fnc")
-genBuiltin("clog", "XdXd", "Fnc")
-genBuiltin("clogf", "XfXf", "Fnc")
-genBuiltin("clogl", "XLdXLd", "Fnc")
-genBuiltin("cproj", "XdXd", "Fnc")
-genBuiltin("cprojf", "XfXf", "Fnc")
-genBuiltin("cprojl", "XLdXLd", "Fnc")
-genBuiltin("cpow", "XdXdXd", "Fnc")
-genBuiltin("cpowf", "XfXfXf", "Fnc")
-genBuiltin("cpowl", "XLdXLdXLd", "Fnc")
+
 genBuiltin("creal", "dXd", "Fnc")
 genBuiltin("crealf", "fXf", "Fnc")
 genBuiltin("creall", "LdXLd", "Fnc")
-genBuiltin("csin", "XdXd", "Fnc")
-genBuiltin("csinf", "XfXf", "Fnc")
-genBuiltin("csinl", "XLdXLd", "Fnc")
-genBuiltin("csinh", "XdXd", "Fnc")
-genBuiltin("csinhf", "XfXf", "Fnc")
-genBuiltin("csinhl", "XLdXLd", "Fnc")
-genBuiltin("csqrt", "XdXd", "Fnc")
-genBuiltin("csqrtf", "XfXf", "Fnc")
-genBuiltin("csqrtl", "XLdXLd", "Fnc")
-genBuiltin("ctan", "XdXd", "Fnc")
-genBuiltin("ctanf", "XfXf", "Fnc")
-genBuiltin("ctanl", "XLdXLd", "Fnc")
-genBuiltin("ctanh", "XdXd", "Fnc")
-genBuiltin("ctanhf", "XfXf", "Fnc")
-genBuiltin("ctanhl", "XLdXLd", "Fnc")
+
+genBuiltin("conj", "XdXd", "Fnc")
+genBuiltin("conjf", "XfXf", "Fnc")
+genBuiltin("conjl", "XLdXLd", "Fnc")
+
+# List of commented out clang builtins:
+# that get expanded to themselves:
+# like __builtin_cabs(x) gets expanded to cabs(x)
+# so function:
+#
+# static inline double cabs(_Complex double c)
+# {
+#   return __builtin_cabs(c);
+# }
+#
+# effectively gets expanded to:
+#
+# static inline double cabs(_Complex double c)
+# {
+#   return cabs(c);
+# }
+#
+# Which is a disaster since it is trivialy self-recursive function.
+#
+# Best way to see if certain function is de facto builtin,
+# is to have a file with call to certain builtin, like:
+#
+# static inline double cabs(_Complex double c)
+# {
+#   return __builtin_cabs(c);
+# }
+# then generate its LLVM IR:
+# clang cabs_inlined.c -S -emit-llvm
+# and look into .ll file if the function seems to be doing what it should
+# if there is generated call to itself it's likely not to be handled.
+# At moment when this script was being written there was no standard way
+# to see if function was really a builtin.
+
+# genBuiltin("cabs", "dXd", "Fnc")
+# genBuiltin("cabsf", "fXf", "Fnc")
+# genBuiltin("cabsl", "LdXLd", "Fnc")
+# genBuiltin("cacos", "XdXd", "Fnc")
+# genBuiltin("cacosf", "XfXf", "Fnc")
+# genBuiltin("cacosh", "XdXd", "Fnc")
+# genBuiltin("cacoshf", "XfXf", "Fnc")
+# genBuiltin("cacoshl", "XLdXLd", "Fnc")
+# genBuiltin("cacosl", "XLdXLd", "Fnc")
+# genBuiltin("carg", "dXd", "Fnc")
+# genBuiltin("cargf", "fXf", "Fnc")
+# genBuiltin("cargl", "LdXLd", "Fnc")
+# genBuiltin("casin", "XdXd", "Fnc")
+# genBuiltin("casinf", "XfXf", "Fnc")
+# genBuiltin("casinh", "XdXd", "Fnc")
+# genBuiltin("casinhf", "XfXf", "Fnc")
+# genBuiltin("casinhl", "XLdXLd", "Fnc")
+# genBuiltin("casinl", "XLdXLd", "Fnc")
+# genBuiltin("catan", "XdXd", "Fnc")
+# genBuiltin("catanf", "XfXf", "Fnc")
+# genBuiltin("catanh", "XdXd", "Fnc")
+# genBuiltin("catanhf", "XfXf", "Fnc")
+# genBuiltin("catanhl", "XLdXLd", "Fnc")
+# genBuiltin("catanl", "XLdXLd", "Fnc")
+# genBuiltin("ccos", "XdXd", "Fnc")
+# genBuiltin("ccosf", "XfXf", "Fnc")
+# genBuiltin("ccosl", "XLdXLd", "Fnc")
+# genBuiltin("ccosh", "XdXd", "Fnc")
+# genBuiltin("ccoshf", "XfXf", "Fnc")
+# genBuiltin("ccoshl", "XLdXLd", "Fnc")
+# genBuiltin("cexp", "XdXd", "Fnc")
+# genBuiltin("cexpf", "XfXf", "Fnc")
+# genBuiltin("cexpl", "XLdXLd", "Fnc")
+# genBuiltin("clog", "XdXd", "Fnc")
+# genBuiltin("clogf", "XfXf", "Fnc")
+# genBuiltin("clogl", "XLdXLd", "Fnc")
+# genBuiltin("cproj", "XdXd", "Fnc")
+# genBuiltin("cprojf", "XfXf", "Fnc")
+# genBuiltin("cprojl", "XLdXLd", "Fnc")
+# genBuiltin("cpow", "XdXdXd", "Fnc")
+# genBuiltin("cpowf", "XfXfXf", "Fnc")
+# genBuiltin("cpowl", "XLdXLdXLd", "Fnc")
+# genBuiltin("csin", "XdXd", "Fnc")
+# genBuiltin("csinf", "XfXf", "Fnc")
+# genBuiltin("csinl", "XLdXLd", "Fnc")
+# genBuiltin("csinh", "XdXd", "Fnc")
+# genBuiltin("csinhf", "XfXf", "Fnc")
+# genBuiltin("csinhl", "XLdXLd", "Fnc")
+# genBuiltin("csqrt", "XdXd", "Fnc")
+# genBuiltin("csqrtf", "XfXf", "Fnc")
+# genBuiltin("csqrtl", "XLdXLd", "Fnc")
+# genBuiltin("ctan", "XdXd", "Fnc")
+# genBuiltin("ctanf", "XfXf", "Fnc")
+# genBuiltin("ctanl", "XLdXLd", "Fnc")
+# genBuiltin("ctanh", "XdXd", "Fnc")
+# genBuiltin("ctanhf", "XfXf", "Fnc")
+# genBuiltin("ctanhl", "XLdXLd", "Fnc")
