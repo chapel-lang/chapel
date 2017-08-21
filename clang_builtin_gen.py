0381fa8fbc0f27c28d7741d75ab72a91a22885dc
coodie <poppinggoovie@gmail.com>
2017-08-21 15:45:54 +0200

    Is based on Builtins.def now

--- clang_builtin_gen.py
+++ clang_builtin_gen.py
@@ -1,7 +1,26 @@
-# This script generates C header that wraps some builtins from clang
-# This has been heavily inspired by "include/clang/Basic/Builtins.def" header
-# in clang sources (version 5.0), so that it is practically possible to copy
-# paste some of builtins provided by clang with little editing effort.
+#!/usr/bin/python3
+
+import sys
+import re
+
+builtins = {
+    "cimag",
+    "cimagf",
+    "cimagl",
+    "creal",
+    "crealf",
+    "creall",
+    "conj",
+    "conjf",
+    "conjl",
+    "memcpy",
+}
+
+# Print to stderr
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 types = {
     "v": "void",
@@ -46,6 +65,7 @@ def parseType(typeString):
     while i < len(typeString) and typeString[i] in typePostfix:
         res.append(typePostfix[typeString[i]])
         i += 1
+
     return (typeString[i:], " ".join(res))
 
 
@@ -57,122 +77,104 @@ def iterTypes(typeString):
         check = parseType(rest)
 
 
-def genBuiltin(functionName, typeString, attrs):
-    functionTypes = list(iterTypes(typeString))
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
-    returnType = functionTypes[0]
-    argTypes = functionTypes[1:]
-    argNames = ["arg" + str(i) for i in range(len(argTypes))]
+# Dictionary, for faster lookups
+builtinsDict = dict()
 
-    args = [argType + " " +
-            argName for (argType, argName) in zip(argTypes, argNames)]
 
-    print(
-        "static inline " + returnType + " " + functionName + "(" + ", ".join(args) + ")\n" +
-        "{\n" +
-        "  return " + "__builtin_" + functionName + "(" + ", ".join(argNames) + ");\n" +
-        "}\n"
-    )
+class ClangBuiltin:
 
-genBuiltin("cimag", "dXd", "Fnc")
-genBuiltin("cimagf", "fXf", "Fnc")
-genBuiltin("cimagl", "LdXLd", "Fnc")
+    def __init__(self, functionName, functionType, attrs):
+        self.functionName = functionName
+        self.functionType = functionType
+        self.attrs = attrs
 
-genBuiltin("creal", "dXd", "Fnc")
-genBuiltin("crealf", "fXf", "Fnc")
-genBuiltin("creall", "LdXLd", "Fnc")
+    def libmPrefixedWithBuiltin(self):
+        return "F" in self.attrs
 
-genBuiltin("conj", "XdXd", "Fnc")
-genBuiltin("conjf", "XfXf", "Fnc")
-genBuiltin("conjl", "XLdXLd", "Fnc")
 
-# List of commented out clang builtins:
-# that get expanded to themselves:
-# like __builtin_cabs(x) gets expanded to cabs(x)
-# so function:
-#
-# static inline double cabs(_Complex double c)
-# {
-#   return __builtin_cabs(c);
-# }
-#
-# effectively gets expanded to:
-#
-# static inline double cabs(_Complex double c)
-# {
-#   return cabs(c);
-# }
-#
-# Which is a disaster since it is trivialy self-recursive function.
-#
-# Best way to see if certain function is de facto builtin,
-# is to have a file with call to certain builtin, like:
-#
-# static inline double cabs(_Complex double c)
-# {
-#   return __builtin_cabs(c);
-# }
-# then generate its LLVM IR:
-# clang cabs_inlined.c -S -emit-llvm
-# and look into .ll file if the function seems to be doing what it should
-# if there is generated call to itself it's likely not to be handled.
-# At moment when this script was being written there was no standard way
-# to see if function was really a builtin.
+class ClangLibBuiltin(ClangBuiltin):
 
-# genBuiltin("cabs", "dXd", "Fnc")
-# genBuiltin("cabsf", "fXf", "Fnc")
-# genBuiltin("cabsl", "LdXLd", "Fnc")
-# genBuiltin("cacos", "XdXd", "Fnc")
-# genBuiltin("cacosf", "XfXf", "Fnc")
-# genBuiltin("cacosh", "XdXd", "Fnc")
-# genBuiltin("cacoshf", "XfXf", "Fnc")
-# genBuiltin("cacoshl", "XLdXLd", "Fnc")
-# genBuiltin("cacosl", "XLdXLd", "Fnc")
-# genBuiltin("carg", "dXd", "Fnc")
-# genBuiltin("cargf", "fXf", "Fnc")
-# genBuiltin("cargl", "LdXLd", "Fnc")
-# genBuiltin("casin", "XdXd", "Fnc")
-# genBuiltin("casinf", "XfXf", "Fnc")
-# genBuiltin("casinh", "XdXd", "Fnc")
-# genBuiltin("casinhf", "XfXf", "Fnc")
-# genBuiltin("casinhl", "XLdXLd", "Fnc")
-# genBuiltin("casinl", "XLdXLd", "Fnc")
-# genBuiltin("catan", "XdXd", "Fnc")
-# genBuiltin("catanf", "XfXf", "Fnc")
-# genBuiltin("catanh", "XdXd", "Fnc")
-# genBuiltin("catanhf", "XfXf", "Fnc")
-# genBuiltin("catanhl", "XLdXLd", "Fnc")
-# genBuiltin("catanl", "XLdXLd", "Fnc")
-# genBuiltin("ccos", "XdXd", "Fnc")
-# genBuiltin("ccosf", "XfXf", "Fnc")
-# genBuiltin("ccosl", "XLdXLd", "Fnc")
-# genBuiltin("ccosh", "XdXd", "Fnc")
-# genBuiltin("ccoshf", "XfXf", "Fnc")
-# genBuiltin("ccoshl", "XLdXLd", "Fnc")
-# genBuiltin("cexp", "XdXd", "Fnc")
-# genBuiltin("cexpf", "XfXf", "Fnc")
-# genBuiltin("cexpl", "XLdXLd", "Fnc")
-# genBuiltin("clog", "XdXd", "Fnc")
-# genBuiltin("clogf", "XfXf", "Fnc")
-# genBuiltin("clogl", "XLdXLd", "Fnc")
-# genBuiltin("cproj", "XdXd", "Fnc")
-# genBuiltin("cprojf", "XfXf", "Fnc")
-# genBuiltin("cprojl", "XLdXLd", "Fnc")
-# genBuiltin("cpow", "XdXdXd", "Fnc")
-# genBuiltin("cpowf", "XfXfXf", "Fnc")
-# genBuiltin("cpowl", "XLdXLdXLd", "Fnc")
-# genBuiltin("csin", "XdXd", "Fnc")
-# genBuiltin("csinf", "XfXf", "Fnc")
-# genBuiltin("csinl", "XLdXLd", "Fnc")
-# genBuiltin("csinh", "XdXd", "Fnc")
-# genBuiltin("csinhf", "XfXf", "Fnc")
-# genBuiltin("csinhl", "XLdXLd", "Fnc")
-# genBuiltin("csqrt", "XdXd", "Fnc")
-# genBuiltin("csqrtf", "XfXf", "Fnc")
-# genBuiltin("csqrtl", "XLdXLd", "Fnc")
-# genBuiltin("ctan", "XdXd", "Fnc")
-# genBuiltin("ctanf", "XfXf", "Fnc")
-# genBuiltin("ctanl", "XLdXLd", "Fnc")
-# genBuiltin("ctanh", "XdXd", "Fnc")
-# genBuiltin("ctanhf", "XfXf", "Fnc")
-# genBuiltin("ctanhl", "XLdXLd", "Fnc")
+    def __init__(self, functionName, functionType, attrs, libHeader, supportedLangs):
+        ClangBuiltin.__init__(self, functionName, functionType, attrs)
+        self.libHeader = libHeader
+        self.supportedLangs = supportedLangs
+
+    def canGenerateWrapper(self):
+        wrappedBuiltinName = "__builtin_" + self.functionName
+        builtinCandidate = builtinsDict.get(wrappedBuiltinName)
+        if builtinCandidate and builtinCandidate.libmPrefixedWithBuiltin():
+            return True
+        return False
+
+    def generateWrapper(self):
+        functionTypes = list(iterTypes(self.functionType))
+
+        returnType = functionTypes[0]
+        argTypes = functionTypes[1:]
+        argNames = ["arg" + str(i) for i in range(len(argTypes))]
+
+        args = [argType + " " +
+                argName for (argType, argName) in zip(argTypes, argNames)]
+
+        return ("static inline " + returnType + " " + self.functionName + "(" + ", ".join(args) + ")\n" +
+                "{\n" +
+                "  return " + "__builtin_" + self.functionName + "(" + ", ".join(argNames) + ");\n" +
+                "}\n")
+
+
+def parseLibBuiltin(b):
+    regex = re.compile(
+        r"LIBBUILTIN\((?P<name>\w+)[, ]+\"(?P<type>[\w*.]+)\"[, ]+\"(?P<attrs>[\w+:]+)\"[, ]+\"(?P<header>[\w./-]+)\"[, ]+(?P<langs>\w+)\)")
+    result = regex.match(b)
+    if(result):
+        d = result.groupdict()
+        return ClangLibBuiltin(d["name"], d["type"], d["attrs"], d["header"], d["langs"])
+    return None
+
+
+def parseBuiltin(b):
+    regex = re.compile(
+        r"BUILTIN\((?P<name>\w+)[, ]+\"(?P<type>[\w*.]+)\"[, ]+\"(?P<attrs>[\w+:]+)\"\)")
+    result = regex.match(b)
+    if(result):
+        d = result.groupdict()
+        return ClangBuiltin(d["name"], d["type"], d["attrs"])
+    return None
+
+
+def main():
+    if len(sys.argv) > 1:
+        clangBuiltinsDefFile = sys.argv[1]
+    else:
+        eprint("No input file provided")
+        sys.exit(1)
+
+    # Parse Builtins.def
+    with open(clangBuiltinsDefFile) as f:
+        content = f.readlines()
+
+    for line in content:
+        result = None
+        if line.startswith("LIBBUILTIN"):
+            result = parseLibBuiltin(line)
+        if line.startswith("BUILTIN"):
+            result = parseBuiltin(line)
+        if result:
+            builtinsDict[result.functionName] = result
+
+    result = []
+    for b in builtins:
+        builtinCandidate = builtinsDict.get(b)
+        if builtinCandidate and builtinCandidate.canGenerateWrapper():
+            result.append(builtinCandidate.generateWrapper())
+        else:
+            eprint("Cannot generate wrapper for ", b)
+            sys.exit(1)
+
+    print("\n".join(result))
+
+
+main()
