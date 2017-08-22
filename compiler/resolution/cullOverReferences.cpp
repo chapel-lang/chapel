4b517ed36d3bc0ac8251d38a6eadabf0cd95ee14
Michael Ferguson <mppf@users.noreply.github.com>
2017-08-22 09:28:31 -0400

    Remove an assert for now

--- compiler/resolution/cullOverReferences.cpp
+++ compiler/resolution/cullOverReferences.cpp
@@ -781,7 +781,8 @@ void gatherLoopDetails(ForLoop*  forLoop,
   }
 
   bool forall = (chpl_iter != NULL);
-  INT_ASSERT(forall == forLoop->isLoweredForallLoop());
+  // MPF: should be the same as isLoweredForallLoop but it isn't yet
+  //INT_ASSERT(forall == forLoop->isLoweredForallLoop());
   bool zippered = forLoop->zipperedGet() &&
                   (iterator->type->symbol->hasFlag(FLAG_TUPLE) ||
                    (chpl_iter != NULL &&
