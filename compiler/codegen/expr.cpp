85edae512bdb0207a1af7ce87be19fbd05bfd8a4
Przemysław Leśniak <poppinggoovie@gmail.com>
2017-08-29 01:09:36 +0200

    [GSoC] Generate nsw for signed integers operations (#7092)
    [GSoC] Generate nsw for signed integers operations (#7092)
    
    [work done by @coodie , testing and review by @mppf and @dmk42 , merged by @dmk42 ]
    
    A couple of months ago I added 'nsw' (meaning undefined behaviour when overflowing on signed operations instead of wrapping) keyword to add instruction in LLVM IR since it was related to vectorization not occurring. According to #6160 discussion there was a little inconsistency until this PR, because multiplications and subtractions would wrap while adds wouldn't, this tiny PR fixes that.

--- compiler/codegen/expr.cpp
+++ compiler/codegen/expr.cpp
@@ -1645,7 +1645,7 @@ GenRet codegenSub(GenRet a, GenRet b)
       // with a negative value.
       INT_ASSERT(bv.val->getType()->isIntegerTy());
       GenRet negbv;
-      negbv.val = info->builder->CreateNeg(bv.val);
+      negbv.val = info->builder->CreateNSWNeg(bv.val);
       negbv.isUnsigned = false;
       ret = codegenAdd(av, negbv);
     } else {
@@ -1654,7 +1654,7 @@ GenRet codegenSub(GenRet a, GenRet b)
       if(values.a->getType()->isFPOrFPVectorTy()) {
         ret.val = info->builder->CreateFSub(values.a, values.b);
       } else {
-        ret.val = info->builder->CreateSub(values.a, values.b);
+        ret.val = info->builder->CreateSub(values.a, values.b, "", false, values.isSigned);
       }
       ret.isUnsigned = !values.isSigned;
     }
@@ -1681,7 +1681,9 @@ GenRet codegenNeg(GenRet a)
     } else if(value->getType()->isFPOrFPVectorTy()) {
       ret.val = info->builder->CreateFNeg(value);
     } else {
-      ret.val = info->builder->CreateNeg(value);
+      bool av_signed = false;
+      if(av.chplType) av_signed = is_signed(av.chplType);
+      ret.val = info->builder->CreateNeg(value, "", false, av_signed);
     }
     ret.isUnsigned = false;
 #endif
@@ -1716,7 +1718,7 @@ GenRet codegenMul(GenRet a, GenRet b)
       if(values.a->getType()->isFPOrFPVectorTy()) {
         ret.val = info->builder->CreateFMul(values.a, values.b);
       } else {
-        ret.val = info->builder->CreateMul(values.a, values.b);
+        ret.val = info->builder->CreateMul(values.a, values.b, "", false, values.isSigned);
       }
       ret.isUnsigned = !values.isSigned;
     }
@@ -1807,7 +1809,9 @@ GenRet codegenLsh(GenRet a, GenRet b)
 #ifdef HAVE_LLVM
     llvm::Value* amt = convertValueToType(bv.val, av.val->getType(),
                                           is_signed(bv.chplType));
-    ret.val = info->builder->CreateShl(av.val, amt);
+    bool av_signed = false;
+    if(av.chplType) av_signed = is_signed(av.chplType);
+    ret.val = info->builder->CreateShl(av.val, amt, "", false, av_signed);
 #endif
   }
   return ret;
