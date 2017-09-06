e84ad9322d1b8d3942ef5b3a0365d73cf3df70e7
Brad Chamberlain <bradc@cray.com>
2017-09-06 17:33:15 -0500

    Add and update comments to playground script
    * added comments describing how to update the script
    * updated an out-of-date comment that I missed in doing my update

--- util/cron/test-perf.chapcs.playground.bash
+++ util/cron/test-perf.chapcs.playground.bash
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 #
 
+#
+# Instructions: To customize this script to time a given branch, see
+# the numbered steps in the comment block below.
+#
+
 CWD=$(cd $(dirname $0) ; pwd)
 
 export CHPL_TEST_PERF_CONFIG_NAME='chapcs'
@@ -9,7 +14,15 @@ source $CWD/common-perf.bash
 
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.chapcs.playground"
 
-# Test performance of implementation changes in forall intents
+#
+# 0) Update the comment that follows this block to describe what's being tested
+# 1) Update GITHUB_USER to the GitHub username owning the branch
+# 2) Update GITHUB_BRANCH to the name of the GitHub branch under that user
+# 3) Update SHORT_NAME to be a short name that will be used in graph generation
+# 4) Update START_DATE to be today
+#
+
+# Test performance of making alignment fields 'void' for non-stridable ranges
 GITHUB_USER=bradcray
 GITHUB_BRANCH=range-void-align-fields
 SHORT_NAME=void-aligns
