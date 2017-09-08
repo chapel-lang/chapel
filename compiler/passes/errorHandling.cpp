e46b7f74d4ff01fc9d3770195894195009d05098
Michael Ferguson <mppf@users.noreply.github.com>
2017-09-08 10:41:30 -0400

    Temporarily change default to ERROR_MODE_FATAL

--- compiler/passes/errorHandling.cpp
+++ compiler/passes/errorHandling.cpp
@@ -899,7 +899,9 @@ static error_checking_mode_t computeErrorCheckingMode(FnSymbol* fn)
     if (mod->hasFlag(FLAG_IMPLICIT_MODULE))
       mode = ERROR_MODE_FATAL;
     else
-      mode = ERROR_MODE_RELAXED;
+      mode = ERROR_MODE_FATAL; // TODO: this should be ERROR_MODE_RELAXED
+                               // this is commented out for now to separate
+                               // PRs.
   }
 
   return mode;
