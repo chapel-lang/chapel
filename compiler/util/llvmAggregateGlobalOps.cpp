78c177f467d3d380ea330580d4bb02e2758bb6cd
David Keaton <dmk42@users.noreply.github.com>
2017-09-26 19:11:49 -0700

    Change LLVM dump() to print()

--- compiler/util/llvmAggregateGlobalOps.cpp
+++ compiler/util/llvmAggregateGlobalOps.cpp
@@ -49,6 +49,7 @@
 
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Debug.h"
 #include "llvm/ADT/SmallSet.h"
 #include "llvm/ADT/Statistic.h"
 
@@ -223,11 +224,15 @@ Instruction* reorderAddressingMemopsUses(Instruction *FirstLoadOrStore,
     // Leave loads/stores where they are (they will be removed)
     if( isa<StoreInst>(insn) || isa<LoadInst>(insn) ) {
       if( DebugThis ) {
-        errs() << "found load/store: "; insn->dump();
+        dbgs() << "found load/store: ";
+        insn->print(dbgs(), true);
+	dbgs() << '\n';
       }
     } else if( memopsUses.count(insn) ) {
       if( DebugThis ) {
-        errs() << "found memop use: "; insn->dump();
+        dbgs() << "found memop use: ";
+        insn->print(dbgs(), true);
+	dbgs() << '\n';
       }
       // Move uses of memops to after the final memop.
       insn->removeFromParent();
@@ -235,7 +240,9 @@ Instruction* reorderAddressingMemopsUses(Instruction *FirstLoadOrStore,
       LastMemopUse = insn;
     } else {
       if( DebugThis ) {
-        errs() << "found other: "; insn->dump();
+        dbgs() << "found other: ";
+        insn->print(dbgs(), true);
+        dbgs() << '\n';
       }
       // Move addressing instructions to before the first memop.
       insn->removeFromParent();
@@ -626,8 +633,9 @@ Instruction *AggregateGlobalOpsOpt::tryAggregating(Instruction *StartInst, Value
     StartPtr = Range.StartPtr;
 
     if( DebugThis ) {
-      errs() << "base is:";
-      StartPtr->dump();
+      dbgs() << "base is:";
+      StartPtr->print(dbgs(), true);
+      dbgs() << '\n';
     }
 
     // Determine alignment
@@ -662,8 +670,9 @@ Instruction *AggregateGlobalOpsOpt::tryAggregating(Instruction *StartInst, Value
         StoreInst* oldStore = cast<StoreInst>(*SI);
 
         if( DebugThis ) {
-          errs() << "have store in range:";
-          oldStore->dump();
+          dbgs() << "have store in range:";
+          oldStore->print(dbgs(), true);
+          dbgs() << '\n';
         }
 
         Value* ptrToAlloc = rebasePointer(oldStore->getPointerOperand(),
@@ -739,8 +748,9 @@ Instruction *AggregateGlobalOpsOpt::tryAggregating(Instruction *StartInst, Value
            SE = Range.TheStores.end(); SI != SE; ++SI) {
         LoadInst* oldLoad = cast<LoadInst>(*SI);
         if( DebugThis ) {
-          errs() << "have load in range:";
-          oldLoad->dump();
+          dbgs() << "have load in range:";
+          oldLoad->print(dbgs(), true);
+          dbgs() << '\n';
         }
 
         Value* ptrToAlloc = rebasePointer(oldLoad->getPointerOperand(),
@@ -800,8 +810,9 @@ bool AggregateGlobalOpsOpt::runOnFunction(Function &F) {
   // Walk all instruction in the function.
   for (Function::iterator BB = F.begin(), BBE = F.end(); BB != BBE; ++BB) {
     if( DebugThis ) {
-      errs() << "Working on BB ";
-      BB->dump();
+      dbgs() << "Working on BB ";
+      BB->print(dbgs(), true);
+      dbgs() << '\n';
     }
 
     for (BasicBlock::iterator BI = BB->begin(), BE = BB->end(); BI != BE;) {
@@ -824,8 +835,9 @@ bool AggregateGlobalOpsOpt::runOnFunction(Function &F) {
     }
 
     if( DebugThis && MadeChange ) {
-      errs() << "After transform BB is ";
-      BB->dump();
+      dbgs() << "After transform BB is ";
+      BB->print(dbgs(), true);
+      dbgs() << '\n';
     }
 
   }
