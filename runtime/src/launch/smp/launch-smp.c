a692b6d43dccc54ae437eca97c384a5695036f2c
Elliot Ronaghan <ronawho@gmail.com>
2017-10-10 16:52:05 -0700

    Add a smp launcher based off of our "dummy" launcher
    We need a way to set GASNET_PSHM_NODES, but we can't do that in our runtime
    because we only parse `--numLocales/-nl` after we init the comm layer.

---
+++ runtime/src/launch/smp/launch-smp.c
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2004-2017 Cray Inc.
+ * Other additional copyright holders may be indicated within.
+ *
+ * The entirety of this work is licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "chpllaunch.h"
+#include "chpl-mem.h"
+#include "error.h"
+
+
+static char* chpl_launch_create_command(int argc, char* argv[],
+                                        int32_t numLocales) {
+  int i;
+  int size;
+  char baseCommand[256];
+  char* command;
+
+  chpl_compute_real_binary_name(argv[0]);
+
+  sprintf(baseCommand, "%s", chpl_get_real_binary_name());
+
+  size = strlen(baseCommand) + 1;
+
+  for (i=1; i<argc; i++) {
+    size += strlen(argv[i]) + 3;
+  }
+
+  command = chpl_mem_allocMany(size, sizeof(char), CHPL_RT_MD_COMMAND_BUFFER, -1, 0);
+
+  sprintf(command, "%s", baseCommand);
+  for (i=1; i<argc; i++) {
+    strcat(command, " '");
+    strcat(command, argv[i]);
+    strcat(command, "'");
+  }
+
+  if (strlen(command)+1 > size) {
+    chpl_internal_error("buffer overflow");
+  }
+
+  return command;
+}
+
+
+int chpl_launch(int argc, char* argv[], int32_t numLocales) {
+  char numlocalesval[11]; // big enough for int32_t
+  snprintf(numlocalesval, sizeof(numlocalesval), "%d", (int)numLocales);
+  if (setenv("GASNET_PSHM_NODES", numlocalesval, 1) != 0) {
+    chpl_error("Cannot setenv(\"GASNET_PSHM_NODES\")", 0, 0);
+  }
+
+  return chpl_launch_using_system(chpl_launch_create_command(argc, argv, numLocales),
+                                  argv[0]);
+}
+
+
+int chpl_launch_handle_arg(int argc, char* argv[], int argNum,
+                           int32_t lineno, int32_t filename) {
+  return 0;
+}
+
+
+void chpl_launch_print_help(void) {
+}
