ce987c1794d1967fa4b9c33441b13ac29fb9f0e5
Michael Ferguson <mppf@users.noreply.github.com>
2017-10-12 20:52:28 -0400

    Add buildErrorStandin

--- compiler/include/build.h
+++ compiler/include/build.h
@@ -58,6 +58,8 @@ Expr* buildDotExpr(BaseAST* base, const char* member);
 Expr* buildDotExpr(const char* base, const char* member);
 
 BlockStmt* buildChapelStmt(Expr* expr = NULL);
+BlockStmt* buildErrorStandin();
+
 BlockStmt* buildUseStmt(CallExpr* modules);
 BlockStmt* buildUseStmt(Expr* mod, std::vector<OnlyRename*>* names, bool except);
 bool processStringInRequireStmt(const char* str, bool parseTime);
