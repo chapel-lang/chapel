af9e50c017277c7e476ecd2eec8956f1fe394cb3
Michael Ferguson <mppf@users.noreply.github.com>
2017-12-18 17:12:09 -0800

    Add a comment about the new warning

--- compiler/resolution/resolveFunction.cpp
+++ compiler/resolution/resolveFunction.cpp
@@ -488,6 +488,9 @@ static void insertUnrefForArrayReturn(FnSymbol* fn) {
             FnSymbol*  unrefFn   = NULL;
 
             if (rhsType->symbol->hasFlag(FLAG_ITERATOR_RECORD)) {
+              // This warning can go away after 1.17 if it gets annoying.
+              // In the meantime, it helps to identify cases that have
+              // different behavior between 1.16 and 1.17.
               USR_WARN(call, "proc returns an iterator that will be "
                              "immediately converted to an array");
             }
