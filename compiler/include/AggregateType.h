75ba5599b9a417edff84ed1dfcc68a3a7170862c
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2017-12-20 09:58:36 -0800

    Generate a copy initializer for non-generic records with initializers, always
    There are many situations where a record requires a copy of itself to be made,
    and it seems like an unnecessary burden for the user to have to always write
    a copy initializer if they provide even one explicit initializer.  So, generate
    a copy initializer, and use it unless there is a better match among the explicit
    initializers

--- compiler/include/AggregateType.h
+++ compiler/include/AggregateType.h
@@ -137,6 +137,8 @@ public:
   bool                        wantsDefaultInitializer();
   void                        buildDefaultInitializer();
 
+  void                        buildCopyInitializer();
+
   AggregateType*              instantiatedFrom;
 
   InitializerStyle            initializerStyle;
