fadde5934d097b5e2f4d4dd7b65d467042fcb148
Tony Wallace <awallace@cray.com>
2018-01-02 14:30:42 -0800

    Update copyrights for year 2018: chplvis
    cd $CHPL_HOME
    grep -r '2017 Cray Inc' tools/chplvis
    t="# above list of files #"
    sed -i -e 's/Copyright \(.*\)2017 Cray Inc./Copyright \12018 Cray Inc./' $t

--- tools/chplvis/ConcurrencyView.h
+++ tools/chplvis/ConcurrencyView.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2017 Cray Inc.
+ * Copyright 2016-2018 Cray Inc.
  * Other additional copyright holders may be indicated within.
  *
  * The entirety of this work is licensed under the Apache License,
