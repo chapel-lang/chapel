b2e8c6cf17b77ae3231482c88d863fb5c6b80776
Michael Ferguson <mppf@users.noreply.github.com>
2018-01-29 16:44:24 -0500

    Revert "Try disabling early dead code elimination"
    This reverts commit 1734f689287e219cbc6694131cf381a7bf4f0b5f.

--- compiler/resolution/functionResolution.cpp
+++ compiler/resolution/functionResolution.cpp
@@ -8328,10 +8328,9 @@ static void removeAggTypeFieldInfo();
 static void pruneResolvedTree() {
   removeUnusedFunctions();
 
-  /*
   if (fRemoveUnreachableBlocks) {
     deadBlockElimination();
-  }*/
+  }
 
   removeRandomPrimitives();
 
