db07a8229e9c1092d17822f42cd8db21611b0112
Elliot Ronaghan <ronawho@gmail.com>
2018-01-29 23:32:49 -0800

    Make some minor cleanups to venv activation script
    Remove some out of date comments and add a log wrapper

--- util/test/activate_chpl_test_venv.py
+++ util/test/activate_chpl_test_venv.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-
 import os
 import sys
 
@@ -11,42 +9,28 @@ sys.path.insert(0, os.path.abspath(chplenv_dir))
 from chplenv import chpl_home_utils
 from chplenv import chpl_make
 
-# Activate a virtualenv that has testing infrastructure requirements installed
-#
-# By default, we will try to use
-#   $CHPL_HOME/third-party/chpl-venv/install/$CHPL_HOST_PLATFORM/$python_version_dir/chpl-virtualenv
-# as our virtualenv. We then check for a sentinel file that test-venv
-# creates when it's been successfully installed and finally activate
-# the virtualenv.
-#
-# A user can also set CHPL_TEST_VENV_DIR to specify the path to a custom
-# virtualenv that will be activated. "none" is a special value that means skip
-# activating a virtualenv. If CHPL_TEST_VENV_DIR is set, a user is asserting
-# that their virtualenv or local install has the requirements available.
-#
-# Note that this method does not allow us to specify python versions to use and
-# instead will use the system default. Long term we probably want to have a
-# wrapper script that activates the virtualenv and just calls start_test.
-
 def error(message):
-    print('[Error: {0}]'.format(message))
+    sys.stdout.write('[Error: {0}]\n'.format(message))
     exit(1)
 
-def activate_venv():
+def log(message):
+    sys.stdout.write('[{0}]\n'.format(message))
 
+# Activate a virtualenv that has testing infrastructure requirements installed
+def activate_venv():
     venv_dir = chpl_home_utils.get_chpl_test_venv()
     default_venv_dir = chpl_home_utils.get_chpl_venv()
 
     # user asserts that system already has the required dependencies installed:
     if venv_dir == 'none':
-        print('[Skipping virtualenv activation because venv_dir={0}. test-venv '
-              'requirements must be available.]'.format(venv_dir))
+        log('Skipping virtualenv activation because CHPL_TEST_VENV_DIR={0}. '
+            'test-venv requirements must be available.'.format(venv_dir))
 
     else:
         # using custom venv, does not check that our test requirements are met
         if venv_dir != default_venv_dir:
-            print('[Using custom virtualenv because venv_dir={1}. test-venv '
-                  'requirements must be available]'.format(venv_dir))
+            log('Using custom virtualenv because CHPL_TEST_VENV_DIR={0}. '
+                'test-venv requirements must be available'.format(venv_dir))
 
         # check Chapel test-venv for successful installation sentinel
         else:
