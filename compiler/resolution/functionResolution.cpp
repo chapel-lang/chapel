e873874e6fa5004d68d6f3168ef524f1c345724f
Ben Harshbarger <ben.harshb@gmail.com>
2018-02-16 10:06:50 -0800

    Do not attempt to copy-init syncs or singles. They must use chpl__intiCopy for the foreseeable future

--- compiler/resolution/functionResolution.cpp
+++ compiler/resolution/functionResolution.cpp
@@ -5031,6 +5031,10 @@ static void resolveInitVar(CallExpr* call) {
   Symbol*  src     = srcExpr->symbol();
   Type*    srcType = src->type;
 
+  if (call->id == breakOnResolveID) {
+    gdbShouldBreakHere();
+  }
+
   if (dst->hasFlag(FLAG_NO_COPY)               == true)  {
     call->primitive = primitives[PRIM_MOVE];
     resolveMove(call);
@@ -5039,7 +5043,9 @@ static void resolveInitVar(CallExpr* call) {
     call->primitive = primitives[PRIM_MOVE];
     resolveMove(call);
 
-  } else if (isRecordWithInitializers(srcType) == true)  {
+  } else if (isRecordWithInitializers(srcType) == true &&
+             isSyncType(srcType) == false &&
+             isSingleType(srcType) == false)  {
     AggregateType* ct  = toAggregateType(srcType);
     SymExpr*       rhs = toSymExpr(call->get(2));
 
