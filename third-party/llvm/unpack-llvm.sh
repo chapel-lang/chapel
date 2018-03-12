acf7ca614ce002d621e6bb32ca6e99285fc3183a
David Keaton <dmk42@users.noreply.github.com>
2018-03-12 16:51:35 -0700

    Don't make LLVM 6 the default yet

--- third-party/llvm/unpack-llvm.sh
+++ third-party/llvm/unpack-llvm.sh
@@ -15,12 +15,12 @@ echo Unpacking LLVM
 #   Makefile.share-system
 #   doc/usingchapel/chplenv.rst
 
-tar xf llvm-6.0.0.src.tar.xz
-tar xf cfe-6.0.0.src.tar.xz
-mv llvm-6.0.0.src llvm
-mv cfe-6.0.0.src llvm/tools/clang
-patch -p0 < llvm-4.0.1-BasicAliasAnalysis-patch.txt
-patch -p0 < llvm-4.0.1-ValueTracking-patch.txt
+#tar xf llvm-6.0.0.src.tar.xz
+#tar xf cfe-6.0.0.src.tar.xz
+#mv llvm-6.0.0.src llvm
+#mv cfe-6.0.0.src llvm/tools/clang
+#patch -p0 < llvm-4.0.1-BasicAliasAnalysis-patch.txt
+#patch -p0 < llvm-4.0.1-ValueTracking-patch.txt
 
 #tar xf llvm-5.0.1.src.tar.xz
 #tar xf cfe-5.0.1.src.tar.xz
@@ -29,12 +29,12 @@ patch -p0 < llvm-4.0.1-ValueTracking-patch.txt
 #patch -p0 < llvm-4.0.1-BasicAliasAnalysis-patch.txt
 #patch -p0 < llvm-4.0.1-ValueTracking-patch.txt
 
-#tar xf llvm-4.0.1.src.tar.xz
-#tar xf cfe-4.0.1.src.tar.xz
-#mv llvm-4.0.1.src llvm
-#mv cfe-4.0.1.src llvm/tools/clang
-#patch -p0 < llvm-4.0.1-BasicAliasAnalysis-patch.txt
-#patch -p0 < llvm-4.0.1-ValueTracking-patch.txt
+tar xf llvm-4.0.1.src.tar.xz
+tar xf cfe-4.0.1.src.tar.xz
+mv llvm-4.0.1.src llvm
+mv cfe-4.0.1.src llvm/tools/clang
+patch -p0 < llvm-4.0.1-BasicAliasAnalysis-patch.txt
+patch -p0 < llvm-4.0.1-ValueTracking-patch.txt
 
 #tar xf llvm-3.9.1.src.tar.xz
 #tar xf cfe-3.9.1.src.tar.xz
