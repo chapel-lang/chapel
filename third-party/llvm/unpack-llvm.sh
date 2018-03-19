302617cb1efd87045254a73dc57056bca93bf4ae
David Keaton <dmk42@users.noreply.github.com>
2018-03-19 13:15:47 -0700

    Make LLVM 6 the default version of LLVM

--- third-party/llvm/unpack-llvm.sh
+++ third-party/llvm/unpack-llvm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -d llvm ]
+if [ -d llvm-src ]
 then
 
 echo LLVM and CLANG already unpacked.
@@ -15,65 +15,25 @@ echo Unpacking LLVM
 #   Makefile.share-system
 #   doc/usingchapel/chplenv.rst
 
-#tar xf llvm-6.0.0.src.tar.xz
-#tar xf cfe-6.0.0.src.tar.xz
-#mv llvm-6.0.0.src llvm
-#mv cfe-6.0.0.src llvm/tools/clang
-#patch -p0 < llvm-4.0.1-BasicAliasAnalysis-patch.txt
-#patch -p0 < llvm-4.0.1-ValueTracking-patch.txt
+tar xf llvm-6.0.0.src.tar.xz
+tar xf cfe-6.0.0.src.tar.xz
+mv llvm-6.0.0.src llvm-src
+mv cfe-6.0.0.src llvm-src/tools/clang
+patch -p0 < llvm-6.0.0-BasicAliasAnalysis-patch.txt
+patch -p0 < llvm-6.0.0-ValueTracking-patch.txt
 
 #tar xf llvm-5.0.1.src.tar.xz
 #tar xf cfe-5.0.1.src.tar.xz
-#mv llvm-5.0.1.src llvm
-#mv cfe-5.0.1.src llvm/tools/clang
+#mv llvm-5.0.1.src llvm-src
+#mv cfe-5.0.1.src llvm-src/tools/clang
 #patch -p0 < llvm-4.0.1-BasicAliasAnalysis-patch.txt
 #patch -p0 < llvm-4.0.1-ValueTracking-patch.txt
 
-tar xf llvm-4.0.1.src.tar.xz
-tar xf cfe-4.0.1.src.tar.xz
-mv llvm-4.0.1.src llvm
-mv cfe-4.0.1.src llvm/tools/clang
-patch -p0 < llvm-4.0.1-BasicAliasAnalysis-patch.txt
-patch -p0 < llvm-4.0.1-ValueTracking-patch.txt
-
-#tar xf llvm-3.9.1.src.tar.xz
-#tar xf cfe-3.9.1.src.tar.xz
-#mv llvm-3.9.1.src llvm
-#mv cfe-3.9.1.src llvm/tools/clang
-
-#tar xf llvm-3.8.1.src.tar.xz
-#tar xf cfe-3.8.1.src.tar.xz
-#mv llvm-3.8.1.src llvm
-#mv cfe-3.8.1.src llvm/tools/clang
-
-#tar xf llvm-3.7.0.src.tar.xz
-#tar xf cfe-3.7.0.src.tar.xz
-#mv llvm-3.7.0.src llvm
-#mv cfe-3.7.0.src llvm/tools/clang
-
-#tar xf llvm-3.6.0.src.tar.xz
-#tar xf cfe-3.6.0.src.tar.xz
-#mv llvm-3.6.0.src llvm
-#mv cfe-3.6.0.src llvm/tools/clang
-
-#tar xf llvm-3.5.1.src.tar.xz
-#tar xf cfe-3.5.1.src.tar.xz
-#mv llvm-3.5.1.src llvm
-#mv cfe-3.5.1.src llvm/tools/clang
-
-#tar xzf llvm-3.4.2.src.tar.gz
-#tar xzf cfe-3.4.2.src.tar.gz
-#mv llvm-3.4.2.src llvm
-#mv cfe-3.4.2.src llvm/tools/clang
-
-#tar xzf llvm-3.3.src.tar.gz
-#tar xzf cfe-3.3.src.tar.gz
-#mv llvm-3.3.src llvm
-#mv cfe-3.3.src llvm/tools/clang
-# steps below are only for 3.3
-#echo Applying SROA address space patch for PR15907
-#cd llvm
-#patch -p0 < ../sroa-patch.txt
-#cd ..
+#tar xf llvm-4.0.1.src.tar.xz
+#tar xf cfe-4.0.1.src.tar.xz
+#mv llvm-4.0.1.src llvm-src
+#mv cfe-4.0.1.src llvm-src/tools/clang
+#patch -p0 < llvm-4.0.1-BasicAliasAnalysis-patch.txt
+#patch -p0 < llvm-4.0.1-ValueTracking-patch.txt
 
 fi
