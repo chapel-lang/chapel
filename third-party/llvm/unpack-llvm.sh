453be433433b95448ab0650883c48db3bbe02cb2
David Keaton <dmk42@users.noreply.github.com>
2018-03-19 17:01:35 -0700

    Revert to llvm from llvm-src temporarily for safety

--- third-party/llvm/unpack-llvm.sh
+++ third-party/llvm/unpack-llvm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -d llvm-src ]
+if [ -d llvm ]
 then
 
 echo LLVM and CLANG already unpacked.
@@ -17,22 +17,22 @@ echo Unpacking LLVM
 
 tar xf llvm-6.0.0.src.tar.xz
 tar xf cfe-6.0.0.src.tar.xz
-mv llvm-6.0.0.src llvm-src
-mv cfe-6.0.0.src llvm-src/tools/clang
+mv llvm-6.0.0.src llvm
+mv cfe-6.0.0.src llvm/tools/clang
 patch -p0 < llvm-6.0.0-BasicAliasAnalysis-patch.txt
 patch -p0 < llvm-6.0.0-ValueTracking-patch.txt
 
 #tar xf llvm-5.0.1.src.tar.xz
 #tar xf cfe-5.0.1.src.tar.xz
-#mv llvm-5.0.1.src llvm-src
-#mv cfe-5.0.1.src llvm-src/tools/clang
+#mv llvm-5.0.1.src llvm
+#mv cfe-5.0.1.src llvm/tools/clang
 #patch -p0 < llvm-4.0.1-BasicAliasAnalysis-patch.txt
 #patch -p0 < llvm-4.0.1-ValueTracking-patch.txt
 
 #tar xf llvm-4.0.1.src.tar.xz
 #tar xf cfe-4.0.1.src.tar.xz
-#mv llvm-4.0.1.src llvm-src
-#mv cfe-4.0.1.src llvm-src/tools/clang
+#mv llvm-4.0.1.src llvm
+#mv cfe-4.0.1.src llvm/tools/clang
 #patch -p0 < llvm-4.0.1-BasicAliasAnalysis-patch.txt
 #patch -p0 < llvm-4.0.1-ValueTracking-patch.txt
 
