f8558933dfe54381b92c8a0ab34602c3ed609c0a
Michael Ferguson <mppf@users.noreply.github.com>
2018-03-22 14:34:23 -0400

    Move FnSymbol defs out of ret type block before removal
    adds test this enables to pass, from issue #8140

--- compiler/resolution/functionResolution.cpp
+++ compiler/resolution/functionResolution.cpp
@@ -9439,8 +9439,20 @@ static void removeMootFields() {
 
 static void removeReturnTypeBlocks() {
   forv_Vec(FnSymbol, fn, gFnSymbols) {
-    if (fn->retExprType && fn->retExprType->parentSymbol)
+    if (fn->retExprType && fn->retExprType->parentSymbol) {
+      // First, move any defs in the return type block out
+      // (e.g. an array return type creates parloopexpr fns)
+      for_alist(expr, fn->retExprType->body) {
+        if (DefExpr* def = toDefExpr(expr)) {
+          if (isFnSymbol(def->sym)) {
+            def->remove();
+            fn->defPoint->insertBefore(def);
+          }
+        }
+      }
+      // Now remove the type block
       fn->retExprType->remove();
+    }
   }
 }
 
