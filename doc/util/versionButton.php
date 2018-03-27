841b8a7899f24dbd1c8c090f03c4bc593c2fcc9c
Tony Wallace <awallace@cray.com>
2018-03-27 15:17:59 -0700

    Pre-release prep. Chapel 1.17.0: post-release markings. Temp. change.
    This change addresses a bradcray review comment.
    
    The idea is to make the published Chapel docs look like we want them
    to do after Chapel 1.17.0 is officially released.
    
    When the 1.18 "pre-release" version strings go in on master, I will
    revert this commit.

--- doc/util/versionButton.php
+++ doc/util/versionButton.php
@@ -106,7 +106,7 @@ function dropSetup() {
   // Choose button color
   if (chplTitle.includes("pre-release")) {
     button.classList.add("preRelease");
-  } else if (chplTitle != chplVersions[1]) {
+  } else if (chplTitle != "1.17") {
     button.classList.add("oldVersion");
   } else {
     button.classList.add("currentVersion");
