112ae834fc7a80684ad077cdd6b15d7f2057e5c3
Elliot Ronaghan <ronawho@gmail.com>
2018-03-27 11:16:08 -0700

    Upgrade from hwloc 1.11.9 to 1.11.10
    This release includes a number of bugfixes and improvements.
    
    A notable fix that's not mentioned in the release notes is that the check for
    sysctl is skipped for linux. sysctl wasn't used for the linux backends, but the
    check for it led to it being linked in, which caused linker warnings on some
    aarch64 systems: https://github.com/open-mpi/hwloc/commit/58b867ac
    
    For a full changelog see:
    https://mail-archive.com/hwloc-announce@lists.open-mpi.org/msg00111.html

--- third-party/hwloc/hwloc-src/utils/hwloc/hwloc-distrib.c
+++ third-party/hwloc/hwloc-src/utils/hwloc/hwloc-distrib.c
@@ -1,6 +1,6 @@
 /*
  * Copyright © 2009 CNRS
- * Copyright © 2009-2017 Inria.  All rights reserved.
+ * Copyright © 2009-2018 Inria.  All rights reserved.
  * Copyright © 2009-2010 Université Bordeaux
  * Copyright © 2009-2011 Cisco Systems, Inc.  All rights reserved.
  * See COPYING in top-level directory.
@@ -54,8 +54,8 @@ int main(int argc, char *argv[])
   int err;
 
   /* enable verbose backends */
-  putenv("HWLOC_XML_VERBOSE=1");
-  putenv("HWLOC_SYNTHETIC_VERBOSE=1");
+  putenv((char *) "HWLOC_XML_VERBOSE=1");
+  putenv((char *) "HWLOC_SYNTHETIC_VERBOSE=1");
 
   hwloc_topology_init(&topology);
 
