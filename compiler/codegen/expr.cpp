e20f7095aa090e5cf3622cbe4bc1db71230618ad
Ben Harshbarger <ben.harshb@gmail.com>
2018-05-16 12:38:09 -0700

    Dummy implementation of IfExpr::codegen that will halt if called

--- compiler/codegen/expr.cpp
+++ compiler/codegen/expr.cpp
@@ -5666,6 +5666,12 @@ GenRet NamedExpr::codegen() {
   return ret;
 }
 
+GenRet IfExpr::codegen() {
+  GenRet ret;
+  INT_FATAL(this, "IfExpr::codegen not implemented");
+  return ret;
+}
+
 /************************************ | *************************************
 *                                                                           *
 *                                                                           *
