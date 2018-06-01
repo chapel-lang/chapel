b998b84962827462722f70cef2b65fc01ea03fc5
Preston Sahabu <psahabu@cray.com>
2018-06-01 08:23:38 -0700

    update testing

---
+++ test/library/standard/DataFrames/psahabu/HelloDataFrame.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+columnOne = ["a", None, "c", None, "e"]
+columnTwo = [1, None, 3, None, 5]
+columnThree = [10.0, 20.0, 30.0, 40.0, 50.0]
+
+idx = pd.Index(["rowOne", "rowTwo", "rowThree", "rowFour", "rowFive"])
+dataFrame = pd.DataFrame({ "columnOne": columnOne,
+                           "columnTwo": columnTwo,
+                           "columnThree": columnThree },
+                          idx)
+
+print dataFrame
+print
+print dataFrame["columnThree"]
