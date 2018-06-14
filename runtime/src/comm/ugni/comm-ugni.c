26f03e990089952a2626ec5e70e4ea39cbee3a0a
Elliot Ronaghan <ronawho@gmail.com>
2018-06-14 13:20:12 -0700

    Add a post_fma_and_wait specialized for network amos
    Currently it's just a clone of post_fma_and_wait, but we should be able to
    optimize it for amos (which are always <= 8 bytes and thus fast)

--- runtime/src/comm/ugni/comm-ugni.c
+++ runtime/src/comm/ugni/comm-ugni.c
@@ -1509,6 +1509,8 @@ static chpl_bool reacquire_comm_dom(int);
 static int       post_fma(c_nodeid_t, gni_post_descriptor_t*);
 static void      post_fma_and_wait(c_nodeid_t, gni_post_descriptor_t*,
                                    chpl_bool);
+static void      post_fma_and_wait_amo(c_nodeid_t, gni_post_descriptor_t*,
+                                       chpl_bool);
 #if HAVE_GNI_FMA_CHAIN_TRANSACTIONS
 static int       post_fma_ct(c_nodeid_t*, gni_post_descriptor_t*);
 static void      post_fma_ct_and_wait(c_nodeid_t*, gni_post_descriptor_t*);
@@ -6744,7 +6746,7 @@ void do_nic_amo_nf(void* opnd1, c_nodeid_t locale,
   //
   PERFSTATS_INC(amo_cnt);
 
-  post_fma_and_wait(locale, &post_desc, true);
+  post_fma_and_wait_amo(locale, &post_desc, true);
 }
 
 
@@ -6829,7 +6831,7 @@ void do_nic_amo(void* opnd1, void* opnd2, c_nodeid_t locale,
   //
   PERFSTATS_INC(amo_cnt);
 
-  post_fma_and_wait(locale, &post_desc, true);
+  post_fma_and_wait_amo(locale, &post_desc, true);
 
   if (p_result != result) {
     memcpy(result, p_result, size);
@@ -7598,6 +7600,33 @@ int post_fma(c_nodeid_t locale, gni_post_descriptor_t* post_desc)
 static
 void post_fma_and_wait(c_nodeid_t locale, gni_post_descriptor_t* post_desc,
                        chpl_bool do_yield)
+{
+  int cdi;
+  atomic_bool post_done;
+
+  atomic_init_bool(&post_done, false);
+  post_desc->post_id = (uint64_t) (intptr_t) &post_done;
+
+  cdi = post_fma(locale, post_desc);
+
+  //
+  // Wait for the transaction to complete.  Yield initially; the
+  // minimum round-trip time on the network isn't small and maybe
+  // we can find something else to do in the meantime.
+  //
+  do {
+    if (do_yield) {
+      local_yield();
+    }
+    consume_all_outstanding_cq_events(cdi);
+  } while (!atomic_load_explicit_bool(&post_done, memory_order_acquire));
+
+  CQ_CNT_DEC(&comm_doms[cdi]);
+}
+
+static
+void post_fma_and_wait_amo(c_nodeid_t locale, gni_post_descriptor_t* post_desc,
+                           chpl_bool do_yield)
 {
   int cdi;
   atomic_bool post_done;
