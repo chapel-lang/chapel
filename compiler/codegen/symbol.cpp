6000c2c4c8f2328b4190429f6098264ab0ce4a71
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2018-06-27 15:46:01 -0700

    Add a comment to my helper function

--- compiler/codegen/symbol.cpp
+++ compiler/codegen/symbol.cpp
@@ -786,6 +786,10 @@ static Type* getArgSymbolCodegenType(ArgSymbol* arg) {
   return useType;
 }
 
+// Alter the C code type to avoid _ref_, c_ptr_ prefixes and instead use a
+// type that C understands without additional information (e.g. instead of
+// _ref_int64_t, just int64_t *).  But only for exported symbols like the
+// the return type of exported functions, or arguments of those functions.
 static std::string
 transformTypeForPointer(std::string typeName, bool exported) {
   if (exported) {
