44bf7bd6e020e63f3f8cc26ce3507176ba5dbee9
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2018-08-16 13:23:39 -0700

    Fix comment on copied tests

--- test/compflags/lydia/library/makefiles/checkRequire.c
+++ test/compflags/lydia/library/makefiles/checkRequire.c
@@ -5,7 +5,7 @@
 extern void chpl_library_init(int argc, char* argv[]);
 extern void chpl_library_finalize(void);
 
-// Test of calling an exported function that takes an array
+// Test of calling an exported function involving require statements
 int main(int argc, char* argv[]) {
   // Initialize the Chapel runtime and standard modules
   chpl_library_init(argc, argv);
