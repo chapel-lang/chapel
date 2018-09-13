6377da7edbc102287d1aa6c4b67e71d742d9fbb2
Brad Chamberlain <bradc@cray.com>
2018-09-13 13:15:20 -0700

    Fix comment style from # to //
    That'll teach me to try and comment my code...

--- doc/util/versionButton.php
+++ doc/util/versionButton.php
@@ -97,13 +97,13 @@ function dropSetup() {
 
   // Choose button color
   if (chplTitle == nextRelease || chplTitle == nextNextRelease) {
-    # add pre-release label using a non-breaking hyphen
+    // add pre-release label using a non-breaking hyphen
     button.innerHTML = "version " + chplTitle + " (pre&#8209;release)" + arrow;
     button.classList.add("preRelease");
   } else if (chplTitle == currentRelease) {
     button.classList.add("currentVersion");
   } else {
-    # add old release label using a non-blocking space
+    // add old release label using a non-blocking space
     button.innerHTML = "version " + chplTitle + " (old&nbsp;release)" + arrow;
     button.classList.add("oldVersion");
   }
