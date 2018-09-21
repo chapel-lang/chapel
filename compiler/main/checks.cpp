63b0dc98cb9787516ed9e03b78369b3865585929
Ben Harshbarger <ben.harshb@gmail.com>
2018-09-21 13:33:47 -0700

    Remove useless verification check

--- compiler/main/checks.cpp
+++ compiler/main/checks.cpp
@@ -587,13 +587,6 @@ static void checkIsIterator() {
 //
 static void checkAggregateTypes() {
   for_alive_in_Vec(AggregateType, at, gAggregateTypes) {
-    if (at->defaultInitializer == NULL &&
-        at->initializerStyle   != DEFINES_INITIALIZER) {
-      INT_FATAL(at,
-                "aggregate type did not define an initializer "
-                "and has no default initializer");
-    }
-
     if (at->typeConstructor == NULL) {
       INT_FATAL(at, "aggregate type has no type constructor");
     }
