370d760d1c8eb3900b6077b055d0bb2f91f28212
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2018-11-01 15:57:23 -0700

    Remove the complex test, as complex literals are virtually non-existant
    From what I can tell, the compiler does not use Immediates in the case of
    complex numbers.  I think this is because we can't tell the difference between
    "2 + 1i" when "i" means imaginary and "2 + 1i" when "i" is the name of a
    symbol available to the current scope.  This means that we can't support default
    values for complex arguments until we support CallExprs.

--- test/interop/python/use_defaultValues.py
+++ test/interop/python/use_defaultValues.py
@@ -14,7 +14,4 @@ defaultValues.intDefault()
 d = b"not the default"
 defaultValues.cstringDefault(d)
 defaultValues.cstringDefault()
-e = 2 + 3j
-defaultValues.complexDefault(e)
-defaultValues.complexDefault()
 defaultValues.chpl_cleanup()
