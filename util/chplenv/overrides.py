chapel-lang/chapel

commit d878236968d60e23e1dbcc8c0f003080d62fc3fd

New printchplenv design following #7827

util/chplenv/overrides.py
@@ -205,12 +205,16 @@ def get_chplconfig(var, default=None):
     """ Check if variable has a default defined in a chplconfig file and return value """
     return chplconfig.get(var) or default
 
-
-def allvars():
-    """ Generate overrides currently set via environment/chplconfig """
+""" Generate env vars currently set via environment/chplconfig """
+def keys():
     for var in [v for v in chplvars if get(v)]:
-        varstring = '{0}={1}'.format(var, get(var))
-        yield varstring
+        yield var
+
+""" Generate (env var, value) pairs currently set via environment/chplconfig """
+def items():
+    for var in keys():
+        yield (var, get(var))
+
 
 
 def _main():

commit e2e1389a80a15916ccfff134ad337c363a6eff0e

Fix overrides.py main
This has been broken since d878236968, but it's not used by any current
scripts so we never noticed.

util/chplenv/overrides.py
@@ -219,9 +219,8 @@ def items():
 
 def _main():
     """ Print overrides that are currently set via environment/chplconfig """
-    for var in allvars():
-        sys.stdout.write(var)
-        sys.stdout.write('\n')
+    for key, val in items():
+        sys.stdout.write('{0}={1}\n'.format(key, val))
 
 
 if __name__ == '__main__':