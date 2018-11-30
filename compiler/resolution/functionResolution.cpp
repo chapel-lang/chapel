a2a36670b03df9405d1a20aa061c01ea09d428d8
Ben Harshbarger <ben.harshb@gmail.com>
2018-11-30 10:41:41 -0800

    Revert to skipping resolution of promotionType for forwarding

--- compiler/resolution/functionResolution.cpp
+++ compiler/resolution/functionResolution.cpp
@@ -3225,6 +3225,10 @@ static FnSymbol* resolveForwardedCall(CallInfo& info, bool checkOnly) {
   if (0 == memcmp(ignorePrefix, calledName, strlen(ignorePrefix)))
     return NULL;
 
+  if (0 == strcmp(calledName, "chpl__promotionType")) {
+    return NULL;
+  }
+
   // Detect cycles
   detectForwardingCycle(call);
 
