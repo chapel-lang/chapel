2e8efeb56df2ea97ef22ef4990e933e57f10a001
David Keaton <dmk42@users.noreply.github.com>
2019-02-28 08:49:49 -0800

    Add a little more explanation of what we are doing

--- third-party/llvm/find-gcc-prefix.sh
+++ third-party/llvm/find-gcc-prefix.sh
@@ -2,12 +2,15 @@
 
 # If building LLVM and Clang with GCC, the built
 # LLVM should use C++ headers from the selected GCC.
+# We are looking for GCC's installation root.  In other words, we want
+# the directory specified in the --prefix argument to GCC's configure
+# script.  Clang knows how to find the C++ header files from there.
 
 # Using the LLVM project's libc++ would be a reasonable alternative.
 
 CC=$1
 
-# First look for the prefix specified when gcc was configured.
+# First look for the prefix specified when GCC was configured.
 DIR=`$CC -v 2>&1 | xargs -n 1 | egrep '^--prefix=' | sed -e 's/^--prefix=//'`
 if [ -d "$DIR" ]
 then
