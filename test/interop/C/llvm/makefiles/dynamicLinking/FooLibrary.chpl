5942d24c30743452152c79350dd6ee1b084465d0
David Longnecker <dlongnecke@cray.com>
2019-04-01 14:29:16 -0700

    Add new test validating --dynamic flag

---
+++ test/interop/C/llvm/makefiles/dynamicLinking/FooLibrary.chpl
@@ -0,0 +1,14 @@
+// Hmmmmmmmmmm...
+
+
+proc hiddenDragon(): string {
+  return "The possession of anything begins in the mind.";
+}
+
+proc crouchingTiger(): string {
+  return hiddenDragon();
+}
+
+export proc wisdom() {
+  writeln(crouchingTiger());
+}
---
+++ test/interop/C/llvm/makefiles/dynamicLinking/useSharedLibFromC.test.c
@@ -0,0 +1,17 @@
+// Make sure we link appropriately from C.
+#include "lib/FooLibrary.h"
+
+extern void wisdom(void);
+
+void spoutWisdom(void) {
+  wisdom();
+}
+
+int main(int argc, char** argv) {
+  chpl_library_init(argc, argv);
+  chpl__init_FooLibrary(0, 0);
+  spoutWisdom();
+  chpl_library_finalize();
+  return 0;
+}
+  
