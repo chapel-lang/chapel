1264002425a4086a82563bc4247e9810e8694731
Elliot Ronaghan <ronawho@gmail.com>
2019-04-11 16:43:06 -0700

    Have Py3CompatPopen.communicate() directly convert bytes to strings
    Instead of only trying to convert to strings when stderr or stdout are
    individually pulled out of communicate (e.g. communicate()[1]) just try
    to convert both stderr and stdout into strings and return a new tuple.
    This makes callsites that do `stdout, stderr = p.communicate()` a lot
    simpler. Note that we try to convert to str independently and we don't
    take the all-or-nothing approach like concat_streams does.

--- util/test/py3_compat.py
+++ util/test/py3_compat.py
@@ -1,17 +1,20 @@
 """
-Python 2/3 compatibility shim. We use subprocess.Popen.communicate() all over
-sub_test. Instead of updating every call site to do the right thing with regard
-to byte to str conversions for 3 compatibility, we have opted to add this
-wrapper. Not all things we run will produce utf-8 (i.e. catfiles for thing like
-mandelbrot will return binary data) so we allow the conversion to utf-8 to
-silently fail and we handle any issues at those call sites.
+Python 2/3 compatibility shim. We use subprocess.Popen.communicate() all over.
+Instead of updating every call site to do the right thing with regard to byte
+to str conversions for 3 compatibility, we have opted to add this wrapper. Not
+all things we run will produce utf-8 (i.e. catfiles for thing like mandelbrot
+will return binary data) so we allow the conversion to utf-8 to silently fail
+and we handle any issues at the call sites.
 """
 
 import subprocess
 import sys
 
+def instance_or_none(val, val_type):
+    return val is None or isinstance(val, val_type)
+
 def bytes_to_str(byte_string):
-    if sys.version_info[0] >= 3 and not isinstance(byte_string, str):
+    if sys.version_info[0] >= 3 and not instance_or_none(byte_string, str):
         try:
             return str(byte_string, 'utf-8')
         except UnicodeDecodeError as e:
@@ -21,7 +24,7 @@ def bytes_to_str(byte_string):
         return byte_string
 
 def str_to_bytes(str_string):
-    if sys.version_info[0] >= 3 and not isinstance(str_string, bytes):
+    if sys.version_info[0] >= 3 and not instance_or_none(str_string, bytes):
         return bytes(str_string, 'utf-8')
     else:
         return str_string
@@ -39,29 +42,24 @@ def concat_streams(stdout, stderr):
     return stdout + stderr
 
 class Py3CompatPopen(object):
+    """ Popen wrapper where communicate will convert input to bytes, and try to
+        convert output to str. Note that communicate() will try to stderr and
+        strerr to str independently (so you could end up with 1 being str and
+        the other being bytes)"""
 
     def __init__(self, popen):
         self._popen = popen
 
-    def communicate(self):
-        c = self._popen.communicate()
-        return Py3CompatCommGetter(c)
+    def communicate(self, input=None):
+        bytes_input = str_to_bytes(input)
+        stdout, stderr = self._popen.communicate(bytes_input)
+        str_stdout = bytes_to_str(stdout)
+        str_stderr = bytes_to_str(stderr)
+        return (str_stdout, str_stderr)
 
     def __getattr__(self, name):
         return getattr(self._popen, name)
 
-
-class Py3CompatCommGetter(object):
-
-    def __init__(self, comm_result):
-        self._c = comm_result
-
-    def __getitem__(self, key):
-        val = self._c[key]
-        if key == 0 or key == 1:
-            return bytes_to_str(val)
-        else:
-            return val
 
 def Popen(*args, **kwargs):
     p = subprocess.Popen(*args, **kwargs)
