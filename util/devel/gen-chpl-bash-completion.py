9a091d511bca26820ac65f7c8e7d1ad81aba443e
Michael Ferguson <mppf@users.noreply.github.com>
2019-04-22 15:28:52 -0400

    Fix it for Python 2

--- util/devel/gen-chpl-bash-completion.py
+++ util/devel/gen-chpl-bash-completion.py
@@ -16,26 +16,26 @@ chpl_home=os.environ["CHPL_HOME"]
 
 def gatherFlags(command):
     ret = [ ]
-    with subprocess.Popen(command, stdout=subprocess.PIPE) as p:
-        for line in p.stdout:
-          s=line.decode("utf-8").strip()
-          if s.startswith("-"):
-              # Split on spaces and on commas
-              toks = re.split(r',| +', s)
-              for t in toks:
-                  st = t.strip()
-                  # Ignore the strip characters
-                  if st == "" or st == ",":
-                      continue
-                  # Stop working on this line for first word not starting with -
-                  if not st.startswith("-"):
-                      break
-                  # OK, we think that it's an option
-                  if st.startswith("--[no-]"):
-                      ret.append(st.replace("--[no-]", "--", 1))
-                      ret.append(st.replace("--[no-]", "--no-", 1))
-                  else:
-                      ret.append(st)
+    lines = subprocess.check_output(command)
+    for line in lines.splitlines():
+        s=line.decode("utf-8").strip()
+        if s.startswith("-"):
+            # Split on spaces and on commas
+            toks = re.split(r',| +', s)
+            for t in toks:
+                st = t.strip()
+                # Ignore the strip characters
+                if st == "" or st == ",":
+                    continue
+                # Stop working on this line for first word not starting with -
+                if not st.startswith("-"):
+                    break
+                # OK, we think that it's an option
+                if st.startswith("--[no-]"):
+                    ret.append(st.replace("--[no-]", "--", 1))
+                    ret.append(st.replace("--[no-]", "--no-", 1))
+                else:
+                    ret.append(st)
 
     return ret
 
@@ -54,42 +54,3 @@ with open(template) as f:
         line=line.replace("OPTS_DEVEL", sep.join(devel))
         line=line.replace("OPTS_NODEVEL", sep.join(nodevel))
         print(line)
-
-"""
-sed -e "s/OPTS_DEVEL/$dev_opts/" -e "s/OPTS_NODEVEL/$nodev_opts/" $template
-res = p.stdout.readlines()
-if len(res) == 0: return False
-return True
-# Make sure chpl is in the path
-# Make sure that chpl is on the path
-source $CHPL_HOME/util/setchplenv.bash > /dev/null 2>&1
-
-single_dev=`chpl --devel --help | grep -v '^[A-Za-z]' | grep '^.*--' |        \
-            sed -e 's;^\(.*--[^ ]*\).*$;\1;' | grep ',' | sed -e 's;,.*$;;' | \
-            xargs echo`
-
-positive_dev=`chpl --devel --help | grep -v '^[A-Za-z]' | grep '^.*--' |      \
-              sed -e 's;^\(.*--[^ ]*\).*$;\1;' | sed -e 's;\[no-\];;' |       \
-              sed -e 's;-[A-Za-z],;   ;' | xargs echo`
-
-negative_dev=`chpl --devel --help | grep -v '^[A-Za-z]' | grep '^.*--' |      \
-              sed -e 's;^\(.*--[^ ]*\).*$;\1;' | grep '\[no-\]' |             \
-              tr -d '[' | tr -d ']' | sed -e 's;-[A-Za-z],;   ;' | xargs echo`
-
-single_nodev=`chpl --no-devel --help | grep -v '^[A-Za-z]' | grep '^.*--' |   \
-            sed -e 's;^\(.*--[^ ]*\).*$;\1;' | grep ',' | sed -e 's;,.*$;;' | \
-            xargs echo`
-
-positive_nodev=`chpl --no-devel --help | grep -v '^[A-Za-z]' | grep '^.*--' | \
-              sed -e 's;^\(.*--[^ ]*\).*$;\1;' | sed -e 's;\[no-\];;' |       \
-              sed -e 's;-[A-Za-z],;   ;' | xargs echo`
-
-negative_nodev=`chpl --no-devel --help | grep -v '^[A-Za-z]' | grep '^.*--' | \
-              sed -e 's;^\(.*--[^ ]*\).*$;\1;' | grep '\[no-\]' |             \
-              tr -d '[' | tr -d ']' | sed -e 's;-[A-Za-z],;   ;' | xargs echo`
-
-nodev_opts="$single_nodev $positive_nodev $negative_nodev"
-dev_opts="$single_dev $positive_dev $negative_dev"
-
-sed -e "s/OPTS_DEVEL/$dev_opts/" -e "s/OPTS_NODEVEL/$nodev_opts/" $template
-"""
