ae43d43ee3ce1bdb8db6a381248dc643dc6e28c1
David Longnecker <dlongnecke@cray.com>
2019-05-06 11:20:51 -0700

    Replace printfs with #ifdef blocks with call to chpl_mli_debugf

--- runtime/etc/mli/mli_client_runtime.c
+++ runtime/etc/mli/mli_client_runtime.c
@@ -60,9 +60,7 @@ static void chpl_mli_spawn_server() {
   strcat(command, mli_servername);
   strcat(command, " -nl 1");
 
-  #ifdef CHPL_MLI_DEBUG_PRINT
-  printf("executing %s as a subprocess\n", command);
-  #endif
+  chpl_mli_debugf("executing %s as a subprocess\n", command);
   server_pipe = popen(command, "r");
 }
 
@@ -98,9 +96,7 @@ void chpl_library_init(int argc, char** argv) {
   chpl_mli_bind(chpl_client.setup_sock, ip, setupPort);
 
   char* setup_sock_conn = chpl_mli_connection_info(chpl_client.setup_sock);
-  #ifdef CHPL_MLI_DEBUG_PRINT
-  printf("setup socket used %s\n", setup_sock_conn);
-  #endif
+  chpl_mli_debugf("setup socket used %s\n", setup_sock_conn);
 
   // TODO: pass in argv/argc, connection information
   chpl_mli_spawn_server();
@@ -138,7 +134,7 @@ void chpl_library_finalize(void) {
     }
   }
   if (pclose(server_pipe)) {
-    printf("Failed to shut down server\n");
+    chpl_mli_debugf("Failed to shut down server\n");
   }
 
   // TODO: It would be a good idea to set LINGER to 0 as well.
