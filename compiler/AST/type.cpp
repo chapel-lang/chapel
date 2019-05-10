ebee97f34e1c6eb6ebb747e2d340558bc8cc0051
Brad Chamberlain <bradcray@users.noreply.github.com>
2019-05-10 13:35:07 -0500

    Comment out some debugging code for now (need to get buy-in)

--- compiler/AST/type.cpp
+++ compiler/AST/type.cpp
@@ -164,8 +164,10 @@ const char* toString(Type* type) {
 
     if (retval == NULL)
       retval = vt->symbol->name;
-    if (developer)
-      retval = vt->symbol->cname;
+    /* This can be helpful when debugging (and perhaps we should enable it
+       by default?): */
+//    if (developer)
+//      retval = vt->symbol->cname;
 
   } else {
     retval = "null type";
