856ac92ea92169b7e11fea8a4fc28e502dc970cc
Brad Chamberlain <bradc@cray.com>
2019-06-24 13:57:01 -0700

    Add comment describing what the new routine is for and requirement to normalize

--- compiler/include/buildDefaultFunctions.h
+++ compiler/include/buildDefaultFunctions.h
@@ -4,6 +4,11 @@
 #include "symbol.h"
 #include "type.h"
 
+// build a hollow `writeThis` FunctionSymbol* for the given class type
+// 'ct', returning its file (channel) argument in 'filearg'.  The
+// callee is expected to fill in the body of the function and to
+// normalize the returned function before continuing.
+
 FnSymbol* buildWriteThisFnSymbol(AggregateType* ct, ArgSymbol** filearg);
 
 #endif
