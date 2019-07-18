38a0b5ac7ce240407a6151de3eba6885e023afc1
Vassily Litvinov <vasslitvinov@users.noreply.github.com>
2019-07-18 18:51:06 -0500

    Improve the error message

--- compiler/AST/foralls.cpp
+++ compiler/AST/foralls.cpp
@@ -157,7 +157,7 @@ ShadowVarSymbol* ShadowVarSymbol::buildForPrefix(ShadowVarPrefix prefix,
 {
   const char* nameString = toUnresolvedSymExpr(nameExp)->unresolved;
   if (nameString == astrThis)
-    USR_FATAL_CONT(nameExp, "cannot apply a forall or task intent to 'this'");
+    USR_FATAL_CONT(nameExp, "cannot currently apply a forall or task intent to 'this'");
 
   if (type == NULL && init == NULL)
     // non-TPV forall intent
