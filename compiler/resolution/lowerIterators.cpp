8d252a13b4a1077f944d285ecec349bfb93ee52b
Brad Chamberlain <bradcray@users.noreply.github.com>
2019-08-06 18:17:46 -0500

    Wordsmith error message about yields within ons and zippering
    And remove duplicate messages!

--- compiler/resolution/lowerIterators.cpp
+++ compiler/resolution/lowerIterators.cpp
@@ -2415,7 +2415,8 @@ expandForLoop(ForLoop* forLoop) {
 
       FnSymbol* iterFn = getTheIteratorFn(iterators.v[i]);
       if (iterFn->hasFlag(FLAG_YIELD_WITHIN_ON)) {
-        USR_FATAL_CONT(forLoop, "'yield' statements within 'on' clauses are not currently supported for serial zippered/non-inlineable iterators");
+        USR_FATAL_CONT(forLoop, "zippering serial iterators with 'yield' statements in 'on' clauses is not currently supported");
+        break;
       }
 
       if (isBoundedIterator(iterFn)) {
