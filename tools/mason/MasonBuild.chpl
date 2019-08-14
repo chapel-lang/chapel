chapel-lang/chapel

commit 72a8f15d918a0f1aa87f398831e64aaf2f4994ca

Deprecate "array-as-vector" methods and adjust failing tests (#13530)
Deprecate "array-as-vector" methods and adjust failing tests (#13530)

This PR replaces _most_ uses of the following array methods:

- push_front
- push_back
- pop_front
- pop_back
- insert
- remove
- clear

With use of the new `list` type where possible. It rewrites several
packages (Mason, UnitTest, LinearAlgebra, TOML) to make use of the
list type as well. Not all of the conversions are pretty - the list
type currently lacks some routines that would result in more elegant
code.

The important thing here is that insofar as possible - when "array-as
vec" deprecation warnings are turned on, the only tests that will
break are those that need to be _removed_, as they are no longer
relevant in a 1.20 world.

Deprecation warnings are off by default, but can be turned on by
setting the config param "showArrayAsVecWarnings" to true at compile
time.

Deprecation warnings will be turned on by default at around release
time.

When deprecation warnings are turned on, any remaining tests with
vector ops in them will have to either:

- Be removed
- Moved to test/deprecated
- Have their .good files updated to contain the warnings

---

Testing:

- [x] `ALL` on `linux64` when `CHPL_COMM=none`
- [x] `ALL` on `linux64` when `CHPL_COMM=gasnet`

---

Reviewed by more people than I can shake a stick at:

@benharsh, @lydia-duncan, @mppf, @e-kayrakli, @bradcray, @daviditen,
@vasslitvinov, @ben-albrecht

tools/mason/MasonBuild.chpl
@@ -18,6 +18,7 @@
  */
 
 
+private use Lists;
 use TOML;
 use Spawn;
 use FileSystem;
@@ -32,13 +33,14 @@ proc masonBuild(args) throws {
   var show = false;
   var release = false;
   var force = false;
-  var compopts: [1..0] string;
+  var compopts: list(string);
   var opt = false;
   var example = false;
   if args.size > 2 {
-    for arg in args[2..] {
+    for i in 3..args.size {
+      var arg = args[i];
       if opt == true {
-        compopts.push_back(arg);
+        compopts.append(arg);
       }
       else if arg == '-h' || arg == '--help' {
         masonBuildHelp();
@@ -66,20 +68,22 @@ proc masonBuild(args) throws {
         continue;
       }
       else {
-        compopts.push_back(arg);
+        compopts.append(arg);
       }
     }
   }
   if example {
     // compopts become test names. Build never runs examples
-    compopts.push_back("--no-run");
-    if show then compopts.push_back("--show");
-    if release then compopts.push_back("--release");
-    if force then compopts.push_back("--force");
+    compopts.append("--no-run");
+    if show then compopts.append("--show");
+    if release then compopts.append("--release");
+    if force then compopts.append("--force");
     masonExample(compopts);
   }
   else {
-    const configNames = UpdateLock(args);
+    var argsList = new list(string);
+    for x in args do argsList.append(x);
+    const configNames = UpdateLock(argsList);
     const tomlName = configNames[1];
     const lockName = configNames[2];
     buildProgram(release, show, force, compopts, tomlName, lockName);
@@ -96,7 +100,7 @@ private proc checkChplVersion(lockFile : borrowed Toml) throws {
 }
 
 
-proc buildProgram(release: bool, show: bool, force: bool, cmdLineCompopts: [?d] string,
+proc buildProgram(release: bool, show: bool, force: bool, ref cmdLineCompopts: list(string),
                   tomlName="Mason.toml", lockName="Mason.lock") throws {
 
 
@@ -129,6 +133,11 @@ proc buildProgram(release: bool, show: bool, force: bool, cmdLineCompopts: [?d] 
 
         // generate list of dependencies and get src code
         var sourceList = genSourceList(lockFile);
+        //
+        // TODO: Temporarily use `toArray` here because `list` does not yet
+        // support parallel iteration, which the `getSrcCode` method _must_
+        // have for good performance.
+        //
         getSrcCode(sourceList, show);
 
         // get compilation options including external dependencies
@@ -164,7 +173,7 @@ proc buildProgram(release: bool, show: bool, force: bool, cmdLineCompopts: [?d] 
    named after the project folder in which it is
    contained */
 proc compileSrc(lockFile: borrowed Toml, binLoc: string, show: bool,
-                release: bool, compopts: [?dom] string, projectHome: string) : bool throws {
+                release: bool, compopts: list(string), projectHome: string) : bool throws {
 
   const sourceList = genSourceList(lockFile);
   const depPath = MASON_HOME + '/src/';
@@ -176,9 +185,9 @@ proc compileSrc(lockFile: borrowed Toml, binLoc: string, show: bool,
     throw new owned MasonError("Mason could not find your project");
   }
   else {
-    var command: string = 'chpl ' + pathToProj + moveTo + ' ' + ' '.join(compopts);
+    var command: string = 'chpl ' + pathToProj + moveTo + ' ' + ' '.join(compopts.these());
     if release then command += " --fast";
-    if sourceList.numElements > 0 then command += " --main-module " + project;
+    if sourceList.size > 0 then command += " --main-module " + project;
 
     for (_, name, version) in sourceList {
       var depSrc = ' ' + depPath + name + "-" + version + '/src/' + name + ".chpl";
@@ -210,14 +219,14 @@ proc compileSrc(lockFile: borrowed Toml, binLoc: string, show: bool,
 /* Generates a list of tuples that holds the git repo
    url and the name for local mason dependency pool */
 proc genSourceList(lockFile: borrowed Toml) {
-  var sourceList: [1..0] (string, string, string);
+  var sourceList: list((string, string, string));
   for (name, package) in zip(lockFile.D, lockFile.A) {
     if package.tag == fieldtag.fieldToml {
       if name == "root" || name == "system" || name == "external" then continue;
       else {
         var version = lockFile[name]["version"].s;
         var source = lockFile[name]["source"].s;
-        sourceList.push_back((source, name, version));
+        sourceList.append((source, name, version));
       }
     }
   }
@@ -239,7 +248,15 @@ proc depExists(dependency: string) {
 
 /* Clones the git repository of each dependency into
    the src code dependency pool */
-proc getSrcCode(sourceList: [?d] 3*string, show) {
+proc getSrcCode(sourceListArg: list(3*string), show) {
+
+  //
+  // TODO: Temporarily use `toArray` here because `list` does not yet
+  // support parallel iteration, which the `getSrcCode` method _must_
+  // have for good performance.
+  //
+  var sourceList = sourceListArg.toArray();
+
   var baseDir = MASON_HOME +'/src/';
   forall (srcURL, name, version) in sourceList {
     const nameVers = name + "-" + version;
@@ -259,12 +276,12 @@ proc getSrcCode(sourceList: [?d] 3*string, show) {
 }
 
 // Retrieves root table compopts, external compopts, and system compopts
-proc getTomlCompopts(lock: borrowed Toml, compopts: [?d] string) {
+proc getTomlCompopts(lock: borrowed Toml, ref compopts: list(string)) {
 
   // Checks for compilation options are present in Mason.toml
   if lock.pathExists('root.compopts') {
     const cmpFlags = lock["root"]["compopts"].s;
-    compopts.push_back(cmpFlags);
+    compopts.append(cmpFlags);
   }
   
   if lock.pathExists('external') {
@@ -272,9 +289,9 @@ proc getTomlCompopts(lock: borrowed Toml, compopts: [?d] string) {
     for (name, depInfo) in zip(exDeps.D, exDeps.A) {
       for (k,v) in allFields(depInfo) {
         select k {
-            when "libs" do compopts.push_back("-L" + v.s); 
-            when "include" do compopts.push_back("-I" + v.s);
-            when "other" do compopts.push_back("-I" + v.s);
+            when "libs" do compopts.append("-L" + v.s); 
+            when "include" do compopts.append("-I" + v.s);
+            when "other" do compopts.append("-I" + v.s);
             otherwise continue;
           }
       }
@@ -283,8 +300,8 @@ proc getTomlCompopts(lock: borrowed Toml, compopts: [?d] string) {
   if lock.pathExists('system') {
     const pkgDeps = lock['system'];
     for (name, depInfo) in zip(pkgDeps.D, pkgDeps.A) {
-      compopts.push_back(depInfo["libs"].s);
-      compopts.push_back("-I" + depInfo["include"].s);
+      compopts.append(depInfo["libs"].s);
+      compopts.append("-I" + depInfo["include"].s);
     }
   }
   return compopts;

commit b9ae757b9a2967124565179bcb2d03048dc11557

Fix failure of test/mason/spack-integration/mason-external.chpl caused by #13530 (#13753)
Fix failure of test/mason/spack-integration/mason-external.chpl caused
by #13530 (#13753)

This PR fixes the failure of a test that was caused by the merge of
#13530.

The routine `masonBuild` is generic and is instantiated with either:

- list(string), which has a start index of 1
- [] string, which has a low of 0 and a high of size - 1 (0-based)

This PR adds code that adapts the start and end index of the loop
accordingly for each instantiation of `masonBuild`.

Testing:

- [x] `test/mason/spack-integration/mason-external.chpl` on `linux64`
      with `COMM=none`
- [x] `test/mason` on `linux64` with `COMM=none`

Trivial and not reviewed.

tools/mason/MasonBuild.chpl
@@ -36,8 +36,23 @@ proc masonBuild(args) throws {
   var compopts: list(string);
   var opt = false;
   var example = false;
+
   if args.size > 2 {
-    for i in 3..args.size {
+
+    //
+    // This function is generic and may be instantiated with either a list
+    // or an array as the type for "args". In the case of list, the
+    // start index is 1, however in the case of an array, the `low` element
+    // is 0. The below code is a stopgap.
+    //
+    var start = 3;
+    var end = args.size; 
+    if isArray(args) && args.eltType == string {
+      start = args.domain.low + 2;
+      end = args.domain.high;
+    }
+
+    for i in start..end {
       var arg = args[i];
       if opt == true {
         compopts.append(arg);