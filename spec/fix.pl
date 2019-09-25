ecc2c234115d64013a80254011b5b720d24009e7
Michael Ferguson <mppf@users.noreply.github.com>
2019-09-25 08:43:22 -0400

    'make rst' is making a sortof reasonable RST version

--- spec/fix.pl
+++ spec/fix.pl
@@ -2,7 +2,52 @@
 
 @buffer = ( "", "", "", "", "" );
 
+print
+".. _chapel-spec:
+
+Chapel Language Specification
+|||||||||||||||||||||||||||||
+
+
+";
+
 while(<>) {
+  $line = $_;
+  # Fix code blocks of a type annotated
+  # -2  BLOCK-chapel
+  # -1
+  #  0  ::
+  #
+  #     some-code
+  if (/^\s*::$/ and $buffer[-2] =~ /(BLOCK-[a-z0-9-]+)$/) {
+    $type = $1;
+    $type =~ s/^\s+|\s+$//g;
+    $spacing = $line;
+    chomp $spacing;
+    $spacing =~ s/::$//g;
+    $buffer[-2] =~ s/(BLOCK-[a-z0-9-]+)$//g;
+
+    if ($type eq "BLOCK-chapel") {
+      $_ = $spacing . ".. code-block:: chapel\n";
+    } elsif ($type eq "BLOCK-protohead") {
+      $_ = $spacing . ".. code-block:: chapel\n";
+    } elsif ($type eq "BLOCK-numbered-chapel") {
+      $_ = $spacing . ".. code-block:: chapel\n   :linenos:\n";
+    } elsif ($type eq "BLOCK-syntax") {
+      $_ = $spacing . ".. code-block:: syntax\n";
+    } elsif ($type eq "BLOCK-syntaxdonotcollect") {
+      $_ = $spacing . ".. code-block:: syntaxdonotcollect\n";
+    } elsif ($type eq "BLOCK-chapelprintoutput") {
+      $_ = $spacing . "::\n";
+    } elsif ($type eq "BLOCK-commandline") {
+      $_ = $spacing . ".. code-block:: bash\n";
+    } elsif ($type eq "BLOCK-invisible" or $type =~ /BLOCK-test-/) {
+      $_ = $spacing . ".. $type\n";
+    } else {
+      die "Unknown BLOCK '$type'";
+    }
+  }
+
   # Fix links like §\ `18.1 <#Record_Declarations>`__
   # to become :ref:`Record_Declarations`
   s/§?\\? ?`[0-9. ]*<#([^>]+)>`__/:ref:`$1`/g;
