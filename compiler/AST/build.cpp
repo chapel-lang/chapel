2b7c08e80386ecf7a9a3d56b0b08844dc87bc9bc
Brad Chamberlain <bradc@cray.com>
2019-10-17 15:52:23 -0700

    Fix unterminated comment

--- compiler/AST/build.cpp
+++ compiler/AST/build.cpp
@@ -563,7 +563,7 @@ BlockStmt* buildRequireStmt(CallExpr* args) {
 
 
 /* This routine seems to handle the case when the RHS of a tuple
-   declaration is a tuple expression like `(8, 4, 3)`
+   declaration is a tuple expression like `(8, 4, 3)` */
 static void
 buildTupleVarDeclHelp(Expr* base, BlockStmt* decls, Expr* insertPoint) {
   int count = 0;
