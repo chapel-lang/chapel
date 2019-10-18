f226a63c40ffb1218b8354b6f5bd84c2020b488b
Michael Ferguson <mppf@users.noreply.github.com>
2019-10-18 12:46:19 -0400

    Fix minimal modules now that owned/shared are handled like string

--- compiler/AST/wellknown.cpp
+++ compiler/AST/wellknown.cpp
@@ -119,6 +119,20 @@ static WellKnownType sWellKnownTypes[] = {
   { "Error",                 &dtError,            true  }
 };
 
+static void removeIfUndefinedGlobalType(AggregateType*& t) {
+  if (t->symbol == NULL || t->symbol->defPoint == NULL) {
+    // This means there was no declaration of this type
+    if (t->symbol)
+      gTypeSymbols.remove(gTypeSymbols.index(t->symbol));
+
+    gAggregateTypes.remove(gAggregateTypes.index(t));
+
+    delete t;
+
+    t = NULL;
+  }
+}
+
 // Gather well-known types from among types known at this point.
 void gatherWellKnownTypes() {
   int nEntries = sizeof(sWellKnownTypes) / sizeof(sWellKnownTypes[0]);
@@ -170,39 +184,11 @@ void gatherWellKnownTypes() {
     USR_STOP();
 
   } else {
-    if (dtString->symbol == NULL || dtString->symbol->defPoint == NULL) {
-      // This means there was no declaration of the string type.
-      if (dtString->symbol)
-        gTypeSymbols.remove(gTypeSymbols.index(dtString->symbol));
-
-      gAggregateTypes.remove(gAggregateTypes.index(dtString));
-
-      delete dtString;
-
-      dtString = NULL;
-    }
-    if (dtBytes->symbol == NULL || dtBytes->symbol->defPoint == NULL) {
-      // This means there was no declaration of the bytes type.
-      if (dtBytes->symbol)
-        gTypeSymbols.remove(gTypeSymbols.index(dtBytes->symbol));
-
-      gAggregateTypes.remove(gAggregateTypes.index(dtBytes));
-
-      delete dtBytes;
-
-      dtBytes = NULL;
-    }
-    if (dtLocale->symbol == NULL || dtLocale->symbol->defPoint == NULL) {
-      // This means there was no declaration of the locale type.
-      if (dtLocale->symbol)
-        gTypeSymbols.remove(gTypeSymbols.index(dtLocale->symbol));
-
-      gAggregateTypes.remove(gAggregateTypes.index(dtLocale));
-
-      delete dtLocale;
-
-      dtLocale = NULL;
-    }
+    removeIfUndefinedGlobalType(dtString);
+    removeIfUndefinedGlobalType(dtBytes);
+    removeIfUndefinedGlobalType(dtLocale);
+    removeIfUndefinedGlobalType(dtOwned);
+    removeIfUndefinedGlobalType(dtShared);
   }
 }
 
