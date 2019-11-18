c14b01660461202d7369abb2e924c30e6c91551a
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2019-11-18 12:49:59 -0800

    Add second traversal in case of parent scopes being accessed via uses
    It could happen that a used module has a use of a parent scope, causing it to
    be found in such a way that we ignore its private uses.  When traversing up
    scopes, if we find that we have already seen a module, look through only its
    private uses, to ensure that we don't miss anything.
    
    TODO: I think I can simplify this further, but I want to make sure it resolves
    the correctness issues first.

--- compiler/resolution/visibleFunctions.cpp
+++ compiler/resolution/visibleFunctions.cpp
@@ -310,6 +310,98 @@ static void getVisibleFunctions(const char*           name,
                             inUseChain);
       }
     }
+  } else if (!inUseChain) {
+    // We've seen this block already, but we just found it again from going up
+    // in scope from the call site.  That means that we may have skipped private
+    // uses, so we should go through only the private uses.
+    bool moduleBlock = false;
+    bool fnBlock = false;
+    ModuleSymbol* inMod = block->getModule();
+    FnSymbol* inFn = block->getFunction();
+    BlockStmt* instantiationPt = NULL;
+    if (block->parentExpr != NULL) {
+      // not a module or function level block
+    } else if (inMod && block == inMod->block) {
+      moduleBlock = true;
+    } else if (inFn != NULL) {
+      // TODO - probably remove this assert
+      INT_ASSERT(block->parentSymbol == inFn ||
+                 isArgSymbol(block->parentSymbol) ||
+                 isShadowVarSymbol(block->parentSymbol));
+      fnBlock = true;
+      BlockStmt* inFnInstantiationPoint = inFn->instantiationPoint();
+      if (inFnInstantiationPoint && !inFnInstantiationPoint->parentSymbol) {
+        INT_FATAL(inFn, "instantiation point not in tree\n"
+                        "try --break-on-remove-id %i and consider making\n"
+                        "that block scopeless",
+                        inFnInstantiationPoint->id);
+      }
+      if (inFnInstantiationPoint && inFnInstantiationPoint->parentSymbol)
+        instantiationPt = inFnInstantiationPoint;
+    }
+
+    if (block->useList != NULL) {
+      // the block uses other modules
+      for_actuals(expr, block->useList) {
+        UseStmt* use = toUseStmt(expr);
+
+        INT_ASSERT(use);
+
+        // Only traverse private use statements at this point.  Public use
+        // statements will have already been handled the first time this scope
+        // was seen
+        if (use->isPrivate) {
+
+          bool isMethodCall = false;
+          if (call->numActuals() >= 2 &&
+              call->get(1)->typeInfo() == dtMethodToken)
+            isMethodCall = true;
+
+          if (use->skipSymbolSearch(name, isMethodCall) == false) {
+            SymExpr* se = toSymExpr(use->src);
+
+            INT_ASSERT(se);
+
+            if (ModuleSymbol* mod = toModuleSymbol(se->symbol())) {
+              // The use statement could be of an enum instead of a module,
+              // but only modules can define functions.
+
+              if (mod->isVisible(call) == true) {
+                if (use->isARename(name) == true) {
+                  getVisibleFunctions(use->getRename(name),
+                                      call,
+                                      mod->block,
+                                      visited,
+                                      visibleFns,
+                                      true);
+                } else {
+                  getVisibleFunctions(name,
+                                      call,
+                                      mod->block,
+                                      visited,
+                                      visibleFns, true);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    // Need to continue going up in case our parent scopes also had private
+    // uses that were skipped.
+    if (block != rootModule->block) {
+      BlockStmt* next  = getVisibilityScope(block);
+
+      // Recurse in the enclosing block
+      getVisibleFunctions(name, call, next, visited, visibleFns, inUseChain);
+    }
+
+    if (instantiationPt != NULL) {
+      // Also look at the instantiation point
+      getVisibleFunctions(name, call, instantiationPt, visited, visibleFns,
+                          inUseChain);
+    }
   }
 }
 
