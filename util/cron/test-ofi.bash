42e6ec6fd696ed6727545c1a553ca642040c2f71
Greg Titus <gbtitus@users.noreply.github.com>
2019-11-21 11:46:05 -0600

    Fix a number of flaws in comm=ofi functional testing.
    Yesterday's trial runs of comm=ofi function testing revealed a number of
    flaws in the scripts.  Fix those here: move cray-cs oriented setup into
    its own script, do a better job of recognizing whether or not we're on a
    cray-cs platform, and add some cray-cs specific setup that was missing
    (notably, loading gcc and python modules).
    
    In a future change, I'll also use the new common Cray CS setup script
    for our cray-cs,comm=gasnet testing.

--- util/cron/test-ofi.bash
+++ util/cron/test-ofi.bash
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 #
-# Test comm=ofi on linux64.
+# Test comm=ofi, functionally.
 
 CWD=$(cd $(dirname $0) ; pwd)
 
 source $CWD/common-ofi.bash || \
   ( echo "Could not set up comm=ofi testing." && exit 1 )
 
+if [ $($CHPL_HOME/util/chplenv/chpl_platform.py --target) == cray-cs ] ; then
+  # Need fixed heap on CS systems.
+  # As we test on more targets we might need to adjust this to be a
+  # check against the expected libfabric provider.
+  export CHPL_RT_MAX_HEAP_SIZE=16g
+fi
+
 source $CWD/common-localnode-paratest.bash
 
-export CHPL_NIGHTLY_TEST_DIRS="release/examples"
+export CHPL_NIGHTLY_TEST_DIRS="release/examples/hello*.chpl"
 
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="ofi"
 
