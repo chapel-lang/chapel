50800d134e853074f220adddee66eaad54c1cf67
David Longnecker <dlongnecke@cray.com>
2019-12-10 19:27:43 -0800

    Adjust C tests after chpl_byte_buffer rename

--- test/interop/C/exportString/noArgsRetString.test.c
+++ test/interop/C/exportString/noArgsRetString.test.c
@@ -2,9 +2,9 @@
 
 int main(int argc, char** argv) {
   chpl_library_init(argc, argv);
-  chpl_bytes_wrapper msg = noArgsRetString();
+  chpl_byte_buffer msg = noArgsRetString();
   printf("%s\n", msg.data);
-  chpl_bytes_wrapper_free(msg);
+  chpl_byte_buffer_free(msg);
   chpl_library_finalize();
   return 0;
 }
