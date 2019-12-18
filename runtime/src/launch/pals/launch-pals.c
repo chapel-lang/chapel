c5ad4d3ba626525905448cf0bca7b40b47aedeef
Greg Titus <gbtitus@users.noreply.github.com>
2019-12-18 12:05:11 -0600

    Add a basic PALS-based launcher.
    This adds a basic Chapel launcher for the Cray PALS system launcher on
    Shasta systems.
    
    In support of this, I also added an argv-builder function to the common
    launcher code, adapted from existing code in the mpirun4ofi launcher.
    This helps avoid the problems with the statically sized buffers and arg
    lists which are used in the other launchers.

---
+++ runtime/src/launch/pals/launch-pals.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2004-2019 Cray Inc.
+ * Other additional copyright holders may be indicated within.
+ * 
+ * The entirety of this work is licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * 
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include "chpllaunch.h"
+#include "pals-utils.h"
+
+
+int chpl_launch(int argc, char* argv[], int32_t numLocales) {
+  return chpl_launch_using_exec("cray",
+                                chpl_create_pals_cmd(argc, argv, numLocales),
+                                argv[0]);
+}
+
+
+int chpl_launch_handle_arg(int argc, char* argv[], int argNum,
+                           int32_t lineno, int32_t filename) {
+  return 0;
+}
+
+
+void chpl_launch_print_help(void) { }
