c5a69e89f1855716f1b21c8ff563dce04220c62a
Vassily Litvinov <vasslitvinov@users.noreply.github.com>
2019-12-19 21:11:37 -0600

    Replace USR_FATAL_CONT with USR_PRINT in two cases
    This converts two error cases in the compiler to our common practice
    of using USR_PRINT, not USR_FATAL_CONT, when supplying additional
    information for an error message.

--- compiler/resolution/wrappers.cpp
+++ compiler/resolution/wrappers.cpp
@@ -1231,7 +1231,7 @@ static void errorIfValueCoercionToRef(CallExpr* call, ArgSymbol* formal) {
     USR_FATAL_CONT(call,
                    "value from coercion passed to ref formal '%s'",
                    formal->name);
-    USR_FATAL_CONT(formal->getFunction(),
+    USR_PRINT(formal->getFunction(),
                    "to function '%s' defined here",
                    formal->getFunction()->name);
    }
@@ -1250,7 +1250,7 @@ static void errorIfValueCoercionToRef(CallExpr* call, ArgSymbol* formal) {
       USR_FATAL_CONT(call,
                      "value from coercion passed to const ref formal '%s'",
                      formal->name);
-      USR_FATAL_CONT(formal->getFunction(),
+      USR_PRINT(formal->getFunction(),
                      "to function '%s' defined here",
                      formal->getFunction()->name);
 
