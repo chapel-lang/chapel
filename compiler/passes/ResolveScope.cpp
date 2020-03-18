95904aff9bd0eff0f19852418e3c2d52b2515573
Michael Ferguson <mppf@users.noreply.github.com>
2020-03-18 16:46:21 -0400

    Address review feedback more, improve errors

--- compiler/passes/ResolveScope.cpp
+++ compiler/passes/ResolveScope.cpp
@@ -593,7 +593,7 @@ void ResolveScope::firstImportedModuleName(Expr* expr,
 Symbol* ResolveScope::lookupForImport(Expr* expr, bool isUse) const {
   Symbol* retval = NULL;
 
-  const char* type = isUse?"use":"import";
+  const char* stmtType = isUse ? "use" : "import";
   const char* name = NULL;
   CallExpr* call = NULL;
   const ResolveScope* relativeScope = NULL;
@@ -601,9 +601,9 @@ Symbol* ResolveScope::lookupForImport(Expr* expr, bool isUse) const {
   firstImportedModuleName(expr, name, call, relativeScope);
   if (name == NULL) {
     if (astrThis == getNameFrom(expr))
-      USR_FATAL(expr, "'this.' can only be used as prefix of %s", type);
+      USR_FATAL(expr, "'this.' can only be used as prefix of %s", stmtType);
     else if (astrSuper == getNameFrom(expr))
-      USR_FATAL(expr, "'super.' can only be used as prefix of %s", type);
+      USR_FATAL(expr, "'super.' can only be used as prefix of %s", stmtType);
     else
       INT_FATAL("case not handled");
   }
@@ -640,8 +640,12 @@ Symbol* ResolveScope::lookupForImport(Expr* expr, bool isUse) const {
           retval = sym;
           break;
         }
-        USR_FATAL(expr, "%s must name a module ('%s' not a module)",
-                  type, sym->name);
+        if (isUse)
+          USR_FATAL(expr, "use must name a module or enum ('%s' is neither)",
+                          sym->name);
+        else
+          USR_FATAL(expr, "import must name a module ('%s' is not a module)",
+                          sym->name);
       }
 
       // otherwise follow uses/imports only (breadth first)
@@ -655,11 +659,12 @@ Symbol* ResolveScope::lookupForImport(Expr* expr, bool isUse) const {
     }
 
     if (retval == NULL && badCloserModule != NULL) {
-      USR_FATAL_CONT(expr, "Cannot %s module '%s'", type, name);
+      USR_FATAL_CONT(expr, "Cannot %s module '%s'", stmtType, name);
       USR_PRINT(badCloserModule, "a module named '%s' is defined here", name);
-      USR_PRINT(expr, "full path or explicit relative %s required", type);
+      USR_PRINT(expr, "full path or explicit relative %s required", stmtType);
       USR_PRINT(expr, "please specify the full path to the module");
-      USR_PRINT(expr, "or use a relative %s e.g. '%s this.M' or '%s super.M'", type, type, type);
+      USR_PRINT(expr, "or use a relative %s e.g. '%s this.M' or '%s super.M'",
+                      stmtType, stmtType, stmtType);
       USR_STOP();
     }
   }
@@ -676,7 +681,7 @@ Symbol* ResolveScope::lookupForImport(Expr* expr, bool isUse) const {
     INT_ASSERT(call->isNamedAstr(astrSdot));
     if (!isModuleSymbol(retval))
       USR_FATAL(call, "cannot make nested %s from non-module '%s'",
-                type, retval->name);
+                stmtType, retval->name);
 
     Symbol* outer = retval;
     ModuleSymbol* outerMod = toModuleSymbol(outer);
