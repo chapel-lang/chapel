75906c91df06e9f0d3268360c6dd74bab9e7d9a8
Michael Ferguson <mppf@users.noreply.github.com>
2020-03-23 16:25:22 -0400

    Add --warn-unstable warning for include module

--- compiler/AST/build.cpp
+++ compiler/AST/build.cpp
@@ -825,6 +825,10 @@ BlockStmt* buildIncludeModule(const char* name,
 
   // docs comment is ignored (the one in the module declaration is used)
 
+  if (fWarnUnstable) {
+    USR_WARN(loc, "module include statements are not yet stable and may change");
+  }
+
   return buildChapelStmt(new DefExpr(mod));
 }
 
