c7286e1d4c54241074b21b823cb3c47f56a4f104
Michael Ferguson <mppf@users.noreply.github.com>
2020-06-16 11:12:37 -0400

    Move convertValueToType, codegenLifetimeStart to llvmUtil

--- compiler/include/llvmUtil.h
+++ compiler/include/llvmUtil.h
@@ -2,15 +2,15 @@
  * Copyright 2020 Hewlett Packard Enterprise Development LP
  * Copyright 2004-2019 Cray Inc.
  * Other additional copyright holders may be indicated within.
- * 
+ *
  * The entirety of this work is licensed under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
- * 
+ *
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -47,6 +47,23 @@ llvm::AllocaInst* makeAlloca(llvm::Type* type, const char* name, llvm::Instructi
 
 llvm::Value* createLLVMAlloca(llvm::IRBuilder<>* irBuilder, llvm::Type* type, const char* name);
 PromotedPair convertValuesToLarger(llvm::IRBuilder<> *irBuilder, llvm::Value *value1, llvm::Value *value2, bool isSigned1 = false, bool isSigned2 = false);
+llvm::Value *convertValueToType(llvm::IRBuilder<>* irBuilder,
+                                const llvm::DataLayout& layout,
+                                llvm::LLVMContext &ctx,
+                                llvm::Value *value, llvm::Type *newType,
+                                llvm::Value **alloca, // an alloca generated
+                                bool isSigned=false, bool force=false);
+
+void makeLifetimeStart(llvm::IRBuilder<>* irBuilder,
+                       const llvm::DataLayout& layout,
+                       llvm::LLVMContext &ctx,
+                       llvm::Type *valType, llvm::Value *addr);
+
+// Returns an alloca
+llvm::Value* makeAllocaAndLifetimeStart(llvm::IRBuilder<>* irBuilder,
+                                        const llvm::DataLayout& layout,
+                                        llvm::LLVMContext &ctx,
+                                        llvm::Type* type, const char* name);
 
 int64_t getTypeSizeInBytes(const llvm::DataLayout& layout, llvm::Type* ty);
 bool isTypeSizeSmallerThan(const llvm::DataLayout& layout, llvm::Type* ty, uint64_t max_size_bytes);
