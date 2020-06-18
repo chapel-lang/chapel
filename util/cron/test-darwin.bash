51f344d1cc4ff9ebc89c8f1625577dd0010c4673
ben-albrecht <balbrecht@cray.com>
2020-06-18 10:16:09 -0400

    Clear mason git submodules for all tests now
    The git clean -ffdx worked as expected in darwin testing, so now we are
    promoting the git clean command to common.bash so that all test configs
    will clear out any mason git submodules before starting.

--- util/cron/test-darwin.bash
+++ util/cron/test-darwin.bash
@@ -7,11 +7,6 @@ source $CWD/common.bash
 source $CWD/common-darwin.bash
 source $CWD/common-localnode-paratest.bash
 
-# TODO: To be promoted to common.bash if this works
-# Work-around to remove git submodules
-#   See Cray/chapel-private#1050 for long term solution
-git clean -ffdx ${CHPL_HOME}/test/mason
-
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="darwin"
 
 $CWD/nightly -cron $(get_nightly_paratest_args)
