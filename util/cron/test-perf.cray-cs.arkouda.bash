e827f2c9c4e3f42750e134248bf963450f1b5c64
Elliot Ronaghan <ronawho@gmail.com>
2020-07-01 16:41:41 -0400

    Remove hacks that tried to help with Arkouda OOM

--- util/cron/test-perf.cray-cs.arkouda.bash
+++ util/cron/test-perf.cray-cs.arkouda.bash
@@ -11,15 +11,10 @@ export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.cray-cs.arkouda"
 source $CWD/common-arkouda.bash
 export ARKOUDA_NUMLOCALES=16
 
-# limit to 1 trial to limit memory fragmentation
-export CHPL_TEST_NUM_TRIALS=1
-# increase timeout for setops tests
-export ARKOUDA_CLIENT_TIMEOUT=900
-
 # setup for CS perf (gasnet-large, gnu, 36-core Broadwell)
 source $CWD/common-cray-cs.bash
 source $CWD/common-perf-cray-cs.bash
-export GASNET_PHYSMEM_MAX=103G
+export GASNET_PHYSMEM_MAX=83G
 export GASNET_ODP_VERBOSE=0
 export CHPL_LAUNCHER=slurm-gasnetrun_ibv
 nightly_args="${nightly_args} -no-buildcheck"
