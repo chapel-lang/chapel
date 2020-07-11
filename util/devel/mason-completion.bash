8a107509a8aa3163af7ee97482340873ca427a0d
ankush.bhardwaj0@gmail.com <ankush.bhardwaj0@gmail.com>
2020-07-11 15:28:27 +0530

    added completion for mason init

--- util/devel/mason-completion.bash
+++ util/devel/mason-completion.bash
@@ -1,31 +1,63 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
+
+_complete_mason_init(){
+  local optional 
+  local optional_abbr
+  local arg
+  local cur
+  cur=${COMP_WORDS[COMP_CWORD]}
+  arg=$(mason init --help | grep -v '^[A-Za-z]' | grep -v '^.*--'  | \
+     grep -v '<command>' | grep -v '\[options\]'  | sed 's/^[ \t]*//' | \
+     cut -d" " -f1 | xargs echo)
+  optional=$(mason init --help | grep -v '^[A-Za-z]' | grep '^.*--' |      \
+            sed -e 's;^\(.*--[^ ]*\).*$;\1;' | sed -e 's;-[A-Za-z],;   ;' | \
+            xargs echo)
+  optional_abbr=$(mason init --help | grep -v '^[A-Za-z]' | grep '^.*--' |        \
+          sed -e 's;^\(.*--[^ ]*\).*$;\1;' | grep ',' | sed -e 's;,.*$;;' | \
+          xargs echo)
+  case "$cur" in
+    -*)
+      COMPREPLY=($(compgen -W "$optional $optional_abbr" -- "$1"))
+      ;;
+    *)
+      COMPREPLY=($(compgen -d -S / -- "$cur"))
+      ;;
+  esac
+}
 
 _complete_mason_new(){
-  local optional=`mason new --help | grep -v '^[A-Za-z]' | grep '^.*--' |      \
+  local optional
+  local optional_abbr
+  optional=$(mason new --help | grep -v '^[A-Za-z]' | grep '^.*--' |      \
             sed -e 's;^\(.*--[^ ]*\).*$;\1;' | sed -e 's;-[A-Za-z],;   ;' | \
-                      xargs echo`
-  local optional_abbr=`mason new --help | grep -v '^[A-Za-z]' | grep '^.*--' |        \
+            xargs echo)
+  optional_abbr=$(mason new --help | grep -v '^[A-Za-z]' | grep '^.*--' |        \
           sed -e 's;^\(.*--[^ ]*\).*$;\1;' | grep ',' | sed -e 's;,.*$;;' | \
-                  xargs echo`
+          xargs echo)
   COMPREPLY=($(compgen -W "$optional $optional_abbr" -- "$1"))
 
 }
 _complete_mason_option(){
-  local optional=`mason --help | grep -v '^[A-Za-z]' | grep '^.*--' |      \
+  local optional
+  local optional_abbr
+  optional=$(mason --help | grep -v '^[A-Za-z]' | grep '^.*--' |      \
             sed -e 's;^\(.*--[^ ]*\).*$;\1;' | sed -e 's;-[A-Za-z],;   ;' | \
-                      xargs echo`
-  local optional_abbr=`mason --help | grep -v '^[A-Za-z]' | grep '^.*--' |        \
+            xargs echo)
+  optional_abbr=$(mason --help | grep -v '^[A-Za-z]' | grep '^.*--' |        \
           sed -e 's;^\(.*--[^ ]*\).*$;\1;' | grep ',' | sed -e 's;,.*$;;' | \
-                  xargs echo`
-  COMPREPLY=($(compgen -W "$optional $optional_abbr" -- $1 ))
+          xargs echo)
+  COMPREPLY=($(compgen -W "$optional $optional_abbr" -- "$1" ))
 }
 
 _complete_mason_command(){
-  local cur=${COMP_WORDS[COMP_CWORD]}
-  local prev="${COMP_WORDS[COMP_CWORD-1]}"
-  local arg=`mason --help | grep -v '^[A-Za-z]' | grep -v '^.*--'  | \
+  local cur
+  local prev
+  local arg
+  cur=${COMP_WORDS[COMP_CWORD]}
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  arg=$(mason --help | grep -v '^[A-Za-z]' | grep -v '^.*--'  | \
      grep -v '<command>' | grep -v '\[options\]'  | sed 's/^[ \t]*//' | \
-     cut -d" " -f1 | xargs echo`
+     cut -d" " -f1 | xargs echo)
     COMPREPLY=($(compgen -W "$arg" "${COMP_WORDS[1]}"))
 }
 
@@ -37,20 +69,41 @@ _mason(){
   then
     case "$cur" in
       -*)
-        _complete_mason_option $cur 
+        _complete_mason_option "$cur"
         ;;
       *)
-        _complete_mason_command $cur
+        _complete_mason_command "$cur"
     esac
   fi
   if [[ ${COMP_CWORD} -ge 2 ]]
   then
     local cur=${COMP_WORDS[COMP_CWORD]}
     local prev=${COMP_WORDS[COMP_CWORD-1]}
-    case $prev in 
+    local alt_prev=${COMP_WORDS[COMP_CWORD-2]}
+    local subcmd
+    local arg
+    arg=$(mason --help | grep -v '^[A-Za-z]' | grep -v '^.*--'  | \
+       grep -v '<command>' | grep -v '\[options\]'  | sed 's/^[ \t]*//' | \
+       cut -d" " -f1 | xargs echo)
+
+    for i in $arg
+    do
+      if [[ $prev == "$i" ]]
+      then
+        subcmd=$prev
+        break;
+      elif [[ $alt_prev == "$i" ]]
+      then
+        subcmd=$alt_prev
+        break;
+      fi
+    done
+    case "$subcmd" in 
       'new')
-        _complete_mason_new $cur
+        _complete_mason_new "$cur"
         ;;
+      'init')
+        _complete_mason_init "$cur"
     esac
   fi
 }
