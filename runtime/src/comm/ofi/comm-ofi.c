eb043e67d03ba036f746a61429ff06e47e3730a3
Greg Titus <gbtitus@users.noreply.github.com>
2020-07-21 14:58:22 -0500

    Don't accept the RxD utility provider without being forced to do so.
    Using the RxD utility provider seems to cause hangs.  Based on the
    libfabric Makefiles not building the man page for it by default, RxD
    isn't yet a full-fledged provider.  So, skip it unless the user asks for
    it specifically.

--- runtime/src/comm/ofi/comm-ofi.c
+++ runtime/src/comm/ofi/comm-ofi.c
@@ -1081,7 +1081,13 @@ void debugOverrideHints(struct fi_info* hints) {
 #endif
 
 
-static
+static inline
+chpl_bool isInProvider(const char* s, struct fi_info* info) {
+  return isInProvName(s, info->fabric_attr->prov_name);
+}
+
+
+static inline
 chpl_bool isGoodCoreProvider(struct fi_info* info) {
   if (!isInProvName("sockets", info->fabric_attr->prov_name)
       && !isInProvName("tcp", info->fabric_attr->prov_name)) {
@@ -1137,6 +1143,8 @@ void init_ofiFabricDomain(void) {
   // - in addition, include the memory registration modes we can support
   //
   const char* prov_name = getProviderName();
+  const chpl_bool mandatory_RxD = isInProvName("ofi_rxd", prov_name);
+  const chpl_bool mandatory_RxM = isInProvName("ofi_rxm", prov_name);
 
   struct fi_info* hints;
   CHK_TRUE((hints = fi_allocinfo()) != NULL);
@@ -1252,13 +1260,19 @@ void init_ofiFabricDomain(void) {
   struct fi_info* infoTxOrd = NULL;
   OFI_CHK_2(fi_getinfo(COMM_OFI_FI_VERSION, NULL, NULL, 0, hints, &infoTxOrd),
             ret, -FI_ENODATA);
-  if (ret == FI_SUCCESS
-      && (isGoodCoreProvider(infoTxOrd) || prov_name != NULL)) {
-    DBG_PRINTF_NODE0(DBG_CFGFAB,
-                     "====================\n"
-                     "with transaction orderings, found desirable provider");
-    ofi_info = infoTxOrd;
-    goto haveProvider;
+
+  if (ret == FI_SUCCESS) {
+    for (struct fi_info* info = infoTxOrd; info != NULL; info = info->next) {
+      if ((prov_name != NULL || isGoodCoreProvider(info))
+          && (mandatory_RxD || !isInProvider("ofi_rxd", info))) {
+        DBG_PRINTF_NODE0(DBG_CFGFAB,
+                         "====================\n"
+                         "found desirable provider with transaction orderings");
+        ofi_info = fi_dupinfo(info);
+        fi_freeinfo(infoTxOrd);
+        goto haveProvider;
+      }
+    }
   }
 
   DBG_PRINTF_NODE0(DBG_CFGFAB,
@@ -1281,29 +1295,23 @@ void init_ofiFabricDomain(void) {
   OFI_CHK_2(fi_getinfo(COMM_OFI_FI_VERSION, NULL, NULL, 0, hints, &infoCmplt),
             ret, -FI_ENODATA);
   if (ret == FI_SUCCESS) {
-    struct fi_info* info = infoCmplt;
-    if (!isInProvName("ofi_rxm", prov_name)) { // find non-RxM, unless forced
-      for ( ; info != NULL; info = info->next) {
-        if ((isGoodCoreProvider(info) || prov_name != NULL)
-            && !isInProvName("ofi_rxm", info->fabric_attr->prov_name)) {
-          break;
-        }
+    for (struct fi_info* info = infoCmplt; info != NULL; info = info->next) {
+      if ((prov_name != NULL || isGoodCoreProvider(info))
+          && (mandatory_RxD || !isInProvider("ofi_rxd", info))
+          && (mandatory_RxM || !isInProvider("ofi_rxm", info))) {
+        DBG_PRINTF_NODE0(DBG_CFGFAB,
+                         "====================\n"
+                         "found desirable provider with delivery-complete");
+        ofi_info = fi_dupinfo(info);
+        fi_freeinfo(infoCmplt);
+        goto haveProvider;
       }
-    }
-
-    if (info != NULL) {
-      DBG_PRINTF_NODE0(DBG_CFGFAB,
-                       "====================\n"
-                       "with delivery-complete, found desirable provider");
-      ofi_info = fi_dupinfo(info);
-      fi_freeinfo(infoCmplt);
-      goto haveProvider;
     }
   }
 
   DBG_PRINTF_NODE0(DBG_CFGFAB,
                    "====================\n"
-                   "with delivery-complete, no desirable provider");
+                   "no desirable provider with delivery-complete");
 
   hints->tx_attr->op_flags = FI_COMPLETION;
 
@@ -1314,8 +1322,8 @@ void init_ofiFabricDomain(void) {
   if (infoTxOrd != NULL) {
     DBG_PRINTF_NODE0(DBG_CFGFAB,
                      "====================\n"
-                     "with transaction orderings, "
-                     "found a less desirable provider");
+                     "found less-desirable provider with transaction "
+                     "orderings");
     ofi_info = infoTxOrd;
     if (infoCmplt != NULL) {
       fi_freeinfo(infoCmplt);
@@ -1326,8 +1334,7 @@ void init_ofiFabricDomain(void) {
   if (infoCmplt != NULL) {
     DBG_PRINTF_NODE0(DBG_CFGFAB,
                      "====================\n"
-                     "with delivery-complete, "
-                     "found a less desirable provider");
+                     "found less-desirable provider with delivery-complete");
     ofi_info = infoCmplt;
     goto haveProvider;
   }
