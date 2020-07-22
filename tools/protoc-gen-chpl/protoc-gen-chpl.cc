e7e09a116eb636a1c3d7ea9bfe315e5d6681d7eb
Aniket21mathur <amathur@ec.iitr.ac.in>
2020-07-22 12:31:42 +0530

    Rename cc files to cpp and add license

--- tools/protoc-gen-chpl/protoc-gen-chpl.cc
+++
@@ -1,8 +0,0 @@
-#include <google/protobuf/compiler/plugin.h>
-
-#include <generator.hh>
-
-int main(int argc, char *argv[]) {
-  chapel::Generator generator;
-  return PluginMain(argc, argv, &generator);
-}
