99297428492a7d986653a61e59b3b89c575b9cb9
Engin Kayraklioglu <ekayrakli@gmail.com>
2020-09-04 12:05:23 -0700

    Adjust valgrind script to use paratest.chapcs

--- util/cron/test-valgrind.bash
+++ util/cron/test-valgrind.bash
@@ -5,11 +5,10 @@
 CWD=$(cd $(dirname $0) ; pwd)
 source $CWD/common.bash
 source $CWD/common-valgrind.bash
-source $CWD/common-localnode-paratest.bash
 
 # valgrind serializes execution, so don't limit to one executable at a time
 unset CHPL_TEST_LIMIT_RUNNING_EXECUTABLES
 
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="valgrind"
 
-$CWD/nightly -cron ${nightly_args} $(get_nightly_paratest_args 20)
+$CWD/nightly -cron ${nightly_args} -chapcs
