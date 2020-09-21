a52a7a8d2687617ae9c0609ff81a91b03ddad27b
Engin Kayraklioglu <ekayrakli@gmail.com>
2020-09-21 16:50:59 -0700

    Add --no-report-auto-local-access
    Signed-off-by: CaptainSharf <mohdsharf7407@gail.com>

--- compiler/main/driver.cpp
+++ compiler/main/driver.cpp
@@ -1076,7 +1076,7 @@ static ArgumentDescription arg_desc[] = {
  {"report-inlined-iterators", ' ', NULL, "Print stats on inlined iterators", "F", &fReportInlinedIterators, NULL, NULL},
  {"report-vectorized-loops", ' ', NULL, "Show which loops have vectorization hints", "F", &fReportVectorizedLoops, NULL, NULL},
  {"report-optimized-on", ' ', NULL, "Print information about on clauses that have been optimized for potential fast remote fork operation", "F", &fReportOptimizedOn, NULL, NULL},
- {"report-auto-local-access", ' ', NULL, "Enable compiler logs for auto local access optimization", "F", &fReportAutoLocalAccess, "CHPL_REPORT_AUTO_LOCAL_ACCESS", NULL},
+ {"report-auto-local-access", ' ', NULL, "Enable compiler logs for auto local access optimization", "N", &fReportAutoLocalAccess, "CHPL_REPORT_AUTO_LOCAL_ACCESS", NULL},
  {"report-optimized-forall-unordered-ops", ' ', NULL, "Show which statements in foralls have been converted to unordered operations", "F", &fReportOptimizeForallUnordered, NULL, NULL},
  {"report-promotion", ' ', NULL, "Print information about scalar promotion", "F", &fReportPromotion, NULL, NULL},
  {"report-scalar-replace", ' ', NULL, "Print scalar replacement stats", "F", &fReportScalarReplace, NULL, NULL},
