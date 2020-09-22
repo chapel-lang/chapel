ebe4f60fa7e54d073854d39c4f056b4353c59e1b
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2020-09-22 14:58:03 -0700

    Update error message when conflict due to re-exporting
    It's possible that the conflicting symbol that was re-exported had been
    re-exported multiple times.  In this case, looking at the re-export point could
    be confusing for the user, since they'll be told about it and the original
    module where the symbol was defined.  By adjusting the error message to say it
    was the last point the symbol was re-exported from before the conflict, the user
    is more likely to be open to the possibility that the symbol was re-exported
    multiple times.
    
    ----
    Signed-off-by: Lydia Duncan <lydia-duncan@users.noreply.github.com>

--- compiler/passes/scopeResolve.cpp
+++ compiler/passes/scopeResolve.cpp
@@ -1562,7 +1562,7 @@ printConflictingSymbols(std::vector<Symbol*>& symbols, Symbol* sym,
     else {
       if (VisibilityStmt* reexport = reexportPts[another]) {
         USR_PRINT(another,
-                  "symbol '%s', defined here, was reexported at %s:%d",
+                  "symbol '%s', defined here, was last reexported at %s:%d",
                   another->name, reexport->astloc.filename,
                   reexport->astloc.lineno);
       }
@@ -1602,7 +1602,7 @@ void checkConflictingSymbols(std::vector<Symbol *>& symbols,
         USR_FATAL_CONT(sym, "symbol %s is multiply defined", name);
 
         if (VisibilityStmt* reexport = reexportPts[sym]) {
-          USR_PRINT("'%s' was reexported at %s:%d", name,
+          USR_PRINT("'%s' was last reexported at %s:%d", name,
                     reexport->astloc.filename, reexport->astloc.lineno);
         }
         astlocT* symRenameLoc = renameLocs[sym];
