cd8f05e2c768893a993253ea3bb1d8a1be9a2eba
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2020-10-02 11:59:11 -0700

    Fixup chpl_bytes into bytes in internal modules
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
    Signed-off-by: CaptainSharf <mohdsharf7407@gail.com>

--- modules/internal/fixInternalDocs.sh
+++ modules/internal/fixInternalDocs.sh
@@ -221,6 +221,7 @@ replace "_complex" "complex" $file
 file="./String.rst"
 fixTitle "Strings" $file
 removeUsage $file
+replace "chpl_bytes" "bytes" $file
 
 ## End of String ##
 
@@ -229,6 +230,7 @@ removeUsage $file
 file="./Bytes.rst"
 fixTitle "Bytes" $file
 removeUsage $file
+replace "chpl_bytes" "bytes" $file
 
 ## End of Bytes ##
 
