ea180ab553fda7d748033f9e5b110f157943efd7
Michael Ferguson <mppf@users.noreply.github.com>
2020-11-03 09:33:13 -0500

    Add python3-venv as a dependency in vagrant testing
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

--- util/devel/test/vagrant/provision-scripts/apt-get-deps.sh
+++ util/devel/test/vagrant/provision-scripts/apt-get-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 apt-get update
-apt-get install -y gcc g++ m4 perl python3 python3-pip python3-dev bash make mawk git pkg-config
+apt-get install -y gcc g++ m4 perl python3 python3-venv python3-pip python3-dev bash make mawk git pkg-config
