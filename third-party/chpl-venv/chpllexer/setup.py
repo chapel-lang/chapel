50c1cc2f0e3e1ab0baf7811ac178a96c97381fb2
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2020-11-04 15:23:57 -0800

    Put the pygments lexer in a separate local package
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

---
+++ third-party/chpl-venv/chpllexer/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r') as fp:
+    long_desc = fp.read()
+
+setup(
+    name='chpllexer',
+    license='Apache License v2.0',
+    author='Chapel Team',
+    description='Chapel lexer for Pygments',
+    long_description=long_desc,
+    packages=find_packages(include=['chpllexer', 'chpllexer.*']),
+    entry_points=
+    """
+    [pygments.lexers]
+    chpllexer = chpllexer.lexer:chpllexer
+    """,
+)
