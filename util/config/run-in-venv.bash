775a1dd57fde8bfa3d6764da54382f684fbb9b0a
Michael Ferguson <mppf@users.noreply.github.com>
2020-11-05 16:32:21 -0500

    Adjust env vars instead of using venv activate
    The venv activate script stores the absolute path
    but that causes problems in case it changes
    (e.g. if CHPL_HOME is renamed or in the case of
     module builds)
    
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>
    
    Signed-off-by: CaptainSharf <mohdsharf7407@gail.com>

--- util/config/run-in-venv.bash
+++ util/config/run-in-venv.bash
@@ -8,18 +8,21 @@ if [ -z "$CHPL_HOME" ]; then
 fi
 
 python=$($CHPL_HOME/util/config/find-python.sh)
-venv_path=$("$python" "$CHPL_HOME/util/chplenv/chpl_home_utils.py" --venv)
+venv_dir=$("$python" "$CHPL_HOME/util/chplenv/chpl_home_utils.py" --venv)
 
-if [ ! -d "$venv_path" ]; then
-  echo "Error: virtualenv '$venv_path' does not exist" 1>&2
+if [ ! -d "$venv_dir" ]; then
+  echo "Error: virtualenv '$venv_dir' does not exist" 1>&2
   exit 1
 fi
 
-if [ ! -f "$venv_path/bin/activate" ]; then
-  echo "Activation file $venv_path/bin/activate is missing" 1>&2
+if [ ! -f "$venv_dir/bin/python3" ]; then
+  echo "python3 wrapper file $venv_dir/bin/python3 is missing" 1>&2
   exit 1
 fi
 
-source "$venv_path/bin/activate"
+# these steps correspond to what a venv activate script contains
+export VIRTUAL_ENV="$venv_dir"
+export PATH="$VIRTUAL_ENV/bin:$PATH"
+unset PYTHONHOME
 
 exec "$1" "${@:2}"
