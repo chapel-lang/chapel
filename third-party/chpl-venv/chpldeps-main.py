b233787b36f43ae720d53310ffaaecda3f76ca98
Michael Ferguson <mppf@users.noreply.github.com>
2020-11-17 11:33:16 -0500

    Improve chpldeps main script
    * include register-python-argcomplete as a subcommand
    * do better checking on the arguments and print out usage
    
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>
    
    Signed-off-by: CaptainSharf <mohdsharf7407@gail.com>

--- third-party/chpl-venv/chpldeps-main.py
+++ third-party/chpl-venv/chpldeps-main.py
@@ -1,5 +1,7 @@
+# This script ends up in chpldeps/__main__.py
+
 """
-chpldeps/__main__.py
+chpldeps
 ======
 
 This Python module contains the dependencies used by the Chapel project.
@@ -7,6 +9,7 @@ Dependencies can be invoked as subcommands in the following ways:
 
   python3 path/to/chpldeps sphinx-build <sphinx options>
   python3 path/to/chpldeps rst2man <rst2man options>
+  python3 path/to/chpldeps register-python-argcomplete
   python3 path/to/chpldeps path/to/something.py <something.py options>
 
 For the last case, this program assumes that something.py has a
@@ -20,14 +23,25 @@ import re
 import sys
 import importlib.util
 
+def missing_subcommand():
+    print("chpldeps requires a subcommand", file=sys.stderr)
+    print(__doc__, file=sys.stderr)
+    sys.exit(-1)
+
 if __name__ == '__main__':
 
     # leave out a python3 call if present
     starti = 1
+    if starti >= len(sys.argv):
+        missing_subcommand()
+
     if (sys.argv[1] == 'python3' or sys.argv[1] == 'python' or
         sys.argv[1].endswith('/python3') or sys.argv[1].endswith('/python')):
 
         starti = 2
+
+    if starti >= len(sys.argv):
+        missing_subcommand()
 
     # compute the subcommand we are running
     subcommand = sys.argv[starti]
@@ -58,6 +72,16 @@ if __name__ == '__main__':
 
         publish_cmdline(writer=manpage.Writer(), description=description)
 
+    elif subcommand == 'register-python-argcomplete':
+
+        # Run the argument as a program within bin/
+        # just by exec'ing it
+        # (can't import something with dashes)
+        my_bin_dir = os.path.join(os.path.dirname(__file__), 'bin')
+        subcommand_path = os.path.join(my_bin_dir, subcommand)
+
+        exec(open(subcommand_path).read())
+
     elif os.path.isfile(subcommand):
         # Run the argument as a python script with the dependencies.
         # Assumes that the script uses the convention of having a
@@ -73,4 +97,5 @@ if __name__ == '__main__':
     else:
         print("Unknown chpldeps subcommand {0}".format(subcommand),
               file=sys.stderr)
+        print(__doc__, file=sys.stderr)
         sys.exit(-1)
