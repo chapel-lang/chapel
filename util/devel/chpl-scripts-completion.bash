23a1be452ceeacd238e36769e335da8f1dcbffa1
Michael Ferguson <mppf@users.noreply.github.com>
2020-11-17 11:33:49 -0500

    Fix chpl-scripts-completion.bash for venv->chpldeps
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

--- util/devel/chpl-scripts-completion.bash
+++ util/devel/chpl-scripts-completion.bash
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
-CWD=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
-venv_path=$(python "$CWD/../chplenv/chpl_home_utils.py" --test-venv)
+# get the chpl home directory
+FIND_CHPL_HOME=$(cd $(dirname ${BASH_SOURCE[0]}); cd ..; cd ..; pwd)
 
-register_argcomplete="register-python-argcomplete"
-if [[ "$venv_path" != "none" && -d "$venv_path" ]]; then
-  register_argcomplete="$venv_path/bin/$register_argcomplete"
+CHPL_PYTHON=$($FIND_CHPL_HOME/util/config/find-python.sh)
+CHPLDEPS=$($CHPL_PYTHON "$FIND_CHPL_HOME/util/chplenv/chpl_home_utils.py" --chpldeps)
+
+register_argcomplete="none"
+if [[ "$CHPLDEPS" != "none" && -d "$CHPLDEPS" ]]; then
+  register_argcomplete="$CHPL_PYTHON $CHPLDEPS register-python-argcomplete"
+else
+  if [ -x "$(command -v register-python-argcomplete)" ]; then
+    register_argcomplete="register-python-argcomplete"
+  fi
 fi
 
 # Register any python scripts that use argparse/argcomplete
-if [ -x "$(command -v $register_argcomplete)" ]; then
+if [ "$register_argcomplete" != "none" ]; then
   eval "$($register_argcomplete start_test)"
 fi
