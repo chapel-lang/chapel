0c9fc10c44682a01e603af7f63db9e9205ecfddf
Elliot Ronaghan <ronawho@gmail.com>
2020-11-17 10:19:19 -0500

    Fix chpl_home_utils.py --test-venv calls
    The --test-venv option was replaced with --chpldeps in 16694, so adjust
    script that were using it.
    
    Signed-off-by: Elliot Ronaghan <ronawho@gmail.com>

--- util/devel/chpl-scripts-completion.bash
+++ util/devel/chpl-scripts-completion.bash
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 CWD=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
-venv_path=$(python "$CWD/../chplenv/chpl_home_utils.py" --test-venv)
+venv_path=$(python "$CWD/../chplenv/chpl_home_utils.py" --chpldeps)
 
 register_argcomplete="register-python-argcomplete"
 if [[ "$venv_path" != "none" && -d "$venv_path" ]]; then
+  export PYTHONPATH="$venv_path":$PYTHONPATH
   register_argcomplete="$venv_path/bin/$register_argcomplete"
 fi
 
