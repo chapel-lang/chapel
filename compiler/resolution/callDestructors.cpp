657d08a639a0c1d2448e40e60cbe8038053cf849
Michael Ferguson <mppf@users.noreply.github.com>
2020-11-18 18:39:08 -0500

    Fix memory leak for global array type expressions
    This commit fixes the following so it does not leak:
    
      type arrOfArr = [1..2][100..200] int;
      writeln(arrOfArr:string);
    
    This is visible in the existing test
      types/typedefs/bradc/arrayTypedef.chpl
    
    The problem here is that evaluateAutoDestroy in normalize.cpp
    hoists some temporaries out of forall-expr functions representing
    types into module scope. Meanwhile, the change to callDestructors in
    PR #16430 caused it to no longer deinitialize such globals (because
    it never found the initialization because it is within another function).
    This commit adjusts collectGlobals in callDestructors to delve into
    functions that might return array types.
    
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

--- compiler/resolution/callDestructors.cpp
+++ compiler/resolution/callDestructors.cpp
@@ -1027,6 +1027,13 @@ static void collectGlobals(ModuleSymbol* mod,
           if (VarSymbol* v = initsVariableOut(formal, actual))
             noteGlobalInitialization(mod, v, inited, initedSet);
         }
+
+        // Recurse into calls to compiler generated functions that
+        // return array types (forall expression functions).
+        // (Seach for globalTemps in normalize.cpp to see related code).
+        if (FnSymbol* calledFn = fCall->resolvedFunction())
+          if (calledFn->hasFlag(FLAG_MAYBE_ARRAY_TYPE))
+            collectGlobals(mod, calledFn->body, inited, initedSet);
       }
 
     // Recurse in to a BlockStmt (or sub-classes of BlockStmt e.g. a loop)
