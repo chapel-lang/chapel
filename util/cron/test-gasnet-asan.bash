0cbcb80aadc6c2b5646fdea15b9945f4010e9f46
David Longnecker <dlongnecke-cray@users.noreply.github.com>
2020-11-18 18:05:31 -0800

    Add first draft of asan gasnet cron script
    Signed-off-by: David Longnecker <dlongnecke-cray@users.noreply.github.com>

---
+++ util/cron/test-gasnet-asan.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# Test ASAN-compatible configuration with gasnet on full suite with
+# ASAN on linux64.
+
+CWD=$(cd $(dirname $0) ; pwd)
+source $CWD/common-gasnet.bash
+source $CWD/common-asan.bash
+source $CWD/common-localnode-paratest.bash
+
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="gasnet-asan"
+
+export GASNET_QUIET=Y
+
+# "Should be able to detect most memory errors, since remote r/w are
+# performed using active messages instead of RDMA".
+export CHPL_COMM_SUBSTRATE=udp
+export CHPL_GASNET_SEGMENT=everything
+
+echo $($CWD/nightly -cron ${nightly_args} $(get_nightly_paratest_args 6) -multilocale)
