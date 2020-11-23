f29889b58f24f04c97eb71a0f097d3420c4aca86
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2020-11-23 12:17:51 -0800

    Fix two issues with python27 testing
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
    Signed-off-by: CaptainSharf <mohdsharf7407@gail.com>

--- util/cron/test-linux64-python27.bash
+++ util/cron/test-linux64-python27.bash
@@ -9,4 +9,18 @@ source $CWD/common.bash
 export CHPL_NIGHTLY_MAKE=gmake
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="linux64-python27"
 
-$CWD/nightly -cron -python2 -examples ${nightly_args}
+# we need to override pythons that were setup by `source .bashrc` in
+# common.bash before running nightly. Also see the llvm config for a similar
+# trick
+py27_setup=/data/cf/chapel/setup_python27.bash
+if [ -f "${py27_setup}" ] ; then
+  source ${py27_setup}
+else
+  echo "[Warning: cannot find the python configuration script: ${py27_setup}]"
+fi
+
+export PATH=/cray/css/users/chapelu/no-python:$PATH
+export PATH=/cray/css/users/chapelu/no-python3:$PATH
+
+
+$CWD/nightly -cron -python2 ${nightly_args}
