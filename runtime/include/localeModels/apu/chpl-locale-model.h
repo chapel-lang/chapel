7921c7a22e075947921e94b38eb91056c1679dd2
Jenna Hoole Starkey <jhoole@users.noreply.github.com>
2020-12-18 11:43:59 -0800

    Update copyright year to extend to 2021.
    Signed-off-by: CaptainSharf <mohdsharf7407@gail.com>

--- runtime/include/localeModels/apu/chpl-locale-model.h
+++ runtime/include/localeModels/apu/chpl-locale-model.h
@@ -1,6 +1,6 @@
 /*
  * Copyright 2017 Advanced Micro Devices, Inc.
- * Copyright 2020 Hewlett Packard Enterprise Development LP
+ * Copyright 2020-2021 Hewlett Packard Enterprise Development LP
  * Copyright 2004-2019 Cray Inc.
  * Other additional copyright holders may be indicated within.
  * 
