d91b31bb92835c94b7f5cbfacea68365c3f288b3
Michael Ferguson <mppf@users.noreply.github.com>
2021-04-08 08:00:30 -0400

    Improve the unique string test
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

--- compiler/libchplcomp/test/testUniqueString.cpp
+++ compiler/libchplcomp/test/testUniqueString.cpp
@@ -1,6 +1,11 @@
 #include "chpl/AST/Context.h"
 #include "chpl/AST/UniqueString.h"
 
+// always check assertions in this test
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+
 #include <chrono>
 #include <fstream>
 #include <sstream>
@@ -12,32 +17,62 @@ using namespace chpl;
 using namespace ast;
 
 void testPerformance(Context* ctx, const char* inputFile, bool printTiming) {
-  int repeat = 10;
+  int outerRepeat = 10;
+  int innerRepeat = 1;
 
-  std::ifstream file(inputFile);
-  if (file.is_open()) {
-    auto start = std::chrono::steady_clock::now();
-    std::string line;
-    std::string word;
-    while (std::getline(file, line)) {
-      for (int i = 0; i < repeat; i++) {
-        std::istringstream iss(line, std::istringstream::in);
-        while (iss >> word) {
-          UniqueString::build(ctx, word);
+  bool dohisto = false;
+  int querybysize[41];
+  int nqueries = 0;
+  for(int i = 0; i < 41; i++) querybysize[i] = 0;
+
+  auto start = std::chrono::steady_clock::now();
+  for (int i = 0; i < outerRepeat; i++) {
+    std::ifstream file(inputFile);
+    if (file.is_open()) {
+      std::string line;
+      std::string word;
+      while (std::getline(file, line)) {
+        for (int j = 0; j < innerRepeat; j++) {
+          std::istringstream iss(line, std::istringstream::in);
+          while (iss >> word) {
+            // double the word length to better match
+            // statistics of Chapel code
+            word += word;
+
+            if (dohisto) {
+              int len = word.size();
+              if (len > 40) len = 40;
+              querybysize[len]++;
+              nqueries++;
+            }
+            UniqueString::build(ctx, word);
+          }
         }
       }
+      file.close();
+    } else {
+      std::cerr << "could not open file " << inputFile << "\n";
+      exit(-1);
     }
-    auto end = std::chrono::steady_clock::now();
+  }
 
-    std::chrono::duration<double> elapsed = end - start;
+  auto end = std::chrono::steady_clock::now();
 
-    if (printTiming)
-      std::cout << "unordered map elapsed time: " << elapsed.count() << " s\n";
+  std::chrono::duration<double> elapsed = end - start;
 
-    file.close();
-  } else {
-    std::cerr << "could not open file " << inputFile << "\n";
-    exit(-1);
+  if (printTiming) {
+    std::cout << "unordered map elapsed time: " << elapsed.count() << " s\n";
+
+    if (dohisto) {
+      std::cout << "Queried " << nqueries << " strings\n";
+      int sum = 0;
+      for(int i = 0; i < 40; i++) {
+        std::cout << "length < " << i << " -- " << sum << "\n";
+        sum += querybysize[i];
+        std::cout << "length " << i << " -- " << querybysize[i] << "\n";
+      }
+      std::cout << "length >= 40 " << " -- " << querybysize[40] << "\n";
+    }
   }
 }
 
@@ -57,24 +92,29 @@ int main(int argc, char** argv) {
 
   // First, add some strings to the map and make sure we get uniqueness.
   {
-    std::string test1 = "test1";
+    // needs to be long enough to require strdup etc.
+    #define TEST1STRING "this is a very very very long string"
+    std::string test1 = TEST1STRING;
     std::string test1Copy = test1;
     assert(test1.c_str() != test1Copy.c_str());
     UniqueString t1 = UniqueString::build(&ctx, test1);
     UniqueString t2 = UniqueString::build(&ctx, test1Copy);
-    UniqueString t3 = UniqueString::build(&ctx, "test1");
+    UniqueString t3 = UniqueString::build(&ctx, TEST1STRING);
     assert(t1.c_str() == t2.c_str());
     assert(t2.c_str() == t3.c_str());
 
-    UniqueString h = UniqueString::build(&ctx, "hello");
-    assert(h.c_str() != t1.c_str());
+    // this string is short enough to be inlined
+    std::string hello = "hello";
+    UniqueString h1 = UniqueString::build(&ctx, "hello");
+    UniqueString h2 = UniqueString::build(&ctx, hello);
+    assert(h1 == h2);
 
     // check that uniqueString(NULL) == uniqueString("")
     assert(UniqueString::build(&ctx, NULL) == UniqueString::build(&ctx, ""));
 
     // check that default-constructed unique string matches one from ""
     UniqueString empty;
-    assert(empty.c_str() == UniqueString::build(&ctx, "").c_str());
+    assert(empty == UniqueString::build(&ctx, ""));
   }
 
   // Next, measure performance
