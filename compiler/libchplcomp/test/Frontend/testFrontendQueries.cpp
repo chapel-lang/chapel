e9eb6b65ce15b509bcd9e379e3a92e92c2167596
Michael Ferguson <mppf@users.noreply.github.com>
2021-04-16 16:27:43 -0400

    Add more testing
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

--- compiler/libchplcomp/test/Frontend/testFrontendQueries.cpp
+++ compiler/libchplcomp/test/Frontend/testFrontendQueries.cpp
@@ -1,5 +1,7 @@
 #include "chpl/Frontend/FrontendQueries.h"
 
+#include "chpl/AST/ModuleDecl.h"
+
 // always check assertions in this test
 #ifdef NDEBUG
 #undef NDEBUG
@@ -30,7 +32,7 @@ static void test1() {
   std::string contents = "/* this is a test */";
   ctx->setFileText(path, contents);
  
-  auto* p = FrontendQueries::parse(ctx, path);
+  FrontendQueries::parse(ctx, path);
 }
 
 static void test2() {
@@ -46,9 +48,35 @@ static void test2() {
                                "a;\n";
   ctx->setFileText(modTwoPath, modTwoContents);
  
-  auto* p = FrontendQueries::parse(ctx, modOnePath);
-  //const ast::Builder::Result* parse(Context* context, UniqueString path);
+  const ast::Builder::Result* p;
+  p = FrontendQueries::parse(ctx, modOnePath);
+  assert(p->topLevelExprs.size() == 1);
+  assert(p->topLevelExprs[0]->isModuleDecl());
+  auto moduleOne = p->topLevelExprs[0]->toModuleDecl()->module();
+  assert(moduleOne->numStmts() == 2);
+  p = FrontendQueries::parse(ctx, modTwoPath);
+  assert(p->topLevelExprs.size() == 1);
+  assert(p->topLevelExprs[0]->isModuleDecl());
+  auto moduleTwo = p->topLevelExprs[0]->toModuleDecl()->module();
+  assert(moduleTwo->numStmts() == 2);
 
+  modOneContents = "/* this is a test */\n"
+                   "\n"
+                   "\n"
+                   "a;\n";
+  ctx->setFileText(modOnePath, modOneContents);
+  ctx->setFileText(modTwoPath, modTwoContents);
+
+  p = FrontendQueries::parse(ctx, modOnePath);
+  assert(p->topLevelExprs.size() == 1);
+  assert(p->topLevelExprs[0]->isModuleDecl());
+  moduleOne = p->topLevelExprs[0]->toModuleDecl()->module();
+  assert(moduleOne->numStmts() == 2);
+  p = FrontendQueries::parse(ctx, modTwoPath);
+  assert(p->topLevelExprs.size() == 1);
+  assert(p->topLevelExprs[0]->isModuleDecl());
+  moduleTwo = p->topLevelExprs[0]->toModuleDecl()->module();
+  assert(moduleTwo->numStmts() == 2);
 }
 
 
