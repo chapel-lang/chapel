a961b23386dc8c0c319500fec93d5bcbe9d77880
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2021-05-07 14:22:57 -0700

    Mods to visibleFunctions that don't solve the current problem but may solve other problems
    ----
    Signed-off-by: Lydia Duncan <lydia-duncan@users.noreply.github.com>

--- compiler/resolution/visibleFunctions.cpp
+++ compiler/resolution/visibleFunctions.cpp
@@ -439,7 +439,7 @@ static void getVisibleMethods(const char* name, CallExpr* call,
 }
 
 static void lookAtTypeFirstHelper(const char* name, CallExpr* call,
-                                  BlockStmt* block, AggregateType* curType,
+                                  BlockStmt* block, Type* curType,
                                   VisibilityInfo* visInfo,
                                   std::set<BlockStmt*>& visited,
                                   Vec<FnSymbol*>& visibleFns) {
@@ -452,9 +452,12 @@ static void lookAtTypeFirstHelper(const char* name, CallExpr* call,
   getVisibleMethodsImpl(name, call, typeScope, visInfo, visited, visibleFns,
                         inScopeJump);
 
-  // Follow inheritance, if necessary
-  forv_Vec(AggregateType, pt, curType->dispatchParents) {
-    lookAtTypeFirstHelper(name, call, block, pt, visInfo, visited, visibleFns);
+  if (AggregateType* at = toAggregateType(curType)) {
+    // Follow inheritance, if necessary
+    forv_Vec(AggregateType, pt, at->dispatchParents) {
+      lookAtTypeFirstHelper(name, call, block, pt, visInfo, visited,
+                            visibleFns);
+    }
   }
 }
 
@@ -473,20 +476,36 @@ static void lookAtTypeFirst(const char* name, CallExpr* call, BlockStmt* block,
   }
   Type* t = typeActual->getValType();
 
-  BlockStmt* typeScope = getVisibilityScope(t->symbol->defPoint);
-  // When searching the type scope, don't follow private uses and imports if we
-  // aren't already in the scope where the type is defined.
-  bool inScopeJump = typeScope != block;
+  lookAtTypeFirstHelper(name, call, block, t, visInfo, visited, visibleFns);
 
-  // Look at own methods
-  getVisibleMethodsImpl(name, call, typeScope, visInfo, visited, visibleFns,
-                        inScopeJump);
+  if (!call->isPrimitive()) {
+    UnresolvedSymExpr* base = toUnresolvedSymExpr(call->baseExpr);
+    INT_ASSERT(base);
+    if (isAstrOpName(base->unresolved)) {
+      if (call->get(1)->typeInfo() != dtMethodToken &&
+          call->numActuals() == 2) {
+        // It's not currently set up as a method call and it is a binary
+        // operator so we need to check the second argument as well
+        Type* t2 = call->get(2)->getValType();
+        lookAtTypeFirstHelper(name, call, block, t2, visInfo, visited,
+                              visibleFns);
 
-  if (AggregateType* at = toAggregateType(t)) {
-    // Follow inheritance, if necessary
-    forv_Vec(AggregateType, pt, at->dispatchParents) {
-      lookAtTypeFirstHelper(name, call, block, pt, visInfo, visited,
-                            visibleFns);
+      } else if (call->get(1)->typeInfo() == dtMethodToken &&
+                 call->numActuals() > 2) {
+        // It is a method call.  So we've only checked against the type we set
+        // the call up for, but there may be other operator methods defined
+        // directly on the arguments, so check them as well.
+        Type* t2 = call->get(3)->getValType();
+        lookAtTypeFirstHelper(name, call, block, t2, visInfo, visited,
+                              visibleFns);
+        if (call->numActuals() == 4) {
+          Type* t3 = call->get(2)->getValType();
+          lookAtTypeFirstHelper(name, call, block, t3, visInfo, visited,
+                                visibleFns);
+        }
+        // If we ever allow trinary or greater operators, we'd do something
+        // about that here
+      }
     }
   }
 }
@@ -616,7 +635,8 @@ static void getVisibleMethodsFromUseListFiltered(const char* name,
     if (UseStmt* use = toUseStmt(expr)) {
       if (!needToTraverseUse(firstVisit, inUseChain, use->isPrivate))
         continue;
-      if (call->numActuals() >= 2) {
+      if (call->numActuals() >= 2 &&
+          call->get(1)->typeInfo() == dtMethodToken) {
         Expr* thisArg = call->get(2);
         Type* thisType = thisArg->getValType();
         namedTypes = use->typeWasNamed(thisType);
@@ -626,6 +646,26 @@ static void getVisibleMethodsFromUseListFiltered(const char* name,
           // this use statement.
           continue;
         }
+      } else if (!call->isPrimitive()) {
+        UnresolvedSymExpr* base = toUnresolvedSymExpr(call->baseExpr);
+        INT_ASSERT(base);
+        if (isAstrOpName(base->unresolved)) {
+          // It's an operator
+          Expr* arg1 = call->get(1);
+          Type* arg1Type = arg1->getValType();
+          namedTypes = use->typeWasNamed(arg1Type);
+          if (call->numActuals() == 2) {
+            Expr* arg2 = call->get(2);
+            Type* arg2Type = arg2->getValType();
+            use->typeWasNamed(arg2Type, &namedTypes);
+          }
+          if (use->hasExceptList() && namedTypes.size() == 0) {
+            // The intersection of symbols allowed by the except list and this
+            // type's name or inherited names was empty.  We should not traverse
+            // this use statement.
+            continue;
+          }
+        }
       }
       if (use->skipSymbolSearch(name) && namedTypes.size() == 0)
         continue;
@@ -633,10 +673,25 @@ static void getVisibleMethodsFromUseListFiltered(const char* name,
     } else if (ImportStmt* import = toImportStmt(expr)) {
       if (!needToTraverseUse(firstVisit, inUseChain, import->isPrivate))
         continue;
-      if (call->numActuals() >= 2) {
+      if (call->numActuals() >= 2 &&
+          call->get(1)->typeInfo() == dtMethodToken) {
         Expr* thisArg = call->get(2);
         Type* thisType = thisArg->getValType();
         namedTypes = import->typeWasNamed(thisType);
+      } else if (!call->isPrimitive()) {
+        UnresolvedSymExpr* base = toUnresolvedSymExpr(call->baseExpr);
+        INT_ASSERT(base);
+        if (isAstrOpName(base->unresolved)) {
+          // It's an operator
+          Expr* arg1 = call->get(1);
+          Type* arg1Type = arg1->getValType();
+          namedTypes = import->typeWasNamed(arg1Type);
+          if (call->numActuals() == 2) {
+            Expr* arg2 = call->get(2);
+            Type* arg2Type = arg2->getValType();
+            import->typeWasNamed(arg2Type, &namedTypes);
+          }
+        }
       }
       if (import->skipSymbolSearch(name) && namedTypes.size() == 0)
         continue;
