31bfac6580ac88ee103db02c93e9a26fe12d2bf1
Brad Chamberlain <bradcray@users.noreply.github.com>
2021-05-11 15:16:27 -0700

    Put in a case to dodge to avoid '--verify' errors
    Something about promotion wrappers not having FLAG_MAYBE_PARAM causes
    --verify errors in _toFollower() and I don't think promotion wrappers
    will dynamically dispatch anyway w.r.t. default arguments, so let's
    just step around them.
    
    ---
    Signed-off-by: Brad Chamberlain <bradcray@users.noreply.github.com>

--- compiler/resolution/wrappers.cpp
+++ compiler/resolution/wrappers.cpp
@@ -689,8 +689,16 @@ static DefaultExprFnEntry buildDefaultedActualFn(FnSymbol*  fn,
     // as an example, see:
     //   test/functions/default-arguments/default-argument-class-override.chpl
     //
+    // Why do we also check for the FLAG_PROMOTION_WRAPPER?  Because
+    // when we didn't, tests like release/examples/primers/associative.chpl
+    // and reductions/bradc/manual/promote.chpl failed their '--verify'
+    // test runs for reasons that seemed puzzling and related to the
+    // intracacies of the _toFollower() code path; and it seemed unlikely
+    // that they would care about default arguments and inheritance.
+    //
     bool classDefaultArgCase = (fn->_this &&
-                                isClassLikeOrManaged(fn->_this->type));
+                                isClassLikeOrManaged(fn->_this->type) &&
+                                !fn->hasFlag(FLAG_PROMOTION_WRAPPER));
 
     if (!classDefaultArgCase) {
       temp->addFlag(FLAG_MAYBE_PARAM);
