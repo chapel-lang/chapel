6539f7e13e24903e652c74787452408b75ac19a8
Michael Ferguson <mppf@users.noreply.github.com>
2021-05-12 10:31:34 -0400

    Add TypeSym base class
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

---
+++ compiler/next/include/chpl/uast/TypeSym.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2021 Hewlett Packard Enterprise Development LP
+ * Other additional copyright holders may be indicated within.
+ *
+ * The entirety of this work is licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CHPL_UAST_TYPESYM_H
+#define CHPL_UAST_TYPESYM_H
+
+#include "chpl/uast/Sym.h"
+
+namespace chpl {
+namespace uast {
+
+
+/**
+  This is an abstract base class for Symbols defining a type
+  (e.g. classes, records, enums).
+ */
+class TypeSym : public Sym {
+ protected:
+  TypeSym(asttags::ASTTag tag, ASTList children,
+          UniqueString name, Visibility vis)
+    : Sym(tag, std::move(children), name, vis) {
+
+    assert(isExpressionASTList(children_));
+  }
+  bool typeSymContentsMatchInner(const TypeSym* other) const {
+    return symContentsMatchInner((const Sym*) other);
+  }
+  void typeSymMarkUniqueStringsInner(Context* context) const {
+    symMarkUniqueStringsInner(context); 
+  }
+
+ public:
+  virtual ~TypeSym() = 0; // this is an abstract base class
+};
+
+
+} // end namespace uast
+} // end namespace chpl
+
+#endif
