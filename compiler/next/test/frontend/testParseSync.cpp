37b8c5e6f41f0b65cdd248bb3422693968aa8223
arezaii <ahmad.rezaii@hpe.com>
2021-06-21 22:03:58 -0600

    add multi-begin test
    Signed-off-by: arezaii <ahmad.rezaii@hpe.com>

--- compiler/next/test/frontend/testParseSync.cpp
+++ compiler/next/test/frontend/testParseSync.cpp
@@ -124,6 +124,58 @@ static void test1(Parser* parser) {
   }
 }
 
+static void test2(Parser* parser) {
+  auto parseResult = parser->parseString("test1.chpl",
+      "/* comment 1 */\n"
+      "sync /* comment 2 */ {\n"
+      "  /* comment 3 */\n"
+      "  begin foo();\n"
+      "  begin bar();\n"
+      "  /* comment 4 */\n"
+      "}\n"
+      "/* comment 5 */\n");
+  assert(parseResult.errors.size() == 0);
+  assert(parseResult.topLevelExpressions.size() == 1);
+  assert(parseResult.topLevelExpressions[0]->isModule());
+  auto mod = parseResult.topLevelExpressions[0]->toModule();
+  assert(mod->numStmts() == 3);
+  assert(mod->stmt(0)->isComment());
+  assert(mod->stmt(1)->isSync());
+  assert(mod->stmt(2)->isComment());
+  const Sync* sync = mod->stmt(1)->toSync();
+  assert(sync);
+  assert(sync->blockStyle() == BlockStyle::EXPLICIT);
+  assert(sync->numStmts() == 4);
+  assert(sync->stmt(0)->isComment());
+  const Begin* begin = sync->stmt(1)->toBegin();
+  assert(begin);
+  assert(begin->numStmts() == 1);
+  assert(begin->blockStyle() == BlockStyle::IMPLICIT);
+  assert(begin->stmt(0)->isFnCall());
+  const Begin* beginBar = sync->stmt(2)->toBegin();
+  assert(beginBar);
+  assert(beginBar->numStmts() == 1);
+  assert(beginBar->blockStyle() == BlockStyle::IMPLICIT);
+  assert(beginBar->stmt(0)->isFnCall());
+  assert(sync->stmt(3)->isComment());
+
+  // Make sure the statements iterator works as expected.
+  {
+    std::array<ASTTag, 4> stmtList = {
+      asttags::Comment,
+      asttags::Begin,
+      asttags::Begin,
+      asttags::Comment
+    };
+    auto i = 0;
+    for (const auto stmt : sync->stmts()) {
+      assert(stmt->tag() == stmtList[i]);
+      assert(stmt->tag() == sync->stmt(i++)->tag());
+    }
+  }
+}
+
+
 int main() {
   Context context;
   Context* ctx = &context;
@@ -133,6 +185,6 @@ int main() {
 
   test0(p);
   test1(p);
-
+  test2(p);
   return 0;
 }
