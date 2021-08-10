53895f34793ac5df17c2df67a736d2f9d15ba71b
Andy Stone <stonea@users.noreply.github.com>
2021-08-10 13:34:47 -0500

    Revert failing changes for index computation in GPU kernel.
    ---
    Signed-off-by: Andy Stone <stonea@users.noreply.github.com>

--- compiler/optimizations/deadCodeElimination.cpp
+++ compiler/optimizations/deadCodeElimination.cpp
@@ -507,7 +507,7 @@ static  CallExpr* generateGPUCall(FnSymbol* kernel,
   return call;
 }
 
-static CallExpr* generateIndexComputation() {
+/*static CallExpr* generateIndexComputation() {
   // Generates Chapel AST corresponding to the following CUDA code:
   // blockIdx.x * blockDim.x + threadIdx.x
   CallExpr* call = new CallExpr(PRIM_ADD,
@@ -516,7 +516,7 @@ static CallExpr* generateIndexComputation() {
       new CallExpr(PRIM_GPU_BLOCKIDX_X)),
     new CallExpr(PRIM_GPU_THREADIDX_X));
   return call;
-}
+}*/
 
 static void outlineGPUKernels() {
   forv_Vec(FnSymbol*, fn, gFnSymbols) {
@@ -588,7 +588,7 @@ static void outlineGPUKernels() {
                       outlinedFunction->insertAtTail(new DefExpr(flatIndex));
                       outlinedFunction->insertAtTail(new CallExpr(PRIM_MOVE,
                                                                   flatIndex,
-                                                                  generateIndexComputation()));
+                                                                  new_IntSymbol(0)));
 
                       copyMap.put(sym, flatIndex);
                     }
