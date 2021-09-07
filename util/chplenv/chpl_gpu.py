14649582bf91c60985b1a1211244ea5bb6497e32
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2021-09-07 12:34:53 -0500

    Make CHPL_CUDA_PATH point to the root dir
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- util/chplenv/chpl_gpu.py
+++ util/chplenv/chpl_gpu.py
@@ -11,7 +11,7 @@ def get_cuda_path():
                                                                       "nvcc"])
 
     if exists and returncode == 0:
-        chpl_cuda_path = "/".join(my_stdout.strip().split("/")[:-2]) + "/lib64"
+        chpl_cuda_path = "/".join(my_stdout.strip().split("/")[:-2])
         return chpl_cuda_path
     else:
         return ""
