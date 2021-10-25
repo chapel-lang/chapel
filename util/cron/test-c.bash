3806e96c3a25939e95e7acb12e25ec841929b604
Joe Tursi <joe.p.tursi@gmail.com>
2021-10-25 03:05:14 -0400

    First draft of llvm to c testing conversion
    Signed-off-by: Joe Tursi <joe.p.tursi@gmail.com>

--- util/cron/test-c.bash
+++ util/cron/test-c.bash
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 #
-# Full test suite with CHPL_LLVM=bundled and CHPL_TARGET_COMPILER=llvm
+# Full test suite with CHPL_LLVM=none and CHPL_TARGET_COMPILER=gnu
 # on linux64. Now uses paratest.server.
 
 # Needs /data/cf/chapel/setup_python27.bash (common-llvm)
 
 CWD=$(cd $(dirname $0) ; pwd)
 source $CWD/common.bash
-source $CWD/common-llvm.bash
+source $CWD/common-c.bash
 source $CWD/common-localnode-paratest.bash
 
 # common-llvm restricts us to extern/ferguson, but we want all the tests
@@ -15,7 +15,7 @@ unset CHPL_NIGHTLY_TEST_DIRS
 
 nightly_args="${nightly_args} $(set +x ; get_nightly_paratest_args) -asserts"
 
-export CHPL_NIGHTLY_TEST_CONFIG_NAME="llvm"
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="c"
 
 log_info START nightly -cron ${nightly_args}
 $CWD/nightly -cron ${nightly_args}
--- util/cron/test-gasnet.c.bash
+++ util/cron/test-gasnet.c.bash
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 #
-# Test gasnet configuration with CHPL_LLVM=bundled & CHPL_TARGET_COMPILER=llvm
+# Test gasnet configuration with CHPL_LLVM=none & CHPL_TARGET_COMPILER=gnu
 # on linux64.
 
 CWD=$(cd $(dirname $0) ; pwd)
 source $CWD/common-gasnet.bash
-source $CWD/common-llvm.bash
+source $CWD/common-c.bash
 
-export CHPL_NIGHTLY_TEST_CONFIG_NAME="gasnet.llvm"
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="gasnet.c"
 
 # common-llvm sets this to a single test directory, but we want a wider test
 # distribution for this configuration.
