3c6f875e79f50a5bb3a7c18f91f63f8c637c34bb
Michael Ferguson <mppf@users.noreply.github.com>
2021-12-07 16:27:30 -0500

    Use 6 Xs in mktemp calls
    On Alpine Linux, make check does not work because the busybox
    mktemp -d call requires six Xs at the end. Seems like six Xs
    is reasonably good practice in any case.
    
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

--- util/cron/test-release.bash
+++ util/cron/test-release.bash
@@ -5,7 +5,7 @@ source $CWD/common.bash
 
 # Create a tmpdir in the current workspace.
 WORKSPACE=${WORKSPACE:-$CWD/../..}
-export TMPDIR=$(mktemp -d $WORKSPACE/chapel-test-release.XXXXX)
+export TMPDIR=$(mktemp -d $WORKSPACE/chapel-test-release.XXXXXX)
 export CHPL_GEN_RELEASE_TMPDIR=$TMPDIR
 
 $CWD/../buildRelease/testRelease -cron
