de3980c7bcd05cb39472c23e9a41fef840e1a443
Michael Ferguson <mppf@users.noreply.github.com>
2022-01-05 16:30:14 -0500

    Simplify chpl_gasnet.py with pkgconfig calls
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

--- util/chplenv/chpl_gasnet.py
+++ util/chplenv/chpl_gasnet.py
@@ -14,50 +14,26 @@ def get_uniq_cfg_path():
 
 
 @memoize
-def get_gasnet_pc_dict():
+def get_gasnet_pc_file():
     substrate = chpl_comm_substrate.get()
     ucp = get_uniq_cfg_path()
     install_path = third_party_utils.get_install_path('gasnet', ucp=ucp)
     pcname = "gasnet-{0}-par.pc".format(substrate)
     pcfile = os.path.join(install_path, 'lib', 'pkgconfig', pcname)
-    ret = { }
-
-    # assume we haven't built gasnet yet if the install dir doesn't exist
-    if os.path.exists(install_path):
-        return third_party_utils.read_pkg_config_file(
-                    pcfile,
-                    'third-party/gasnet/install/' + ucp,
-                    install_path)
-    else:
-        warning("gasnet path {0} missing".format(install_path))
-
-    return ret
+    return pcfile
 
 # returns 2-tuple of lists
 #  (compiler_bundled_args, compiler_system_args)
 @memoize
 def get_compile_args():
-    bundled = [ ]
-    system = [ ]
-
-    d = get_gasnet_pc_dict()
-
-    if 'Cflags' in d:
-        bundled.extend(d['Cflags'].split())
-
-    return (bundled, system)
-
+    return third_party_utils.pkgconfig_get_compile_args(
+                       'gasnet', ucp=get_uniq_cfg_path(),
+                        system=False, pcfile=get_gasnet_pc_file())
 
 # returns 2-tuple of lists
 #  (linker_bundled_args, linker_system_args)
 @memoize
 def get_link_args():
-    bundled = [ ]
-    system = [ ]
-
-    d = get_gasnet_pc_dict()
-
-    if 'Libs' in d:
-        bundled.extend(d['Libs'].split())
-
-    return (bundled, system)
+    return third_party_utils.pkgconfig_get_link_args(
+                       'gasnet', ucp=get_uniq_cfg_path(),
+                        system=False, pcfile=get_gasnet_pc_file())
