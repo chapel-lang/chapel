773ad67f9a47d92a53ce85e53296f71fae4893f0
Andrew Consroe <andrew.consroe@hpe.com>
2022-01-06 09:25:38 -0600

    Add missing fields to ErrorMessage::operator==
    Was missing details_ and id_
    
    Signed-off-by: Andrew Consroe <andrew.consroe@hpe.com>

--- compiler/next/include/chpl/queries/ErrorMessage.h
+++ compiler/next/include/chpl/queries/ErrorMessage.h
@@ -79,9 +79,11 @@ class ErrorMessage final {
   const std::vector<ErrorMessage>& details() const { return details_; }
 
   inline bool operator==(const ErrorMessage& other) const {
-    return this->level_ == other.level_ &&
-           this->location_ == other.location_ &&
-           this->message_ == other.message_;
+    return level_ == other.level_ &&
+           location_ == other.location_ &&
+           message_ == other.message_ &&
+           details_ == other.details_ &&
+           id_ == other.id_;
   }
   inline bool operator!=(const ErrorMessage& other) const {
     return !(*this == other);
