2bac5835c21b7d8a60c55e3068618b71148f4c57
Michael Ferguson <mppf@users.noreply.github.com>
2022-01-18 16:39:47 -0500

    Add notes about installing LLVM/clang
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

--- util/devel/test/vagrant/provision-scripts/apk-deps.sh
+++ util/devel/test/vagrant/provision-scripts/apk-deps.sh
@@ -2,3 +2,7 @@
 
 # install packages for Alpine Linux
 apk add gcc g++ m4 perl python3 python3-dev bash make gawk git cmake
+
+# and LLVM stuff
+apk add llvm11-dev clang-dev
+#clang-static clang-dev.
