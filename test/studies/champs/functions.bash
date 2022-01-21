d1e5446a9b55bfea3adfa493c11e2e6d9664f6cf
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2022-01-20 23:10:09 -0800

    Add CHAMPS nightly testing
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

---
+++ test/studies/champs/functions.bash
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+
+# Helper functions for logging and marking test start/stop. Formats output in
+# the way the testing infrastructure expects.
+
+function subtest_start() {
+  subtest_start_time=$(date +%s)
+  echo "[Starting subtest - $(date)]"
+}
+
+function subtest_end() {
+  local subtest_end_time=$(date +%s)
+  local elapsed=$(( $subtest_end_time - $subtest_start_time ))
+  echo "[Finished subtest \"studies/champs\" - $elapsed seconds"
+}
+
+function test_start() {
+  subtest_test_name=$1
+  echo "[test: \"$subtest_test_name\"]"
+  SECONDS=0
+}
+
+function test_end() {
+  elapsed=$SECONDS
+  echo "[Elapsed time to compile and execute all versions of \"$subtest_test_name\" - $elapsed.000 seconds]"
+  unset subtest_test_name
+}
+
+function log_error() {
+  local msg=$@
+  echo "[Error ${msg}]"
+}
+
+function log_fatal_error() {
+  local msg=$@
+  echo "[Error ${msg}]"
+  if [[ -n $subtest_test_name ]]; then
+    test_end
+  fi
+  subtest_end
+  exit 0
+}
+
+function log_success() {
+  local msg=$@
+  echo "[Success matching ${msg}]"
+}
+
+function apply_patch() {
+  local patch_file=$@
+  echo "Applying patch ${patch_file}"
+  if ! patch -p1 < $patch_file ; then
+    log_fatal_error "applying patch"
+  else
+    echo "applied patch successfully"
+  fi
+}
+
+function test_compile_exec() {
+  local kind=$@
+  make MOD=$kind 2> $kind.comp.out.tmp
+  local status=$?
+  cat $kind.comp.out.tmp
+
+  if [[ $status -ne 0 ]] ; then
+    log_fatal_error "compiling ${kind}"
+  fi
+
+  $CHPL_HOME/util/test/computePerfStats comp-time-$kind $CHPL_TEST_PERF_DIR $CHAMPS_GRAPH_PATH/comp-time.perfkeys $kind.comp.out.tmp
+  $CHPL_HOME/util/test/computePerfStats emitted-code-size-$kind $CHPL_TEST_PERF_DIR $CHAMPS_GRAPH_PATH/emitted-code-size.perfkeys $kind.comp.out.tmp
+}
+
+function test_run_exec() {
+  local kind=$1
+  local nl=$2
+
+  ./bin/champs_${CHAMPS_VERSION}_$kind -nl $nl -f $CHAMPS_CFG_PATH/$kind.in 2>&1 >$kind.exec.out.tmp
+
+  local status=$?
+  cat $kind.exec.out.tmp
+
+  if [[ $status -ne 0 ]] ; then
+    log_fatal_error "running ${kind}"
+  fi
+
+  # execute prediff if it exists
+  if [ -f $CHAMPS_GRAPH_PATH/$kind.prediff ] ; then
+    $CHAMPS_GRAPH_PATH/$kind.prediff dummy $kind.exec.out.tmp
+  fi
+
+  $CHPL_HOME/util/test/computePerfStats exec-time-$kind $CHPL_TEST_PERF_DIR $CHAMPS_GRAPH_PATH/$kind.perfkeys $kind.exec.out.tmp
+}
+
---
+++ util/cron/common-champs.bash
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+#
+# Configure environment for CHAMPS testing
+
+CWD=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
+
+COMMON_DIR=/cray/css/users/chapelu
+if [ ! -d "$COMMON_DIR" ]; then
+  COMMON_DIR=/cy/users/chapelu
+fi
+
+# All CHAMPS testing is currently on a cray-cs
+module list
+
+source $CWD/common-cray-cs.bash
+export CHPL_LAUNCHER_PARTITION=clx24
+export CHPL_TARGET_CPU=none
+export CHPL_COMM=gasnet
+
+loadCSModule intel
+loadCSModule cray-mvapich2_nogpu
+
+module list
+
+export CPATH=$CPATH:$MPIROOT/include
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HDF5ROOT/lib
+
+# Perf configuration
+source $CWD/common-perf.bash
+CHAMPS_PERF_DIR=${CHAMPS_PERF_DIR:-$COMMON_DIR/NightlyPerformance/champs} # TODO
+export CHPL_TEST_PERF_DIR=$CHAMPS_PERF_DIR/$CHPL_TEST_PERF_CONFIG_NAME
+export CHPL_TEST_NUM_TRIALS=3
+export CHPL_TEST_PERF_START_DATE=01/21/22
+
+# Run champs correctness and performance testing
+export CHPL_NIGHTLY_TEST_DIRS=studies/champs/
+export CHPL_TEST_CHAMPS=true
+
+CHAMPS_DEP_DIR=$COMMON_DIR/champs-nightly/deps-manual
+if [ -d "$CHAMPS_DEP_DIR" ]; then
+  export HDF5ROOT=${HDF5ROOT:-$CHAMPS_DEP_DIR}
+  export METISROOT=${METISROOT:-$CHAMPS_DEP_DIR}
+  export CGNSROOT=${CGNSROOT:-$CHAMPS_DEP_DIR}
+fi
+
+
+# these may be unnecessary
+export GASNET_PHYSMEM_MAX=124G
+export GASNET_IBV_PORTS=mlx5_1
+export GASNET_ODP_VERBOSE=0
+
+export CHPL_TEST_PERF_CONFIGS="llvm:v,c"  # v: visible by def
+
+function sync_graphs() {
+  $CHPL_HOME/util/cron/syncPerfGraphs.py $CHPL_TEST_PERF_DIR/html/ champs/$CHPL_TEST_PERF_CONFIG_NAME
+}
