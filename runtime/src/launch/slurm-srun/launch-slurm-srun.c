3cb8be5c4160af452fca6b70b0acfe4f565cc01d
Greg Titus <gbtitus@users.noreply.github.com>
2022-03-08 11:27:45 -0600

    Remove 2 trailing spaces from the Slurm srun system launch command.
    Signed-off-by: Greg Titus <gbtitus@users.noreply.github.com>

--- runtime/src/launch/slurm-srun/launch-slurm-srun.c
+++ runtime/src/launch/slurm-srun/launch-slurm-srun.c
@@ -430,16 +430,16 @@ static char* chpl_launch_create_command(int argc, char* argv[],
     }
 
     // add the (possibly wrapped) binary name
-    len += sprintf(iCom+len, "%s %s ",
+    len += sprintf(iCom+len, "%s %s",
         chpl_get_real_binary_wrapper(), chpl_get_real_binary_name());
 
     // add any arguments passed to the launcher to the binary
     for (i=1; i<argc; i++) {
-      len += sprintf(iCom+len, "%s ", argv[i]);
+      len += sprintf(iCom+len, " %s", argv[i]);
     }
 
     // launch the job using srun
-    sprintf(baseCommand, "srun %s ", iCom);
+    sprintf(baseCommand, "srun %s", iCom);
   }
 
   // copy baseCommand into command and return it
