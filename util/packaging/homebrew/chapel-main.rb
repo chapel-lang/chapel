f4a328a3a3eca0a2c882547a451bf7d4af8db7b6
Tim Zinsky <tim.zinsky@hpe.com>
2022-03-31 16:28:43 -0500

    Updated chapel-main formula to match released build
    This is the 1.26 homebrew formula
    
    Signed-off-by: Tim Zinsky <tim.zinsky@hpe.com>

--- util/packaging/homebrew/chapel-main.rb
+++ util/packaging/homebrew/chapel-main.rb
@@ -1,8 +1,8 @@
 class Chapel < Formula
   desc "Programming language for productive parallel computing at scale"
   homepage "https://chapel-lang.org/"
-  url "https://chapel-lang.org/tmp/chapel-1.26.0.tar.gz"
-  sha256 "77b0aebdb948507314a91bdf2c5969ec2cc7a1b0e4be93df7cfb3c3d8caa0d4a"
+  url "https://github.com/chapel-lang/chapel/releases/download/1.26/chapel-1.26.0.tar.gz"
+  sha256 "ba396b581f0a17f8da3f365a3f8b079b8d2e229a393fbd1756966b0019931ece"
 #  url "<URL of Chapel tarball>"
 #  sha256 "<sha256 sum for the associated tarball>"
   license "Apache-2.0"
