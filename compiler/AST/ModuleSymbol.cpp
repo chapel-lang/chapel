7d5ee7f81fcc47633932f44408a74d4975108100
Brad Chamberlain <bradcray@users.noreply.github.com>
2022-04-08 11:56:23 -0700

    Improve detection of internal modules
    Update our Makefile to pass a path relative to CHPL_HOME to the
    internal modules, and then use logic within the compiler to
    note modules that are in the internal module path and flag
    them as internal modules for downstream aspects of chpldoc.
    
    ---
    Signed-off-by: Brad Chamberlain <bradcray@users.noreply.github.com>

--- compiler/AST/ModuleSymbol.cpp
+++ compiler/AST/ModuleSymbol.cpp
@@ -353,7 +353,7 @@ void ModuleSymbol::printDocs(std::ostream* file,
     // effect of making references to the :mod: tag for the module
     // illegal, which is appropriate since the modules are not
     // user-facing.
-    if (strncmp(this->filename, "internal/", 9) == 0) {
+    if (this->modTag == MOD_INTERNAL) {
       *file << "   :noindex:" << std::endl;
     } else if (this->doc != NULL) {
       this->printTabs(file, tabs + 1);
