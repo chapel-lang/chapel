4550c37b38da630c473d90b3d0d9e0b63409942d
David Longnecker <dlongnecke-cray@users.noreply.github.com>
2022-04-12 09:34:38 -0700

    Tweak wording of comment in file for YYLOC_DEFAULT override
    Signed-off-by: David Longnecker <dlongnecke-cray@users.noreply.github.com>

--- compiler/dyno/lib/parsing/parser-yylloc-default.h
+++ compiler/dyno/lib/parsing/parser-yylloc-default.h
@@ -33,12 +33,12 @@
       = YYRHSLOC(rhs__, 0).last_column; \
   ```
 
-  The else branch defines what occurs for a zero application reduction (as
+  The else branch defines what occurs for a zero component reduction (as
   far as I can tell this only occurs for null/empty rules). The previous
-  behavior would set the location of the entire application to be the
-  last line/column of the symbol right before the application occurred.
-  This has a strange effect on line positions. Given the following Chapel
-  code:
+  behavior would set the location of the entire reduction to be the
+  line/column of the last consumed input token. Since a null reduction
+  consumes no input, it is possible for the last consumed token to be
+  one used in a previous reduction.
 
   ```
   module Foo {    // Line 1
@@ -51,7 +51,7 @@
   The effect is for the productions in front of 'module Bar' (which are
   both null rules for the access specifier and 'prototype' keyword) to
   have the location (3:2), which is the location of the newline right
-  before the reduction for the entire module rule occurs.
+  before the reduction for the entire 'Bar' module rule occurs.
 
   If you look at how locations are computed in the IF branch, that means
   that the '@$' for the entire 'module Bar {' ends up being (3:2-...),
@@ -59,27 +59,22 @@
   basically an off-by-one error for line numbers across the entire program
   (or at least, a program using any `opt_` productions with null rules).
 
-  This wasn't a problem/didn't manifest itself much in the old AST world
-  due to how...mutable...everything was. But in the UAST land where we
-  have to carry components around until we're done reducing, and then
-  finally build our result UAST node, the line numbers being off becomes
-  very apparent (in the form of test errors).
+  This problem didn't manifest itself much in the old AST world due to
+  how...mutable...everything was. In the uAST land we have to carry
+  components around until we're done reducing, and then finally build
+  the uAST node. So line numbers are off quite often.
 
-  What I've done to fix this is: for zero application reductions, set the
-  location of the entire reduction to be the location of the currrent
+  What's been done to fix this is: for zero application reductions, set
+  the location of the entire reduction to be the location of the currrent
   lookahead token. In essence, optimistically look 1 forward instead of
-  1 back (assume that we will consume some amount of input - which have
-  to, because eventually we'll do a reduction over 1+ components).
+  1 back (assume that we will consume some non-zero amount of input in a
+  future reduction).
 
-  Caveats:
-
-    - I'm not sure if it's legal to access yylloc within this macro, but
-      it seems to compile, so...
-    - I am not sure if it's valid to use the lookahead token value when
-      in an error handling state, though the documentation does say that
-      when collapsing erroneous reductions the IF branch should be taken.
-      That is to say - what happens for error states needs to be tested...
-    - TODO: Might have to use 'yyclearin'.
+  TODO (dlongnecke): I am not sure if it's valid to use the lookahead
+  token value when in an error handling state, though the documentation
+  does say that when collapsing erroneous reductions the IF branch
+  should be taken. That is to say - what happens for error states
+  needs to be tested. Might have to use 'yyclearin'.
 */
 
 #define CHPL_YYLLOC_DEFAULT(cur__, rhs__, n__) \
