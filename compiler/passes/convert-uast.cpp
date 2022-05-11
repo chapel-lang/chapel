e60dc8027900bcc59c9aeb21439d90eeb8f8b589
arezaii <ahmad.rezaii@hpe.com>
2022-05-11 15:42:41 -0600

    dyno: don't convert _ in As statements
    Signed-off-by: arezaii <ahmad.rezaii@hpe.com>

--- compiler/passes/convert-uast.cpp
+++ compiler/passes/convert-uast.cpp
@@ -642,7 +642,13 @@ struct Converter {
     astlocMarker markAstLoc(node->id());
 
     Expr* one = toExpr(convertAST(node->symbol()));
-    Expr* two = toExpr(convertAST(node->rename()));
+    Expr* two = nullptr;
+    // if this rename is to '_', don't convert it to chpl__tuple_blank!
+    if (node->rename()->toIdentifier()->name()==USTR("_"))
+      two = new UnresolvedSymExpr("_");
+    else
+      two = toExpr(convertAST(node->rename()));
+    assert(two);
     return std::pair<Expr*, Expr*>(one, two);
   }
 
