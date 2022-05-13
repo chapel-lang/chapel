d27b31c4d183ec265758bca98d887b663552379d
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2022-05-12 17:09:39 -0700

    Remove an outdated comment
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- runtime/src/chpl-gpu.c
+++ runtime/src/chpl-gpu.c
@@ -204,8 +204,6 @@ static void chpl_gpu_launch_kernel_help(int ln,
       // TODO this allocation needs to use `chpl_mem_alloc` with a proper desc
       kernel_params[i] = chpl_malloc(1*sizeof(CUdeviceptr));
 
-      // TODO pass the location info to this function and use a proper mem
-      // desc
       *kernel_params[i] = chpl_gpu_mem_alloc(cur_arg_size,
                                              CHPL_RT_MD_GPU_KERNEL_ARG,
                                              ln, fn);
