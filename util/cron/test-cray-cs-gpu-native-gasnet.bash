acd65945ddf950aa02ef0ff87784f555c0d36f80
Andy Stone <stonea@users.noreply.github.com>
2022-05-26 10:40:24 -0700

    Ensure we use LLVM 13 for gpu gasnet tests
    ---
    Signed-off-by: Andy Stone <stonea@users.noreply.github.com>

--- util/cron/test-cray-cs-gpu-native-gasnet.bash
+++ util/cron/test-cray-cs-gpu-native-gasnet.bash
@@ -3,9 +3,7 @@
 # GPU native testing on a Cray CS (using gasnet for CHPL_COMM)
 
 CWD=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
-source $CWD/common-slurm-gasnet-cray-cs.bash
 source $CWD/common-native-gpu.bash
 
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="cray-cs-gpu-native-gasnet"
-
 $CWD/nightly -cron ${nightly_args}
--- util/cron/test-cray-cs-gpu-native.bash
+++ util/cron/test-cray-cs-gpu-native.bash
@@ -2,16 +2,9 @@
 #
 # GPU native testing on a Cray CS (using none for CHPL_COMM)
 
-# Use LLVM-13 to work around https://github.com/chapel-lang/chapel/issues/19740
-source /cray/css/users/chapelu/setup_system_llvm.bash 13
-# Lie to prevent common.bash adding a newer llvm
-export OFFICIAL_SYSTEM_LLVM=true
-
 CWD=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
-source $CWD/common-slurm-gasnet-cray-cs.bash
 source $CWD/common-native-gpu.bash
-
 export CHPL_COMM=none
-export CHPL_NIGHTLY_TEST_CONFIG_NAME="cray-cs-gpu-native"
 
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="cray-cs-gpu-native-gasnet"
 $CWD/nightly -cron ${nightly_args}
