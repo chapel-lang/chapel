c1038b3f03e17fdcb825cd790bae6a404b573c66
Andy Stone <stonea@users.noreply.github.com>
2022-05-26 10:49:41 -0700

    Fix config name
    ---
    Signed-off-by: Andy Stone <stonea@users.noreply.github.com>
    Signed-off-by: Brandon Neth <nethbrandone@gmail.com>

--- util/cron/test-cray-cs-gpu-native.bash
+++ util/cron/test-cray-cs-gpu-native.bash
@@ -6,5 +6,5 @@ CWD=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
 source $CWD/common-native-gpu.bash
 export CHPL_COMM=none
 
-export CHPL_NIGHTLY_TEST_CONFIG_NAME="cray-cs-gpu-native-gasnet"
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="cray-cs-gpu-native"
 $CWD/nightly -cron ${nightly_args}
