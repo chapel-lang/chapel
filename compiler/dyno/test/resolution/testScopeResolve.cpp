83cf65b8bfeb8d9231233c6a96399828199e7c67
Michael Ferguson <mppf@users.noreply.github.com>
2022-06-24 10:57:17 -0400

    Improve testScopeResolve
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

--- compiler/dyno/test/resolution/testScopeResolve.cpp
+++ compiler/dyno/test/resolution/testScopeResolve.cpp
@@ -37,12 +37,73 @@ using namespace parsing;
 using namespace resolution;
 using namespace uast;
 
+// helper functions
+/*
+static const Module* findModule(const AstNode* ast, const char* name) {
+  if (auto v = ast->toModule()) {
+    if (v->name() == name) {
+      return v;
+    }
+  }
+
+  for (auto child : ast->children()) {
+    auto got = findModule(child, name);
+    if (got) return got;
+  }
+
+  return nullptr;
+}
+
+static const Module* findModule(const ModuleVec& vec, const char* name) {
+  for (auto mod : vec) {
+    auto got = findModule(mod, name);
+    if (got) return got;
+  }
+
+  return nullptr;
+}
+*/
+
+static const Variable* findVariable(const AstNode* ast, const char* name) {
+  if (auto v = ast->toVariable()) {
+    if (v->name() == name) {
+      return v;
+    }
+  }
+
+  for (auto child : ast->children()) {
+    auto got = findVariable(child, name);
+    if (got) return got;
+  }
+
+  return nullptr;
+}
+
+static const Variable* findVariable(const ModuleVec& vec, const char* name) {
+  for (auto mod : vec) {
+    auto got = findVariable(mod, name);
+    if (got) return got;
+  }
+
+  return nullptr;
+}
+
+static const ResolvedExpression&
+scopeResolveIt(Context* context, const AstNode* ast) {
+  ID mId = idToParentModule(context, ast->id());
+  assert(!mId.isEmpty());
+  // if scopeResolveModule ever goes away, this can safely
+  // be replaced by resolveModule.
+  const ResolutionResultByPostorderID& rr = scopeResolveModule(context, mId);
+  return rr.byAst(ast);
+}
+
 static void test1() {
   printf("test1\n");
   Context ctx;
   Context* context = &ctx;
 
-  auto path = UniqueString::get(context, "test1.chpl");
+  auto path = UniqueString::get(context, "input.chpl");
   std::string contents = R""""(
       module M {
         var y: int;
@@ -58,35 +119,141 @@ static void test1() {
   setFileText(context, path, contents);
 
   const ModuleVec& vec = parse(context, path);
-  assert(vec.size() == 2);
-  const Module* m = vec[0]->toModule();
-  assert(m);
-  assert(m->numStmts() == 1);
-  const Variable* y = m->stmt(0)->toVariable();
-  assert(y);
-  const Module* n = vec[1]->toModule();
-  assert(n);
-  assert(n->numStmts() == 1);
-  const Block* nBlock = n->stmt(0)->toBlock();
-  assert(nBlock);
-  assert(nBlock->numStmts() == 2);
-  const Variable* x = nBlock->stmt(1)->toVariable();
+  const Variable* x = findVariable(vec, "x");
   assert(x);
-  const AstNode* xInit = x->initExpression();
-  assert(xInit);
+  const Variable* y = findVariable(vec, "y");
+  assert(y);
 
-  // if scopeResolveModule ever goes away, this can safely
-  // be replaced by resolveModule.
-  const ResolutionResultByPostorderID& rr =
-    scopeResolveModule(context, n->id());
-  const ResolvedExpression& re = rr.byAst(xInit);
-
+  const ResolvedExpression& re = scopeResolveIt(context, x->initExpression());
   assert(re.toId() == y->id());
 }
 
+static void test2() {
+  printf("test2\n");
+  Context ctx;
+  Context* context = &ctx;
+
+  auto path = UniqueString::get(context, "input.chpl");
+  std::string contents = R""""(
+      module M {
+        var y: int;
+      }
+
+      module N {
+        {
+          import M.y;
+          var x = y;
+        }
+      }
+   )"""";
+  setFileText(context, path, contents);
+
+  const ModuleVec& vec = parse(context, path);
+  const Variable* x = findVariable(vec, "x");
+  assert(x);
+  const Variable* y = findVariable(vec, "y");
+  assert(y);
+
+  const ResolvedExpression& re = scopeResolveIt(context, x->initExpression());
+  assert(re.toId() == y->id());
+}
+
+static void test3() {
+  printf("test3\n");
+  Context ctx;
+  Context* context = &ctx;
+
+  auto path = UniqueString::get(context, "input.chpl");
+  std::string contents = R""""(
+      module M {
+        var y: int;
+      }
+
+      module N {
+        {
+          import M.{y};
+          var x = y;
+        }
+      }
+   )"""";
+  setFileText(context, path, contents);
+
+  const ModuleVec& vec = parse(context, path);
+  const Variable* x = findVariable(vec, "x");
+  assert(x);
+  const Variable* y = findVariable(vec, "y");
+  assert(y);
+
+  const ResolvedExpression& re = scopeResolveIt(context, x->initExpression());
+  assert(re.toId() == y->id());
+}
+
+static void test4() {
+  printf("test4\n");
+  Context ctx;
+  Context* context = &ctx;
+
+  auto path = UniqueString::get(context, "input.chpl");
+  std::string contents = R""""(
+      module M {
+        var y: int;
+      }
+
+      module N {
+        {
+          use M;
+          var x = y;
+        }
+      }
+   )"""";
+  setFileText(context, path, contents);
+
+  const ModuleVec& vec = parse(context, path);
+  const Variable* x = findVariable(vec, "x");
+  assert(x);
+  const Variable* y = findVariable(vec, "y");
+  assert(y);
+
+  const ResolvedExpression& re = scopeResolveIt(context, x->initExpression());
+  assert(re.toId() == y->id());
+}
+
+static void test5() {
+  printf("test5\n");
+  Context ctx;
+  Context* context = &ctx;
+
+  auto path = UniqueString::get(context, "input.chpl");
+  std::string contents = R""""(
+      module M {
+        var y: int;
+      }
+
+      module N {
+        {
+          use M;
+          var x = M.y;
+        }
+      }
+   )"""";
+  setFileText(context, path, contents);
+
+  const ModuleVec& vec = parse(context, path);
+  const Variable* x = findVariable(vec, "x");
+  assert(x);
+  const Variable* y = findVariable(vec, "y");
+  assert(y);
+
+  const ResolvedExpression& re = scopeResolveIt(context, x->initExpression());
+  assert(re.toId() == y->id());
+}
 
 int main() {
   test1();
+  test2();
+  test3();
+  test4();
+  test5();
 
   return 0;
 }
