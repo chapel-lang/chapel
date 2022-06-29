f8dfa3325f21e4f59ec8a30b077037678e908062
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2022-06-29 14:35:55 -0700

    Cleanup chplmath header, remove sunos headers
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- runtime/include/chplmath.h
+++ runtime/include/chplmath.h
@@ -25,14 +25,6 @@
 extern "C" {
 #endif
 
-static inline float chpl_macro_INFINITY(void) {
-  return INFINITY;
-}
-
-static inline float chpl_macro_NAN(void) {
-  return NAN;
-}
-
 MAYBE_GPU static inline int chpl_macro_double_isinf(double x) { return isinf(x); }
 MAYBE_GPU static inline int chpl_macro_float_isinf(float x) { return isinf(x); }
 MAYBE_GPU static inline int chpl_macro_double_isfinite(double x) { return isfinite(x); }
@@ -59,42 +51,6 @@ MAYBE_GPU static inline float chpl_float_jn(int n, float x) { return (float)jn(n
 MAYBE_GPU static inline float chpl_float_y0(float x) { return (float)y0(x); }
 MAYBE_GPU static inline float chpl_float_y1(float x) { return (float)y1(x); }
 MAYBE_GPU static inline float chpl_float_yn(int n, float x) { return (float)yn(n, x); }
-#endif
-
-#ifdef DEFINE_32_BIT_MATH_FNS
-#define fabsf(x) (float)fabs(x)
-#define cosf(x) (float)cos(x)
-#define erff(x) (float)erf(x)
-#define expf(x) (float)exp(x)
-#define logf(x) (float)log(x)
-#define sinf(x) (float)sin(x)
-#define tanf(x) (float)tan(x)
-#define acosf(x) (float)acos(x)
-#define asinf(x) (float)asin(x)
-#define atanf(x) (float)atan(x)
-#define cbrtf(x) (float)cbrt(x)
-#define ceilf(x) (float)ceil(x)
-#define coshf(x) (float)cosh(x)
-#define erfcf(x) (float)erfc(x)
-#define exp2f(x) (float)exp2(x)
-#define log2f(x) (float)log2(x)
-#define rintf(x) (float)rint(x)
-#define sinhf(x) (float)sinh(x)
-#define tanhf(x) (float)tanh(x)
-#define sqrtf(x) (float)sqrt(x)
-#define lgammaf(x) (float)lgamma(x)
-#define acoshf(x) (float)acosh(x)
-#define asinhf(x) (float)asinh(x)
-#define atan2f(x,y) (float)atan2(x,y)
-#define atanhf(x) (float)atanh(x)
-#define expm1f(x) (float)expm1(x)
-#define floorf(x) (float)floor(x)
-#define log10f(x) (float)log10(x)
-#define log1pf(x) (float)log1p(x)
-#define roundf(x) (float)round(x)
-#define truncf(x) (float)trunc(x)
-#define nearbyintf(x) (float)nearbyint(x)
-#define tgammaf(x) (float)tgamma(x)
 #endif
 
 #ifdef __cplusplus
