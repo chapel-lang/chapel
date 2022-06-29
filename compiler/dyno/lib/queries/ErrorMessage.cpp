1d85c175ae160cc6b503b224c521eb9208fae24c
Michael Ferguson <mppf@users.noreply.github.com>
2022-06-29 11:34:08 -0400

    Fix bug in vprint_to_string
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

--- compiler/dyno/lib/queries/ErrorMessage.cpp
+++ compiler/dyno/lib/queries/ErrorMessage.cpp
@@ -30,13 +30,31 @@ namespace chpl {
 
 
 static std::string vprint_to_string(const char* format, va_list vl) {
+  // using an argument list after va_end is undefined
+  // so in other words, can't use 'vl' more than once,
+  // so make a copy for the 1st call.
+  va_list vlCopy;
+  va_copy(vlCopy, vl);
+
   int size = 128;
   char* buf = (char*) malloc(size);
-  int got = vsnprintf(buf, size, format, vl);
+  // this call destructively consumes 'vlCopy'
+  int got = vsnprintf(buf, size, format, vlCopy);
+  // each va_copy should be matched to a va_end
+  va_end(vlCopy);
+
+  // 'got' is number of characters to write not including terminating null byte
   if (got >= size) {
     // output was truncated, so try again
-    size = got+1;
-    buf = (char*) realloc(buf, size);
+    size = got+1; // include '\0' terminator
+    char* newbuf = (char*) realloc(buf, size);
+    assert(newbuf != nullptr);
+    if (newbuf == nullptr) {
+      free(buf);
+      return "<internal error in saving error>";
+    }
+    buf = newbuf;
+    // this call destructively consumes 'vl'
     got = vsnprintf(buf, size, format, vl);
     assert(got < size);
     if (got >= size) {
