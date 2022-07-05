289d69c37b34e06500a675d2c2151206f077dcdc
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2022-07-05 11:15:40 -0700

    Get the dynamic loaded version to work
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- runtime/src/gpu/cuda/gpu-cuda-target.cc
+++ runtime/src/gpu/cuda/gpu-cuda-target.cc
@@ -1,14 +1,32 @@
 #include "chplrt.h"
 #include "gpu-cuda-target.h"
+#include "dlfcn.h"
 
 /*#ifdef __cplusplus*/
 #include "device.h"
+#include "omptargetplugin.h"
 /*#endif*/
+
+//void initialize() {
+  //PM->RTLs.initAllRTLs();
+  //DeviceTy &d = *PM->Devices[0];
+  //printf("isinit: %d\n", d.IsInit);
+//}
 
 extern "C" {
 
 void chpl_gpu_test() {
+  //initialize();
+
+  void *rtl_handle = dlopen("libomptarget.rtl.cuda.so", RTLD_NOW);
+  assert(rtl_handle);
+  int32_t (*num_devices)(void) = (int32_t(*)())dlsym(rtl_handle, "__tgt_rtl_number_of_devices");
+  assert(num_devices);
+
+
+
   printf("In gpu tsest\n");
+  printf("%d\n", num_devices());
 }
 
 
