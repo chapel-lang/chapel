2d605d4172d46a915b3732f3ae5a21c38a62909a
Michael Ferguson <mppf@users.noreply.github.com>
2022-07-12 16:29:04 -0400

    Error on build_tuple with 0 args
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

--- compiler/AST/build.cpp
+++ compiler/AST/build.cpp
@@ -935,7 +935,7 @@ BlockStmt* buildSerialStmt(Expr* cond, BlockStmt* body) {
 void
 checkIndices(BaseAST* indices) {
   if (CallExpr* call = toCallExpr(indices)) {
-    if (!call->isNamed("_build_tuple"))
+    if (!call->isNamed("_build_tuple") || call->numActuals() == 0)
       USR_FATAL(indices, "invalid index expression");
     for_actuals(actual, call)
       checkIndices(actual);
