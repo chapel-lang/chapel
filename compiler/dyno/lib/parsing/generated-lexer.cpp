d8031bc975a48050c897002af08f3add1a97d509
Michael Ferguson <mppf@users.noreply.github.com>
2022-07-13 10:00:18 -0400

    Use a generated-lexer.cpp to do a #define
    To address problems building on cygwin
    
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

---
+++ compiler/dyno/lib/parsing/generated-lexer.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2020-2022 Hewlett Packard Enterprise Development LP
+ * Copyright 2004-2019 Cray Inc.
+ * Other additional copyright holders may be indicated within.
+ *
+ * The entirety of this work is licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// This file exists to do some #defines to address portability
+// issues before compiling the generated lexer
+
+// make sure to get the fileno function since the generated lexer uses it
+#ifndef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200809L
+#endif
+
+#include "flex-chpl-lib.cpp"
