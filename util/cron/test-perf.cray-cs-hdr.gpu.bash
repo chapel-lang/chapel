3c49c7f8f105bf691abfb648e0f3d5f5a35eee45
Andy Stone <stonea@users.noreply.github.com>
2022-07-18 15:45:49 -0700

    Address reviewer feedback
    ---
    Signed-off-by: Andy Stone <stonea@users.noreply.github.com>

--- util/cron/test-perf.cray-cs-hdr.gpu.bash
+++ util/cron/test-perf.cray-cs-hdr.gpu.bash
@@ -10,4 +10,4 @@ export CHPL_TEST_PERF_CONFIG_NAME='gpu'
 source $CWD/common-perf.bash
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.cray-cs-hdr.gpu"
 
-$CWD/nightly -cron -performance -releasePerformance -numtrials 5 -startdate 07/15/22
+$CWD/nightly -cron -performance -numtrials 5 -startdate 07/15/22
