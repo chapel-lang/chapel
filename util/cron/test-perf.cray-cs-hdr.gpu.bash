4e541e538f033077c8454826e8943c21b829d079
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2022-07-18 16:15:13 -0700

    Add GRAPHFILES and teach GPU test script where to find it
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- util/cron/test-perf.cray-cs-hdr.gpu.bash
+++ util/cron/test-perf.cray-cs-hdr.gpu.bash
@@ -9,5 +9,6 @@ export CHPL_COMM=none
 export CHPL_TEST_PERF_CONFIG_NAME='gpu'
 source $CWD/common-perf.bash
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.cray-cs-hdr.gpu"
+export CHPL_START_TEST_ARGS="-test-root $CHPL_NIGHTLY_TEST_DIRS"
 
 $CWD/nightly -cron -performance -numtrials 5 -startdate 07/15/22
