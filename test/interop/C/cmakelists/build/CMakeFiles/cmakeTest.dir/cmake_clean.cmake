7f8ccfefa59f30f44e0e2700472b30aeee0411ba
Brandon Neth <brandon.neth@hpe.com>
2022-07-24 10:42:10 -0700

    "start of a test for the cmake system. I need a littel bit more of the support before i can continue the the test I think."
    Signed-off-by: Brandon Neth <brandon.neth@hpe.com>

---
+++ test/interop/C/cmakelists/build/CMakeFiles/cmakeTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmakeTest.dir/cmakeTest.c.o"
+  "CMakeFiles/cmakeTest.dir/cmakeTest.c.o.d"
+  "cmakeTest"
+  "cmakeTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmakeTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
