4ca80795f8f2d00f0c2a68ee0c489f39c6600aa3
Michael Ferguson <mppf@users.noreply.github.com>
2022-09-03 10:52:40 -0400

    Fix comment
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

--- util/devel/test/vagrant/provision-scripts/apk-llvm.sh
+++ util/devel/test/vagrant/provision-scripts/apk-llvm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # and LLVM stuff
-# llvm 11 is the newest available on 314
-apk add llvm11-dev clang-dev
+# llvm 12 is the newest available on 3.15
+apk add llvm-dev clang-dev
 #clang-static clang-dev.
