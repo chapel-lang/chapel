1072fff933db4679a233d250ffcabb3494462dbb
Michael Ferguson <mppf@users.noreply.github.com>
2022-09-03 10:52:48 -0400

    Remove unnecessary parts from pacman-llvm
    so docs come out nicer
    
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>
    Signed-off-by: bhavanijayakumaran <82669529+bhavanijayakumaran@users.noreply.github.com>

--- util/devel/test/vagrant/provision-scripts/pacman-llvm.sh
+++ util/devel/test/vagrant/provision-scripts/pacman-llvm.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
-# Note: you can search arch packages with
-#   pacman -Ss search-term
-# You can update the system and packages list with
-#  pacman -Syu
-
-# update to avoid 404 errors
-pacman --noconfirm -Syu
-
 # and LLVM stuff, this installs LLVM 13 as of 202205 version
 pacman --noconfirm -S llvm clang
