53df560de39857b1c66e5228d118930b8d94f4c7
Anna Rift <anna.rift@hpe.com>
2022-09-14 14:02:49 -0700

    Add various additional operator overloading tests
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- compiler/dyno/test/resolution/testOperatorOverloads.cpp
+++ compiler/dyno/test/resolution/testOperatorOverloads.cpp
@@ -26,6 +26,7 @@
 
 #include <cassert>
 
+// basic definition tests with cast operator
 static void test1() {
   Context ctx;
   auto context = &ctx;
@@ -78,7 +79,7 @@ static void test1() {
   QualifiedType qt4 = resolveTypeOfXInit(context,
     R""""(
       record R {
-      var field: int;
+        var field: int;
       }
       operator :(z: R, type t: int) { return z.field; }
       var myR: R;
@@ -90,6 +91,7 @@ static void test1() {
   ctx.advanceToNextRevision(false);
 }
 
+// scope issue tests with cast operator
 static void test2() {
   Context ctx;
   auto context = &ctx;
@@ -98,7 +100,7 @@ static void test2() {
   QualifiedType qt1 = resolveTypeOfXInit(context,
     R""""(
       record R {
-      var field: int;
+        var field: int;
       }
       operator R.:(z: R, type t: int) { return z.field; }
       operator :(z: R, type t: int) { return z.field; }
@@ -115,9 +117,9 @@ static void test2() {
     R""""(
       module M {
         record R {
-        var field: int;
-          operator :(z: R, type t: int) { return z.field; }
+          var field: int;
         }
+        operator R.:(z: R, type t: int) { return z.field; }
       }
 
       import this.M.R;
@@ -130,9 +132,103 @@ static void test2() {
   ctx.advanceToNextRevision(false);
 }
 
+// various additional tests with other operators
+static void test3() {
+  Context ctx;
+  auto context = &ctx;
+
+  // incorrectly defined unary operator overload
+  QualifiedType qt1 = resolveTypeOfXInit(context,
+    R""""(
+      record R {
+        var field: int;
+        operator !(z: R, type t: int) { return z.field; }
+      }
+      var myR: R;
+      var x = !myR;
+    )""""
+  );
+  assert(qt1.kind() == QualifiedType::UNKNOWN);
+  assert(qt1.type() && qt1.type()->isErroneousType());
+  ctx.advanceToNextRevision(false);
+
+  // correct unary operator overload
+  QualifiedType qt2 = resolveTypeOfXInit(context,
+    R""""(
+      record R {
+        var field: int;
+        operator !(z: R) { return z.field; }
+      }
+      var myR: R;
+      var x = !myR;
+    )""""
+  );
+  assert(qt2.type() && qt2.type()->isIntType());
+  assert(qt2.kind() == QualifiedType::VAR);
+  ctx.advanceToNextRevision(false);
+
+  // overloading more operators
+  QualifiedType qt3 = resolveTypeOfXInit(context,
+    R""""(
+      record R {
+        var field: int;
+        operator +(z: R, t: int) { return z.field; }
+      }
+      var myR: R;
+      var x = myR + 3;
+    )""""
+  );
+  assert(qt3.type() && qt3.type()->isIntType());
+  assert(qt3.kind() == QualifiedType::VAR);
+  ctx.advanceToNextRevision(false);
+
+  QualifiedType qt4 = resolveTypeOfXInit(context,
+    R""""(
+      record R {
+        var field: int;
+        operator >(z: R, t: complex) { return true; }
+      }
+      var myR: R;
+      var x = myR > 2;
+    )""""
+  );
+  assert(qt4.type() && qt4.type()->isBoolType());
+  assert(qt4.kind() == QualifiedType::VAR);
+  ctx.advanceToNextRevision(false);
+
+  QualifiedType qt5 = resolveTypeOfXInit(context,
+    R""""(
+      record R {
+        var field: int;
+      }
+      operator <(z: R, otherZ: R) { return 3; }
+      var myR: R;
+      var myOtherR: R;
+      var x = myR < myOtherR;
+    )""""
+  );
+  assert(qt5.type() && qt5.type()->isIntType());
+  assert(qt5.kind() == QualifiedType::VAR);
+  ctx.advanceToNextRevision(false);
+
+  // overloading operator for non-compound types
+  QualifiedType qt6 = resolveTypeOfXInit(context,
+    R""""(
+      operator *(a: int, b: int) { return true; }
+      var a: int = 2;
+      var b: int = 3;
+      var x = a * b;
+    )""""
+  );
+  assert(qt6.type() && qt6.type()->isBoolType());
+  assert(qt6.kind() == QualifiedType::VAR);
+  ctx.advanceToNextRevision(false);
+}
+
 
 int main() {
   test1();
   test2();
+  test3();
   return 0;
 }
