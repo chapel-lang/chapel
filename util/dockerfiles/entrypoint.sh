f22aef45a033063de8168a0d0e47d1da0eb85710
bhavanijayakumaran <82669529+bhavanijayakumaran@users.noreply.github.com>
2022-09-14 21:49:11 -0500

    Address review comment. Updated entrypoint.sh to use chpl_bin_subdir.py
    Signed-off-by: bhavanijayakumaran <82669529+bhavanijayakumaran@users.noreply.github.com>

--- util/dockerfiles/entrypoint.sh
+++ util/dockerfiles/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export CHPL_TARGET_ARCH=`$CHPL_HOME/util/chplenv/chpl_arch.py --target`
-export PATH=$PATH:$CHPL_HOME/bin/linux64-$CHPL_TARGET_ARCH:$CHPL_HOME/util
+export CHPL_TARGET_ARCH=`$CHPL_HOME/util/chplenv/chpl_bin_subdir.py --host`
+export PATH=$PATH:$CHPL_HOME/bin/$CHPL_TARGET_ARCH:$CHPL_HOME/util
 exec "/bin/bash"
