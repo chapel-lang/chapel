766a5cf657032cf89443949016d75eeb6ff6f51e
Jeremiah Corrado <jeremiah.corrado@hpe.com>
2022-10-05 15:42:59 -0600

    removed generated _file type from compiler. replaced with stub in chapelBase
    Signed-off-by: Jeremiah Corrado <jeremiah.corrado@hpe.com>

--- compiler/AST/type.cpp
+++ compiler/AST/type.cpp
@@ -906,10 +906,6 @@ void initPrimitiveTypes() {
   dtCFnPtr->symbol->addFlag(FLAG_NO_CODEGEN);
   dtCFnPtr->defaultValue = gNil;
 
-  dtFile = createPrimitiveType("_file", "_cfile");
-  dtFile->symbol->addFlag(FLAG_EXTERN);
-  dtFile->symbol->addFlag(FLAG_DEPRECATED);
-
   dtOpaque = createPrimitiveType("opaque", "chpl_opaque");
 
   CREATE_DEFAULT_SYMBOL(dtOpaque, gOpaque, "_nullOpaque");
@@ -1687,7 +1683,6 @@ bool needsCapture(Type* t) {
       isRecord(t) ||
       isUnion(t) ||
       t == dtTaskID || // false?
-      t == dtFile ||
       // TODO: Move these down to the "no" section.
       t == dtNil ||
       t == dtOpaque ||
