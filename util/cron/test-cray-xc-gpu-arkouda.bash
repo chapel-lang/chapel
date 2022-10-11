68deeb0cdad962af380fa65f751fd3504be3cee7
Andy Stone <stonea@users.noreply.github.com>
2022-10-11 17:19:22 -0500

    Nightly testing for GPU locale model and Arkouda
    ---
    Signed-off-by: Andy Stone <stonea@users.noreply.github.com>

---
+++ util/cron/test-cray-xc-gpu-arkouda.bash
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#
+# Run arkouda testing on a cray-cs with HDR IB
+
+CWD=$(cd $(dirname $0) ; pwd)
+source $CWD/common-native-gpu.bash
+export CHPL_COMM=none
+
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="cray-xc-gpu-arkouda"
+
+# setup arkouda
+export CHPL_TEST_ARKOUDA_PERF=false
+source $CWD/common-arkouda.bash
+export ARKOUDA_NUMLOCALES=16
+
+# List of Arkouda server modules we exempt from testing (that goal is to
+# eventually have this be an empty list).
+export CHPL_TEST_ARKOUDA_DISABLE_MODELS=ArraySetopsMsg:KExtremeMsg:ArgSortMsg:SegmentedMsg:DataFrameIndexingMsg:UniqueMsg:In1dMsg:SortMsg:ReductionMsg:EfuncMsg:HDF5Msg:EncodingMsg
+export CHPL_TEST_ARKOUDA_STOP_AFTER_BUILD="true"
+
+module list
+
+# setup for CS perf (gasnet-large, gnu, 128-core Rome)
+source $CWD/common-cray-cs.bash
+export CHPL_LAUNCHER_PARTITION=rome64Share
+export CHPL_TARGET_CPU=none
+
+module list
+
+export GASNET_PHYSMEM_MAX="9/10"
+nightly_args="${nightly_args} -no-buildcheck"
+
+test_nightly
+sync_graphs
