addc48a94fa44cd48b7c2bd6fdebc5d6b241caa3
arezaii <ahmad.rezaii@hpe.com>
2022-10-12 14:35:16 -0600

    style(test): separate chpldoc version helper from chpl script
    * adds a copy of the chpl version test helper script with a
      small customization for chpldoc to omit the LLVM version
      information
    
    Signed-off-by: arezaii <ahmad.rezaii@hpe.com>

---
+++ test/chpldoc/compflags/combinations/versionhelp-chpldoc.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+CWD=$(cd $(dirname $0) ; pwd)
+compiler=$3
+
+echo -n `basename $compiler`
+cat $CWD/../../../compflags/bradc/printstuff/version.goodstart
+# During pre-release mode
+diff $CWD/../../../../compiler/main/BUILD_VERSION $CWD/zero.txt > /dev/null 2>&1 && echo "" || \
+  { echo -n " pre-release (" && cat $CWD/../../../../compiler/main/BUILD_VERSION | tr -d \"\\n && echo ")" ; }
+# During release mode:
+# echo ""
