854966245b01d9f2226c5b0c71f67a360f9cf31c
Michael Ferguson <mppf@users.noreply.github.com>
2022-10-12 10:35:05 -0400

    Make header simpler
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

---
+++ frontend/lib/resolution/call-init-deinit.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2021-2022 Hewlett Packard Enterprise Development LP
+ * Other additional copyright holders may be indicated within.
+ *
+ * The entirety of this work is licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CALL_INIT_DEINIT_H
+#define CALL_INIT_DEINIT_H
+
+#include "chpl/resolution/ResolvedVisitor.h"
+#include "chpl/resolution/resolution-types.h"
+#include "chpl/uast/all-uast.h"
+
+namespace chpl {
+namespace resolution {
+
+class Resolver;
+
+/* Add calls to default init, copy init, deinit, etc to the Resolver's
+   results after analyzing for split-init and copy-elision. */
+void callInitDeinit(Resolver& resolver);
+
+} // end namespace resolution
+} // end namespace chpl
+
+#endif
--- frontend/lib/resolution/resolution-queries.cpp
+++ frontend/lib/resolution/resolution-queries.cpp
@@ -31,7 +31,7 @@
 #include "chpl/types/all-types.h"
 #include "chpl/uast/all-uast.h"
 
-#include "CallInitDeinit.h"
+#include "call-init-deinit.h"
 #include "Resolver.h"
 #include "default-functions.h"
 #include "prims.h"
@@ -1695,7 +1695,7 @@ resolveFunctionByInfoQuery(Context* context,
     auto newTfsForInitializer = visitor.initResolver->finalize();
 
     // then, resolve '=' and add any copy init/deinit calls as needed
-    CallInitDeinit::process(visitor);
+    callInitDeinit(visitor);
 
     // TODO: can this be encapsulated in a method?
     resolvedPoiInfo.swap(visitor.poiInfo);
