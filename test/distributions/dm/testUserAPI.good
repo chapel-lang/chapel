chapel-lang/chapel

commit d41692c7557fae36a72397a414ff89886f9e4818

Replace alignedLow/High queries with simply low/high
---
Signed-off-by: Brad Chamberlain <bradcray@users.noreply.github.com>

test/domains/userAPI/domainAPItest.chpl
@@ -15,6 +15,8 @@ proc testDomainAPI2D(lbl, D: domain, idx, OOBidx1, OOBidx2, intDom) {
   writeln("alignment        = ", D.alignment);
   writeln("first            = ", D.first);
   writeln("last             = ", D.last);
+  writeln("low              = ", D.low);
+  writeln("high             = ", D.high);
   writeln("dims()           = ", D.dims());
   writeln("dim(0)           = ", D.dim(0));
   writeln("dim(1)           = ", D.dim(1));

test/domains/userAPI/test2Ddomain.good
@@ -12,6 +12,8 @@ stride           = (1, 1)
 alignment        = (1, 0)
 first            = (1, 0)
 last             = (5, 4)
+low              = (1, 0)
+high             = (5, 4)
 dims()           = (1..5, 0..4)
 dim(0)           = 1..5
 dim(1)           = 0..4
@@ -72,6 +74,8 @@ stride           = (2, 2)
 alignment        = (1, 1)
 first            = (1, 1)
 last             = (5, 3)
+low              = (1, 1)
+high             = (5, 3)
 dims()           = (1..5 by 2, 0..4 by 2 align 1)
 dim(0)           = 1..5 by 2
 dim(1)           = 0..4 by 2 align 1

test/domains/userAPI/testEnumDomain.good
@@ -12,6 +12,8 @@ stride           = (1, 1)
 alignment        = (orange, yellow)
 first            = (orange, yellow)
 last             = (green, blue)
+low              = (orange, yellow)
+high             = (green, blue)
 dims()           = (orange..green, yellow..blue)
 dim(0)           = orange..green
 dim(1)           = yellow..blue
@@ -72,6 +74,8 @@ stride           = (2, 2)
 alignment        = (orange, yellow)
 first            = (orange, yellow)
 last             = (green, blue)
+low              = (orange, yellow)
+high             = (green, blue)
 dims()           = (orange..green by 2, yellow..blue by 2)
 dim(0)           = orange..green by 2
 dim(1)           = yellow..blue by 2

commit 71db83b1871183cd186e510b89576d601757cdd9

Update a .good file broken by d41692c7557fae36a72397a414ff89886f9e4818
In making a last-minute update to a test in my PR, I failed to take
into account that the test file might be used from other directories,
which caused this test to break.  This should fix it.  I did not find
other cases that were broken due to this with an additional check.

---
Signed-off-by: Brad Chamberlain <bradcray@users.noreply.github.com>

test/distributions/dm/testUserAPI.good
@@ -12,6 +12,8 @@ stride           = (1, 1)
 alignment        = (1, 0)
 first            = (1, 0)
 last             = (5, 4)
+low              = (1, 0)
+high             = (5, 4)
 dims()           = (1..5, 0..4)
 dim(0)           = 1..5
 dim(1)           = 0..4
@@ -72,6 +74,8 @@ stride           = (1, 1)
 alignment        = (1, 0)
 first            = (1, 0)
 last             = (5, 4)
+low              = (1, 0)
+high             = (5, 4)
 dims()           = (1..5, 0..4)
 dim(0)           = 1..5
 dim(1)           = 0..4
@@ -132,6 +136,8 @@ stride           = (1, 1)
 alignment        = (1, 0)
 first            = (1, 0)
 last             = (5, 4)
+low              = (1, 0)
+high             = (5, 4)
 dims()           = (1..5, 0..4)
 dim(0)           = 1..5
 dim(1)           = 0..4
@@ -192,6 +198,8 @@ stride           = (1, 1)
 alignment        = (1, 0)
 first            = (1, 0)
 last             = (5, 4)
+low              = (1, 0)
+high             = (5, 4)
 dims()           = (1..5, 0..4)
 dim(0)           = 1..5
 dim(1)           = 0..4
@@ -252,6 +260,8 @@ stride           = (2, 2)
 alignment        = (1, 1)
 first            = (1, 1)
 last             = (5, 3)
+low              = (1, 1)
+high             = (5, 3)
 dims()           = (1..5 by 2, 0..4 by 2 align 1)
 dim(0)           = 1..5 by 2
 dim(1)           = 0..4 by 2 align 1
@@ -312,6 +322,8 @@ stride           = (2, 2)
 alignment        = (1, 1)
 first            = (1, 1)
 last             = (5, 3)
+low              = (1, 1)
+high             = (5, 3)
 dims()           = (1..5 by 2, 0..4 by 2 align 1)
 dim(0)           = 1..5 by 2
 dim(1)           = 0..4 by 2 align 1
@@ -372,6 +384,8 @@ stride           = (2, 2)
 alignment        = (1, 1)
 first            = (1, 1)
 last             = (5, 3)
+low              = (1, 1)
+high             = (5, 3)
 dims()           = (1..5 by 2, 0..4 by 2 align 1)
 dim(0)           = 1..5 by 2
 dim(1)           = 0..4 by 2 align 1
@@ -432,6 +446,8 @@ stride           = (2, 2)
 alignment        = (1, 1)
 first            = (1, 1)
 last             = (5, 3)
+low              = (1, 1)
+high             = (5, 3)
 dims()           = (1..5 by 2, 0..4 by 2 align 1)
 dim(0)           = 1..5 by 2
 dim(1)           = 0..4 by 2 align 1