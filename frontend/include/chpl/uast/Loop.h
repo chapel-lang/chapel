96cbeeb10d59912a9352583787c4767a63e2b830
Ben Harshbarger <ben.harshb@gmail.com>
2022-11-28 10:30:27 -0800

    Serialization for some Loop nodes
    Signed-off-by: Ben Harshbarger <ben.harshb@gmail.com>

--- frontend/include/chpl/uast/Loop.h
+++ frontend/include/chpl/uast/Loop.h
@@ -45,6 +45,12 @@ class Loop: public AstNode {
     CHPL_ASSERT(children_[loopBodyChildNum_]->isBlock());
   }
 
+  Loop(AstTag tag, Deserializer& des)
+    : AstNode(tag, des) {
+    blockStyle_ = des.read<BlockStyle>();
+    loopBodyChildNum_ = des.read<int>();
+  }
+
   bool loopContentsMatchInner(const Loop* other) const {
     const Loop* lhs = this;
     const Loop* rhs = other;
@@ -103,6 +109,12 @@ class Loop: public AstNode {
   */
   BlockStyle blockStyle() const {
     return blockStyle_;
+  }
+
+  void serializePart(Serializer& ser) const {
+    AstNode::serializePart(ser);
+    ser(blockStyle_);
+    ser(loopBodyChildNum_);
   }
 };
 
