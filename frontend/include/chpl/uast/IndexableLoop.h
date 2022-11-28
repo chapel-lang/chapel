f8cc65ebd879584f182943752931a56c57523c9b
Ben Harshbarger <ben.harshb@gmail.com>
2022-11-28 10:30:27 -0800

    Serialization for some Loop nodes
    Signed-off-by: Ben Harshbarger <ben.harshb@gmail.com>

--- frontend/include/chpl/uast/IndexableLoop.h
+++ frontend/include/chpl/uast/IndexableLoop.h
@@ -51,6 +51,14 @@ class IndexableLoop : public Loop {
     CHPL_ASSERT(iterandChildNum >= 0);
   }
 
+  IndexableLoop(AstTag tag, Deserializer& des)
+    : Loop(tag, des) {
+    indexChildNum_ = des.read<int8_t>();
+    iterandChildNum_ = des.read<int8_t>();
+    withClauseChildNum_ = des.read<int8_t>();
+    isExpressionLevel_ = des.read<bool>();
+  }
+
   bool indexableLoopContentsMatchInner(const IndexableLoop* other) const {
     const IndexableLoop* lhs = this;
     const IndexableLoop* rhs = other;
@@ -124,6 +132,14 @@ class IndexableLoop : public Loop {
   */
   bool isExpressionLevel() const {
     return isExpressionLevel_;
+  }
+
+  void serializePart(Serializer& ser) const {
+    Loop::serializePart(ser);
+    ser(indexChildNum_);
+    ser(iterandChildNum_);
+    ser(withClauseChildNum_);
+    ser(isExpressionLevel_);
   }
 
 };
