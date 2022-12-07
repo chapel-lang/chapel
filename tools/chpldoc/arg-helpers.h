0cb0eb01249af417c9864ee0da0d986a0aa61111
bhavanijayakumaran <82669529+bhavanijayakumaran@users.noreply.github.com>
2022-12-07 09:43:21 -0600

    update copy rights from 2022 to 2023 exclude the files in third party and test folders
    Signed-off-by: bhavanijayakumaran <82669529+bhavanijayakumaran@users.noreply.github.com>

--- tools/chpldoc/arg-helpers.h
+++ tools/chpldoc/arg-helpers.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Hewlett Packard Enterprise Development LP
+ * Copyright 2023 Hewlett Packard Enterprise Development LP
  * Other additional copyright holders may be indicated within.
  *
  * The entirety of this work is licensed under the Apache License,
