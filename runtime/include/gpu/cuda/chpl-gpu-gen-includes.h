e5468daace7d8687e930676619f996e8ea45aba0
Andy Stone <stonea@users.noreply.github.com>
2023-01-23 12:12:58 -0800

    Address reviewer comments
    ---
    Signed-off-by: Andy Stone <stonea@users.noreply.github.com>

--- runtime/include/gpu/cuda/chpl-gpu-gen-includes.h
+++ runtime/include/gpu/cuda/chpl-gpu-gen-includes.h
@@ -107,11 +107,14 @@ __device__ __host__ static inline void chpl_gpu_printTimeDelta(
 }
 
 __device__ static inline void chpl_gpu_force_sync() {
+  // Using __syncThreads() directly causes issues when compiling programs with
+  // --fast.  It's likely due to an issue with clang discussed here:
+  // https://github.com/llvm/llvm-project/issues/58626
   asm volatile("bar.sync 0;" : : : "memory");
 }
 
 __host__ static inline void chpl_gpu_force_sync() {
-  // TODO
+  chpl_internal_error("chpl_gpu_force_sync called from host");
 }
 
 #endif // HAS_GPU_LOCALE
