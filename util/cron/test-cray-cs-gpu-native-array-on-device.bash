84df12d0c9203325ffc5b5ecd542903be92c73bc
Andy Stone <stonea@users.noreply.github.com>
2023-01-26 20:17:59 -0800

    Fix CHPL_NIGHTLY_TEST_CONFIG_NAME in cron script
    ---
    Signed-off-by: Andy Stone <stonea@users.noreply.github.com>

--- util/cron/test-cray-cs-gpu-native-array-on-device.bash
+++ util/cron/test-cray-cs-gpu-native-array-on-device.bash
@@ -8,5 +8,5 @@ export CHPL_COMM=none
 export CHPL_NIGHTLY_TEST_DIRS="gpu/native/array_on_device"
 export CHPL_GPU_MEM_STRATEGY=array_on_device
 
-export CHPL_NIGHTLY_TEST_CONFIG_NAME="cray-cs-gpu-native"
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="cray-cs-gpu-native-array-on-device"
 $CWD/nightly -cron ${nightly_args}
