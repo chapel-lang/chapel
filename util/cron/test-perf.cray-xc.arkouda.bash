d0e69e8b51dec96fcc27ba64a9f4021b62f1a7b2
Ben McDonald <46734217+bmcdonald3@users.noreply.github.com>
2023-02-07 11:21:20 -0500

    Add logging for Arkouda XC performance test This PR adds a new test logging for the creation of the Arkouda log directory located within the Arkouda clone.
    Signed-off-by: Ben McDonald <46734217+bmcdonald3@users.noreply.github.com>

--- util/cron/test-perf.cray-xc.arkouda.bash
+++ util/cron/test-perf.cray-xc.arkouda.bash
@@ -6,11 +6,12 @@ CWD=$(cd $(dirname $0) ; pwd)
 
 export CHPL_TEST_PERF_CONFIG_NAME='16-node-xc'
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.cray-xc.arkouda"
-export GEN_ARKOUDA_GRAPHS="false"
 
 # setup arkouda
 source $CWD/common-arkouda.bash
 export ARKOUDA_NUMLOCALES=16
+
+export CHPL_TEST_GEN_ARKOUDA_GRAPHS="false"
 
 module list
 
