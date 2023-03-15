f975ba2614de0bb92a9de307eabbc844620c739b
Danila Fedorin <daniel.fedorin@hpe.com>
2023-03-15 16:27:35 -0700

    Add an `Ast` object that wraps AST nodes.
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

--- tools/chapel-py/test.cpp
+++ tools/chapel-py/test.cpp
@@ -3,26 +3,72 @@
 #include "chpl/framework/Context.h"
 #include "chpl/parsing/parsing-queries.h"
 
-static PyMethodDef ChapelMethods[] = {
-  { NULL, NULL, 0, NULL } /* Sentinel */
-};
-
+typedef struct {
+  PyObject_HEAD
+  PyObject* contextObject;
+  const chpl::uast::AstNode* astNode;
+} AstObject;
 
 typedef struct {
-    PyObject_HEAD
-    chpl::Context context;
-    /* Type-specific fields go here. */
+  PyObject_HEAD
+  chpl::Context context;
+  /* Type-specific fields go here. */
 } ContextObject;
 
-static PyObject* ChapelContext_new(PyTypeObject* type, PyObject* args, PyObject* kwargs) {
-  ContextObject* self;
-  self = (ContextObject*) type->tp_alloc(type, 0);
-  if (!self) return nullptr;
-  new (&self->context) chpl::Context(getenv("CHPL_HOME"));
-  return (PyObject*) self;
+static int AstObject_init(AstObject* self, PyObject* args, PyObject* kwargs) {
+  PyObject* contextObjectPy;
+  const char* filePath;
+  if (!PyArg_ParseTuple(args, "Os", &contextObjectPy, &filePath))
+      return -1;
+  auto contextObject = (ContextObject*) contextObjectPy;
+  auto context = &contextObject->context;
+  auto pathUS = chpl::UniqueString::get(context, filePath);
+  auto parentPathUS = chpl::UniqueString();
+  auto& buildResult = chpl::parsing::parseFileToBuilderResult(context, pathUS, parentPathUS);
+  auto firstAst = buildResult.topLevelExpression(0);
+
+  // TODO: clearly not the right API
+  Py_INCREF(contextObjectPy);
+  self->astNode = firstAst;
+  self->contextObject = contextObjectPy;
+
+  return 0;
 }
 
-static void ChapelContext_dealloc(ContextObject* self) {
+static void AstObject_dealloc(AstObject* self) {
+  Py_XDECREF(self->contextObject);
+  Py_TYPE(self)->tp_free((PyObject *) self);
+}
+
+static PyObject* AstObject_dump(AstObject *self, PyObject *Py_UNUSED(ignored)) {
+  self->astNode->dump();
+  Py_RETURN_NONE;
+}
+
+static PyMethodDef AstObject_methods[] = {
+  {"dump", (PyCFunction) AstObject_dump, METH_NOARGS, "Dump the internal representation of the given AST node"},
+  {NULL}  /* Sentinel */
+};
+
+static PyTypeObject AstType = {
+  PyVarObject_HEAD_INIT(NULL, 0)
+  .tp_name = "chapel.Ast",
+  .tp_doc = PyDoc_STR("An opaque reference to a Chapel AST node"),
+  .tp_basicsize = sizeof(AstObject),
+  .tp_itemsize = 0,
+  .tp_flags = Py_TPFLAGS_DEFAULT,
+  .tp_new = PyType_GenericNew,
+  .tp_init = (initproc) AstObject_init,
+  .tp_dealloc = (destructor) AstObject_dealloc,
+  .tp_methods = AstObject_methods,
+};
+
+static int ContextObject_init(ContextObject* self, PyObject* args, PyObject* kwargs) {
+  new (&self->context) chpl::Context(getenv("CHPL_HOME"));
+  return 0;
+}
+
+static void ContextObject_dealloc(ContextObject* self) {
   self->context.~Context();
   Py_TYPE(self)->tp_free((PyObject *) self);
 }
@@ -34,8 +80,13 @@ static PyTypeObject ContextType = {
   .tp_basicsize = sizeof(ContextObject),
   .tp_itemsize = 0,
   .tp_flags = Py_TPFLAGS_DEFAULT,
-  .tp_new = ChapelContext_new,
-  .tp_dealloc = (destructor) ChapelContext_dealloc,
+  .tp_new = PyType_GenericNew,
+  .tp_init = (initproc) ContextObject_init,
+  .tp_dealloc = (destructor) ContextObject_dealloc,
+};
+
+static PyMethodDef ChapelMethods[] = {
+  { NULL, NULL, 0, NULL } /* Sentinel */
 };
 
 static PyModuleDef ChapelModule {
@@ -51,11 +102,18 @@ extern "C" {
 PyMODINIT_FUNC PyInit_chapel() {
   PyObject* chapelModule = nullptr;
   if (PyType_Ready(&ContextType) < 0) return nullptr;
+  if (PyType_Ready(&AstType) < 0) return nullptr;
   chapelModule = PyModule_Create(&ChapelModule);
   if (!chapelModule) return nullptr;
 
   if (PyModule_AddObject(chapelModule, "Context", (PyObject *) &ContextType) < 0) {
     Py_DECREF(&ContextType);
+    Py_DECREF(chapelModule);
+    return NULL;
+  }
+  if (PyModule_AddObject(chapelModule, "Ast", (PyObject *) &AstType) < 0) {
+    Py_DECREF(&ContextType);
+    Py_DECREF(&AstType);
     Py_DECREF(chapelModule);
     return NULL;
   }
