702339088cda6705f1a44150c9e1f46dfefd07ee
Brad Chamberlain <bradcray@users.noreply.github.com>
2023-03-23 17:02:57 -0700

    Bump version button for today's 1.30 release
    ---
    Signed-off-by: Brad Chamberlain <bradcray@users.noreply.github.com>

--- doc/util/versionButton.php
+++ doc/util/versionButton.php
@@ -87,8 +87,8 @@ if (pagePath == "") {
   pagePath = "index";
 }
 function dropSetup() {
-  var currentRelease = "1.29"; // what does the public have?
-  var stagedRelease = "1.30";  // is there a release staged but not yet public?
+  var currentRelease = "1.30"; // what does the public have?
+  var stagedRelease = "1.31";  // is there a release staged but not yet public?
   var nextRelease = "1.31";    // what's the next release? (on docs/main)
   var button = document.getElementById("versionButton");
   // Uses unicode down-pointing triangle
