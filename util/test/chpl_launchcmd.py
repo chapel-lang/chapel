b05ad4b7b4a7b0a58c8205e22a85c6f1286340ba
Elliot Ronaghan <ronawho@gmail.com>
2023-03-24 10:45:43 -0400

    Fix chpl_launchcmd syntax error introduced in #21907
    Signed-off-by: Elliot Ronaghan <ronawho@gmail.com>

--- util/test/chpl_launchcmd.py
+++ util/test/chpl_launchcmd.py
@@ -906,7 +906,7 @@ class PbsProJob(AbstractJob):
         elif num_locales > 0:
             select_stmt = select_pattern.format(num_locales)
 
-            if self.num_cpus_resource is not None
+            if self.num_cpus_resource is not None:
                 select_stmt += ':{0}={1}'.format(
                     self.num_cpus_resource, self.num_cpus)
 
