4dbc4888740d1e9ccec24e6fb95de03d00a97f34
bhavanijayakumaran <82669529+bhavanijayakumaran@users.noreply.github.com>
2023-03-28 08:53:50 -0500

    Fix the if conditions in brew_install.sh
    Signed-off-by: bhavanijayakumaran <82669529+bhavanijayakumaran@users.noreply.github.com>

--- util/packaging/docker/test/brew_install.bash
+++ util/packaging/docker/test/brew_install.bash
@@ -1,67 +1,42 @@
 #!/usr/bin/env bash
 
-# Script used in docker exec command to test homebrew formula
+Script used in docker exec command to test homebrew formula
 brew test-bot --only-tap-syntax
-    if [$? -ne 0 ]
-    then
+if [ $? -ne 0 ]; then
       echo "brew test-bot --only-tap-syntax failed" 
       exit 1
-      else
+else
       echo "brew test-bot --only-tap-syntax succeeded"
-    fi
+fi
 
-brew test-bot --only-formulae-detect
-    if [$? -ne 0 ]
-    then
-      echo "brew test-bot --only-formulae-detect failed" 
-      exit 1
-      else
-      echo "brew test-bot --only-formulae-detect succeeded"
-    fi
-
-brew test-bot --only-setup
-    if [$? -ne 0 ]
-    then
-      echo "brew test-bot --only-setup failed" 
-      exit 1
-      else
-      echo "brew test-bot --only-setup succeeded"
-    fi
-
-# tests commands
 brew-test --only-cleanup-before
-
-    if [$? -ne 0 ]
-    then
+if [ $? -ne 0 ]; then
       echo "brew-test --only-cleanup-before failed" 
       exit 1
-      else
+else
       echo "brew-test --only-cleanup-before succeeded"
-    fi
-brew test-bot --only-setup
+fi
 
-    if [$? -ne 0 ]
-    then
+brew test-bot --only-setup
+if [ $? -ne 0 ]; then
       echo "brew test-bot --only-setup failed" 
       exit 1
-      else
+else
       echo "brew test-bot --only-setup succeeded"
-    fi
-brew test-bot --only-formulae --junit --only-json-tab --skip-dependents --testing-formula=chapel --added-formulae= --deleted-formulae=
+fi
 
-    if [$? -ne 0 ]
-    then
+brew test-bot --only-formulae --junit --only-json-tab --skip-dependents --testing-formula=chapel --added-formulae= --deleted-formulae=
+if [ $? -ne 0 ]; then
       echo "brew test-bot --only-formulae --junit --only-json-tab --skip-dependents --testing-formula=chapel --added-formulae= --deleted-formulae= failed" 
       exit 1
-      else
+else
       echo "brew test-bot --only-formulae --junit --only-json-tab --skip-dependents --testing-formula=chapel --added-formulae= --deleted-formulae= succeeded"
-    fi
-brew test-bot --only-formulae-dependents --junit --testing-formulae=chapel --skipped-or-failed-formulae=chapel
+fi
 
-    if [$? -ne 0 ]
-    then
+brew test-bot --only-formulae-dependents --junit --testing-formulae=chapel --skipped-or-failed-formulae=chapel
+if [ $? -ne 0 ]; then
       echo "brew test-bot --only-formulae-dependents --junit --testing-formulae=chapel --skipped-or-failed-formulae=chapel failed" 
       exit 1
-      else
+else
       echo "brew test-bot --only-formulae-dependents --junit --testing-formulae=chapel --skipped-or-failed-formulae=chapel succeeded"
-    fi
+fi
