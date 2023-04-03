b1adc938fc671fa6e5c4181d58a7cf6b45a7b2b8
bhavanijayakumaran <82669529+bhavanijayakumaran@users.noreply.github.com>
2023-04-03 08:20:39 -0500

    fix test-bot --only cleanup
    Signed-off-by: bhavanijayakumaran <82669529+bhavanijayakumaran@users.noreply.github.com>

--- util/packaging/docker/test/brew_install.bash
+++ util/packaging/docker/test/brew_install.bash
@@ -18,12 +18,12 @@ brew test-bot --only-setup
     fi
 
 # tests commands
-brew-test --only-cleanup-before
+brew test-bot --only-cleanup-before
 if [ $? -ne 0 ]; then
-      echo "brew-test --only-cleanup-before failed" 
+      echo "brew test-bot --only-cleanup-before failed" 
       exit 1
 else
-      echo "brew-test --only-cleanup-before succeeded"
+      echo "brew test-bot --only-cleanup-before succeeded"
 fi
 
 brew test-bot --only-formulae --junit --only-json-tab --skip-dependents --testing-formula=chapel --added-formulae= --deleted-formulae=
