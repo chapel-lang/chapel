cde4101981a6877023a56a5e40c45b02c2e74028
Ben Harshbarger <ben.harshb@gmail.com>
2023-04-16 18:06:36 -0700

    Compiler-generated methods for .domain and .eltType on arrays
    Signed-off-by: Ben Harshbarger <ben.harshb@gmail.com>

--- frontend/lib/resolution/default-functions.cpp
+++ frontend/lib/resolution/default-functions.cpp
@@ -130,6 +130,12 @@ needCompilerGeneratedMethod(Context* context, const Type* type,
         return true;
       }
     }
+  } else if (type->isArrayType()) {
+    if (parenless) {
+      if (name == "domain" || name == "eltType") {
+        return true;
+      }
+    }
   }
 
   return false;
@@ -385,6 +391,42 @@ generateDomainMethod(Context* context,
   return result;
 }
 
+static const TypedFnSignature*
+generateArrayMethod(Context* context,
+                    const ArrayType* at,
+                    UniqueString name) {
+  // Build a basic function signature for methods on an array
+  // TODO: we should really have a way to just set the return type here
+  const TypedFnSignature* result = nullptr;
+  std::vector<UntypedFnSignature::FormalDetail> formals;
+  std::vector<QualifiedType> formalTypes;
+
+  formals.push_back(UntypedFnSignature::FormalDetail(USTR("this"), false, nullptr));
+  formalTypes.push_back(QualifiedType(QualifiedType::CONST_REF, at));
+
+  auto ufs = UntypedFnSignature::get(context,
+                        /*id*/ at->id(),
+                        /*name*/ name,
+                        /*isMethod*/ true,
+                        /*isTypeConstructor*/ false,
+                        /*isCompilerGenerated*/ true,
+                        /*throws*/ false,
+                        /*idTag*/ parsing::idToTag(context, at->id()),
+                        /*kind*/ uast::Function::Kind::PROC,
+                        /*formals*/ std::move(formals),
+                        /*whereClause*/ nullptr);
+
+  // now build the other pieces of the typed signature
+  result = TypedFnSignature::get(context, ufs, std::move(formalTypes),
+                                 TypedFnSignature::WHERE_NONE,
+                                 /* needsInstantiation */ false,
+                                 /* instantiatedFrom */ nullptr,
+                                 /* parentFn */ nullptr,
+                                 /* formalsInstantiated */ Bitmap());
+
+  return result;
+}
+
 static const TypedFnSignature* const&
 fieldAccessorQuery(Context* context,
                    const types::CompositeType* compType,
@@ -466,6 +508,8 @@ getCompilerGeneratedMethodQuery(Context* context, const Type* type,
       result = generateDeinitSignature(context, compType);
     } else if (type->isDomainType()) {
       result = generateDomainMethod(context, type->toDomainType(), name);
+    } else if (type->isArrayType()) {
+      result = generateArrayMethod(context, type->toArrayType(), name);
     } else {
       CHPL_ASSERT(false && "Not implemented yet!");
     }
