87546cf040aa712c9c455a0c780a040293153c94
Ben Harshbarger <ben.harshb@gmail.com>
2023-04-21 20:48:22 -0700

    Remove old, unused checkNoUnresolveds function
    Signed-off-by: Ben Harshbarger <ben.harshb@gmail.com>

--- compiler/include/passes.h
+++ compiler/include/passes.h
@@ -86,7 +86,6 @@ void verify();
 void checkInvariants(char log_tag);
 void checkPrimitives();                 // constrains primitive use
 void checkPostResolution();
-void checkNoUnresolveds();
 // These checks can be applied after any pass.
 void checkForDuplicateUses();
 void checkArgsAndLocals();
