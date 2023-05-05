c126c424904cff55fd62a7d572cc6fffabc88f58
Michael Ferguson <mppf@users.noreply.github.com>
2023-05-05 14:42:24 -0400

    Improve comments about PRIM_GET_REAL/_IMAG
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

--- compiler/AST/primitive.cpp
+++ compiler/AST/primitive.cpp
@@ -811,9 +811,9 @@ initPrimitive() {
 
   // new keyword
   prim_def(PRIM_NEW, "new", returnInfoFirst);
-  // get complex real component
+  // given a complex value, produce get a reference to the real component
   prim_def(PRIM_GET_REAL, "complex_get_real", returnInfoComplexField);
-  // get complex imag component
+  // given a complex value, produce get a reference to the imag component
   prim_def(PRIM_GET_IMAG, "complex_get_imag", returnInfoComplexField);
   // query expression primitive
   prim_def(PRIM_QUERY, "query", returnInfoUnknown);
