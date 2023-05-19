044ca32cab4fa31178bcad90e69308543a3a89db
Jade Abraham <jade.abraham@hpe.com>
2023-05-19 14:53:01 -0700

    revert to using `lib/compiler`
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- tools/chapel-py/setup.py
+++ tools/chapel-py/setup.py
@@ -15,21 +15,17 @@ for line in chpl_variables_lines:
 
 llvm_config = chpl_variables.get("CHPL_LLVM_CONFIG")
 
-chpl_frontend_path = os.path.join(chpl_home, "build", "compiler", chpl_variables.get("CHPL_COMPILER_SUBDIR"))
-chpl_lib_path = os.path.join(chpl_frontend_path, "frontend", "lib")
-chpl_include_path = os.path.join(chpl_home, "frontend", "include")
+chpl_lib_path = os.path.join(chpl_home, "lib", "compiler", chpl_variables.get("CHPL_HOST_BIN_SUBDIR"))
 
 CXXFLAGS = []
 CXXFLAGS += ["-Wno-c99-designator"]
-CXXFLAGS += subprocess.check_output([llvm_config, "--cppflags"]).decode(sys.stdout.encoding).strip().split(" ")
-CXXFLAGS += ["-std=c++17", f"-I{chpl_include_path}"]
+CXXFLAGS += subprocess.check_output([llvm_config, "--cxxflags"]).decode(sys.stdout.encoding).strip().split(" ")
+CXXFLAGS += ["-std=c++17", "-I{}/frontend/include".format(chpl_home)]
 
 LDFLAGS = []
-LDFLAGS += [f"-L{chpl_lib_path}", "-lChplFrontend"]
-LDFLAGS += chpl_variables.get("CHPL_HOST_BUNDLED_LINK_ARGS").split(" ")
-LDFLAGS += subprocess.check_output([llvm_config, "--system-libs"]).decode(sys.stdout.encoding).strip().split(" ")
+LDFLAGS += ["-L{}".format(chpl_lib_path), "-lChplFrontend"]
 
 setup(name = "chapel",
       version = "0.1",
       ext_modules = [Extension("chapel.core", ["chapel.cpp"], extra_compile_args = CXXFLAGS, extra_link_args=LDFLAGS)]
-      );
+      )
