c89b146aae94ff1cfbefadd22d42dff4e033d72e
Andy Stone <stonea@users.noreply.github.com>
2023-05-24 12:01:07 -0500

    Fix nightly GPU ChOP testing to not timeout
    Specifically by using an https based url for the ChOp repos isntead of
    ssh
    
    ---
    Signed-off-by: Andy Stone <stonea@users.noreply.github.com>

--- util/cron/test-perf.gpu-cuda.bash
+++ util/cron/test-perf.gpu-cuda.bash
@@ -13,7 +13,5 @@ export CHPL_TEST_PERF_CONFIG_NAME='gpu'
 source $CWD/common-perf.bash
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.gpu-cuda"
 
-export CHOP_URL="git@github.com:tcarneirop/ChOp.git"
-
 nightly_args="${nightly_args} -performance -perflabel gpu- -numtrials 5 -startdate 07/15/22"
 $CWD/nightly -cron ${nightly_args}
