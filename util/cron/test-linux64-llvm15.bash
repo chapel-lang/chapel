bbfd321e8fb8393d994eb9bc65ddc4371e11bb0c
Anna Rift <anna.rift@hpe.com>
2023-05-26 15:31:18 -0700

    Reorder dependency sourcing so requested LLVM version isn't replaced
    Also removes the OFFICIAL_SYSTEM_LLVM variable as it no longer has any
    use, and wasn't even working since other loads in common.bash
    incidentally load the new LLVM 15 install from Spack.
    
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/cron/test-linux64-llvm15.bash
+++ util/cron/test-linux64-llvm15.bash
@@ -2,11 +2,10 @@
 #
 # Test default configuration on examples only, on linux64, with llvm 15
 
-export OFFICIAL_SYSTEM_LLVM=true
-source /data/cf/chapel/setup_system_llvm.bash 15
-
 CWD=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
 source $CWD/common.bash
+
+source /data/cf/chapel/setup_system_llvm.bash 15
 
 clang_version=$(clang -dumpversion)
 if [ "$clang_version" != "15.0.0" ]; then
