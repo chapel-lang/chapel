5e8c1eb87de4aadc38c9ef80a2519c18e8921539
Anna Rift <anna.rift@hpe.com>
2023-05-30 12:21:55 -0700

    Still source .bashrc for other systems
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/cron/common.bash
+++ util/cron/common.bash
@@ -7,15 +7,23 @@ CWD=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
 source $CWD/functions.bash
 
 if [ -f /data/cf/chapel/chpl-deps/setup_chpl_deps.bash ] ; then
-  # just load all dependencies via spack
+  # for chapcs/chapvm, just load all dependencies via spack
   source /data/cf/chapel/chpl-deps/setup_chpl_deps.bash
 else
+  # For our internal testing, this is necessary to get the latest version of gcc
+  # on the system.
+  if [ -z "${CHPL_SOURCED_BASHRC}" -a -f ~/.bashrc ] ; then
+      source ~/.bashrc
+      export CHPL_SOURCED_BASHRC=true
+  fi
+
   # load llvm
   if [ -f /cray/css/users/chapelu/setup_system_llvm.bash ] ; then
     source /cray/css/users/chapelu/setup_system_llvm.bash
   elif [ -f /cy/users/chapelu/setup_system_llvm.bash ] ; then
     source /cy/users/chapelu/setup_system_llvm.bash
   fi
+
   # load cmake
   if [ -f /cray/css/users/chapelu/setup_cmake_nightly.bash ] ; then
     source /cray/css/users/chapelu/setup_cmake_nightly.bash
