f85081be71d9a5da93023639195cf3c867efc160
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2023-07-20 16:10:00 -0700

    Reorder sources in script
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- util/cron/test-perf.gpu-rocm.bash
+++ util/cron/test-perf.gpu-rocm.bash
@@ -4,15 +4,15 @@
 
 CWD=$(cd $(dirname $0) ; pwd)
 source $CWD/common-native-gpu.bash
-module load rocm
 
 export CHPL_GPU=amd
 export CHPL_GPU_ARCH=gfx906
-export CHPL_GPU_MEM_STRATEGY=array_on_device
-export CHPL_LAUNCHER_PARTITION=amdMI60
-
-export CHPL_COMM=none
 export CHPL_LLVM=bundled
+export CHPL_COMM=none
+export CHPL_LAUNCHER_PARTITION=amdMI60
+export CHPL_GPU_MEM_STRATEGY=array_on_device
+module load rocm
+
 
 export CHPL_TEST_PERF_CONFIG_NAME='gpu-rocm'
 source $CWD/common-perf.bash
