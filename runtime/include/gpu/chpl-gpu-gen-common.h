a7bbb381546986cc006e811f4b99a1c05b29962d
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2023-07-21 16:33:44 -0700

    Refactor common device functions into a new header
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

---
+++ runtime/include/gpu/chpl-gpu-gen-common.h
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2020-2023 Hewlett Packard Enterprise Development LP
+ * Copyright 2004-2019 Cray Inc.
+ * Other additional copyright holders may be indicated within.
+ *
+ * The entirety of this work is licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _CHPL_GPU_GEN_COMMON_H
+#define _CHPL_GPU_GEN_COMMON_H
+
+#ifdef HAS_GPU_LOCALE
+
+#include "chpltypes.h"
+#include "chpl-comm.h"
+
+__device__ static inline c_sublocid_t chpl_task_getRequestedSubloc(void)
+{
+  // TODO
+  // We probably need an idea similar to `chpl_nodeID`. But, in our current
+  // design, the subloc ID that this function is supposed to return is the same
+  // as the device ID. So, maybe we call some device function to grab that and
+  // return it?
+  return 0;
+}
+
+__device__ static inline void* c_pointer_return(void* x) { return x; }
+__device__ static inline void* c_pointer_return_const(const void* x) {
+  return (void*)x;
+}
+
+__device__ static inline chpl_localeID_t chpl_rt_buildLocaleID(c_nodeid_t node,  c_sublocid_t subloc) {
+  chpl_localeID_t loc = { node, subloc };
+  return loc;
+}
+
+__device__ static inline c_nodeid_t chpl_rt_nodeFromLocaleID(chpl_localeID_t loc) {
+  return loc.node;
+}
+
+__device__ static inline c_nodeid_t chpl_rt_sublocFromLocaleID(chpl_localeID_t loc) {
+  return loc.subloc;
+}
+
+__device__ static inline void chpl_gen_comm_get(void *addr, c_nodeid_t node,
+  void* raddr, size_t size, int32_t commID, int ln, int32_t fn)
+{
+  // TODO
+}
+
+__device__ static inline void chpl_gen_comm_put(void* addr, c_nodeid_t node,
+  void* raddr, size_t size, int32_t commID, int ln, int32_t fn)
+{
+  // TODO
+}
+
+MAYBE_GPU static inline void chpl_gpu_write(const char *str) { printf("%s", str); }
+
+__device__ static inline void chpl_assert_on_gpu(int32_t lineno, int32_t filenameIdx) { /* no op */ }
+__host__ static inline void chpl_assert_on_gpu(int32_t lineno, int32_t filenameIdx) {
+  chpl_error("assertOnGpu() failed", lineno, filenameIdx);
+}
+
+__device__ static inline unsigned int chpl_gpu_clock(void) {
+  return (unsigned int)clock();
+}
+__host__ static inline unsigned int chpl_gpu_clock(void) {
+  return 0;
+}
+
+MAYBE_GPU static inline void chpl_gpu_printTimeDelta(
+  const char *msg, unsigned int start, unsigned int end)
+{
+  printf("%s%u\n", msg, end - start);
+}
+
+__host__ static inline void chpl_gpu_force_sync() {
+  chpl_internal_error("chpl_gpu_force_sync called from host");
+}
+
+__device__ static inline
+void chpl_gen_comm_get_gpu(void *addr, c_nodeid_t src_node,
+                           c_sublocid_t src_subloc, void* raddr, size_t size,
+                           int32_t commID, int ln, int32_t fn) { }
+
+__device__ static inline
+void chpl_gen_comm_put_gpu(void* addr,
+                           c_nodeid_t dst_node, c_sublocid_t dst_subloc,
+                           void* raddr, size_t size, int32_t commID, int ln,
+                           int32_t fn) { }
+
+
+
+
+
+#endif // HAS_GPU_LOCALE
+
+#endif // _CHPL_GPU_GEN_COMMON_H
