65f15ac77eddbc92288d337122d319ecda2f64fd
Anna Rift <anna.rift@hpe.com>
2023-07-24 14:15:33 -0700

    Adjust test to be like other C tests
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- test/types/cptr/takeClassToCAndBack/lib/takeClassToCAndBack.h
+++
@@ -1,9 +0,0 @@
-#include "stdchpl.h"
-#include "ctype.h"
-#include "wctype.h"
-void chpl__init_takeClassToCAndBack(int64_t _ln,
-                                    int32_t _fn);
-void getInstance(c_void_ptr * addrOut,
-                 int64_t arg);
-int64_t useInstance(c_void_ptr instancePtr);
-void deleteInstance(c_void_ptr instancePtr);
