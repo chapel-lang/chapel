b71f28e0aa043e70ec57fb41e872d37a5e63d9e5
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2023-08-01 17:07:08 -0700

    Refactor the compiler code a bit
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- compiler/codegen/codegen.cpp
+++ compiler/codegen/codegen.cpp
@@ -286,7 +286,8 @@ static void genGlobalRawString(const char *cname, std::string &value, size_t len
 #endif
 
 static void
-genGlobalInt(const char* cname, int value, bool isHeader) {
+genGlobalInt(const char* cname, int value, bool isHeader,
+             bool isConstant=true) {
   GenInfo* info = gGenInfo;
   if( info->cfile ) {
     if(isHeader)
@@ -299,7 +300,7 @@ genGlobalInt(const char* cname, int value, bool isHeader) {
         info->module->getOrInsertGlobal(
           cname, llvm::IntegerType::getInt32Ty(info->module->getContext())));
     globalInt->setInitializer(info->irBuilder->getInt32(value));
-    globalInt->setConstant(true);
+    globalInt->setConstant(isConstant);
     info->lvt->addGlobalValue(cname, globalInt, GEN_PTR, false, dtInt[INT_SIZE_32]);
 #endif
   }
@@ -2511,17 +2512,7 @@ static void embedGpuCode() {
 }
 
 static void codegenGpuGlobals() {
-  GenInfo* info = gGenInfo;
-
-  // codegen chpl_nodeID
-  const char* cname = "chpl_nodeID";
-  llvm::GlobalVariable *globalInt =
-      llvm::cast<llvm::GlobalVariable>(
-          info->module->getOrInsertGlobal(cname,
-                                          llvm::IntegerType::getInt32Ty(
-                                              info->module->getContext())));
-  globalInt->setInitializer(info->irBuilder->getInt32(-1));
-  info->lvt->addGlobalValue(cname, globalInt, GEN_PTR, false, dtInt[INT_SIZE_32]);
+  genGlobalInt("chpl_nodeID", -1, false, false);
 }
 #endif
 
