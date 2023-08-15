3c8d361aa5a546f49ad409d69caed439ece9951a
Brad Chamberlain <bradcray@users.noreply.github.com>
2023-08-15 10:38:03 -0700

    Fix a typo Lydia pointed out
    ---
    Signed-off-by: Brad Chamberlain <bradcray@users.noreply.github.com>

--- test/interop/C/errorMessage/initTwice.test.c
+++ test/interop/C/errorMessage/initTwice.test.c
@@ -2,7 +2,7 @@
 
 // Test of calling an exported Chapel library.
 int main(int argc, char* argv[]) {
-  // Initialize the Chapel runtime and standard modules twice This is
+  // Initialize the Chapel runtime and standard modules twice. This is
   // not legal/supported, so the second should result in an error.
   chpl_library_init(argc, argv);
   chpl_library_init(argc, argv);
