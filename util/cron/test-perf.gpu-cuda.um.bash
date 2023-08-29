2955c743b703062258edaa78dd1c8dc584e86707
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2023-08-29 11:24:05 -0700

    Adjust CUDA perf testing to plot both aod:v and um
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- util/cron/test-perf.gpu-cuda.um.bash
+++ util/cron/test-perf.gpu-cuda.um.bash
@@ -13,9 +13,9 @@ export CHPL_LAUNCHER_PARTITION=stormP100
 export CHPL_COMM=none
 export CHPL_GPU_MEM_STRATEGY=unified_memory
 
-export CHPL_TEST_PERF_CONFIG_NAME='gpu'
-source $CWD/common-perf.bash
+source $CWD/common-native-gpu-perf.bash
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.gpu-cuda.um"
+export CHPL_TEST_PERF_DESCRIPTION='gpu-cuda.um'
 
 nightly_args="${nightly_args} -performance -perflabel gpu- -numtrials 5 -startdate 07/15/22"
 $CWD/nightly -cron ${nightly_args}
