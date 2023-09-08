6139be68159a7e8004c6f60bdddcca539c3c9a7c
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2023-09-08 09:59:15 -0700

    Do some more adjustments in common script
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- util/cron/test-hpe-cray-ex-ofi.bash
+++ util/cron/test-hpe-cray-ex-ofi.bash
@@ -5,3 +5,9 @@ CWD=$(cd $(dirname $0) ; pwd)
 source $CWD/common.bash
 source $CWD/common-ofi.bash || \
   ( echo "Could not set up comm=ofi testing." && exit 1 )
+
+nightly_args="${nightly_args} -no-buildcheck"
+
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="hpe-cray-ex-ofi"
+
+$CWD/nightly -cron -examples ${nightly_args}
