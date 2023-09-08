6564e29dbe743f7af874fdb927da15d1e6fbb42f
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2023-09-08 14:30:58 -0700

    Drop -no-buildcheck
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- util/cron/test-hpe-cray-ex-ofi.bash
+++ util/cron/test-hpe-cray-ex-ofi.bash
@@ -7,8 +7,6 @@ source $CWD/common-ofi.bash || \
   ( echo "Could not set up comm=ofi testing." && exit 1 )
 source $CWD/common-hpe-cray-ex.bash
 
-nightly_args="${nightly_args} -no-buildcheck"
-
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="hpe-cray-ex-ofi"
 
 $CWD/nightly -cron -examples ${nightly_args}
