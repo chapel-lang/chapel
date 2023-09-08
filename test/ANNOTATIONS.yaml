Bears-R-Us/arkouda

commit 3c66f2a7a5c4ae7ec5d0577b61a892e580e8ab2f

Closes #2734: Fix remaining missed symbol table entry creation for distributed builds (#2735)
This PR (closes #2734) fixes the remaining missed symbol table entry creation for distributed builds. This is related to #2732, which only covered occurrences that were in `KExtremeMsg`

Co-authored-by: Pierce Hayes <pierce314159@users.noreply.github.com>

src/ArgSortMsg.chpl
@@ -257,7 +257,7 @@ module ArgSortMsg
           var merged = mergeNumericArrays(numDigits, arrSize, totalDigits, bitWidths, negs, names, st);
 
           var iv = argsortDefault(merged, algorithm=algorithm);
-          st.addEntry(ivname, new shared SymEntry(iv));
+          st.addEntry(ivname, createSymEntry(iv));
 
           var repMsg = "created " + st.attrib(ivname);
           asLogger.debug(getModuleName(),getRoutineName(),getLineNumber(),repMsg);
@@ -360,17 +360,17 @@ module ArgSortMsg
                 when (DType.Int64) {
                     var e = toSymEntry(gEnt,int);
                     var iv = argsortDefault(e.a, algorithm=algorithm);
-                    st.addEntry(ivname, new shared SymEntry(iv));
+                    st.addEntry(ivname, createSymEntry(iv));
                 }
                 when (DType.UInt64) {
                     var e = toSymEntry(gEnt,uint);
                     var iv = argsortDefault(e.a, algorithm=algorithm);
-                    st.addEntry(ivname, new shared SymEntry(iv));
+                    st.addEntry(ivname, createSymEntry(iv));
                 }
                 when (DType.Float64) {
                     var e = toSymEntry(gEnt, real);
                     var iv = argsortDefault(e.a);
-                    st.addEntry(ivname, new shared SymEntry(iv));
+                    st.addEntry(ivname, createSymEntry(iv));
                 }
                 otherwise {
                     var errorMsg = notImplementedError(pn,gEnt.dtype);
@@ -385,7 +385,7 @@ module ArgSortMsg
             overMemLimit((8 * strings.size * 8)
                          + (2 * here.maxTaskPar * numLocales * 2**16 * 8));
             var iv = strings.argsort();
-            st.addEntry(ivname, new shared SymEntry(iv));
+            st.addEntry(ivname, createSymEntry(iv));
           }
           otherwise {
               var errorMsg = notImplementedError(pn, objtype: string);

src/ArraySetopsMsg.chpl
@@ -58,7 +58,7 @@ module ArraySetopsMsg
             var f = toSymEntry(gEnt2,int);
 
             var aV = intersect1d(e.a, f.a, isUnique);
-            st.addEntry(vname, new shared SymEntry(aV));
+            st.addEntry(vname, createSymEntry(aV));
 
             repMsg = "created " + st.attrib(vname);
             asLogger.debug(getModuleName(),getRoutineName(),getLineNumber(),repMsg);
@@ -69,7 +69,7 @@ module ArraySetopsMsg
             var f = toSymEntry(gEnt2,uint);
 
             var aV = intersect1d(e.a, f.a, isUnique);
-            st.addEntry(vname, new shared SymEntry(aV));
+            st.addEntry(vname, createSymEntry(aV));
 
             repMsg = "created " + st.attrib(vname);
             asLogger.debug(getModuleName(),getRoutineName(),getLineNumber(),repMsg);
@@ -112,7 +112,7 @@ module ArraySetopsMsg
              var f = toSymEntry(gEnt2,int);
              
              var aV = setxor1d(e.a, f.a, isUnique);
-             st.addEntry(vname, new shared SymEntry(aV));
+             st.addEntry(vname, createSymEntry(aV));
 
              repMsg = "created " + st.attrib(vname);
              asLogger.debug(getModuleName(),getRoutineName(),getLineNumber(),repMsg);
@@ -123,7 +123,7 @@ module ArraySetopsMsg
              var f = toSymEntry(gEnt2,uint);
              
              var aV = setxor1d(e.a, f.a, isUnique);
-             st.addEntry(vname, new shared SymEntry(aV));
+             st.addEntry(vname, createSymEntry(aV));
 
              repMsg = "created " + st.attrib(vname);
              asLogger.debug(getModuleName(),getRoutineName(),getLineNumber(),repMsg);
@@ -166,7 +166,7 @@ module ArraySetopsMsg
              var f = toSymEntry(gEnt2, int);
              
              var aV = setdiff1d(e.a, f.a, isUnique);
-             st.addEntry(vname, new shared SymEntry(aV));
+             st.addEntry(vname, createSymEntry(aV));
 
              var repMsg = "created " + st.attrib(vname);
              asLogger.debug(getModuleName(),getRoutineName(),getLineNumber(),repMsg);
@@ -177,7 +177,7 @@ module ArraySetopsMsg
              var f = toSymEntry(gEnt2, uint);
              
              var aV = setdiff1d(e.a, f.a, isUnique);
-             st.addEntry(vname, new shared SymEntry(aV));
+             st.addEntry(vname, createSymEntry(aV));
 
              var repMsg = "created " + st.attrib(vname);
              asLogger.debug(getModuleName(),getRoutineName(),getLineNumber(),repMsg);
@@ -219,7 +219,7 @@ module ArraySetopsMsg
            var f = toSymEntry(gEnt2,int);
 
            var aV = union1d(e.a, f.a);
-           st.addEntry(vname, new shared SymEntry(aV));
+           st.addEntry(vname, createSymEntry(aV));
 
            var repMsg = "created " + st.attrib(vname);
            asLogger.debug(getModuleName(),getRoutineName(),getLineNumber(),repMsg);
@@ -230,7 +230,7 @@ module ArraySetopsMsg
            var f = toSymEntry(gEnt2,uint);
 
            var aV = union1d(e.a, f.a);
-           st.addEntry(vname, new shared SymEntry(aV));
+           st.addEntry(vname, createSymEntry(aV));
 
            var repMsg = "created " + st.attrib(vname);
            asLogger.debug(getModuleName(),getRoutineName(),getLineNumber(),repMsg);

src/BigIntMsg.chpl
@@ -48,7 +48,7 @@ module BigIntMsg {
         }
 
         var retname = st.nextName();
-        st.addEntry(retname, new shared SymEntry(bigIntArray, max_bits));
+        st.addEntry(retname, createSymEntry(bigIntArray, max_bits));
         var syment = toSymEntry(getGenericTypedArrayEntry(retname, st), bigint);
         repMsg = "created %s".doFormat(st.attrib(retname));
         biLogger.debug(getModuleName(),getRoutineName(),getLineNumber(),repMsg);
@@ -73,7 +73,7 @@ module BigIntMsg {
                 while !all_zero {
                   low = tmp:uint;
                   var retname = st.nextName();
-                  st.addEntry(retname, new shared SymEntry(low));
+                  st.addEntry(retname, createSymEntry(low));
                   retList.pushBack("created %s".doFormat(st.attrib(retname)));
 
                   all_zero = true;

src/CSVMsg.chpl
@@ -400,7 +400,7 @@ module CSVMsg {
                 when DType.Int64 {
                     var a = makeDistArray(record_count, int);
                     read_files_into_dist_array(a, dset, filenames, subdoms, skips, true, col_delim, offsets);
-                    var entry = new shared SymEntry(a);
+                    var entry = createSymEntry(a);
                     var rname = st.nextName();
                     st.addEntry(rname, entry);
                     rtnData.pushBack((dset, ObjType.PDARRAY, rname));
@@ -408,7 +408,7 @@ module CSVMsg {
                 when DType.UInt64 {
                     var a = makeDistArray(record_count, uint);
                     read_files_into_dist_array(a, dset, filenames, subdoms, skips, true, col_delim, offsets);
-                    var entry = new shared SymEntry(a);
+                    var entry = createSymEntry(a);
                     var rname = st.nextName();
                     st.addEntry(rname, entry);
                     rtnData.pushBack((dset, ObjType.PDARRAY, rname));
@@ -416,7 +416,7 @@ module CSVMsg {
                 when DType.Float64 {
                     var a = makeDistArray(record_count, real);
                     read_files_into_dist_array(a, dset, filenames, subdoms, skips, true, col_delim, offsets);
-                    var entry = new shared SymEntry(a);
+                    var entry = createSymEntry(a);
                     var rname = st.nextName();
                     st.addEntry(rname, entry);
                     rtnData.pushBack((dset, ObjType.PDARRAY, rname));
@@ -424,7 +424,7 @@ module CSVMsg {
                 when DType.Bool {
                     var a = makeDistArray(record_count, bool);
                     read_files_into_dist_array(a, dset, filenames, subdoms, skips, true, col_delim, offsets);
-                    var entry = new shared SymEntry(a);
+                    var entry = createSymEntry(a);
                     var rname = st.nextName();
                     st.addEntry(rname, entry);
                     rtnData.pushBack((dset, ObjType.PDARRAY, rname));

src/Cast.chpl
@@ -48,7 +48,7 @@ module Cast {
       castLogger.error(getModuleName(),getRoutineName(),getLineNumber(),errorMsg);
       return "Error: %s".doFormat(errorMsg);
     }
-    var after = st.addEntry(name, new shared SymEntry(tmp));
+    var after = st.addEntry(name, createSymEntry(tmp));
 
     var returnMsg = "created " + st.attrib(name);
     castLogger.debug(getModuleName(),getRoutineName(),getLineNumber(),returnMsg);
@@ -185,11 +185,11 @@ module Cast {
       // do something like segmented computation w/o the aggregation
       select errors {
         when ErrorMode.strict {
-          var entry = st.addEntry(name, new shared SymEntry(computeOnSegmentsWithoutAggregation(oa, va, SegFunction.StringToNumericStrict, bigint)));
+          var entry = st.addEntry(name, createSymEntry(computeOnSegmentsWithoutAggregation(oa, va, SegFunction.StringToNumericStrict, bigint)));
           returnMsg = "created " + st.attrib(name);
         }
         when ErrorMode.ignore {
-          var entry = st.addEntry(name, new shared SymEntry(computeOnSegmentsWithoutAggregation(oa, va, SegFunction.StringToNumericIgnore, bigint)));
+          var entry = st.addEntry(name, createSymEntry(computeOnSegmentsWithoutAggregation(oa, va, SegFunction.StringToNumericIgnore, bigint)));
           returnMsg = "created " + st.attrib(name);
         }
         when ErrorMode.return_validity {
@@ -200,7 +200,7 @@ module Cast {
           forall (t, v, vf) in zip(tmp, valid.a, valWithFlag) {
             (t, v) = vf;
           }
-          var entry = st.addEntry(name, new shared SymEntry(tmp));
+          var entry = st.addEntry(name, createSymEntry(tmp));
           returnMsg = "created " + st.attrib(name);
           returnMsg += "+created " + st.attrib(vname);
         }

src/ConcatenateMsg.chpl
@@ -361,7 +361,7 @@ module ConcatenateMsg
                               start += o.size;
                             }
                         }
-                        st.addEntry(rname, new shared SymEntry(tmp, max_bits));
+                        st.addEntry(rname, createSymEntry(tmp, max_bits));
                         cmLogger.debug(getModuleName(),getRoutineName(),getLineNumber(),
                                          "created concatenated pdarray: %s".doFormat(st.attrib(rname)));
                     }

src/DataFrameIndexingMsg.chpl
@@ -89,9 +89,9 @@
         }
 
         var s_name = st.nextName();
-        st.addEntry(s_name, new shared SymEntry(rsegs));
+        st.addEntry(s_name, createSymEntry(rsegs));
         var v_name = st.nextName();
-        st.addEntry(v_name, new shared SymEntry(rvals));
+        st.addEntry(v_name, createSymEntry(rvals));
 
         return "SegArray+%s+created %s+created %s".doFormat(col, st.attrib(s_name), st.attrib(v_name));
     }

src/FlattenMsg.chpl
@@ -29,7 +29,7 @@ module FlattenMsg {
         repMsg = "created %s+created bytes.size %?".doFormat(st.attrib(stringsObj.name), stringsObj.nBytes);
         if returnSegs {
           const optName: string = st.nextName();
-          st.addEntry(optName, new shared SymEntry(segs));
+          st.addEntry(optName, createSymEntry(segs));
           repMsg += "+created %s".doFormat(st.attrib(optName));
         }
       } otherwise {
@@ -70,7 +70,7 @@ module FlattenMsg {
         var retString = getSegString(off, val, st);
         repMsg = "created " + st.attrib(retString.name) + "+created bytes.size %?".doFormat(retString.nBytes);
         if returnSegs {
-          st.addEntry(optName, new shared SymEntry(segs));
+          st.addEntry(optName, createSymEntry(segs));
           repMsg += "+created %s".doFormat(st.attrib(optName));
         }
       }

chapel-lang/chapel

commit f8bd0477895ffd0f4cd24ee9b1c2d7b5a87cb290

Add perf annotations for 2023-09-07 Add annotations for: - fasta and revcomp improvements from https://github.com/chapel-lang/chapel/pull/23162 - Arkouda regressions from https://github.com/Bears-R-Us/arkouda/pull/2735
Signed-off-by: Ben McDonald <46734217+bmcdonald3@users.noreply.github.com>

test/ANNOTATIONS.yaml
@@ -925,6 +925,8 @@ fasta:
     - Optimize enum->int casts for concrete types (#10308)
   08/23/23:
     - Deprecate 'iokind' type and 'kind' field (#23007)
+  08/30/23:
+    - Improve performance of readBinary and writeBinary for dense arrays (#23162)
 
 fastaredux:
   01/14/14:
@@ -1835,6 +1837,10 @@ revcomp: &revcomp-base
     - Reduce uses of Math by automatically included modules (#19885)
   03/07/23:
     - Parallelize array.find() (#21750)
+  08/23/23:
+    - Deprecate 'iokind' type and 'kind' field (#23007)
+  08/30/23:
+    - Improve performance of readBinary and writeBinary for dense arrays (#23162)
 
 revcomp-submitted:
   06/07/22:
@@ -2379,6 +2385,8 @@ arkouda: &arkouda-base
     - Resolve Arkouda deprecation warnings for 1.32 to date (Bears-R-Us/arkouda#2632)
   07/06/23:
     - Upgrade Arkouda release testing from 1.30 to 1.31 (#22669)
+  08/29/23:
+    - Closes 2734 Fix remaining missed symbol table entry creation for distributed builds (Bears-R-Us/arkouda#2735)
 
 
 arkouda-string: