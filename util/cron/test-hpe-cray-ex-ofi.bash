b1ea4c5cd458eb460b0abb775ef81123895d33e0
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2023-09-08 16:00:08 -0700

    Remove an extra comment
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- util/cron/test-hpe-cray-ex-ofi.bash
+++ util/cron/test-hpe-cray-ex-ofi.bash
@@ -5,8 +5,6 @@ CWD=$(cd $(dirname $0) ; pwd)
 source $CWD/common.bash
 source $CWD/common-ofi.bash || \
   ( echo "Could not set up comm=ofi testing." && exit 1 )
-
-# this should come after common-
 source $CWD/common-hpe-cray-ex.bash
 
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="hpe-cray-ex-ofi"
