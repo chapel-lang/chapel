e515b01d5a7b6a82d1024c8c17844a66a6aaef8d
Brad Chamberlain <bradcray@users.noreply.github.com>
2023-09-13 12:20:48 -0700

    Rip out some believed-dead logic that I wanted to check separately
    ---
    Signed-off-by: Brad Chamberlain <bradcray@users.noreply.github.com>

--- frontend/lib/types/PrimitiveType.cpp
+++ frontend/lib/types/PrimitiveType.cpp
@@ -47,8 +47,6 @@ PrimitiveType::getWithNameAndWidth(Context* context,
   if (name == USTR("uint"))
     if (bitwidth == 8 || bitwidth == 16 || bitwidth == 32 || bitwidth == 64)
       return UintType::get(context, bitwidth);
-  if (name == USTR("bool"))
-    return BoolType::get(context);
   if (name == USTR("real"))
     if (bitwidth == 32 || bitwidth == 64)
       return RealType::get(context, bitwidth);
