928fa5b5072258478c87f819af9244c1343f9250
Andy Stone <stonea@users.noreply.github.com>
2023-09-29 15:33:01 -0700

    add gatherFromPerfKeys
    ---
    Signed-off-by: Andy Stone <stonea@users.noreply.github.com>

--- test/gpu/native/streamPrototype/stream.plot.gather.chpl.bash
+++ test/gpu/native/streamPrototype/stream.plot.gather.chpl.bash
@@ -8,26 +8,17 @@ sizes=( 1 2 4 8 16 32 64 128)
 # -----------------------------------------------------------------------------
 # Build Chapel code
 # -----------------------------------------------------------------------------
-chpl stream.chpl --fast -M../../../release/examples/benchmarks/hpcc
+chpl -M../../../release/examples/benchmarks/hpcc --fast stream.chpl
 
 # -----------------------------------------------------------------------------
 # Run Chapel trials
 # -----------------------------------------------------------------------------
-
-echo "" > $runLog
 for x in "${sizes[@]}"; do
   size=$((x * 1024 * 1024))
-  ./stream --useGpuDiags=false --SI=false --doValidation=false --m=$size | tee -a $runLog
+  runAndLog ./stream --useGpuDiags=false --SI=false --doValidation=false --m=$size
 done
 
 # -----------------------------------------------------------------------------
 # Collect data; store in results.dat
 # -----------------------------------------------------------------------------
-
-chpl_data=$(cat $runLog | sed -r -n 's/Performance \(GiB\/s\) = (.*)/\1/p')
-
-set +x
-echo -e "\t$experimentName" > $datFile
-paste \
-  <(printf "%s\n" "${sizes[@]}") \
-  <(printf "%s\n" "${chpl_data[@]}") >> $datFile
+gatherFromPerfkeys --keyfile stream.forplot.gpu-keys --rows ${sizes[@]} --columns "$experimentName"
