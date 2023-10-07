38dc53fd6ddf1c5d11ca58123a52eaf3668e71e5
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2023-10-06 20:46:26 -0700

    Drop additional makefile logic
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- runtime/src/gpu/nvidia/gpu-nvidia-aux.cc
+++ runtime/src/gpu/nvidia/gpu-nvidia-aux.cc
@@ -2,7 +2,7 @@
 #include <cub/cub.cuh>
 
 #include "chpl-gpu.h"
-#include "../../common/cuda-utils.h"
+#include "../common/cuda-utils.h"
 
 int64_t chpl_gpu_aux_sum_reduce(int64_t* data, int n) {
 
@@ -14,18 +14,18 @@ int64_t chpl_gpu_aux_sum_reduce(int64_t* data, int n) {
   size_t temp_bytes = 0;
   cub::DeviceReduce::Sum(temp, temp_bytes, data, (int64_t*)result, n);
 
-  printf("Allocation for scratch %zu\n", temp_bytes);
+  //printf("Allocation for scratch %zu\n", temp_bytes);
 
   // Allocate temporary storage
   CUDA_CALL(cuMemAlloc(((CUdeviceptr*)&temp), temp_bytes));
 
-  printf("Temporary allocated %p\n", temp);
+  //printf("Temporary allocated %p\n", temp);
 
   // Run sum-reduction
-  cub::DeviceReduce::Sum(temp, temp_bytes, data, (int64_t*)result, n, 0, true);
+  cub::DeviceReduce::Sum(temp, temp_bytes, data, (int64_t*)result, n, 0);
 
   int64_t result_host;
   CUDA_CALL(cuMemcpyDtoH(&result_host, result, sizeof(int64_t)));
-  printf("Result %ld\n", result_host);
+  //printf("Result %ld\n", result_host);
   return result_host;
 }
