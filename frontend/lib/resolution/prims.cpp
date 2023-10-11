2cc4376a68f4d8cd16fd0712d7453b180801923a
Danila Fedorin <daniel.fedorin@hpe.com>
2023-10-11 12:25:00 -0700

    Fix bugs in some of the primitives exposed by tests
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

--- frontend/lib/resolution/prims.cpp
+++ frontend/lib/resolution/prims.cpp
@@ -445,13 +445,15 @@ static QualifiedType primFamilyIsSubtype(Context* context,
     // We allow the type to be the same OR require instantiation; thus,
     // it's sufficient to check if no conversion occurs (both instntiates()
     // and !instantiates() are allowed).
-    result = cpr.passes() && !cpr.converts();
+    result = cpr.passes() && !cpr.converts() && !cpr.promotes();
   } else if (prim == PRIM_IS_SUBTYPE) {
     result = cpr.passes() && (cpr.conversionKind() == CanPassResult::NONE ||
                               cpr.conversionKind() == CanPassResult::SUBTYPE);
   } else {
     CHPL_ASSERT(prim == PRIM_IS_PROPER_SUBTYPE);
-    result = cpr.passes() && (cpr.conversionKind() == CanPassResult::SUBTYPE);
+    result = cpr.passes() && (cpr.conversionKind() == CanPassResult::NONE ||
+                              cpr.conversionKind() == CanPassResult::SUBTYPE) &&
+             newSubQT != newParentQT;
   }
 
   return QualifiedType(QualifiedType::PARAM, BoolType::get(context),
