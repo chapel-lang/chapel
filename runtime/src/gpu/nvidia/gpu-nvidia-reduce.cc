cf1e526a0b016cddc6a9ef4b792c3e3a6a202fed
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2023-10-16 16:14:56 -0700

    Runtime cleanup
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- runtime/src/gpu/nvidia/gpu-nvidia-reduce.cc
+++ runtime/src/gpu/nvidia/gpu-nvidia-reduce.cc
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2020-2023 Hewlett Packard Enterprise Development LP
+ * Copyright 2004-2019 Cray Inc.
+ * Other additional copyright holders may be indicated within.  *
+ * The entirety of this work is licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifdef HAS_GPU_LOCALE
+
 #include <cuda.h>
 #include <cub/cub.cuh>
 
@@ -56,3 +77,6 @@ GPU_IMPL_REDUCE(DEF_ONE_REDUCE_RET_VAL_IDX, ArgMax, maxloc)
 #undef DEF_ONE_REDUCE_RET_VAL_IDX
 
 #undef DEF_REDUCE
+
+#endif // HAS_GPU_LOCALE
+
