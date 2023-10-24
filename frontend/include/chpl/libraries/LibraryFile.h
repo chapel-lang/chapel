fc83f2819e61a34c72f46807bd5015c1ded1d744
Michael Ferguson <mppf@users.noreply.github.com>
2023-10-24 15:10:18 -0400

    Split LibraryFile.h into 3 headers
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

--- frontend/include/chpl/libraries/LibraryFile.h
+++ frontend/include/chpl/libraries/LibraryFile.h
@@ -17,15 +17,8 @@
  * limitations under the License.
  */
 
-/*
-  This file implements the generic chpl::serialize and chpl::deserialize
-  as well as specializations for some common types.
- */
-
-// TODO: should we move this to its own chpl/ subdirectory and namespace?
-
-#ifndef CHPL_FRAMEWORK_LIBRARY_FILE_H
-#define CHPL_FRAMEWORK_LIBRARY_FILE_H
+#ifndef CHPL_LIBRARIES_LIBRARY_FILE_H
+#define CHPL_LIBRARIES_LIBRARY_FILE_H
 
 #include "chpl/framework/UniqueString.h"
 
@@ -33,161 +26,13 @@
 #include <vector>
 
 namespace chpl {
-
 // forward declarations
 class Context;
 namespace uast {
   class Module;
 }
-
 namespace libraries {
-  // magic numbers
-  // file magic number is <7F>HPECHPL
-  static const uint64_t FILE_HEADER_MAGIC =      0x4C5048434550487F;
-  static const uint64_t MODULE_SECTION_MAGIC =   0x0030d01e5ec110e0;
-  static const uint64_t SYMBOL_TABLE_MAGIC =     0x0003bb1e5ec110e0;
-  static const uint32_t LONG_STRINGS_TABLE_MAGIC =       0x51e17601;
-  static const uint64_t UAST_SECTION_MAGIC =     0x0003bb1e5ec110e0;
-  static const uint64_t LOCATION_SECTION_MAGIC = 0x10ca11075ec110e0;
 
-  // file format version numbers
-  static const uint32_t FORMAT_VERSION_MAJOR =  0;
-  static const uint32_t FORMAT_VERSION_MINOR =  1;
-
-  // number of bytes in a file hash
-  static const int HASH_SIZE = 256;
-
-  struct FileHeader {
-    uint64_t magic;
-    uint32_t fileFormatVersionMajor;
-    uint32_t fileFormatVersionMinor;
-    uint32_t chplVersionMajor;
-    uint32_t chplVersionMinor;
-    uint32_t chplVersionUpdate;
-    uint32_t nModules;
-    uint8_t hash[HASH_SIZE];
-    // followed by nModules 8 bytes file offsets
-  };
-
-  struct ModuleHeader {
-    uint64_t magic;
-    uint64_t flags;
-    // the following are offsets relative to the start of the module header
-    uint64_t symbolTable;
-    uint64_t uAstSection;
-    uint64_t longStringsTable;
-    uint64_t locationSection;
-    uint64_t typesSection;
-    uint64_t functionsSection;
-    uint64_t dependenciesSection;
-    // followed by a variable-byte length & string storing the module ID
-  };
-
-  struct SymbolTableHeader {
-    uint64_t magic;
-    uint32_t nEntries;
-    // followed by nEntries SymbolTableEntrys
-  };
-
-  struct SymbolTableEntry {
-    // relative to the start of the module header:
-    uint64_t uAstEntry;
-    uint64_t locationEntry;
-    uint64_t typeOrFnEntry;
-    // followed by
-    //  * a byte storing uAst tag
-    //  * a variable-byte length & string storing a symbol table ID
-  };
-
-  struct uAstSectionHeader {
-    uint64_t magic;
-    uint64_t nEntries;
-    // followed by nEntries uAstEntrys
-  };
-
-  struct LongStringsTableHeader {
-    uint32_t magic;
-    uint32_t nLongStrings;
-    // followed by module-section relative offset for each long string
-    // followed by an extra offset (just after the last long string)
-  };
-
-  struct LocationSectionHeader {
-    uint64_t magic;
-    uint32_t nFilePaths;
-    uint32_t nGroups;
-    // followed by file path strings
-    // then followed nGroups LocationGroups
-  };
-  struct LocationGroupHeader {
-    // relative to the start of the module header:
-    uint64_t uAstEntry;
-    uint32_t startingLineNumber;
-    uint32_t nLocations;
-    // followed by location entries
-  };
-
-/** For writing a .dyno library file */
-class LibraryFileWriter {
- private:
-  Context* context = nullptr;
-  std::vector<UniqueString> inputFiles;
-  std::string outputFilePath;
-  std::ofstream fileStream;
-  std::vector<const uast::Module*> topLevelModules;
-
-  /** Gather the top-level modules */
-  void gatherTopLevelModules();
-
-  /** Open the file */
-  void openFile();
-
-  /** Write the file header */
-  void writeHeader();
-
-  /** Write the module section for the given module. Returns
-     the file offset to this section. */
-  uint64_t writeModuleSection(const uast::Module* mod);
-
-  /** Write the symbol table for a given module. Returns the
-      relative offset to the symbol table. */
-  uint64_t writeSymbolTable(const uast::Module* mod,
-                            uint64_t moduleSectionStart);
-
-  /** Write the uAST section. This will populate 'longStringsOffsets'
-      with 0 offsets. Returns the relative offset of the uAST section. */
-  uint64_t writeAst(const uast::Module* mod,
-                    uint64_t moduleSectionStart,
-                    Serializer& ser);
-
-  /** Write the long strings in 'longStringsOffsets' that were gathered
-      in the process of writing the module & update their indices.
-      Returns the relative offset to the long strings section. */
-  uint64_t writeLongStrings(uint64_t moduleSectionStart, Serializer& ser);
-
-  /** Write the locations. Returns the relative offset of the locations section.
-  */
-  uint64_t writeLocations(const uast::Module* mod, uint64_t moduleSectionStart);
-
- public:
-  /**
-    Construct a LibraryFileWriter to output the uAST from the
-    top-level modules in the source files provided in 'paths'. */
-  LibraryFileWriter(Context* context,
-                    std::vector<UniqueString> paths,
-                    std::string outputFilePath) :
-    context(context),
-    inputFiles(paths),
-    outputFilePath(outputFilePath)
-  { }
-
-  /**
-    Write the header and sections to the library file.
-    Returns 'true' if everything is OK, or 'false' if there was an
-    error in the process.
-    */
-  bool writeAllSections();
-};
 
 /** For reading a .dyno LibraryFile.
     Some data is read from the file on-demand.
---
+++ frontend/lib/libraries/LibraryFileWriter.cpp
@@ -0,0 +1,268 @@
+/*
+ * Copyright 2021-2023 Hewlett Packard Enterprise Development LP
+ * Other additional copyright holders may be indicated within.
+ *
+ * The entirety of this work is licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "chpl/libraries/LibraryFileWriter.h"
+
+#include "chpl/libraries/LibraryFileFormat.h"
+#include "chpl/parsing/parsing-queries.h"
+#include "chpl/util/filesystem.h"
+#include "chpl/util/version-info.h"
+
+namespace chpl {
+namespace libraries {
+
+static UniqueString cleanLocalPath(Context* context, UniqueString path) {
+  if (path.startsWith("/") ||
+      path.startsWith("./") == false) {
+    return path;
+  }
+
+  auto str = path.str();
+  while (str.find("./") == 0) {
+    str = str.substr(2);
+  }
+
+  return chpl::UniqueString::get(context, str);
+}
+
+void LibraryFileWriter::gatherTopLevelModules() {
+  // Parse the paths and gather a vector of top-level modules.
+  for (auto path : inputFiles) {
+    path = cleanLocalPath(context, path);
+    UniqueString empty;
+    std::vector<const uast::Module*> modsInFile =
+      parsing::parse(context, path, empty);
+    for (auto mod : modsInFile) {
+      topLevelModules.push_back(mod);
+    }
+  }
+}
+
+void LibraryFileWriter::openFile() {
+  fileStream.open(outputFilePath,
+                  std::ios::out | std::ios::trunc | std::ios::binary);
+}
+
+void LibraryFileWriter::writeHeader() {
+  // Construct a file header and write it
+  FileHeader header;
+  memset(&header, 0, sizeof(header));
+
+  header.magic = FILE_HEADER_MAGIC;
+  header.fileFormatVersionMajor = FORMAT_VERSION_MAJOR;
+  header.fileFormatVersionMinor = FORMAT_VERSION_MINOR;
+  header.chplVersionMajor = getMajorVersion();
+  header.chplVersionMinor = getMinorVersion();
+  header.chplVersionUpdate = getUpdateVersion();
+  header.nModules = topLevelModules.size();
+  // hash remains 0s at this point
+  fileStream.write((const char*) &header, sizeof(header));
+
+  // write the placeholder module section table
+  size_t n = topLevelModules.size();
+  for (size_t i = 0; i < n; i++) {
+    uint64_t zero = 0;
+    fileStream.write((const char*) &zero, sizeof(zero));
+  }
+}
+
+uint64_t LibraryFileWriter::writeModuleSection(const uast::Module* mod) {
+  auto moduleSectionStart = fileStream.tellp();
+
+  // Construct a module section header and write it
+  ModuleHeader header;
+  memset(&header, 0, sizeof(header));
+  header.magic = MODULE_SECTION_MAGIC;
+  fileStream.write((const char*) &header, sizeof(header));
+
+  // create a serializer to write the uAST
+  auto ser = Serializer(fileStream);
+
+  // write the symbol table section
+  header.symbolTable = writeSymbolTable(mod, moduleSectionStart);
+  header.uAstSection = writeAst(mod, moduleSectionStart, ser);
+  header.longStringsTable = writeLongStrings(moduleSectionStart, ser);
+  header.locationSection = writeLocations(mod, moduleSectionStart);
+
+  // update the module header with the saved locations by writing the
+  // header again.
+  auto savePos = fileStream.tellp();
+
+  fileStream.seekp(moduleSectionStart);
+  fileStream.write((const char*) &header, sizeof(header));
+
+  // seek back where we were
+  fileStream.seekp(savePos);
+
+  return moduleSectionStart;
+}
+
+uint64_t LibraryFileWriter::writeSymbolTable(const uast::Module* mod,
+                                             uint64_t moduleSectionStart) {
+  uint64_t symTableStart = fileStream.tellp();
+  SymbolTableHeader header;
+  memset(&header, 0, sizeof(header));
+  header.magic = SYMBOL_TABLE_MAGIC;
+  fileStream.write((const char*) &header, sizeof(header));
+
+  // TODO TODO TODO actually output something meaningful
+
+  return symTableStart - moduleSectionStart;
+}
+
+uint64_t LibraryFileWriter::writeAst(const uast::Module* mod,
+                                     uint64_t moduleSectionStart,
+                                     Serializer& ser) {
+  uint64_t astStart = fileStream.tellp();
+  uAstSectionHeader header;
+  memset(&header, 0, sizeof(header));
+  header.magic = MODULE_SECTION_MAGIC;
+  fileStream.write((const char*) &header, sizeof(header));
+
+  // serialize the data
+  mod->serialize(ser);
+  header.nEntries = ser.numAstsSerialized();
+
+  // update the number serialized by rewriting the header
+  auto savePos = fileStream.tellp();
+
+  fileStream.seekp(astStart);
+  fileStream.write((const char*) &header, sizeof(header));
+
+  // seek back where we were
+  fileStream.seekp(savePos);
+
+  return astStart - moduleSectionStart;
+}
+
+uint64_t
+LibraryFileWriter::writeLongStrings(uint64_t moduleSectionStart,
+                                    Serializer& ser) {
+  uint64_t longStringsStart = fileStream.tellp();
+
+  LongStringsTableHeader header;
+  memset(&header, 0, sizeof(header));
+  header.magic = LONG_STRINGS_TABLE_MAGIC;
+  header.nLongStrings = ser.nextStringIdx();
+  fileStream.write((const char*) &header, sizeof(header));
+
+  size_t n = header.nLongStrings;
+
+  std::vector<uint64_t> offsets;
+  offsets.resize(n);
+  // write 0s for the offsets. We will rewrite it in a moment.
+  fileStream.write((const char*) &offsets[0], n*sizeof(uint64_t));
+
+  // gather the strings by idx so we can output them
+  std::vector<std::pair<const char*, size_t>> byId;
+  byId.resize(n);
+
+  for (const auto& kv : ser.stringCache()) {
+    const char* ptr = kv.first;
+    const auto& pair = kv.second;
+    uint32_t idx = pair.first;
+    size_t len = pair.second;
+    if (0 < idx && idx < n) {
+      byId[idx] = std::make_pair(ptr, len);
+    }
+  }
+
+  // output the strings in idx order and update offsets
+  for (size_t i = 0; i < n; i++) {
+    const auto& pair = byId[i];
+    const char* ptr = pair.first;
+    size_t len = pair.second;
+    if (byId[i].first != nullptr) {
+      uint64_t pos = fileStream.tellp();
+      offsets[i] = pos - moduleSectionStart;
+      fileStream.write(ptr, len);
+    }
+  }
+
+  // update the offsets in the file
+  fileStream.seekp(longStringsStart+sizeof(header));
+
+  // write the actual offsets
+  fileStream.write((const char*) &offsets[0], n*sizeof(uint64_t));
+
+  return longStringsStart - moduleSectionStart;
+}
+
+uint64_t LibraryFileWriter::writeLocations(const uast::Module* mod,
+                                           uint64_t moduleSectionStart) {
+  uint64_t locationsSectionStart = fileStream.tellp();
+
+  // TODO: actually write the locations
+
+  return locationsSectionStart - moduleSectionStart;
+}
+
+bool LibraryFileWriter::writeAllSections() {
+  gatherTopLevelModules();
+  openFile();
+  writeHeader();
+
+  std::vector<uint64_t> moduleSectionOffsets;
+
+  for (auto mod : topLevelModules) {
+    // write the module section & update the header's table
+    uint64_t offset = writeModuleSection(mod);
+    moduleSectionOffsets.push_back(offset);
+  }
+
+  // update the module section table
+  // seek just after the fixed portion of the file header
+  fileStream.seekp(sizeof(FileHeader));
+  // write the offsets
+  fileStream.write((const char*) &moduleSectionOffsets[0],
+                   sizeof(uint64_t)*moduleSectionOffsets.size());
+
+  // compute and store the file hash
+  CHPL_ASSERT(sizeof(HashFileResult) == HASH_SIZE);
+
+  // flush the file data so the 'hashFile' call will read everything
+  // we just wrote
+  fileStream.flush();
+
+  llvm::ErrorOr<HashFileResult> hashOrErr = hashFile(outputFilePath);
+
+  if (hashOrErr) {
+    const HashFileResult& h = hashOrErr.get();
+    // use the minimum of HASH_SIZE and sizeof(HashFileResult)
+    // (should get assert above in case these stop matching, but
+    //  that will be off in some builds)
+    size_t n = sizeof(HashFileResult);
+    if (HASH_SIZE < n) n = HASH_SIZE;
+    // seek to the hash position within the file
+    fileStream.seekp(offsetof(FileHeader, hash));
+    // write the hash bytes
+    fileStream.write((const char*) &h, n);
+  }
+
+  // TODO: if further error information is desired, probably best
+  // to switch to using the C FILE* and fwrite.
+  bool ok = fileStream.good();
+  fileStream.close();
+
+  return ok;
+}
+
+
+} // end namespace libraries
+} // end namespace chpl
