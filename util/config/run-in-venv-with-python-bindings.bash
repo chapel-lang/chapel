e0a99bba5135457248a46482a957b88c9ac5a0c4
Danila Fedorin <daniel.fedorin@hpe.com>
2023-10-30 12:07:50 -0700

    Switch Python runner script to detect chapel folder
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

--- util/config/run-in-venv-with-python-bindings.bash
+++ util/config/run-in-venv-with-python-bindings.bash
@@ -7,7 +7,7 @@ CWD=$(cd $(dirname $0) ; pwd)
 # Perform checks and set up environment variables for virtual env.
 source $CWD/run-in-venv-common.bash
 
-if [ ! -f "$chpldeps/chapel.egg-link" ]; then
+if [ ! -d "$chpldeps/chapel" ]; then
   echo "chapel python bindings not built - try make chapel-py-venv " 1>&2
   exit 1
 fi
