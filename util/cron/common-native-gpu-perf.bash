580e7b264867dae24d7e4895c8c0046e9555a08e
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2023-10-31 12:38:37 -0700

    Make adjustments to make things more aligned with other performance testing
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- util/cron/common-native-gpu-perf.bash
+++ util/cron/common-native-gpu-perf.bash
@@ -2,9 +2,7 @@
 #
 # Common GPU performance testing configs
 
-source $CWD/common-perf.bash
-
+# set this before sourcing common-perf
+export CHPL_TEST_PERF_SUBDIR='gpu'
 
 nightly_args="${nightly_args} -performance -perflabel gpu- -numtrials 5"
-export CHPL_TEST_PERF_SUBDIR='gpu'
-export CHPL_TEST_PERF_CONFIGS="aod:v,um"  # v: visible by def
--- util/cron/test-perf.gpu-rocm.bash
+++ util/cron/test-perf.gpu-rocm.bash
@@ -6,18 +6,22 @@ CWD=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
 source $CWD/common-slurm-gasnet-cray-cs.bash
 source $CWD/common-native-gpu.bash
 
-export CHPL_GPU=amd
-export CHPL_GPU_ARCH=gfx906
-export CHPL_LLVM=bundled
-export CHPL_COMM=none
-export CHPL_LAUNCHER_PARTITION=amdMI60
 module load rocm
 
-
-source $CWD/common-native-gpu-perf.bash
-export CHPL_TEST_PERF_CONFIG_NAME="1-node-mi60"
+export CHPL_GPU=amd
+export CHPL_LAUNCHER_PARTITION=amdMI60
+export CHPL_GPU_ARCH=gfx906
+export CHPL_COMM=none
+export CHPL_LLVM=bundled
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.gpu-rocm"
-unset CHPL_TEST_PERF_CONFIGS      # not applicable for this config
+
+export CHPL_TEST_PERF_CONFIG_NAME="1-node-mi60"
+source $CWD/common-native-gpu-perf.bash
+# make sure this comes after setting SUBDIR (set by native-gpu-perf) and
+# CONFIG_NAME
+source $CWD/common-perf.bash
+
 
 nightly_args="${nightly_args} -startdate 07/20/23"
+
 $CWD/nightly -cron ${nightly_args}
