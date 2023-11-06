dcdfaa0c7e328f0421cca01c5c95248132e78210
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2023-11-06 10:42:01 -0800

    Remove host name
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- util/cron/common-hpe-cray-ex.bash
+++ util/cron/common-hpe-cray-ex.bash
@@ -4,11 +4,11 @@ CWD=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
 
 module unload $(module --terse list 2>&1 | grep PrgEnv-)
 
-# PrgEnv-gnu/8.5.0 currently has a problem on pinoak
+# PrgEnv-gnu/8.5.0 currently has a problem on the Cray EX we run on
 module load PrgEnv-gnu/8.4.0
 module load cray-pmi
 
-# cray-libsci currently has a problem on pinoak
+# cray-libsci currently has a problem on the Cray EX we run on
 module unload cray-libsci
 
 export CHPL_HOST_PLATFORM=hpe-cray-ex
