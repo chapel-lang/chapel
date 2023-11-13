2fdc6a5b8efe1aa2d69ed1fcd806553ae9a450b8
Danila Fedorin <daniel.fedorin@hpe.com>
2023-11-13 15:38:03 -0800

    Add some comments to python-types.h
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

--- tools/chapel-py/src/python-types.h
+++ tools/chapel-py/src/python-types.h
@@ -23,9 +23,25 @@
 #include "core-types.h"
 #include "iterator-support.h"
 
+/* The general idea here is the following: we want to add an allowlist
+   of C++ types that have defined conversions to and from Python types.
+   We can then use this information to save some boilerplate when generating
+   methods (the method author can accept and return the C++ types and we'll
+   take care of converting them to/from Python), as well as to generate
+   documentation (we know the Python type string for each allowed C++ type).
+
+   To achieve this 'allow list', we use template specialization. By default,
+   the PythonReturnTypeInfo struct has no fields, so it would issue a compilation
+   error if we tried to access the `wrap` or `unwrap` method (as would happen
+   if someone tried to add a method that uses an unsupported type). However,
+   we also specialize the template for types that we do support, providing
+   the Python type string and `wrap`/`unwrap`. */
+
 template <typename CppType>
 struct PythonReturnTypeInfo {};
 
+/** This macro is used to help define template specializations for PythonReturnTypeInfo.
+    It just hides some of the boilerplate. */
 #define DEFINE_RETURN_TYPE(TYPE, TYPESTR, WRAP, UNWRAP) \
   template <> \
   struct PythonReturnTypeInfo<TYPE> { \
@@ -41,6 +57,11 @@ struct PythonReturnTypeInfo {};
   \
   }
 
+/* Invoke the DEFINE_RETURN_TYPE macro for each type we want to support.
+   New types should be added here. We might consider performing these invocations
+   using X-macros for the entire AST class hierarchy if we wanted to be
+   more specific. */
+
 DEFINE_RETURN_TYPE(bool, "bool", Py_BuildValue("b", TO_WRAP), PyLong_AsLong(TO_UNWRAP));
 DEFINE_RETURN_TYPE(int, "int", Py_BuildValue("i", TO_WRAP), PyLong_AsLong(TO_UNWRAP));
 DEFINE_RETURN_TYPE(const char*, "str", Py_BuildValue("s", TO_WRAP), PyUnicode_AsUTF8(TO_UNWRAP));
@@ -48,6 +69,12 @@ DEFINE_RETURN_TYPE(chpl::UniqueString, "str", Py_BuildValue("s", TO_WRAP.c_str()
 DEFINE_RETURN_TYPE(std::string, "str", Py_BuildValue("s", TO_WRAP.c_str()), std::string(PyUnicode_AsUTF8(TO_UNWRAP)));
 DEFINE_RETURN_TYPE(const chpl::uast::AstNode*, "AstNode", wrapAstNode(CONTEXT, TO_WRAP), ((AstNodeObject*) TO_UNWRAP)->astNode);
 DEFINE_RETURN_TYPE(IterAdapterBase*, "Iterator[AstNode]", wrapIterAdapter(CONTEXT, TO_WRAP), ((AstIterObject*) TO_UNWRAP)->iterAdapter);
+
+/* In the `method-tables.h` file, we encode a method signature using the C++
+   function type in the form `R(Args...)`. This template, PythonFnHelper, is
+   used to extract information back out of this function type, including
+   a full argument unwrapping function that applies the corresponding 'unwrap'
+   call to each element of the argument tuple. */
 
 template<typename T> struct PythonFnHelper{};
 template<typename R, typename ...Args>
