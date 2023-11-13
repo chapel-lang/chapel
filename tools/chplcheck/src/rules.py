4fec8b41d8470b118558b0543a52ef04aedf3f3e
Brandon Neth <brandon.neth@hpe.com>
2023-11-13 11:05:36 -0700

    linter rule and updated file names
    Signed-off-by: Brandon Neth <brandon.neth@hpe.com>

--- tools/chplcheck/src/rules.py
+++ tools/chplcheck/src/rules.py
@@ -72,7 +72,17 @@ def register_rules(driver):
                 return False
             parent = parent.parent()
         return True
-
+    
+    @driver.basic_rule(Record)
+    @driver.basic_rule(Class)
+    def MethodsAfterFields(context, node):
+        method_seen = False
+        for child in node:
+            if isinstance(child, VarLikeDecl) and method_seen:
+                return False
+            if isinstance(child, Function):
+                method_seen = True
+        return True
     @driver.basic_rule([Conditional, BoolLiteral, chapel.rest])
     def BoolLitInCondStmt(context, node):
         return False
