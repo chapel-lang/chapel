99ba2f8ba4992d87ee6f470c0f6251b337a25fe3
arezaii <ahmad.rezaii@hpe.com>
2023-11-29 14:13:40 -0700

    add test for disambiguation behavior
    Signed-off-by: arezaii <ahmad.rezaii@hpe.com>

--- frontend/test/resolution/testDisambiguation.cpp
+++ frontend/test/resolution/testDisambiguation.cpp
@@ -266,19 +266,6 @@ static void test3() {
       )"""",
     1);
 
-  // updated disambiguation based on width of argument
-  // checkCalledIndex(context,
-  //     R""""(
-  //       proc f(arg: int(8)) { }                   // 1
-  //       proc f(arg: uint(64)) { }                 // 2
-  //       var x: int(64);
-  //       f(x);
-  //     )"""",
-  //   2); // The answer should be 2 once disambiguation changes are made to
-  //          // match prod, but today it doesn't run because the helper
-  //          // checkCalledIndex requires just one fnCall to disambiguate
-  //          // and this test has four
-
   // updated disambiguation based on visibility of proc
   checkCalledIndex(context,
       R""""(
@@ -293,11 +280,67 @@ static void test3() {
     2);
 }
 
+// updated disambiguation based on width of argument
+static void test4() {
+  Context ctx;
+  Context* context = &ctx;
+
+  std::string program =
+    R""""(
+      proc f(arg: int(8)) { }                   // 1
+      proc f(arg: uint(64)) { }                 // 2
+      var x: int(64);
+      f(x);
+    )"""";
+
+  auto name = std::string("test4.chpl");
+
+  printf("%s\n", name.c_str());
+
+  auto path = UniqueString::get(context, name);
+
+  setFileText(context, path, program);
+
+  const ModuleVec& vec = parseToplevel(context, path);
+  assert(vec.size() == 1);
+  const Module* m = vec[0]->toModule();
+  assert(m);
+
+  // gather the Function and FnCalls
+  GatherStuff stuff;
+  m->traverse(stuff);
+
+  assert(stuff.fns.size() > 1); // > 1 needed for disambiguation
+  assert(stuff.fnCalls.size() == 4); // we have 3 calls defining types prior to
+                                      // the call to f that we care about
+
+  const ResolutionResultByPostorderID& rr = resolveModule(context, m->id());
+  auto fnCall = stuff.fnCalls[3];
+  assert(fnCall->calledExpression()->toIdentifier()->name().compare("f") == 0);
+  const ResolvedExpression& re = rr.byAst(fnCall);
+  const MostSpecificCandidates& s = re.mostSpecific();
+
+  // disambiguation tests should either have a best candidate or be ambiguous.
+  assert (!s.isEmpty());
+
+  int foundOnlyIdx = 0;
+  int i = 1;
+  for (auto fn : stuff.fns) {
+    if (s.only() && s.only().fn()->id() == fn->id()) {
+      foundOnlyIdx = i;
+    }
+    i++;
+  }
+  // should pick the uint(64) overload
+  assert(foundOnlyIdx == 2);
+}
+
 int main() {
 
   test1();
   test2();
   test3();
+  test4();
 
   return 0;
 }
