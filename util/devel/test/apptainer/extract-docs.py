a3f54f526a55d5c8773a1b0da87db3d18db43584
Michael Ferguson <mppf@users.noreply.github.com>
2023-12-04 10:38:46 -0500

    Don't skip Fedora versions since we will have a compatability note
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

--- util/devel/test/apptainer/extract-docs.py
+++ util/devel/test/apptainer/extract-docs.py
@@ -145,9 +145,6 @@ for d in directories:
         if "nix" in subpath:
             continue # skip these configurations
                      # (not sure how useful this is)
-        if ("fedora-38" in subpath or
-            "fedora-39" in subpath):
-            continue # skip due to not having working LLVM dependency right now
         if "generic-x32-debian11" in subpath:
             continue # skip this one, redudant with other debian ones
 
