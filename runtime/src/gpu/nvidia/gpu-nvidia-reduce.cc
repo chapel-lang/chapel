2be0e25fb2bdfe264c656fa1b6bb69cff577912b
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2023-12-04 11:56:33 -0800

    Use single arch and a wrapper for NVIDIA reduction support
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- runtime/src/gpu/nvidia/gpu-nvidia-reduce.cc
+++ runtime/src/gpu/nvidia/gpu-nvidia-reduce.cc
@@ -31,6 +31,13 @@
 // the implementation in the rest of the runtime and the modules more
 // straightforward.
 #define DEF_ONE_REDUCE_RET_VAL(cub_kind, chpl_kind, data_type) \
+static void __cub_val_##cub_kind##_##data_type(void* temp, size_t temp_bytes, \
+                                           data_type* data, data_type* result, \
+                                           int n, CUstream stream) { \
+  cub::DeviceReduce::cub_kind(temp, temp_bytes, data, (data_type*)result, n,\
+                              (CUstream)stream); \
+}\
+\
 void chpl_gpu_impl_##chpl_kind##_reduce_##data_type(data_type* data, int n,\
                                                     data_type* val, int* idx,\
                                                     void* stream) {\
@@ -38,11 +45,10 @@ void chpl_gpu_impl_##chpl_kind##_reduce_##data_type(data_type* data, int n,\
   CUDA_CALL(cuMemAlloc(&result, sizeof(data_type))); \
   void* temp = NULL; \
   size_t temp_bytes = 0; \
-  cub::DeviceReduce::cub_kind(temp, temp_bytes, data, (data_type*)result, n,\
-                              (CUstream)stream); \
+  __cub_val_##cub_kind##_##data_type(temp, temp_bytes, data, (data_type*)result, n, \
+                           (CUstream)stream); \
   CUDA_CALL(cuMemAlloc(((CUdeviceptr*)&temp), temp_bytes)); \
-  cub::DeviceReduce::cub_kind(temp, temp_bytes, data, (data_type*)result, n,\
-                              (CUstream)stream); \
+  __cub_val_##cub_kind##_##data_type(temp, temp_bytes, data, (data_type*)result, n, (CUstream)stream); \
   CUDA_CALL(cuMemcpyDtoHAsync(val, result, sizeof(data_type),\
                               (CUstream)stream)); \
   CUDA_CALL(cuMemFree(result)); \
@@ -54,22 +60,29 @@ GPU_IMPL_REDUCE(DEF_ONE_REDUCE_RET_VAL, Max, max)
 
 #undef DEF_ONE_REDUCE_RET_VAL
 
+template <typename data_type>
+using kvp = cub::KeyValuePair<int, data_type>;
+
 #define DEF_ONE_REDUCE_RET_VAL_IDX(cub_kind, chpl_kind, data_type) \
+\
+static void __cub_val_idx_##cub_kind##_##data_type(void* temp, size_t temp_bytes, data_type* data, \
+                         kvp<data_type>* result, int n, CUstream stream) { \
+  cub::DeviceReduce::cub_kind(temp, temp_bytes, data, (kvp<data_type>*)result, n,\
+                              (CUstream)stream);\
+} \
+\
 void chpl_gpu_impl_##chpl_kind##_reduce_##data_type(data_type* data, int n,\
                                                     data_type* val, int* idx,\
                                                     void* stream) {\
-  using kvp = cub::KeyValuePair<int,data_type>; \
   CUdeviceptr result; \
-  CUDA_CALL(cuMemAlloc(&result, sizeof(kvp))); \
+  CUDA_CALL(cuMemAlloc(&result, sizeof(kvp<data_type>))); \
   void* temp = NULL; \
   size_t temp_bytes = 0; \
-  cub::DeviceReduce::cub_kind(temp, temp_bytes, data, (kvp*)result, n,\
-                              (CUstream)stream);\
+  __cub_val_idx_##cub_kind##_##data_type(temp, temp_bytes, data, (kvp<data_type>*)result, n, (CUstream)stream);\
   CUDA_CALL(cuMemAlloc(((CUdeviceptr*)&temp), temp_bytes)); \
-  cub::DeviceReduce::cub_kind(temp, temp_bytes, data, (kvp*)result, n,\
-                              (CUstream)stream);\
-  kvp result_host; \
-  CUDA_CALL(cuMemcpyDtoHAsync(&result_host, result, sizeof(kvp),\
+  __cub_val_idx_##cub_kind##_##data_type(temp, temp_bytes, data, (kvp<data_type>*)result, n, (CUstream)stream);\
+  kvp<data_type> result_host; \
+  CUDA_CALL(cuMemcpyDtoHAsync(&result_host, result, sizeof(kvp<data_type>),\
                               (CUstream)stream)); \
   *val = result_host.value; \
   *idx = result_host.key; \
