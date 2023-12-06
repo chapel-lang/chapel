79fbc96d07e83e0e7a0f28e795cdf5d4b1a69880
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2023-12-05 18:06:10 -0600

    Adjust ROCM_VERSION_MAJOR implementation
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- runtime/src/gpu/common/rocm-version.h
+++ runtime/src/gpu/common/rocm-version.h
@@ -30,6 +30,11 @@
 #include <rocm/rocm_version.h>
 #elif __has_include(<rocm_version.h>)  // Deprecated. 5.x used to want this
 #include <rocm_version.h>
-#elif !defined(ROCM_VERSION_MAJOR)
-#define ROCM_VERSION_MAJOR 4 // this is the safe bet
+#endif
+
+// we should have found the correct header by now. But if not, we set
+// ROCM_VERSION_MAJOR to 4 as it is the lowest version we support and has fewer
+// runtime features enabled. So, it is a safer choice.
+#if !defined(ROCM_VERSION_MAJOR)
+#define ROCM_VERSION_MAJOR 4
 #endif
