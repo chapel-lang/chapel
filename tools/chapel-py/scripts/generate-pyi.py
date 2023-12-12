088112d95d27fa9b873d1a0508eecf282980615b
Jade Abraham <jade.abraham@hpe.com>
2023-12-11 16:06:16 -0800

    refactor `_wrap_method`
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- tools/chapel-py/scripts/generate-pyi.py
+++ tools/chapel-py/scripts/generate-pyi.py
@@ -28,153 +28,213 @@ def _get_base_header() -> str:
     return c._get_pyi_file()
 
 
-def _wrap_docstring(docstring: str, indent: int = 8) -> List[str]:
-    spaces = " " * indent
-    triple_quote = spaces + '"""'
-    return [triple_quote, spaces + docstring, triple_quote]
+def _section(*args: str | List[str], indent: int = 4) -> List[str]:
+    temp = []
+    for arg in args:
+        if not isinstance(arg, list):
+            temp.append(arg)
+        else:
+            temp.extend(arg)
+    return [(" " * indent + s).rstrip() for s in temp]
 
 
-def _def_body(indent: int = 8) -> List[str]:
-    spaces = " " * indent
-    return [spaces + "..."]
+def _wrap_docstring(docstring: str) -> List[str]:
+    return ['"""', docstring, '"""']
 
 
 def _wrap_method(
     name: str,
     args: List[Tuple[str, str | None]] = [],
     rettype: str = "None",
-    indent: int = 4,
+    docstring: str | None = None,
 ) -> List[str]:
-    spaces = " " * indent
     argstr = ""
     for a, t in args:
         typestr = f": {t}" if t else ""
         argstr += f", {a}{typestr}"
-    return [spaces + f"def {name}(self{argstr}) -> {rettype}:"]
+    methodbody = []
+    if docstring:
+        methodbody.extend(_wrap_docstring(docstring))
+    methodbody.append("...")
+    methodbody.append("")
+    return _section(
+        f"def {name}(self{argstr}) -> {rettype}:", _section(methodbody), indent=0
+    )
 
 
 def get_Context_header() -> str:
-    s = [
+    s = _section(
         "class Context:",
-        *_wrap_method("parse", args=[("path", "str")], rettype="List[AstNode]"),
-        *_wrap_docstring("Parse a top-level AST node from the given file"),
-        *_def_body(),
-        "",
-        *_wrap_method("is_bundled_path", args=[("path", "str")], rettype="bool"),
-        *_wrap_docstring(
-            "Check if the given file path is within the bundled (built-in) Chapel files"
+        _section(
+            _wrap_method(
+                "parse",
+                args=[("path", "str")],
+                rettype="List[AstNode]",
+                docstring="Parse a top-level AST node from the given file",
+            )
         ),
-        *_def_body(),
-        "",
-        *_wrap_method("advance_to_next_revision"),
-        *_wrap_docstring("Advance the context to the next revision"),
-        *_def_body(),
-        "",
-        *_wrap_method("track_errors", rettype="ErrorManager"),
-        *_wrap_docstring("Generate a stub file for the Chapel AST nodes"),
-        *_def_body(),
-        "",
-        *_wrap_method("_get_pyi_file", rettype="str"),
-        *_wrap_docstring("Generate a basic pyi stub file"),
-        *_def_body(),
-        "",
-    ]
+        _section(
+            _wrap_method(
+                "is_bundled_path",
+                args=[("path", "str")],
+                rettype="bool",
+                docstring="Check if the given file path is within the bundled (built-in) Chapel files",
+            )
+        ),
+        _section(
+            _wrap_method(
+                "advance_to_next_revision",
+                docstring="Advance the context to the next revision",
+            )
+        ),
+        _section(
+            _wrap_method(
+                "track_errors",
+                rettype="ErrorManager",
+                docstring="Generate a stub file for the Chapel AST nodes",
+            )
+        ),
+        _section(
+            _wrap_method(
+                "_get_pyi_file",
+                rettype="str",
+                docstring="Generate a basic pyi stub file",
+            )
+        ),
+        indent=0,
+    )
     return "\n".join(s)
 
 
 def get_Location_header() -> str:
-    s = [
+    s = _section(
         "class Location:",
-        *_wrap_method("start", rettype="int"),
-        *_wrap_docstring("Get the start of a Location object"),
-        *_def_body(),
-        "",
-        *_wrap_method("end", rettype="int"),
-        *_wrap_docstring("Get the end of a Location object"),
-        *_def_body(),
-        "",
-        *_wrap_method("path", rettype="str"),
-        *_wrap_docstring("Get the path of a Location object"),
-        *_def_body(),
-        "",
-    ]
+        _section(
+            _wrap_method(
+                "start", rettype="int", docstring="Get the start of a Location object"
+            ),
+        ),
+        _section(
+            _wrap_method(
+                "end", rettype="int", docstring="Get the end of a Location object"
+            ),
+        ),
+        _section(
+            _wrap_method(
+                "path", rettype="str", docstring="Get the path of a Location object"
+            ),
+        ),
+        indent=0,
+    )
     return "\n".join(s)
 
 
 def get_ErrorManager_header() -> str:
-    s = [
+    s = _section(
         "class ErrorManager:",
-        *_wrap_method("__enter__", rettype="List[Error]"),
-        *_def_body(),
-        "",
-        *_wrap_method("__exit__", args=[("*args", None)]),
-        *_def_body(),
-        "",
-    ]
+        _section(_wrap_method("__enter__", rettype="List[Error]")),
+        _section(_wrap_method("__exit__", args=[("*args", None)])),
+        indent=0,
+    )
     return "\n".join(s)
 
 
 def get_Error_header() -> str:
-    s = [
+    s = _section(
         "class Error:",
-        *_wrap_method("location", rettype="Location"),
-        *_wrap_docstring("Get the location at which this error occurred"),
-        *_def_body(),
-        "",
-        *_wrap_method("message", rettype="str"),
-        *_wrap_docstring("Retrieve the contents of this error message"),
-        *_def_body(),
-        "",
-        *_wrap_method("kind", rettype="str"),
-        *_wrap_docstring(
-            "Retrieve the kind ('error', 'warning') of this type of error"
+        _section(
+            _wrap_method(
+                "location",
+                rettype="Location",
+                docstring="Get the location at which this error occurred",
+            )
         ),
-        *_def_body(),
-        "",
-        *_wrap_method("type", rettype="str"),
-        *_wrap_docstring("Retrieve the unique name of this type of error"),
-        *_def_body(),
-        "",
-    ]
+        _section(
+            _wrap_method(
+                "message",
+                rettype="str",
+                docstring="Retrieve the contents of this error message",
+            )
+        ),
+        _section(
+            _wrap_method(
+                "kind",
+                rettype="str",
+                docstring="Retrieve the kind ('error', 'warning') of this type of error",
+            )
+        ),
+        _section(
+            _wrap_method(
+                "type",
+                rettype="str",
+                docstring="Retrieve the unique name of this type of error",
+            )
+        ),
+        indent=0,
+    )
     return "\n".join(s)
 
 
 def get_AstNode_header() -> str:
-    s = [
+    s = _section(
         "class AstNode:",
-        *_wrap_method("dump"),
-        *_wrap_docstring("Dump the internal representation of the given AST node"),
-        *_def_body(),
-        "",
-        *_wrap_method("tag", rettype="str"),
-        *_wrap_docstring("Get a string representation of the AST node's type"),
-        *_def_body(),
-        "",
-        *_wrap_method("attribute_group", rettype="AttributeGroup | None"),
-        *_wrap_docstring("Get the attribute group, if any, associated with this node"),
-        *_def_body(),
-        "",
-        *_wrap_method("location", rettype="Location"),
-        *_wrap_docstring("Get the location of this AST node in its file"),
-        *_def_body(),
-        "",
-        *_wrap_method("parent", rettype="AstNode | None"),
-        *_wrap_docstring("Get the parent node of this AST node"),
-        *_def_body(),
-        "",
-        *_wrap_method("pragmas", rettype="Set[str]"),
-        *_wrap_docstring("Get the pragmas of this AST node"),
-        *_def_body(),
-        "",
-        *_wrap_method("unique_id", rettype="int"),
-        *_wrap_docstring("Get a unique identifier for this AST node"),
-        *_def_body(),
-        "",
-        *_wrap_method("__iter__", rettype="Iterator[AstNode]"),
-        *_wrap_docstring("Iterate over this AST node's children"),
-        *_def_body(),
-        "",
-    ]
+        _section(
+            _wrap_method(
+                "dump",
+                docstring="Dump the internal representation of the given AST node",
+            )
+        ),
+        _section(
+            _wrap_method(
+                "tag",
+                rettype="str",
+                docstring="Get a string representation of the AST node's type",
+            )
+        ),
+        _section(
+            _wrap_method(
+                "attribute_group",
+                rettype="AttributeGroup | None",
+                docstring="Get the attribute group, if any, associated with this node",
+            )
+        ),
+        _section(
+            _wrap_method(
+                "location",
+                rettype="Location",
+                docstring="Get the location of this AST node in its file",
+            )
+        ),
+        _section(
+            _wrap_method(
+                "parent",
+                rettype="AstNode | None",
+                docstring="Get the parent node of this AST node",
+            )
+        ),
+        _section(
+            _wrap_method(
+                "pragmas",
+                rettype="Set[str]",
+                docstring="Get the pragmas of this AST node",
+            )
+        ),
+        _section(
+            _wrap_method(
+                "unique_id",
+                rettype="int",
+                docstring="Get a unique identifier for this AST node",
+            )
+        ),
+        _section(
+            _wrap_method(
+                "__iter__",
+                rettype="Iterator[AstNode]",
+                docstring="Iterate over this AST node's children",
+            )
+        ),
+        indent=0,
+    )
     return "\n".join(s)
 
 
