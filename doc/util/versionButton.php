67133f54ec6e7d58cc9c9c6d934143ca8d17c5f5
Brad Chamberlain <bradcray@users.noreply.github.com>
2023-12-14 14:46:56 -0800

    Bump docs version menu to reflect today's release
    ---
    Signed-off-by: Brad Chamberlain <bradcray@users.noreply.github.com>

--- doc/util/versionButton.php
+++ doc/util/versionButton.php
@@ -87,8 +87,8 @@ if (pagePath == "") {
   pagePath = "index";
 }
 function dropSetup() {
-  var currentRelease = "1.32"; // what does the public have?
-  var stagedRelease = "1.33";  // is there a release staged but not yet public?
+  var currentRelease = "1.33"; // what does the public have?
+  var stagedRelease = "1.34";  // is there a release staged but not yet public?
   var nextRelease = "1.34";    // what's the next release? (on docs/main)
   var button = document.getElementById("versionButton");
   // Uses unicode down-pointing triangle
