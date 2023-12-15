55e953c2abe5d72a8164d36030d0d2f3e37c034f
Jade Abraham <jade.abraham@hpe.com>
2023-12-15 15:13:50 -0800

    fix docstring
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- tools/chapel-py/src/method-tables.h
+++ tools/chapel-py/src/method-tables.h
@@ -187,7 +187,7 @@ CLASS_BEGIN(Import)
 CLASS_END(Import)
 
 CLASS_BEGIN(Include)
-  PLAIN_GETTER(Include, name, "Get the visibility of this Include node",
+  PLAIN_GETTER(Include, name, "Get the name of this Include node",
                const char*, return node->name())
   PLAIN_GETTER(Include, is_prototype, "Check if this Include node is for a prototype module",
                bool, return node->isPrototype())
