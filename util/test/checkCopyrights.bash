a7681d264969ae65fef2c86ef4a31fd94b145c33
Anna Rift <anna.rift@hpe.com>
2024-01-02 10:06:41 -0800

    Clarify hardcoded year in checkCopyrights.bash is a placeholder
    Updated comment and changed placeholder value to 1970 so it's more
    obvious this doesn't need to be updated.
    
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/test/checkCopyrights.bash
+++ util/test/checkCopyrights.bash
@@ -21,9 +21,9 @@
 set -e
 
 todate=`date "+%m%d"`
-# This hardcoding is intentional. Date accepts a full date like this,
-# but %m%d cuts off the year so it only compares 0107 to the current date
-cond=`date -d 2024-01-07 "+%m%d"`
+# The hardcoded year is just a placeholder. Date accepts a full date like this,
+# but %m%d cuts off the year so it only compares 0107 to the current date.
+cond=`date -d 1970-01-07 "+%m%d"`
 
 if [ $todate -le $cond ];
 then
