8bd874ba0b694ade24aa0f9ad96cb292b5cd7501
Anna Rift <anna.rift@hpe.com>
2024-01-02 10:34:25 -0800

    Fix some bash mistakes
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/test/updateCopyrights.bash
+++ util/test/updateCopyrights.bash
@@ -10,24 +10,24 @@ EXCLUDE_PATHS="':!third-party/' ':!test/'"
 
 
 # Read in and validate input years
-if [[ $# -ne 1 ]]; then
+if [[ $# -ne 2 ]]; then
   echo "Usage: $0 [previous_year] [current_year]"
   exit 1
 fi
 PREVIOUS_YEAR=$1
 CURRENT_YEAR=$2
-if [ "$CURRENT_YEAR" != `date '+%Y'`]; then
-  echo "Error: Specified current year $CURRENT_YEAR does not appear to be the actual current year"
+if [ "$CURRENT_YEAR" != `date '+%Y'` ]; then
+  echo "Specified current year $CURRENT_YEAR does not appear to be the actual current year"
   exit 1
 fi
-if [ $(PREVIOUS_YEAR + 1) != $(CURRENT_YEAR)]; then
-  echo "Error: Current year $CURRENT_YEAR is not exactly 1 greater than previous year $PREVIOUS_YEAR"
+if [[ $(($PREVIOUS_YEAR + 1)) != $CURRENT_YEAR ]]; then
+  echo "Current year $CURRENT_YEAR is not exactly 1 greater than previous year $PREVIOUS_YEAR"
   exit 1
 fi
 
 
 # Work from $CHPL_HOME for exclude dir paths
-if [[ ! -v $CHPL_HOME]]; then
+if [ -z $CHPL_HOME ]; then
   echo "CHPL_HOME not set"
   exit 1
 fi
@@ -36,7 +36,7 @@ cd $CHPL_HOME
 
 
 # Require working from clean repo for safety.
-if [ ! -z `git status -u --porcelain` ]; then
+if [ -n "$(git status -u --porcelain)" ]; then
   echo "Refusing to run from unclean working directory for safety"
   exit 1
 fi
@@ -51,21 +51,25 @@ function tryReplacementForPattern {
   SED_PATTERN=$2
 
   # Error if a pattern has no matches (and therefore wouldn't result in any updates).
-  if ! `git grep -l $SEARCH_PATTERN -- $EXCLUDE_PATHS`; then
-    echo "No matches found for pattern $SEARCH_PATTERN, stopping with no further replacement. Script may need to be updated"
+  cmd="PAGER=cat git grep -l \""$SEARCH_PATTERN"\" -- $EXCLUDE_PATHS"
+  search_output=$(eval "$cmd")
+  if [[ -z "$search_output" ]]; then
+    echo "No matches found for pattern '$SEARCH_PATTERN', stopping with no further replacement. Script may need to be updated"
     exit 1
   fi
 
   # Perform replacement
-  git grep -l $SEARCH_PATTERN -- $EXCLUDE_PATHS | xargs sed -i '' -e $SED_PATTERN
+  eval $cmd | xargs sed -i '' -e "$SED_PATTERN"
+  # git grep -l "$SEARCH_PATTERN" -- $EXCLUDE_PATHS | xargs sed -i '' -e "$SED_PATTERN"
 
-  echo "Did replacements for pattern $SEARCH_PATTERN"
+  echo "Did replacements for pattern '$SEARCH_PATTERN' with sed set input '$SED_PATTERN'"
 }
 
 # Replace different cases for the variations of copyright notice we have.
 # These patterns will need to be updated if how we write copyright notices does.
 # Case: "Copyright 202x-[previous year]"->"Copyright 202x-[current year]"
-tryReplacementForPattern "Copyright 202[0-9]-$PREVIOUS_YEAR" "s/Copyright (202[0-9])-$PREVIOUS_YEAR/Copyright \1-$CURRENT_YEAR/g"
+echo "about to do replacements"
+tryReplacementForPattern "Copyright 202[0-9]-$PREVIOUS_YEAR" "s/Copyright \(202[0-9]\)-$PREVIOUS_YEAR/Copyright \1-$CURRENT_YEAR/g"
 # Case: "Copyright [previous year]"->"Copyright [previous year]-[current year]"
 tryReplacementForPattern "Copyright $PREVIOUS_YEAR" "s/Copyright $PREVIOUS_YEAR/Copyright $PREVIOUS_YEAR-$CURRENT_YEAR/g"
 # Case: "copyright_year = [previous year]"->"copyright_year = [current year]"
