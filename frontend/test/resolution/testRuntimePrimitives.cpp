ae3c8313ba8f6cad4944d1f5eee451efdd9cc036
Danila Fedorin <daniel.fedorin@hpe.com>
2024-01-02 12:25:40 -0800

    Use some helper functions to simplify testRuntimePrimitives.cpp
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

--- frontend/test/resolution/testRuntimePrimitives.cpp
+++ frontend/test/resolution/testRuntimePrimitives.cpp
@@ -21,303 +21,179 @@
 
 #include "chpl/types/all-types.h"
 
-
-static void voidTypeTestHelper(std::string program) {
+template <typename F>
+static void predicatePrimTypeHelper(const char* primName, std::vector<const char*> args,
+                                    F&& predicate,
+                                    const char* prelude = "",
+                                    QualifiedType::Kind expectedKind = QualifiedType::CONST_VAR) {
+  std::string program = prelude;
+  program += "var x = __primitive(\"";
+  program += primName;
+  program += "\"";
+  for (auto arg : args) {
+    program += ", ";
+    program += arg;
+  }
+  program += ");";
   Context ctx;
   auto context = &ctx;
-  QualifiedType qt =  resolveTypeOfXInit(context, program);
-  assert(qt.kind() == QualifiedType::CONST_VAR);
+  QualifiedType qt =  resolveTypeOfXInit(context, std::move(program));
+  assert(qt.kind() == expectedKind);
   auto typePtr = qt.type();
   assert(typePtr);
-  assert(typePtr->isVoidType());
+  assert(predicate(typePtr, qt.param()));
+}
+
+template <typename T>
+static void primTypeHelper(const char* primName, std::vector<const char*> args, const char* prelude = "", QualifiedType::Kind expectedKind = QualifiedType::CONST_VAR) {
+  predicatePrimTypeHelper(primName, args, [](const Type* typePtr, const Param* param) {
+    return typePtr->is<T>();
+  }, prelude, expectedKind);
+}
+
+static void intPrimTypeHelper(int width, const char* primName, std::vector<const char*> args, const char* prelude = "", QualifiedType::Kind expectedKind = QualifiedType::CONST_VAR) {
+  predicatePrimTypeHelper(primName, args, [width](const Type* typePtr, const Param* param) {
+    return typePtr->isIntType() && typePtr->toIntType()->bitwidth() == width;
+  }, prelude, expectedKind);
+}
+
+static void realPrimTypeHelper(int width, const char* primName, std::vector<const char*> args, const char* prelude = "", QualifiedType::Kind expectedKind = QualifiedType::CONST_VAR) {
+  predicatePrimTypeHelper(primName, args, [width](const Type* typePtr, const Param* param) {
+    return typePtr->isRealType() && typePtr->toRealType()->bitwidth() == width;
+  }, prelude, expectedKind);
 }
 
 // tests for primitives that should return void
 static void testVoidPrims() {
   // test for primitive "chpl_init_record"
-  voidTypeTestHelper(R"""(var x = __primitive("chpl_init_record");)""");
+  primTypeHelper<VoidType>("chpl_init_record", {});
 
   // test for primitive "chpl_comm_get"
-  voidTypeTestHelper(R"""(var x = __primitive("chpl_comm_get");)""");
+  primTypeHelper<VoidType>("chpl_comm_get", {});
 
   // test for primitive "chpl_comm_put"
-  voidTypeTestHelper(R"""(var x = __primitive("chpl_comm_put");)""");
+  primTypeHelper<VoidType>("chpl_comm_put", {});
 
   // test for primitive "chpl_comm_array_get"
-  voidTypeTestHelper(R"""(var x = __primitive("chpl_comm_array_get");)""");
+  primTypeHelper<VoidType>("chpl_comm_array_get", {});
 
   // test for primitive "chpl_comm_array_put"
-  voidTypeTestHelper(R"""(var x = __primitive("chpl_comm_array_put");)""");
+  primTypeHelper<VoidType>("chpl_comm_array_put", {});
 
   // test for primitive "chpl_comm_remote_prefetch"
-  voidTypeTestHelper(R"""(var x = __primitive("chpl_comm_remote_prefetch");)""");
+  primTypeHelper<VoidType>("chpl_comm_remote_prefetch", {});
 
   // test for primitive "chpl_comm_get_strd"
-  voidTypeTestHelper(R"""(var x = __primitive("chpl_comm_get_strd");)""");
+  primTypeHelper<VoidType>("chpl_comm_get_strd", {});
 
   // test for primitive "chpl_comm_put_strd"
-  voidTypeTestHelper(R"""(var x = __primitive("chpl_comm_put_strd");)""");
+  primTypeHelper<VoidType>("chpl_comm_put_strd", {});
 
   // test for primitive "shift_base_pointer"
-  voidTypeTestHelper(R"""(var x = __primitive("shift_base_pointer");)""");
+  primTypeHelper<VoidType>("shift_base_pointer", {});
 
   // test for primitive "array_set"
-  voidTypeTestHelper(R"""(var x = __primitive("array_set");)""");
+  primTypeHelper<VoidType>("array_set", {});
 
   // test for primitive "array_set_first"
-  voidTypeTestHelper(R"""(var x = __primitive("array_set_first");)""");
+  primTypeHelper<VoidType>("array_set_first", {});
 
   // test for primitive "auto destroy runtime type"
-  voidTypeTestHelper(R"""(var x = __primitive("auto destroy runtime type");)""");
+  primTypeHelper<VoidType>("auto destroy runtime type", {});
 
   // test for primitive "create fn type"
-  voidTypeTestHelper(R"""(var x = __primitive("create fn type");)""");
+  primTypeHelper<VoidType>("create fn type", {});
 }
 
 // test that we can handle non-param string for string_length_bytes
 static void test1() {
-  Context ctx;
-  auto context = &ctx;
-  QualifiedType qt =  resolveTypeOfXInit(context,
-                         R""""(
-                           var s:string;
-                           var x = __primitive("string_length_bytes", s);
-                         )"""");
-  assert(qt.kind() == QualifiedType::CONST_VAR);
-  auto typePtr = qt.type();
-  assert(typePtr);
-  assert(typePtr->isIntType());
-  assert(typePtr->toIntType()->bitwidth() == 64);
+  intPrimTypeHelper(IntType::defaultBitwidth(), "string_length_bytes", {"s"}, "var s: string;");
 }
 
 // test that we can handle non-param bytes for string_length_bytes
 static void test2() {
-  Context ctx;
-  auto context = &ctx;
-  QualifiedType qt =  resolveTypeOfXInit(context,
-                         R""""(
-                           var b:bytes;
-                           var x = __primitive("string_length_bytes", b);
-                         )"""");
-  assert(qt.kind() == QualifiedType::CONST_VAR);
-  auto typePtr = qt.type();
-  assert(typePtr);
-  assert(typePtr->isIntType());
-  assert(typePtr->toIntType()->bitwidth() == 64);
+  intPrimTypeHelper(IntType::defaultBitwidth(), "string_length_bytes", {"b"}, "var b: bytes;");
 }
 
 static void test3() {
   // test for primitive return type for get real/imag
-  Context ctx;
-  auto context = &ctx;
-  QualifiedType qt =  resolveTypeOfXInit(context,
-                         R""""(
-                           var a: complex(128);
-                           var x = __primitive("complex_get_real", a);
-                         )"""");
-  assert(qt.kind() == QualifiedType::REF);
-  auto typePtr = qt.type();
-  assert(typePtr);
-  auto realPtr = typePtr->toRealType();
-  assert(realPtr->bitwidth()==64);
+  realPrimTypeHelper(64, "complex_get_real", {"a"}, "var a: complex(128);", QualifiedType::REF);
 }
 
 static void test4() {
   // test for primitive return type for get real/imag
-  Context ctx;
-  auto context = &ctx;
-  QualifiedType qt =  resolveTypeOfXInit(context,
-                         R""""(
-                           var a: complex(64);
-                           var x = __primitive("complex_get_real", a);
-                         )"""");
-  assert(qt.kind() == QualifiedType::REF);
-  auto typePtr = qt.type();
-  assert(typePtr);
-  auto realPtr = typePtr->toRealType();
-  assert(realPtr->bitwidth()==32);
+  realPrimTypeHelper(32, "complex_get_real", {"a"}, "var a: complex(64);", QualifiedType::REF);
 }
 
 static void test5() {
   // test for primitive return type for get real/imag
-  Context ctx;
-  auto context = &ctx;
-  QualifiedType qt =  resolveTypeOfXInit(context,
-                         R""""(
-                           var a: complex(128);
-                           var x = __primitive("complex_get_imag", a);
-                         )"""");
-  assert(qt.kind() == QualifiedType::REF);
-  auto typePtr = qt.type();
-  assert(typePtr);
-  auto realPtr = typePtr->toRealType();
-  assert(realPtr->bitwidth()==64);
+  realPrimTypeHelper(64, "complex_get_imag", {"a"}, "var a: complex(128);", QualifiedType::REF);
 }
 
 static void test6() {
   // test for primitive return type for get real/imag
-  Context ctx;
-  auto context = &ctx;
-  QualifiedType qt =  resolveTypeOfXInit(context,
-                         R""""(
-                           var a: complex(64);
-                           var x = __primitive("complex_get_imag", a);
-                         )"""");
-  assert(qt.kind() == QualifiedType::REF);
-  auto typePtr = qt.type();
-  assert(typePtr);
-  auto realPtr = typePtr->toRealType();
-  assert(realPtr->bitwidth()==32);
+  realPrimTypeHelper(32, "complex_get_imag", {"a"}, "var a: complex(64);", QualifiedType::REF);
 }
 
 static void test7() {
   // test for primitive "is wide pointer", which should return a bool
-  Context ctx;
-  auto context = &ctx;
-  QualifiedType qt =  resolveTypeOfXInit(context,
-                         R""""(
-                           var x = __primitive("is wide pointer");
-                         )"""");
-  assert(qt.kind() == QualifiedType::CONST_VAR);
-  auto typePtr = qt.type();
-  assert(typePtr);
-  assert(typePtr->isBoolType());
+  primTypeHelper<BoolType>("is wide pointer", {});
 }
 
 // test for primitive "_wide_get_addr", which should return a void ptr
 static void test8() {
-  Context ctx;
-  auto context = &ctx;
-  QualifiedType qt =  resolveTypeOfXInit(context,
-                         R""""(
-                           var x = __primitive("_wide_get_addr");
-                         )"""");
-  assert(qt.kind() == QualifiedType::CONST_VAR);
-  auto typePtr = qt.type();
-  assert(typePtr);
-  auto cPtrType = typePtr->toCPtrType();
-  assert(cPtrType);
-  assert(cPtrType->isVoidPtr());
+  predicatePrimTypeHelper("_wide_get_addr", {}, [](const Type* typePtr, const Param* param) {
+    return typePtr->isCPtrType() && typePtr->toCPtrType()->isVoidPtr();
+  });
 }
 
 // test for primitive "steal", which should return the type of the argument
 static void test9() {
-  Context ctx;
-  auto context = &ctx;
-  QualifiedType qt =  resolveTypeOfXInit(context,
-                         R""""(
-                           var a = 1;
-                           var x = __primitive("steal", a);
-                         )"""");
-  assert(qt.kind() == QualifiedType::CONST_VAR);
-  auto typePtr = qt.type();
-  assert(typePtr);
-  assert(typePtr->isIntType());
+  intPrimTypeHelper(IntType::defaultBitwidth(), "steal", {"v"}, "var v: int;");
+  primTypeHelper<BoolType>("steal", {"v"}, "var v: bool;");
+  realPrimTypeHelper(32, "steal", {"v"}, "var v: real(32);");
+  realPrimTypeHelper(64, "steal", {"v"}, "var v: real(64);");
 }
 
 // test for primitive "getcid", which should return an int32
 static void test10() {
-  Context ctx;
-  auto context = &ctx;
-  QualifiedType qt =  resolveTypeOfXInit(context,
-                         R""""(
-                           var x = __primitive("getcid");
-                         )"""");
-  assert(qt.kind() == QualifiedType::CONST_VAR);
-  auto typePtr = qt.type();
-  assert(typePtr);
-  assert(typePtr->isIntType());
-  assert(typePtr->toIntType()->bitwidth() == 32);
+  intPrimTypeHelper(32, "getcid", {});
 }
 
 // test for primitive "get_union_id", which should return a default int
 static void test11() {
-  Context ctx;
-  auto context = &ctx;
-  QualifiedType qt =  resolveTypeOfXInit(context,
-                         R""""(
-                           var x = __primitive("get_union_id");
-                         )"""");
-  assert(qt.kind() == QualifiedType::CONST_VAR);
-  auto typePtr = qt.type();
-  assert(typePtr);
-  assert(typePtr->isIntType());
-  assert(typePtr->toIntType()->bitwidth() == 64);
+  intPrimTypeHelper(IntType::defaultBitwidth(), "get_union_id", {});
 }
-
 
 // "chpl_task_getRequestedSubloc", which should return int 64
 static void test12() {
-  Context ctx;
-  auto context = &ctx;
-  QualifiedType qt =  resolveTypeOfXInit(context,
-                         R"""(var x = __primitive("chpl_task_getRequestedSubloc");)""");
-  assert(qt.kind() == QualifiedType::CONST_VAR);
-  auto typePtr = qt.type();
-  assert(typePtr);
-  assert(typePtr->isIntType());
-  assert(typePtr->toIntType()->bitwidth() == 64);
+  intPrimTypeHelper(64, "chpl_task_getRequestedSubloc", {});
 }
 
 // "class name by id", which should return a cString
 static void test13() {
-  Context ctx;
-  auto context = &ctx;
-  QualifiedType qt =  resolveTypeOfXInit(context,
-                         R"""(var x = __primitive("class name by id");)""");
-  assert(qt.kind() == QualifiedType::CONST_VAR);
-  auto typePtr = qt.type();
-  assert(typePtr);
-  assert(typePtr->isCStringType());
+  primTypeHelper<CStringType>("class name by id", {});
 }
 
 // "ref to string", which should return a cString
 static void test14() {
-  Context ctx;
-  auto context = &ctx;
-  QualifiedType qt =  resolveTypeOfXInit(context,
-                         R"""(var x = __primitive("ref to string");)""");
-  assert(qt.kind() == QualifiedType::CONST_VAR);
-  auto typePtr = qt.type();
-  assert(typePtr);
-  assert(typePtr->isCStringType());
+  primTypeHelper<CStringType>("ref to string", {});
 }
 
 // "chpl_lookupFilename", which should return a cString
 static void test15() {
-  Context ctx;
-  auto context = &ctx;
-  QualifiedType qt =  resolveTypeOfXInit(context,
-                         R"""(var x = __primitive("chpl_lookupFilename");)""");
-  assert(qt.kind() == QualifiedType::CONST_VAR);
-  auto typePtr = qt.type();
-  assert(typePtr);
-  assert(typePtr->isCStringType());
+  primTypeHelper<CStringType>("chpl_lookupFilename", {});
 }
 
 
 // "_get_user_line", which should return a default int.
 static void test16() {
-  Context ctx;
-  auto context = &ctx;
-  QualifiedType qt =  resolveTypeOfXInit(context,
-                         R"""(var x = __primitive("_get_user_line");)""");
-  assert(qt.kind() == QualifiedType::CONST_VAR);
-  auto typePtr = qt.type();
-  assert(typePtr);
-  assert(typePtr->isIntType() && typePtr->toIntType()->isDefaultWidth());
+  intPrimTypeHelper(IntType::defaultBitwidth(), "_get_user_line", {});
 }
 
 // "_get_user_file", which should return an int32
 static void test17() {
-  Context ctx;
-  auto context = &ctx;
-  QualifiedType qt =  resolveTypeOfXInit(context,
-                         R"""(var x = __primitive("_get_user_file");)""");
-  assert(qt.kind() == QualifiedType::CONST_VAR);
-  auto typePtr = qt.type();
-  assert(typePtr);
-  assert(typePtr->isIntType() && typePtr->toIntType()->bitwidth() == 32);
+  intPrimTypeHelper(32, "_get_user_file", {});
 }
 
 int main() {
