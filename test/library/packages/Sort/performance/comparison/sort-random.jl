e89f590de9a0cee044ed1d4c6bcbf5db62b9b3e2
Michael Ferguson <mppf@users.noreply.github.com>
2024-01-04 17:03:38 -0500

    Add Rust and Julia comparisons
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

---
+++ test/library/packages/Sort/performance/comparison/sort-random.jl
@@ -0,0 +1,11 @@
+# run it with
+#   julia sort-random.jl
+
+function testSort(n)
+  v = rand(Int, n)
+  @timed sort!(v);
+end
+
+n = 1024*1024*128
+t = testSort(n)
+println(8*n / t.time / 1024.0 / 1024.0, " MiB/s")
