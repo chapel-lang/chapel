8dcae1d069fdb2f7f529e39d2187bbe81b4684e9
Anna Rift <anna.rift@hpe.com>
2024-01-24 11:40:16 -0800

    Load deps from Spack install on Osprey
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/cron/load-base-deps.bash
+++ util/cron/load-base-deps.bash
@@ -6,6 +6,11 @@
 if [ -f /data/cf/chapel/chpl-deps/setup_chpl_deps.bash ] ; then
   # for chapcs/chapvm, just load all dependencies via spack
   source /data/cf/chapel/chpl-deps/setup_chpl_deps.bash
+elif [ "$(hostname -s)" == "osprey" ]; then
+  # ditto for osprey
+  if [ -f /cray/css/users/chapelu/chpl-deps/load_chpl_deps.bash ] ; then
+    source /cray/css/users/chapelu/chpl-deps/load_chpl_deps.bash
+  fi
 else
   # For our internal testing, this is necessary to get the latest version of gcc
   # on the system.
