18fe73c5557b1b47e65b9d7d2061d43e9039a5ba
Michael Ferguson <mppf@users.noreply.github.com>
2024-01-25 10:53:02 -0500

    Add timing for random array generation
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

--- test/library/packages/Sort/performance/comparison/python-sort-random.py
+++ test/library/packages/Sort/performance/comparison/python-sort-random.py
@@ -4,7 +4,12 @@ import time
 #n = 1024*1024*16 # for 16MiB
 n = 1024*1024*128 # for 128MiB
 
+t1 = time.time()
 array = [random.randint(0, 0xffffffffffffffff) for _ in range(n)]
+t2 = time.time()
+print ("Generated ", n, " elements in ", t2-t1, " seconds")
+print (((8*n) / (t2-t1))/1024.0/1024.0, " MiB/s")
+
 start = time.time()
 array.sort()
 stop = time.time()
--- test/library/packages/Sort/performance/comparison/sort-random-rust/src/main.rs
+++ test/library/packages/Sort/performance/comparison/sort-random-rust/src/main.rs
@@ -8,7 +8,10 @@ use std::time::Instant;
 
 fn main() {
     let n = 1024*1024*128;
+    let t1 = Instant::now();
     let mut values: Vec<u64> = rand::thread_rng().sample_iter(Standard).take(n).collect();
+    let gen_duration = t1.elapsed();
+    println!("{:?} MiB/s", 8.0*(n as f64)/gen_duration.as_secs_f64()/1024.0/1024.0);
 
     let start = Instant::now();
     values.sort_unstable();
