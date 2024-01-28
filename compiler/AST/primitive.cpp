f0efc4928851fac6a53e3ed14d4ea478f2d6eb20
Paul Cassella <gitgitgit@manetheren.bigw.org>
2024-01-27 18:27:57 -0800

    Fix typo in primitive.cpp
    ---
    Signed-off-by: Paul Cassella <gitgitgit@manetheren.bigw.org>

--- compiler/AST/primitive.cpp
+++ compiler/AST/primitive.cpp
@@ -1313,7 +1313,7 @@ initPrimitive() {
   prim_def(PRIM_CHECK_CONST_ARG_HASH, "check hashes of const arguments", returnInfoVoid, true, true);
 
   // we need to carry information about 'in' intents lowered from foreach loops
-  // until gpu transforms. To do that we add an assigment
+  // until gpu transforms. To do that we add an assignment
   //   `taskIndVar = TASK_INDEPENDENT_SVAR_CAPTURE(capturedVar)` into the AST.
   prim_def(PRIM_TASK_INDEPENDENT_SVAR_CAPTURE, "task independent svar capture", returnInfoUnknown);
 }
