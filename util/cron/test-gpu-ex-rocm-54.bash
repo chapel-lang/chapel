65b3cb8d966074ef26a1d639effadd78867d550b
Jade Abraham <jade.abraham@hpe.com>
2024-01-31 11:41:33 -0800

    stop using llvm bundled in rocm tests
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/cron/test-gpu-ex-rocm-54.bash
+++ util/cron/test-gpu-ex-rocm-54.bash
@@ -9,7 +9,7 @@ source $CWD/common-hpe-cray-ex.bash
 module load rocm/5.4.3  # pin to rocm 5.4.3
 
 export CHPL_COMM=none
-export CHPL_LLVM=bundled
+export CHPL_LLVM=system
 unset CHLP_LLVM_CONFIG  # we need this to avoid warnings
 export CHPL_LOCALE_MODEL=gpu
 export CHPL_LAUNCHER_PARTITION=bardpeak  # bardpeak is the default queue
