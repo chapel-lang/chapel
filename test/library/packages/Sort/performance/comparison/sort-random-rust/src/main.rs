df310ec3d68edc874914a0a6b53a87cbad41ae5b
Michael Ferguson <mppf@users.noreply.github.com>
2024-02-05 10:00:34 -0500

    Comment out the Rayon array generation in the serial version
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

--- test/library/packages/Sort/performance/comparison/sort-random-rust/src/main.rs
+++ test/library/packages/Sort/performance/comparison/sort-random-rust/src/main.rs
@@ -2,20 +2,32 @@
 //  cd sort-random-rust
 //  cargo run --release
 
-use rand::{distributions::Standard, rngs::SmallRng, Rng, SeedableRng};
-use rayon::prelude::*;
+use rand::{distributions::Standard, Rng};
 use std::time::Instant;
+// these are used in the alternative way to generate the array in parallel
+//use rand::{rngs::SmallRng,SeedableRng};
+//use rayon::prelude::*;
 
 fn main() {
     let n = 128 * 1024 * 1024;
 
     let t1 = Instant::now();
+
+    // This is arguably the most obvious way to generate the
+    // random array, but it does it in one thread
+    let mut values: Vec<u64> = rand::thread_rng().sample_iter(Standard).take(n).collect();
+
+    // This alternative generates in parallel.
+    // Using it increases the sequential speed (probably due to NUMA effects)
+    /*
     let rng = SmallRng::seed_from_u64(42);
     let mut values = Vec::<u64>::new();
     (0..n)
         .into_par_iter()
         .map_with(rng, |rng, _| rng.sample(Standard))
         .collect_into_vec(&mut values);
+    */
+
     let gen_duration = t1.elapsed();
     println!(
         "generating {} MiB/s",
