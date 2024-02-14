f6021153b44319921686a0e4fdfc3871f2e596b5
Ben Harshbarger <ben.harshb@gmail.com>
2024-02-13 17:19:35 -0800

    Modify normalization of domain queries to avoid extra copies for arrays
    In #24330 we observed that extra copies were occuring for initializers
    written to take an array by in-intent:
    
    ```chpl
    proc ArrayWrapper.init(in a: [?d] ?t) {
      this.etype = t;
      this.rank = d.rank;
      this.shape = d.shape;
      this.a = a; // extra/unexpected copy here!
    
      writeln(d); // <- commenting this line out removes the extra copy
    }
    ```
    
    This was surprising since the 'in' intent rules are fairly well-defined
    and implemented.
    
    The cause was that the normalize pass was transforming the writeln call
    into something like ``writeln(a.domain)``. The late mention of 'a' would
    then prevent the split-init logic from eliding the copy.
    
    This commit changes the normalized representation of domain queries to
    just use a single 'const ref' temp that we set up at the beginning of
    the function.
    
    Signed-off-by: Ben Harshbarger <ben.harshb@gmail.com>

--- compiler/passes/normalize.cpp
+++ compiler/passes/normalize.cpp
@@ -4018,12 +4018,34 @@ static void fixupArrayDomainExpr(FnSymbol*                    fn,
                                  const std::vector<SymExpr*>& symExprs) {
   // : [?D]   -> defExpr('D')
   if (DefExpr* queryDomain = toDefExpr(domExpr)) {
+    VarSymbol* domRef = nullptr;
     // Walk the body of 'fn' and replace uses of 'D' with 'D'._dom
     for_vector(SymExpr, se, symExprs) {
       if (se->symbol() == queryDomain->sym) {
         SET_LINENO(se);
 
-        se->replace(new CallExpr(".", formal, new_CStringSymbol("_dom")));
+        if (isArgSymbol(se->parentSymbol) || isInWhereBlock(fn, se)) {
+          // Need to use the call in arguments' expression and where-clauses
+          se->replace(new CallExpr(".", formal, new_CStringSymbol("_dom")));
+        } else {
+          // Otherwise, create a temporary at the top of the function and use
+          // that in the function body.
+          if (domRef == nullptr) {
+            VarSymbol* vs = new VarSymbol(astr("_chpl__domain_expr_", queryDomain->sym->name));
+            vs->addFlag(FLAG_TEMP);
+            vs->addFlag(FLAG_REF_VAR);
+            vs->addFlag(FLAG_CONST);
+            vs->qual = QUAL_CONST_REF;
+
+            DefExpr* def = new DefExpr(vs, new CallExpr(".", formal, new_CStringSymbol("_dom")));
+            fn->insertAtHead(def);
+            normalizeVariableDefinition(def);
+
+            domRef = vs;
+          }
+
+          se->replace(new SymExpr(domRef));
+        }
       }
     }
 
