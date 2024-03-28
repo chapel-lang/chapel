9323b9428554b3fc59ee2b90d99a98fd8da03886
Jade Abraham <jade.abraham@hpe.com>
2024-03-28 09:11:54 -0700

    add python libs to version test
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- test/chpldoc/compflags/combinations/versionhelp-chpldoc.sh
+++ test/chpldoc/compflags/combinations/versionhelp-chpldoc.sh
@@ -9,3 +9,11 @@ diff $CWD/../../../../compiler/main/BUILD_VERSION $CWD/zero.txt > /dev/null 2>&1
  { echo -n " pre-release (" && cat $CWD/../../../../compiler/main/BUILD_VERSION | tr -d \"\\n && echo ")" ; }
 # During release mode:
 # echo ""
+
+# print Shpinx and chapeldomain version from pip
+PIP="$CWD/../../../../util/config/run-in-venv.bash python3 -m pip"
+python_deps=$($PIP freeze)
+printf "\tSphinx version "
+echo $python_deps | grep -o 'Sphinx==[^ ]*' | grep -o '[^=]*$'
+printf "\tsphinxcontrib-chapeldomain version "
+echo $python_deps | grep -o 'sphinxcontrib-chapeldomain==[^ ]*' | grep -o '[^=]*$'
