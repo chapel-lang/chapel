ece89f61d6138b8f5a81f66a038cea14143ee015
Shreyas Khandekar <60454060+ShreyasKhandekar@users.noreply.github.com>
2024-04-04 17:35:07 -0500

    Source the right common scripts
    Signed-off-by: Shreyas Khandekar <60454060+ShreyasKhandekar@users.noreply.github.com>

--- util/cron/test-perf.gpu-ex-cuda-12.bash
+++ util/cron/test-perf.gpu-ex-cuda-12.bash
@@ -3,8 +3,8 @@
 # Run GPU performance testing on a Cray EX
 
 CWD=$(cd $(dirname $0) ; pwd)
-source $CWD/common-slurm-gasnet-cray-cs.bash
 source $CWD/common-native-gpu.bash
+source $CWD/common-hpe-cray-ex.bash
 
 module load cudatoolkit/23.3_12.0  # this is the default on this system
 
@@ -12,7 +12,7 @@ export CHPL_LLVM=bundled  # CUDA 12 is only supported with bundled LLVM
 export CHPL_COMM=none
 export CHPL_LOCALE_MODEL=gpu
 export CHPL_LAUNCHER_PARTITION=allgriz
-export CHPL_GPU=nvidia  # amd is also detected automatically
+export CHPL_GPU=nvidia  # amd is detected automatically
 
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.gpu-ex-cuda-12"
 
