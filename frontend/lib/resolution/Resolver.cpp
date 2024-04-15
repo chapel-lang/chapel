f0ac97a06b6f4303298237909a56e5a0706c6c11
Anna Rift <anna.rift@hpe.com>
2024-04-15 12:25:55 -0700

    Replace _range AST existence check with isMissingBundledRecordType
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- frontend/lib/resolution/Resolver.cpp
+++ frontend/lib/resolution/Resolver.cpp
@@ -3314,8 +3314,7 @@ void Resolver::exit(const Range* range) {
   // should be replaced with one that resolves the call.
 
   const RecordType* rangeType = CompositeType::getRangeType(context);
-  auto rangeAst = parsing::idToAst(context, rangeType->id());
-  if (!rangeAst) {
+  if (CompositeType::isMissingBundledRecordType(context, rangeType->id())) {
     // The range record is part of the standard library, but
     // it's possible to invoke the resolver without the stdlib.
     // In this case, mark ranges as UnknownType, but do not error.
