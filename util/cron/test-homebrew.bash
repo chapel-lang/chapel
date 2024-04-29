3f178ca2cf271c26105b6c383098eede3891a618
Anna Rift <anna.rift@hpe.com>
2024-04-29 12:52:30 -0700

    Quote reference path, use /missing by default
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/cron/test-homebrew.bash
+++ util/cron/test-homebrew.bash
@@ -41,7 +41,7 @@ log_info "Building tarball with version: ${version}"
 # run home-brew scripts to install chapel.
 
 mkdir -p $HOME/test
-git clone --reference-if-able $REPO_CACHE_PATH/homebrew-core.git git@github.com:Homebrew/homebrew-core.git 2> /dev/null || (cd $HOME/test/homebrew-core; git pull)
+git clone --reference-if-able "${REPO_CACHE_PATH:-/missing}/homebrew-core.git" git@github.com:Homebrew/homebrew-core.git 2> /dev/null || (cd $HOME/test/homebrew-core; git pull)
 #cp $HOME/test/homebrew-core/Formula/c/chapel.rb  ${CHPL_HOME}/util/packaging/homebrew/chapel.rb
 # fix to make the home-brew working. After 1.32 release uncomment the above line
 cp ${CHPL_HOME}/util/packaging/homebrew/chapel-main.rb  ${CHPL_HOME}/util/packaging/homebrew/chapel.rb
