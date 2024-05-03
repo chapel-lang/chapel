81f275789e371919b975932b63e57de35f13de00
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2024-05-03 11:01:27 -0700

    Update CUDA versions for the EX testing system upgrade
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- util/cron/test-gpu-ex-cuda-12.bash
+++ util/cron/test-gpu-ex-cuda-12.bash
@@ -6,7 +6,7 @@ CWD=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
 source $CWD/common-native-gpu.bash
 source $CWD/common-hpe-cray-ex.bash
 
-module load cudatoolkit/23.3_12.0  # this is the default on this system
+module load cudatoolkit  # default is CUDA 12
 
 export CHPL_LLVM=bundled  # CUDA 12 is only supported with bundled LLVM
 export CHPL_COMM=none
