461a9eb5dbbad5a54cd261424863817edd34e165
Danila Fedorin <daniel.fedorin@hpe.com>
2024-05-02 17:10:45 -0700

    Add a test for completion lists
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

--- tools/chpl-language-server/test/test_server.py
+++ tools/chpl-language-server/test/test_server.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import tempfile
+import typing
 
 from lsprotocol.types import ClientCapabilities
 from lsprotocol.types import CompletionList
@@ -12,6 +14,36 @@ import pytest
 import pytest_lsp
 from pytest_lsp import ClientServerConfig
 from pytest_lsp import LanguageClient
+
+class SourceFilesContext:
+    def __init__(self, **files: str):
+        self.tempdir = tempfile.TemporaryDirectory()
+        for (name, contents) in files.items():
+            with open(os.path.join(self.tempdir.name, name + ".chpl"), "w") as f:
+                f.write(contents)
+
+    def __enter__(self):
+        return lambda name: f"file://{os.path.join(self.tempdir.name, name + '.chpl')}"
+
+    def __exit__(self, *exc):
+        return False
+
+class SourceFileContext():
+    def __init__(self, main_file: str, **files: str):
+        self.main_file = main_file
+        self.source_files_context = SourceFilesContext(**files)
+
+    def __enter__(self):
+        return self.source_files_context.__enter__()(self.main_file)
+
+    def __exit__(self, *exc):
+        return self.source_files_context.__exit__(*exc)
+
+def source_files(**files: str):
+    return SourceFilesContext(**files)
+
+def source_file(contents: str):
+    return SourceFileContext("main", main=contents)
 
 CHPL_HOME = os.environ.get("CHPL_HOME")
 if not CHPL_HOME:
@@ -30,5 +62,38 @@ async def client(lsp_client: LanguageClient):
     # Teardown
     await lsp_client.shutdown_session()
 
-def test_initialize(client: LanguageClient):
+@pytest.mark.asyncio
+async def test_global_completion(client: LanguageClient):
+    # All completion is based entirely on globla symbols right now; thus,
+    # anywhere you hover, you should get the same results.
+
+    positions = [ (n, 0) for n in range(1, 5) ]
+    file = """ for i in 1..10 {
+                 for j in 1..10 {
+                   writeln("Hello, world!", (i, j));
+                 }
+               }
+           """
+
+    global_symbols = ["here", "strideKind", "boundKind", "Locales"]
+
+    with source_file(file) as uri:
+        for (line, column) in positions:
+            results = await client.text_document_completion_async(
+                params=CompletionParams(
+                    position=Position(line=line, character=column),
+                    text_document=TextDocumentIdentifier(uri=uri),
+                )
+            )
+
+            # spec: it could be a completion item list, or a CompletionList,
+            # or null. We care about the list of items.
+            assert results is not None
+            if isinstance(results, CompletionList):
+                results = results.items
+            result_names = [ r.label for r in results ]
+
+            for symbol in global_symbols:
+                assert symbol in result_names
+
     pass
