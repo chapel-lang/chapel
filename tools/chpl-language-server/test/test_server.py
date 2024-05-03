0df849cd4fa77da8fb5439307171351d57d8ea36
Danila Fedorin <daniel.fedorin@hpe.com>
2024-05-02 17:50:37 -0700

    Add basic testing for 'go to def' and 'go to decl'
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

--- tools/chpl-language-server/test/test_server.py
+++ tools/chpl-language-server/test/test_server.py
@@ -6,7 +6,11 @@ import typing
 from lsprotocol.types import ClientCapabilities
 from lsprotocol.types import CompletionList
 from lsprotocol.types import CompletionParams
+from lsprotocol.types import DefinitionParams
+from lsprotocol.types import DeclarationParams
 from lsprotocol.types import InitializeParams
+from lsprotocol.types import LocationLink
+from lsprotocol.types import Location
 from lsprotocol.types import Position
 from lsprotocol.types import TextDocumentIdentifier
 
@@ -15,15 +19,25 @@ import pytest_lsp
 from pytest_lsp import ClientServerConfig
 from pytest_lsp import LanguageClient
 
+def strip_leading_whitespace(text: str) -> str:
+    lines = text.split("\n")
+
+    # Ignore leading empty lines
+    while lines and not lines[0].strip():
+        lines.pop(0)
+
+    min_indent = min(len(line) - len(line.lstrip()) for line in lines if line.strip())
+    return "\n".join(line[min_indent:] for line in lines)
+
 class SourceFilesContext:
     def __init__(self, **files: str):
         self.tempdir = tempfile.TemporaryDirectory()
         for (name, contents) in files.items():
             with open(os.path.join(self.tempdir.name, name + ".chpl"), "w") as f:
-                f.write(contents)
+                f.write(strip_leading_whitespace(contents))
 
     def __enter__(self):
-        return lambda name: f"file://{os.path.join(self.tempdir.name, name + '.chpl')}"
+        return lambda name: TextDocumentIdentifier(uri=f"file://{os.path.join(self.tempdir.name, name + '.chpl')}")
 
     def __exit__(self, *exc):
         return False
@@ -44,6 +58,10 @@ def source_files(**files: str):
 
 def source_file(contents: str):
     return SourceFileContext("main", main=contents)
+
+def pos(coord: typing.Tuple[int, int]):
+    line, column = coord
+    return Position(line=line, character=column)
 
 CHPL_HOME = os.environ.get("CHPL_HOME")
 if not CHPL_HOME:
@@ -67,23 +85,21 @@ async def test_global_completion(client: LanguageClient):
     # All completion is based entirely on global symbols right now; thus,
     # anywhere you hover, you should get the same results.
 
-    positions = [ (n, 0) for n in range(1, 5) ]
-    file = """ for i in 1..10 {
-                 for j in 1..10 {
-                   writeln("Hello, world!", (i, j));
-                 }
-               }
+    positions = [ pos((n, 0)) for n in range(1, 5) ]
+    file = """
+           for i in 1..10 {
+             for j in 1..10 {
+               writeln("Hello, world!", (i, j));
+             }
+           }
            """
 
     global_symbols = ["here", "strideKind", "boundKind", "Locales"]
 
-    with source_file(file) as uri:
-        for (line, column) in positions:
+    with source_file(file) as doc:
+        for position in positions:
             results = await client.text_document_completion_async(
-                params=CompletionParams(
-                    position=Position(line=line, character=column),
-                    text_document=TextDocumentIdentifier(uri=uri),
-                )
+                params=CompletionParams(position=position, text_document=doc)
             )
 
             # spec: it could be a completion item list, or a CompletionList,
@@ -96,4 +112,75 @@ async def test_global_completion(client: LanguageClient):
             for symbol in global_symbols:
                 assert symbol in result_names
 
-    pass
+        assert len(client.diagnostics) == 0
+
+async def check_goto_decl_def(client: LanguageClient, doc: TextDocumentIdentifier, src: Position, dst: typing.Optional[Position]):
+    def validate(results: typing.Optional[typing.Union[Location, typing.List[Location], typing.List[LocationLink]]]):
+        if dst is None:
+            assert results is None or (isinstance(results, list) and len(results) == 0)
+            return
+
+        assert results is not None
+        result = None
+        if isinstance(results, list):
+            assert len(results) == 1
+            result = results[0]
+        else:
+            result = results
+
+        if isinstance(result, LocationLink):
+            real_dst = result.target_range.start
+        else:
+            real_dst = result.range.start
+
+        assert real_dst == dst
+
+    results = await client.text_document_definition_async(
+        params=DefinitionParams(text_document=doc, position=src)
+    )
+    validate(results)
+
+    results = await client.text_document_declaration_async(
+        params=DeclarationParams(text_document=doc, position=src)
+    )
+    validate(results)
+
+@pytest.mark.asyncio
+async def test_go_to_definition_simple(client: LanguageClient):
+
+    file = """
+           var x: int = 5;
+           var y: int = x;
+           var z: int = x + y;
+           {
+             var x = 10;
+             var y = x;
+           }
+           """
+
+    with source_file(file) as doc:
+        # Definitions link to themselves
+        await check_goto_decl_def(client, doc, pos((0, 4)), pos((0, 4)))
+        await check_goto_decl_def(client, doc, pos((1, 4)), pos((1, 4)))
+        await check_goto_decl_def(client, doc, pos((2, 4)), pos((2, 4)))
+        await check_goto_decl_def(client, doc, pos((4, 6)), pos((4, 6)))
+        await check_goto_decl_def(client, doc, pos((5, 6)), pos((5, 6)))
+
+        # References link to their definitions
+        await check_goto_decl_def(client, doc, pos((1, 13)), pos((0, 4)))
+        await check_goto_decl_def(client, doc, pos((2, 13)), pos((0, 4)))
+        await check_goto_decl_def(client, doc, pos((2, 17)), pos((1, 4)))
+        await check_goto_decl_def(client, doc, pos((5, 10)), pos((4, 6)))
+
+        # Other things don't go anywhere
+        # 'var' tokens
+        await check_goto_decl_def(client, doc, pos((0, 2)), None)
+        await check_goto_decl_def(client, doc, pos((1, 2)), None)
+        await check_goto_decl_def(client, doc, pos((2, 2)), None)
+        await check_goto_decl_def(client, doc, pos((4, 4)), None)
+        await check_goto_decl_def(client, doc, pos((5, 4)), None)
+        # numbers
+        await check_goto_decl_def(client, doc, pos((0, 13)), None)
+        await check_goto_decl_def(client, doc, pos((4, 10)), None)
+
+        assert len(client.diagnostics) == 0
