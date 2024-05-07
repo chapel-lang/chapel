d9beab36e486268ed5989182c5c6772bf995da39
Jade Abraham <jade.abraham@hpe.com>
2024-05-06 17:06:48 -0700

    cleanup test homebrew
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/cron/test-homebrew.bash
+++ util/cron/test-homebrew.bash
@@ -35,15 +35,10 @@ cd $CHPL_HOME
 log_info "Building tarball with version: ${version}"
 ./util/buildRelease/gen_release ${version}
 
-# This will clone the home-brew repository under test and copies the chapel formula in chapel-lang repo under
-# util/packaging/home-brew
-# replace the url and sha in the chapel formula with the url pointing to the tarball created and sha of the tarball.
-# run home-brew scripts to install chapel.
+# This will replace the url and sha in the chapel formula with the url pointing
+# to the tarball created and sha of the tarball and then run home-brew scripts
+# to install chapel.
 
-mkdir -p $HOME/test
-git clone --reference-if-able "${REPO_CACHE_PATH:-/missing}/homebrew-core.git" git@github.com:Homebrew/homebrew-core.git 2> /dev/null || (cd $HOME/test/homebrew-core; git pull)
-#cp $HOME/test/homebrew-core/Formula/c/chapel.rb  ${CHPL_HOME}/util/packaging/homebrew/chapel.rb
-# fix to make the home-brew working. After 1.32 release uncomment the above line
 cp ${CHPL_HOME}/util/packaging/homebrew/chapel-main.rb  ${CHPL_HOME}/util/packaging/homebrew/chapel.rb
 cd ${CHPL_HOME}/util/packaging/homebrew
 # Get the tarball from the root tar/ directory and replace the url in chapel.rb with the tarball location
@@ -51,16 +46,16 @@ location="${CHPL_HOME}/tar/chapel-${version}.tar.gz"
 log_info $location
 
 # Replace the url and sha236 in chapel.rb with the location of the tarball and sha256 of the tarball generated.
-# create sed -i '' -e for macOS 
+# create sed -i '' -e for macOS
 sed_command="sed -i '' -e"
-$sed_command "s#url.*#url \"file\:///$location\"#" chapel.rb 
+$sed_command "s#url.*#url \"file\:///$location\"#" chapel.rb
 sha=($(shasum -a 256 $location))
 sha256=${sha[0]}
 log_info $sha256
 $sed_command  "1s/sha256.*/sha256 \"$sha256\"/;t" -e "1,/sha256.*/s//sha256 \"$sha256\"/" chapel.rb
 
 # Test if homebrew install using the chapel formula works.
-brew upgrade 
+brew upgrade
 brew uninstall --force chapel
 # Remove the cached chapel tar file before running brew install --build-from-source chapel.rb
 rm /Users/chapelu/Library/Caches/Homebrew/downloads/*.tar.gz
