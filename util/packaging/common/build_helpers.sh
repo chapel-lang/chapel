2350fc99b33e4686f1ed26ba5cf70914f4569e3b
Jade Abraham <jade.abraham@hpe.com>
2024-05-23 09:28:36 -0700

    add additional build helpers for debugging purposes
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/packaging/common/build_helpers.sh
+++ util/packaging/common/build_helpers.sh
@@ -51,3 +51,42 @@ __build_packages() {
     -f Dockerfile ../..
   popd
 }
+
+__build_image() {
+  # use this to build the package for linux/amd64 and linux/arm64
+  local pkg_type=$1
+  local os=$2
+  local package_name=$3
+  local chapel_version=$4
+  local package_version=$5
+  local docker_dir_name=$6
+
+  # default to 1 core
+  local para=${7:-1}
+
+  __wget_chpl_release $chapel_version
+
+  local package_dir="${CHPL_HOME}/util/packaging/${pkg_type}/${docker_dir_name}"
+  __get_docker_tag $os $package_name $chapel_version $package_version
+
+  pushd ${package_dir}
+  DOCKER_BUILDKIT=1 docker buildx build \
+    --load \
+    --target=build \
+    --build-arg "CHAPEL_VERSION=$chapel_version" \
+    --build-arg "PACKAGE_VERSION=$package_version" \
+    --build-arg "PARALLEL=$para" \
+    -t $__docker_tag \
+    -f Dockerfile ../..
+  popd
+}
+__run_container() {
+  local os=$1
+  local package_name=$2
+  local chapel_version=$3
+  local package_version=$4
+
+  __get_docker_tag $os $package_name $chapel_version $package_version
+
+  docker run -it --rm $__docker_tag
+}
