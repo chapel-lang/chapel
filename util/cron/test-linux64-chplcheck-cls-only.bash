966a515540653c507ca891cdcf8168a483fd5b69
Danila Fedorin <daniel.fedorin@hpe.com>
2024-06-03 20:25:17 -0700

    Fix invalid git config name
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

--- util/cron/test-linux64-chplcheck-cls-only.bash
+++ util/cron/test-linux64-chplcheck-cls-only.bash
@@ -7,6 +7,6 @@ CWD=$(cd $(dirname $0) ; pwd)
 source $CWD/common.bash
 source $CWD/common-localnode-paratest.bash
 
-export CHPL_NIGHTLY_TEST_CONFIG_NAME="linux64-chplcheck-only"
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="linux64-chplcheck-cls-only"
 
 $CWD/nightly -cron -assertions -chplcheckonly -chpl-language-server ${nightly_args} $(get_nightly_paratest_args 8)
