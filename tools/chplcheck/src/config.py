494ec0c097bb5f45cb9b4ca9881ce30400a58d8d
Jade Abraham <jade.abraham@hpe.com>
2024-06-10 20:11:33 -0700

    add license
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- tools/chplcheck/src/config.py
+++ tools/chplcheck/src/config.py
@@ -1,3 +1,22 @@
+#
+# Copyright 2023-2024 Hewlett Packard Enterprise Development LP
+# Other additional copyright holders may be indicated within.
+#
+# The entirety of this work is licensed under the Apache License,
+# Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.
+#
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 from dataclasses import dataclass
 from typing import List, Union, Dict, Any
 import argparse
@@ -22,14 +41,14 @@ class Config:
             action="append",
             dest="chplcheck_disabled_rules",
             default=[],
-            help="Disable a lint rule by name"
+            help="Disable a lint rule by name",
         )
         parser.add_argument(
             f"--{prefix}enable-rule",
             action="append",
             dest="chplcheck_enabled_rules",
             default=[],
-            help="Enable a lint rule by name"
+            help="Enable a lint rule by name",
         )
         parser.add_argument(
             f"--{prefix}skip-unstable",
