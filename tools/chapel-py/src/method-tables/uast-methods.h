ddb52ff7dc8a570a19f21adb5d56788a84e7b4a8
Danila Fedorin <daniel.fedorin@hpe.com>
2024-06-11 13:51:15 -0700

    Expose the parenth locations for expressions to AST nodes
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

--- tools/chapel-py/src/method-tables/uast-methods.h
+++ tools/chapel-py/src/method-tables/uast-methods.h
@@ -104,6 +104,10 @@ CLASS_BEGIN(AstNode)
                std::optional<chpl::Location>,
                auto loc = chpl::parsing::locateCurlyBracesWithAst(context, node);
                return getValidLocation(loc))
+  PLAIN_GETTER(AstNode, parenth_location, "Get the Location of the parentheses of this AstNode node",
+               std::optional<chpl::Location>,
+               auto loc = chpl::parsing::locateExprParenthWithAst(context, node);
+               return getValidLocation(loc))
 CLASS_END(AstNode)
 
 CLASS_BEGIN(AnonFormal)
