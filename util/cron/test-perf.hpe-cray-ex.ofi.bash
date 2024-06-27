868692e115e68c3fbc35c18e522ce9d14dc28da3
Andy Stone <stonea@users.noreply.github.com>
2024-06-27 10:43:20 -0700

    fix names
    ---
    Signed-off-by: Andy Stone <stonea@users.noreply.github.com>

--- util/cron/test-perf.hpe-cray-ex.ofi.bash
+++ util/cron/test-perf.hpe-cray-ex.ofi.bash
@@ -13,9 +13,9 @@ export CHPL_NIGHTLY_TEST_CONFIG_NAME="hpe-cray-ex-ofi"
 export CHPL_RT_COMM_OFI_EXPECTED_PROVIDER="cxi"
 export CHPL_RT_MAX_HEAP_SIZE=16g
 
-export CHPL_TEST_PERF_SUBDIR="hpe-ex"
-export CHPL_TEST_PERF_CONFIG_NAME='16-node-hpe-ex'
-export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.hpe-ex.ofi"
+export CHPL_TEST_PERF_SUBDIR="hpe-cray-ex"
+export CHPL_TEST_PERF_CONFIG_NAME='16-node-hpe-cray-ex'
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.hpe-cray-ex.ofi"
 
 nightly_args="${nightly_args} -no-buildcheck"
 perf_args="-numtrials 1"
