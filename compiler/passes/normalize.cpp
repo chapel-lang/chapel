69bd6d704ef2c8d1072cd746826dd7b1844f1ab9
Danila Fedorin <daniel.fedorin@hpe.com>
2024-06-28 17:52:03 -0700

    More thunk lowering before iterator lowering
    The iterator building code should be deferred, and this
    can only be achieved if it's lowered after thunks
    
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

--- compiler/passes/normalize.cpp
+++ compiler/passes/normalize.cpp
@@ -863,11 +863,11 @@ static void normalizeBase(BaseAST* base, bool addEndOfStatements) {
     }
   }
 
+  lowerThunkPrims(base);
+
   lowerIfExprs(base);
 
   lowerLoopExprs(base);
-
-  lowerThunkPrims(base);
 
 
   //
