fa6a9cdc5498ef7d7bac2a6c91f7fedf6862dec9
Danila Fedorin <daniel.fedorin@hpe.com>
2024-07-11 13:14:21 -0700

    Avoid warning for standalone function in type expr
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

--- compiler/resolution/fcf-support.cpp
+++ compiler/resolution/fcf-support.cpp
@@ -1180,6 +1180,16 @@ void emitWarningForStandaloneCapture(Expr* expr, const char* name) {
   //
   // This is likely wrong; the user probably meant to call it.
   if (expr->getStmtExpr() == expr) {
+    if (auto parent = expr->parentExpr) {
+      if (auto block = toBlockStmt(parent)) {
+        if (block->blockTag == BLOCK_TYPE) {
+          // When used in a type expression, it's not really standalone.
+          return;
+        }
+      }
+    }
+
+
     USR_WARN(expr, "function '%s' is mentioned but not called, so it is being "
              "captured as a first-class function", name);
     USR_PRINT(expr, "'%s' is a parenful function, so it needs '(...)' to be called", name);
