f646eac5d36f17bd3703307e6a8f772683cc9a53
Andy Stone <stonea@users.noreply.github.com>
2024-08-05 16:09:25 -0700

    Allow pruning nonscalar ref syms on foreach loop
    ---
    Signed-off-by: Andy Stone <stonea@users.noreply.github.com>

--- compiler/resolution/implementForallIntents.cpp
+++ compiler/resolution/implementForallIntents.cpp
@@ -779,13 +779,14 @@ static void resolveShadowVarTypeIntent(LoopWithShadowVarsInterface* fs,
 
   // Prune, as discussed in the above comment.
   //
-  // However, for foreach, we do not want to prune (replace a ref intent'd
-  // shadow variable with its definition outside the loop) because if the loop
-  // is gpuized we need to pass that variable by pointer. We do special
-  // processing to handle this case in iterator lowering and gpu transforms and
-  // as such do not want to prematurely prune it.
-  if (intent == TFI_REF && fs->isForallStmt())
+  // However, for scalars passed to foreach, we do not want to prune (replace a
+  // ref intent'd shadow variable with its definition outside the loop) because
+  // if the loop is gpuized we need to pass that variable by pointer. We do
+  // special processing to handle this case in iterator lowering and gpu
+  // transforms and as such do not want to prematurely prune it.
+  if (intent == TFI_REF && (fs->isForallStmt() || !isPrimitiveScalar(type))) {
     prune = true;
+  }
 }
 
 /////////////////////////////////////////////////////////////////////////////
