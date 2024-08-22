17519a7e1056b9290b7e08bc8895d1bc0dd7bc9f
Ahmad Rezaii <ahmad.rezaii@hpe.com>
2024-08-22 16:12:54 -0600

    fix pathing relative to CHPL_HOME
    Signed-off-by: Ahmad Rezaii <ahmad.rezaii@hpe.com>

--- util/packaging/docker/test/homebrew_ci.bash
+++ util/packaging/docker/test/homebrew_ci.bash
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 #This script will build a docker image using the Dockerfile and will execute home-brew install scripts inside the container
-CWD=$(cd $(dirname $0) ; pwd)
-export CHPL_HOME=$(cd $CWD/../.. ; pwd)
-
-cd ${CHPL_HOME}/packaging/docker/test
+cd ${CHPL_HOME}/util/packaging/docker/test
 
 # Remove image with name homebrew_ci before creating a fresh image to avoid failures.
 docker image rm --force homebrew_ci
@@ -11,7 +8,7 @@ docker build . --load -t homebrew_ci
 containerid= docker image ls | grep 'homebrew_ci' | awk '{print$3}'
 
 # Start the container and run a script to check homebrew install inside the running container
-docker run --rm -i homebrew_ci /bin/bash < ${CHPL_HOME}/packaging/docker/test/brew_install.bash
+docker run --rm -i homebrew_ci /bin/bash < ${CHPL_HOME}/util/packaging/docker/test/brew_install.bash
 CONTAINER_RUN=$?
     if [ $CONTAINER_RUN -ne 0 ]
     then
