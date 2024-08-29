daa4572a85005c4d2d37bf60b31af3f07675b4c3
Ahmad Rezaii <ahmad.rezaii@hpe.com>
2024-08-29 13:42:18 -0600

    update comments, fix typos and formatting
    Signed-off-by: Ahmad Rezaii <ahmad.rezaii@hpe.com>

--- util/packaging/docker/test/homebrew_ci.bash
+++ util/packaging/docker/test/homebrew_ci.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-#This script will build a docker image using the Dockerfile and will execute home-brew install scripts inside the container
+#This script will build a docker image using the Dockerfile and will execute homebrew install scripts inside the container
 cd ${CHPL_HOME}/util/packaging/docker/test
 
-# specify the platform as linux/amd64 to avoid warnings from aarm64 platforms
+# specify the platform as linux/amd64 to avoid warnings from aarch64 platforms
 
 # Remove image with name homebrew_ci before creating a fresh image to avoid failures.
 docker image rm --force homebrew_ci
@@ -12,10 +12,10 @@ containerid= docker image ls | grep 'homebrew_ci' | awk '{print$3}'
 # Start the container and run a script to check homebrew install inside the running container
 docker run --platform linux/amd64 --rm -i homebrew_ci /bin/bash < ${CHPL_HOME}/util/packaging/docker/test/brew_install.bash
 CONTAINER_RUN=$?
-    if [ $CONTAINER_RUN -ne 0 ]
-    then
-      echo "brew test bot commands failed inside ubuntu container"
-      exit 1
-      else
-      echo "brew test bot commands succeeded inside ubuntu container"
-    fi
+if [ $CONTAINER_RUN -ne 0 ]
+then
+  echo "brew test bot commands failed inside ubuntu container"
+  exit 1
+  else
+  echo "brew test bot commands succeeded inside ubuntu container"
+fi
