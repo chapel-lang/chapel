113686cb5d93d18af449a89165c1f2c86e2c6dbc
Ahmad Rezaii <ahmad.rezaii@hpe.com>
2024-08-29 12:04:07 -0600

    specify platform in homebrew_ci to avoid warning on apple silicon
    Signed-off-by: Ahmad Rezaii <ahmad.rezaii@hpe.com>

--- util/packaging/docker/test/homebrew_ci.bash
+++ util/packaging/docker/test/homebrew_ci.bash
@@ -2,13 +2,15 @@
 #This script will build a docker image using the Dockerfile and will execute home-brew install scripts inside the container
 cd ${CHPL_HOME}/util/packaging/docker/test
 
+# specify the platform as linux/amd64 to avoid warnings from aarm64 platforms
+
 # Remove image with name homebrew_ci before creating a fresh image to avoid failures.
 docker image rm --force homebrew_ci
-docker build . --load -t homebrew_ci
+docker build . --load --platform linux/amd64 -t homebrew_ci
 containerid= docker image ls | grep 'homebrew_ci' | awk '{print$3}'
 
 # Start the container and run a script to check homebrew install inside the running container
-docker run --rm -i homebrew_ci /bin/bash < ${CHPL_HOME}/util/packaging/docker/test/brew_install.bash
+docker run --platform linux/amd64 --rm -i homebrew_ci /bin/bash < ${CHPL_HOME}/util/packaging/docker/test/brew_install.bash
 CONTAINER_RUN=$?
     if [ $CONTAINER_RUN -ne 0 ]
     then
