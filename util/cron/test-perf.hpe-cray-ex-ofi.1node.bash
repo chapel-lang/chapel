6f07f33165c0727e31ede04750d453a0d897e33a
Andy Stone <stonea@users.noreply.github.com>
2024-09-09 11:04:22 -0700

    fix dates and test name
    ---
    Signed-off-by: Andy Stone <stonea@users.noreply.github.com>

--- util/cron/test-perf.hpe-cray-ex-ofi.1node.bash
+++ util/cron/test-perf.hpe-cray-ex-ofi.1node.bash
@@ -9,7 +9,7 @@ export CHPL_TEST_PERF_CONFIG_NAME='1-node-hpe-cray-ex'
 
 source $CWD/common-perf.bash
 
-export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.hpe-cray-ex.ofi.1node"
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.hpe-cray-ex-ofi.1node"
 
 source $CWD/common-ofi.bash || \
   ( echo "Could not set up comm=ofi testing." && exit 1 )
@@ -20,6 +20,6 @@ export CHPL_RT_MAX_HEAP_SIZE=16g
 
 nightly_args="${nightly_args} -no-buildcheck"
 perf_args="-performance -perflabel perf -numtrials 1"
-perf_hpe_cray_ex_args="-startdate 09/10/24"
+perf_hpe_cray_ex_args="-startdate 06/25/24"
 
 $CWD/nightly -cron ${perf_args} ${perf_hpe_cray_ex_args} ${nightly_args}
