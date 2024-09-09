639345fb6de68fde983b182b1928dcca5ca1ea3d
John H. Hartman <jhh67@users.noreply.github.com>
2024-09-09 16:04:06 -0700

    Fix GASNet co-locale performance testing scripts
    Signed-off-by: John H. Hartman <jhh67@users.noreply.github.com>

--- util/cron/common-perf-hpe-apollo-hdr.bash
+++ util/cron/common-perf-hpe-apollo-hdr.bash
@@ -6,4 +6,4 @@ export CHPL_LAUNCHCMD_NUM_CPUS=144
 export CHPL_LAUNCHCMD_QUEUE=f2401THP
 export CHPL_TARGET_CPU=none
 
-perf_hpe_apollo_args="-performance-configs gn-ibv-large:v,gn-ibv-fast:v -perflabel ml- -startdate 03/11/21"
+perf_hpe_apollo_args="-performance-configs gn-ibv-large:v,gn-ibv-fast:v,gn-ibv-fast-colo:v -perflabel ml- -startdate 03/11/21"
