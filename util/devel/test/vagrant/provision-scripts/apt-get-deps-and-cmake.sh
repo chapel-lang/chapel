f5602f950f64c189248f892b5a7de03f665c0d92
Ahmad Rezaii <ahmad.rezaii@hpe.com>
2024-09-09 08:34:10 -0600

    update cmake version for older apt-get based OSs
    Signed-off-by: Ahmad Rezaii <ahmad.rezaii@hpe.com>

---
+++ util/devel/test/vagrant/provision-scripts/apt-get-deps-and-cmake.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+alias hide=eval
+
+apt-get update
+apt-get install -y gcc g++ m4 perl python3 python3-dev bash make mawk git pkg-config
+
+apt-get install -y wget libssl-dev
+hide CMAKE_VERSION=3.30.3
+hide MYTMP=`mktemp -d`
+hide cd $MYTMP
+wget https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION.tar.gz
+tar xvzf cmake-$CMAKE_VERSION.tar.gz
+cd cmake-$CMAKE_VERSION
+./bootstrap
+make -j8
+make install
+
+update-alternatives --install /usr/bin/cmake cmake /usr/local/bin/cmake 1
