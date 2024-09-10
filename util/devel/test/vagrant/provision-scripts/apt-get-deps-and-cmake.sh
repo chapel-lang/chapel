b01a85565f1873d99e5752d8bdac4c4848c58630
Ahmad Rezaii <ahmad.rezaii@hpe.com>
2024-09-10 17:38:32 -0600

    remove CMake version variable from dependency script
    Signed-off-by: Ahmad Rezaii <ahmad.rezaii@hpe.com>

--- util/devel/test/vagrant/provision-scripts/apt-get-deps-and-cmake.sh
+++ util/devel/test/vagrant/provision-scripts/apt-get-deps-and-cmake.sh
@@ -4,16 +4,15 @@ alias hide=eval
 
 apt-get update
 apt-get install -y gcc g++ m4 perl python3 python3-dev bash make mawk git pkg-config
-
 apt-get install -y wget libssl-dev
-hide CMAKE_VERSION=3.30.3
+
 hide MYTMP=`mktemp -d`
 hide cd $MYTMP
-wget https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION.tar.gz
-tar xvzf cmake-$CMAKE_VERSION.tar.gz
-cd cmake-$CMAKE_VERSION
+wget https://github.com/Kitware/CMake/releases/download/v3.30.3/cmake-3.30.3.tar.gz
+tar xvzf cmake-3.30.3.tar.gz
+cd cmake-3.30.3
 ./bootstrap
-make $MAKEJ
+make
 make install
 
 update-alternatives --install /usr/bin/cmake cmake /usr/local/bin/cmake 1
