98239d55ddf81a84d13a4d564caf10c5075318c6
Tim Zinsky <tim.zinsky@hpe.com>
2024-09-17 22:48:24 -0500

    Fix versionhelp.sh comment for release mode
    Signed-off-by: Tim Zinsky <tim.zinsky@hpe.com>

--- test/compflags/bradc/printstuff/versionhelp.sh
+++ test/compflags/bradc/printstuff/versionhelp.sh
@@ -6,7 +6,7 @@ echo -n `basename $compiler`
 cat $CWD/version.goodstart
 # During pre-release mode
 # diff $CWD/../../../../compiler/main/BUILD_VERSION $CWD/zero.txt > /dev/null 2>&1 && echo "" || \
-  { echo -n " pre-release (" && cat $CWD/../../../../compiler/main/BUILD_VERSION | tr -d \"\\n && echo ")" ; }
+#  { echo -n " pre-release (" && cat $CWD/../../../../compiler/main/BUILD_VERSION | tr -d \"\\n && echo ")" ; }
 # During release mode:
 echo ""
 
