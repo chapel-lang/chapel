984c41816b739510725f76aaac393e519c5ff07b
Brad Chamberlain <bradcray@users.noreply.github.com>
2024-09-23 14:47:04 -0700

    Update docs menu to indicate 'main' is now 2.3 pre-release
    ---
    Signed-off-by: Brad Chamberlain <bradcray@users.noreply.github.com>

--- doc/util/versionButton.php
+++ doc/util/versionButton.php
@@ -89,7 +89,7 @@ if (pagePath == "") {
 function dropSetup() {
   var currentRelease = "2.1"; // what does the public have?
   var stagedRelease = "2.2";  // is there a release staged but not yet public?
-  var nextRelease = "2.2";    // what's the next release? (on docs/main)
+  var nextRelease = "2.3";    // what's the next release? (on docs/main)
   var button = document.getElementById("versionButton");
   // Uses unicode down-pointing triangle
   var arrow = " &#9660;";
