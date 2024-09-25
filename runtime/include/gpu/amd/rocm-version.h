9b5d8964e620eb47b56d4fa028956d220a80f718
Jade Abraham <jade.abraham@hpe.com>
2024-09-25 15:14:47 -0500

    use CHPL_GPU_SDK_VERSION_MAJOR
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- runtime/include/gpu/amd/rocm-version.h
+++ runtime/include/gpu/amd/rocm-version.h
@@ -21,20 +21,6 @@
 #ifndef __HIP_PLATFORM_AMD__
 #define __HIP_PLATFORM_AMD__
 #endif
-#include <hip/hip_common.h>
-#include <hip/hip_runtime.h>
 
-#if __has_include(<rocm-core/rocm_version.h>)  // 5.x wants this
-#include <rocm-core/rocm_version.h>
-#elif __has_include(<rocm/rocm_version.h>)  // 4.x wants this
-#include <rocm/rocm_version.h>
-#elif __has_include(<rocm_version.h>)  // Deprecated. 5.x used to want this
-#include <rocm_version.h>
-#endif
-
-// we should have found the correct header by now. But if not, we set
-// ROCM_VERSION_MAJOR to 4 as it is the lowest version we support and has fewer
-// runtime features enabled. So, it is a safer choice.
-#if !defined(ROCM_VERSION_MAJOR)
-#define ROCM_VERSION_MAJOR 4
-#endif
+// CHPL_GPU_SDK_VERSION_MAJOR is determined by printchplenv
+#define ROCM_VERSION_MAJOR CHPL_GPU_SDK_VERSION_MAJOR
