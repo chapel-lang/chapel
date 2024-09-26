ae4756c4acd8273208d4991674b4654709987981
Paul Cassella <gitgitgit@manetheren.bigw.org>
2024-09-26 00:57:55 -0700

    Fix typo in Builder.h
    ---
    Signed-off-by: Paul Cassella <gitgitgit@manetheren.bigw.org>

--- frontend/include/chpl/uast/Builder.h
+++ frontend/include/chpl/uast/Builder.h
@@ -160,7 +160,7 @@ class Builder final {
   #include "all-location-maps.h"
   #undef LOCATION_MAP
 
-  /** Delete all the locations storedfor the current AST. This is useful if the
+  /** Delete all the locations stored for the current AST. This is useful if the
       AST is being deallocated, which means future uses of this pointer
       (the memory could be re-used) must not have locations out of the box.
     */
