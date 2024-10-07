d575b153e731bea5b14150cef94a2e0fc76add24
Michael Ferguson <mppf@users.noreply.github.com>
2024-10-07 17:56:09 -0400

    Remove byAstOrNull from non-const RV
    because with the non-const RV callers might want to add it, so probably
    better to use hasAst in that case.
    
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

--- frontend/include/chpl/resolution/ResolvedVisitor.h
+++ frontend/include/chpl/resolution/ResolvedVisitor.h
@@ -309,11 +309,6 @@ public:
   ResolvedExpression& byAst(const uast::AstNode* ast) const {
     return byPostorder_.byAst(ast);
   }
-  /** Return the ResolvedExpression for a particular uAST node,
-      or nullptr if none is present*/
-  ResolvedExpression* byAstOrNull(const uast::AstNode* ast) const {
-    return byPostorder_.byAstOrNull(ast);
-  }
   /** Returns if the ResolutionResultByPostorderID has a result for
       a particular ID */
   bool hasId(const ID& id) const {
