f0089839b68a0794338505cf92273ce7217f0e2b
Shreyas Khandekar <60454060+ShreyasKhandekar@users.noreply.github.com>
2024-10-08 18:21:43 -0500

    Add new configs for gpu on hpe cray ex
    This adds the following new configurations scripts
    for testing gpus on HPE Cray EX systems:
    - `test-gpu-ex-cpu` (analogous to `test-gpu-cpu` for Cray CS)
    - `test-gpu-ex-cuda-12.interop` (analogous to
      `test-gpu-cuda.interop` for Cray CS)
    - `test-gpu-ex-cuda-12.specialization` (analogous to
      `test-gpu-cuda.specialization` for Cray CS)
    - `test-perf.gpu-ex-cuda-12.um` (analogous to
      `test-perf.gpu-cuda.um` for Cray CS)
    
    Signed-off-by: Shreyas Khandekar <60454060+ShreyasKhandekar@users.noreply.github.com>

--- util/cron/test-gpu-ex-cuda-12.specialization.bash
+++ util/cron/test-gpu-ex-cuda-12.specialization.bash
@@ -15,5 +15,7 @@ export CHPL_LAUNCHER_PARTITION=allgriz
 export CHPL_TEST_GPU=true
 export CHPL_GPU=nvidia  # amd is also detected automatically
 
-export CHPL_NIGHTLY_TEST_CONFIG_NAME="gpu-ex-cuda-12"
+export CHPL_GPU_SPECIALIZATION=y
+
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="gpu-ex-cuda-12.specialization"
 $CWD/nightly -cron ${nightly_args}
---
+++ util/cron/test-perf.gpu-ex-cuda-12.um.bash
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+# Run GPU performance testing on a Cray EX
+
+CWD=$(cd $(dirname $0) ; pwd)
+source $CWD/common-native-gpu.bash
+source $CWD/common-hpe-cray-ex.bash
+
+module load cudatoolkit  # default is CUDA 12
+
+export CHPL_LLVM=bundled  # CUDA 12 is only supported with bundled LLVM
+export CHPL_COMM=none
+export CHPL_LOCALE_MODEL=gpu
+export CHPL_LAUNCHER_PARTITION=allgriz
+export CHPL_GPU=nvidia  # amd is detected automatically
+export CHPL_GPU_MEM_STRATEGY=unified_memory
+
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.gpu-ex-cuda-12.um"
+
+export CHPL_TEST_PERF_CONFIG_NAME="1-node-a100" # pinoak has ampere GPUs
+source $CWD/common-native-gpu-perf.bash
+# make sure this comes after setting SUBDIR (set by native-gpu-perf) and
+# CONFIG_NAME
+source $CWD/common-perf.bash
+
+SHORT_NAME=um
+nightly_args="${nightly_args} -performance-description $SHORT_NAME -performance-configs default:v,$SHORT_NAME:v -sync-dir-suffix $SHORT_NAME"
+nightly_args="${nightly_args} -startdate 10/10/24"
+
+$CWD/nightly -cron ${nightly_args}
