c013cdd1bf408d466aa2e382d7416b797c3c1d11
Danila Fedorin <daniel.fedorin@hpe.com>
2024-10-22 15:07:56 -0700

    Lock down go-to-def for called functions
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

--- tools/chpl-language-server/test/basic_resolver.py
+++ tools/chpl-language-server/test/basic_resolver.py
@@ -53,6 +53,65 @@ async def test_go_to_record_def(client: LanguageClient):
         await check_goto_type_def(client, doc, pos((2, 4)), pos((0, 7)))
         await check_goto_type_def(client, doc, pos((3, 9)), pos((0, 7)))
 
+@pytest.mark.asyncio
+async def test_go_to_call_basic(client: LanguageClient):
+    """
+    Ensure that 'go to definition' on a type actually works.
+    """
+
+    file = """
+           proc foo(x: int) {}
+           proc foo(x: bool) {}
+           foo(1);
+           foo(true);
+           """
+
+    async with source_file(client, file) as doc:
+        await check_goto_decl_def(client, doc, pos((2, 0)), pos((0, 5)))
+        await check_goto_decl_def(client, doc, pos((3, 0)), pos((1, 5)))
+
+@pytest.mark.asyncio
+async def test_go_to_call_generic(client: LanguageClient):
+    """
+    Ensure that 'go to definition' on a type actually works.
+    """
+
+    file = """
+           proc foo(x: int) {}
+           proc foo(x: bool) {}
+           proc bar(x) do foo(x);
+           bar(1);
+           bar(true);
+           """
+    lenses = [(pos((2, 5)), 3)]
+
+    async with source_file(client, file) as doc:
+        got_lenses = await check_generic_code_lenses(client, doc, lenses)
+        lenses_for_bar = got_lenses[(2, 5)]
+
+        async def select_inst(n, expect_title):
+            assert lenses_for_bar[n].command is not None
+            assert lenses_for_bar[n].command.title == expect_title
+            await execute_command(client, lenses_for_bar[n].command)
+
+        # Before selecting instantiation, should not know which call it is
+        await check_goto_decl_def(client, doc, pos((2, 15)), None)
+        await check_goto_decl_def(client, doc, pos((2, 19)), pos((2, 9)))
+
+        # Select the first instantiation
+        await select_inst(1, "Show instantiation")
+        await check_goto_decl_def(client, doc, pos((2, 15)), pos((0, 5)))
+        await check_goto_decl_def(client, doc, pos((2, 19)), pos((2, 9)))
+
+        # Select the second instantiation
+        await select_inst(2, "Show instantiation")
+        await check_goto_decl_def(client, doc, pos((2, 15)), pos((1, 5)))
+        await check_goto_decl_def(client, doc, pos((2, 19)), pos((2, 9)))
+
+        # Clear instantiation; call to 'foo' should be unclickable again.
+        await select_inst(0, "Show Generic")
+        await check_goto_decl_def(client, doc, pos((2, 15)), None)
+        await check_goto_decl_def(client, doc, pos((2, 19)), pos((2, 9)))
 
 @pytest.mark.asyncio
 @pytest.mark.xfail
