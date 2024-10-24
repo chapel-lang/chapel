5fbf9c8165306ebf66315107588ca49b8faa0811
Danila Fedorin <daniel.fedorin@hpe.com>
2024-10-24 11:18:34 -0700

    Add a script for invoking colocales as a nightly job
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

---
+++ util/cron/test-gpu-ex-cuda-12.colocales.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# GPU native testing on a Cray EX for colocales specifically
+
+CWD=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
+source $CWD/common-native-gpu.bash
+source $CWD/common-hpe-cray-ex.bash
+source $CWD/common-ofi.bash
+
+export SLURM_NETWORK=single_node_vni
+module load cudatoolkit  # default is CUDA 12
+
+export CHPL_LLVM=bundled  # CUDA 12 is only supported with bundled LLVM
+export CHPL_LAUNCHER_PARTITION=allgriz
+export CHPL_GPU=nvidia
+export CHPL_NIGHTLY_TEST_DIRS="test/gpu/native/multiLocale/"
+
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="gpu-ex-cuda-12.colocales"
+$CWD/nightly -cron ${nightly_args}
