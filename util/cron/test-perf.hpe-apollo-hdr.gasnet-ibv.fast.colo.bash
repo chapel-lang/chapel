66ba07451d0a05310de1a3296e9cca191a93786a
John H. Hartman <jhh67@users.noreply.github.com>
2024-10-29 10:38:55 -0700

    Add GASNet co-locale performance testing with PSHM disabled
    Signed-off-by: John H. Hartman <jhh67@users.noreply.github.com>

--- util/cron/test-perf.hpe-apollo-hdr.gasnet-ibv.fast.colo.bash
+++ util/cron/test-perf.hpe-apollo-hdr.gasnet-ibv.fast.colo.bash
@@ -10,18 +10,18 @@ export CHPL_TEST_PERF_CONFIG_NAME='16-node-apollo-hdr'
 
 source $CWD/common-perf.bash
 
-export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.hpe-apollo-hdr.gasnet-ibv.fast.colo-nopshm"
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.hpe-apollo-hdr.gasnet-ibv.fast.colo"
 
 source $CWD/common-hpe-apollo.bash
 source $CWD/common-perf-hpe-apollo-hdr.bash
-perf_hpe_apollo_args="-performance-configs gn-ibv-fast:v,gn-ibv-fast-colo:v,gasnet-ibv.fast.colo-nopshm:v -perflabel ml- -startdate 09/04/24"
+perf_hpe_apollo_args="-performance-configs gn-ibv-fast:v,gn-ibv-fast-colo:v -perflabel ml- -startdate 09/04/24"
 
 export CHPL_GASNET_SEGMENT=fast
 export GASNET_PHYSMEM_MAX="0.90"
 export CHPL_RT_LOCALES_PER_NODE=2
-export CHPL_GASNET_MORE_CFG_OPTIONS=--disable-pshm
+export CHPL_GASNET_MORE_CFG_OPTIONS=--enable-pshm
 
 nightly_args="${nightly_args} -no-buildcheck"
-perf_args="-performance-description gn-ibv-fast-colo-nopshm -numtrials 1 -sync-dir-suffix colocales"
+perf_args="-performance-description gn-ibv-fast-colo -numtrials 1 -sync-dir-suffix colocales"
 
 $CWD/nightly -cron ${perf_args} ${perf_hpe_apollo_args} ${nightly_args}
