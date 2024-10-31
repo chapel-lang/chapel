8d40e442fd6054a38474415b1525e8d83ee5441e
Jade Abraham <jade.abraham@hpe.com>
2024-10-31 16:28:38 -0700

    force a default of None
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- test/chplcheck/RuleSettings/rules.py
+++ test/chplcheck/RuleSettings/rules.py
@@ -1,6 +1,6 @@
-
 from chapel import AstNode
 from typing import Optional
+
 
 def var_string(**kwargs):
     s = []
@@ -9,17 +9,15 @@ def var_string(**kwargs):
         s.append(f"{k}={val}")
     return ", ".join(s)
 
+
 def rules(driver):
 
     @driver.basic_rule(AstNode, settings=[".Local", "Global"])
-    def One(context, node, Local: Optional[str]=None, Global: str="default"):
-
+    def One(c, n, Local: Optional[str] = None, Global: Optional[str] = None):
         print(f"In One:", var_string(Local=Local, Global=Global))
         return True
 
     @driver.advanced_rule(settings=[".Local", "Global"])
-    def Two(context, node, Local: Optional[str]=None, Global: str="different default"):
-
+    def Two(c, n, Local: Optional[str] = None, Global: Optional[str] = None):
         print(f"In Two:", var_string(Local=Local, Global=Global))
         yield from []
-
--- tools/chplcheck/src/rule_types.py
+++ tools/chplcheck/src/rule_types.py
@@ -207,6 +207,8 @@ class Rule(typing.Generic[VarResultType], metaclass=ABCMeta):
         # either they have no specific rule name, or they match this rule
         setting_kwargs = {}
         for setting in self.settings:
+            # default to None if the setting is not in the driver's settings
+            setting_kwargs[setting.setting_name] = None
             # if setting is in the driver's settings, use that value
             # otherwise, use the default value (ie pass nothing in the kwargs)
             driver_setting = self.driver.config.rule_settings.get(setting)
