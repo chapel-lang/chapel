1a5262a4e4f24842d652139dad0285c8a16a8bd3
Jade Abraham <jade.abraham@hpe.com>
2024-12-03 12:46:43 -0800

    start adding support for memleaks testing
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

---
+++ test/library/packages/Python/skipIfAndInstallPackage.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+INSTALL_DIR=$1
+shift
+PACKAGE=$1
+shift
+# technically this could be something like `-r requirements.txt` as well
+OTHER_PACKAGES=$@
+
+# respect CHPL_TEST_VENV_DIR if it is set and not none
+if [ -n "$CHPL_TEST_VENV_DIR" ] && [ "$CHPL_TEST_VENV_DIR" != "none" ]; then
+  chpl_python=$CHPL_TEST_VENV_DIR/bin/python3
+else
+  chpl_python=$($CHPL_HOME/util/config/find-python.sh)
+fi
+
+# try and import the first package, if it fails, try and install the packages
+if ! $chpl_python -c "import $PACKAGE" &>/dev/null; then
+
+  # if the SKIP_INSTALL env is set, then we should skip the installation
+  if [ ! -z "$SKIP_INSTALL" ]; then
+    echo "True"
+    exit 0
+  fi
+
+  MY_LIB_DIR=$INSTALL_DIR/python_libs
+  $chpl_python -m pip install $PACKAGE $OTHER_PACKAGES --target=$MY_LIB_DIR 2>&1
+  export PYTHONPATH=$MY_LIB_DIR:$PYTHONPATH
+  if ! $chpl_python -c "import $PACKAGE" &>/dev/null; then
+    echo "True"
+  else
+    echo "False"
+  fi
+else
+  echo "False"
+fi
