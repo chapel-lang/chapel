3ffb65cff2fa74f1e994e949f48ecff0a642395f
Anna Rift <anna.rift@hpe.com>
2024-12-06 10:27:09 -0800

    Consolidate redundant Darwin test config scripts
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/cron/test-darwin.bash
+++ util/cron/test-darwin.bash
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 #
-# Test examples for default configuration on darwin
+# Test full suite for default configuration on darwin
 
 UTIL_CRON_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
 source $UTIL_CRON_DIR/common.bash
 source $UTIL_CRON_DIR/common-darwin.bash
+source $UTIL_CRON_DIR/common-localnode-paratest.bash
 
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="darwin"
 
-$UTIL_CRON_DIR/nightly -cron -examples -blog
+$UTIL_CRON_DIR/nightly -cron $(get_nightly_paratest_args)
