d3076ef928784fe2b105ad5315b8c6eccf4bf852
Anna Rift <anna.rift@hpe.com>
2024-12-06 11:54:50 -0800

    Add check for remaining Python test scripts
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/cron/test-linux64-python39.bash
+++ util/cron/test-linux64-python39.bash
@@ -10,4 +10,13 @@ export CHPL_NIGHTLY_TEST_CONFIG_NAME="linux64-python39"
 
 set_python_version "3.9"
 
+# Check correct Python version loaded
+expected_python_version="3.9.13"
+actual_python_version=$(python3 --version | cut -d' ' -f2)
+if [ "$actual_python_version" != "$expected_python_version" ]; then
+  echo "Wrong Python version"
+  echo "Expected Version: $expected_python_version. Actual Version: $actual_python_version"
+  exit 2
+fi
+
 $UTIL_CRON_DIR/nightly -cron -hellos ${nightly_args}
