0f87b19f18a6d56a0bc2cc64a39e3e1cb95d0695
Jade Abraham <jade.abraham@hpe.com>
2024-12-06 17:12:49 -0800

    add todos
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/cmake/CMakeChplInformation.cmake
+++ util/cmake/CMakeChplInformation.cmake
@@ -1,6 +1,10 @@
+
+# TODO: this cp is a hack, how do we tell cmake that we can't compile to object files?
 set(CMAKE_CHPL_COMPILE_OBJECT "cp <SOURCE> <OBJECT>")
 set(CMAKE_CHPL_LINK_EXECUTABLE "<CMAKE_CHPL_COMPILER> -o <TARGET> <OBJECTS> <FLAGS> <LINK_FLAGS>")
 # <COMPILE_FLAGS> doesn't work on link step
+
+# TODO: support --library directly to build Chapel shared libs for interoperability
 
 set(CMAKE_CHPL_SOURCE_FILE_EXTENSIONS chpl)
 
