8f1d38646fea5c7354b9b44cbe180cf81f03cec2
Jade Abraham <jade.abraham@hpe.com>
2024-12-09 08:35:00 -0800

    make cmake respect CHPL_HOME
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/cmake/CMakeDetermineChplCompiler.cmake
+++ util/cmake/CMakeDetermineChplCompiler.cmake
@@ -1,6 +1,24 @@
+
+# if there is an env CHPL_HOME, use it to find printchplenv
+if (DEFINED ENV{CHPL_HOME})
+  set(CHPL_HOME $ENV{CHPL_HOME})
+
+  execute_process(
+    COMMAND ${CHPL_HOME}/util/printchplenv --value --only CHPL_HOST_BIN_SUBDIR
+    OUTPUT_VARIABLE CHPL_HOST_BIN_SUBDIR
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  set(CHPL_HOME_BIN_PATH ${CHPL_HOME}/bin/${CHPL_HOST_BIN_SUBDIR})
+  # TODO: what if its a prefix install?
+else()
+  set(CHPL_HOME "")
+  set(CHPL_HOME_BIN_PATH "")
+endif()
+
 find_program(
   CMAKE_CHPL_COMPILER
   NAMES "chpl"
+  HINTS ${CHPL_HOME} ${CHPL_HOME_BIN_PATH}
   DOC "chpl compiler"
 )
 # TODO: how to find chpl with just CHPL_HOME?
