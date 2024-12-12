54da6bf0419c7084f1f330d0a0c53a5710d8ff78
Brad Chamberlain <bradcray@users.noreply.github.com>
2024-12-11 16:49:49 -0800

    Update docs version menu for 2.3 release
    This updates the website's docs version menu to reflect tomorrow's
    2.3 release
    
    ---
    Signed-off-by: Brad Chamberlain <bradcray@users.noreply.github.com>

--- doc/util/versionButton.php
+++ doc/util/versionButton.php
@@ -87,8 +87,8 @@ if (pagePath == "") {
   pagePath = "index";
 }
 function dropSetup() {
-  var currentRelease = "2.2"; // what does the public have?
-  var stagedRelease = "2.3";  // is there a release staged but not yet public?
+  var currentRelease = "2.3"; // what does the public have?
+  var stagedRelease = "2.4";  // is there a release staged but not yet public?
   var nextRelease = "2.4";    // what's the next release? (on docs/main)
   var button = document.getElementById("versionButton");
   // Uses unicode down-pointing triangle
