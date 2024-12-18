42f5c3c24841471665b75d217462f1adc122a68e
Jade Abraham <jade.abraham@hpe.com>
2024-12-18 09:11:05 -0800

    add chplenv-verify
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

---
+++ util/chplenv/chplenv_verify.py
@@ -0,0 +1,289 @@
+"""
+Verifies that the current environment is set up correctly for building Chapel, and that once built the compiler will function correctly.
+"""
+
+import functools
+from utils import try_run_command, memoize
+import tempfile
+import shutil
+import sys
+import os
+import shlex
+
+passes = []
+
+def log(*args, **kwargs):
+    kwargs = {**kwargs, "file": sys.stderr}
+    verbose = False
+    if "verbose" in kwargs:
+        verbose = kwargs["verbose"]
+        del kwargs["verbose"]
+    if verbose:
+        print(*args, **kwargs)
+
+def register(_cls=None, *, after=None):
+    def decorator_register(cls):
+        global passes
+        if after:
+            idx = next(
+                i
+                for i, (name, _) in enumerate(passes)
+                if name == after.__name__
+            )
+            passes.insert(idx + 1, (cls.__name__, cls))
+        else:
+            passes.append((cls.__name__, cls))
+
+        @functools.wraps(cls)
+        def wrapper_register(*args, **kwargs):
+            return cls(*args, **kwargs)
+
+        return wrapper_register
+
+    if _cls is None:
+        return decorator_register
+    else:
+        return decorator_register(_cls)
+
+
+class VerificationPass:
+    def __init__(self, chplenv, verbose=False):
+        self.chplenv = chplenv
+        self.verbose = verbose
+        self.msg = None
+
+    def log(self, *args, **kwargs):
+        log("  ", *args, **kwargs, verbose=self.verbose)
+
+    def verify(self):
+        return False
+
+    def explain(self):
+        return self.msg or "Unknown error"
+
+
+class FileCleanupManager:
+    def __init__(self, *files):
+        self.files = list(files)
+
+    def track(self, f):
+        self.files.append(f)
+
+    def cleanup(self):
+        for f in self.files:
+            shutil.rmtree(f)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.cleanup()
+
+
+class TestCompile(VerificationPass):
+    """
+    Try and compile a hello world program
+    """
+
+    def _suffix(self):
+        return ""
+
+    def _lang(self):
+        return ""
+
+    def _compiler(self):
+        return []
+
+    def _compiler_args(self):
+        return []
+
+    def _program(self):
+        return ""
+
+
+    def verify(self):
+        tmpdir = tempfile.mkdtemp()
+
+        hello_file = tempfile.mkstemp(dir=tmpdir, suffix=self._suffix(), text=True)
+        with open(hello_file[1], "w") as f:
+            f.write(self._program())
+
+        with FileCleanupManager(tmpdir) as fcm:
+            cmd = self._compiler() + [hello_file[1], "-o", f"{tmpdir}/a.out"] + self._compiler_args()
+            self.log("Trying to compile with '{}'".format(" ".join(cmd)))
+            succ, ret, out, _ = try_run_command(cmd, combine_output=True)
+            if not succ or ret != 0:
+                out = out if out is not None else "unknown error"
+                self.msg = "Failed to compile program: {}".format(out)
+                return False
+
+            # Run the program
+            cmd = [f"{tmpdir}/a.out"]
+            self.log("Trying to run with '{}'".format(" ".join(cmd)))
+            succ, ret, out, _ = try_run_command(cmd, combine_output=True)
+            if not succ or ret != 0:
+                out = out if out is not None else "unknown error"
+                self.msg = "Failed to run program: {}".format(out)
+                return False
+
+        return True
+
+    def explain(self):
+        if not self.msg:
+            compiler = self._compiler()
+            if not compiler:
+                self.msg = "No compiler found"
+            self.msg = "{} compiler '{}' cannot compile programs".format(self._lang(), compiler[0])
+        return super().explain()
+
+
+@register
+class TestHostCompile(TestCompile):
+    """
+    Try and compile a hello world program using CHPL_HOST_CXX
+    """
+
+    def _suffix(self):
+        return ".cpp"
+
+    def _lang(self):
+        return "C++"
+
+    def _compiler(self):
+        cxx = self.chplenv.get("CHPL_HOST_CXX")
+        if not cxx:
+            self.msg = "CHPL_HOST_CXX is missing"
+            return []
+        return shlex.split(cxx)
+
+    def _program(self):
+        return """
+#include <iostream>
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
+"""
+
+@register(after=TestHostCompile)
+class TestTargetCompileCC(TestCompile):
+    """
+    Try and compile a hello world program using CHPL_TARGET_CC
+    """
+
+    def _suffix(self):
+        return ".c"
+
+    def _lang(self):
+        return "C"
+
+    def _compiler(self):
+        cc = self.chplenv.get("CHPL_TARGET_CC")
+        if not cc:
+            self.msg = "CHPL_TARGET_CC is missing"
+            return []
+        return shlex.split(cc)
+
+    def _program(self):
+        return """
+#include <stdio.h>
+int main() {
+    printf("Hello, World!\\n");
+    return 0;
+}
+"""
+
+
+@register(after=TestTargetCompileCC)
+class TestTargetCompileCXX(TestCompile):
+    """
+    Try and compile a hello world program using CHPL_TARGET_CXX
+    """
+
+    def _suffix(self):
+        return ".cpp"
+
+    def _lang(self):
+        return "C++"
+
+    def _compiler(self):
+        cxx = self.chplenv.get("CHPL_TARGET_CXX")
+        if not cxx:
+            self.msg = "CHPL_TARGET_CXX is missing"
+            return []
+        return shlex.split(cxx)
+
+    def _program(self):
+        return """
+#include <iostream>
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+"""
+
+@register(after=TestHostCompile)
+class TestHostCanFindLLVM(TestCompile):
+    """
+    Try and compile a hello world program using CHPL_HOST_CXX that includes a header from LLVM
+    """
+
+    def _suffix(self):
+        return ".cpp"
+
+    def _lang(self):
+        return "C++"
+
+    def _compiler(self):
+        cxx = self.chplenv.get("CHPL_HOST_CXX")
+        if not cxx:
+            self.msg = "CHPL_HOST_CXX is missing"
+            return []
+        return shlex.split(cxx)
+
+    def _compiler_args(self):
+        comp_args = self.chplenv.get("CHPL_HOST_SYSTEM_COMPILE_ARGS", "")
+        link_args = self.chplenv.get("CHPL_HOST_SYSTEM_LINK_ARGS", "")
+        return super()._compiler_args() + shlex.split(comp_args) + shlex.split(link_args)
+
+    def _program(self):
+        return """
+#include "clang/Basic/Version.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/LLVMContext.h"
+#include <iostream>
+int main() {
+    std::cout << clang::getLLVMRevision() << std::endl;
+    llvm::LLVMContext Context;
+    llvm::Module M("mymod", Context);
+    return 0;
+}
+"""
+
+    def verify(self):
+        # if CHPL_LLVM=none, then we don't need to test this
+        if self.chplenv.get("CHPL_LLVM") == "none":
+            return True
+        return super().verify()
+
+    def explain(self):
+        compiler = self._compiler()
+        if not compiler:
+            return "No compiler found"
+        return "{} cannot find LLVM headers".format(compiler[0])
+
+
+def verify(chplenv, verbose=False):
+
+    mychplenv = {}
+    # Remove leading/trailing whitespace from keys
+    for k, v in chplenv.items():
+        mychplenv[k.strip()] = v
+
+    global passes
+    for name, cls in passes:
+        log(f"Running verification pass {name}", verbose=verbose)
+        p = cls(mychplenv, verbose=verbose)
+        if not p.verify():
+            return (False, p.explain())
+    return (True, None)
