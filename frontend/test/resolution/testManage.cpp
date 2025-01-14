70836b0bd32d190fee7f4ceee708e24c9ed810b0
Danila Fedorin <daniel.fedorin@hpe.com>
2025-01-13 16:11:18 -0800

    Add more tests
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

--- frontend/test/resolution/testManage.cpp
+++ frontend/test/resolution/testManage.cpp
@@ -17,6 +17,8 @@
  * limitations under the License.
  */
 
+#include "chpl/parsing/parsing-queries.h"
+#include "chpl/resolution/resolution-queries.h"
 #include "test-resolution.h"
 
 static void testBasic() {
@@ -65,11 +67,199 @@ static void testResourceByVar() {
 
   auto x = resolveTypesOfVariables(ctx, program, {"x"}).at("x");
   assert(x.type()->isIntType());
+}
 
+static void testResourceByRef() {
+  // test saving the resource into a variable.
+  auto ctx = buildStdContext();
+  ErrorGuard guard(ctx);
+  std::string program =
+    R"""(
+    var global: int;
+    record R : contextManager {
+      proc type contextReturnType type do return int;
+
+      proc enterContext() ref {
+        return global;
+      }
+      proc exitContext(in error: owned Error?) {}
+    }
+    manage new R() as ref res {
+
+    }
+    )""";
+
+  auto x = resolveTypesOfVariables(ctx, program, {"res"}).at("res");
+  assert(!x.isUnknownOrErroneous());
+  assert(x.type()->isIntType());
+  assert(x.isRef());
+}
+
+static void testResourceByConstRef() {
+  // test saving the resource into a variable.
+  auto ctx = buildStdContext();
+  ErrorGuard guard(ctx);
+  std::string program =
+    R"""(
+    var global: int;
+    record R : contextManager {
+      proc type contextReturnType type do return int;
+
+      proc enterContext() ref {
+        return global;
+      }
+      proc exitContext(in error: owned Error?) {}
+    }
+    manage new R() as const ref res {
+
+    }
+    )""";
+
+  auto x = resolveTypesOfVariables(ctx, program, {"res"}).at("res");
+  assert(!x.isUnknownOrErroneous());
+  assert(x.type()->isIntType());
+  assert(x.isConst() && x.isRef());
+}
+
+static void testInferReturn() {
+  // test saving the resource into a variable.
+  auto ctx = buildStdContext();
+  ErrorGuard guard(ctx);
+  std::string program =
+    R"""(
+    record R : contextManager {
+      proc enterContext() {
+        return 42;
+      }
+      proc exitContext(in error: owned Error?) {}
+    }
+    manage new R() as res {}
+    )""";
+
+  auto x = resolveTypesOfVariables(ctx, program, {"res"}).at("res");
+  assert(x.type()->isIntType());
+}
+
+static void testReturnIntentOverload() {
+  // test saving the resource into a variable.
+  auto ctx = buildStdContext();
+  ErrorGuard guard(ctx);
+  std::string program =
+    R"""(
+    module M {
+      var global: int;
+      record R : contextManager {
+        proc type contextReturnType type do return int;
+
+        proc enterContext() {
+          return global;
+        }
+        proc enterContext() ref {
+          return global;
+        }
+        proc enterContext() const ref {
+          return global;
+        }
+        proc exitContext(in error: owned Error?) {}
+      }
+      manage new R() as byVal, new R() as ref byRef, new R() as const ref byConstRef {
+
+      }
+    }
+    )""";
+
+  setFileText(ctx, "test.chpl", program);
+  auto& modules = parsing::parse(ctx, UniqueString::get(ctx, "test.chpl"), UniqueString());
+  assert(modules.size() == 1);
+  auto& rr = resolveModule(ctx, modules[0]->id());
+
+  bool found;
+
+  auto byVal = findVariable(modules[0], "byVal");
+  auto byValParent = parsing::parentAst(ctx, byVal)->toAs()->symbol();
+  assert(byValParent);
+  assert(rr.hasAst(byValParent));
+  found = false;
+  for (auto aa : rr.byAst(byValParent).associatedActions()) {
+    if (aa.action() == AssociatedAction::ENTER_CONTEXT) {
+      found = true;
+      assert(aa.fn()->id().symbolPath() == "M.R.enterContext");
+    }
+  }
+  assert(found);
+
+  auto byRef = findVariable(modules[0], "byRef");
+  auto byRefParent = parsing::parentAst(ctx, byRef)->toAs()->symbol();
+  assert(byRefParent);
+  assert(rr.hasAst(byRefParent));
+  found = false;
+  for (auto aa : rr.byAst(byRefParent).associatedActions()) {
+    if (aa.action() == AssociatedAction::ENTER_CONTEXT) {
+      found = true;
+      assert(aa.fn()->id().symbolPath() == "M.R.enterContext#1");
+    }
+  }
+  assert(found);
+
+  auto byConstRef = findVariable(modules[0], "byConstRef");
+  auto byConstRefParent = parsing::parentAst(ctx, byConstRef)->toAs()->symbol();
+  assert(byConstRefParent);
+  assert(rr.hasAst(byConstRefParent));
+  found = false;
+  for (auto aa : rr.byAst(byConstRefParent).associatedActions()) {
+    if (aa.action() == AssociatedAction::ENTER_CONTEXT) {
+      found = true;
+      assert(aa.fn()->id().symbolPath() == "M.R.enterContext#2");
+    }
+  }
+  assert(found);
+}
+
+static void testNoExplicitImplements() {
+  auto ctx = buildStdContext();
+  ErrorGuard guard(ctx);
+  std::string program =
+    R"""(
+    record R {
+      proc enterContext() {
+        return 42;
+      }
+      proc exitContext(in error: owned Error?) {}
+    }
+    manage new R() as res {}
+    )""";
+
+  auto x = resolveTypesOfVariables(ctx, program, {"res"}).at("res");
+  assert(x.type()->isIntType());
+}
+
+static void testWithoutInterfaceMatch() {
+  auto ctx = buildStdContext();
+  ErrorGuard guard(ctx);
+  std::string program =
+    R"""(
+    record R {
+      proc enterContext() {
+        return 42;
+      }
+      proc exitContext(in err: owned Error?) {} /* wrong name of 'err' */
+    }
+    manage new R() as res {}
+    )""";
+
+  auto x = resolveTypesOfVariables(ctx, program, {"res"}).at("res");
+  assert(x.isUnknownOrErroneous());
+  assert(guard.realizeErrors());
 }
 
 int main() {
   testBasic();
   testResourceByVar();
+  testResourceByRef();
+  testResourceByConstRef();
+  testInferReturn();
+  testReturnIntentOverload();
+  testNoExplicitImplements();
+  testWithoutInterfaceMatch();
   return 0;
 }
