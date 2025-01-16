c2084e04384a0d179f59343e89f97e734aa91cfd
John H. Hartman <jhh67@users.noreply.github.com>
2025-01-16 14:13:53 -0700

    Run complete test suite on HPE Cray EX
    Remove the -examples argument to nightly so the complete test suite is run.
    
    Signed-off-by: John H. Hartman <jhh67@users.noreply.github.com>

--- util/cron/test-hpe-cray-ex-ofi.bash
+++ util/cron/test-hpe-cray-ex-ofi.bash
@@ -11,4 +11,4 @@ export CHPL_NIGHTLY_TEST_CONFIG_NAME="hpe-cray-ex-ofi"
 export CHPL_RT_COMM_OFI_EXPECTED_PROVIDER="cxi"
 export CHPL_RT_MAX_HEAP_SIZE=16g
 
-$UTIL_CRON_DIR/nightly -cron -examples -blog ${nightly_args}
+$UTIL_CRON_DIR/nightly -cron -blog ${nightly_args}
