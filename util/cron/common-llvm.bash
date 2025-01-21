1e15657e5c64eaf2af692a7bfa4ac29db1b05d1a
Anna Rift <anna.rift@hpe.com>
2025-01-21 12:09:43 -0800

    Remove some accomodation for Python 2 in nightly configs
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/cron/common-llvm.bash
+++ util/cron/common-llvm.bash
@@ -25,23 +25,6 @@ esac
 
 if test "$CHPL_LLVM" = bundled; then
 
-    # Ensure that python 2.7 is at front of PATH. This is only done for
-    # llvm configuration because the test systems are _very_ finicky about
-    # python 2.6 due to some environmental configurations.
-    #
-    # Ideally, this would only apply to the `make compile` step in
-    # nightly, but there is not a good way to do that right now. One can
-    # imagine adding a "pre-make-compile hook" to nightly. Maybe an env
-    # var with a comment to executes, but it seems ok for the chpldoc test
-    # to fail on llvm for now.
-    # (thomasvandoren, 2015-03-11)
-     py27_setup=/data/cf/chapel/setup_python27.bash
-    if [ -f "${py27_setup}" ] ; then
-        source ${py27_setup}
-    else
-        echo "[Warning: llvm may not build correctly with python: $(which python)]"
-    fi
-
     # 2021-10-26: Developer-installed cmake version required by LLVM 12
     cmake_setup=/data/cf/chapel/setup_cmake_nightly.bash
     if [ -f "${cmake_setup}" ] ; then
