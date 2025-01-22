0f732a157e42f2f7aa7cdad14f085cd0e7f9e082
Jade Abraham <jade.abraham@hpe.com>
2025-01-21 17:19:02 -0800

    fix build system for stable api
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/config/run-in-venv-with-python-bindings.bash
+++ util/config/run-in-venv-with-python-bindings.bash
@@ -10,13 +10,16 @@ PLATFORM=$("$CHPL_HOME/util/printchplenv" --value --only CHPL_HOST_PLATFORM)
 # Perform checks and set up environment variables for virtual env.
 source $CWD/run-in-venv-common.bash
 
-chpl_frontend_py_deps=$("$python" "$CHPL_HOME/util/chplenv/chpl_home_utils.py" --chpldeps)
+chpl_frontend_py_deps=$("$python" "$CHPL_HOME/util/chplenv/chpl_home_utils.py" --chpldeps-chapel-py)
 export PYTHONPATH="$chpl_frontend_py_deps":$PYTHONPATH
 
 if [ ! -d "$chpl_frontend_py_deps/chapel" ]; then
   python_version=$("$python" -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
   echo "chapel python bindings are not built for python ${python_version}" 1>&2
-  echo "make sure your current python version matches the one used to build chapel" 1>&2
+  # if chpl_frontend_py_deps does not end with 'deps', tell user to match python version
+  if [[ "$chpl_frontend_py_deps" != *deps ]]; then
+    echo "make sure your current python version matches the one used to build chapel" 1>&2
+  fi
   exit 1
 fi
 
