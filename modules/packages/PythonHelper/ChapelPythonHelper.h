76ca5a939be33bb1268045f2e4aef77fb9b56c3b
Jade Abraham <jade.abraham@hpe.com>
2025-01-23 09:47:21 -0800

    initial array shim work
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- modules/packages/PythonHelper/ChapelPythonHelper.h
+++ modules/packages/PythonHelper/ChapelPythonHelper.h
@@ -67,4 +67,82 @@ static inline PyObject* chpl_Py_None(void) { return (PyObject*)Py_None; }
 static inline PyObject* chpl_Py_True(void) { return (PyObject*)Py_True; }
 static inline PyObject* chpl_Py_False(void) { return (PyObject*)Py_False; }
 
+
+#include <stdint.h>
+typedef struct {
+  PyObject_HEAD
+  int64_t* data;
+  Py_ssize_t size;
+} ArrayShimObject;
+static PyTypeObject* ArrayShimType = NULL;
+
+static int ArrayShimObject_init(ArrayShimObject* self, PyObject* args, PyObject* kwargs) {
+  return 0;
+}
+static void ArrayShimObject_dealloc(ArrayShimObject* self) {
+  void (*tp_free)(PyObject*) = (void (*)(PyObject*))(PyType_GetSlot(ArrayShimType, Py_tp_free));
+  if (!tp_free) PyErr_SetString(PyExc_RuntimeError, "Could not free object");
+  else          tp_free((PyObject*) self);
+}
+static PyObject* ArrayShimObject_str(ArrayShimObject* self) {
+  return PyUnicode_FromFormat("ArrayI64(data: 0x%p, size: %zu)", self->data, self->size);
+}
+static int ArrayShimObject_setitem(ArrayShimObject* self, Py_ssize_t index, PyObject* value) {
+  if (!PyLong_Check(value)) {
+    PyErr_SetString(PyExc_TypeError, "ArrayShimObject only supports setting items with integers");
+    return -1;
+  }
+  int64_t val = PyLong_AsLong(value);
+  if (val == -1 && PyErr_Occurred()) return -1;
+  if (index < 0 || index >= self->size) {
+    PyErr_SetString(PyExc_IndexError, "ArrayShimObject index out of range");
+    return -1;
+  }
+  self->data[index] = val;
+  return 0;
+}
+static PyObject* ArrayShimObject_getitem(ArrayShimObject* self, Py_ssize_t index) {
+  if (index < 0 || index >= self->size) {
+    PyErr_SetString(PyExc_IndexError, "ArrayShimObject index out of range");
+    return NULL;
+  }
+  return PyLong_FromLong(self->data[index]);
+}
+static Py_ssize_t ArrayShimObject_length(ArrayShimObject* self) {
+  return self->size;
+}
+
+// should only be called once. returns 0 on failure, 1 on success
+static int createArrayShimType(void) {
+  PyType_Slot slots[] = {
+    {Py_tp_init, (void*) ArrayShimObject_init},
+    {Py_tp_dealloc, (void*) ArrayShimObject_dealloc},
+    {Py_tp_doc, (void*) PyDoc_STR("A shim to allow Chapel arrays to be used in Python")},
+    {Py_tp_new, (void*) PyType_GenericNew},
+    {Py_tp_str, (void*) ArrayShimObject_str},
+    {Py_sq_ass_item, (void*) ArrayShimObject_setitem},
+    {Py_sq_item, (void*) ArrayShimObject_getitem},
+    {Py_mp_length, (void*)ArrayShimObject_length},
+    {0, NULL}
+  };
+  PyType_Spec spec = {
+    /*name*/ "ArrayI64", // TODO: qualified name
+    /*basicsize*/ sizeof(ArrayShimObject),
+    /*itemsize*/ 0,
+    /*flags*/ Py_TPFLAGS_DEFAULT,
+    /*slots*/ slots
+  };
+  ArrayShimType = (PyTypeObject*)PyType_FromSpec(&spec);
+  if (!ArrayShimType || PyType_Ready(ArrayShimType) < 0) return 0;
+  return 1;
+}
+
+static PyObject* createArrayShim(int64_t* data, Py_ssize_t size) {
+  PyObject* objPy = PyObject_CallNoArgs((PyObject *) ArrayShimType);
+  ArrayShimObject* obj = (ArrayShimObject*) objPy;
+  obj->data = data;
+  obj->size = size;
+  return objPy;
+}
+
 #endif
