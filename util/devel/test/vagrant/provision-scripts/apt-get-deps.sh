34c0f136dced838011b377611a076e56163224f3
Ahmad Rezaii <ahmad.rezaii@hpe.com>
2025-03-11 11:10:23 -0600

    isolate non-interactive to ubuntu-plucky
    Signed-off-by: Ahmad Rezaii <ahmad.rezaii@hpe.com>

--- util/devel/test/vagrant/provision-scripts/apt-get-deps.sh
+++ util/devel/test/vagrant/provision-scripts/apt-get-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y gcc g++ m4 perl python3 python3-dev bash make mawk git pkg-config cmake
+apt-get install -y gcc g++ m4 perl python3 python3-dev bash make mawk git pkg-config cmake
