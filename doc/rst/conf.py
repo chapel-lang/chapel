aec520d74fe9576c04557acfc151b1caf01d9fad
Anna Rift <anna.rift@hpe.com>
2025-03-12 11:24:52 -0700

    Set 2.4 to official release mode
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- doc/rst/conf.py
+++ doc/rst/conf.py
@@ -137,7 +137,7 @@ shortversion = chplversion.replace('-', '&#8209') # prevent line-break at hyphen
 html_context = {"chplversion":chplversion}
 
 # The full version, including alpha/beta/rc tags.
-release = '2.4.0 (pre-release)'
+release = '2.4.0'
 
 # General information about the project.
 project = u'Chapel Documentation'
