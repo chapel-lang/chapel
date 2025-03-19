e1556c384875a5c1a84c3e3d404ccd6015141237
Ben Harshbarger <ben.harshb@gmail.com>
2025-03-19 10:34:29 -0700

    Remove QIO_STYLE_ELEMENT_SKIP_UNKNOWN_FIELDS
    Signed-off-by: Ben Harshbarger <ben.harshb@gmail.com>

--- runtime/include/qio/qio_style.h
+++ runtime/include/qio/qio_style.h
@@ -45,7 +45,6 @@ typedef uint8_t style_char_t;
 #define QIO_STYLE_ELEMENT_TUPLE 5
 #define QIO_STYLE_ELEMENT_BYTE_ORDER 6
 #define QIO_STYLE_ELEMENT_IS_NATIVE_BYTE_ORDER 7
-#define QIO_STYLE_ELEMENT_SKIP_UNKNOWN_FIELDS 8
 
 
 // If these values change, also change iostringformat in IO.chpl
@@ -218,11 +217,6 @@ typedef struct qio_style_s {
   // QIO_TUPLE_FORMAT_CHPL make it look like (a,b,c)
   // QIO_TUPLE_FORMAT_JSON make it look like a JSON array [1,2]
   uint8_t tuple_style;
-
-  // If this is set, skip any unknown record/class fields
-  // when reading (ie the data to read might have more fields
-  // than a record, and the additional fields will be ignored).
-  uint8_t skip_unknown_fields;
 
 } qio_style_t;
 
