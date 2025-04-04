e99ca2fbc4a2943a8a3570f021b3bbbcd38904b7
Jade Abraham <jade.abraham@hpe.com>
2025-04-04 07:43:52 -0700

    add C_contiguous
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- modules/packages/PythonHelper/ChapelPythonHelper.h
+++ modules/packages/PythonHelper/ChapelPythonHelper.h
@@ -102,5 +102,6 @@ static const int chpl_PyBUF_WRITABLE = PyBUF_WRITABLE;
 static const int chpl_PyBUF_FORMAT = PyBUF_FORMAT;
 static const int chpl_PyBUF_ND = PyBUF_ND;
 static const int chpl_PyBUF_STRIDES = PyBUF_STRIDES;
+static const int chpl_PyBUF_C_CONTIGUOUS = PyBUF_C_CONTIGUOUS;
 
 #endif
