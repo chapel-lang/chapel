f7e79dbd5b56ad07b7f2e39890ed0edbff3df5db
Jade Abraham <jade.abraham@hpe.com>
2025-04-23 10:14:12 -0700

    rework the default for launcher selection to avoid surprising defaults
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/chplenv/chpl_launcher.py
+++ util/chplenv/chpl_launcher.py
@@ -29,29 +29,7 @@ def get():
     if not launcher_val:
         platform_val = chpl_platform.get('target')
 
-        if platform_val.startswith('cray-x') or platform_val.startswith('hpe-cray-'):
-            has_aprun = which('aprun')
-            has_slurm = which('srun')
-            if has_aprun and has_slurm:
-                launcher_val = 'none'
-                warning("Both 'aprun' and 'srun' are available on this system. Please explicitly set CHPL_LAUNCHER.")
-            elif has_aprun:
-                launcher_val = 'aprun'
-            elif has_slurm:
-                launcher_val = 'slurm-srun'
-            else:
-                # FIXME: Need to detect aprun/srun differently. On a cray
-                #        system with an eslogin node, it is possible that aprun
-                #        will not be available on the eslogin node (only on the
-                #        login node).
-                #
-                #        has_aprun and has_slurm should look other places
-                #        (maybe the modules?) to decide.
-                #        (thomasvandoren, 2014-08-12)
-                launcher_val = 'none'
-                warning('Cannot detect launcher on this system. Please '
-                        'set CHPL_LAUNCHER in the environment.')
-        elif comm_val == 'gasnet':
+        if comm_val == 'gasnet':
             if substrate_val == 'smp':
                 launcher_val = 'smp'
             elif substrate_val == 'mpi':
@@ -63,10 +41,35 @@ def get():
             elif substrate_val == 'ofi':
                 launcher_val = slurm_prefix('gasnetrun_ofi')
         else:
-            if which('srun'):
-                launcher_val = 'slurm-srun'
+            is_known_cluster = (platform_val.startswith('cray-') or
+                                platform_val.startswith('hpe-'))
+            if is_known_cluster:
+                has_aprun = which('aprun')
+                has_slurm = which('srun')
+                if has_aprun and has_slurm:
+                    launcher_val = 'none'
+                    warning("Both 'aprun' and 'srun' are available on this system. Please explicitly set CHPL_LAUNCHER.")
+                elif has_aprun:
+                    launcher_val = 'aprun'
+                elif has_slurm:
+                    launcher_val = 'slurm-srun'
+                else:
+                    # FIXME: Need to detect aprun/srun differently. On a cray
+                    #        system with an eslogin node, it is possible that aprun
+                    #        will not be available on the eslogin node (only on the
+                    #        login node).
+                    #
+                    #        has_aprun and has_slurm should look other places
+                    #        (maybe the modules?) to decide.
+                    #        (thomasvandoren, 2014-08-12)
+                    launcher_val = 'none'
+                    warning('Cannot detect launcher on this system. Please '
+                            'set CHPL_LAUNCHER in the environment.')
             else:
-                launcher_val = 'none'
+                if comm_val != "none" and which('srun'):
+                    launcher_val = 'slurm-srun'
+                else:
+                    launcher_val = 'none'
 
     if launcher_val is None:
         launcher_val = 'none'
