243c24c4303841f764000c9da1d80a52688dbf81
Jade Abraham <jade.abraham@hpe.com>
2025-04-29 15:26:46 -0700

    fix typo
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/chplenv/chpl_gpu.py
+++ util/chplenv/chpl_gpu.py
@@ -554,7 +554,7 @@ def _validate_rocm_version_impl():
 
     if not is_ver_in_range(rocm_version, MIN_REQ_VERSION, MAX_REQ_VERSION) and not is_ver_in_range(rocm_version, MIN_ROCM6_REQ_VERSION, MAX_ROCM6_REQ_VERSION):
         _reportMissingGpuReq(
-            "Chapel requires ROCm versions %s to %s or %s and %s, "
+            "Chapel requires ROCm versions %s to %s or %s to %s, "
             "detected version %s on system." %
             (MIN_REQ_VERSION, MAX_REQ_VERSION_NICE, MIN_ROCM6_REQ_VERSION, MAX_ROCM6_REQ_VERSION_NICE, rocm_version))
         return False
