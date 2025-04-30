0ebe0135efe31cbe4ac95c2a2cf6c4b0374c77c2
Anna Rift <anna.rift@hpe.com>
2025-04-30 10:20:33 -0700

    Use 4 threads for make
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/devel/test/vagrant/chapeldefaultcheck.sh
+++ util/devel/test/vagrant/chapeldefaultcheck.sh
@@ -3,4 +3,4 @@
 # Check if default (util/setchplenv) Chapel build passes make check
 # Prints summary at the end
 
-./tryit.sh 'bash -c '\''cd chapel && source util/setchplenv.bash && export GMAKE=`which gmake` && export MAKE=${GMAKE:-make} && $MAKE && $MAKE check'\'
+./tryit.sh 'bash -c '\''cd chapel && source util/setchplenv.bash && export GMAKE=`which gmake` && export MAKE=${GMAKE:-make} && $MAKE -j4 && $MAKE -j4 check'\'
