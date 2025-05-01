49ce7f721c99e381aa4036570e16cb586e144c7b
Ben Harshbarger <ben.harshb@gmail.com>
2025-05-01 16:38:33 -0700

    add a breakpoint in CHPL_ASSERT to help with lldb scripts
    Signed-off-by: Ben Harshbarger <ben.harshb@gmail.com>

--- frontend/lib/util/assertions.cpp
+++ frontend/lib/util/assertions.cpp
@@ -57,6 +57,7 @@ void assertion(bool expr, const char* filename, const char* func,
     fprintf(stderr, "[%s:%d in %s] Assertion failed: %s\n",
             filename, lineno, func, exprText);
     if (assertionsAreFatal()) {
+      debuggerBreakHere();
       std::abort();
     }
   }
