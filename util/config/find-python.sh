556030cb66c3dc9c8c54d03c9feda4b459feb5ae
Jade Abraham <jade.abraham@hpe.com>
2025-05-07 20:33:20 -0700

    add python version checking
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/config/find-python.sh
+++ util/config/find-python.sh
@@ -7,8 +7,15 @@ check_python() {
   then
     if $1 --version > /dev/null 2>&1
     then
-      echo $1
-      exit 0
+      MIN_MINOR_PYTHON_VERSION=5
+      if $1 -c "import sys; sys.exit(int(sys.version_info[:2] >= (3, $MIN_MINOR_PYTHON_VERSION)))" >/dev/null 2>&1
+      then
+        vers=$($1 --version 2>&1)
+        echo "Chapel requires Python 3.$MIN_MINOR_PYTHON_VERSION or later, but found $vers" 1>&2
+      else
+        echo $1
+        exit 0
+      fi
     fi
   fi
 }
