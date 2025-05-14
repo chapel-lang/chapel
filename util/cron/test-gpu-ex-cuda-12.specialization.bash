cddb6ad680f6514e1d5a8a8196e9884fe7ebb156
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2025-05-14 16:11:38 -0700

    Add the missing include to the GPU specialization config
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- util/cron/test-gpu-ex-cuda-12.specialization.bash
+++ util/cron/test-gpu-ex-cuda-12.specialization.bash
@@ -6,6 +6,7 @@ UTIL_CRON_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
 source $UTIL_CRON_DIR/common-native-gpu.bash
 source $UTIL_CRON_DIR/common-hpe-cray-ex.bash
 source $UTIL_CRON_DIR/common-gpu-hpe-cray-ex.bash
+source $UTIL_CRON_DIR/common-gpu-nvidia-hpe-cray-ex.bash
 
 module load cuda/12.4
 
