83001b1cd9d2b71846b01e54c0db64fb5124a430
Anna Rift <anna.rift@hpe.com>
2025-05-15 11:54:33 -0700

    Split repo adjustment step into separate provision script
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/devel/test/vagrant/provision-scripts/freebsd-pkg-deps.sh
+++ util/devel/test/vagrant/provision-scripts/freebsd-pkg-deps.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-mkdir -p /usr/local/etc/pkg/repos
-echo 'FreeBSD: { url: "https://pkg.FreeBSD.org/${ABI}/quarterly", mirror_type: "none" }' > /usr/local/etc/pkg/repos/FreeBSD.conf
-
 pkg install --yes gcc m4 perl5 python3 bash gmake gawk git pkgconf cmake
