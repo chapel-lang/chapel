48af6c3859df6cbc9031d94d07d9eb488667cef9
Brad Chamberlain <bradcray@users.noreply.github.com>
2025-05-22 22:51:24 -0700

    One more removal of debug print
    ---
    Signed-off-by: Brad Chamberlain <bradcray@users.noreply.github.com>

--- compiler/include/symbol.h
+++ compiler/include/symbol.h
@@ -705,7 +705,7 @@ inline bool Symbol::hasFlag(Flag flag) const {
 inline void Symbol::addFlag(Flag flag) {
   CHECK_FLAG(flag);
   if (id == 832287 && flag == FLAG_NO_AUTO_DESTROY) {
-    printf("Hey, wait!\n");
+    //    printf("Hey, wait!\n");
   }// else {  // Skipping this closes the leak
   flags.set(flag);
   //  }
