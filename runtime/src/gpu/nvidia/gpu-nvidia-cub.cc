132d6f7c291583f2ad2864fa0f7b7461ce3e393d
Jade Abraham <jade.abraham@hpe.com>
2025-05-27 08:12:19 -0700

    add missing checks for error codes on calls to cub
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- runtime/src/gpu/nvidia/gpu-nvidia-cub.cc
+++ runtime/src/gpu/nvidia/gpu-nvidia-cub.cc
@@ -48,11 +48,11 @@ void chpl_gpu_impl_##chpl_kind##_reduce_##data_type(data_type* data, int n,\
   CUDA_CALL(cuMemAlloc(&result, sizeof(data_type))); \
   void* temp = NULL; \
   size_t temp_bytes = 0; \
-  cub::DeviceReduce::cub_kind(temp, temp_bytes, data, (data_type*)result, n,\
-                              (CUstream)stream); \
+  CUDA_CALL(cub::DeviceReduce::cub_kind(temp, temp_bytes, data, (data_type*)result, n,\
+                              (CUstream)stream)); \
   CUDA_CALL(cuMemAlloc(((CUdeviceptr*)&temp), temp_bytes)); \
-  cub::DeviceReduce::cub_kind(temp, temp_bytes, data, (data_type*)result, n,\
-                              (CUstream)stream); \
+  CUDA_CALL(cub::DeviceReduce::cub_kind(temp, temp_bytes, data, (data_type*)result, n,\
+                              (CUstream)stream)); \
   CUDA_CALL(cuMemcpyDtoHAsync(val, result, sizeof(data_type),\
                               (CUstream)stream)); \
   CUDA_CALL(cuMemFree(result)); \
@@ -74,11 +74,11 @@ void chpl_gpu_impl_##chpl_kind##_reduce_##data_type(data_type* data, int n,\
   CUDA_CALL(cuMemAlloc(&result, sizeof(kvp))); \
   void* temp = NULL; \
   size_t temp_bytes = 0; \
-  cub::DeviceReduce::cub_kind(temp, temp_bytes, data, (kvp*)result, n,\
-                              (CUstream)stream);\
+  CUDA_CALL(cub::DeviceReduce::cub_kind(temp, temp_bytes, data, (kvp*)result, n,\
+                              (CUstream)stream));\
   CUDA_CALL(cuMemAlloc(((CUdeviceptr*)&temp), temp_bytes)); \
-  cub::DeviceReduce::cub_kind(temp, temp_bytes, data, (kvp*)result, n,\
-                              (CUstream)stream);\
+  CUDA_CALL(cub::DeviceReduce::cub_kind(temp, temp_bytes, data, (kvp*)result, n,\
+                              (CUstream)stream));\
   kvp result_host; \
   CUDA_CALL(cuMemcpyDtoHAsync(&result_host, result, sizeof(kvp),\
                               (CUstream)stream)); \
