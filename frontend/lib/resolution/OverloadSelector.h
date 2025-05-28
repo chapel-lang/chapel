54fc37fd26d2fc3563582633c18fd90459dbcaa4
Danila Fedorin <daniel.fedorin@hpe.com>
2025-05-28 10:16:46 -0700

    Don't rely on function pointers being constexpr
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

--- frontend/lib/resolution/OverloadSelector.h
+++ frontend/lib/resolution/OverloadSelector.h
@@ -7,35 +7,53 @@ namespace resolution {
 
 using BuilderFunc = uast::BuilderResult const&(*)(Context* context, ID typeId, int overloadIdx);
 
+// note: this uses a templated implementation because in modern C++,
+// we should be able to make `dispatch` select the function at
+// compile time. However, presently, some of the older compilers we support
+// do not consider function pointers to be constant expressions, so we
+// instead use an array that's iterated at runtime.
+
+/*
+ The OverloadSelector is a utility struct that keeps track of the ordering
+ of compiler-generated overloads. Specifically, there are two processes that
+ occur when a compiler-generated function is handled:
+
+ 1. When a user call occurs with certain types, the frontend decides if the
+    compiler should generate a matching overload. At this point, the builder
+    function is invoked. The builder function needs to know the index
+    of the overload, since this index is reflected in the generated IDs.
+
+    Here, the `invoke` method can be used to request a call to the appropriate
+    builder function, with OverloadSelector providing the function its index
+    automatically based on its position in the overload list.
+ 2. Later, during resolution, we need to retrieve the generated AST for the
+    function from its ID. To do this, we effectively have to invoke
+    the builder function again. This time, we know the index (it's encoded
+    in the overload ID, like `foo#1`), but we don't know the function.
+
+    Here, the `select` method handles mapping the index back to the function
+    that produced it.
+
+  By having the OverloadSelector handle both calls to the builder function
+  described above, we ensure a consistent ordering of overloads.
+ */
 template <BuilderFunc... Fs>
 struct OverloadSelector {
+ private:
+  static constexpr std::array<BuilderFunc, sizeof...(Fs)> overloads = {Fs...};
 
-  template <int idx, BuilderFunc F, BuilderFunc Head, BuilderFunc ... Tail>
-  static std::enable_if_t<F == Head, uast::BuilderResult const&> invokeImpl(Context* context, const ID& id) {
-    return F(context, id, idx);
-  }
-
-  template <int idx, BuilderFunc F, BuilderFunc Head, BuilderFunc ... Tail>
-  static std::enable_if_t<F != Head, uast::BuilderResult const&> invokeImpl(Context* context, const ID& id) {
-    return invokeImpl<idx + 1, F, Tail...>(context, id);
-  }
-
+ public:
   template <BuilderFunc F>
   static uast::BuilderResult const& invoke(Context* context, const ID& id) {
-    return invokeImpl<0, F, Fs...>(context, id);
-  }
-
-  template <BuilderFunc Head, BuilderFunc ... Tail>
-  static uast::BuilderResult const& selectImpl(Context* context, const ID& id, int targetIdx, int currentIdx) {
-    if (currentIdx == targetIdx) {
-      return Head(context, id, currentIdx);
-    } else {
-      if constexpr (sizeof...(Tail) == 0) {
-        throw std::out_of_range("Index out of range");
-      } else {
-        return selectImpl<Tail...>(context, id, targetIdx, currentIdx + 1);
+    int idx = 0;
+    for (auto overload : overloads) {
+      if (overload == F) {
+        return F(context, id, idx);
       }
+      idx++;
     }
+    CHPL_ASSERT(false && "overload not found, is the function registered?");
+    return overloads[0](context, id, 0); // Fallback to the first overload
   }
 
   static uast::BuilderResult const& select(Context* context, const ID& id, UniqueString overloadPart) {
@@ -46,7 +64,15 @@ struct OverloadSelector {
       targetIdx = atoi(overloadPart.c_str());
     }
 
-    return selectImpl<Fs...>(context, id, targetIdx, 0);
+    int idx = 0;
+    for (auto overload : overloads) {
+      if (idx == targetIdx) {
+        return overload(context, id, idx);
+      }
+      idx++;
+    }
+    CHPL_ASSERT(false && "overload not found, was the overloadPart incorrectly constructed?");
+    return overloads[0](context, id, 0); // Fallback to the first overload
   }
 };
 
