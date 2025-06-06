c14110c4cb18dad1714d976ee44cc1f4ca32e1f4
Anna Rift <anna.rift@hpe.com>
2025-06-06 11:04:53 -0700

    Add Darwin quickstart test script
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

---
+++ util/cron/test-darwin-quickstart.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# Test quickstart configuration on examples only, on darwin
+
+UTIL_CRON_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
+source $UTIL_CRON_DIR/common-quickstart.bash
+source $UTIL_CRON_DIR/common-darwin.bash
+source $UTIL_CRON_DIR/common-localnode-paratest.bash
+
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="darwin"
+
+$UTIL_CRON_DIR/nightly -cron -examples $(get_nightly_paratest_args)
