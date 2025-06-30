139e983b3f55da5c3c9d7bc60c39db034bca76dd
Anna Rift <anna.rift@hpe.com>
2025-06-30 13:45:26 -0700

    Fix paths in running other provision scripts
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/devel/test/portability/provision-scripts/chapel-default.sh
+++ util/devel/test/portability/provision-scripts/chapel-default.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+PROVISION_SCRIPTS_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
+
 if [ ! -d chapel ]
 then
-  ../../provision-scripts/chapel-update.sh
+  $PROVISION_SCRIPTS_DIR/chapel-update.sh
 fi
 
-source ../../provision-scripts/chapel-setmakej.sh
+source $PROVISION_SCRIPTS_DIR/chapel-setmakej.sh
 
 echo "cd chapel && source util/setchplenv.bash && make $MAKEJ && make check"
 cd chapel && source util/setchplenv.bash && make $MAKEJ && make check
