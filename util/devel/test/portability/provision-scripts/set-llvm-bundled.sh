790532329f327c52e0d4cbc95ffe0f5b3870e3c6
Anna Rift <anna.rift@hpe.com>
2025-07-17 10:52:50 -0700

    Add TODO note about the use of sudo
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/devel/test/portability/provision-scripts/set-llvm-bundled.sh
+++ util/devel/test/portability/provision-scripts/set-llvm-bundled.sh
@@ -3,4 +3,7 @@
 alias unsudo="sudo -u bin"
 alias hide=eval
 
+# NOTE: Root is not needed here, but using `unsudo` doesn't work as it's an
+# invocation of `sudo` and you can't use shell builtins like `export` with
+# `sudo`.
 export CHPL_LLVM=bundled
