af60353fe5cf4a88ef4dfdd7ce98f5d4fc87ed55
David Longnecker <dlongnecke-cray@users.noreply.github.com>
2025-07-30 16:26:16 -0700

    Make progress towards supporting procedure pointers with C backend
    Remove some old code doing GC of 'FunctionType'. It's not doing what
    I thought it did and could cause 'null' holes in the various 'gVec'
    vectors (e.g., 'gTypeSymbols'). I'll worry about GC with '--verify'
    at a later point in this effort.
    
    There is context-sensitive code in 'SymExpr::codegen()' which checks
    to see how a 'SymExpr' over a 'FnSymbol' is used. If it looks like
    it's being used as value, we generate it differently by adding it to
    the procedure pointer cache and producing a wide index.
    
    Previously this code was only firing for LLVM. I moved it out into a
    more "backend agnostic" block of code.
    
    I added code to generate a C function type from a 'FunctionType', but
    currently that code is commented out. I am opting to generate 'wide
    proc' as 'int64_t' and 'local proc' as 'void*' in order to work around
    circular dependencies in C (e.g., a record contains a procedure
    pointer and the procedure pointer accepts that record as a formal).
    
    Signed-off-by: David Longnecker <dlongnecke-cray@users.noreply.github.com>

--- compiler/codegen/cg-type.cpp
+++ compiler/codegen/cg-type.cpp
@@ -93,58 +93,101 @@ void PrimitiveType::codegenDef() {
 void ConstrainedType::codegenDef() {
 }
 
-#ifdef HAVE_LLVM
-static void codegenFunctionTypeWideLlvm(FunctionType* ft) {
-  // Use a 'int64' dynamic index. Do not bother generating a function type.
-  auto t = llvm::Type::getInt64Ty(gContext->llvmContext());
-  if (!ft->symbol->hasLLVMType()) {
-    gGenInfo->lvt->addGlobalType(ft->symbol->cname, t, false);
-    ft->symbol->llvmImplType = t;
-    ft->symbol->llvmAlignment = ALIGNMENT_DEFER;
+static void codegenFunctionTypeWide(FunctionType* ft, bool forLlvm) {
+  if (forLlvm) {
+  #ifdef HAVE_LLVM
+    // Use a 'int64' dynamic index. Do not bother generating a function type.
+    auto t = llvm::Type::getInt64Ty(gContext->llvmContext());
+    if (!ft->symbol->hasLLVMType()) {
+      gGenInfo->lvt->addGlobalType(ft->symbol->cname, t, false);
+      ft->symbol->llvmImplType = t;
+      ft->symbol->llvmAlignment = ALIGNMENT_DEFER;
+    }
+  #endif
+  } else {
+    auto outfile = gGenInfo->cfile;
+    INT_ASSERT(outfile);
+
+    // TODO: Should I get the code-generated 'int(64)' type here instead?
+    fprintf(outfile, "typedef %s %s;\n\n", "int64_t", ft->symbol->cname);
   }
 }
-#endif
 
-#ifdef HAVE_LLVM
-static void codegenFunctionTypeLocalLlvm(FunctionType* ft) {
-  // Generate the underlying type. This is a function value type.
-  const auto& info = fetchLocalFunctionTypeLlvm(ft);
-  auto baseType = info.type;
+static void codegenFunctionTypeLocal(FunctionType* ft, bool forLlvm) {
+  if (forLlvm) {
+  #ifdef HAVE_LLVM
+    // Generate the underlying type. This is a function value type.
+    const auto& info = fetchLocalFunctionTypeLlvm(ft);
+    auto baseType = info.type;
 
-  // The final type is a pointer to the underlying function type.
-  auto& layout = gGenInfo->module->getDataLayout();
-  auto addrSpace = layout.getAllocaAddrSpace();
-  llvm::Type* t = llvm::PointerType::get(baseType, addrSpace);
+    // The final type is a pointer to the underlying function type.
+    auto& layout = gGenInfo->module->getDataLayout();
+    auto addrSpace = layout.getAllocaAddrSpace();
+    llvm::Type* t = llvm::PointerType::get(baseType, addrSpace);
 
-  if (!ft->symbol->hasLLVMType()) {
-    gGenInfo->lvt->addGlobalType(ft->symbol->cname, t, false);
-    ft->symbol->llvmImplType = t;
-    ft->symbol->llvmAlignment = ALIGNMENT_DEFER;
+    if (!ft->symbol->hasLLVMType()) {
+      gGenInfo->lvt->addGlobalType(ft->symbol->cname, t, false);
+      ft->symbol->llvmImplType = t;
+      ft->symbol->llvmAlignment = ALIGNMENT_DEFER;
+    }
+  #endif
+  } else {
+    auto outfile = gGenInfo->cfile;
+    INT_ASSERT(outfile);
+
+    /* TODO (dlongnecke): May have no relevance now.
+
+    // Assemble a string representing the function type in C.
+    std::string str;
+
+    // E.g., 'void (*theTypedefName)(argt1, argt2, argt3)';
+    str += ft->returnType()->codegen().c.c_str();
+    str += "(*";
+    str += ft->symbol->cname;
+    str += ")(";
+
+    // TODO: Keep the formal names omitted here?
+    for (int i = 0; i < ft->numFormals(); i++) {
+      const bool last = (i+1) == ft->numFormals();
+      auto formal = ft->formal(i);
+      INT_ASSERT(formal);
+
+      // Emit the type (neither C nor Chapel cares about formal names now).
+      str += formal->type()->codegen().c.c_str();
+      if (!last) str += ", ";
+    }
+
+    str += ")";
+
+    fprintf(outfile, "typedef %s;\n\n", str.c_str());
+    */
+
+    // Declare local procs as 'void*' to handle circular dependencies.
+    fprintf(outfile, "typedef %s %s;\n\n", "void*", ft->symbol->cname);
   }
 }
-#endif
 
-// TODO: See 'codegenFunctionTypeLLVM' for hints about what ABI stuff to
-// do when code generating extern/export stuff. It's a mess in there!
 void FunctionType::codegenDef() {
-  auto info = gGenInfo;
-  auto outfile = info->cfile;
-
   // This must hold, otherwise we'd be using class instances instead...
   INT_ASSERT(fcfs::usePointerImplementation());
 
-  if (outfile) {
-    INT_FATAL("The C backend is not supported yet!");
+  #ifdef HAVE_LLVM
+  if (gGenInfo->cfile == nullptr) {
+    // A LLVM-only precondition to make sure we remain idempotent.
+    llvm::Type *type = gGenInfo->lvt->getType(symbol->cname);
+    if (type) return;
+  }
+
+  // Now set a flag to indicate what backend we are generating for.
+  const bool forLlvm = true && gGenInfo->cfile == nullptr;
+  #else
+  const bool forLlvm = false;
+  #endif
+
+  if (this->isWide()) {
+    codegenFunctionTypeWide(this, forLlvm);
   } else {
-    #ifdef HAVE_LLVM
-      llvm::Type *type = info->lvt->getType(symbol->cname);
-      if (type) return;
-      if (this->isWide()) {
-        codegenFunctionTypeWideLlvm(this);
-      } else {
-        codegenFunctionTypeLocalLlvm(this);
-      }
-    #endif
+    codegenFunctionTypeLocal(this, forLlvm);
   }
 }
 
