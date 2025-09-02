ad1b26b5c84e99eb2d190be616fb7c463027bab0
Ben Harshbarger <ben.harshb@gmail.com>
2025-09-02 16:23:37 -0700

    Remove old compiler workaround for deprecated 'kind' field
    Signed-off-by: Ben Harshbarger <ben.harshb@gmail.com>

--- compiler/resolution/preFold.cpp
+++ compiler/resolution/preFold.cpp
@@ -3248,18 +3248,6 @@ static Symbol* determineQueriedField(CallExpr* call) {
     Vec<Symbol*> args;
     int position = var->immediate->int_value();
 
-    // A couple of variables to help us deal with the deprecated 'kind' field
-    // in fileReader and fileWriter.
-    bool isReaderWriter = false;
-    bool specifiesKind = false;
-
-    {
-      AggregateType* root = at->getRootInstantiation();
-      isReaderWriter = root->getModule() == ioModule &&
-          (strcmp(root->symbol->name, "fileReader") == 0 ||
-           strcmp(root->symbol->name, "fileWriter") == 0);
-    }
-
     if (at->symbol->hasFlag(FLAG_TUPLE)) {
       return at->getField(position);
     }
@@ -3295,23 +3283,12 @@ static Symbol* determineQueriedField(CallExpr* call) {
 
       INT_ASSERT(var->immediate->const_kind == CONST_KIND_STRING);
 
-      if (isReaderWriter &&
-          strcmp("kind", var->immediate->v_string.c_str()) == 0) {
-        specifiesKind = true;
-      }
-
       for (int j = 0; j < args.n; j++) {
         if (args.v[j] != NULL &&
             strcmp(args.v[j]->name, var->immediate->v_string.c_str()) == 0) {
           args.v[j] = NULL;
         }
       }
-    }
-
-    // Need to increment by one so that expressions like 'fileWriter(false)'
-    // match up correctly.
-    if (isReaderWriter && !specifiesKind) {
-      position += 1;
     }
 
     forv_Vec(Symbol, arg, args) {
