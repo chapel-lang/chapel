dea1ae9f4ddc68e33da7ea944cafe6c0fc50cc63
Anna Rift <anna.rift@hpe.com>
2025-09-05 10:13:06 -0700

    Add a small amount of logging to test-release.bash, and exit on error
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/cron/test-release.bash
+++ util/cron/test-release.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -e
+set -x
+
 UTIL_CRON_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
 source $UTIL_CRON_DIR/common.bash
 
@@ -7,5 +10,6 @@ source $UTIL_CRON_DIR/common.bash
 WORKSPACE=${WORKSPACE:-$UTIL_CRON_DIR/../..}
 export TMPDIR=$(mktemp -d $WORKSPACE/chapel-test-release.XXXXXX)
 export CHPL_GEN_RELEASE_TMPDIR=$TMPDIR
+echo "Using tmpdir location: $TMPDIR"
 
 $UTIL_CRON_DIR/../buildRelease/testRelease
