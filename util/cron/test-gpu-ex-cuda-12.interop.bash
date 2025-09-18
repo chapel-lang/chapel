65b895d00fade4c02968f50dab1de38c46a1eb77
Jade Abraham <jade.abraham@hpe.com>
2025-09-18 09:15:52 -0700

    remove cublas pin
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/cron/test-gpu-ex-cuda-12.interop.bash
+++ util/cron/test-gpu-ex-cuda-12.interop.bash
@@ -8,12 +8,6 @@ source $UTIL_CRON_DIR/common-hpe-cray-ex.bash
 source $UTIL_CRON_DIR/common-gpu-nvidia-hpe-cray-ex.bash
 source $UTIL_CRON_DIR/common-gpu-nvidia-hpe-cray-ex-cuda-12.bash
 
-# We need cublas for the cublas interop test, but since we are using 12.4 above
-# pinoak doesn't have the cublas library for 12.4, so we need to use the cublas
-# from 12.5 (which is compatible across minor versions)
-# This can be removed once we use CUDA 12.5
-export CHPL_LIB_PATH="/opt/nvidia/hpc_sdk/Linux_x86_64/24.7/math_libs/lib64:$CHPL_LIB_PATH"
-
 export CHPL_TEST_GPU=true
 export CHPL_NIGHTLY_TEST_DIRS="gpu/interop/"
 
