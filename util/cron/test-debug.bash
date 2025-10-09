558c4b574531a0bae10f40e6bbd593fdf2cd3a81
Jade Abraham <jade.abraham@hpe.com>
2025-10-09 09:17:08 -0700

    add standalone debug symbols config
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

---
+++ util/cron/test-debug.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# Test --fast configuration on full suite.
+
+UTIL_CRON_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
+source $UTIL_CRON_DIR/common.bash
+source $UTIL_CRON_DIR/common-localnode-paratest.bash
+
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="debug"
+
+$UTIL_CRON_DIR/nightly -cron -debug-symbols $(get_nightly_paratest_args)
