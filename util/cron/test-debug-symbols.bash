6997ab61c01823117d420f1f9d679761b7da9555
Jade Abraham <jade.abraham@hpe.com>
2025-10-09 10:12:54 -0700

    fix CHPL_NIGHTLY_TEST_CONFIG_NAME
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/cron/test-debug-symbols.bash
+++ util/cron/test-debug-symbols.bash
@@ -6,6 +6,6 @@ UTIL_CRON_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
 source $UTIL_CRON_DIR/common.bash
 source $UTIL_CRON_DIR/common-localnode-paratest.bash
 
-export CHPL_NIGHTLY_TEST_CONFIG_NAME="debug"
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="debug-symbols"
 
 $UTIL_CRON_DIR/nightly -cron -debug-symbols $(get_nightly_paratest_args)
