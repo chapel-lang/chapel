2923d5dea43ec855b1694322d604a6833b48e161
David Longnecker <dlongnecke-cray@users.noreply.github.com>
2025-10-09 15:50:49 -0700

    Start adding code to 'FunctionType' to identify widenable components
    Signed-off-by: David Longnecker <dlongnecke-cray@users.noreply.github.com>

--- compiler/include/pass-manager-passes.h
+++ compiler/include/pass-manager-passes.h
@@ -320,6 +320,16 @@ class StreamlineProcPtrTypesForCodegen : public AdjustSymbolTypes {
   Type* computeAdjustedType(Type* t) const override;
 };
 
+/**
+  This pass adjusts any symbol with a procedure-pointer type so that it is
+  a new, similiar type where all of its widenable components are widened.
+  A widenable component is e.g., a 'ref' formal or a 'class' formal.
+
+  After this pass the AST will be in an inconsistent state, and the pass
+  'InsertWideReferences' pass must be run in order to fix it up.
+
+  TODO: Transform 'IWR' from a on old-style to new-style compiler pass.
+*/
 class WidenComponentsOfProcPtrTypes : public PassT<Symbol*> {
  public:
   bool shouldProcess(Symbol* sym) override;
