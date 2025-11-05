768954b9af396f2f0b07597fdf03d9b7cd41ef48
Jade Abraham <jade.abraham@hpe.com>
2025-11-04 16:00:06 -0800

    add libunwind to portability tests
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/devel/test/portability/provision-scripts/apk-deps.sh
+++ util/devel/test/portability/provision-scripts/apk-deps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # install packages for Alpine Linux
-apk add gcc g++ m4 perl python3 python3-dev bash make gawk git cmake
+apk add gcc g++ m4 perl python3 python3-dev bash make gawk git cmake libunwind-dev
 
