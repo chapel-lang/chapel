4f449133a8dc567129f406742966b4bc333d0821
Ben Harshbarger <ben.harshb@gmail.com>
2025-12-03 11:00:16 -0800

    Remove unused provision scripts
    Signed-off-by: Ben Harshbarger <ben.harshb@gmail.com>

--- util/devel/test/portability/provision-scripts/dnf-llvm-15.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# what version to put here? try e.g.
-# dnf --showduplicates list llvm
-
-# Unclear to me why clang-15 or clang15 does not work
-dnf -y install llvm-devel-15.0.7 clang-15.0.7 clang-devel-15.0.7
