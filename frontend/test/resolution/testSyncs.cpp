a0f8691af61a23b7186cf921b3a617551ef57c67
Danila Fedorin <daniel.fedorin@hpe.com>
2025-12-05 21:33:34 -0800

    Test that syncs properly coerce to their underlying type on copy init
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

---
+++ frontend/test/resolution/testSyncs.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2025 Hewlett Packard Enterprise Development LP
+ * Other additional copyright holders may be indicated within.
+ *
+ * The entirety of this work is licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "test-resolution.h"
+
+static void testCoerceToDesync() {
+  auto context = buildStdContext();
+  ErrorGuard guard(context);
+
+  auto vars = resolveTypesOfVariables(context,
+      R"""(
+      var tmp1: sync int;
+      var tmp2: sync real;
+      var tmp3: sync bool;
+
+      var x = tmp1;
+      var y = tmp2;
+      var z = tmp3;
+      )""", {"x", "y", "z"});
+
+  assert(vars.at("x").type()->isIntType());
+  assert(vars.at("y").type()->isRealType());
+  assert(vars.at("z").type()->isBoolType());
+}
+
+
+int main() {
+  testCoerceToDesync();
+}
