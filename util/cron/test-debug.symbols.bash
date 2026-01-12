0d5b82ce18dbfcb2fbaadfaa540ff90fb2e83944
Jade Abraham <jade.abraham@hpe.com>
2026-01-12 12:56:20 -0800

    add no-local
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/cron/test-debug.symbols.bash
+++ util/cron/test-debug.symbols.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Test --debug-symbols configuration on full suite.
+# Test --debug-symbols configuration on full suite with --no-local.
 
 UTIL_CRON_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
 source $UTIL_CRON_DIR/common.bash
@@ -8,4 +8,4 @@ source $UTIL_CRON_DIR/common-localnode-paratest.bash
 
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="debug.symbols"
 
-$UTIL_CRON_DIR/nightly -cron -debug-symbols $(get_nightly_paratest_args)
+$UTIL_CRON_DIR/nightly -cron -debug-symbols -no-local $(get_nightly_paratest_args)
