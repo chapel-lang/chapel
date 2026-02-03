daf77983e0bbd20068af0bdae1306f94bd170356
Jade Abraham <jade.abraham@hpe.com>
2026-02-03 11:26:00 -0800

    fix homebrew changes
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/packaging/homebrew/chapel-main.rb
+++ util/packaging/homebrew/chapel-main.rb
@@ -18,7 +18,7 @@ class Chapel < Formula
   depends_on "gmp"
   depends_on "hwloc"
   depends_on "jemalloc"
-  depends_on "llvm"
+  depends_on "llvm@21"
   depends_on "pkgconf"
   depends_on "python@3.14"
 
