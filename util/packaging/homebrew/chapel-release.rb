95383d6ca4aa19e4fb1e2132d40333f76d941e96
Jade Abraham <jade.abraham@hpe.com>
2026-02-03 09:32:28 -0800

    more cleanups for LLVM version
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/packaging/homebrew/chapel-release.rb
+++ util/packaging/homebrew/chapel-release.rb
@@ -23,7 +23,7 @@ class Chapel < Formula
   depends_on "gmp"
   depends_on "hwloc"
   depends_on "jemalloc"
-  depends_on "llvm@20"
+  depends_on "llvm@21"
   depends_on "pkgconf"
   depends_on "python@3.14"
 
@@ -178,9 +178,6 @@ class Chapel < Formula
   end
 
   test do
-    # Hide ld warning until formula uses LLVM 21+ or if we apply backports to `llvm@20`
-    ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version.to_s if OS.mac? && MacOS.version >= :tahoe
-
     ENV["CHPL_HOME"] = libexec
     ENV["CHPL_INCLUDE_PATH"] = HOMEBREW_PREFIX/"include"
     ENV["CHPL_LIB_PATH"] = HOMEBREW_PREFIX/"lib"
