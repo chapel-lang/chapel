3d6413978aae5065bc7ed3b3659c61e57e97dc46
Anna Rift <anna.rift@hpe.com>
2026-02-24 11:15:07 -0800

    Run black on doc/
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- doc/util/literate_chapel.py
+++ doc/util/literate_chapel.py
@@ -6,27 +6,31 @@ from __future__ import print_function
 import sys
 import re
 
+
 def title_comment(line):
     """Condition for a line to be a title comment"""
-    return line.startswith('//') and len(line.lstrip('//').strip()) > 0
+    return line.startswith("//") and len(line.lstrip("//").strip()) > 0
+
 
 def metadata_comment(line):
     """Condition for a line to be a front matter comment"""
-    return line.startswith('//') and re.match(r"^\/\/ [a-zA-Z]+:.*$", line)
+    return line.startswith("//") and re.match(r"^\/\/ [a-zA-Z]+:.*$", line)
+
 
 def to_pieces(handle, islearnChapelInYMinutes=False):
     output = []
-    current_type = ''
+    current_type = ""
     current_content = []
 
     def flush():
         nonlocal current_content, current_type
-        if len(current_content) != 0 and any(s != '' for s in current_content):
-            while current_content[0] == '': current_content.pop(0)
-            while current_content[-1] == '': current_content.pop(-1)
+        if len(current_content) != 0 and any(s != "" for s in current_content):
+            while current_content[0] == "":
+                current_content.pop(0)
+            while current_content[-1] == "":
+                current_content.pop(-1)
             output.append((current_type, current_content))
             current_content = []
-
 
     def switch_type(new_type):
         nonlocal current_type
@@ -40,113 +44,115 @@ def to_pieces(handle, islearnChapelInYMinutes=False):
         current_content.append(line)
 
     commentdepth = 0
-    state = ''
+    state = ""
     indentation = -1
     chunk_index = 0
     inFrontMatter = True
     foundCommentExample = False
 
     # Each line is prose or code-block
-    for (i, line) in enumerate([l.strip('\n') for l in handle.readlines()]):
+    for i, line in enumerate([l.strip("\n") for l in handle.readlines()]):
         # Skip title comment if present
         if i == 0 and title_comment(line):
-            switch_type('title')
-            push_line(line.lstrip('//').strip())
-            switch_type('prose')
+            switch_type("title")
+            push_line(line.lstrip("//").strip())
+            switch_type("prose")
             continue
 
         # Collect metadata strings
         if inFrontMatter and metadata_comment(line):
-            switch_type('frontmatter')
-            push_line(line.lstrip('//').strip())
+            switch_type("frontmatter")
+            push_line(line.lstrip("//").strip())
             continue
 
         inFrontMatter = False
 
         # Skip empty lines
         if len(line.strip()) == 0:
-            push_line('')
+            push_line("")
             continue
 
         # Comment canaries - note: we don't support escaped comments: \/*
-        commentstarts = line.count('/*')
-        commentends = line.count('*/')
+        commentstarts = line.count("/*")
+        commentends = line.count("*/")
         commentdepth += commentstarts - commentends
 
         # State tracking
         laststate = state
-        state = ''
+        state = ""
 
         # Identification of line
         if commentdepth > 0 or commentends > 0:
-            state = 'blockcomment'
+            state = "blockcomment"
             if commentstarts > 0:
                 # reset indentation at the start of a block comment
-                #indentation = -1
+                # indentation = -1
                 pass
-        elif line.startswith('//'):
-            state = 'linecomment'
-        elif 'code' in laststate:
-            state = 'code'
+        elif line.startswith("//"):
+            state = "linecomment"
+        elif "code" in laststate:
+            state = "code"
         else:
-            state = 'codeblock'
+            state = "codeblock"
 
-        if 'comment' in state:
+        if "comment" in state:
             proseline = line
-            if state == 'linecomment':
+            if state == "linecomment":
                 # Strip white space for line comments
-                proseline = proseline.replace('//', '  ', 1)
+                proseline = proseline.replace("//", "  ", 1)
                 proseline = proseline.strip()
-                indentation = -1 # indentation should be recomputed for /* */
+                indentation = -1  # indentation should be recomputed for /* */
             else:
                 # Preserve white space for block comments, for indent purposes
                 if commentstarts:
-                    proseline = proseline.replace('/*', '  ')
+                    proseline = proseline.replace("/*", "  ")
                 if commentends > 0:
-                    proseline = proseline.replace('*/', '  ')
+                    proseline = proseline.replace("*/", "  ")
                 # No need for trailing white space... ever
-                proseline = proseline.rstrip(' ')
+                proseline = proseline.rstrip(" ")
 
                 # Handle indentation for block comments
                 if indentation == -1 and proseline.strip() != "":
                     # Detect level of indentation (count leading whitespaces)
-                    baseline = len(proseline) - len(proseline.lstrip(' '))
+                    baseline = len(proseline) - len(proseline.lstrip(" "))
                     if baseline > 0:
                         # Set indentation for this block
                         indentation = baseline
                         # Set indentation for baseline to 0
-                        proseline = proseline.lstrip(' ')
+                        proseline = proseline.lstrip(" ")
                     elif baseline == 0:
                         indentation = 0
                 elif indentation > 0:
                     # Remove the the leading whitespace according
                     # to the indent level computed previously for this block
-                    if proseline.startswith(' '*indentation):
+                    if proseline.startswith(" " * indentation):
                         proseline = proseline[indentation:]
                     else:
-                        proseline = proseline.lstrip(' ')
+                        proseline = proseline.lstrip(" ")
 
             # Special case for multi line comment in learnChapelInYMinutes
-            if islearnChapelInYMinutes and 'multi-line comment' in proseline:
-                push_line(' '*indentation + '/*')
+            if islearnChapelInYMinutes and "multi-line comment" in proseline:
+                push_line(" " * indentation + "/*")
                 push_line(proseline)
-                push_line(' '*indentation + '*/')
+                push_line(" " * indentation + "*/")
                 if foundCommentExample:
-                    print("Error: Found more than one line containing \"multi-line comment\" in \
-                        learnChapelInYMinutes.chpl")
+                    print(
+                        'Error: Found more than one line containing "multi-line comment" in \
+                        learnChapelInYMinutes.chpl'
+                    )
                     sys.exit(1)
                 foundCommentExample = True
             else:
-               switch_type('prose')
-               push_line(proseline)
+                switch_type("prose")
+                push_line(proseline)
         else:
             # Reset indentation as we enter codeblock state
             indentation = -1
-            switch_type('code')
+            switch_type("code")
 
             if "__BREAK__" in line:
                 flush()
-                output.append(('output', chunk_index))
+                output.append(("output", chunk_index))
                 chunk_index += 1
                 continue
 
@@ -163,6 +169,8 @@ def to_pieces(handle, islearnChapelInYMinutes=False):
         pass
 
     if islearnChapelInYMinutes and not foundCommentExample:
-        print('Error: Failed to find special case of comment example in learnChapelInYMinutes.chpl')
+        print(
+            "Error: Failed to find special case of comment example in learnChapelInYMinutes.chpl"
+        )
         sys.exit(1)
     return output
