f3e8cc5c27b9072350e30c2652ee9252c43693ff
Danila Fedorin <daniel.fedorin@hpe.com>
2026-03-05 15:11:58 -0800

    Extract test helper for inlays
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

--- tools/chpl-language-server/test/show_generic.py
+++ tools/chpl-language-server/test/show_generic.py
@@ -10,6 +10,7 @@ from lsprotocol.types import ClientCapabilities
 from lsprotocol.types import InitializeParams
 import pytest
 import pytest_lsp
+import typing
 from pytest_lsp import ClientServerConfig, LanguageClient
 
 from util.utils import *
@@ -65,6 +66,51 @@ async def test_lenses_show(client: LanguageClient):
         await check_generic_code_lenses(client, doc, lenses)
 
 
+EXPECTED_INLAY = tuple[Position, str, typing.Optional[InlayHintKind]]
+EXPECTED_INLAYS = typing.Sequence[EXPECTED_INLAY]
+
+async def click_lenses_and_check_inlays(
+    client: LanguageClient,
+    file: str,
+    expected_lens: tuple[Position, int],
+    all_inlays: typing.Sequence[EXPECTED_INLAYS],
+):
+    async with source_file(client, file) as doc:
+        actual_lenses = await check_generic_code_lenses(
+            client, doc, [expected_lens]
+        )
+        key = depos(expected_lens[0])
+        assert len(actual_lenses.keys()) == 1 and key in actual_lenses
+        lenses = actual_lenses[key]
+
+        # for each lens, apply the command and check the inlays
+        # cycle through this twice to ensure that the lenses are not being
+        # broken by other lenses
+        for _ in range(2):
+            for lens in lenses:
+                # each lens should have a command. The 3rd argument to the command is the index of the inlays to check
+                # apply the command, and then check the inlays
+                command = lens.command
+                assert command is not None
+                if command.title == "Show Generic":
+                    idx = 0
+                else:
+                    assert (
+                        command.arguments is not None
+                        and len(command.arguments) == 3
+                    )
+                    idx = command.arguments[2]
+                    assert idx is not None
+                    idx = int(idx) + 1
+                # apply the command and get the inlays to check
+                await execute_command(client, command)
+                inlays = all_inlays[idx]
+
+                # check the inlays
+                await check_inlay_hints(
+                    client, doc, rng((0, 0), endpos(file)), inlays
+                )
+
 @pytest.mark.asyncio
 async def test_lenses_switch(client: LanguageClient):
     """
@@ -119,38 +165,4 @@ async def test_lenses_switch(client: LanguageClient):
         int_inlays,
     ]
 
-    async with source_file(client, file) as doc:
-        actual_lenses = await check_generic_code_lenses(
-            client, doc, [expected_lens]
-        )
-        key = depos(expected_lens[0])
-        assert len(actual_lenses.keys()) == 1 and key in actual_lenses
-        lenses = actual_lenses[key]
-
-        # for each lens, apply the command and check the inlays
-        # cycle through this twice to ensure that the lenses are not being
-        # broken by other lenses
-        for _ in range(2):
-            for lens in lenses:
-                # each lens should have a command. The 3rd argument to the command is the index of the inlays to check
-                # apply the command, and then check the inlays
-                command = lens.command
-                assert command is not None
-                if command.title == "Show Generic":
-                    idx = 0
-                else:
-                    assert (
-                        command.arguments is not None
-                        and len(command.arguments) == 3
-                    )
-                    idx = command.arguments[2]
-                    assert idx is not None
-                    idx = int(idx) + 1
-                # apply the command and get the inlays to check
-                await execute_command(client, command)
-                inlays = all_inlays[idx]
-
-                # check the inlays
-                await check_inlay_hints(
-                    client, doc, rng((0, 0), endpos(file)), inlays
-                )
+    await click_lenses_and_check_inlays(client, file, expected_lens, all_inlays)
