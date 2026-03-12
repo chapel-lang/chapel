584ad90c98e73273a0360cc835bc64d0ba95b472
Anna Rift <anna.rift@hpe.com>
2026-03-12 11:07:37 -0700

    Use one job per core for remainder of build
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/cron/test-make-install-check.bash
+++ util/cron/test-make-install-check.bash
@@ -9,8 +9,5 @@ source $UTIL_CRON_DIR/common.bash
 
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="make-install-check"
 
-# Limit build jobs to avoid OOM, as this does a build of bundled LLVM.
-export CHPL_MAKE_MAX_CPU_COUNT=2
-
 source $CHPL_HOME/util/setchplenv.sh
 $UTIL_CRON_DIR/../buildRelease/test_install.bash
