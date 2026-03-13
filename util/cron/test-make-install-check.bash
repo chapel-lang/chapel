06c58ef846316f96da1ddf8737cf42ac832630a4
Anna Rift <anna.rift@hpe.com>
2026-03-13 10:24:26 -0700

    Bump up to 4 cores
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/cron/test-make-install-check.bash
+++ util/cron/test-make-install-check.bash
@@ -10,7 +10,7 @@ source $UTIL_CRON_DIR/common.bash
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="make-install-check"
 
 # Limit build jobs to avoid OOM, as this does a build of bundled LLVM.
-export CHPL_MAKE_MAX_CPU_COUNT=2
+export CHPL_MAKE_MAX_CPU_COUNT=4
 
 source $CHPL_HOME/util/setchplenv.sh
 $UTIL_CRON_DIR/../buildRelease/test_install.bash
