48c9528fdc8967e403bce375b26917a33d35ac95
David Longnecker <dlongnecke-cray@users.noreply.github.com>
2026-04-23 17:56:10 -0700

    Set chapcs playground to track chapel-lang/chapel@main
    Signed-off-by: David Longnecker <dlongnecke-cray@users.noreply.github.com>

--- util/cron/test-perf.chapcs.playground.bash
+++ util/cron/test-perf.chapcs.playground.bash
@@ -34,10 +34,10 @@ export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.chapcs.playground"
 #
 # E.g., 'CHPL_COMM' is replaced with 'program->data.CHPL_COMM'.
 
-GITHUB_USER=insertinterestingnamehere
-GITHUB_BRANCH=some-branch-1.2.3
-SHORT_NAME=somebranch123
-START_DATE=4/22/2026
+GITHUB_USER=chapel-lang
+GITHUB_BRANCH=main
+SHORT_NAME=unset
+START_DATE=4/23/2026
 
 set -e
 checkout_branch $GITHUB_USER $GITHUB_BRANCH
