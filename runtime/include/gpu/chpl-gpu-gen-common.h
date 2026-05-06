066dbd7a16bf89c9e9fcc2303e566990f1edeac0
Jade Abraham <jade.abraham@hpe.com>
2026-05-06 10:29:28 -0700

    use __attribute__
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- runtime/include/gpu/chpl-gpu-gen-common.h
+++ runtime/include/gpu/chpl-gpu-gen-common.h
@@ -107,49 +107,49 @@ MAYBE_GPU static inline void chpl_gpu_write(const char *str) { printf("%s", str)
 MAYBE_GPU static inline void chpl_gpu_printf0(const char *fmt) {
   printf("%s", fmt);
 }
-[[gnu::format(printf, 1, 2)]]
+__attribute__ ((format (printf, 1, 2)))
 MAYBE_GPU static inline void chpl_gpu_printf1(const char *fmt,
  void *x1)
 {
   printf(fmt, x1);
 }
-[[gnu::format(printf, 1, 2)]]
+__attribute__ ((format (printf, 1, 2)))
 MAYBE_GPU static inline void chpl_gpu_printf2(const char *fmt,
   void *x1, void *x2)
 {
   printf(fmt, x1, x2);
 }
-[[gnu::format(printf, 1, 2)]]
+__attribute__ ((format (printf, 1, 2)))
 MAYBE_GPU static inline void chpl_gpu_printf3(const char *fmt,
   void *x1, void *x2, void *x3)
 {
   printf(fmt, x1, x2, x3);
 }
-[[gnu::format(printf, 1, 2)]]
+__attribute__ ((format (printf, 1, 2)))
 MAYBE_GPU static inline void chpl_gpu_printf4(const char *fmt,
   void *x1, void *x2, void *x3, void *x4)
 {
   printf(fmt, x1, x2, x3, x4);
 }
-[[gnu::format(printf, 1, 2)]]
+__attribute__ ((format (printf, 1, 2)))
 MAYBE_GPU static inline void chpl_gpu_printf5(const char *fmt,
   void *x1, void *x2, void *x3, void *x4, void *x5)
 {
   printf(fmt, x1, x2, x3, x4, x5);
 }
-[[gnu::format(printf, 1, 2)]]
+__attribute__ ((format (printf, 1, 2)))
 MAYBE_GPU static inline void chpl_gpu_printf6(const char *fmt,
   void *x1, void *x2, void *x3, void *x4, void *x5, void *x6)
 {
   printf(fmt, x1, x2, x3, x4, x5, x6);
 }
-[[gnu::format(printf, 1, 2)]]
+__attribute__ ((format (printf, 1, 2)))
 MAYBE_GPU static inline void chpl_gpu_printf7(const char *fmt,
   void *x1, void *x2, void *x3, void *x4, void *x5, void *x6, void *x7)
 {
   printf(fmt, x1, x2, x3, x4, x5, x6, x7);
 }
-[[gnu::format(printf, 1, 2)]]
+__attribute__ ((format (printf, 1, 2)))
 MAYBE_GPU static inline void chpl_gpu_printf8(const char *fmt,
   void *x1, void *x2, void *x3, void *x4, void *x5, void *x6, void *x7,
   void *x8)
