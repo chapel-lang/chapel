4a794eba60868721825f4dc99718af87923db012
Danila Fedorin <daniel.fedorin@hpe.com>
2026-05-07 15:17:28 -0700

    Do not insert temporaries for out-formals just like for inout-formals
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

--- compiler/resolution/wrappers.cpp
+++ compiler/resolution/wrappers.cpp
@@ -2062,10 +2062,15 @@ static void handleOutIntents(FnSymbol* fn, CallExpr* call,
     SET_LINENO(currActual);
     nextActual = currActual->next;
 
-    bool out = formal->intent == INTENT_OUT ||
-               formal->originalIntent == INTENT_OUT;
-    bool inout = formal->intent == INTENT_INOUT ||
-                 formal->originalIntent == INTENT_INOUT;
+    // Promotion wrappers already model written formals with ref wrapper
+    // formals, so only add a write-back for out/inout formals in non-promotion
+    // wrappers.
+    bool out = (formal->intent == INTENT_OUT ||
+                formal->originalIntent == INTENT_OUT) &&
+               fn->hasFlag(FLAG_PROMOTION_WRAPPER) == false;
+    bool inout = (formal->intent == INTENT_INOUT ||
+                  formal->originalIntent == INTENT_INOUT) &&
+                 fn->hasFlag(FLAG_PROMOTION_WRAPPER) == false;
 
     if (out || inout) {
       Expr* useExpr = currActual;
