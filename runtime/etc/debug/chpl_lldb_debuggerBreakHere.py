794368e5ab32a124e3c5e11cde754b8946942c1c
Jade Abraham <jade.abraham@hpe.com>
2026-05-15 09:18:15 -0700

    use stop-hooks to implement the auto up feature of debuggerBreakHere instead of debugger commands, since in newer LLDB versions the changes made to the current state are not reflected
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- runtime/etc/debug/chpl_lldb_debuggerBreakHere.py
+++ runtime/etc/debug/chpl_lldb_debuggerBreakHere.py
@@ -1,15 +1,23 @@
-def debuggerBreakHere_callback(frame, bp_loc, internal_dict):
-    thread = frame.GetThread()
-    thread.SetSelectedFrame(1)
+class DebuggerBreakHereStopHook:
+    def __init__(self, target, extra_args, internal_dict):
+        pass
 
-    curFrame = thread.GetSelectedFrame()
-    fn = curFrame.GetFunction()
-    if fn and fn.name.startswith("debuggerBreakHere("):
-        thread.SetSelectedFrame(2)
+    def handle_stop(self, exe_ctx, stream):
+        thread = exe_ctx.GetThread()
+        if not thread or not thread.IsValid():
+            return True
+
+        thread.SetSelectedFrame(1)
+
+        curFrame = thread.GetSelectedFrame()
+        fn = curFrame.GetFunction()
+        if fn and fn.name.startswith("debuggerBreakHere("):
+            thread.SetSelectedFrame(2)
+        return True
 
 
 def __lldb_init_module(debugger, internal_dict):
-    debugger.HandleCommand("breakpoint command delete debuggerBreakHere")
+    debugger.HandleCommand("target stop-hook delete 1")
     debugger.HandleCommand(
-        "breakpoint command add --python-function chpl_lldb_debuggerBreakHere.debuggerBreakHere_callback debuggerBreakHere"
+        "target stop-hook add --script-class chpl_lldb_debuggerBreakHere.DebuggerBreakHereStopHook"
     )
