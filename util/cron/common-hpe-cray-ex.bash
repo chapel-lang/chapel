a869bfd5c259b7d6b0db855baa0a12de1ea9522c
Brad Chamberlain <bradcray@users.noreply.github.com>
2026-05-18 15:03:28 -0700

    Unset CHPL_TARGET_CPU for EX testing
    CHPL_TARGET_CPU has been set to 'none' in testing through common.bash
    for years now, but as of the LLVM 22 upgrade, we're seeing some linker
    errors on GMP routines as a result of this.  At the same time, leaving
    it unset is more similar to what real users will do in the field, and
    on EX systems, we should default it to reasonable values, so update
    the EX testing to unset this variable and do the testing in that mode.
    
    ---
    Signed-off-by: Brad Chamberlain <bradcray@users.noreply.github.com>

--- util/cron/common-hpe-cray-ex.bash
+++ util/cron/common-hpe-cray-ex.bash
@@ -15,6 +15,10 @@ module load cray-pmi
 
 export CHPL_HOST_PLATFORM=hpe-cray-ex
 
+# `common.bash` sets CHPL_TARGET_CPU to 'none' but we'd prefer to test
+# the CPU defined by the environment on the EX
+unset CHPL_TARGET_CPU
+
 # Work around cxi provider bugs that limit memory registration
 export CHPL_RT_MAX_HEAP_SIZE="50%"
 export CHPL_LAUNCHER_MEM=unset
