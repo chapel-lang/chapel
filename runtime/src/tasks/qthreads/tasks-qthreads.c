ab75ad1f4bd81986fb53927d4b5743ce000b8e07
David Longnecker <dlongnecke-cray@users.noreply.github.com>
2026-05-21 16:55:10 -0700

    Move another use of 'CHPL_LOCALE_MODEL' into assert
    Signed-off-by: David Longnecker <dlongnecke-cray@users.noreply.github.com>

--- runtime/src/tasks/qthreads/tasks-qthreads.c
+++ runtime/src/tasks/qthreads/tasks-qthreads.c
@@ -1003,13 +1003,12 @@ void chpl_task_addTask(chpl_fn_int_t       fid,
                        int32_t             filename)
 {
     chpl_fn_p requested_fn = chpl_ftable[fid];
-    CHPL_RT_PRGINFO_DECLARE(CHPL_RT_ROOT_PROGRAM_PLACEHOLDER,
-                            CHPL_LOCALE_MODEL);
 
     // We allow using c_sublocid_none to represent the CPU in the gpu locale
     // model. This isn't currently used by the numa (or other locale) models.
     assert(isActualSublocID(full_subloc) || full_subloc == c_sublocid_none ||
-        !strcmp(CHPL_LOCALE_MODEL, "gpu"));
+        !strcmp(CHPL_RT_PRGINFO_DATA(CHPL_RT_ROOT_PROGRAM_PLACEHOLDER,
+                                     CHPL_LOCALE_MODEL), "gpu"));
 
     PROFILE_INCR(profile_task_addTask,1);
 
