1a69ce1089f425acdb351ff5ff95c5266fbb061c
David Longnecker <dlongnecke-cray@users.noreply.github.com>
2026-05-21 13:17:05 -0700

    Remove unused symbol 'CHPL_ATOMICS'
    Signed-off-by: David Longnecker <dlongnecke-cray@users.noreply.github.com>

--- runtime/include/chplcgfns.h
+++ runtime/include/chplcgfns.h
@@ -46,7 +46,6 @@ extern const char* CHPL_COMM;
 extern const char* CHPL_GASNET_SEGMENT;
 extern const char* CHPL_LAUNCHER;
 extern const char* CHPL_TARGET_MEM;
-extern const char* CHPL_ATOMICS;
 extern const char* CHPL_LLVM;
 extern const char* CHPL_LLVM_BIN_DIR;
 extern const char* CHPL_UNWIND;
