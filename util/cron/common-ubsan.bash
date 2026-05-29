af986fc737d3617c756ecdb8ea5de42990fb8755
Jade Abraham <jade.abraham@hpe.com>
2026-05-29 11:46:08 -0700

    remove env
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/cron/common-ubsan.bash
+++ util/cron/common-ubsan.bash
@@ -7,4 +7,3 @@ UTIL_CRON_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
 export CHPL_TARGET_MEM=cstdlib
 export CHPL_HOST_MEM=cstdlib
 export CHPL_SANITIZE=undefined
-export CHPL_RT_NUM_THREADS_PER_LOCALE_QUIET=yes
