e2c107e0e4aeb0d6df39758d7dcb009d656657e5
Anna Rift <anna.rift@hpe.com>
2026-07-24 13:41:19 -0700

    Break
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- compiler/llvm/clangUtil.cpp
+++ compiler/llvm/clangUtil.cpp
@@ -62,11 +62,7 @@
 #include "llvm/Linker/Linker.h"
 #include "llvm/MC/MCSubtargetInfo.h"
 
-#if HAVE_LLVM_VER >= 170
-#include "llvm/TargetParser/SubtargetFeature.h"
-#else
 #include "llvm/MC/SubtargetFeature.h"
-#endif
 
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/StandardInstrumentations.h"
