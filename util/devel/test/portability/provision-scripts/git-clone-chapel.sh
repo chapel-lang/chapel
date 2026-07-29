3973912f7581adbe96eb1e6a1757283a2656ac51
Anna Rift <anna.rift@hpe.com>
2026-07-29 13:10:36 -0700

    Work from correct git repo and commit
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/devel/test/portability/provision-scripts/git-clone-chapel.sh
+++ util/devel/test/portability/provision-scripts/git-clone-chapel.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+set -exuo pipefail
+
 if [ -d chapel ]
 then
   echo WARNING: chapel directory already exists, not cloning
 else
   GIT_BRANCH=${GIT_BRANCH:-main}
   GIT_REPO_URL=${GIT_REPO_URL:-https://github.com/chapel-lang/chapel}
+  GIT_COMMIT=${GIT_COMMIT:-HEAD}
   git clone --reference-if-able "${REPO_CACHE_PATH:-/missing}/chapel.git" --depth 1 --branch="$GIT_BRANCH" "$GIT_REPO_URL"
+  git reset --hard "$GIT_COMMIT"
 fi
