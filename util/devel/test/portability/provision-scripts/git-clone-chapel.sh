02c1524cc2e31132846f43d8f4b0d213e189e82b
Anna Rift <anna.rift@hpe.com>
2026-07-29 14:29:19 -0700

    Only pass --branch to clone if GIT_BRANCH is set
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/devel/test/portability/provision-scripts/git-clone-chapel.sh
+++ util/devel/test/portability/provision-scripts/git-clone-chapel.sh
@@ -6,9 +6,9 @@ if [ -d chapel ]
 then
   echo WARNING: chapel directory already exists, not cloning
 else
-  GIT_BRANCH=${GIT_BRANCH:-main}
+  GIT_BRANCH_ARG=${GIT_BRANCH:+"--branch $GIT_BRANCH"}
   GIT_REPO_URL=${GIT_REPO_URL:-https://github.com/chapel-lang/chapel}
   GIT_COMMIT=${GIT_COMMIT:-HEAD}
-  git clone --reference-if-able "${REPO_CACHE_PATH:-/missing}/chapel.git" --depth 1 --branch="$GIT_BRANCH" "$GIT_REPO_URL"
+  git clone --reference-if-able "${REPO_CACHE_PATH:-/missing}/chapel.git" --depth 1 "$GIT_BRANCH_ARG" "$GIT_REPO_URL"
   git reset --hard "$GIT_COMMIT"
 fi
