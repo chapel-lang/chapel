3a4e26ab70693272b9d04efd466a7fcac11e54d0
Anna Rift <anna.rift@hpe.com>
2026-07-30 14:49:19 -0700

    Fix cloning inside VM
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/devel/test/portability/provision-scripts/git-clone-chapel.sh
+++ util/devel/test/portability/provision-scripts/git-clone-chapel.sh
@@ -7,13 +7,13 @@ then
   echo WARNING: chapel directory already exists, not cloning
 else
   GIT_REPO_URL=${GIT_REPO_URL:-https://github.com/chapel-lang/chapel}
-  GIT_COMMIT=${GIT_COMMIT:-HEAD}
 
   git_cmd=(git clone --reference-if-able "${REPO_CACHE_PATH:-/missing}/chapel.git" --depth 1 "$GIT_REPO_URL")
   if [ -n "${GIT_BRANCH-}" ]; then
     git_cmd+=(--branch "$GIT_BRANCH")
   fi
+  if [ -n "${GIT_REVISION-}" ]; then
+    git_cmd+=(--revision "$GIT_REVISION")
+  fi
   "${git_cmd[@]}"
-
-  pushd chapel && git reset --hard "$GIT_COMMIT" && popd
 fi
