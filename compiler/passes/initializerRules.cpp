0ae4a05b66906ed7b525e00273fe466f4da0cb20
Brad Chamberlain <bradcray@users.noreply.github.com>
2026-08-11 17:36:01 -0700

    Make unions treated more like records
    While working on debugging why union copy initializers weren't called,
    I started looking for other ways in which records and unions were
    treated differently and came up with the following changes, which seem
    sensible to me.  These are currently passing testing, so I'm taking
    this snapshot and then will look at whether more tests could be added
    to lock these behaviors in.
    
    ---
    Signed-off-by: Brad Chamberlain <bradcray@users.noreply.github.com>

--- compiler/passes/initializerRules.cpp
+++ compiler/passes/initializerRules.cpp
@@ -160,7 +160,7 @@ static AggregateType* typeForNewExprHelper(CallExpr* newExpr, int type_idx) {
     if (SymExpr* baseExpr = toSymExpr(constructor->baseExpr)) {
       if (TypeSymbol* sym = toTypeSymbol(baseExpr->symbol())) {
         if (AggregateType* type = toAggregateType(sym->type)) {
-          if (isClass(type) == true || isRecord(type) == true) {
+          if (isClass(type) == true || isRecord(type) == true) { // TODO: Or union?
             retval = type;
           }
         }
@@ -519,8 +519,8 @@ static InitNormalize preNormalize(AggregateType* at,
           }
           state.completePhase0(callExpr);
 
-          if (at->isRecord() == true) {
-            USR_FATAL_CONT(stmt, "super.init() not allowed in records");
+          if (at->isRecord() == true || at->isUnion() == true) {
+            USR_FATAL_CONT(stmt, "super.init() is only allowed in classes");
             callExpr->remove();
 
           } else if (at->symbol->hasFlag(FLAG_EXTERN) == true) {
@@ -1543,7 +1543,7 @@ void preNormalizePostInit(AggregateType* at) {
     }
   }
 
-  if (isRecord(at) && at->hasPostInitializer()) {
+  if ((isRecord(at) || isUnion(at)) && at->hasPostInitializer()) {
     at->symbol->addFlag(FLAG_NOT_POD);
   }
 
