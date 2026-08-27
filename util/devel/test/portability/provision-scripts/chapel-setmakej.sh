241e1ce8d6a5fa14e5a4cbdf226597527efb9847
Anna Rift <anna.rift@hpe.com>
2026-08-27 15:24:48 -0700

    Use -j4 and increase VM cores + RAM accordingly
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/devel/test/portability/provision-scripts/chapel-setmakej.sh
+++ util/devel/test/portability/provision-scripts/chapel-setmakej.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export MAKEJ=${MAKEJ:=-j 2}
+export MAKEJ=${MAKEJ:=-j 4}
 
 # could use $(chapel/util/buildRelease/chpl-make-cpu_count)}
 # but that doesn't currently incorporate the amount of RAM per thread
