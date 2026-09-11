4c56c89f094cdd594be6bce5ef179f7e23a63e70
Jade Abraham <jade.abraham@hpe.com>
2026-09-11 09:44:36 -0700

    format
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- tools/chplcheck/src/rules.py
+++ tools/chplcheck/src/rules.py
@@ -269,7 +269,6 @@ def rules(driver: LintDriver):
         internal_prefixes = driver.config.internal_prefixes
         return check_camel_case(context, node, internal_prefixes)
 
-
     @driver.basic_rule(Module, default=False)
     def UseExplicitModules(_, node: Module):
         """
